c8cc8485171758d4a24f404666e61d8c50e83385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:11 +0000

    Add package gmail2json/0.1/gmail2json-0.1.tar.gz

---
+++ packages/gmail2json/gmail2json-0.1.tar.gz/gmail2json-0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="gmail2json",
+    version="0.1",
+    author="Hudge",
+    author_email="",
+    description="",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="",
+    py_modules=["gmail2json"],
+    install_requires=[""],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
