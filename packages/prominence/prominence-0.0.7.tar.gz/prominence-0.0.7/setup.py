a50d41fa6609cad6e52e6383bb0d8f350be592df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:28 +0000

    Add package prominence/0.0.7/prominence-0.0.7.tar.gz

---
+++ packages/prominence/prominence-0.0.7.tar.gz/prominence-0.0.7/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="prominence",
+    version="0.0.7",
+    author="Andrew Lahiff",
+    author_email="andrew.lahiff@ukaea.uk",
+    description="PROMINENCE CLI for managing batch jobs running across clouds",
+    url="https://alahiff.github.io/prominence/",
+    platforms=["any"],
+    install_requires=["requests"],
+    package_dir={'': '.'},
+    scripts=["bin/prominence"],
+    packages=['prominence'],
+    package_data={"": ["README.md"]},
+)
