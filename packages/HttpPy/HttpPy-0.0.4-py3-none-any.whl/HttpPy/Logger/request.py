6e9958f340b5b831f4546b9358fd0ee080e231bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:00 +0000

    Add package HttpPy/0.0.4/HttpPy-0.0.4-py3-none-any.whl

---
+++ packages/HttpPy/HttpPy-0.0.4-py3-none-any.whl/HttpPy/Logger/request.py
@@ -0,0 +1,47 @@
+from .generics import log_success, log_main
+
+def log_request(req, verbose: bool=False) -> None:
+    log_data = []
+    url = req.url
+    method = req.method
+    log_data.append("=====> ")
+    log_data.append("Making {} request to: {} \n".format(method, url))
+
+    if verbose:
+        headers = req.headers
+        body = req.body.decode("utf-8")
+        title = req.title
+
+        if title is not None:
+            log_data.insert(0,"\n{} \n".format("-"*8) )
+            log_data.insert(1, "{}{} \n".format("-"*8, title.upper()))
+            log_data.insert(2,"{} \n".format("-"*8) )
+
+        if headers is not None:
+            log_data.append("HEADERS: {}\n".format(str(headers)))
+        if body is not None:
+            log_data.append("BODY: {}\n".format(str(body)))
+
+    log_main("".join(log_data))
+
+def log_request_response(response, verbose=False):
+    log_data = []
+
+    if not verbose:
+        log_data.append("<===== ")
+        log_data.append("{}\n".format(response.status_code))
+        log_success("".join(log_data))
+    else:
+        __print_request_part("[{}] {}".format(response.status_code, response.url), "response")
+        __print_request_part(response.headers, "headers")
+        __print_request_part(response.text, "body")
+
+
+def __print_request_part(data, part):
+    __print_request_part_header(part)
+    print(data)
+
+def __print_request_part_header(part="extra"):
+    log_success("-"*30, end="")
+    log_success(" {} ".format(part.upper()), end="")
+    log_success("-"*30, flush=True)
---
+++ packages/HttpPy/HttpPy-0.0.4-py3-none-any.whl/HttpPy/Request.py
@@ -0,0 +1,34 @@
+# Python
+import requests
+from threading import Lock
+
+# Modules
+from .Logger import log_request, log_request_response, log_error, log_warning
+
+lock_print = Lock()
+
+def make_request(req):
+    res = None
+    type = req.get("type")
+    url = req.get("url")
+    headers = req.get("headers")
+    body = req.get("body")
+    verbose = req.get("verbose")
+
+    req_fn = getattr(requests, type, None)
+    if req_fn is None:
+        log_warning("[!] Method {} is not valid".format(type))
+    else:
+
+        try:
+            if type in ["get", "delete"]:
+                res = req_fn(url, headers=headers)
+            else:
+                res = req_fn(url, json=body, headers=headers)
+            with lock_print:
+                res.request.title = req.get("title")
+                log_request(res.request, verbose)
+                log_request_response(res, verbose)
+        except requests.exceptions.ConnectionError as error:
+            log_error("[X] Connection Error")
+            log_error(error)
