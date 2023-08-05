027a28a38882a22df5f34a312c70142f507fc533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:47 +0000

    Add package myhelloworldxyz/0.2/myhelloworldxyz-0.2.tar.gz

---
+++ packages/myhelloworldxyz/myhelloworldxyz-0.2.tar.gz/myhelloworldxyz-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='myhelloworldxyz',
+      version='0.2',
+      description='My Hello World',
+      author='ai-linh',
+      author_email='ailinh.le@gmail.com',
+      license='MIT',
+      packages=['myhelloworldxyz'],
+      install_requires=[
+          'markdown',
+      ],
+      zip_safe=False)
