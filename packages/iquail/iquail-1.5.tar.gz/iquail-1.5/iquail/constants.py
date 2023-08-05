22733caead1bfd25cad9d5ef84143347238cbbc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:03 +0000

    Add package iquail/1.5/iquail-1.5.tar.gz

---
+++ packages/iquail/iquail-1.5.tar.gz/iquail-1.5/iquail/constants.py
@@ -0,0 +1,11 @@
+
+class Constants:
+    IQUAIL_ROOT_NAME = ".iquail"
+    ARGUMENT_UNINSTALL = "--iquail_uninstall"
+    ARGUMENT_BUILD = "--iquail_build"
+    ARGUMENT_RM = "--iquail_rm"
+    CHECKSUMS_FILE = ".integrity.json"
+    INTEGRITY_IGNORE_FILE = ".integrity_ignore"
+    VERSION_FILE = "solution_version.txt"
+    CONFIG_FILE = "config.ini"
+    IQUAIL_LAUNCHER_NAME = "iquail_launcher"
