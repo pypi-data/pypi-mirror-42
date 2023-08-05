eafc6fe85bf88867d6902b76abc3a7bd21058513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:32 +0000

    Add package interrogatio/1.0.0b2/interrogatio-1.0.0b2-py3-none-any.whl

---
+++ packages/interrogatio/interrogatio-1.0.0b2-py3-none-any.whl/interrogatio/__init__.py
@@ -0,0 +1,12 @@
+from .core import interrogatio
+from .utils.constants import InputMode
+from .utils.registries import (get_input_handlers_registry,
+                               get_validators_registry)
+
+__version__ = '1.0.0b2'
+
+__version_info__ = tuple([int(num) if num.isdigit() else num for num in __version__.replace('-', '.', 1).split('.')])
+
+
+def get_version():
+    return __version__
