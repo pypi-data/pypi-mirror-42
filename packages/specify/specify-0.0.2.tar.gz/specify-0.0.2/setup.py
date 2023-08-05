6c462b076c330fb11e5a6256e9af01ced5a87d9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:27 +0000

    Add package specify/0.0.2/specify-0.0.2.tar.gz

---
+++ packages/specify/specify-0.0.2.tar.gz/specify-0.0.2/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['specify']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['prophepy>=0.0.1']
+
+setup_kwargs = {
+    'name': 'specify',
+    'version': '0.0.2',
+    'description': 'A spec tool to describe your classes',
+    'long_description': None,
+    'author': 'Einenlum',
+    'author_email': 'yann.rabiller@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
