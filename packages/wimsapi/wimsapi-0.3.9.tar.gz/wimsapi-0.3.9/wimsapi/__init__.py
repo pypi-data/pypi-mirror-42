6b38b25d1f8c4bc561d550167c119774332de3bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:56 +0000

    Add package wimsapi/0.3.9/wimsapi-0.3.9.tar.gz

---
+++ packages/wimsapi/wimsapi-0.3.9.tar.gz/wimsapi-0.3.9/wimsapi/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from .api import WimsAPI
+from .user import User
+from .wclass import Class
+from .exceptions import AdmRawError, NotSavedError, WimsAPIError, InvalidItemTypeError
+
+
+name = "wimsapi"
+__title__ = 'wimsapi'
+__version__ = VERSION = '0.3.9'
