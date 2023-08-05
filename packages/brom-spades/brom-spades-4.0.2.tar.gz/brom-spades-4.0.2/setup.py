5b7a4e29a80f38e1d0e88818903f031719d3b1c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:34 +0000

    Add package brom-spades/4.0.2/brom-spades-4.0.2.tar.gz

---
+++ packages/brom-spades/brom-spades-4.0.2.tar.gz/brom-spades-4.0.2/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name='brom-spades',
+      version='4.0.2',
+      description='Scoring agent for Spades',
+      author='Ralph Embree',
+      author_email='ralph.embree@brominator.org',
+      url='https://gitlab.com/ralphembree/brom-spades',
+      scripts=['bin/brom-spades'],
+      py_modules=["brom_spades"],
+      keywords="brom-spades spades score scoring",
+)
