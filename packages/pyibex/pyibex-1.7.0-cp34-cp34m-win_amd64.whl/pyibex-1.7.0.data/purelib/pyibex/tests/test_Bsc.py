586feab309bc8116baacfc47734c5dbc00edb3da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:53 +0000

    Add package pyibex/1.7.0/pyibex-1.7.0-cp34-cp34m-win_amd64.whl

---
+++ packages/pyibex/pyibex-1.7.0-cp34-cp34m-win_amd64.whl/pyibex-1.7.0.data/purelib/pyibex/tests/test_Bsc.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#============================================================================
+#                                P Y I B E X
+# File        : test_Bsc.py
+# Author      : Benoit Desrochers
+# Copyright   : Benoit Desrochers
+# License     : See the LICENSE file
+# Created     : Dec 28, 2014
+#============================================================================
+
+import unittest
+import pyibex
+from pyibex import *
+
+class TestBsc(unittest.TestCase):
+    def test_LargestFirst(self):
+
+        bsc = LargestFirst(0.1)
+        a = IntervalVector([[1,2], [2,6]])
+        (c,b) = bsc.bisect(a)
+        self.assertEqual(c, IntervalVector([[1,2],[2,3.8]]))
+        self.assertEqual(b, IntervalVector([[1,2],[3.8,6]]))
+        self.assertTrue(True)
+
+    def test_LargestFirst2(self):
+        bsc = LargestFirst([0.1, 1])
+        a = IntervalVector([[1,2], [2,6]])
+        (c,b) = bsc.bisect(a)
+        self.assertEqual(c, IntervalVector([[1,1.45],[2,6]]))
+        self.assertEqual(b, IntervalVector([[1.45,2],[2,6]]))
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+
+    unittest.main()
