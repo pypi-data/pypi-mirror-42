76f0c8e1e7087491ca2025652578f0d486282ab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:04 +0000

    Add package deepaugment/0.12.1/deepaugment-0.12.1.tar.gz

---
+++ packages/deepaugment/deepaugment-0.12.1.tar.gz/deepaugment-0.12.1/deepaugment/run.py
@@ -0,0 +1,27 @@
+import sys
+from os.path import dirname, realpath
+
+file_path = realpath(__file__)
+dir_of_file = dirname(file_path)
+sys.path.insert(0, dir_of_file)
+
+from deepaugment import DeepAugment
+from build_features import DataOp
+
+deepaug = DeepAugment("cifar10", config={"child_epochs": 1, "model":"wrn_40_2"})
+
+best_policies = deepaug.optimize(1)
+
+
+
+X, y, input_shape = DataOp.load("cifar10")
+
+
+train_size = int(len(X)*0.9)
+
+data = DataOp.preprocess(X, y, train_size)
+
+
+imgen = deepaug.image_generator_with_top_policies(data["X_train"], data["y_train"])
+
+print("End")
