00f2cd4556a838eca1cedb32e7fff5aeb166a8f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:42 +0000

    Add package holidays-jp/1.0.2/holidays-jp-1.0.2.tar.gz

---
+++ packages/holidays-jp/holidays-jp-1.0.2.tar.gz/holidays-jp-1.0.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
+long_description = f.read()
+f.close()
+
+setup(
+    name='holidays-jp',
+    version='1.0.2',
+    description='Calculate the Japanese holidays since 1948.',
+    long_description=long_description,
+    author='mo.kejp',
+    url='https://github.com/mokejp/holidays_jp',
+    packages=['holidays_jp', 'holidays_jp.countries'],
+    install_requires=[
+        'python-dateutil',
+    ],
+    license='MIT License',
+    zip_safe=True,
+    keywords=[
+        'holidays_jp',
+        'holidays-jp',
+        'holiday',
+        'japanese',
+        'japan',
+    ],
+    classifiers=(
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7'
+    ),
+    test_suite="holidays_jp.tests",
+)
