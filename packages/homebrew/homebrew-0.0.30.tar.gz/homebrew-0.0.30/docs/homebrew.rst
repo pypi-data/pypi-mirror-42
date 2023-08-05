90cb5a7896cc253e18a8ce128cc2c7dea3c591f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:27 +0000

    Add package homebrew/0.0.30/homebrew-0.0.30.tar.gz

---
+++ packages/homebrew/homebrew-0.0.30.tar.gz/homebrew-0.0.30/docs/homebrew.rst
@@ -0,0 +1,38 @@
+homebrew package
+================
+
+Submodules
+----------
+
+homebrew.command\_line module
+-----------------------------
+
+.. automodule:: homebrew.command_line
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+homebrew.homebrew module
+------------------------
+
+.. automodule:: homebrew.homebrew
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+homebrew.logger module
+----------------------
+
+.. automodule:: homebrew.logger
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+
+Module contents
+---------------
+
+.. automodule:: homebrew
+    :members:
+    :undoc-members:
+    :show-inheritance:
---
+++ packages/homebrew/homebrew-0.0.30.tar.gz/homebrew-0.0.30/tests/test_homebrew.py
@@ -0,0 +1,44 @@
+from homebrew import HomeBrew
+
+
+def test_homebrew(monkeypatch, caplog):
+    def _fake_get_uses(self):
+        return {"foo": ["bar"], "bar": []}
+
+    monkeypatch.setattr(HomeBrew, "_get_uses", _fake_get_uses)
+    monkeypatch.setattr("subprocess.check_output", lambda popenargs: b"foo\nbar")
+
+    hb = HomeBrew()
+
+    assert len(hb.installed_packages) == 2
+    assert hb.installed_packages[0] == "foo"
+    assert hb.installed_packages[1] == "bar"
+
+    assert len(hb._uses) == 2
+    assert hb._uses["foo"] == ["bar"]
+    assert hb._uses["bar"] == []
+
+    assert hb.packages_not_needed_by_other == {"bar": []}
+    assert hb.packages_needed_by_other == {"foo": ["bar"]}
+    assert hb.package_dependencies == {"bar": ["foo"]}
+
+    hb.info
+    caplog_lines = [record.msg for record in caplog.records]
+    assert caplog_lines == [
+        "Installed packages:",
+        "-------------------",
+        "bar, foo",
+        "",
+        "No package depends on these packages:",
+        "-------------------------------------",
+        "bar",
+        "",
+        "These packages are needed by other packages:",
+        "--------------------------------------------",
+        "Package foo is needed by: bar",
+        "",
+        "These packages depend on other packages:",
+        "----------------------------------------",
+        "Package bar depends on: foo",
+        "",
+    ]
