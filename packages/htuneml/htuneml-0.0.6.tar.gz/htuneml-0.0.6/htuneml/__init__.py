df48cb2abdbb8e171df375e72cf0e02000e295cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:38 +0000

    Add package htuneml/0.0.6/htuneml-0.0.6.tar.gz

---
+++ packages/htuneml/htuneml-0.0.6.tar.gz/htuneml-0.0.6/htuneml/__init__.py
@@ -0,0 +1,1 @@
+from .hmonitor import Job
---
+++ packages/htuneml/htuneml-0.0.6.tar.gz/htuneml-0.0.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='htuneml',
+      version='0.0.6',
+      description='Monitor machine learning experiments',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='http://github.com/johnsmithm/htuneml',
+      author='Ion Mosnoi',
+      author_email='moshnoi2000@gmail.com',
+      license='MIT',
+      packages=['htuneml'],
+      install_requires=[
+           'requests'
+      ],      
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      zip_safe=False)
