175de7685bff3b6a2430b26e38d3f76bcbf50292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:40 +0000

    Add package rf-sclibrary/0.6.8/rf-sclibrary-0.6.8.tar.gz

---
+++ packages/rf-sclibrary/rf-sclibrary-0.6.8.tar.gz/rf-sclibrary-0.6.8/src/SCLibrary/__init__.py
@@ -0,0 +1,39 @@
+# -*- coding:utf-8 -*-
+
+import os
+from SCLibrary.builtin import ValidatorKeywords
+from SCLibrary.builtin import RequesterKeywords
+from SCLibrary.builtin import DBKeywords
+from SCLibrary.builtin import LogListener
+from SCLibrary.builtin import RandomKeywords
+from robot.libraries.BuiltIn import BuiltIn
+from robot.api import logger
+from SCLibrary.base import DynamicCore, hook_zh
+
+__version__ = '0.6.8'
+
+
+class SCLibrary(DynamicCore):
+
+    ROBOT_LIBRARY_SCOPE = 'GLOBAL'
+    ROBOT_LIBRARY_VERSION = __version__
+
+    def __init__(self):
+        libraries = [ValidatorKeywords(), RequesterKeywords(),
+                     DBKeywords(), RandomKeywords()]
+        DynamicCore.__init__(self, libraries)
+        built_in = BuiltIn()
+        try:
+            exec_dir = built_in.get_variable_value("${EXECDIR}")
+            env = built_in.get_variable_value("${RF_ENV}")
+            if env is None:
+                logger.warn('无法找到doraemon.py的RF_ENV变量，已切换到默认环境：dev')
+                env = 'dev'
+            built_in.set_global_variable(
+                "${ENVDIR}", '%s/config/%s.env.robot' % (exec_dir, env))
+            if built_in.get_variable_value("${RF_DEBUG}") == True:
+                self.ROBOT_LIBRARY_LISTENER = LogListener()
+        except:
+            pass
+        # 复写robot的unic.py，支持Log打印中文
+        # hook_zh()
