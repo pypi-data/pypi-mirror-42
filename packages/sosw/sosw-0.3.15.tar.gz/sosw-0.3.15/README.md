08e4fe3fb21755257035c4cc5f02d5e1d383213c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:07 +0000

    Add package sosw/0.3.15/sosw-0.3.15.tar.gz

---
+++ packages/sosw/sosw-0.3.15.tar.gz/sosw-0.3.15/README.md
@@ -0,0 +1,5 @@
+# sosw
+Serverless Orchestrator of Serverless Workers for AWS - Worker
+[![Build Status](https://travis-ci.org/bimpression/sosw.svg?branch=master)](https://travis-ci.org/bimpression/sosw)
+
+Documentation can be found in [sosw.rtfd.io](https://sosw.rtfd.io/latest)
---
+++ packages/sosw/sosw-0.3.15.tar.gz/sosw-0.3.15/sosw/test/suite_3_6_unit.py
@@ -0,0 +1,22 @@
+import unittest
+
+from .test_app import app_UnitTestCase
+from ..components.test.test_helpers import helpers_UnitTestCase
+from ..components.test.test_siblings import siblings_TestCase
+from ..components.test.test_sns import sns_TestCase
+
+
+def suite():
+    test_suite = unittest.TestSuite()
+    test_suite.addTest(unittest.makeSuite(app_UnitTestCase))
+    test_suite.addTest(unittest.makeSuite(helpers_UnitTestCase))
+    test_suite.addTest(unittest.makeSuite(siblings_TestCase))
+    test_suite.addTest(unittest.makeSuite(sns_TestCase))
+
+    return test_suite
+
+
+mySuit = suite()
+
+runner = unittest.TextTestRunner()
+runner.run(mySuit)
