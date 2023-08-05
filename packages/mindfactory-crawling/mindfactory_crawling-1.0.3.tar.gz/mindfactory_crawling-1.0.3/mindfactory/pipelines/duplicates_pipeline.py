cce8c4f82ef6f7d9dc74533a3a2a01c8ff4dc50f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:54 +0000

    Add package mindfactory-crawling/1.0.3/mindfactory_crawling-1.0.3.tar.gz

---
+++ packages/mindfactory-crawling/mindfactory_crawling-1.0.3.tar.gz/mindfactory_crawling-1.0.3/mindfactory/pipelines/duplicates_pipeline.py
@@ -0,0 +1,18 @@
+from scrapy.exceptions import DropItem
+
+
+class DuplicatesPipeline(object):
+    """
+    Removes already processed items from the pipeline.
+    """
+
+    def __init__(self):
+        self.seen_urls = set()
+
+    def process_item(self, item, spider):
+        url = item["url"]
+        if url in self.seen_urls:
+            raise DropItem(f"Duplicate item found with URL {url}.")
+        else:
+            self.seen_urls.add(url)
+            return item
---
+++ packages/mindfactory-crawling/mindfactory_crawling-1.0.3.tar.gz/mindfactory_crawling-1.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name='mindfactory_crawling',
+    version='1.0.3',
+    author="Robert McHardy",
+    author_email="robert@robertmchardy.de",
+    description="A crawler for mindfactory.de",
+    url="https://github.com/RobMcH/mindfactory_crawling",
+    packages=find_packages(),
+    long_description=long_description,
+    entry_points={'scrapy': ['settings = mindfactory.settings']}, install_requires=['scrapy'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
