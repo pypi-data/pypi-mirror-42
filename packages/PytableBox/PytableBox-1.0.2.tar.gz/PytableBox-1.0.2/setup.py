659409942cf7fb4fe88ea09cd107b9a6a013ba3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:50 +0000

    Add package PytableBox/1.0.2/PytableBox-1.0.2.tar.gz

---
+++ packages/PytableBox/PytableBox-1.0.2.tar.gz/PytableBox-1.0.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="PytableBox",
+    version="1.0.2",
+    author="jimmy kumar ahalpara",
+    author_email="jimmyahalpara123@gmail.com",
+    description="can be used to create tables in python",
+    long_description="module to make tables in python",
+    long_description_content_type="text/markdown",
+    url="https://github.com/jimmyahalpara/Pytable/tree/Pytable",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+    ),
+)
