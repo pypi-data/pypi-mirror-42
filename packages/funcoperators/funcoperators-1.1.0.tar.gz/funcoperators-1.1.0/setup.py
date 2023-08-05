4c565703ed98bcfa40d491b614b709b3d1ba45bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:34 +0000

    Add package funcoperators/1.1.0/funcoperators-1.1.0.tar.gz

---
+++ packages/funcoperators/funcoperators-1.1.0.tar.gz/funcoperators-1.1.0/setup.py
@@ -0,0 +1,23 @@
+# from distutils.core import setup
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+import re
+def get_property(prop, project):
+    with open(project + '.py') as f:
+        return re.search(r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop), f.read()).group(1)
+
+setup(
+    name='funcoperators',
+    version=get_property('__version__', 'funcoperators'),
+    description='Allow natural function notations like (1,2) *dot* (3,4) for dot((1,2), (3,4)) or 1 /frac/ 3 for Fraction(1,3), pipes and other useful operators to functions.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Robert Vanden Eynde',
+    author_email='robertvandeneynde@hotmail.com',
+    # packages=['funcoperators'], # setuptools.find_packages() 'mymath.adv'
+    py_modules=['funcoperators'],
+    url='https://github.com/robertvandeneynde/python',
+)
