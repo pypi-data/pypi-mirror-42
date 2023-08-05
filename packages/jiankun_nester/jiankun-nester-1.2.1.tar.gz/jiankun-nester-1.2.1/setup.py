4cd81e9dd79d1845819aa4ef6bed5c4706c67458
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:04 +0000

    Add package jiankun_nester/1.2.1/jiankun-nester-1.2.1.tar.gz

---
+++ packages/jiankun_nester/jiankun-nester-1.2.1.tar.gz/jiankun-nester-1.2.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='jiankun-nester',
+    version='1.2.1',
+    py_modules=['jiankun-nester'],
+    author='jiankun.cai',
+    author_email='caijiankun1994@gmail.com',
+    url='http://www.headfirstlabs.com',
+    description='A simple printer of nested lists',
+)
