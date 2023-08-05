65f51d773a1ef7957023427025c699efb28de0f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:16 +0000

    Add package katka-core/0.9.0/katka-core-0.9.0.tar.gz

---
+++ packages/katka-core/katka-core-0.9.0.tar.gz/katka-core-0.9.0/katka/models.py
@@ -0,0 +1,93 @@
+import uuid
+
+from django.contrib.auth.models import Group
+from django.db import models
+
+from encrypted_model_fields.fields import EncryptedCharField
+from katka.auditedmodel import AuditedModel
+from katka.fields import KatkaSlugField
+
+
+class Team(AuditedModel):
+    public_identifier = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    slug = KatkaSlugField(unique=True)
+    name = models.CharField(max_length=100)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+    def __str__(self):  # pragma: no cover
+        return f'{self.group.name}'
+
+
+class Project(AuditedModel):
+    public_identifier = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    slug = KatkaSlugField()
+    name = models.CharField(max_length=100)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('team', 'slug')
+
+    def __str__(self):  # pragma: no cover
+        return f'{self.name}'
+
+
+class Credential(AuditedModel):
+    public_identifier = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    slug = KatkaSlugField()
+    name = models.CharField(max_length=100)
+    credential_type = models.CharField(max_length=50)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('team', 'slug')
+
+    def __str__(self):  # pragma: no cover
+        return f'{self.name}'
+
+
+class CredentialSecret(AuditedModel):
+    key = models.CharField(max_length=50)
+    value = EncryptedCharField(max_length=200)
+    credential = models.ForeignKey(Credential, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('credential', 'key')
+
+    def __str__(self):  # pragma: no cover
+        return f'{self.credential.name}/{self.key}'
+
+
+class SCMService(AuditedModel):
+
+    public_identifier = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    type = models.CharField(max_length=48)
+    server_url = models.URLField(null=False)
+
+    def __str__(self):  # pragma: no cover
+        return f'{self.type}/{self.server_url}'
+
+
+class SCMRepository(AuditedModel):
+
+    public_identifier = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    organisation = models.CharField(max_length=128)
+    repository_name = models.CharField(max_length=128)
+    credential = models.ForeignKey(Credential, on_delete=models.PROTECT)
+    scm_service = models.ForeignKey(SCMService, on_delete=models.PROTECT)
+
+    def __str__(self):  # pragma: no cover
+        return f'{self.organisation}/{self.repository_name}'
+
+
+class Application(AuditedModel):
+    public_identifier = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    slug = KatkaSlugField()
+    name = models.CharField(max_length=100)
+    project = models.ForeignKey(Project, on_delete=models.PROTECT)
+    scm_repository = models.OneToOneField(SCMRepository, on_delete=models.PROTECT)
+
+    class Meta:
+        unique_together = ('project', 'slug')
+
+    def __str__(self):  # pragma: no cover
+        return f'{self.name}'
