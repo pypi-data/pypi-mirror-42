97b43cd164bfe982302c3ebd310422a3e173bb6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:47 +0000

    Add package rulm/0.0.1/rulm-0.0.1.tar.gz

---
+++ packages/rulm/rulm-0.0.1.tar.gz/rulm-0.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='rulm',
+    packages=find_packages(),
+    version='0.0.1',
+    description='',
+    author='Ilya Gusev',
+    author_email='phoenixilya@gmail.com',
+    url='https://github.com/IlyaGusev/rulm',
+    download_url='https://github.com/IlyaGusev/rulm/archive/0.0.1.tar.gz',
+    keywords=['nlp', 'russian', 'language model'],
+    install_requires=[
+        'numpy>=1.11.3',
+        'pygtrie>=2.2',
+        'torch>=1.0.0',
+        'overrides>=1.9',
+        'allennlp>=0.8.0',
+        'scipy>=1.0.1'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+
+        'Topic :: Text Processing :: Linguistic',
+
+        'License :: OSI Approved :: Apache Software License',
+
+        'Natural Language :: Russian',
+
+        'Programming Language :: Python :: 3.6',
+    ],
+)
