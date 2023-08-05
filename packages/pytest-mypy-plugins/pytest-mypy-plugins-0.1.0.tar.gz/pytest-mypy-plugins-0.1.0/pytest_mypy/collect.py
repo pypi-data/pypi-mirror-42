6ec646f3616edd2b7514239bc40dfa47e0a66460
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:38 +0000

    Add package pytest-mypy-plugins/0.1.0/pytest-mypy-plugins-0.1.0.tar.gz

---
+++ packages/pytest-mypy-plugins/pytest-mypy-plugins-0.1.0.tar.gz/pytest-mypy-plugins-0.1.0/pytest_mypy/collect.py
@@ -0,0 +1,37 @@
+import pytest
+from _pytest.config.argparsing import Parser
+
+from pytest_mypy.item import TestItem
+from pytest_mypy.parser import parse_test_chunk, split_test_chunks
+
+
+class DotTestFile(pytest.File):
+    def collect(self):
+        file_contents = self.fspath.read_text('utf8')
+        for raw_chunk in split_test_chunks(file_contents):
+            chunk = parse_test_chunk(raw_chunk, pytest_config=self.config)
+            yield TestItem(name=chunk.name,
+                           collector=self,
+                           config=self.config,
+                           source_code=chunk.source_code,
+                           starting_lineno=chunk.starting_lineno,
+                           output_lines=chunk.output_lines,
+                           files=chunk.files_to_create,
+                           custom_environment=chunk.custom_environment,
+                           temp_dir=chunk.temp_dir,
+                           mypy_options=chunk.custom_mypy_options)
+
+
+def pytest_collect_file(path, parent):
+    if path.ext == '.test':
+        return DotTestFile(path, parent=parent, config=parent.config)
+
+
+def pytest_addoption(parser: Parser) -> None:
+    group = parser.getgroup('mypy-tests')
+    group.addoption('--mypy-testing-base', type=str, default='/tmp',
+                    help='Base directory for tests to use')
+    group.addoption('--mypy-ini-file', type=str,
+                    help='Which .ini file to use as a default config for tests')
+    group.addoption('--mypy-no-cache', action='store_true',
+                    help='Disable mypy caching for files created in tests')
