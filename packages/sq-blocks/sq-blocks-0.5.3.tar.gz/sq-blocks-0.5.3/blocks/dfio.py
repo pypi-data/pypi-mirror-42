f304e7c846ae36fa82704ae1b0ce5313193f4619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:15 +0000

    Add package sq-blocks/0.5.3/sq-blocks-0.5.3.tar.gz

---
+++ packages/sq-blocks/sq-blocks-0.5.3.tar.gz/sq-blocks-0.5.3/blocks/dfio.py
@@ -0,0 +1,197 @@
+import gzip
+import os
+import six
+import warnings
+import pandas as pd
+from io import TextIOWrapper
+
+try:
+    import pyarrow.parquet as pq
+    parquet = True
+except ImportError:
+    parquet = False
+
+try:
+    import fastavro as avro
+    avro_imported = True
+except ImportError:
+    avro_imported = False
+
+
+def read_df(datafile, **read_args):
+    """ Read a dataframe from file based on the file extension
+
+    The following formats are supported:
+    parquet, avro, csv, pickle
+
+    Parameters
+    ----------
+    datafile : DataFile
+        DataFile instance with the path and handle
+    read_args : optional
+        All keyword args are passed to the read function
+
+    Returns
+    -------
+    data : pd.DataFrame
+
+    Notes
+    -----
+    The read functions are taken from pandas, e.g. pd.read_csv
+    Check the pandas doc for more information on the supported arguments
+
+    """
+    datafile.handle.seek(0)  # ensure we start from the beginning of the file
+    filetype, compression = _get_extension(datafile.path)
+    reader = _readers[filetype]
+    if reader in (pd.read_csv, pd.read_pickle) and compression is not None:
+        read_args["compression"] = compression
+    elif reader in (pd.read_csv, pd.read_pickle):
+        read_args["compression"] = None  # default "infer" incompatible with handles as of 0.24
+    return reader(datafile.handle, **read_args)
+
+
+def write_df(df, datafile, **write_args):
+    """ Write a dataframe to file based on the file extension
+
+    The following formats are supported:
+    parquet, avro, csv, pickle
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The dataframe to write to disk
+    datafile : DataFile
+        Datafile instance with the path and file handle
+    write_args : optional
+        All keyword args are passed to the write function
+
+    Notes
+    -----
+    The write functions are taken from pandas, e.g. pd.to_csv
+    Check the pandas doc for more information on the supported arguments
+
+    """
+    extension, compression = _get_extension(datafile.path)
+    write_name = _writers[extension]
+    # infer compression from filepath or from explicit arg
+    compression = compression or write_args.get('compression')
+    buffer = datafile.handle
+
+    # Some customizations for different file types
+    if write_name == 'to_avro':
+        return _write_avro(df, buffer, **write_args)
+
+    if write_name == 'to_parquet' and not pd.Series(df.columns).map(type).eq(str).all():
+        warnings.warn(
+            'Dataframe contains non-string column names, which cannot be saved in parquet.\n'
+            'Blocks will attempt to convert them to strings.'
+        )
+        df.columns = df.columns.astype('str')
+
+    if write_name == 'to_csv':
+        # make index=False the default for similar behaviour to other formats
+        write_args['index'] = write_args.get('index', False)
+
+    # For csv and pickle we have to manually compress
+    csv_pkl = write_name in ('to_csv', 'to_pickle')
+    if csv_pkl:
+        write_args['compression'] = None  # default "infer" incompatible with handles as of 0.24
+
+    if csv_pkl and compression == 'gzip':
+        buffer = gzip.GzipFile(fileobj=buffer, mode='w')
+    elif csv_pkl and compression is not None:
+        raise ValueError('Compression {} is not supported for CSV/Pickle'.format(compression))
+
+    # And for 23 compatibility need a textiowrapper for csv
+    if write_name == 'to_csv' and six.PY3:
+        buffer = TextIOWrapper(buffer)
+
+    write_fn = getattr(df, write_name)
+    return write_fn(buffer, **write_args)
+
+
+def _read_parquet(path, **read_args):
+    """ Read a dataframe from parquet file
+
+    Parameters
+    ----------
+    path : str
+        Path to the parquet file
+    read_args : optional
+        All keyword args are passed to pyarrow.parquet.read_table
+
+    Returns
+    -------
+    data : pd.DataFrame
+
+    """
+    if not parquet:
+        raise ImportError('Parquet support requires pyarrow.\n'
+                          'Install blocks with the [pq] option or `pip install pyarrow`')
+    return pq.read_table(path, **read_args).to_pandas()
+
+
+def _read_avro(handle, **read_args):
+    if not avro_imported:
+        raise ImportError('Avro support requires fastavro.\n'
+                          'Install blocks with the [avro] option or `pip install fastavro`')
+    records = []
+    avro_reader = avro.reader(handle)
+    for record in avro_reader:
+        records.append(record)
+    return pd.DataFrame.from_dict(records)
+
+
+def _write_avro(df, handle, **write_args):
+    if not avro_imported:
+        raise ImportError('Avro support requires fastavro.\n'
+                          'Install blocks with the [avro] option or `pip install fastavro`')
+    schema = None
+    schema_path = None
+    try:
+        schema = write_args['schema']
+    except KeyError:
+        try:
+            schema_path = write_args['schema_path']
+        except KeyError:
+            raise Exception("You must provide a schema or schema path when writing to Avro")
+    if schema is None:
+        schema = avro.schema.load_schema(schema_path)
+    records = df.to_dict('records')
+    avro.writer(handle, schema, records)
+
+
+def _get_extension(path):
+    name, ext = os.path.splitext(path)
+    # Support compression extensions, eg part.csv.gz
+    comp = None
+    if ext in _compressions and '.' in name:
+        comp = ext
+        name, ext = os.path.splitext(name)
+    return ext, _compressions.get(comp)
+
+
+_readers = {
+    '.pq': _read_parquet,
+    '.parquet': _read_parquet,
+    '.csv': pd.read_csv,
+    '.pkl': pd.read_pickle,
+    '.avro': _read_avro,
+}
+
+
+_writers = {
+    '.pq': 'to_parquet',
+    '.parquet': 'to_parquet',
+    '.csv': 'to_csv',
+    '.pkl': 'to_pickle',
+    '.avro': 'to_avro',
+}
+
+_compressions = {
+    '.gz': 'gzip',
+    '.bz2': 'bz2',
+    '.zip': 'zip',
+    '.xz': 'xz'
+}
---
+++ packages/sq-blocks/sq-blocks-0.5.3.tar.gz/sq-blocks-0.5.3/blocks/filesystem.py
@@ -0,0 +1,383 @@
+import fnmatch
+import os
+import glob
+import logging
+import subprocess
+import tempfile
+import atexit
+import shutil
+
+from abc import ABCMeta, abstractmethod
+from six import add_metaclass
+from io import BytesIO
+from collections import namedtuple
+from contextlib import contextmanager
+from google.cloud import storage
+
+DataFile = namedtuple('DataFile', ['path', 'handle'])
+
+
+@add_metaclass(ABCMeta)
+class FileSystem(object):
+    """ The required interface for any filesystem implementation
+
+    See GCSFileSystem for a full implementation. This FileSystem is intended
+    to be extendable to support cloud file systems, encryption strategies, etc...
+    """
+    @abstractmethod
+    def ls(self, path):
+        """ List files correspond to path, including glob wildcards
+
+        Parameters
+        ----------
+        path : str
+            The path to the file or directory to list; supports wildcards
+        """
+        pass
+
+    @abstractmethod
+    def access(self, paths):
+        """ Access multiple paths as file-like objects
+
+        This allows for optimization like parallel downloads
+
+        Parameters
+        ----------
+        paths: list of str
+            The paths of the files to access
+
+        Returns
+        -------
+        files: list of DataFile
+            A list of datafile instances, one for each input path
+        """
+        pass
+
+    @abstractmethod
+    def store(self, bucket, files):
+        """ Store multiple data objects
+
+        This allows for optimizations when storing several files
+
+        Parameters
+        ----------
+        bucket : str
+            The GCS bucket to use to store the files
+        files : list of str
+            The file names to store
+
+        Returns
+        -------
+        datafiles : contextmanager
+           A contextmanager that will yield datafiles and place them
+           on the filesystem when finished
+        """
+        pass
+
+
+class GCSFileSystem(FileSystem):
+    """ File system interface that supports both local and GCS files
+
+    This implementation uses subprocess and gsutil, which has excellent performance.
+    However this can lead to problems in very multi-threaded applications and might not be
+    as portable. For a python native implementation use GCSNativeFileSystem
+    """
+    GCS = 'gs://'
+
+    def __init__(self, parallel=True, quiet=True):
+        flags = []
+        if parallel:
+            flags.append('-m')
+        if quiet:
+            flags.append('-q')
+        self.gcscp = ['gsutil'] + flags + ['cp', '-r']
+
+    def local(self, path):
+        """ Check if the path is available as a local file
+        """
+        return not path.startswith(self.GCS)
+
+    def ls(self, path):
+        """ List files correspond to path, including glob wildcards
+
+        Parameters
+        ----------
+        path : str
+            The path to the file or directory to list; supports wildcards
+        """
+        logging.info('Globbing file content in {}'.format(path))
+        if not self.local(path):
+            with open(os.devnull, 'w') as DEVNULL:
+                p = subprocess.Popen(
+                    ['gsutil', 'ls', path],
+                    stdout=subprocess.PIPE,
+                    stderr=DEVNULL,
+                    universal_newlines=True,
+                )
+                stdout = p.communicate()[0]
+            output = [line for line in stdout.split('\n') if line and line[-1] != ':']
+        elif '**' in path:
+            # Manual recursive glob, since in 2.X glob doesn't have recursive support
+            path = path.rstrip('*')
+            output = []
+            for root, subdirs, files in os.walk(path):
+                for fname in files:
+                    output.append(os.path.join(root, fname))
+        elif os.path.isdir(path):
+            output = [os.path.join(path, f) for f in os.listdir(path)]
+        else:
+            output = glob.glob(path)
+        return sorted(p.rstrip('/') for p in output)
+
+    def copy(self, sources, dest):
+        """ Copy the files in sources (recursively) to dest
+
+        Parameters
+        ----------
+        sources : list of str
+            The list of paths to copy, which can be directories
+        dest : str
+            The destination for the copy of source(s)
+        """
+        summary = ', '.join(sources)
+        logging.info('Copying {} to {}...'.format(summary, dest))
+
+        # TODO perhaps use a python API rather than subprocess
+        if any(self.GCS in x for x in sources + [dest]):
+            # at least one location is on GCS
+            cmd = self.gcscp
+        else:
+            cmd = ['cp', '-r']
+        # Break this into pieces so we don't hit OS limit for arguments
+        # TODO: use getconf ARG_MAX and actually test number of bytes in sources
+        CHUNK_SIZE = 1000
+        sources_chunks = [sources[x:x+CHUNK_SIZE] for x in range(0, len(sources), CHUNK_SIZE)]
+        for sources in sources_chunks:
+            subprocess.check_call(cmd + sources + [dest])
+
+    def access(self, paths):
+        """ Access multiple paths as file-like objects
+
+        This allows for optimization like parallel downloads
+
+        Parameters
+        ----------
+        paths: list of str
+            The paths of the files to access
+
+        Returns
+        -------
+        files: list of DataFile
+            A list of datafile instances, one for each input path
+        """
+        # Move the files into a tempdir from GCS
+        tmpdir = _session_tempdir()
+        self.copy(paths, tmpdir)
+
+        # Then get file handles for each
+        datafiles = []
+        for path in paths:
+            local = os.path.join(tmpdir, os.path.basename(path))
+            datafiles.append(DataFile(path, open(local, 'rb')))
+        return datafiles
+
+    @contextmanager
+    def store(self, bucket, files):
+        """ Create file stores that will be written to the filesystem on close
+
+        This allows for optimizations when storing several files
+
+        Parameters
+        ----------
+        bucket : str
+            The path of the bucket (on GCS) or folder (local) to store the data in
+        files : list of str
+            The filenames to create
+
+        Returns
+        -------
+        datafiles : contextmanager
+            A context manager that yields datafiles and when the context is closed
+            they are written to GCS
+
+        Usage
+        -----
+        >>> with filesystem.store('gs://bucket/sub/', ['ex1.txt', 'ex2.txt']) as datafiles:
+        >>>     datafiles[0].handle.write('example 1')
+        >>>     datafiles[1].handle.write('example 2')
+        """
+        # Make local files in a tempdir that serve as the file handles
+        tmpdir = _session_tempdir()
+        datafiles = []
+        local_files = []
+        for f in files:
+            local = os.path.join(tmpdir, f)
+            local_files.append(local)
+            datafiles.append(DataFile(os.path.join(bucket, f), open(local, 'wb')))
+
+        yield datafiles
+
+        for d in datafiles:
+            d.handle.close()
+
+        if self.local(bucket) and not os.path.exists(bucket):
+            os.makedirs(bucket)
+
+        self.copy(local_files, os.path.join(bucket, ''))
+
+
+class GCSNativeFileSystem(GCSFileSystem):
+    """ File system interface that supports GCS and local files
+
+    This uses the native python cloud storage library for read and write, rather than gsutil.
+    The performance is significantly slower when reading/writing several files but is thread-safe
+    for applications which are already parallelized. It also stores the files entirely in
+    memory rather than using tempfiles.
+    """
+    def __init__(self, *args, **kwargs):
+        self.client = storage.Client()
+        super(GCSNativeFileSystem, self).__init__(*args, **kwargs)
+
+    def ls(self, path):
+        logging.info('Globbing file content in {}'.format(path))
+
+        # use GCSFileSystem's implementation for local paths
+        if self.local(path):
+            return super(GCSNativeFileSystem, self).ls(path)
+
+        # find all names that start with the prefix for this path
+        bucket, path = self._split(path)
+        prefix = self._prefix(path)
+        names = [b.name for b in self.client.get_bucket(bucket).list_blobs(prefix=prefix)]
+
+        if prefix == path:
+            # no pattern strings, listing a single file or a folder
+            try:
+                # one blob exactly matches the requested path
+                names = [next(p for p in names if p == path)]
+            except StopIteration:
+                # no exact match, so we are listing based on prefix
+                # find files in this directory or subfolders
+                names = self._list_single(prefix, names)
+        # we have a pattern to match
+        else:
+            # for recursive glob, do not attempt to match folders, only files
+            if '**' in path:
+                names = fnmatch.filter(names, path)
+            else:
+                # make sure we can match folders in addition to blobs
+                candidates = set(names) | set(self._list_single(prefix, names))
+                names = fnmatch.filter(candidates, path)
+                # one more filter because fnmatch recurses single *
+                names = [name for name in names if name.count('/') == path.count('/')]
+
+        paths = ['gs://{}/{}'.format(bucket, name) for name in names]
+        return sorted(paths)
+
+    def _prefix(self, path):
+        # find the lowest prefix that doesn't include a pattern match
+        splits = path.split('/')
+        accumulate = []
+        while splits:
+            sub = splits.pop(0)
+            if any(x in sub for x in ['*', '?', '[', ']']):
+                break
+            accumulate.append(sub)
+        return '/'.join(accumulate) if accumulate else None
+
+    def _list_single(self, prefix, names):
+        # find files that are directly in the dir specified by prefix, not in subfolders
+        valid = set(n.replace(prefix, '').lstrip('/').split('/')[0] for n in names)
+        return [os.path.join(prefix, n) for n in valid]
+
+    def access(self, paths):
+        """ Access multiple paths as file-like objects
+
+        This allows for optimization like parallel downloads
+
+        Parameters
+        ----------
+        paths: list of str
+            The paths of the files to access
+
+        Returns
+        -------
+        files: list of DataFile
+            A list of datafile instances, one for each input path
+        """
+        datafiles = []
+        for path in paths:
+            datafiles.append(DataFile(path, BytesIO()))
+        for datafile in datafiles:
+            self._read(datafile)
+        return datafiles
+
+    @contextmanager
+    def store(self, bucket, files):
+        """ Create file stores that will be written to the filesystem on close
+
+        This allows for optimizations when storing several files
+
+        Parameters
+        ----------
+        bucket : str
+            The path of the bucket (on GCS) or folder (local) to store the data in
+        files : list of str
+            The filenames to create
+
+        Returns
+        -------
+        datafiles : contextmanager
+            A context manager that yields datafiles and when the context is closed
+            they are written to GCS
+
+        Usage
+        -----
+        >>> with filesystem.store('gs://bucket/sub/', ['ex1.txt', 'ex2.txt']) as datafiles:
+        >>>     datafiles[0].handle.write('example 1')
+        >>>     datafiles[1].handle.write('example 2')
+        """
+        # Make BytesIO instances that serve as the file handles
+        datafiles = []
+        for f in files:
+            datafiles.append(DataFile(os.path.join(bucket, f), BytesIO()))
+
+        yield datafiles
+
+        for d in datafiles:
+            d.handle.seek(0)
+            self._write(d)
+
+    def _split(self, path):
+        bucket = path.replace(self.GCS, '').split('/')[0]
+        prefix = "gs://{}".format(bucket)
+        path = path[len(prefix) + 1:]
+        return bucket, path
+
+    def _blob(self, path):
+        bucket, path = self._split(path)
+        return storage.Blob(path, self.client.get_bucket(bucket))
+
+    def _read(self, datafile):
+        if self.local(datafile.path):
+            with open(datafile.path, 'rb') as f:
+                datafile.handle.write(f.read())
+        else:
+            self._blob(datafile.path).download_to_file(datafile.handle)
+        datafile.handle.seek(0)
+
+    def _write(self, datafile):
+        if self.local(datafile.path):
+            with open(datafile.path, 'w') as f:
+                f.write(datafile.handle.read())
+        else:
+            self._blob(datafile.path).upload_from_file(datafile.handle)
+
+
+def _session_tempdir():
+    """ Create a tempdir that will be cleaned up at session exit
+    """
+    tmpdir = tempfile.mkdtemp()
+    # create and use a subdir of specified name to preserve cgroup logic
+    atexit.register(lambda: shutil.rmtree(tmpdir))
+    return tmpdir
