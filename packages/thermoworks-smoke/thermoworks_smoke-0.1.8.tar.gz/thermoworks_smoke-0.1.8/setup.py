93276203fead4b7c8cdaf20f9b31b547e515584d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:52 +0000

    Add package thermoworks-smoke/0.1.8/thermoworks_smoke-0.1.8.tar.gz

---
+++ packages/thermoworks-smoke/thermoworks_smoke-0.1.8.tar.gz/thermoworks_smoke-0.1.8/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='thermoworks_smoke',
+    version='0.1.8',
+    url='https://github.com/nhorvath/python-thermoworks-smoke',
+    description='Pull data for your thermoworks smoke thermometer',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='nhorvath',
+    author_email='nhorvath@gmail.com',
+    license='GPLv3',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='Thermoworks',
+    packages=find_packages(exclude=['tests']),
+    install_requires=[
+        'pyrebase4>=4.3.0'
+    ]
+)
