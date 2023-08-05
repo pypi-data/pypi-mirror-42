8e0a2ec4a12777307d085645878d8f83378ef809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:28 +0000

    Add package pynpoint/0.6.1/pynpoint-0.6.1.tar.gz

---
+++ packages/pynpoint/pynpoint-0.6.1.tar.gz/pynpoint-0.6.1/docs/index.rst
@@ -0,0 +1,61 @@
+.. _index:
+
+PynPoint
+========
+
+PynPoint is a Python package for processing and analysis of high-contrast imaging data of faint companions and circumstellar disks. The package has been developed at the |ipa| of ETH Zurich in a collaboration between the |spf| and the |cosmo|.
+
+.. figure:: _images/eso.jpg
+   :width: 100 %
+   :target: http://www.eso.org/public/news/eso1310
+
+   Credit: ESO/L. Calçada
+
+.. |ipa| raw:: html
+	
+	<a href="http://www.ipa.phys.ethz.ch" target="_blank">Institute of Particle Physics and Astrophysics</a>
+
+.. |spf| raw:: html
+
+   <a href="http://www.schmid-group.ethz.ch/" target="_blank">Star and Planet Formation Group</a>
+
+.. |cosmo| raw:: html
+
+   <a href="http://www.cosmology.ethz.ch" target="_blank">Cosmology Research Group</a>
+
+.. _contents:
+
+User Guide
+----------
+
+.. toctree::
+   :maxdepth: 2
+
+   quickstart
+   tutorial
+   architecture
+   writing
+
+API Documentation
+-----------------
+
+.. toctree::
+   :maxdepth: 2
+
+   modules
+
+Mailing List
+------------
+
+.. toctree::
+   :maxdepth: 2
+
+   mailing
+
+About
+-----
+
+.. toctree::
+   :maxdepth: 2
+
+   about
---
+++ packages/pynpoint/pynpoint-0.6.1.tar.gz/pynpoint-0.6.1/pynpoint/util/psf.py
@@ -0,0 +1,88 @@
+"""
+Functions for PSF subtraction.
+"""
+
+from __future__ import absolute_import
+
+import numpy as np
+
+from scipy.ndimage import rotate
+from sklearn.decomposition import PCA
+
+
+def pca_psf_subtraction(images,
+                        angles,
+                        pca_number,
+                        pca_sklearn=None,
+                        im_shape=None,
+                        indices=None):
+    """
+    Function for PSF subtraction with PCA.
+
+    :param images: Stack of images. Also used as reference images if pca_sklearn is set to None.
+                   Should be in the original 3D shape if pca_sklearn is set to None or in the 2D
+                   reshaped format if pca_sklearn is not None.
+    :type images: numpy.ndarray
+    :param parang: Derotation angles (deg).
+    :type parang: numpy.ndarray
+    :param pca_number: Number of principal components used for the PSF model.
+    :type pca_number: int
+    :param pca_sklearn: PCA object with the basis if not set to None.
+    :type pca_sklearn: sklearn.decomposition.pca.PCA
+    :param im_shape: Original shape of the stack with images. Required if pca_sklearn is not
+                     set to None.
+    :type im_shape: tuple(int, int, int)
+    :param indices: Non-masked image indices, required if pca_sklearn is not set to None. Optional
+                    if pca_sklearn is set to None.
+    :type indices: numpy.ndarray
+
+    :return: Mean residuals of the PSF subtraction and the derotated but non-stacked residuals.
+    :rtype: numpy.ndarray, numpy.ndarray
+    """
+
+    if pca_sklearn is None:
+        pca_sklearn = PCA(n_components=pca_number, svd_solver="arpack")
+
+        im_shape = images.shape
+
+        if indices is None:
+            # select the first image and get the unmasked image indices
+            im_star = images[0, ].reshape(-1)
+            indices = np.where(im_star != 0.)[0]
+
+        # reshape the images and select the unmasked pixels
+        im_reshape = images.reshape(im_shape[0], im_shape[1]*im_shape[2])
+        im_reshape = im_reshape[:, indices]
+
+        # subtract mean image
+        im_reshape -= np.mean(im_reshape, axis=0)
+
+        # create pca basis
+        pca_sklearn.fit(im_reshape)
+
+    else:
+        im_reshape = np.copy(images)
+
+    # create pca representation
+    zeros = np.zeros((pca_sklearn.n_components - pca_number, im_reshape.shape[0]))
+    pca_rep = np.matmul(pca_sklearn.components_[:pca_number], im_reshape.T)
+    pca_rep = np.vstack((pca_rep, zeros)).T
+
+    # create psf model
+    psf_model = pca_sklearn.inverse_transform(pca_rep)
+
+    # create original array size
+    residuals = np.zeros((im_shape[0], im_shape[1]*im_shape[2]))
+
+    # subtract the psf model
+    residuals[:, indices] = im_reshape - psf_model
+
+    # reshape to the original image size
+    residuals = residuals.reshape(im_shape)
+
+    # derotate the images
+    res_rot = np.zeros(residuals.shape)
+    for j, item in enumerate(angles):
+        res_rot[j, ] = rotate(residuals[j, ], item, reshape=False)
+
+    return residuals, res_rot
