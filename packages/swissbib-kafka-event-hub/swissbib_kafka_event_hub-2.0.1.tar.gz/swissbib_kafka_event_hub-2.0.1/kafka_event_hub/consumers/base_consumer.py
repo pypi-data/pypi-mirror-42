0ccdd10748d086053483a16a72797a4ccad91ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:38 +0000

    Add package swissbib-kafka-event-hub/2.0.1/swissbib_kafka_event_hub-2.0.1.tar.gz

---
+++ packages/swissbib-kafka-event-hub/swissbib_kafka_event_hub-2.0.1.tar.gz/swissbib_kafka_event_hub-2.0.1/kafka_event_hub/consumers/base_consumer.py
@@ -0,0 +1,51 @@
+from kafka_event_hub.config import ConsumerConfig
+from kafka import KafkaConsumer, TopicPartition
+from typing import List
+import logging
+
+
+class AbstractBaseConsumer(object):
+
+    def __init__(self, config: str, config_class: type(ConsumerConfig), **kwargs):
+        """
+
+        Consumer configs:
+        
+        """
+        self._configuration = config_class(config)
+        self._consumer = KafkaConsumer(**self.configuration.consumer)
+
+        self._error_logger = logging.getLogger(self.configuration.logger_name + '-errors')
+        if 'handler' in kwargs and isinstance(kwargs['handler'], logging.Handler):
+            self._error_logger.addHandler(kwargs['handler'])
+
+        error_handler = logging.FileHandler(self.configuration.errorlogging)
+        error_handler.setLevel(logging.ERROR)
+        error_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s'))
+        self._error_logger.addHandler(error_handler)
+
+        self._time_logger = logging.getLogger(self.configuration.logger_name + '-summary')
+        time_handler = logging.FileHandler(self.configuration.logging)
+        time_handler.setLevel(logging.INFO)
+        time_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s'))
+        self._time_logger.addHandler(time_handler)
+        self._time_logger.info("Initialized Consumer.")
+
+        self.subscribe(self.configuration.topic)
+
+    @property
+    def configuration(self) -> ConsumerConfig:
+        return self._configuration
+
+    def assign(self, topic: TopicPartition):
+        self._consumer.assign([topic])
+
+    def close(self):
+        self._consumer.close()
+
+    def subscribe(self, topics: List[str]):
+        self._consumer.subscribe(topics)
+        self._time_logger.info("Subscribed to topics: %s", topics)
+
+    def unsubscribe(self):
+        self._consumer.unsubscribe()
---
+++ packages/swissbib-kafka-event-hub/swissbib_kafka_event_hub-2.0.1.tar.gz/swissbib_kafka_event_hub-2.0.1/test/test_lines_producer.py
@@ -0,0 +1,144 @@
+import sys
+import os
+import logging
+import pytest
+
+logging.basicConfig(filename='logs/line-producer-tests.log', filemode='w', level=logging.DEBUG)
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from kafka_event_hub.producers import LineProducer
+from kafka_event_hub.consumers import SimpleConsumer
+from kafka import KafkaAdminClient
+from kafka.errors import UnknownTopicOrPartitionError
+
+
+class TestLineProducer(object):
+
+    def setup_class(self):
+        self.admin = KafkaAdminClient(bootstrap_servers='localhost:9092')
+        self.producer = LineProducer('configs/lines/producer.yml')
+        self.producer_gz = LineProducer('configs/lines/producer_gz.yml')
+        self.producer_bz2 = LineProducer('configs/lines/producer_bz2.yml')
+        self.consumer = SimpleConsumer('configs/lines/consumer.yml')
+        self.consumer_gz = SimpleConsumer('configs/lines/consumer_gz.yml')
+        self.consumer_bz2 = SimpleConsumer('configs/lines/consumer_bz2.yml')
+
+    def teardown_class(self):
+        self.consumer.close()
+        self.consumer_gz.close()
+        self.consumer_bz2.close()
+        try:
+            self.admin.delete_topics(['test-lines-gz'])
+        except UnknownTopicOrPartitionError:
+            pass
+        try:
+            self.admin.delete_topics(['test-lines'])
+        except UnknownTopicOrPartitionError:
+            pass
+        try:
+            self.admin.delete_topics(['test-lines-bz2'])
+        except UnknownTopicOrPartitionError:
+            pass
+        self.admin.close()
+
+    #@pytest.mark.skip()
+    def test_produce(self):
+        self.producer.process()
+        key = None
+        message = None
+        while key is None and message is None:
+            key, message = self.consumer.consume()
+        assert key == '0'
+        assert message == "This is a line"
+        key = None
+        message = None
+        while key is None and message is None:
+            key, message = self.consumer.consume()
+        assert key == '1'
+        assert message == "and another line"
+        key = None
+        message = None
+        while key is None and message is None:
+            key, message = self.consumer.consume()
+        assert key == '2'
+        assert message == "a third line"
+        key = None
+        message = None
+        while key is None and message is None:
+            key, message = self.consumer.consume()
+        assert key == '3'
+        assert message == "a forth line"
+        key = None
+        message = None
+        while key is None and message is None:
+            key, message = self.consumer.consume()
+        assert key == '4'
+        assert message == "a lot of lines now"
+
+    #@pytest.mark.skip("Currently way too slow")
+    def test_produce_gz(self):                
+        self.producer_gz.process()
+        key = None
+        message = None
+        while key is None and message is None:
+            key, message = self.consumer_gz.consume()
+        assert key == '0'
+        assert message == "This is a line"
+        key = None
+        message = None
+        while key is None and message is None:
+            key, message = self.consumer_gz.consume()
+        assert key == '1'
+        assert message == "and another line"
+        key = None
+        message = None
+        while key is None and message is None:
+            key, message = self.consumer_gz.consume()
+        assert key == '2'
+        assert message == "a third line"
+        key = None
+        message = None
+        while key is None and message is None:
+            key, message = self.consumer_gz.consume()
+        assert key == '3'
+        assert message == "a forth line"
+        key = None
+        message = None
+        while key is None and message is None:
+            key, message = self.consumer_gz.consume()
+        assert key == '4'
+        assert message == "a lot of lines now"
+
+    # @pytest.mark.skip("Currently way too slow")
+    def test_produce_bz2(self):
+        self.producer_bz2.process()
+        key = None
+        message = None
+        while key is None and message is None:
+            key, message = self.consumer_bz2.consume()
+        assert key == '0'
+        assert message == "This is a line"
+        key = None
+        message = None
+        while key is None and message is None:
+            key, message = self.consumer_bz2.consume()
+        assert key == '1'
+        assert message == "and another line"
+        key = None
+        message = None
+        while key is None and message is None:
+            key, message = self.consumer_bz2.consume()
+        assert key == '2'
+        assert message == "a third line"
+        key = None
+        message = None
+        while key is None and message is None:
+            key, message = self.consumer_bz2.consume()
+        assert key == '3'
+        assert message == "a forth line"
+        key = None
+        message = None
+        while key is None and message is None:
+            key, message = self.consumer_bz2.consume()
+        assert key == '4'
+        assert message == "a lot of lines now"
