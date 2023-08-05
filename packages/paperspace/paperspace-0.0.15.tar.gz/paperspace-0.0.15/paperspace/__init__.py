483007a4b2a35eb0167a6437fbaf807e22969816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:27 +0000

    Add package paperspace/0.0.15/paperspace-0.0.15.tar.gz

---
+++ packages/paperspace/paperspace-0.0.15.tar.gz/paperspace-0.0.15/paperspace/__init__.py
@@ -0,0 +1,12 @@
+from .config import *
+from .login import login, logout
+from .method import print_json_pretty
+from .jobs import run
+from . import jobs
+from . import machines
+from . import networks
+from . import scripts
+from . import templates
+from . import users
+
+__version__ = "0.0.15"
---
+++ packages/paperspace/paperspace-0.0.15.tar.gz/paperspace-0.0.15/setup.py
@@ -0,0 +1,39 @@
+from codecs import open
+from os import path
+from setuptools import setup, find_packages
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError, OSError):
+    with open('README.md', encoding='utf-8') as f:
+        long_description = f.read()
+
+setup(
+    name='paperspace',
+    version='0.0.15',
+    description='Paperspace Python',
+    long_description=long_description,
+    url='https://github.com/paperspace/paperspace-python',
+    author='Paperspace Co.',
+    author_email='info@paperspace.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='paperspace api development library',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=['requests[security]', 'boto3', 'botocore', 'six'],
+    entry_points={'console_scripts': [
+        'paperspace-python = paperspace.main:main',
+    ]},
+)
