21a124f74eb732553ac9069137bffcf1a47d2bc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:35 +0000

    Add package sortvis/1.0.1/sortvis-1.0.1.tar.gz

---
+++ packages/sortvis/sortvis-1.0.1.tar.gz/sortvis-1.0.1/sortvis/bubblesort.py
@@ -0,0 +1,19 @@
+from data import SortData
+
+def BubbleSort(sv):
+    assert isinstance(sv, SortData), "Type Error"
+
+    Length = sv.length
+    for i in range(Length-1, -1, -1):
+        for j in range(0, i, 1):
+            if sv.data[j] > sv.data[j+1]:
+                sv.Swap(j, j+1)
+
+if __name__ == "__main__":
+    sv=SortData(64)
+    sv.Visualize()
+    sv.StartTimer()
+    BubbleSort(sv)
+    sv.StopTimer()
+    sv.SetTimeInterval(0)
+    sv.Visualize()
