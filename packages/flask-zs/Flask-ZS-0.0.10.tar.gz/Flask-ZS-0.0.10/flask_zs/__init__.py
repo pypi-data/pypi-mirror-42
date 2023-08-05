817c62e938910f48da42802fc8c8b30bf960a176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:09 +0000

    Add package flask-zs/0.0.10/Flask-ZS-0.0.10.tar.gz

---
+++ packages/flask-zs/Flask-ZS-0.0.10.tar.gz/Flask-ZS-0.0.10/flask_zs/__init__.py
@@ -0,0 +1,13 @@
+from .errorhandler import register_error_handlers
+from .helpers import BaseItemView, BaseModel, CustomFlask, register_api, register_blueprints
+from .response import ok_resp
+
+__all__ = (
+    'BaseItemView',
+    'BaseModel',
+    'CustomFlask',
+    'register_api',
+    'register_blueprints',
+    'ok_resp',
+    'register_error_handlers',
+)
