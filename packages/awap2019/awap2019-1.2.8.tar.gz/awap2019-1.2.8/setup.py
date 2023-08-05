2863177cf57d40ef39290b62ccf1937ffcef09b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:17 +0000

    Add package awap2019/1.2.8/awap2019-1.2.8.tar.gz

---
+++ packages/awap2019/awap2019-1.2.8.tar.gz/awap2019-1.2.8/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(name='awap2019',
+      version='1.2.8',
+      description='Module for AWAP 2019.',
+      long_description='Contains essential classes for competitors.',
+      url='http://github.com/cuebeomc/awap2019',
+      author='ACM@CMU',
+      author_email='cuebeomc@andrew.cmu.edu',
+      license='GPL',
+      packages=find_packages(),
+      install_requires=[
+          'numpy',
+          'absl-py',
+          'matplotlib',
+          'scipy'
+      ],
+      )
