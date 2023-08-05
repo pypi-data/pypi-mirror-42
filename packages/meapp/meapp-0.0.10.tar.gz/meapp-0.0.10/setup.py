f575848c18073782bf3a6c05e8a3b06919ec9206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:00 +0000

    Add package meapp/0.0.10/meapp-0.0.10.tar.gz

---
+++ packages/meapp/meapp-0.0.10.tar.gz/meapp-0.0.10/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="meapp",
+    version="0.0.10",
+    author="zhuxietong",
+    author_email="zhuxietong@me.com",
+    description="A small example packcage add tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/example-project",
+    packages=setuptools.find_packages(),
+    # packages=setuptools.find_packages(include=['core', 'location', 'tools','api']),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=[
+        'django',
+        'djangorestframework',
+        'six',
+        'Pillow'
+        # 'mysqlclient'
+    ],
+    include_package_data=True,
+)
