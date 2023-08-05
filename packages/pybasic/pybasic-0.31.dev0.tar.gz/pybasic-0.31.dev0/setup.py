e5456b03d76ab82ec9ad0203a0d9f9ff815c074f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:57 +0000

    Add package pybasic/0.31.dev0/pybasic-0.31.dev0.tar.gz

---
+++ packages/pybasic/pybasic-0.31.dev0.tar.gz/pybasic-0.31.dev0/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="pybasic",
+    version="0.31dev",
+    author="Henry Benson",
+    author_email="tomfleetwitter@gmail.com",
+    description="A Python semi-beginner module",
+    long_description="pybasic is a module aimed at Python beginners, when they've learned enough but don't remember much, this is the module.",
+    url="https://afsoft.eu/s/win95/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
