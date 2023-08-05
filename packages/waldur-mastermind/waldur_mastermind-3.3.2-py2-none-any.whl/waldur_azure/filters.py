1bd20d0179650e48b01259fe3f8c908856a76f35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:30 +0000

    Add package waldur-mastermind/3.3.2/waldur_mastermind-3.3.2-py2-none-any.whl

---
+++ packages/waldur-mastermind/waldur_mastermind-3.3.2-py2-none-any.whl/waldur_azure/filters.py
@@ -0,0 +1,45 @@
+import django_filters
+
+from waldur_core.core import filters as core_filters
+from waldur_core.structure import filters as structure_filters
+
+from . import models
+
+
+class ImageFilter(structure_filters.BaseServicePropertyFilter):
+    class Meta(structure_filters.BaseServicePropertyFilter.Meta):
+        model = models.Image
+
+
+class BaseResourceGroupFilter(structure_filters.BaseResourceFilter):
+    resource_group = core_filters.URLFilter(
+        view_name='azure-resource-group-detail',
+        name='resource_group__uuid'
+    )
+    resource_group_uuid = django_filters.UUIDFilter(name='resource_group__uuid')
+
+
+class VirtualMachineFilter(BaseResourceGroupFilter):
+    class Meta(BaseResourceGroupFilter.Meta):
+        model = models.VirtualMachine
+
+
+class PublicIPFilter(BaseResourceGroupFilter):
+    class Meta(BaseResourceGroupFilter.Meta):
+        model = models.PublicIP
+
+
+class SQLServerFilter(BaseResourceGroupFilter):
+    class Meta(BaseResourceGroupFilter.Meta):
+        model = models.SQLServer
+
+
+class SQLDatabaseFilter(BaseResourceGroupFilter):
+    class Meta(BaseResourceGroupFilter.Meta):
+        model = models.SQLDatabase
+
+    server = core_filters.URLFilter(
+        view_name='azure-server-detail',
+        name='server__uuid'
+    )
+    server_uuid = django_filters.UUIDFilter(name='server__uuid')
