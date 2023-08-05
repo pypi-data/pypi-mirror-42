a664c126ec21d92352eb72e39f4ebb956116aa27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:09 +0000

    Add package kpc-nifi-utils/0.1.22/kpc_nifi_utils-0.1.22-py3-none-any.whl

---
+++ packages/kpc-nifi-utils/kpc_nifi_utils-0.1.22-py3-none-any.whl/kpc_nifi_utils/common/json_encoder.py
@@ -0,0 +1,25 @@
+import datetime
+import decimal
+import json
+
+from bson import ObjectId
+
+
+class EnhancedJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            return obj.isoformat()
+        elif isinstance(obj, datetime.date):
+            return obj.isoformat()
+        elif isinstance(obj, datetime.time):
+            return obj.isoformat()
+        elif isinstance(obj, datetime.timedelta):
+            return obj.seconds
+        elif isinstance(obj, decimal.Decimal):
+            return float(str(obj))
+        elif isinstance(obj, ObjectId):
+            return str(obj)
+        elif isinstance(obj, bytes):
+            return obj.decode(encoding="utf8", errors='ignore')
+        else:
+            return super().default(obj)
