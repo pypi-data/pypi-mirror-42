4cda4f79ee9ed1d9265a44323c29c3b50e8fd6ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:35 +0000

    Add package tor/1.0.0/tor-1.0.0.tar.gz

---
+++ packages/tor/tor-1.0.0.tar.gz/tor-1.0.0/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import *
+
+setup(
+    name="tor",
+    version="1.0.0",
+    description="A Simple, Lightweight and Easy To Use, TOR Proxy Client Library For Python",
+    author="petrexxy",
+    author_email="psmall.775@gmail.com",
+    url="https://github.com/noctua-github",
+    packages=["tor"],
+    classifiers=[
+              "Programming Language :: Python",
+#              "Intended Audience :: Penetration Testers/Developers",
+ #             "Topic :: Networking"
+    ]
+)
