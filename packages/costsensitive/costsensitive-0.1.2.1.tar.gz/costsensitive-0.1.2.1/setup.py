bb83b6329f8cf396f43662509868fb8b48cbebe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:09 +0000

    Add package costsensitive/0.1.2.1/costsensitive-0.1.2.1.tar.gz

---
+++ packages/costsensitive/costsensitive-0.1.2.1.tar.gz/costsensitive-0.1.2.1/setup.py
@@ -0,0 +1,29 @@
+try:
+  from setuptools import setup
+except:
+  from distutils.core import setup
+import numpy as np
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+setup(
+  name = 'costsensitive',
+  packages = ['costsensitive'],
+  install_requires=[
+   'numpy',
+   'scipy',
+   'joblib>=0.13',
+   'cython'
+],
+  python_requires = ">=3",
+  version = '0.1.2.1',
+  description = 'Reductions for Cost-Sensitive Multi-Class Classification',
+  author = 'David Cortes',
+  author_email = 'david.cortes.rivera@gmail.com',
+  url = 'https://github.com/david-cortes/costsensitive',
+  keywords = ['cost sensitive multi class', 'cost-sensitive multi-class classification', 'weighted all pairs', 'filter tree'],
+  classifiers = [],
+
+  cmdclass = {'build_ext': build_ext},
+  ext_modules = [Extension("costsensitive._vwrapper", sources=["costsensitive/vwrapper.pyx"], include_dirs=[np.get_include()], extra_link_args=["-fopenmp"], extra_compile_args=["-O2","-fopenmp"])]
+)
