839677953f95d872d8c9837700a7a74d449279de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:54 +0000

    Add package aidu/0.0.1/aidu-0.0.1.tar.gz

---
+++ packages/aidu/aidu-0.0.1.tar.gz/aidu-0.0.1/aidu/aidu.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""AIDU - TODO
+"""
+
+def main():
+    """Main (standalone executable?)
+    """
+    print("AIDU")
+
+if __name__ == "__main__":
+    main()
