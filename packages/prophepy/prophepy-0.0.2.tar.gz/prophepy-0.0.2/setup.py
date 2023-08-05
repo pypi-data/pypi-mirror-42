7df017e9d8d57a2dc1aaa431afa22201775bafcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:57 +0000

    Add package prophepy/0.0.2/prophepy-0.0.2.tar.gz

---
+++ packages/prophepy/prophepy-0.0.2.tar.gz/prophepy-0.0.2/setup.py
@@ -0,0 +1,28 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="prophepy",
+    version="0.0.2",
+    description="Python mocks made for humans",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Einenlum/prophepy",
+    author="Einenlum",
+    author_email="yann.rabiller@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["prophepy"],
+    package_dir={'prophepy': 'prophepy'},
+    include_package_data=True,
+    install_requires=[]
+)
