f1492fb5464770a6fb939a7305eb7d387668acdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:23 +0000

    Add package FolderAnalyse/0.1.5/FolderAnalyse-0.1.5-py3-none-any.whl

---
+++ packages/FolderAnalyse/FolderAnalyse-0.1.5-py3-none-any.whl/FolderAnalyse/fileparser.py
@@ -0,0 +1,172 @@
+"""
+Ryan Pepper (2018)
+
+fileparser.py
+
+This file contains a parser function that processes a text file.
+
+"""
+import re
+import logging
+
+
+def _read(filename, encodings=['utf-8', 'utf-16', 'ascii', 'latin-1']):
+    """
+    _read(filename)
+
+    Try to read files and return the text regardless of their encoding.
+    Raises UnicodeError on failure.
+    
+    Input:
+    
+    filename, str
+        File to be read
+    
+    Output:
+        str:
+            Contents of the file
+    """
+    text = None
+    
+    
+    for encoding in encodings:
+        try:
+            f = open(filename, encoding=encoding)
+            text = f.read()
+        except UnicodeDecodeError:
+            f.close()
+        except FileNotFoundError:
+            raise FileNotFoundError("Could not open file.")
+
+    if not text:
+        raise UnicodeError(filename)
+
+    f.close()
+    return text
+    
+def parse(filename, case_sensitive=False, sort=False):
+    """
+    parse(filename, case_sensitive=False, sort=False)
+    
+    Opens a file, and reads it line by line, returning a dictionary
+    containing key-value pairs of words and their frequency in the file.
+    Note: newline characters are *always* removed from the file.
+
+    Inputs:
+
+    filename, str
+        The file to be calculate word frequencies.
+
+    Examples:
+
+    >>> text = "The quick brown fox jumped over the lazy dog."
+    >>> f = open('example.txt', 'w')
+    >>> f.write(text)
+    >>> f.close()
+    >>> FolderAnalyse.fileparser.parse('example.txt', case_sensitive=False)
+    {'the': 2, 'quick': 1, 'brown': 1, 'fox': 1, 'jumped': 1,
+     'over: 1, 'the': 1, 'lazy': 1, 'dog.': 1}
+
+    >>> FolderAnalyse.fileparser.parse('example.txt', case_sensitive=False)
+    {'the': 2, 'quick': 1, 'brown': 1, 'fox': 1, 'jumped': 1,
+     'over: 1, 'the': 1, 'lazy': 1, 'dog.': 1}
+    """
+
+
+    text = _read(filename)
+
+
+    # If case_sensitive is true we make the text all lower case.
+    if not case_sensitive:
+        text = text.lower()
+
+    words = re.findall(r'\w*[\'-]*\w', text)
+
+    frequency_dict = {word: 0 for word in words}
+    # Do it this way rather than using count, because it avoids
+    # doing len(words) lookups over the whole text.
+    for word in words:
+        frequency_dict[word] += 1
+
+    if sort:
+        # Sort the dictionary by the values. Dictionarys are ordered in Python
+        # 3.6 and above but that is not the case for old versions where you
+        # must use collections.OrderedDict.
+        frequency_dict = sort_dict(frequency_dict)
+
+
+    return frequency_dict
+
+
+def sort_dict(frequency_dict, reverse=True):
+    """
+    sort_dict(dictionary)
+
+    Sorts a dictionary based on the numerical values stored in the dict.
+
+    Inputs:
+
+    dictionary, dict
+        Dictionary with integer values.
+
+    Examples:
+
+    >>> a = {'a': 2, 'b': 1, 'c': 5}
+    >>> FolderAnalyse.fileparser.sort_dict(a, reverse=True)
+    {'c': 5, 'a': 2, 'b': 1}
+    >>> FolderAnalyse.fileparser.sort_dict(a, reverse=False)
+    {'b': 1, 'a': 2, 'c': 5}
+
+    Notes:
+
+    Dictionarys are ordered in Python 3.6 and above but that is not the
+    case for old versions where you must use collections.OrderedDict.
+    """
+    sorted_dict_keys = sorted(frequency_dict, key=frequency_dict.get,
+                              reverse=reverse)
+    sorted_dict = {key: frequency_dict[key] for key in sorted_dict_keys}
+    return sorted_dict
+
+
+def combine_dicts(dicts_list):
+    """
+    combine_dicts(dicts_list)
+
+    Combines dictionaries by summing the numerical values of any keys
+    which are shared between them.
+
+    Inputs:
+
+    dicts_list, list
+        A list containing dictionaries with integer values.
+
+    Example:
+
+    >>> a = {'a': 2, 'b': 1, 'c': 5}
+    >>> b = {'b': 4, 'c': 12, 'e': 4}
+    >>> FolderAnalyse.parser.combine_dicts([a, b])
+    {'a': 2, 'b': 5, 'c': 17, 'e': 4}
+    """
+
+    # Combine all dictionarys keys into a single
+    # list and find the unique set of them.
+    all_keys = []
+    for freq_dict in dicts_list:
+        all_keys += list(freq_dict.keys())
+    keys = set(all_keys)
+
+    # Generate the new dictionary with all keys
+    combined_dict = {key: 0 for key in keys}
+
+    # Iterate over the list of keys so that
+    # the memory access pattern to the combined_dict
+    # avoids jumping around. If key is not found in
+    # a given fdict, just pass over.
+    for key in keys:
+        for fdict in dicts_list:
+            try:
+                combined_dict[key] += fdict[key]
+            except:
+                pass
+
+    return combined_dict
---
+++ packages/FolderAnalyse/FolderAnalyse-0.1.5-py3-none-any.whl/FolderAnalyse/script.py
@@ -0,0 +1,124 @@
+"""
+Ryan Pepper (2018)
+
+This script contains the main entrypoint to the folder-analyse application.
+
+"""
+
+import argparse
+import os
+import sys
+import FolderAnalyse
+from FolderAnalyse.process import process_dir, process_file
+
+
+def _get_parser():
+    """
+    _get_parser()
+
+    Construct a parser using argparse to collect user supplied
+    arguments. This is designed to be called from the entrypoint
+    specified in setup.py
+    """
+
+    parser = argparse.ArgumentParser(
+            description="%(prog)s  [options]\n"
+                        "Produce word statistics for files in a directory."
+                         )
+
+    parser.add_argument('path', type=str,
+                        help="path to a directory or a file.")
+
+    parser.add_argument('-t', '--type', type=str, default='txt',
+                        help="if path is to a directory, file extension of\n"
+                             "files that to be processed.")
+
+    parser.add_argument('-c', '--case', action='store_false',
+                        help="by default, processing is not case sensitive.\n"
+                             "Add this flag to make it case sensitive")
+
+    parser.add_argument('-N', '--number', type=int, default=10,
+                        help="show the top N frequencies for the file(s) and\n"
+                             "directory.")
+
+    parser.add_argument('-s', '--save', type=str,
+                        help="save the statistics report to the filename.")
+
+    parser.add_argument('-r', '--runtests', action='store_true',
+                        help="Ignore all other options and run the tests for"
+                             "the module")
+    return parser
+
+
+def _exit(message, parser):
+    """
+    exit(error, parser)
+
+    Quits the running application and prints the parser help, before printing
+    the specified error message.
+    """
+
+    parser.print_help()
+    print(f"\n\nError: {message}. See above for help.")
+    sys.exit()
+
+
+def main():
+    """
+    Main func
+    """
+    parser = _get_parser()
+    args = parser.parse_args()
+
+    if args.runtests:
+        FolderAnalyse.runtests()
+        sys.exit()
+
+    path = args.path
+    extension = args.type
+    case = args.case
+
+    N = args.number
+
+    directory = os.path.isdir(path)
+    file = os.path.isfile(path)
+
+    if file:
+        try:
+            stats_text, freq_dict = process_file(path, N, case)
+
+        except FileNotFoundError:
+            _exit(f"File {path} not found.",
+                 parser)
+
+        except UnicodeError:
+            _exit(f"Could not open file {e.args[0]} as encoding could not be "
+                   "detected.")
+
+    elif directory:
+        try:
+            stats_text, freq_dicts, combined_dict = process_dir(path, 
+                                                                extension,
+                                                                N, case)
+
+        except FileNotFoundError:
+            _exit(f"No files with extension {extension} found in directory",
+                 parser)
+        except UnicodeError as e:
+            _exit(f"Could not open file {e.args[0]} as encoding could not be "
+                   "detected and could not be processed.")
+
+    else:
+         _exit("File or directory does not exist", parser)
+
+    print(stats_text)
+
+    if args.save:
+        f = open(args.save, 'w')
+        f.write(stats_text)
+        f.close()
+        print(f"\nSaved report to \"{args.save}.\"")
+
+
+if __name__ == '__main__':
+    main()
