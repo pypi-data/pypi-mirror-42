124d0535a1c108bb95df159442479c1959a9c0a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:29 +0000

    Add package jsop/0.7.1/jsop-0.7.1.tar.gz

---
+++ packages/jsop/jsop-0.7.1.tar.gz/jsop-0.7.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='jsop',
+    version='0.7.1',
+    author='Hagai Helman Tov',
+    author_email='hagai.helman@gmail.com',
+    description='A way to store large amounts of JSON-style data on disk, and to access it quickly.',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/hagai-helman/jsop',
+    keywords=['JSON', 'dbm', 'persistence'],
+    py_modules=['jsop'],
+    license='MIT',
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        ],
+)
