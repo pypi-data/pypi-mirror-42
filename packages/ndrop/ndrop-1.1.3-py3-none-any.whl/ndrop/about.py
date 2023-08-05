44f4f70c20fea4f69216031bb356a25e20033bcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:47 +0000

    Add package ndrop/1.1.3/ndrop-1.1.3-py3-none-any.whl

---
+++ packages/ndrop/ndrop-1.1.3-py3-none-any.whl/ndrop/about.py
@@ -0,0 +1,8 @@
+
+name = 'ndrop'
+version = '1.1.3'
+author = 'Yugang LIU'
+email = 'liuyug@gmail.com'
+url = 'https://github.com/liuyug/ndrop.git'
+license = 'GPLv2'
+description ='a File Transfer Tool. Support "Dukto"'
