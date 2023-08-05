a15d43bc0769f1cc3c484ec12598b83d2561ecce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:46 +0000

    Add package Y2T/1.2/Y2T-1.2.tar.gz

---
+++ packages/Y2T/Y2T-1.2.tar.gz/Y2T-1.2/Y2T/Y2T.py
@@ -0,0 +1,69 @@
+# coding: utf8
+
+###########
+# IMPORTS #
+###########
+
+import os
+from Playlist import *
+from Presentation import *
+from Nfo import *
+from Torrent import *
+from __init__ import ydl_opts, log
+
+
+#############
+# CONSTANTS #
+#############
+
+nfoPackage = "mediainfo"
+torrentPackage = "transmission-cli"
+
+#########
+# CLASS #
+#########
+
+class Upload:
+
+	############
+	# ATRIBUTS #
+	############
+
+	playlist = None
+	presentation = None
+	nfo = None
+	torrent = None
+
+	###############
+	# CONSTRUCTOR #
+	###############
+
+	def __init__(self, playlistUrl, artist, coverUrl, description, videoUrl, videoDescription, format, tracker):
+		self.playlist = Playlist(playlistUrl, artist, format)
+		self.presentation = Presentation(artist, coverUrl, description, videoUrl, videoDescription, artist, format)
+		self.nfo = Nfo()
+		self.torrent = Torrent(tracker)
+
+	###########
+	# METHODS #
+	###########
+	
+	def upload(self, album, cover, year=None, month=None, maximumDuration=600):
+
+		#Téléchargement
+		self.playlist.download(album, cover, year, month, maximumDuration)
+
+		#Création de la presentation	
+		self.presentation.create(album)
+
+		#Creation du nfo
+		if(nfoPackage in os.popen("dpkg -l | grep " + nfoPackage).read()):
+			self.nfo.create(album)
+		else:
+			log.warning("Impossible de créer le fichier \"" + album + ".nfo\". Vous devez installer " + nfoPackage)
+
+		#Création du .torrent
+		if(torrentPackage in os.popen("dpkg -l | grep " + torrentPackage).read()):
+			self.torrent.create(album)
+		else:
+			log.warning("Impossible de créer le fichier \"" + album + ".torrent\". Vous devez installer " + torrentPackage)
---
+++ packages/Y2T/Y2T-1.2.tar.gz/Y2T-1.2/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='Y2T',
+      version='1.2',
+      author='Esisar Pro-G',
+      author_email='esisar.pro.g@gmail.com',
+      description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/Esisar-Pro-G/Y2T',
+      packages=setuptools.find_packages(),
+      install_requires=['beautifulsoup4 ', 'youtube-dl', 'mutagen'],
+      platforms='Linux',
+     )
