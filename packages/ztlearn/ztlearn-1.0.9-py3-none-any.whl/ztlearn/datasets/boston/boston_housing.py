b48adec48820ddd253880ca14b61fbc2dc1c6a4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:17 +0000

    Add package ztlearn/1.0.9/ztlearn-1.0.9-py3-none-any.whl

---
+++ packages/ztlearn/ztlearn-1.0.9-py3-none-any.whl/ztlearn/datasets/boston/boston_housing.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+from ztlearn.utils import maybe_download
+from ztlearn.utils import train_test_split
+from ztlearn.datasets.data_set import DataSet
+
+URL = 'https://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.data'
+
+def fetch_boston(data_target = True):
+    file_path = maybe_download('../../ztlearn/datasets/boston/', URL)
+    describe  = [
+        'CRIM',
+        'ZN',
+        'INDUS',
+        'CHAS',
+        'NOX',
+        'RM',
+        'AGE',
+        'DIS',
+        'RAD',
+        'TAX',
+        'PTRATIO',
+        'B',
+        'LSTAT',
+        'MEDV'
+    ]
+
+    dataframe    = pd.read_csv(file_path, delim_whitespace = True, names = describe)
+    data, target = dataframe.values[:,0:13], dataframe.values[:,13]
+
+    if data_target:
+        return DataSet(data, target, describe)
+    else:
+        return train_test_split(data, target, test_size = 0.2, random_seed = 2)
---
+++ packages/ztlearn/ztlearn-1.0.9-py3-none-any.whl/ztlearn/datasets/digits/digits.py
@@ -0,0 +1,21 @@
+import gzip
+import numpy as np
+
+from ztlearn.utils import maybe_download
+from ztlearn.utils import train_test_split
+from ztlearn.datasets.data_set import DataSet
+
+URL = 'https://github.com/scikit-learn/scikit-learn/raw/master/sklearn/datasets/data/digits.csv.gz'
+
+def fetch_digits(data_target = True):
+    file_path = maybe_download('../../ztlearn/datasets/digits/', URL)
+
+    with gzip.open(file_path, 'rb') as digits_path:
+        digits_data = np.loadtxt(digits_path, delimiter=',')
+
+    data, target = digits_data[:, :-1], digits_data[:, -1].astype(np.int)
+
+    if data_target:
+        return DataSet(data, target)
+    else:
+        return train_test_split(data, target, test_size = 0.33, random_seed = 5)
