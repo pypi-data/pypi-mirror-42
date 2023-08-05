18990c5df97ff504466e762f266cc53e31b56d34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:01 +0000

    Add package tomtoolkit/0.0.2/tomtoolkit-0.0.2.tar.gz

---
+++ packages/tomtoolkit/tomtoolkit-0.0.2.tar.gz/tomtoolkit-0.0.2/tom_targets/forms.py
@@ -0,0 +1,113 @@
+from django import forms
+from django.forms.models import inlineformset_factory
+from astropy.coordinates import Angle
+from astropy import units as u
+from django.forms import ValidationError
+from django.conf import settings
+
+from .models import Target, TargetExtra, SIDEREAL_FIELDS, NON_SIDEREAL_FIELDS, REQUIRED_SIDEREAL_FIELDS
+from .models import REQUIRED_NON_SIDEREAL_FIELDS
+
+
+def extra_field_to_form_field(field_type):
+    if field_type == 'number':
+        return forms.FloatField(required=False)
+    elif field_type == 'boolean':
+        return forms.BooleanField(required=False)
+    elif field_type == 'datetime':
+        return forms.DateTimeField(required=False)
+    elif field_type == 'string':
+        return forms.CharField(required=False)
+    else:
+        raise ValueError(
+            'Invalid field type {}. Field type must be one of: number, boolean, datetime string'.format(field_type)
+        )
+
+
+class CoordinateField(forms.CharField):
+    def __init__(self, *args, **kwargs):
+        c_type = kwargs.pop('c_type')
+        self.c_type = c_type
+        super().__init__(*args, **kwargs)
+
+    def to_python(self, value):
+        try:
+            a = float(value)
+            return a
+        except ValueError:
+            try:
+                if self.c_type == 'ra':
+                    a = Angle(value, unit=u.hourangle)
+                else:
+                    a = Angle(value, unit=u.degree)
+                return a.to(u.degree).value
+            except Exception as e:
+                raise ValidationError('Invalid format. Please use sexigesimal or degrees')
+
+
+class TargetForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for extra_field in settings.EXTRA_FIELDS:
+            # Add extra fields to the form
+            field_name = extra_field['name']
+            self.fields[field_name] = extra_field_to_form_field(extra_field['type'])
+            # Populate them with initial values if this is an update
+            if kwargs['instance']:
+                te = TargetExtra.objects.filter(target=kwargs['instance'], key=field_name)
+                if te.exists():
+                    self.fields[field_name].initial = te.first().typed_value(extra_field['type'])
+
+    def save(self, commit=True):
+        instance = super().save(commit=commit)
+        if commit:
+            for field in settings.EXTRA_FIELDS:
+                if self.cleaned_data.get(field['name']) is not None:
+                    TargetExtra.objects.update_or_create(
+                            target=instance,
+                            key=field['name'],
+                            defaults={'value': self.cleaned_data[field['name']]}
+                    )
+        return instance
+
+    class Meta:
+        abstract = True
+        model = Target
+        fields = '__all__'
+        widgets = {'type': forms.HiddenInput()}
+
+
+class SiderealTargetCreateForm(TargetForm):
+    ra = CoordinateField(required=True, label='Right Ascension', c_type='ra')
+    dec = CoordinateField(required=True, label='Declination', c_type='dec')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in REQUIRED_SIDEREAL_FIELDS:
+            self.fields[field].required = True
+
+    class Meta(TargetForm.Meta):
+        fields = SIDEREAL_FIELDS
+
+
+class NonSiderealTargetCreateForm(TargetForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in REQUIRED_NON_SIDEREAL_FIELDS:
+            self.fields[field].required = True
+
+    class Meta(TargetForm.Meta):
+        fields = NON_SIDEREAL_FIELDS
+
+
+class TargetVisibilityForm(forms.Form):
+    start_time = forms.DateTimeField(required=True, label='Start Time', widget=forms.TextInput(attrs={'type': 'date'}))
+    end_time = forms.DateTimeField(required=True, label='End Time', widget=forms.TextInput(attrs={'type': 'date'}))
+    airmass = forms.DecimalField(required=False, label='Maximum Airmass')
+
+    def clean(self):
+        cleaned_data = super().clean()
+        start_time = cleaned_data.get('start_time')
+        end_time = cleaned_data.get('end_time')
+        if end_time < start_time:
+            raise forms.ValidationError('Start time must be before end time')
