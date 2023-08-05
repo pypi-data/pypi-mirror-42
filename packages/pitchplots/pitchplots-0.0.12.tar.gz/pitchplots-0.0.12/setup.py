c1023ea59812893ec743251aa68dac0c1a1c0325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:57 +0000

    Add package pitchplots/0.0.12/pitchplots-0.0.12.tar.gz

---
+++ packages/pitchplots/pitchplots-0.0.12.tar.gz/pitchplots-0.0.12/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pitchplots",
+    version="0.0.12",
+    author="Fabian Moss",
+    author_email="fabian.moss@epfl.ch",
+    description="A package containing representation tools for musical purposes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    url="https://github.com/DCMLab/pitchplots",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
