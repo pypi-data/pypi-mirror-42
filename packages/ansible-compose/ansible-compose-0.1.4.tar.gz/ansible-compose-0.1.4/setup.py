d6c96679dcf098efae6aabb405f75be2bcd91d9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:04 +0000

    Add package ansible-compose/0.1.4/ansible-compose-0.1.4.tar.gz

---
+++ packages/ansible-compose/ansible-compose-0.1.4.tar.gz/ansible-compose-0.1.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ansible-compose',
+    setup_requires='setupmeta',
+    versioning='post',
+    description='The obscene docker-compose deploy with ansible cli',
+    author='James Pic',
+    author_email='jamespic@gmail.com',
+    url='https://yourlabs.io/oss/ansible-compose',
+    include_package_data=True,
+)
