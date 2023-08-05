951c3d68866124b0f578e83e31ed9f13e2175674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:15 +0000

    Add package featuretools/0.6.1/featuretools-0.6.1.tar.gz

---
+++ packages/featuretools/featuretools-0.6.1.tar.gz/featuretools-0.6.1/featuretools/primitives/standard/cum_transform_feature.py
@@ -0,0 +1,97 @@
+from featuretools.primitives.base import TransformPrimitive
+from featuretools.variable_types import Discrete, Id, Numeric
+
+
+class CumSum(TransformPrimitive):
+    """Returns the cumulative sum after grouping"""
+
+    name = "cum_sum"
+    input_types = [[Numeric, Id],
+                   [Numeric, Discrete]]
+    return_type = Numeric
+    uses_full_entity = True
+
+    def get_function(self):
+        def cum_sum(values, groups):
+            return values.groupby(groups).cumsum()
+
+        return cum_sum
+
+    def generate_name(self, base_feature_names):
+        return "CUM_SUM(%s by %s)" % (base_feature_names[0], base_feature_names[1])
+
+
+class CumCount(TransformPrimitive):
+    """Returns the cumulative count after grouping"""
+
+    name = "cum_count"
+    input_types = [[Id], [Discrete]]
+    return_type = Numeric
+    uses_full_entity = True
+
+    def get_function(self):
+        def cum_count(values):
+            return values.groupby(values).cumcount() + 1
+
+        return cum_count
+
+    def generate_name(self, base_feature_names):
+        return "CUM_COUNT(%s)" % (base_feature_names[0])
+
+
+class CumMean(TransformPrimitive):
+    """Returns the cumulative mean after grouping"""
+
+    name = "cum_mean"
+    input_types = [[Numeric, Id],
+                   [Numeric, Discrete]]
+    return_type = Numeric
+    uses_full_entity = True
+
+    def get_function(self):
+        def cum_mean(values, groups):
+            temp = values.groupby(groups)
+            return temp.cumsum() / (temp.cumcount() + 1)
+
+        return cum_mean
+
+    def generate_name(self, base_feature_names):
+        return "CUM_MEAN(%s by %s)" % (base_feature_names[0], base_feature_names[1])
+
+
+class CumMin(TransformPrimitive):
+    """Returns the cumulative min after grouping"""
+
+    name = "cum_min"
+    input_types = [[Numeric, Id],
+                   [Numeric, Discrete]]
+    return_type = Numeric
+    uses_full_entity = True
+
+    def get_function(self):
+        def cum_min(values, groups):
+            return values.groupby(groups).cummin()
+
+        return cum_min
+
+    def generate_name(self, base_feature_names):
+        return "CUM_MIN(%s by %s)" % (base_feature_names[0], base_feature_names[1])
+
+
+class CumMax(TransformPrimitive):
+    """Returns the cumulative max after grouping"""
+
+    name = "cum_max"
+    input_types = [[Numeric, Id],
+                   [Numeric, Discrete]]
+    return_type = Numeric
+    uses_full_entity = True
+
+    def get_function(self):
+        def cum_max(values, groups):
+            return values.groupby(groups).cummax()
+
+        return cum_max
+
+    def generate_name(self, base_feature_names):
+        return "CUM_MAX(%s by %s)" % (base_feature_names[0], base_feature_names[1])
---
+++ packages/featuretools/featuretools-0.6.1.tar.gz/featuretools-0.6.1/featuretools/primitives/utils.py
@@ -0,0 +1,62 @@
+from inspect import isclass
+
+import pandas as pd
+
+import featuretools
+
+
+def get_aggregation_primitives():
+    aggregation_primitives = set([])
+    for attribute_string in dir(featuretools.primitives):
+        attribute = getattr(featuretools.primitives, attribute_string)
+        if isclass(attribute):
+            if issubclass(attribute,
+                          featuretools.primitives.AggregationPrimitive):
+                if attribute.name:
+                    aggregation_primitives.add(attribute)
+    return {prim.name.lower(): prim for prim in aggregation_primitives}
+
+
+def get_transform_primitives():
+    transform_primitives = set([])
+    for attribute_string in dir(featuretools.primitives):
+        attribute = getattr(featuretools.primitives, attribute_string)
+        if isclass(attribute):
+            if issubclass(attribute,
+                          featuretools.primitives.TransformPrimitive):
+                if attribute.name:
+                    transform_primitives.add(attribute)
+    return {prim.name.lower(): prim for prim in transform_primitives}
+
+
+def list_primitives():
+    trans_names, trans_primitives = _get_names_primitives(get_transform_primitives)
+    transform_df = pd.DataFrame({'name': trans_names,
+                                 'description': _get_descriptions(trans_primitives)})
+    transform_df['type'] = 'transform'
+
+    agg_names, agg_primitives = _get_names_primitives(get_aggregation_primitives)
+    agg_df = pd.DataFrame({'name': agg_names,
+                           'description': _get_descriptions(agg_primitives)})
+    agg_df['type'] = 'aggregation'
+
+    return pd.concat([agg_df, transform_df], ignore_index=True)[['name', 'type', 'description']]
+
+
+def _get_descriptions(primitives):
+    descriptions = []
+    for prim in primitives:
+        description = ''
+        if prim.__doc__ is not None:
+            description = prim.__doc__.split("\n")[0]
+        descriptions.append(description)
+    return descriptions
+
+
+def _get_names_primitives(primitive_func):
+    names = []
+    primitives = []
+    for name, primitive in primitive_func().items():
+        names.append(name)
+        primitives.append(primitive)
+    return names, primitives
