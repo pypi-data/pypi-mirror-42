35daa8cc7da178ca5f92f2546a06f7ea45b65b51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:34 +0000

    Add package ssh-docker/1.2.2/ssh_docker-1.2.2-py3-none-any.whl

---
+++ packages/ssh-docker/ssh_docker-1.2.2-py3-none-any.whl/sshdocker/client.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import paramiko
+from sshdocker import ssh
+
+
+class Client(object):
+    hostname: str = None
+    paramiko_client: paramiko.SSHClient = None
+    cache: dict = None
+
+    def __init__(self, hostname, username: str, interactive: bool = False):
+        self.cache = {
+            'containers': []
+        }
+        self.paramiko_client = ssh.get_client(
+            hostname, username=username, interactive=True)
+        self.hostname = hostname
+
+    def close(self):
+        self.paramiko_client.close()
+
+    def __call__(self, *args, **kwargs):
+        return self.paramiko_client.exec_command(*args, **kwargs)
