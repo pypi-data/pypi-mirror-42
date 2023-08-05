cec3dfdc30f2ef6f9e94a18bfc071e2bad9324d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:39 +0000

    Add package TaiwanTrainVerificationCode2text/1.0.7/TaiwanTrainVerificationCode2text-1.0.7.tar.gz

---
+++ packages/TaiwanTrainVerificationCode2text/TaiwanTrainVerificationCode2text-1.0.7.tar.gz/TaiwanTrainVerificationCode2text-1.0.7/TaiwanTrainVerificationCode2text/download.py
@@ -0,0 +1,23 @@
+
+
+import urllib
+import os
+
+PATH = "/".join( os.path.abspath(__file__).split('/')[:-1])
+
+def weight():
+
+    url = 'https://raw.githubusercontent.com/linsamtw/TaiwanTrainVerificationCode2text/master/cnn_weight/verificatioin_code.h5'  
+    os.makedirs('cnn_weight')
+    urllib.request.urlretrieve(url, '/{}/{}/verificatioin_code.h5'.format(PATH,'cnn_weight')) 
+
+
+def ttf():
+    
+    url = 'https://raw.githubusercontent.com/linsamtw/TaiwanTrainVerificationCode2text/master/Courier-BoldRegular.ttf'  
+    urllib.request.urlretrieve(url, '/{}/Courier-BoldRegular.ttf'.format(PATH)) 
+
+    url = 'https://raw.githubusercontent.com/linsamtw/TaiwanTrainVerificationCode2text/master/Times%20Bold.ttf'  
+    urllib.request.urlretrieve(url, '/{}/Times Bold.ttf'.format(PATH)) 
+
+
