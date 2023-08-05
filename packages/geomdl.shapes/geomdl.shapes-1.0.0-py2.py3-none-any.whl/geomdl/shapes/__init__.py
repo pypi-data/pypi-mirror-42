a46685ee6da089ca8289b94c824d0bed2c61992e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:28 +0000

    Add package geomdl.shapes/1.0.0/geomdl.shapes-1.0.0-py2.py3-none-any.whl

---
+++ packages/geomdl.shapes/geomdl.shapes-1.0.0-py2.py3-none-any.whl/geomdl/shapes/__init__.py
@@ -0,0 +1,19 @@
+""" Shapes Module for NURBS-Python (geomdl)
+
+.. moduleauthor:: Onur Rauf Bingol <orbingol@gmail.com>
+
+"""
+
+# Module version
+__version__ = "1.0.0"
+
+# Author and license
+__author__ = "Onur Rauf Bingol"
+__license__ = "MIT"
+
+
+# Support for "from geomdl.shapes import *"
+__all__ = [
+    'curve2d',
+    'surface'
+]
