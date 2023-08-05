13e0ab2a73359deaff6a8cf227c5de7fae0c985f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:03 +0000

    Add package damefunniest/0.1/damefunniest-0.1.tar.gz

---
+++ packages/damefunniest/damefunniest-0.1.tar.gz/damefunniest-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='damefunniest',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/davidam/damefunniest',
+      author='David Arroyo Menéndez',
+      author_email='davidam@gnu.org',
+      license='GPLv3',
+      packages=['damefunniest'],
+      zip_safe=False)
