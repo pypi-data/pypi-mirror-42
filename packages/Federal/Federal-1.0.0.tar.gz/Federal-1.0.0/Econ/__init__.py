7a2d3759bb23adc74a46bf47931698b09366383b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:56 +0000

    Add package Federal/1.0.0/Federal-1.0.0.tar.gz

---
+++ packages/Federal/Federal-1.0.0.tar.gz/Federal-1.0.0/Econ/__init__.py
@@ -0,0 +1,3 @@
+# __init__.py
+from .GDP import *
+from .Unemployment import *
---
+++ packages/Federal/Federal-1.0.0.tar.gz/Federal-1.0.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+INSTALL_REQUIRES = (
+    ['pandas>=0.19.2', 'pandas-datareader=0.7.0', 're', 'datetime']
+)
+
+setuptools.setup(
+    name='Federal',
+    version='1.0.0',
+    author='Jeremy A. Seibert',
+    author_email='Jaseibert2@eagles.usi.edu',
+    description="A wrapper on to the pandas-datareader package for easier handling of federal reserve (FRED) data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Jaseibert/Federal",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
