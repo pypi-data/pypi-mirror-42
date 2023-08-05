93e5912f471ffbca34123c8c5836d156244b03bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:45 +0000

    Add package monki/0.1.0/monki-0.1.0.tar.gz

---
+++ packages/monki/monki-0.1.0.tar.gz/monki-0.1.0/setup.py
@@ -0,0 +1,24 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="monki",
+    version="0.1.0",
+    description="Easily patch functions at runtime.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/avivc/monki",
+    author="Aviv Cohn",
+    author_email="avivcohn1@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["monki"],
+    include_package_data=True
+)
