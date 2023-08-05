b998ea883d64e97abf60b21e47f99e2a455f0a00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:42 +0000

    Add package testlinkmap/1.0.1.2/testlinkmap-1.0.1.2.tar.gz

---
+++ packages/testlinkmap/testlinkmap-1.0.1.2.tar.gz/testlinkmap-1.0.1.2/testlinkmap/utils/__init__.py
@@ -0,0 +1,9 @@
+#! /usr/local/bin/python3
+
+
+# from . import *
+from .print_with_color import *
+from .time_profile import *
+
+# __all__ = ['print_with_color', 'time_profile']
+__version__ = '1.0'
