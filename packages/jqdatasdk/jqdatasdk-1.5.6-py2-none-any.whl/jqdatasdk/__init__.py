c59db018924433f162faa5c0e3717bb243c35dd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:26 +0000

    Add package jqdatasdk/1.5.6/jqdatasdk-1.5.6-py2-none-any.whl

---
+++ packages/jqdatasdk/jqdatasdk-1.5.6-py2-none-any.whl/jqdatasdk/__init__.py
@@ -0,0 +1,87 @@
+# coding=utf-8
+import sys
+import os
+sys.modules["ROOT_DIR"] = os.path.abspath(os.path.dirname(__file__))
+
+from .api import *
+from .finance_service import *
+from . import alpha101
+from . import alpha191
+from . import technical_analysis
+from .table import macro, finance, opt
+
+
+def auth(username, password, host="39.107.190.114", port=7000):
+    from .client import JQDataClient
+    JQDataClient.set_auth_params(host=host, port=port, username=username, password=password)
+
+
+def auth_by_token(token, host="39.107.190.114", port=7000):
+    from .client import JQDataClient
+    JQDataClient.set_auth_params(host=host, port=port, token=token)
+
+
+__version__ = "1.5.6"
+
+
+__all__ = [
+    "__version__",
+    "auth",
+    "auth_by_token",
+    "get_price",
+    "get_trade_days",
+    "get_all_trade_days",
+    "get_extras",
+    "get_index_stocks",
+    "get_industry_stocks",
+    "get_industries",
+    "get_industry",
+    "get_concept_stocks",
+    "get_concepts",
+    "get_all_securities",
+    "get_security_info",
+    "get_money_flow",
+    "get_fundamentals",
+    "get_fundamentals_continuously",
+    "get_billboard_list",
+    "get_locked_shares",
+    "get_mtss",
+    "get_margincash_stocks",
+    "get_marginsec_stocks",
+    "get_future_contracts",
+    "get_dominant_future",
+    "normalize_code",
+    "query",
+    "balance",
+    "income",
+    "cash_flow",
+    "indicator",
+    "bank_indicator",
+    "security_indicator",
+    "insurance_indicator",
+    "valuation",
+    "macro",
+    "alpha101",
+    "alpha191",
+    "technical_analysis",
+    "get_baidu_factor",
+    "get_ticks",
+    "finance",
+    "get_factor_values",
+    "get_index_weights",
+    "get_bars",
+    "get_current_tick",
+    "get_fund_info",
+    "get_query_count",
+    "get_price_engine",
+    "history_engine",
+    "attribute_history_engine",
+    "get_bars_engine",
+    "get_ticks_engine",
+    "get_current_tick_engine",
+    "get_daily_info_engine",
+    "opt",
+    "get_last_price"
+]
+
+# __all__.extend(fin.__all__)
