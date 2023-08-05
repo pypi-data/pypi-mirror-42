c50d4a2956029d4645a442e48dcaee1a6d0d5fad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:58 +0000

    Add package dss-python/0.10.1/dss_python-0.10.1-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/dss-python/dss_python-0.10.1-cp36-cp36m-manylinux1_x86_64.whl/dss/__init__.py
@@ -0,0 +1,9 @@
+'''A compatibility layer for DSS_CAPI that mimics the official OpenDSS COM interface.
+
+Copyright (c) 2016-2018 Paulo Meira
+'''
+from __future__ import absolute_import
+from .v7 import *
+from .patch_dss_com import patch_dss_com
+
+__version__ = '0.10.1'
