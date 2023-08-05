a280b624c6a68609c9f69eeda1cab8811130525b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:04 +0000

    Add package bincrafters-package-tools/0.17.9/bincrafters_package_tools-0.17.9.tar.gz

---
+++ packages/bincrafters-package-tools/bincrafters_package_tools-0.17.9.tar.gz/bincrafters_package_tools-0.17.9/bincrafters/build_template_header_only.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from bincrafters import build_shared
+
+
+def get_builder(**kwargs):
+    builder = build_shared.get_builder(**kwargs)
+
+    builder.add()
+
+    return builder
