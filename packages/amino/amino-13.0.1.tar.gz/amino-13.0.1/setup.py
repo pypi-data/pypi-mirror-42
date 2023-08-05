351de8181497befa5f7dd01000d09d95af708589
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:25 +0000

    Add package amino/13.0.1/amino-13.0.1.tar.gz

---
+++ packages/amino/amino-13.0.1.tar.gz/amino-13.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+version_parts = (13, 0, 1)
+version = '.'.join(map(str, version_parts))
+
+setup(
+    name='amino',
+    description='functional data structures and type classes',
+    version=version,
+    author='Torsten Schmits',
+    author_email='torstenschmits@gmail.com',
+    license='MIT',
+    url='https://github.com/tek/amino',
+    packages=find_packages(exclude=['unit', 'unit.*']),
+    install_requires=[
+        'toolz',
+        'lenses==0.4.0',
+    ],
+    tests_require=[
+        'kallikrein',
+    ],
+)
