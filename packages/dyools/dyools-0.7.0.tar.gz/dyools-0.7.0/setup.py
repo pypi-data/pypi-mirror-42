4b166f49b43beeceea65609a05c610042e564424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:45 +0000

    Add package dyools/0.7.0/dyools-0.7.0.tar.gz

---
+++ packages/dyools/dyools-0.7.0.tar.gz/dyools-0.7.0/setup.py
@@ -0,0 +1,33 @@
+import os
+
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+version = '0.7.0'
+
+setup(
+    name='dyools',
+    version=version,
+    description="dyools",
+    long_description=README,
+    classifiers=[],
+    keywords='dyools',
+    author='me',
+    author_email='me@example.org',
+    url='https://example.org',
+    license='LGPL v3',
+    zip_safe=True,
+    py_modules=['dyools'],
+    include_package_data=True,
+    package_dir={},
+    install_requires=[
+        'click',
+        'future',
+        'python-dateutil',
+    ],
+    setup_requires=['pytest-runner', ],
+    tests_require=['pytest', ],
+
+)
