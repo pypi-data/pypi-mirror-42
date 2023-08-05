48f64d886e43107ffa0142f3e09f310aa5cf2956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:54 +0000

    Add package ztlearn/1.1.1/ztlearn-1.1.1-py3-none-any.whl

---
+++ packages/ztlearn/ztlearn-1.1.1-py3-none-any.whl/ztlearn/__version__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+VERSION = (1, 1, 1)
+
+__version__ = '.'.join(map(str, VERSION))
---
+++ packages/ztlearn/ztlearn-1.1.1-py3-none-any.whl/ztlearn/datasets/pima/pima_indians.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+from ztlearn.utils import maybe_download
+from ztlearn.utils import train_test_split
+from ztlearn.datasets.data_set import DataSet
+
+URL = 'http://ftp.ics.uci.edu/pub/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data'
+
+def fetch_pima_indians(data_target = True):
+    file_path = maybe_download('../../ztlearn/datasets/pima/', URL)
+    describe  = [
+        'Pregnancies',
+        'Glucose',
+        'BloodPressure',
+        'SkinThickness',
+        'DiabetesPedigreeFunction',
+        'Age',
+        'Insulin',
+        'BMI',
+        'Outcome (0 or 1)'
+    ]
+
+    dataframe    = pd.read_csv(file_path, names = describe)
+    data, target = dataframe.values[:,0:8], dataframe.values[:,8]
+
+    if data_target:
+        return DataSet(data, target, describe)
+    else:
+        return train_test_split(data, target, test_size = 0.2, random_seed = 2)
