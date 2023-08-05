e3e3646f49a8ab607546c22fc4291f26295866ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:28 +0000

    Add package sfftk/0.3.2.dev5/sfftk-0.3.2.dev5-py2-none-any.whl

---
+++ packages/sfftk/sfftk-0.3.2.dev5-py2-none-any.whl/sfftk/core/print_tools.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+print_tools.py
+==============
+
+Utilities for printing stuff to the screen.
+"""
+from __future__ import division, print_function
+
+import sys
+import time
+
+__author__ = 'Paul K. Korir, PhD'
+__email__ = 'pkorir@ebi.ac.uk'
+__date__ = '2016-16-06'
+__updated__ = '2018-02-14'
+
+
+def print_date(print_string, stream=sys.stderr, newline=True, incl_date=True):
+    """Print the given string with date on the given stream
+    
+    :param str print_string: the string to be printed
+    :param unicode print_string: the unicode string to be printed
+    :param stream: the stream to write to
+    :type stream: `sys.stderr` or `sys.stdout`
+    :param bool newline: whether (default) or not to add a newline at the end
+    """
+    try:
+        assert isinstance(print_string, basestring)
+    except AssertionError:
+        raise ValueError("input should be subclass of basestring: str or unicode")
+    if isinstance(print_string, str):
+        print_string = unicode(print_string)
+    if newline:
+        if incl_date:
+            print(u"%s\t%s" % (time.ctime(time.time()), print_string), file=stream)
+        else:
+            print(u"%s" % (print_string), file=stream)
+    else:
+        if incl_date:
+            print(u"%s\t%s" % (time.ctime(time.time()), print_string), file=stream, end='')
+        else:
+            print(u"%s" % (print_string), file=stream, end='')
+
+
+def get_printable_ascii_string(s):
+    """Given a string of ASCII and non-ASCII return the maximal substring with a printable ASCII prefix.
+    
+    :param str s: the string to search
+    :return str ascii_s: the minimal ASCII string
+    """
+    # get the list of ordinals
+    s_ord = map(ord, s)
+    # ASCII have ordinals on 0-127
+    # get the first non-printable ASCII i.e. 32 < ord > 127
+    try:
+        non_ascii = filter(lambda x: x < 32 or x > 127, s_ord)[0]
+    except IndexError:
+        return ''
+    # get the index along the string where this character exists
+    non_ascii_index = s_ord.index(non_ascii)
+    # return the prefix upto and excluding the first non-ASCII
+    ascii_s = s[:non_ascii_index]
+    return ascii_s
+
+
+def print_static(print_string, stream=sys.stderr, incl_date=True):
+    """Print the given string with date on the given stream from the first position 
+    overwriting any characters. 
+    
+    This is a useful way to display progress without overcrowding the screen.
+    
+    :param str print_string: the string to be printed
+    :param stream: the stream to write to
+    :type stream: `sys.stderr` or `sys.stdout`
+    :param bool newline: whether (default) or not to add a newline at the end
+    """
+    try:
+        assert stream is sys.stderr or stream is sys.stdout
+    except:
+        print(
+            "Invalid stream '{}'; should be Python objects `sys.stderr` or `sys.stdout`".format(stream),
+            file=sys.stderr
+        )
+
+    if incl_date:
+        print("\r%s\t%s" % (time.ctime(time.time()), print_string), file=stream, end='')
+    else:
+        print("\r%s" % (print_string), file=stream, end='')
---
+++ packages/sfftk/sfftk-0.3.2.dev5-py2-none-any.whl/sfftk/readers/amreader.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+'''
+sfftk.readers.amreader
+======================
+
+Ad hoc reader for AmiraMesh files
+'''
+from __future__ import print_function
+import ahds
+
+from ..core.print_tools import print_date
+
+
+__author__ = 'Paul K. Korir, PhD'
+__email__ = 'pkorir@ebi.ac.uk, paul.korir@gmail.com'
+__date__ = '2016-01-21'
+
+
+def get_data(fn, *args, **kwargs):
+    '''Reads and returns structured data given the file name
+    
+    :param str fn: filename
+    :return header: AmiraMesh header
+    :rtype header: ``ahds.header.AmiraHeader``
+    :return segments_by_stream: segments organised by stream
+    :rtype segments_by_stream: ``ahds.data_streams.ImageSet``
+    '''
+    af = ahds.AmiraFile(fn, *args, **kwargs)
+    header = af.header
+
+    '''
+    :TODO: handle <hxsurface> from .am files
+    '''
+    if header.designation.extra_format == "<hxsurface>":
+        return header, None
+    else:
+        # read now
+        af.read()
+        data_streams = af.data_streams
+
+        if len(data_streams) == 1:
+            # get the index for the first (and only) data pointer
+            index = header.data_pointers.data_pointer_1.data_index
+            volume = data_streams[index].to_volume()
+            return header, volume
+        else:
+            # get the first one and warn the user
+            print_date("Multiple lattices defined. Is this file formatted properly? Trying to work with the first one...")
+            index = header.data_pointers.data_pointer_1.data_index
+            volume = data_streams[index].to_volume()
+            return header, volume
+
+        '''
+        images_by_stream = dict()
+        for data_stream in af.data_streams:
+            images = data_stream.to_images()
+            data_stream_index = data_stream.data_pointer.data_index
+            images_by_stream[data_stream_index] = images.data
+        \'''
+        # convert the data into images
+        images_by_stream = dict()
+        for stream in data_streams:
+            stream_id = stream.data_pointer.data_index
+            images_by_stream[stream_id] = stream.to_images()
+        
+        # convert the images into segments
+        segments_by_stream = dict()
+        for stream_id, image_set in images_by_stream.iteritems():
+            segments_by_stream[stream_id] = image_set.segments
+            
+        return header, segments_by_stream
+        '''
