e673194fdcdca80fc6fff50e5e0ced54b444d9aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:23 +0000

    Add package ngslite/0.5.1/ngslite-0.5.1.tar.gz

---
+++ packages/ngslite/ngslite-0.5.1.tar.gz/ngslite-0.5.1/ngslite/__init__.py
@@ -0,0 +1,33 @@
+"""
+Light-weight functions for next-generation sequencing (NGS) data analysis
+
+Python API for NGS-related command line tools
+Python functions for manipulating NGS-related data and files
+"""
+
+
+__version__ = '0.5'
+
+
+from .arrfunc import *
+from .bedtools import *
+from .bowtie2 import *
+from .bwa import *
+from .cmdtools import *
+from .count import *
+from .dnatools import *
+from .fasta import *
+from .fastq import *
+from .file_conversion import *
+from .filetools import *
+from .gtftools import *
+from .hmmer import *
+from .hmmer_parse import *
+from .jellyfish import *
+from .kmertools import *
+from .metaspades import *
+from .samtools import *
+from .subsample import *
+from .trim import *
+from .trinity import *
+from .vcftools import *
---
+++ packages/ngslite/ngslite-0.5.1.tar.gz/ngslite-0.5.1/ngslite/gtftools.py
@@ -0,0 +1,150 @@
+class GtfParser:
+    def __init__(self, file):
+        """
+        Args:
+            file: str, path-like object
+        """
+        self.__gtf = open(file, 'r')
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+        return
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        r = self.next()
+        if r[1] is None:
+            raise StopIteration
+        else:
+            return r
+
+    def next(self):
+        """
+        Each line of the GTF file has 9 fields````
+
+            Col	Field       Type
+        0   1   seqname     string
+        1   2   source      string
+        2   3   feature     string
+        3   4   start       int
+        4   5   end         int
+        5   6   score       float
+        6   7   strand      string ('+', '-')
+        7   8   frame       int (0, 1, 2)
+        8   9   attribute   string
+
+        Returns: tuple of str or int
+            9 fields of a feature (i.e. a line)
+        """
+        line = self.__gtf.readline().rstrip()
+        if line == '':
+            return (None, ) * 9
+        else:
+            fields = line.split('\t')
+            for i in (3, 4, 7):
+                if fields[i] != '.':
+                    fields[i] = int(fields[i])
+            if fields[5] != '.':
+                fields[5] = float(fields[5])
+            return tuple(fields)
+
+    def close(self):
+        self.__gtf.close()
+
+
+class GtfWriter:
+    def __init__(self, file, mode='w'):
+        """
+        Args:
+            file: str, path-like object
+            mode: str, 'w' or 'a'
+        """
+        self.__gtf = open(file, mode)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+        return
+
+    def write(self, feature):
+        """
+        Args:
+            feature: tuple of str or int
+                Containing 9 fields of a line of GTF file
+        """
+        self.__gtf.write('\t'.join(map(str, feature)) + '\n')
+
+    def close(self):
+        self.__gtf.close()
+
+
+def subset_gtf(file, seqname, output):
+    """
+    Args:
+        file: str, path-like
+            The input gtf file
+
+        seqname: str, or list of str
+            Each str is a seqname (chromosome name) to be included
+
+        output: str, path-like
+            The output gtf file
+    """
+    if isinstance(seqname, str):
+        seqname = [seqname]
+
+    parser = GtfParser(file)
+    writer = GtfWriter(output)
+
+    while True:
+        item = parser.next()
+        if item[0] is None:
+            break
+
+        if item[0] in seqname:
+            writer.write(item)
+
+    parser.close()
+    writer.close()
+
+
+def gtf_replace_blank_with(file, s, output):
+    """
+    Replace blank spaces with <s> in the input GTF file.
+    Check the fields that are strings: <seqname>, <source>, <feature>, <attribute>
+
+    Args:
+        file: str, path-like
+            The input GTF file
+
+        s: str
+            The string used to replace blank space
+
+        output:
+            The output GTF file
+    """
+    parser = GtfParser(file)
+    writer = GtfWriter(output)
+
+    while True:
+        seqname, source, feature, start, end, score, strand, frame, attribute = parser.next()
+        if seqname is None:
+            break
+
+        seqname = s.join(seqname.split(' '))
+        source = s.join(source.split(' '))
+        feature = s.join(feature.split(' '))
+        attribute = s.join(attribute.split(' '))
+
+        writer.write((seqname, source, feature, start, end, score, strand, frame, attribute))
+
+    parser.close()
+    writer.close()
+
