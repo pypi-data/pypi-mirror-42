8b29da5ddcc292933d7a270d82e97bca3f6b051d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:00 +0000

    Add package trio-websocket/0.6.0/trio-websocket-0.6.0.tar.gz

---
+++ packages/trio-websocket/trio-websocket-0.6.0.tar.gz/trio-websocket-0.6.0/trio_websocket/__init__.py
@@ -0,0 +1,14 @@
+from ._impl import (
+    CloseReason,
+    ConnectionClosed,
+    connect_websocket,
+    connect_websocket_url,
+    open_websocket,
+    open_websocket_url,
+    WebSocketConnection,
+    WebSocketRequest,
+    WebSocketServer,
+    wrap_client_stream,
+    wrap_server_stream,
+    serve_websocket,
+)
