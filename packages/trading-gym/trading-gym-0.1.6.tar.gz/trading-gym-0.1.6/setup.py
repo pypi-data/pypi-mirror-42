440027afbb670b3789b5eb08bd8f8fbc79c869eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:36 +0000

    Add package trading-gym/0.1.6/trading-gym-0.1.6.tar.gz

---
+++ packages/trading-gym/trading-gym-0.1.6.tar.gz/trading-gym-0.1.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+requires = [
+    "gym",
+    "numpy",
+    "matplotlib",
+    "mpl-finance",
+]
+
+setup(name='trading-gym',
+      version='0.1.6',
+      description='A gym tool for trading.',
+      url='https://github.com/mymusise/Trading-Gym',
+      author='mymusise',
+      author_email='mymusise1@gmail.com',
+      license='MIT',
+      packages=['trading_gym'],
+      install_requires=requires,
+      zip_safe=False)
