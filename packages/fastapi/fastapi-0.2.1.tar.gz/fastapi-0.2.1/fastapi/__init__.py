7fe5f007b83a85c6af78970f26e115a5ae796965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:22 +0000

    Add package fastapi/0.2.1/fastapi-0.2.1.tar.gz

---
+++ packages/fastapi/fastapi-0.2.1.tar.gz/fastapi-0.2.1/fastapi/__init__.py
@@ -0,0 +1,7 @@
+"""FastAPI framework, high performance, easy to learn, fast to code, ready for production"""
+
+__version__ = "0.2.1"
+
+from .applications import FastAPI
+from .routing import APIRouter
+from .params import Body, Path, Query, Header, Cookie, Form, File, Security, Depends
