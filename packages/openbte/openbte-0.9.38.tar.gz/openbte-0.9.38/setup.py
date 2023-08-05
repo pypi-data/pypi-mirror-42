e626f903a8d2df5f1ecc7039385cf4eeb763104d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:58 +0000

    Add package openbte/0.9.38/openbte-0.9.38.tar.gz

---
+++ packages/openbte/openbte-0.9.38.tar.gz/openbte-0.9.38/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup,find_packages
+import os
+
+setup(name='openbte',
+      version='0.9.38',
+      description='Boltzmann Transport Equation for Phonons',
+      author='Giuseppe Romano',
+      author_email='romanog@mit.edu',
+      classifiers=['Programming Language :: Python :: 2.7',\
+                   'Programming Language :: Python :: 3.6'],
+      long_description=open('README.rst').read(),
+      install_requires=['numpy',
+                        'scipy',
+                        'sparse',
+                        'shapely',
+                        'pyvtk',
+                        'unittest2',
+                        'ipython',
+                        'termcolor',
+                        'future',
+                        'alabaster',
+                        'deepdish',
+                        'mpi4py',
+                        'matplotlib',
+                         ],
+      license='GPLv2',\
+      packages = ['openbte'],
+      package_data = {'openbte':['materials/*.dat','../extern/gmsh']},
+      #packages = find_packages(exclude=['openbte_new']),
+      entry_points = {
+     'console_scripts': [
+      'openbte=openbte.__main__:main','shengbte2openbte=openbte.shengbte2openbte:main'],
+      },
+      zip_safe=False)
