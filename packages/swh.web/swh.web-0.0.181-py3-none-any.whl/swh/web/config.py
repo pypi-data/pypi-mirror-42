d6675b9008b9ff15c282c6d0f2f3087162994d05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:00 +0000

    Add package swh.web/0.0.181/swh.web-0.0.181-py3-none-any.whl

---
+++ packages/swh.web/swh.web-0.0.181-py3-none-any.whl/swh/web/config.py
@@ -0,0 +1,132 @@
+# Copyright (C) 2017-2018  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU Affero General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+from swh.core import config
+from swh.storage import get_storage
+from swh.indexer.storage import get_indexer_storage
+from swh.vault.api.client import RemoteVaultClient
+from swh.scheduler import get_scheduler
+
+
+DEFAULT_CONFIG = {
+    'allowed_hosts': ('list', []),
+    'storage': ('dict', {
+        'cls': 'remote',
+        'args': {
+            'url': 'http://127.0.0.1:5002/',
+            'timeout': 10,
+        },
+    }),
+    'indexer_storage': ('dict', {
+        'cls': 'remote',
+        'args': {
+            'url': 'http://127.0.0.1:5007/',
+            'timeout': 1,
+        }
+    }),
+    'vault': ('string', 'http://127.0.0.1:5005/'),
+    'log_dir': ('string', '/tmp/swh/log'),
+    'debug': ('bool', False),
+    'serve_assets': ('bool', False),
+    'host': ('string', '127.0.0.1'),
+    'port': ('int', 5004),
+    'secret_key': ('string', 'development key'),
+    # do not display code highlighting for content > 1MB
+    'content_display_max_size': ('int', 1024 * 1024),
+    'snapshot_content_max_size': ('int', 1000),
+    'throttling': ('dict', {
+        'cache_uri': None,  # production: memcached as cache (127.0.0.1:11211)
+                            # development: in-memory cache so None
+        'scopes': {
+            'swh_api': {
+                'limiter_rate': {
+                    'default': '120/h'
+                },
+                'exempted_networks': ['127.0.0.0/8']
+            },
+            'swh_vault_cooking': {
+                'limiter_rate': {
+                    'default': '120/h',
+                    'GET': '60/m'
+                },
+                'exempted_networks': ['127.0.0.0/8']
+            },
+            'swh_save_origin': {
+                'limiter_rate': {
+                    'default': '120/h',
+                    'POST': '10/h'
+                },
+                'exempted_networks': ['127.0.0.0/8']
+            }
+        }
+    }),
+    'scheduler': ('dict', {
+        'cls': 'remote',
+        'args': {
+            'url': 'http://localhost:5008/'
+        }
+    }),
+    'grecaptcha': ('dict', {
+        'activated': True,
+        'validation_url': 'https://www.google.com/recaptcha/api/siteverify',
+        'site_key': '',
+        'private_key': ''
+    }),
+    'production_db': ('string', '/var/lib/swh/web.sqlite3'),
+    'deposit': ('dict', {
+        'private_api_url': 'https://deposit.softwareheritage.org/1/private/',
+        'private_api_user': 'swhworker',
+        'private_api_password': ''
+    }),
+    'coverage_count_origins': ('bool', False)
+}
+
+swhweb_config = {}
+
+
+def get_config(config_file='web/web'):
+    """Read the configuration file `config_file`, update the app with
+       parameters (secret_key, conf) and return the parsed configuration as a
+       dict. If no configuration file is provided, return a default
+       configuration."""
+
+    if not swhweb_config:
+        cfg = config.load_named_config(config_file, DEFAULT_CONFIG)
+        swhweb_config.update(cfg)
+        config.prepare_folders(swhweb_config, 'log_dir')
+        swhweb_config['storage'] = get_storage(**swhweb_config['storage'])
+        swhweb_config['vault'] = RemoteVaultClient(swhweb_config['vault'])
+        swhweb_config['indexer_storage'] = \
+            get_indexer_storage(**swhweb_config['indexer_storage'])
+        swhweb_config['scheduler'] = get_scheduler(**swhweb_config['scheduler']) # noqa
+    return swhweb_config
+
+
+def storage():
+    """Return the current application's storage.
+
+    """
+    return get_config()['storage']
+
+
+def vault():
+    """Return the current application's vault.
+
+    """
+    return get_config()['vault']
+
+
+def indexer_storage():
+    """Return the current application's indexer storage.
+
+    """
+    return get_config()['indexer_storage']
+
+
+def scheduler():
+    """Return the current application's scheduler.
+
+    """
+    return get_config()['scheduler']
---
+++ packages/swh.web/swh.web-0.0.181-py3-none-any.whl/swh/web/tests/browse/views/test_directory.py
@@ -0,0 +1,135 @@
+# Copyright (C) 2017-2018  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU Affero General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+from unittest.mock import patch
+
+from swh.web.common.exc import BadInputExc, NotFoundExc
+from swh.web.common.utils import reverse, get_swh_persistent_id
+from swh.web.common.utils import gen_path_info
+from swh.web.tests.testcase import WebTestCase
+
+from .data.directory_test_data import (
+    stub_root_directory_sha1, stub_root_directory_data,
+    stub_sub_directory_path, stub_sub_directory_data
+)
+
+
+class SwhBrowseDirectoryTest(WebTestCase):
+
+    def directory_view(self, root_directory_sha1, directory_entries,
+                       path=None):
+        dirs = [e for e in directory_entries if e['type'] in ('dir', 'rev')]
+        files = [e for e in directory_entries if e['type'] == 'file']
+
+        url_args = {'sha1_git': root_directory_sha1}
+        if path:
+            url_args['path'] = path
+
+        url = reverse('browse-directory',
+                      url_args=url_args)
+
+        root_dir_url = reverse('browse-directory',
+                               url_args={'sha1_git': root_directory_sha1})
+
+        resp = self.client.get(url)
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed('browse/directory.html')
+        self.assertContains(resp, '<a href="' + root_dir_url + '">' +
+                            root_directory_sha1[:7] + '</a>')
+        self.assertContains(resp, '<td class="swh-directory">',
+                            count=len(dirs))
+        self.assertContains(resp, '<td class="swh-content">',
+                            count=len(files))
+
+        for d in dirs:
+            if d['type'] == 'rev':
+                dir_url = reverse('browse-revision',
+                                  url_args={'sha1_git': d['target']})
+            else:
+                dir_path = d['name']
+                if path:
+                    dir_path = "%s/%s" % (path, d['name'])
+                dir_url = reverse('browse-directory',
+                                  url_args={'sha1_git': root_directory_sha1,
+                                            'path': dir_path})
+            self.assertContains(resp, dir_url)
+
+        for f in files:
+            file_path = "%s/%s" % (root_directory_sha1, f['name'])
+            if path:
+                file_path = "%s/%s/%s" % (root_directory_sha1, path, f['name'])
+            query_string = 'sha1_git:' + f['target']
+            file_url = reverse('browse-content',
+                               url_args={'query_string': query_string},
+                               query_params={'path': file_path})
+            self.assertContains(resp, file_url)
+
+        path_info = gen_path_info(path)
+
+        self.assertContains(resp, '<li class="swh-path">',
+                            count=len(path_info)+1)
+        self.assertContains(resp, '<a href="%s">%s</a>' %
+                                  (root_dir_url, root_directory_sha1[:7]))
+
+        for p in path_info:
+            dir_url = reverse('browse-directory',
+                              url_args={'sha1_git': root_directory_sha1,
+                                        'path': p['path']})
+            self.assertContains(resp, '<a href="%s">%s</a>' %
+                                      (dir_url, p['name']))
+
+        self.assertContains(resp, 'vault-cook-directory')
+
+        swh_dir_id = get_swh_persistent_id('directory', directory_entries[0]['dir_id']) # noqa
+        swh_dir_id_url = reverse('browse-swh-id',
+                                 url_args={'swh_id': swh_dir_id})
+        self.assertContains(resp, swh_dir_id)
+        self.assertContains(resp, swh_dir_id_url)
+
+    @patch('swh.web.browse.utils.service')
+    def test_root_directory_view(self, mock_service):
+        mock_service.lookup_directory.return_value = \
+            stub_root_directory_data
+
+        self.directory_view(stub_root_directory_sha1, stub_root_directory_data)
+
+    @patch('swh.web.browse.utils.service')
+    @patch('swh.web.browse.views.directory.service')
+    def test_sub_directory_view(self, mock_directory_service,
+                                mock_utils_service):
+        mock_utils_service.lookup_directory.return_value = \
+            stub_sub_directory_data
+        mock_directory_service.lookup_directory_with_path.return_value = \
+            {'target': stub_sub_directory_data[0]['dir_id'],
+             'type': 'dir'}
+
+        self.directory_view(stub_root_directory_sha1, stub_sub_directory_data,
+                            stub_sub_directory_path)
+
+    @patch('swh.web.browse.utils.service')
+    @patch('swh.web.browse.views.directory.service')
+    def test_directory_request_errors(self, mock_directory_service,
+                                      mock_utils_service):
+
+        mock_utils_service.lookup_directory.side_effect = \
+            BadInputExc('directory not found')
+
+        dir_url = reverse('browse-directory',
+                          url_args={'sha1_git': '1253456'})
+
+        resp = self.client.get(dir_url)
+        self.assertEqual(resp.status_code, 400)
+        self.assertTemplateUsed('browse/error.html')
+
+        mock_utils_service.lookup_directory.side_effect = \
+            NotFoundExc('directory not found')
+
+        dir_url = reverse('browse-directory',
+                          url_args={'sha1_git': '1253456'})
+
+        resp = self.client.get(dir_url)
+        self.assertEqual(resp.status_code, 404)
+        self.assertTemplateUsed('browse/error.html')
