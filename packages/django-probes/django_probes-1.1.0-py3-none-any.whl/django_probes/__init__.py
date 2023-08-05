a72a4ec8b907a7dbd6cebb7f90603f5dff490005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:20 +0000

    Add package django-probes/1.1.0/django_probes-1.1.0-py3-none-any.whl

---
+++ packages/django-probes/django_probes-1.1.0-py3-none-any.whl/django_probes/__init__.py
@@ -0,0 +1,8 @@
+"""
+Test whether access to the database is possible
+"""
+__author__ = 'VSHN AG'
+__email__ = 'tech@vshn.ch'
+__url__ = 'https://github.com/vshn/django-probes'
+__version__ = '1.1.0'
+__license__ = 'MIT'
