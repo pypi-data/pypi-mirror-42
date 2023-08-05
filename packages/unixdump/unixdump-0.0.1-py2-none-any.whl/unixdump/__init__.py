5bd6346e7c987a0c7c3ef046094c47866dfa33bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:57 +0000

    Add package unixdump/0.0.1/unixdump-0.0.1-py2-none-any.whl

---
+++ packages/unixdump/unixdump-0.0.1-py2-none-any.whl/unixdump/__init__.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2018 NCC Group Security Services, Inc. All rights reserved.
+# Licensed under Dual BSD/GPLv2 per the repo LICENSE file.
+
+# stub to claim pip project, current public version is available at:
+#  https://github.com/nccgroup/ebpf/blob/master/unixdump/unixdump
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+from bcc import BPF
+
+def main():
+  pass
+
+if __name__ == '__main__':
+  main()
+
+__all__ = ["main"]
