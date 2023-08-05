c5d14b05bd63d86a50ad366d1ccb45cbb476c88b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:05 +0000

    Add package youtube-python/1.0.13/youtube-python-1.0.13.tar.gz

---
+++ packages/youtube-python/youtube-python-1.0.13.tar.gz/youtube-python-1.0.13/setup.py
@@ -0,0 +1,31 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+try:
+    long_description = open('README.md').read()
+except IOError:
+    long_description = ""
+
+setup(
+    name='youtube-python',
+    version='1.0.13',
+    description='Python Youtube Data API v3',
+    long_description=long_description,
+    url='https://github.com/rohitkhatri/youtube-python',
+    author='Rohit Khatri',
+    author_email='developer.rohitkhatri@gmail.com',
+    license='GPL',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3.5',
+        "Operating System :: OS Independent",
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    keywords="youtube data api python v3",
+    packages=['youtube'],
+    install_requires=['requests']
+)
