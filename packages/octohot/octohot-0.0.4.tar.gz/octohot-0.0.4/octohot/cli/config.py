5671dcf0d6fff6135591edbbb3c1e0446876f404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:27 +0000

    Add package octohot/0.0.4/octohot-0.0.4.tar.gz

---
+++ packages/octohot/octohot-0.0.4.tar.gz/octohot-0.0.4/octohot/cli/config.py
@@ -0,0 +1,36 @@
+default_filename = 'octohot.yml'
+default_config = {
+    'repositories': [],
+    'github_token': None,
+    'github_organization': None
+
+}
+
+repositories = config = None
+
+import os
+config_file = os.path.join(os.getcwd(), default_filename)
+
+
+def load():
+    global config
+    global repositories
+
+    import os.path
+    if os.path.isfile(config_file):
+        import yaml
+        config = yaml.load(open(config_file, "r"))
+    else:
+        config = default_config
+
+    from octohot.cli.git.repository import Repository
+    repositories = [Repository(repo_name) for repo_name in
+                    config['repositories']]
+
+
+def save():
+    import yaml
+    yaml.dump(config, open(config_file, "w"), default_flow_style=False)
+
+
+load()
