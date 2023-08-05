b8e291d04be6ef7a49798cdc2fa97f548a136480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:57 +0000

    Add package o2-google-spreadsheet/0.4/o2_google_spreadsheet-0.4.tar.gz

---
+++ packages/o2-google-spreadsheet/o2_google_spreadsheet-0.4.tar.gz/o2_google_spreadsheet-0.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="o2_google_spreadsheet",
+    version="0.4",
+    author="Hudge",
+    author_email="doug@mybusinessautomated.com",
+    description="Google Spreadsheet helper file",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hudgeon",
+    py_modules=["o2_google_spreadsheet"],
+    install_requires=["requests", "pandas", "gspread", "oauth2client", "money2float"],
+    extras_require={"": ""},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
