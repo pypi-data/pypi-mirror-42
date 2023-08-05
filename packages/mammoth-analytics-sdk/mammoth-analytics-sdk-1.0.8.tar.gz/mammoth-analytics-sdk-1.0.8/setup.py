e1d6d5e6567f1f752d2109960abefb9c7dbd76b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:07 +0000

    Add package mammoth-analytics-sdk/1.0.8/mammoth-analytics-sdk-1.0.8.tar.gz

---
+++ packages/mammoth-analytics-sdk/mammoth-analytics-sdk-1.0.8.tar.gz/mammoth-analytics-sdk-1.0.8/setup.py
@@ -0,0 +1,23 @@
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Mammoth Analytics sdk',
+    'author': 'Mammoth developer',
+    'author_email': 'developer@mammoth.io',
+    'version': '1.0.8',
+    'packages': ['MammothAnalytics'],
+    'scripts': [],
+    'name': 'mammoth-analytics-sdk',
+    'install_requires': [
+        'requests',
+        'pytest',
+        'names',
+        'pydash'
+    ]
+}
+
+setup(**config)
