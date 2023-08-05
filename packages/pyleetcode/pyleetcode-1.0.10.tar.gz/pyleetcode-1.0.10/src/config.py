7042b10eead8187a51a86a4ac5b22c29d5775934
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:27 +0000

    Add package pyleetcode/1.0.10/pyleetcode-1.0.10.tar.gz

---
+++ packages/pyleetcode/pyleetcode-1.0.10.tar.gz/pyleetcode-1.0.10/src/config.py
@@ -0,0 +1,25 @@
+lang_dict = {
+    'java': 'java',
+    'cpp': 'cpp',
+    'python': 'py',
+    'python3': 'py',
+    'c': 'c',
+    'csharp': 'cs',
+    'javascript': 'js',
+    'ruby': 'rb',
+    'swift': 'swift',
+    'golang': 'go',
+    'scala': 'scala',
+    'kotlin': 'kt',
+    'rust': 'rs',
+    'php': 'php'
+}
+
+urls = {
+    'login': 'https://leetcode.com/accounts/login/',
+    'graphql': 'https://leetcode.com/graphql',
+    'submit': 'https://leetcode.com/problems/add-two-numbers/submit/',
+    'all_problems': 'https://leetcode.com/api/problems/algorithms/',
+    'test': 'https://leetcode.com/problems/add-two-numbers/interpret_solution/',
+    'check': 'https://leetcode.com/submissions/detail/$ID/check/',
+}
