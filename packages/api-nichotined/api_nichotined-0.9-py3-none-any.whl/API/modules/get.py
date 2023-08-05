92d3371f34aa8b5b03f37ac9aa91bbf1ab3299b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:35 +0000

    Add package api-nichotined/0.9/api_nichotined-0.9-py3-none-any.whl

---
+++ packages/api-nichotined/api_nichotined-0.9-py3-none-any.whl/API/modules/get.py
@@ -0,0 +1,18 @@
+import requests
+
+from .baseApi import BaseApi
+
+
+class Get(BaseApi):
+    def __init__(self):
+        super().__init__()
+
+    def execute(self):
+        self.logger_request()
+        self._response = requests.get(url=self.url,
+                                      params=self.params, data=self.data, headers=self.headers, cookies=self.cookies,
+                                      files=self.files, auth=self.auth, timeout=self.timeout,
+                                      allow_redirects=self.allow_redirects, proxies=self.proxies,
+                                      hooks=self.hooks, stream=self.stream, verify=self.verify, cert=self.cert,
+                                      json=self.json)
+        self.logger_response()
