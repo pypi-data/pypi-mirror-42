c76d71fd6ebac3b556fc16559a45cd0fc1682274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:00 +0000

    Add package invoker/1.0.0/invoker-1.0.0.tar.gz

---
+++ packages/invoker/invoker-1.0.0.tar.gz/invoker-1.0.0/README.md
@@ -0,0 +1,10 @@
+# Invoker Readme
+invoker utility for DI(Injector) and context configuration.
+
+## How to use
+```sh
+cd sample
+invoker logging.yaml invoke_option.yaml \
+  --config config_base.yaml \
+  --config config_diff.yaml
+```
---
+++ packages/invoker/invoker-1.0.0.tar.gz/invoker-1.0.0/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name="invoker",
+    version="1.0.0",
+    description='Invoker utility for DI(Injector) and context configuration.',
+    packages=find_packages(),
+    install_requires=["injector==0.14.1", "pyyaml>=4.2b1"],
+    extras_require={
+        "develop": ["autopep8", "pep8"]
+    },
+    entry_points={
+        "console_scripts": [
+            "invoker = invoker.cmd:execute"
+        ],
+    }
+)
