a9dc29f9e22c2435b10cebd20f198e1d240bc22c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:51 +0000

    Add package mkinit/0.2.0/mkinit-0.2.0-py2.py3-none-any.whl

---
+++ packages/mkinit/mkinit-0.2.0-py2.py3-none-any.whl/mkinit/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Regenerate Input Command
+mkinit ~/code/mkinit/mkinit
+"""
+__version__ = '0.2.0'
+
+__submodules__ = [
+    'dynamic_mkinit',
+    'static_mkinit',
+]
+
+__DYNAMIC__ = False
+if __DYNAMIC__:
+    from mkinit import dynamic_mkinit
+    exec(dynamic_mkinit.dynamic_init(__name__))
+else:
+    # <AUTOGEN_INIT>
+    from mkinit import dynamic_mkinit
+    from mkinit import static_mkinit
+
+    from mkinit.dynamic_mkinit import (dynamic_init,)
+    from mkinit.static_mkinit import (autogen_init, static_init,)
+
+    __all__ = ['autogen_init', 'dynamic_init', 'dynamic_mkinit', 'static_init',
+               'static_mkinit']
+    # </AUTOGEN_INIT>
---
+++ packages/mkinit/mkinit-0.2.0-py2.py3-none-any.whl/mkinit/__main__.py
@@ -0,0 +1,78 @@
+def main():
+    from mkinit import static_mkinit
+    import textwrap
+    import argparse
+    import logging
+    description = textwrap.dedent(
+        '''
+        discover and run doctests within a python package
+
+        Respects the varaibles:
+            `__submodules__`, `__explicit__`, `__protected__`, `__private__`
+        ''').strip('\n')
+
+    parser = argparse.ArgumentParser(prog='python -m mkinit', description=description)
+    parser.add_argument('modname_or_path', nargs='?', help='module or path to generate __init__.py for', default='.')
+
+    parser.add_argument('--dry', dest='_dry_old', action='store_true', default=True)
+
+    parser.add_argument(*('-i', '-w', '--write', '--inplace'), dest='dry', action='store_false', default=True)
+    # parser.add_argument('-i', dest='dry', action='store_false', default=True)
+
+    parser.add_argument('--noattrs',
+                        dest='with_attrs', action='store_false', default=True,
+                        help='Do not generate attribute from imports')
+    parser.add_argument('--nomods',
+                        dest='with_mods', action='store_false', default=True,
+                        help='Do not generate modules imports')
+    parser.add_argument('--noall',
+                        dest='with_all', action='store_false', default=True,
+                        help='Do not generate an __all__ variable')
+
+    parser.add_argument('--relative',
+                        action='store_true', default=False,
+                        help='Use relative . imports instead of <modname>')
+
+    # parser.add_argument('--ignore_all',
+    #                     dest='use_all', action='store_false', default=True,
+    #                     help='Ignores __all__ variables when parsing')
+
+    parser.add_argument('--verbose', nargs='?', default=0, type=int,
+                        help='Verbosity level')
+
+    args, unknown = parser.parse_known_args()
+    ns = args.__dict__.copy()
+
+    modname_or_path = ns['modname_or_path']
+    if ns['verbose'] is None:
+        ns['verbose'] = 1
+
+    use_all = ns['with_all']
+    verbose = ns['verbose']
+    dry = ns['dry']
+
+    # Formatting options
+    options = {
+        'with_attrs': ns['with_attrs'],
+        'with_mods': ns['with_mods'],
+        'with_all': ns['with_all'],
+        'relative': ns['relative'],
+    }
+
+    if verbose == 0:
+        level = logging.WARNING
+    elif verbose == 1:
+        level = logging.INFO
+    elif verbose >= 2:
+        level = logging.DEBUG
+
+    logging.basicConfig(
+        format='%(levelname)s: %(message)s',
+        level=level,
+    )
+
+    static_mkinit.autogen_init(modname_or_path, use_all=use_all,
+                               options=options, dry=dry)
+
+if __name__ == '__main__':
+    main()
