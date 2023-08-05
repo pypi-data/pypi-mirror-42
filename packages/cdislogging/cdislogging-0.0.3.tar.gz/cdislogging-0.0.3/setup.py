3588bd6c0dd5f092c03636326bdb44b38427a0c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:23 +0000

    Add package cdislogging/0.0.3/cdislogging-0.0.3.tar.gz

---
+++ packages/cdislogging/cdislogging-0.0.3.tar.gz/cdislogging-0.0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cdislogging",
+    version="0.0.3",
+    description="Standardized logging tool and format for cdis applications",
+    license="Apache",
+    packages=find_packages(),
+)
