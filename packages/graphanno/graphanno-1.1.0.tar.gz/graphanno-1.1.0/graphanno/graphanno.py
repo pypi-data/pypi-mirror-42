0f2176599518d26ac5d37dc88bad813cccc0c38c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:46 +0000

    Add package graphanno/1.1.0/graphanno-1.1.0.tar.gz

---
+++ packages/graphanno/graphanno-1.1.0.tar.gz/graphanno-1.1.0/graphanno/graphanno.py
@@ -0,0 +1,78 @@
+"""Main module."""
+import typing
+from datetime import date, datetime, time
+
+import graphene
+
+BASIC_TYPE_MAPPINGS = {
+    str: graphene.String,
+    int: graphene.Int,
+    bool: graphene.Boolean,
+    float: graphene.Float,
+    date: graphene.Date,
+    datetime: graphene.DateTime,
+    time: graphene.Time,
+}
+
+UNSUPORTED_TYPES = (list, dict, tuple, set)
+
+
+class UnsupportedAnnotationError(Exception):
+    """Raised on unsupported annotation resolve attempt."""
+
+
+class NoAnnotationsError(Exception):
+    """Raised when no annotations have been found (or all are excluded)."""
+
+
+class ObjectType(graphene.ObjectType):
+    """
+    Base class for type annotated graphene schemas.
+    The subclass still has to be decorated, the purpose of this class is to provide hints
+    for special graphanno attributes and those inherited from ObjectType.
+    """
+    __model__: typing.Any = None
+    __excluded_fields__: typing.Iterable = tuple()
+    __ignore_unsupported__: bool = False
+
+
+def graph_annotations(cls):
+    """Prepare GraphQL schema based on the type annotations."""
+    attributes = {}
+    target_class = cls.__model__ if hasattr(cls, '__model__') else cls
+    ignore_unsupported = getattr(cls, '__ignore_unsupported__', False)
+    excluded_keys = getattr(cls, '__excluded_fields__', tuple())
+
+    annotations = dict(**getattr(target_class, '__annotations__', {}))
+    annotations.update(getattr(cls, '__annotations__', {}))
+
+    private_keys = tuple(key for key in annotations.keys() if key.startswith('_'))
+
+    for key in excluded_keys + private_keys:
+        annotations.pop(key)
+
+    if not annotations:
+        raise NoAnnotationsError(f'No included annotations for class {cls.__name__}.')
+
+    for name, annotation in annotations.items():
+        if annotation in UNSUPORTED_TYPES:
+            if ignore_unsupported:
+                continue
+            raise UnsupportedAnnotationError(f'The type annotation: {annotation} is not supported.')
+        type_, args = _get_type_from_annotation(annotation)
+        attributes[name] = type_(*args)
+
+    superclasses = (cls,) if issubclass(cls, graphene.ObjectType) else (cls, graphene.ObjectType)
+    return type(cls.__name__, superclasses, attributes)
+
+
+def _get_type_from_annotation(annotation, type_only=False):
+    basic_type = BASIC_TYPE_MAPPINGS.get(annotation)
+    if basic_type:
+        return basic_type if type_only else (basic_type, tuple())
+
+    if str(annotation).startswith('typing.List'):
+        return graphene.List, (_get_type_from_annotation(annotation.__args__[0], True),)
+
+    type_ = graph_annotations(annotation)
+    return type_ if type_only else (graphene.Field, (type_,))
---
+++ packages/graphanno/graphanno-1.1.0.tar.gz/graphanno-1.1.0/tests/test_objects/advanced.py
@@ -0,0 +1,8 @@
+"""Advanced class definition."""
+from typing import List
+
+
+class Advanced:
+    """Class with advanced data types."""
+    array: List[str]
+    _private: bool
