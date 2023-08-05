50cf040e3dc744351fc3cc303ad6a700a309b3e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:18 +0000

    Add package circlex/0.1/circlex-0.1.tar.gz

---
+++ packages/circlex/circlex-0.1.tar.gz/circlex-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='circlex',
+      version='0.1',
+      author='Circle',
+      author_email='eriks@circle.com',
+      license='MIT',
+      packages=['circlex'])
