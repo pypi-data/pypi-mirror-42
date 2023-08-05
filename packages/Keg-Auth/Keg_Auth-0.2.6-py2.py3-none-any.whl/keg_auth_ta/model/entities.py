b3cc5aebf6e0c7ae87624362afd4b508eedd6ea1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:46 +0000

    Add package Keg-Auth/0.2.6/Keg_Auth-0.2.6-py2.py3-none-any.whl

---
+++ packages/Keg-Auth/Keg_Auth-0.2.6-py2.py3-none-any.whl/keg_auth_ta/model/entities.py
@@ -0,0 +1,44 @@
+import logging
+
+from keg.db import db
+from keg_elements.db.mixins import DefaultColsMixin, MethodsMixin
+import keg_auth
+
+from keg_auth_ta.extensions import auth_entity_registry
+
+log = logging.getLogger(__name__)
+
+
+class EntityMixin(DefaultColsMixin, MethodsMixin):
+    pass
+
+
+@auth_entity_registry.register_user
+class User(db.Model, keg_auth.UserEmailMixin, keg_auth.UserMixin, EntityMixin):
+    __tablename__ = 'users'
+
+
+class UserNoEmail(db.Model, keg_auth.UserMixin, EntityMixin):
+    __tablename__ = 'users_no_email'
+
+
+class UserWithToken(EntityMixin, keg_auth.UserEmailMixin, keg_auth.UserTokenMixin,  db.Model):
+    __tablename__ = 'users_with_token'
+
+
+@auth_entity_registry.register_permission
+class Permission(db.Model, keg_auth.PermissionMixin, EntityMixin):
+    __tablename__ = 'permissions'
+
+    def __repr__(self):
+        return '<Permission id={} token={}>'.format(self.id, self.token)
+
+
+@auth_entity_registry.register_bundle
+class Bundle(db.Model, keg_auth.BundleMixin, EntityMixin):
+    __tablename__ = 'bundles'
+
+
+@auth_entity_registry.register_group
+class Group(db.Model, keg_auth.GroupMixin, EntityMixin):
+    __tablename__ = 'groups'
