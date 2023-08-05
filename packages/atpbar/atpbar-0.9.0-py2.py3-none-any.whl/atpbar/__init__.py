9f2ad314a9a66373fe93fac45f64e7b5f9136a52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:47 +0000

    Add package atpbar/0.9.0/atpbar-0.9.0-py2.py3-none-any.whl

---
+++ packages/atpbar/atpbar-0.9.0-py2.py3-none-any.whl/atpbar/__init__.py
@@ -0,0 +1,11 @@
+# Tai Sakuma <tai.sakuma@gmail.com>
+from .main import atpbar
+from .funcs import find_reporter, register_reporter
+
+##__________________________________________________________________||
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
+##__________________________________________________________________||
+
