b42c49d62b8aff4e85d965c0b58158a58bdedb1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:23 +0000

    Add package MeleeUploader/1.10.0/MeleeUploader-1.10.0.tar.gz

---
+++ packages/MeleeUploader/MeleeUploader-1.10.0.tar.gz/MeleeUploader-1.10.0/meleeuploader/main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import subprocess
+
+from . import forms
+from . import consts
+from . import youtube as yt
+
+import pyforms_lite
+
+
+def main():
+    subprocess.call(('pip3', 'install', '-U', 'meleeuploader'))
+    if "linux" in sys.platform:  # root needed for writing files
+        if os.geteuid() != 0:
+            print("Need sudo for writing files")
+            subprocess.call(['sudo', 'python3', sys.argv[0]])
+    # Always get the initial YT credentials outside of your app for safety.
+    try:
+        consts.youtube = yt.get_youtube_service()
+        consts.sheets = yt.get_spreadsheet_service()
+        pyforms_lite.start_app(forms.MeleeUploader, geometry=(200, 200, 1, 1))
+    except Exception as e:
+        print(e)
+        print("This program needs internet access to work")
+        sys.exit(1)
+
+
+def ult():
+    consts.melee = False
+    main()
+
+
+if __name__ == "__main__":
+    main()
