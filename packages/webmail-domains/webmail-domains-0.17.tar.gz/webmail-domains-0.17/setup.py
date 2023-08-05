4a46421e291d15a3e4aded20f9e276ee614e4dfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:22 +0000

    Add package webmail-domains/0.17/webmail-domains-0.17.tar.gz

---
+++ packages/webmail-domains/webmail-domains-0.17.tar.gz/webmail-domains-0.17/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md') as file:
+    long_description = file.read()
+
+setup(
+    name="webmail-domains",
+    version="0.17",
+    py_modules=["webmail_domains"],
+    url='https://github.com/zevaverbach/Webmail-Domains',
+    description=('A pretty thorough list of webmail domains!'),
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+        )
