621d94ac483940e774fcece7afa4c17ffa43925e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:56 +0000

    Add package go-ml-core/0.1.0.dev45/go-ml-core-0.1.0.dev45.tar.gz

---
+++ packages/go-ml-core/go-ml-core-0.1.0.dev45.tar.gz/go-ml-core-0.1.0.dev45/mlcore/datahelper/api/dataplatform_api_helper.py
@@ -0,0 +1,213 @@
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+import json
+import base64
+import ast
+import sys
+import logging
+
+logging.basicConfig(stream=sys.stdout,
+                    level=logging.INFO,
+                    format='%(asctime)-s %(levelname)-7s %(name)-s.%(funcName)s(): %(message)s',
+                    datefmt='%Y-%m-%dT%H:%M:%S')
+
+
+
+def str_to_dict(str):
+    str = str.replace('false', 'False')
+    str = str.replace('true', 'True')
+    return ast.literal_eval(str)
+
+
+class KeycloakSDK():
+
+    def __init__(self, keycloak):
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.keycloak = keycloak
+        self.token = None
+
+    def get_access_token(self):
+        # 1. service_account_login
+        self.service_account_login()
+        # 2. get_authorization
+        self.get_authorization()
+        # 3. refresh_access_token (schedule)
+        #self.refresh_access_token()
+
+        return self.token['access_token']
+
+    def service_account_login(self):
+        '''
+        Service Account Login
+        Method : basicAuth(resource, serviceConfig)
+        '''
+
+        url = '%s/realms/%s/protocol/openid-connect/token' % (self.keycloak["auth-server-url"], self.keycloak["realm"])
+        
+        headers = {
+            'Content-Type' : 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic %s' % base64.b64encode(bytes('%s:%s'% (self.keycloak["resource"], self.keycloak["credentials"]["secret"]),'utf-8')).decode("utf-8")
+        }
+        payload = {
+            'grant_type' : 'client_credentials'
+        }
+        # print('url=%s' % url)
+        # print('header=%s' % headers)
+        # print('data=%s' % payload)
+        response = requests.post(url, headers=headers, data=payload, verify=False).text
+        
+        self.token = str_to_dict(response)
+
+    def get_authorization(self): 
+        '''
+        GetAuthorization
+        Description: Use following information to get authorization. resource 為目標要取得的 application resource
+        '''
+        
+        url = '%s/realms/%s/protocol/openid-connect/token' % (self.keycloak["auth-server-url"], self.keycloak["realm"]) 
+        
+        headers = {
+            'Content-Type' : 'application/x-www-form-urlencoded',
+            'Authorization': 'Bearer %s' % self.token['access_token']
+        }
+        payload = {
+            'grant_type' : 'urn:ietf:params:oauth:grant-type:uma-ticket',
+            'audience' : self.keycloak["resource"]
+        }
+        # print('url=%s' % url)
+        # print('header=%s' % headers)
+        # print('data=%s' % payload)
+        response = requests.post(url, headers=headers, data=payload, verify=False).text
+        
+        self.token = str_to_dict(response)
+
+    def refresh_access_token(self):
+        '''
+        RefreshGrant
+        Method refreshGrant(grant)
+        '''
+
+        url = '%s/realms/%s/protocol/openid-connect/token' % (self.auth_server_url, self.realm) 
+        
+        headers = {
+            'Content-Type' : 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic %s' % base64.b64encode(bytes('%s:%s'% (self.resource, self.secret),'utf-8')).decode("utf-8")
+        }
+        payload = {
+            'client_id': self.resource,
+            'refresh_token' : self.token['refresh_token'],
+            'grant_type' : 'refresh_token'
+        }
+        # print('url=%s' % url)
+        # print('header=%s' % headers)
+        # print('data=%s' % payload)
+        response = requests.post(url, headers=headers, data=payload, verify=False).text
+        
+        self.token = str_to_dict(response)
+
+class APIHelper():
+    def __init__(self, config):
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.config = config['mlcore']['datahelper']['api']['dataplatform']
+
+        self.keycloakSDK = KeycloakSDK(self.config['keycloak'])
+        self.keycloak = self.config['keycloak']
+        self.access_token = None
+
+
+    def get_access_token(self):
+        return self.keycloakSDK.get_access_token()
+
+    def query_vms(self):
+        return self.query_vms_v1()
+
+    def query_vms_v1(self):
+        url = '%s/v1/vms' % self.config['server_url']
+
+        self.access_token = self.get_access_token()
+
+        headers = {
+            'Go-Client':self.keycloak['resource'],
+            'Authorization':'Bearer {}'.format(self.access_token),
+        }
+        json_obj = {
+            'op_code': 'get',
+            'get_data': {
+                'fields_type': 'detail'
+            }
+        }
+        json_option = None
+        # print('url=%s' % url)
+        # print('header=%s' % headers)
+        # print('json=%s' % json_obj)
+        r = requests.post(url, headers=headers, json=json_obj)
+        if json_option is None:
+            json_option = {}
+        #self.logger.info('r.content = %s' % r.content)
+        res = json.loads(r.content.decode('utf-8'),**json_option)
+
+        if res['code']==0:
+            return res['data']
+        else:
+            self.logger.warn('Failed to query_vms_v1: %s', res)
+
+    def query_esrates(self):
+        return self.query_esrates_v1()
+
+    def query_esrates_v1(self):
+        url = '%s/v1/es-rates' % self.config['server_url']
+
+        self.access_token = self.get_access_token()
+
+        headers = {
+            'Go-Client':self.keycloak['resource'],
+            'Authorization':'Bearer {}'.format(self.access_token),
+        }
+        json_obj = {
+            'op_code': 'get',
+            'get_data': {
+                'get_flag': 1
+            }
+        }
+        json_option = None
+        # print('url=%s' % url)
+        # print('header=%s' % headers)
+        # print('json=%s' % json_obj)
+        r = requests.post(url, headers=headers, json=json_obj)
+        if json_option is None:
+            json_option = {}
+        res = json.loads(r.content.decode('utf-8'),**json_option)
+
+        if res['code']==0:
+            return res['data']
+        else:
+            self.logger.warn('Failed to query_esrates_v1: %s', res)
+
+    def query_vms_nearby(self):
+        return self.query_vms_nearby_v1()
+
+    def query_vms_nearby_v1(self):
+        url = '%s/v1/vms/nearby-vms' % self.config['server_url']
+         
+        self.access_token = self.get_access_token()
+
+        headers = {
+            'Go-Client':self.keycloak['resource'],
+            'Authorization':'Bearer {}'.format(self.access_token),
+        }
+        json_obj = {
+            'op_code': 'get',
+            'get_data': {
+            }
+        }
+        json_option = None
+        # print('url=%s' % url)
+        # print('header=%s' % headers)
+        # print('json=%s' % json_obj)
+        r = requests.post(url, headers=headers, json=json_obj)
+        #print(r.content)
+        if json_option is None:
+            json_option = {}
+        return json.loads(r.content.decode('utf-8'),**json_option)
+
