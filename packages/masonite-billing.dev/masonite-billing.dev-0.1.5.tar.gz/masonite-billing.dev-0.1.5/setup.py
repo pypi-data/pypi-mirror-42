f774c764db991cce7fd0a5188f5943e1df466308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:27 +0000

    Add package masonite-billing.dev/0.1.5/masonite-billing.dev-0.1.5.tar.gz

---
+++ packages/masonite-billing.dev/masonite-billing.dev-0.1.5.tar.gz/masonite-billing.dev-0.1.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+setup(
+    name="masonite-billing.dev",
+    version='0.1.5',
+    packages=[
+        'billing',
+        'billing.commands',
+        'billing.contracts',
+        'billing.controllers',
+        'billing.drivers',
+        'billing.factories',
+        'billing.models',
+        'billing.snippets',
+    ],
+    install_requires=[
+        'masonite',
+        'cleo',
+        'stripe',
+    ],
+    include_package_data=True,
+)
