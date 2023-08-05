2624f8801227dd6cfef3afd54d03d79f0e911980
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:56 +0000

    Add package djangofloor/1.1.39/djangofloor-1.1.39.tar.gz

---
+++ packages/djangofloor/djangofloor-1.1.39.tar.gz/djangofloor-1.1.39/djangofloor/celery.py
@@ -0,0 +1,32 @@
+"""load Celery and discover tasks
+==============================
+
+You should not use this module (or rename it), as it is only used to auto-discover tasks.
+
+"""
+import logging.config
+from django.conf import settings
+from djangofloor.scripts import set_env
+import celery
+
+# used to avoid strange import bug with Python 3.2/3.3
+# noinspection PyStatementEffect
+celery.__file__
+from celery import Celery, signals
+
+__author__ = "Matthieu Gallet"
+project_name = set_env()
+app = Celery(project_name)
+app.config_from_object("django.conf:settings")
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
+# noinspection PyUnusedLocal
+@signals.setup_logging.connect
+def setup_celery_logging(**kwargs):
+    """Use to setup the logs, overriding the default Celery configuration """
+    logging.config.dictConfig(settings.LOGGING)
+
+
+# Moving the call here works
+app.log.setup()
