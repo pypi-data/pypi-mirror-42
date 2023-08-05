60011c84def761983531c94741011397442074bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:14 +0000

    Add package SpatialDE/1.1.1/SpatialDE-1.1.1.tar.gz

---
+++ packages/SpatialDE/SpatialDE-1.1.1.tar.gz/SpatialDE-1.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='SpatialDE',
+        version='1.1.1',
+        description='Spatial and Temporal DE test',
+        url='https://github.com/Teichlab/SpatialDE',
+        packages=find_packages(),
+        include_package_data=True,
+        install_requires=['numpy', 'scipy', 'pandas>=0.23', 'tqdm',
+                          'NaiveDE', 'Click'],
+        entry_points='''
+            [console_scripts]
+            spatialde=SpatialDE.scripts.spatialde_cli:main
+        ''',
+        author='Valentine Svensson',
+        author_email='valentine@nxn.se',
+        license='MIT'
+    )
