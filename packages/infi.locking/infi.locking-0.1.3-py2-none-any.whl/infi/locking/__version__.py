fc9e439114daf098209ad0889180b7b8d94216f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:14 +0000

    Add package infi.locking/0.1.3/infi.locking-0.1.3-py2-none-any.whl

---
+++ packages/infi.locking/infi.locking-0.1.3-py2-none-any.whl/infi/locking/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.1.3"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.1.3)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'fe8405960be6b688033bef300a6212314a30e59b'
+__git_head_subject__ = u'Finished Release v0.1.3'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2019-02-18 12:45:37'
