2f633a8242746b08c94082030340e37bfa033621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:01 +0000

    Add package wpt-superset/1.0.1/wpt-superset-1.0.1.tar.gz

---
+++ packages/wpt-superset/wpt-superset-1.0.1.tar.gz/wpt-superset-1.0.1/superset/migrations/versions/f62ddec4bce8_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: f62ddec4bce8
+Revises: ('fbd55e0f83eb', 'fb13d49b72f9')
+Create Date: 2019-01-19 19:04:57.583455
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'f62ddec4bce8'
+down_revision = ('fbd55e0f83eb', 'fb13d49b72f9')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
