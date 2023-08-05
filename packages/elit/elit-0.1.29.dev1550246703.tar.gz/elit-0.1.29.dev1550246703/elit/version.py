f21b2809f38ec30e8cb35bdb6ce7036c1b1e770d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:13 +0000

    Add package elit/0.1.29.dev1550246703/elit-0.1.29.dev1550246703.tar.gz

---
+++ packages/elit/elit-0.1.29.dev1550246703.tar.gz/elit-0.1.29.dev1550246703/elit/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM SCIPY SETUP.PY
+short_version = '0.1.29'
+version = '0.1.29'
+full_version = '0.1.29.dev1550246703'
+dev_version = '1550246703'
+release = False
+if not release:
+    version = full_version
