d0346c2d74f80f65f0dfd6849eb558e31b18af1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:16 +0000

    Add package FolderAnalyse/0.1.3/FolderAnalyse-0.1.3.tar.gz

---
+++ packages/FolderAnalyse/FolderAnalyse-0.1.3.tar.gz/FolderAnalyse-0.1.3/setup.py
@@ -0,0 +1,50 @@
+"""
+Ryan Pepper (2019-)
+
+This script sets configuration options for setuptools for the FolderAnalyse
+project.
+
+"""
+from setuptools import setup, find_packages
+import os
+
+def read(fname):
+    """ 
+    Read in text from a file. 
+
+    Function from Python setuptools documenation.
+    https://pythonhosted.org/an_example_pypi_project/setuptools.html
+    """
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='FolderAnalyse',
+    version='0.1.3',
+    description='A program that analyses files in a directory with '
+                'word frequency statistics.',
+    long_description=read("README.md"),
+    author='Ryan Alexander Pepper',
+    author_email="ryan.pepper@soton.ac.uk",
+    url="http://github.com/rpep/FolderAnalyse",
+    packages=find_packages(),
+    package_data={'FolderAnalyse': ['*.txt', os.path.join('FolderAnalyse', 
+                                                          'tests'
+                                                          'example_documents',
+                                                          '*.txt')]
+    },
+    include_package_data = True,
+    entry_points = {
+        "console_scripts": [ "FolderAnalyse=FolderAnalyse.script:main" ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    project_urls = {
+        "Documentation": "https://folderanalyse.readthedocs.io",
+        "Source Code": "https://github.com/rpep/FolderAnalyse",
+    },
+    install_requires = ["pytest"],
+    python_requires=">= 3.6",
+)
