23abe3cd2539a1fefd933962f671544181cc52fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:27 +0000

    Add package funniest-xkn/0.1/funniest_xkn-0.1.tar.gz

---
+++ packages/funniest-xkn/funniest_xkn-0.1.tar.gz/funniest_xkn-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest_xkn',
+      version='0.1',
+      description='The funniest joke in Japan',
+      url='http://github.com/xinkent/funniest_xkn.git',
+      author='K Xin',
+      author_email='xinkent1994@gmail.com',
+      license='MIT',
+      pakages=['funniest_xkn'],
+      zip_safe=False)
