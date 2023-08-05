f8dd1403bcad4a27dac801f7b5706d31eb0e1620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:39 +0000

    Add package azureml-contrib-tensorboard/1.0.15/azureml_contrib_tensorboard-1.0.15-py3-none-any.whl

---
+++ packages/azureml-contrib-tensorboard/azureml_contrib_tensorboard-1.0.15-py3-none-any.whl/azureml/contrib/tensorboard/__init__.py
@@ -0,0 +1,8 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+"""Init file for azureml-contrib-tensorboard/azureml/contrib/tensorboard."""
+from .tensorboard import Tensorboard
+
+__all__ = ["Tensorboard"]
