4b12d88c5cf307a88c29a90c24ca07bdec870c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:16 +0000

    Add package FolderAnalyse/0.1.3/FolderAnalyse-0.1.3-py3-none-any.whl

---
+++ packages/FolderAnalyse/FolderAnalyse-0.1.3-py3-none-any.whl/FolderAnalyse/__init__.py
@@ -0,0 +1,21 @@
+import pytest
+import glob
+import os
+name='FolderAnalyse'
+
+dirname = os.path.dirname(__file__)
+
+
+def runtests():
+    """
+    runtests()
+
+    Run the test suite for FolderAnalyse
+
+    Notes
+    -----
+    Adapted from:
+    https://docs.pytest.org/en/latest/usage.html#calling-pytest-from-python-code
+    """
+
+    pytest.main(['-v', os.path.join(dirname, 'tests')])
