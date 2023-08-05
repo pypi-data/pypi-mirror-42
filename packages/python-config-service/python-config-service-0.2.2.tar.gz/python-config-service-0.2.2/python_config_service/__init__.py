f8bae29d6947e9cff02b7cd3a5f63c62aebd3711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:01 +0000

    Add package python-config-service/0.2.2/python-config-service-0.2.2.tar.gz

---
+++ packages/python-config-service/python-config-service-0.2.2.tar.gz/python-config-service-0.2.2/python_config_service/__init__.py
@@ -0,0 +1,5 @@
+from configuration.config import get_vault_config
+__version__ = '0.2.2'
+__author__ = 'Danie Cilliers <daniel.cilliers@hetzner.co.za>'
+__all__ = []
+name = "python_config_service"
---
+++ packages/python-config-service/python-config-service-0.2.2.tar.gz/python-config-service-0.2.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+setuptools.setup(
+    name="python-config-service",
+    version="0.2.2",
+    url="https://gitlab.host-h.net/DanieCilliers/python-config-service",
+    author="Danie Cilliers",
+    author_email="daniel.cilliers@hetzner.co.za",
+    description="Python package to communicate with the config service",
+    long_description=open('README.md').read(),
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
