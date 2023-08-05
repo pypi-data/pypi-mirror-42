0b9a15a5af1d44b1fb88e98f2fa54e37dfd5d990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:31 +0000

    Add package ruia/0.5.0/ruia-0.5.0-py2.py3-none-any.whl

---
+++ packages/ruia/ruia-0.5.0-py2.py3-none-any.whl/ruia/exceptions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+class InvalidCallbackResult(Exception):
+    pass
+
+
+class InvalidFuncType(Exception):
+    pass
+
+
+class InvalidRequestMethod(Exception):
+    pass
+
+
+class NotImplementedParseError(Exception):
+    pass
+
+
+class NothingMatchedError(Exception):
+    pass
