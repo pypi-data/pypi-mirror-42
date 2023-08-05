6c0e49f8f1853aadcc401896ed0e28767bcb520a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:23 +0000

    Add package lzw3/0.3/lzw3-0.3.tar.gz

---
+++ packages/lzw3/lzw3-0.3.tar.gz/lzw3-0.3/setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(file_name):
+    with open(os.path.join(os.path.dirname(__file__), file_name)) as f:
+        return f.read()
+
+
+setup(
+    name="lzw3",
+    version="0.3",
+
+    # Requires python3.5
+    python_requires=">=3.5",
+
+    # Automatically import lzw3 packages
+    packages=find_packages(),
+
+    # Include the files specified in MANIFEST.in in the release archive
+    include_package_data=True,
+
+    # Scripts to install to the user executable path.
+    # Note that this might be something like /home/user/.local/bin
+    # which in Debian distributions is not included in $PATH.
+    # If you want to use just "compress" or "uncompress", you should add that
+    # path to your $PATH.
+    scripts=["scripts/compress", "scripts/uncompress"],
+
+    # Metadata
+    author="Stefano Dottore",
+    author_email="docheinstein@gmail.com",
+    description="Compressor and decompressor for arbitrary files that use LZW algorithm",
+    long_description=read('README.rst'),
+    license="MIT",
+    keywords="lzw lzw3 compressor decompressor compression compress uncompress",
+    url="https://github.com/Docheinstein/lzw3"
+)
