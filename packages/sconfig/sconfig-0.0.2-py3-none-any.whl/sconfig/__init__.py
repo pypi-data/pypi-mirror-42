f36a18de3204da8420bc10ad5416eb561f5b42db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:00 +0000

    Add package sconfig/0.0.2/sconfig-0.0.2-py3-none-any.whl

---
+++ packages/sconfig/sconfig-0.0.2-py3-none-any.whl/sconfig/__init__.py
@@ -0,0 +1,33 @@
+import os
+import re
+import sys
+
+
+def configure(module):
+    if isinstance(module, str):
+        module = sys.modules[module]
+
+    for name, value in vars(module).items():
+        if not re.match("^[A-Z_]+$", name):
+            continue
+
+        if name in os.environ:
+            setattr(module, name, _convert(value, os.environ[name]))
+        else:
+            os.environ[name] = str(value)
+
+
+# TODO add test case
+def _convert(base, value):
+    if base is True or base is False:
+        if value:
+            return value.lower() == 'true'
+        return False
+
+    if isinstance(base, str):
+        return value
+
+    if isinstance(base, int):
+        return int(value)
+
+    raise NotImplemented()
