835d03ce1a8c71c949224d81c1a9d9024ffea79c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:48 +0000

    Add package sw-cli/0.1.1/sw-cli-0.1.1.tar.gz

---
+++ packages/sw-cli/sw-cli-0.1.1.tar.gz/sw-cli-0.1.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    name='sw-cli',
+    version='0.1.1',
+    author='Gabriel Chiconi',
+    author_email='gabriel.chiconi@gmail.com',
+    description='SSH Wallet for your personal computer!',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/gabrielchiconi/sw',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: Unix',
+    ],
+    scripts=['bin/sw'],
+)
