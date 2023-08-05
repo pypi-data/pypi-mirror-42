9b96dc9dd8af8d7c581c337c07be656e0be67b2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:31 +0000

    Add package codon-harmony/0.9.3/codon_harmony-0.9.3.tar.gz

---
+++ packages/codon-harmony/codon_harmony-0.9.3.tar.gz/codon_harmony-0.9.3/tests/test_codon_harmony.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `codon_tools` package."""
+
+
+import unittest
+
+from codon_harmony import codon_harmony
+
+
+class TestCodon_tools(unittest.TestCase):
+    """Tests for `codon_tools` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
