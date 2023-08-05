868f78a696bfa0114bb5bca34997f9fca0a596c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:04 +0000

    Add package dal-admin-filters/0.3.3/dal_admin_filters-0.3.3-py3-none-any.whl

---
+++ packages/dal-admin-filters/dal_admin_filters-0.3.3-py3-none-any.whl/dal_admin_filters/__init__.py
@@ -0,0 +1,92 @@
+# -*- encoding: utf-8 -*-
+from dal import autocomplete
+from django import forms
+from django.contrib.admin.filters import SimpleListFilter
+from django.core.exceptions import ImproperlyConfigured
+from django.forms.widgets import Media, MEDIA_TYPES
+
+
+class AutocompleteFilter(SimpleListFilter):
+    template = "dal_admin_filters/autocomplete-filter.html"
+    title = ''
+    field_name = ''
+    field_pk = 'id'
+    autocomplete_url = ''
+    is_placeholder_title = False
+    widget_attrs = {}
+
+    class Media:
+        css = {
+            'all': (
+                'dal_admin_filters/css/select2.min.css',
+                'autocomplete_light/select2.css',
+                'dal_admin_filters/css/autocomplete-fix.css'
+            )
+        }
+        js = (
+            'autocomplete_light/jquery.init.js',
+            'dal_admin_filters/js/select2.full.min.js',
+            'autocomplete_light/autocomplete.init.js',
+            'autocomplete_light/forward.js',
+            'autocomplete_light/select2.js',
+            'dal_admin_filters/js/querystring.js',
+        )
+
+    def __init__(self, request, params, model, model_admin):
+        if self.parameter_name:
+            raise AttributeError(
+                'Rename attribute `parameter_name` to '
+                '`field_name` for {}'.format(self.__class__)
+            )
+        self.parameter_name = '{}__{}__exact'.format(self.field_name, self.field_pk)
+        super(AutocompleteFilter, self).__init__(request, params, model, model_admin)
+
+        self._add_media(model_admin)
+
+        field = forms.ModelChoiceField(
+            queryset=self.get_queryset_for_field(model, self.field_name),
+            widget=autocomplete.ModelSelect2(
+                url=self.get_autocomplete_url(request),
+            )
+        )
+
+        attrs = self.widget_attrs.copy()
+        attrs['id'] = 'id-%s-dal-filter' % self.field_name
+        if self.is_placeholder_title:
+            attrs['data-placeholder'] = self.title
+        self.rendered_widget = field.widget.render(
+            name=self.parameter_name,
+            value=self.used_parameters.get(self.parameter_name, ''),
+            attrs=attrs
+        )
+
+    def get_queryset_for_field(self, model, name):
+        return getattr(model, self.field_name).get_queryset()
+
+    def _add_media(self, model_admin):
+
+        if not hasattr(model_admin, 'Media'):
+            raise ImproperlyConfigured('Add empty Media class to %s. Sorry about this bug.' % model_admin)
+
+        def _get_media(obj):
+            return Media(media=getattr(obj, 'Media', None))
+
+        media = _get_media(model_admin) + _get_media(AutocompleteFilter) + _get_media(self)
+
+        for name in MEDIA_TYPES:
+            setattr(model_admin.Media, name, getattr(media, "_" + name))
+
+    def get_autocomplete_url(self, request):
+        return self.autocomplete_url
+
+    def has_output(self):
+        return True
+
+    def lookups(self, request, model_admin):
+        return ()
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(**{self.parameter_name: self.value()})
+        else:
+            return queryset
