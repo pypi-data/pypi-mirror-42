dbe34a51cc07ca3777f95e9eb7c2362c86dd0122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:59 +0000

    Add package trol/0.3.5/trol-0.3.5.tar.gz

---
+++ packages/trol/trol-0.3.5.tar.gz/trol-0.3.5/test/test_model.py
@@ -0,0 +1,178 @@
+import unittest
+from redis import Redis
+from .common import ensure_redis_is_online
+from trol import Model, ModelType, Property, null
+
+
+class Alpha(Model):
+    pass
+
+
+class OfflineModelTests(unittest.TestCase):
+
+    def test_key_retrieval(self):
+        a = Alpha()
+
+        with self.assertRaises(AttributeError):
+            a.key
+
+        a.id = "a"
+        self.assertEquals(a.key, "Alpha:a")
+
+        a.model_name = "FOO"
+        self.assertEquals(a.key, "FOO:a")
+
+    def test_redis_retrieval(self):
+        a = Alpha()
+
+        self.assertIsNone(a.redis)
+
+        canary = object()
+        a.redis = canary
+        self.assertIs(a.redis, canary)
+
+
+class X(Model):
+    def __init__(self, id):
+        self.id = id
+
+    one = Property(autocommit=False, typ=bytes)
+    two = Property('2', typ=str)
+    three = Property(alwaysfetch=True, typ=int)
+    four = Property(autocommit=False, typ=float)
+
+
+class OnlineModelTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.container_token = ensure_redis_is_online()
+        X.redis = Redis(host='localhost', port=6379, db=0)
+
+    def setUp(self):
+        X.redis.flushall()
+
+    def test_invalidate(self):
+        x = X("xyz")
+
+        X.redis.set("X:xyz:one", b'\xDE\xAD\xBE\xEF')
+        X.redis.set("X:xyz:2", "canary")
+        X.redis.set("X:xyz:three", 42)
+        X.redis.set("X:xyz:four", 3.14)
+
+        self.assertEquals(x.one, b'\xDE\xAD\xBE\xEF')
+        self.assertEquals(x.two,  "canary")
+        self.assertEquals(x.three, 42)
+        self.assertEquals(x.four, 3.14)
+
+        X.redis.set("X:xyz:one", b'\x8B\xAD\xF0\x0D')
+        X.redis.set("X:xyz:2", "swallow")
+        X.redis.set("X:xyz:three", 1024)
+        X.redis.set("X:xyz:four", 2.71828)
+
+        self.assertEquals(x.one, b'\xDE\xAD\xBE\xEF')
+        self.assertEquals(x.two,  "canary")
+        self.assertEquals(x.three, 1024)
+        self.assertEquals(x.four, 3.14)
+
+        x.invalidate('two', 'one')
+
+        self.assertEquals(x.one, b'\x8B\xAD\xF0\x0D')
+        self.assertEquals(x.two, "swallow")
+        self.assertEquals(x.three, 1024)
+        self.assertEquals(x.four, 3.14)
+
+        x.invalidate()
+
+        self.assertEquals(x.one, b'\x8B\xAD\xF0\x0D')
+        self.assertEquals(x.two, "swallow")
+        self.assertEquals(x.three, 1024)
+        self.assertEquals(x.four, 2.71828)
+
+    def test_commit(self):
+        x = X("xyz")
+
+        x.one = b'\xDE\xAD\xBE\xEF'
+        x.two = "canary"
+        x.three = 42
+        x.four = 3.14
+
+        self.assertIsNone(X.redis.get("X:xyz:one"))
+        self.assertEquals(X.redis.get("X:xyz:2").decode('utf-8'),  "canary")
+        self.assertEquals(X.redis.get("X:xyz:three").decode('utf-8'), '42')
+        self.assertIsNone(X.redis.get("X:xyz:four"))
+
+        x.commit('one')
+
+        self.assertEquals(X.redis.get("X:xyz:one"), b'\xDE\xAD\xBE\xEF')
+        self.assertEquals(X.redis.get("X:xyz:2").decode('utf-8'),  "canary")
+        self.assertEquals(X.redis.get("X:xyz:three").decode('utf-8'), '42')
+        self.assertIsNone(X.redis.get("X:xyz:four"))
+
+        x.commit()
+
+        self.assertEquals(X.redis.get("X:xyz:one"), b'\xDE\xAD\xBE\xEF')
+        self.assertEquals(X.redis.get("X:xyz:2").decode('utf-8'),  "canary")
+        self.assertEquals(X.redis.get("X:xyz:three").decode('utf-8'), '42')
+        self.assertEquals(X.redis.get("X:xyz:four").decode('utf-8'), '3.14')
+
+    def test_delete(self):
+        x = X("xyz")
+
+        x.one = b'\xDE\xAD\xBE\xEF'
+        x.two = "canary"
+        x.three = 42
+        x.four = 3.14
+        x.commit()
+
+        self.assertEquals(X.redis.get("X:xyz:one"), b'\xDE\xAD\xBE\xEF')
+        self.assertEquals(X.redis.get("X:xyz:2").decode('utf-8'),  "canary")
+        self.assertEquals(X.redis.get("X:xyz:three").decode('utf-8'), '42')
+        self.assertEquals(X.redis.get("X:xyz:four").decode('utf-8'), '3.14')
+
+        x.delete('two', 'three')
+
+        self.assertEquals(X.redis.get("X:xyz:one"), b'\xDE\xAD\xBE\xEF')
+        self.assertIsNone(X.redis.get("X:xyz:2"))
+        self.assertIsNone(X.redis.get("X:xyz:three"))
+        self.assertEquals(X.redis.get("X:xyz:four").decode('utf-8'), '3.14')
+
+        x.delete()
+
+        self.assertIsNone(X.redis.get("X:xyz:one"))
+        self.assertIsNone(X.redis.get("X:xyz:2"))
+        self.assertIsNone(X.redis.get("X:xyz:three"))
+        self.assertIsNone(X.redis.get("X:xyz:four"))
+
+    def test_update(self):
+        x = X("xyz")
+
+        x.update(
+            one=b'\xDE\xAD\xBE\xEF',
+            two="canary",
+            three=42
+        )
+
+        self.assertEquals(X.one.value(x), b'\xDE\xAD\xBE\xEF')
+        self.assertEquals(X.two.value(x),  "canary")
+        self.assertEquals(X.three.value(x), 42)
+        self.assertIs(X.four.value(x), null)
+
+        self.assertIsNone(X.redis.get("X:xyz:one"))
+        self.assertEquals(X.redis.get("X:xyz:2").decode('utf-8'),  "canary")
+        self.assertEquals(X.redis.get("X:xyz:three").decode('utf-8'), '42')
+        self.assertIsNone(X.redis.get("X:xyz:four"))
+
+    def test_exists(self):
+        x = X("xyz")
+
+        self.assertFalse(x.exists())
+
+        x.update(
+            one=b'\xDE\xAD\xBE\xEF',
+            two="canary",
+            three=42
+        )
+
+        self.assertTrue(x.exists())
+        self.assertFalse(x.exists('three', 'four'))
+        self.assertTrue(x.exists('two', 'three'))
---
+++ packages/trol/trol-0.3.5.tar.gz/trol-0.3.5/test/test_property.py
@@ -0,0 +1,190 @@
+import docker
+import unittest
+import pickle
+from redis import Redis
+from trol import Property, null
+from .common import ensure_redis_is_online
+
+
+class A:
+    def __init__(self, redis=None, autocommit=False):
+        self.redis = redis
+
+    alpha = Property("abc", autocommit=False)
+
+
+class OfflinePropertyTests(unittest.TestCase):
+    def test_mangle(self):
+        nameA, nameB = "fred", "wilma"
+        self.assertNotEqual(Property.mangle(nameA), nameA)
+        self.assertNotEqual(Property.mangle(nameA), Property.mangle(nameB))
+
+    def test_key(self):
+        class X:
+            prop = Property("prop")
+            key = None
+
+        x = X()
+        self.assertEquals(X.prop.key(x), "prop")
+
+        x.key = "xkey"
+        self.assertEquals(X.prop.key(x), "xkey:prop")
+
+    def test_value_and_set(self):
+        class X:
+            prop = Property("prop")
+            key = None
+
+        x = X()
+        self.assertIs(X.prop.value(x), null)
+        canary = object()
+        X.prop.set(x, canary)
+        self.assertIs(X.prop.value(x), canary)
+
+
+class OnlinePropertyTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.container_token = ensure_redis_is_online()
+        cls.redis = Redis(host='localhost', port=6379, db=0)
+
+    def setUp(self):
+        self.redis.flushall()
+
+    def test_self(self):
+        """Make sure the docker container can be booted and redis can be connected to
+
+        If this test fails, then the rest surely won't pass
+        """
+        self.redis.set("key", "value")
+        self.assertEquals(self.redis.get("key").decode('utf-8'), "value")
+        self.redis.delete("key")
+
+    def test_commit_nothing(self):
+        class X:
+            prop = Property("p")
+            key = "xkey"
+
+        x = X()
+        key = X.prop.key(x)
+        self.assertIsNone(self.redis.get(key))
+
+        self.redis.set(key, "canary")
+        self.assertTrue(X.prop.commit(x))
+        self.assertEquals(self.redis.get(key).decode('utf-8'), "canary")
+
+    def test_commit_something(self):
+        class X:
+            redis = self.redis
+            prop = Property("p")
+            key = "xkey"
+
+        x = X()
+        key = X.prop.key(x)
+        X.prop.set(x, "something")
+        self.assertIsNone(self.redis.get(key))
+
+        self.redis.set(key, "canary")
+        self.assertTrue(X.prop.commit(x))
+        self.assertEquals(pickle.loads(self.redis.get(key)), "something")
+
+    def test_setter_autocommit(self):
+        class X:
+            redis = self.redis
+            prop = Property("p", autocommit=True)
+            key = "xkey"
+
+        x = X()
+        key = X.prop.key(x)
+        self.assertIsNone(self.redis.get(key))
+        self.redis.set(key, "canary")
+
+        x.prop = "something"
+        self.assertEquals(pickle.loads(self.redis.get(key)), "something")
+
+    def test_setter_no_autocommit(self):
+        class X:
+            redis = self.redis
+            prop = Property("p", autocommit=False)
+            key = "xkey"
+
+        x = X()
+        key = X.prop.key(x)
+        self.assertIsNone(self.redis.get(key))
+        self.redis.set(key, "canary")
+
+        x.prop = "something"
+        self.assertEquals(self.redis.get(key).decode('utf-8'), "canary")
+        self.assertTrue(X.prop.commit(x))
+        self.assertEquals(pickle.loads(self.redis.get(key)), "something")
+
+    def test_redis_returns_null(self):
+        class X:
+            redis = self.redis
+            prop = Property("p", alwaysfetch=True)
+            key = "xkey"
+
+        x = X()
+        key = X.prop.key(x)
+        self.assertIs(x.prop, null)
+
+    def test_getter_alwaysfetch(self):
+        class X:
+            redis = self.redis
+            prop = Property("p", alwaysfetch=True)
+            key = "xkey"
+
+        x = X()
+        key = X.prop.key(x)
+
+        self.assertIs(x.prop, null)
+
+        self.redis.set(key, pickle.dumps("canary"))
+        self.assertEquals(x.prop, "canary")
+
+    def test_getter_no_alwaysfetch(self):
+        class X:
+            redis = self.redis
+            prop = Property("p", alwaysfetch=False)
+            key = "xkey"
+
+        x = X()
+        key = X.prop.key(x)
+
+        self.assertIs(x.prop, null)
+
+        self.redis.set(key, pickle.dumps("canary"))
+        self.assertEquals(x.prop, "canary")
+
+        self.redis.set(key, pickle.dumps("sparrow"))
+        self.assertEquals(x.prop, "canary")
+
+        X.prop.invalidate(x)
+        self.assertEquals(x.prop, "sparrow")
+
+    def test_delete(self):
+        class X:
+            redis = self.redis
+            prop = Property("p")
+            key = "xkey"
+
+        x = X()
+        key = X.prop.key(x)
+
+        self.redis.set(key, "canary")
+        self.assertTrue(X.prop.delete(x))
+        self.assertIsNone(self.redis.get(key))
+        self.assertFalse(X.prop.delete(x))
+
+    def test_exists(self):
+        class X:
+            redis = self.redis
+            prop = Property("p")
+            key = "xkey"
+
+        x = X()
+        key = X.prop.key(x)
+
+        self.assertFalse(X.prop.exists(x))
+        self.redis.set(key, "canary")
+        self.assertTrue(X.prop.exists(x))
