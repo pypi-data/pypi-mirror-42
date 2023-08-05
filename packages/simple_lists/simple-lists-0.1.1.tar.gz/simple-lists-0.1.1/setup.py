bbe0775f20bc2521e37e7bbd314fa4f355021154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:27 +0000

    Add package simple_lists/0.1.1/simple-lists-0.1.1.tar.gz

---
+++ packages/simple_lists/simple-lists-0.1.1.tar.gz/simple-lists-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'simple-lists',
+    version = '0.1.1',
+    description = 'A Python package example',
+    author = 'Reynir Freyr Hauksson',
+    author_email = 'reynirfreyr9@gmail.com',
+    url = 'https://github.com/reynirf/Simple-Lists',
+    py_modules=['simple-lists'],
+)
