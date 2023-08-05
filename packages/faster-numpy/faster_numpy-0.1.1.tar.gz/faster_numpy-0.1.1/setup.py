bfffac4f6341318f0bd75971dab04c177bad3570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:14 +0000

    Add package faster-numpy/0.1.1/faster_numpy-0.1.1.tar.gz

---
+++ packages/faster-numpy/faster_numpy-0.1.1.tar.gz/faster_numpy-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from Cython.Distutils import build_ext
+from Cython.Build import cythonize
+from setuptools import setup, Extension
+import numpy
+
+ext_modules = [
+    Extension('faster_numpy.cylib', sources=['faster_numpy/cylib.pyx'], include_dirs=[numpy.get_include()]),
+    Extension('faster_numpy.clibrary', sources = ['packages/clibrary.cpp'], include_dirs=[numpy.get_include()])
+]
+
+setup(
+    name='faster_numpy',
+    version="0.1.1",
+    packages=['faster_numpy'],
+    ext_modules=cythonize(ext_modules),
+    cmdclass={'build_ext': build_ext},
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
