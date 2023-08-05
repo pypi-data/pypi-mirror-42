c96688b5d1cb4d8ed2d6b34ce517328d36cff90e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:36 +0000

    Add package vectorfields/0.1.2/vectorfields-0.1.2-py2.py3-none-any.whl

---
+++ packages/vectorfields/vectorfields-0.1.2-py2.py3-none-any.whl/vectorfields/__init__.py
@@ -0,0 +1,12 @@
+from .vectorfields import VectorField,\
+                          VectorField2D,\
+                          Vortex2D,\
+                          Convolution2D, \
+                          ElectricDipole2D, \
+                          TwirlFlow2D, \
+                          Belt2D, \
+                          CustomUV2D, \
+                          CustomUVW
+
+__version__ = '0.1.2'
+
