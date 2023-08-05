1e5ad4b502d03af0ed5ff9f9e2dcbf720bbee2bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:52 +0000

    Add package venmosplit/0.7/venmosplit-0.7.tar.gz

---
+++ packages/venmosplit/venmosplit-0.7.tar.gz/venmosplit-0.7/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+  name = 'venmosplit',
+  packages = ['venmosplit'],
+  version = '0.7',
+  description = 'Split the tab the right way',
+  author = 'Nicholas Fix',
+  author_email = 'njfix6@gmail.com',
+  url = 'https://github.com/njfix6/venmo-split.git',
+  download_url = 'https://github.com/njfix6/venmo-split.git/archive/0.2.tar.gz',
+  keywords = ['venmo'],
+  classifiers = [],
+  entry_points = {
+        'console_scripts': ['venmosplit=venmosplit.venmosplit:main']
+        },
+)
