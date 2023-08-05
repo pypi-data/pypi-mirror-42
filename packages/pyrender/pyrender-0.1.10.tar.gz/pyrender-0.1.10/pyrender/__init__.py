ceb95364668d30f24a2aee4475d9fa9d5b62952a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:57 +0000

    Add package pyrender/0.1.10/pyrender-0.1.10.tar.gz

---
+++ packages/pyrender/pyrender-0.1.10.tar.gz/pyrender-0.1.10/pyrender/__init__.py
@@ -0,0 +1,14 @@
+from .camera import Camera, PerspectiveCamera, OrthographicCamera
+from .light import Light, PointLight, DirectionalLight, SpotLight
+from .sampler import Sampler
+from .texture import Texture
+from .material import Material, MetallicRoughnessMaterial #, SpecularGlossinessMaterial
+from .primitive import Primitive
+from .mesh import Mesh
+from .node import Node
+from .scene import Scene
+from .renderer import Renderer
+from .viewer import Viewer
+from .offscreen import OffscreenRenderer
+from .version import __version__
+from .constants import RenderFlags, TextAlign
