6834f9d999f175eaf44cc056fae4ea66c4c0421e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:35 +0000

    Add package gprofiler/1.2.2/gprofiler-1.2.2.tar.gz

---
+++ packages/gprofiler/gprofiler-1.2.2.tar.gz/gprofiler-1.2.2/setup.py
@@ -0,0 +1,15 @@
+''' Setup file for python-gprofiler
+'''
+
+from setuptools import setup, find_packages
+
+setup(
+        name='gprofiler',
+        version='1.2.2',
+        description='Python port of the R wrapper for the g:Profiler functional enrichment tool.',
+        packages=find_packages(),
+        install_requires=['requests', 'pandas'],
+        url='https://github.com/vals/python-gprofiler',
+        author='Valentine Svensson',
+        author_email='valentine@nxn.se'
+    )
