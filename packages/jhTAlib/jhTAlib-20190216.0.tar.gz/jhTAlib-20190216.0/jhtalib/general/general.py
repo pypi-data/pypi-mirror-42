620f407e97d7b10fdea729f9e232eecd0cfaaedd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:50 +0000

    Add package jhTAlib/20190216.0/jhTAlib-20190216.0.tar.gz

---
+++ packages/jhTAlib/jhTAlib-20190216.0.tar.gz/jhTAlib-20190216.0/jhtalib/general/general.py
@@ -0,0 +1,163 @@
+import jhtalib as jhta
+
+
+def NORMALIZE(df, price_max='High', price_min='Low', price='Close'):
+    """
+    Normalize
+    source: https://machinelearningmastery.com/normalize-standardize-time-series-data-python/
+    """
+    normalize_list = []
+    i = 0
+    start = None
+    while i < len(df[price]):
+        if df[price_max][i] != df[price_max][i] or df[price_min][i] != df[price_min][i] or df[price][i] != df[price][i] or i < 1:
+            normalize = float('NaN')
+        else:
+            if start is None:
+                start = i
+            end = i + 1
+            norm_max = max(df[price_max][start:end])
+            norm_min = min(df[price_min][start:end])
+            normalize = (df[price][i] - norm_min) / (norm_max - norm_min)
+        normalize_list.append(normalize)
+        i += 1
+    return normalize_list
+
+def STANDARDIZE(df, price='Close'):
+    """
+    Standardize
+    source: https://machinelearningmastery.com/normalize-standardize-time-series-data-python/
+    """
+    standardize_list = []
+    i = 0
+    start = None
+    while i < len(df[price]):
+        if df[price][i] != df[price][i] or i < 1:
+            standardize = float('NaN')
+        else:
+            if start is None:
+                start = i
+            end = i + 1
+            x = df[price][start:end]
+            mean = jhta.MEAN({'x': x}, len(x), 'x')[-1]
+            standard_deviation = jhta.STDEV({'x': x}, len(x), 'x')[-1]
+            standardize = (df[price][i] - mean) / standard_deviation
+        standardize_list.append(standardize)
+        i += 1
+    return standardize_list
+
+def COV(df1, df2, price1='Close', price2='Close'):
+    """
+    Covariance
+    source: https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Covariance
+    """
+    mean1 = jhta.MEAN(df1, len(df1[price1]), price1)[-1]
+    mean2 = jhta.MEAN(df2, len(df2[price2]), price2)[-1]
+    covariance = .0
+    i = 0
+    while i < len(df1[price1]):
+        a = df1[price1][i] - mean1
+        b = df2[price2][i] - mean2
+        covariance += a * b / len(df1[price1])
+        i += 1
+    return covariance
+
+def BETA(df1, df2, price1='Close', price2='Close'):
+    """
+    Beta
+    source: https://en.wikipedia.org/wiki/Beta_(finance)
+    """
+    beta_list = []
+    i = 0
+    start = None
+    while i < len(df1[price1]):
+        if df1[price1][i] != df1[price1][i] or df2[price2][i] != df2[price2][i] or i < 1:
+            beta = float('NaN')
+        else:
+            if start is None:
+                start = i
+            end = i + 1
+            list1 = df1[price1][start:end]
+            list2 = df2[price2][start:end]
+            covariance = COV({'list1': list1}, {'list2': list2}, 'list1', 'list2')
+            variance = jhta.VARIANCE({'list2': list2}, len(list2), 'list2')[-1]
+            beta = covariance / variance
+        beta_list.append(beta)
+        i += 1
+    return beta_list
+
+def CP(df):
+    """
+    Comparative Performance
+    source: https://www.fmlabs.com/reference/default.htm?url=CompPerformance.htm
+    """
+
+def CRSI(df, n):
+    """
+    Comparative Relative Strength Index
+    source: https://www.fmlabs.com/reference/default.htm?url=RSIC.htm
+    """
+
+def CS(df):
+    """
+    Comparative Strength
+    source: https://www.fmlabs.com/reference/default.htm?url=CompStrength.htm
+    """
+
+def HR(hit_trades_int, total_trades_int):
+    """
+    Hit Rate / Win Rate
+    source: http://traderskillset.com/hit-rate-stock-trading/
+    """
+    return float(hit_trades_int / total_trades_int)
+
+def PLR(mean_trade_profit_float, mean_trade_loss_float):
+    """
+    Profit/Loss Ratio
+    source: https://www.investopedia.com/terms/p/profit_loss_ratio.asp
+    """
+    return float(mean_trade_profit_float / mean_trade_loss_float)
+
+def EV(hitrate_float, mean_trade_profit_float, mean_trade_loss_float):
+    """
+    Expected Value
+    source: https://en.wikipedia.org/wiki/Expected_value
+    """
+    return float((hitrate_float * mean_trade_profit_float) + ((1 - hitrate_float) * mean_trade_loss_float))
+
+def POR(hitrate_float, profit_loss_ratio_float):
+    """
+    Probability of Ruin (Table of Lucas and LeBeau)
+    source: book: Computer Analysis of the Futures Markets
+    """
+    hitrate_list = [.0, .25, .3, .35, .4, .45, .5, .55, .6, .65, .7]
+    profit_loss_ratio_list = [.0, .75, 1, 1.5, 2, 2.5, 3, 3.5, 4]
+
+    table_lucas_lebeau_list = [
+        [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100],
+        [100, 100, 100, 100, 100, 100,  98,  77,  15,   1,   0],
+        [100, 100, 100, 100,  99,  92,  50,   7,   1,   0,   0],
+        [100, 100,  99,  90,  50,  12,   2,   0,   0,   0,   0],
+        [100,  97,  79,  35,   9,   2,   1,   0,   0,   0,   0],
+        [100,  79,  38,  12,   4,   1,   0,   0,   0,   0,   0],
+        [100,  50,  19,   6,   2,   1,   0,   0,   0,   0,   0],
+        [100,  31,  12,   5,   2,   1,   0,   0,   0,   0,   0],
+        [100,  21,   9,   4,   2,   1,   0,   0,   0,   0,   0]
+    ]
+
+    key_hitrate = 0
+    i = 0
+    while i < len(hitrate_list):
+        if hitrate_float >= hitrate_list[i]:
+            key_hitrate = i
+        i += 1
+
+    key_profit_loss_ratio = 0
+    i = 0
+    while i < len(profit_loss_ratio_list):
+        if profit_loss_ratio_float >= profit_loss_ratio_list[i]:
+            key_profit_loss_ratio = i
+        i += 1
+
+    return int(table_lucas_lebeau_list[key_profit_loss_ratio][key_hitrate])
+
---
+++ packages/jhTAlib/jhTAlib-20190216.0.tar.gz/jhTAlib-20190216.0/jhtalib/momentum_indicators/momentum_indicators.py
@@ -0,0 +1,341 @@
+def ADX(df, n):
+    """
+    Average Directional Movement Index
+    """
+
+def ADXR(df, n):
+    """
+    Average Directional Movement Index Rating
+    """
+
+def APO(df, n_fast, n_slow, price='Close'):
+    """
+    Absolute Price Oscillator
+    source: http://www.fmlabs.com/reference/default.htm?url=PriceOscillator.htm
+    """
+    apo_list = []
+    i = 0
+    while i < len(df[price]):
+        if i + 1 < n_slow:
+            apo = float('NaN')
+        else:
+            start_fast = i + 1 - n_fast
+            end = i + 1
+            sma_fast = sum(df[price][start_fast:end]) / n_fast
+            start_slow = i + 1 - n_slow
+            end = i + 1
+            sma_slow = sum(df[price][start_slow:end]) / n_slow
+            apo = sma_slow - sma_fast
+#            apo *= -1
+        apo_list.append(apo)
+        i += 1
+    return apo_list
+
+def AROON(df, n):
+    """
+    Aroon
+    """
+
+def AROONOSC(df, n):
+    """
+    Aroon Oscillator
+    """
+
+def BOP(df):
+    """
+    Balance Of Power
+    """
+
+def CCI(df, n):
+    """
+    Commodity Channel Index
+    """
+
+def CMO(df, n, price='Close'):
+    """
+    Chande Momentum Oscillator
+    """
+
+def DX(df, n):
+    """
+    Directional Movement Index
+    """
+
+def IMI(df):
+    """
+    Intraday Momentum Index
+    source: http://www.fmlabs.com/reference/default.htm?url=IMI.htm
+    """
+    imi_list = []
+    upsum = .0
+    downsum = .0
+    i = 0
+    while i < len(df['Close']):
+        if df['Close'][i] > df['Open'][i]:
+            upsum = upsum + (df['Close'][i] - df['Open'][i])
+        else:
+            downsum = downsum + (df['Open'][i] - df['Close'][i])
+        imi = 100 * (upsum / (upsum + downsum))
+        imi_list.append(imi)
+        i += 1
+    return imi_list
+
+def MACD(df, price='Close'):
+    """
+    Moving Average Convergence/Divergence
+    """
+
+def MACDEXT(df, price='Close'):
+    """
+    MACD with controllable MA type
+    """
+
+def MACDFIX(df, n, price='Close'):
+    """
+    Moving Average Convergence/Divergence Fix 12/26
+    """
+
+def MFI(df, n):
+    """
+    Money Flow Index
+    """
+
+def MINUS_DI(df, n):
+    """
+    Minus Directional Indicator
+    source: http://www.fmlabs.com/reference/default.htm?url=DI.htm
+    """
+    # TODO
+    # NOT FINISHED
+    minus_di_list = []
+    tr_list = []
+    i = 0
+    while i < len(df['Close']):
+        if i + 1 < n:
+            minus_di = float('NaN')
+
+            plus_dm_sum = .0
+            minus_dm_sum = .0
+
+            true_high = max(df['High'][i], df['Close'][i - 1])
+            true_low = min(df['Low'][i], df['Close'][i - 1])
+            tr = true_high - true_low
+            tr_list.append(tr)
+        else:
+            delta_high = df['High'][i - 1] - df['High'][i]
+            delta_low = df['Low'][i] - df['Low'][i - 1]
+            if (delta_high < 0 and delta_low < 0) or delta_high == delta_low:
+                plus_dm = 0
+                minus_dm = 0
+            if delta_high > delta_low:
+                plus_dm = delta_high
+                minus_dm = 0
+            if delta_high < delta_low:
+                plus_dm = 0
+                minus_dm = delta_low
+
+            plus_dm_sum = plus_dm_sum - (plus_dm_sum / n) + plus_dm
+            minus_dm_sum = minus_dm_sum - (minus_dm_sum / n) + minus_dm
+
+            true_high = max(df['High'][i], df['Close'][i - 1])
+            true_low = min(df['Low'][i], df['Close'][i - 1])
+            tr = true_high - true_low
+            tr_list.append(tr)
+
+            tr_sum = tr_list[i -1] - (tr_list[i - 1] / n) + tr_list[i]
+
+            minus_di = 100 * (minus_dm_sum / tr_sum)
+        minus_di_list.append(minus_di)
+        i += 1
+    return minus_di_list
+
+def MINUS_DM(df, n):
+    """
+    Minus Directional Movement
+    """
+    # TODO
+    # NOT FINISHED
+    minus_dm_list = []
+    i = 0
+    while i < len(df['Close']):
+        if i + 1 < n:
+            minus_dm = float('NaN')
+            minus_dm_sum = 1.
+        else:
+            delta_high = df['High'][i - 1] - df['High'][i]
+            delta_low = df['Low'][i] - df['Low'][i - 1]
+            if (delta_high < 0 and delta_low < 0) or delta_high == delta_low:
+                minus_dm = 0
+            if delta_high > delta_low:
+                minus_dm = 0
+            if delta_high < delta_low:
+                minus_dm = delta_low
+            minus_dm_sum = minus_dm_sum - (minus_dm_sum / n) + minus_dm
+            minus_dm = minus_dm_sum
+        minus_dm_list.append(minus_dm)
+        i += 1
+    return minus_dm_list
+
+
+def MOM(df, n, price='Close'):
+    """
+    Momentum
+    source: http://www.fmlabs.com/reference/default.htm?url=Momentum.htm
+    """
+    mom_list = []
+    i = 0
+    while i < len(df[price]):
+        if i + 1 < n:
+            mom = float('NaN')
+        else:
+            mom = df[price][i] - df[price][i - n]
+        mom_list.append(mom)
+        i += 1
+    return mom_list
+
+def PLUS_DI(df, n):
+    """
+    Plus Directional Indicator
+    """
+
+def PLUS_DM(df, n):
+    """
+    Plus Directional Movement
+    """
+
+def PPO(df, price='Close'):
+    """
+    Percentage Price Oscillator
+    """
+
+def ROC(df, n, price='Close'):
+    """
+    Rate of change : ((price/prevPrice)-1)*100
+    """
+    roc_list = []
+    i = 0
+    while i < len(df[price]):
+        if i + 1 < n:
+            roc = float('NaN')
+        else:
+            roc = ((df[price][i] / df[price][i - n]) - 1) * 100
+        roc_list.append(roc)
+        i += 1
+    return roc_list
+
+def ROCP(df, n, price='Close'):
+    """
+    Rate of change Percentage: (price-prevPrice)/prevPrice
+    """
+    rocp_list = []
+    i = 0
+    while i < len(df[price]):
+        if i + 1 < n:
+            rocp = float('NaN')
+        else:
+            rocp = (df[price][i] - df[price][i - n]) / df[price][i - n]
+        rocp_list.append(rocp)
+        i += 1
+    return rocp_list
+
+def ROCR(df, n, price='Close'):
+    """
+    Rate of change ratio: (price/prevPrice)
+    """
+    rocr_list = []
+    i = 0
+    while i < len(df[price]):
+        if i + 1 < n:
+            rocr = float('NaN')
+        else:
+            rocr = df[price][i] / df[price][i - n]
+        rocr_list.append(rocr)
+        i += 1
+    return rocr_list
+
+def ROCR100(df, n, price='Close'):
+    """
+    Rate of change ratio 100 scale: (price/prevPrice)*100
+    source: https://www.fmlabs.com/reference/default.htm?url=RateOfChange.htm
+    """
+    rocr100_list = []
+    i = 0
+    while i < len(df[price]):
+        if i + 1 < n:
+            rocr100 = float('NaN')
+        else:
+            rocr100 = (df[price][i] / df[price][i - n]) * 100
+        rocr100_list.append(rocr100)
+        i += 1
+    return rocr100_list
+
+def RSI(df, n, price='Close'):
+    """
+    Relative Strength Index
+    source: https://www.fmlabs.com/reference/default.htm?url=RSI.htm
+    """
+    rsi_list = []
+    upavg = .0
+    dnavg = .0
+    i = 0
+    while i < len(df[price]):
+        if i + 1 < n:
+            rsi = float('NaN')
+        else:
+            if df[price][i] > df[price][i - 1]:
+                up = df[price][i] - df[price][i - 1]
+                dn = 0
+            else:
+                up = 0
+                dn = df[price][i - 1] - df[price][i]
+            upavg = (upavg * (n - 1) + up) / n
+            dnavg = (dnavg * (n - 1) + dn) / n
+            rsi = 100 * upavg / (upavg + dnavg)
+        rsi_list.append(rsi)
+        i += 1
+    return rsi_list
+
+def STOCH(df):
+    """
+    Stochastic
+    """
+
+def STOCHF(df):
+    """
+    Stochastic Fast
+    """
+
+def STOCHRSI(df, n, price='Close'):
+    """
+    Stochastic Relative Strength Index
+    """
+
+def TRIX(df, n, price='Close'):
+    """
+    1-day Rate-Of-Change (ROC) of a Triple Smooth EMA
+    """
+
+def ULTOSC(df):
+    """
+    Ultimate Oscillator
+    """
+
+def WILLR(df, n):
+    """
+    Williams' %R
+    source: http://www.fmlabs.com/reference/default.htm?url=WilliamsR.htm
+    """
+    willr_list = []
+    i = 0
+    while i < len(df['Close']):
+        if i + 1 < n:
+            willr = float('NaN')
+        else:
+            start = i + 1 - n
+            end = i + 1
+            willr = (max(df['High'][start:end]) - df['Close'][i]) / (max(df['High'][start:end]) - min(df['Low'][start:end])) * 100
+#            willr *= -1
+        willr_list.append(willr)
+        i += 1
+    return willr_list
+
