a886befd99f1397399d2715d4b6992b75d96a96f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:04 +0000

    Add package jiankun_nester/1.2.2/jiankun_nester-1.2.2.tar.gz

---
+++ packages/jiankun_nester/jiankun_nester-1.2.2.tar.gz/jiankun_nester-1.2.2/jiankun_nester.py
@@ -0,0 +1,9 @@
+def print_lol(the_list, level=0):
+    for item in the_list:
+        if isinstance(item, list):
+            print_lol(item, level+1)
+        else:
+            for tab_step in level:
+                print("\t", end='')
+            print(item)
+
