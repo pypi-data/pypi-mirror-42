c42a9ebb102dae593981083de748711be938d4b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:29 +0000

    Add package whatshap/0.18/whatshap-0.18-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/whatshap/whatshap-0.18-cp36-cp36m-manylinux1_x86_64.whl/whatshap/utils.py
@@ -0,0 +1,45 @@
+import gzip
+import pyfaidx
+import os.path
+
+
+class UnknownFileFormatError(Exception):
+	pass
+
+
+class FastaNotIndexedError(Exception):
+	pass
+
+
+def detect_file_format(path):
+	"""
+	Detect file format and return 'BAM', 'CRAM', 'VCF' or raise an UnknownFileFormatError.
+	'VCF' is returned for both uncompressed and compressed VCFs (.vcf and .vcf.gz).
+	"""
+	try:
+		with open(path, 'rb') as f:
+			first_bytes = f.read(16)
+			if first_bytes.startswith(b'CRAM'):
+				return 'CRAM'
+			if first_bytes.startswith(b'##fileformat=VCF'):
+				return 'VCF'
+
+		# Even 'uncompressed' BAM files have a BGZF header and are therefore
+		# detected correctly
+		with gzip.GzipFile(path, 'rb') as f:
+			first_bytes = f.read(16)
+			if first_bytes.startswith(b'BAM\1'):
+				return 'BAM'
+			elif first_bytes.startswith(b'##fileformat=VCF'):
+				return 'VCF'
+	except OSError:
+		pass
+	raise UnknownFileFormatError()
+
+
+def IndexedFasta(path):
+	try:
+		f = pyfaidx.Fasta(path, as_raw=True, sequence_always_upper=True, build_index=False)
+	except pyfaidx.IndexNotFoundError:
+		raise FastaNotIndexedError()
+	return f
