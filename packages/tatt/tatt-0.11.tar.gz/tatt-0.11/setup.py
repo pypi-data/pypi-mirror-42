9b21f67bcc0eb8be5c7ba2cf589a19f2b7cc1baf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:08 +0000

    Add package tatt/0.11/tatt-0.11.tar.gz

---
+++ packages/tatt/tatt-0.11.tar.gz/tatt-0.11/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md') as file:
+    long_description = file.read()
+
+setup(
+    name="tatt",
+    version="0.11",
+    py_modules=['tatt'],
+    install_requires=[
+        'Click',
+        'awscli',
+        'boto3',
+        'requests',
+        ],
+    include_package_data=True,
+    packages=find_packages(),
+    description=('Tatt creates a uniform API for multiple speech-to-text '
+                 '(STT) services.'),
+    long_description=long_description,
+    entry_points='''
+        [console_scripts]
+        transcribe=tatt.transcribe:cli
+    ''',
+        )
