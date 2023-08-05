510e4f225f8fc830338f0fc6a59ad8a12c7fa8dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:05 +0000

    Add package unittest-parallel/0.5/unittest-parallel-0.5.tar.gz

---
+++ packages/unittest-parallel/unittest-parallel-0.5.tar.gz/unittest-parallel-0.5/setup.py
@@ -0,0 +1,31 @@
+# Licensed under the MIT License
+# https://github.com/craigahobbs/unittest-parallel/blob/master/LICENSE
+
+from setuptools import setup
+
+setup(
+    name='unittest-parallel',
+    version='0.5',
+    author='Craig Hobbs',
+    author_email='craigahobbs@gmail.com',
+    description=('Parallel unittest runner.'),
+    keywords='unittest parallel',
+    url='https://github.com/craigahobbs/unittest-parallel',
+    license='MIT',
+    classifiers=[
+        "Environment :: Console",
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        "Topic :: Utilities",
+    ],
+    packages=['unittest_parallel'],
+    install_requires=[],
+    entry_points={
+        'console_scripts': ['unittest-parallel = unittest_parallel:main'],
+    },
+    test_suite='unittest_parallel.tests'
+)
