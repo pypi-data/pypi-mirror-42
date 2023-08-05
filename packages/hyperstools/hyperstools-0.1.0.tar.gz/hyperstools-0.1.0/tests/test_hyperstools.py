56bdd4348132f8bb7dfd0b6dc70cdb7b1932187b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:48 +0000

    Add package hyperstools/0.1.0/hyperstools-0.1.0.tar.gz

---
+++ packages/hyperstools/hyperstools-0.1.0.tar.gz/hyperstools-0.1.0/tests/test_hyperstools.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `hyperstools` package."""
+
+
+import unittest
+
+from hyperstools import hyperstools
+
+
+class TestHyperstools(unittest.TestCase):
+    """Tests for `hyperstools` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
