96363cf8a6de1d6850475703cf109be40daf5587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:44 +0000

    Add package dsnd-probability-ursula/0.1/dsnd_probability_ursula-0.1.tar.gz

---
+++ packages/dsnd-probability-ursula/dsnd_probability_ursula-0.1.tar.gz/dsnd_probability_ursula-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_ursula',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dsnd_probability_ursula'],
+      zip_safe=False)
