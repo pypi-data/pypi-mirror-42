db42e3fcba9742aee2f4fe181c9d140945c0f08d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:24 +0000

    Add package gmopg/0.0.6/gmopg-0.0.6.tar.gz

---
+++ packages/gmopg/gmopg-0.0.6.tar.gz/gmopg-0.0.6/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='gmopg',
+      version='0.0.6',
+      description='Python API Client for GMO Payment Gateway',
+      license="MIT",
+      keywords="gmopg",
+      author='a_r_g_v',
+      author_email='info@arg.vc',
+      url='https://github.com/a-r-g-v/gmo-payment-python',
+      packages=['gmopg'],
+      install_requires=['requests', 'six', 'typing']
+      )
