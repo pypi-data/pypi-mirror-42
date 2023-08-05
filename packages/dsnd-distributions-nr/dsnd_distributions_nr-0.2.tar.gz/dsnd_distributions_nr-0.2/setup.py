6847b698a367dbaac8377d5cb39daee922f6fcd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:57 +0000

    Add package dsnd-distributions-nr/0.2/dsnd_distributions_nr-0.2.tar.gz

---
+++ packages/dsnd-distributions-nr/dsnd_distributions_nr-0.2.tar.gz/dsnd_distributions_nr-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dsnd_distributions_nr',
+      version='0.2',
+      description='Gaussian and binomial distributions',
+      author='Nils Randau',
+      packages=['dsnd_distributions_nr'],
+      zip_safe=False)
