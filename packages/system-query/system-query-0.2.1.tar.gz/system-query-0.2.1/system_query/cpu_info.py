0169231c60c77aace0855a25e221b3e71bb70250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:59 +0000

    Add package system-query/0.2.1/system-query-0.2.1.tar.gz

---
+++ packages/system-query/system-query-0.2.1.tar.gz/system-query-0.2.1/system_query/cpu_info.py
@@ -0,0 +1,58 @@
+"""Functions to query system's CPUs/APUs."""
+
+import logging
+import typing as t
+
+import cpuinfo
+
+from .errors import QueryError
+
+_LOG = logging.getLogger(__name__)
+
+
+try:
+
+    try:
+        import psutil
+    except ImportError as err:
+        raise QueryError('unable to import psutil') from err
+
+    def query_cpu_clock() -> t.Tuple[t.Optional[int], t.Optional[int], t.Optional[int]]:
+        """Get current, minimum and maximum clock frequency of the CPU in the system."""
+        cpu_clock = None
+        try:
+            cpu_clock = psutil.cpu_freq()
+        except FileNotFoundError:
+            pass
+        if cpu_clock is None:
+            return None, None, None
+        return cpu_clock.current, cpu_clock.min, cpu_clock.max
+
+    def query_cpu_cores() -> t.Tuple[t.Optional[int], t.Optional[int]]:
+        """Get number of logical and physical cores of the system's CPU."""
+        return psutil.cpu_count(), psutil.cpu_count(logical=False)
+
+
+except QueryError:
+
+    _LOG.info('proceeding without CPU clock and core count query support', exc_info=1)
+
+    def query_cpu_clock() -> t.Tuple[t.Optional[int], t.Optional[int], t.Optional[int]]:
+        return None, None, None
+
+    def query_cpu_cores() -> t.Tuple[t.Optional[int], t.Optional[int]]:
+        return None, None
+
+
+def query_cpu(**_) -> t.Mapping[str, t.Any]:
+    """Get information about CPU present in the system."""
+    cpu = cpuinfo.get_cpu_info()
+    clock_current, clock_min, clock_max = query_cpu_clock()
+    logical_cores, physical_cores = query_cpu_cores()
+    return {
+        'brand': cpu["brand"],
+        'logical_cores': logical_cores,
+        'physical_cores': physical_cores,
+        'clock': clock_current,
+        'clock_min': clock_min,
+        'clock_max': clock_max}
