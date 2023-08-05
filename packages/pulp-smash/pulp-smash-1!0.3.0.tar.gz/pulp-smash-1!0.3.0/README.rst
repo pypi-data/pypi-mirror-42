0b63c1b80c3970b19554ac5a4294ff06c5250f06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:39 +0000

    Add package pulp-smash/1!0.3.0/pulp-smash-1!0.3.0.tar.gz

---
+++ packages/pulp-smash/pulp-smash-1!0.3.0.tar.gz/pulp-smash-1!0.3.0/README.rst
@@ -0,0 +1,20 @@
+Pulp Smash
+==========
+
+.. image:: https://coveralls.io/repos/github/PulpQE/pulp-smash/badge.svg?branch=master
+    :target: https://coveralls.io/github/PulpQE/pulp-smash?branch=master
+
+Pulp Smash is a toolkit for testing `Pulp`.
+
+Pulp Smash has a presence on the following websites:
+
+* `Documentation`_ is available on ReadTheDocs.
+* A `Python package`_ is available on PyPi.
+* `Source code`_ and the issue tracker are available on GitHub.
+
+.. _Documentation: https://pulp-smash.readthedocs.io
+.. _Pulp: https://pulpproject.org
+.. _Python package: https://pypi.python.org/pypi/pulp-smash
+.. _Source code: https://github.com/PulpQE/pulp-smash/
+
+.. Everything above this comment should also be in docs/index.rst, word for word.
---
+++ packages/pulp-smash/pulp-smash-1!0.3.0.tar.gz/pulp-smash-1!0.3.0/pulp_smash/pulp3/constants.py
@@ -0,0 +1,41 @@
+# coding=utf-8
+"""Constants for Pulp 3 tests."""
+from urllib.parse import urljoin
+
+from pulp_smash.api import _P3_TASK_END_STATES as P3_TASK_END_STATES  # pylint:disable=unused-import
+
+BASE_PATH = '/pulp/api/v3/'
+
+API_DOCS_PATH = urljoin(BASE_PATH, 'docs/')
+
+ARTIFACTS_PATH = urljoin(BASE_PATH, 'artifacts/')
+
+BASE_REMOTE_PATH = urljoin(BASE_PATH, 'remotes/')
+
+BASE_PUBLISHER_PATH = urljoin(BASE_PATH, 'publishers/')
+
+CONTENT_GUARDS_PATH = urljoin(BASE_PATH, 'content-guards/')
+
+CONTENT_PATH = urljoin(BASE_PATH, 'content/')
+
+DISTRIBUTION_PATH = urljoin(BASE_PATH, 'distributions/')
+
+DOWNLOAD_POLICIES = ('immediate', 'on_demand', 'streamed',)
+
+JWT_PATH = urljoin(BASE_PATH, 'jwt/')
+
+MEDIA_PATH = '/var/lib/pulp'
+
+ORPHANS_PATH = urljoin(BASE_PATH, 'orphans/')
+
+PUBLICATIONS_PATH = urljoin(BASE_PATH, 'publications/')
+
+REPO_PATH = urljoin(BASE_PATH, 'repositories/')
+
+STATUS_PATH = urljoin(BASE_PATH, 'status/')
+
+TASKS_PATH = urljoin(BASE_PATH, 'tasks/')
+
+USER_PATH = urljoin(BASE_PATH, 'users/')
+
+WORKER_PATH = urljoin(BASE_PATH, 'workers/')
