e47309392a8084a21bf24f875832eed5a575cb78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:02 +0000

    Add package twitchbot/0.1.0.dev13/twitchbot-0.1.0.dev13-py3-none-any.whl

---
+++ packages/twitchbot/twitchbot-0.1.0.dev13-py3-none-any.whl/twitchbot/bot/check.py
@@ -0,0 +1,68 @@
+from twitchbot.bot.context import Context
+
+
+class Check:
+    def __init__(self):
+        self.description = f''
+
+    async def check(self, ctx: Context):
+        return True
+
+
+class HasRank(Check):
+    def __init__(self, rank):
+        super().__init__()
+        self._rank = rank
+        self.description = f'rank {self._rank} is required'
+
+    async def check(self, ctx: Context):
+        return ctx.author.rank.has(self._rank)
+
+
+class NotRank(HasRank):
+    def __init__(self, rank):
+        super().__init__(rank)
+        self.description = f'rank {self._rank} is not allowed'
+
+    async def check(self, ctx: Context):
+        return not await super().check(ctx)
+
+
+class IsOwner(Check):
+    def __init__(self):
+        super().__init__()
+        self.description = f'you should be owner'
+
+    async def check(self, ctx: Context):
+        return ctx.author.login == ctx.bot.channel
+
+
+class NotOwner(IsOwner):
+    def __init__(self):
+        super().__init__()
+        self.description = f'you should not be owner'
+
+    async def check(self, ctx: Context):
+        return not await super().check(ctx)
+
+
+class HasState(Check):
+    def __init__(self, *states):
+        super().__init__()
+        self._states = states
+        self.description = f'following state(s) should be true: {", ".join(self._states)}'
+
+    async def check(self, ctx: Context):
+        for state in self._states:
+            if not ctx.bot.get_state(state):
+                return False
+        return True
+
+
+class NotState(HasState):
+    def __init__(self, *states):
+        super().__init__(*states)
+        self.description = f'following state(s) should be false: {", ".join(self._states)}'
+
+    async def check(self, ctx: Context):
+        return not await super().check(ctx)
