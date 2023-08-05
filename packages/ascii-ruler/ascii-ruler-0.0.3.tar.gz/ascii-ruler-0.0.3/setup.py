96232c8aba34b525ff4297a7e24aa0a68cf87532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:19 +0000

    Add package ascii-ruler/0.0.3/ascii-ruler-0.0.3.tar.gz

---
+++ packages/ascii-ruler/ascii-ruler-0.0.3.tar.gz/ascii-ruler-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(
+    name='ascii-ruler',
+    version='0.0.3',
+	author="Blake Nedved",
+	author_email="blakeanedved@gmail.com",
+	description="A terminal based ruler for displaying the length of a file, primarily used for codegolfing",
+	url="https://github.com/blakeanedved/ascii-ruler",
+    entry_points={
+        'console_scripts': [
+            'ascii-ruler=ascii_ruler.__init__:main'
+        ]
+    }
+)
