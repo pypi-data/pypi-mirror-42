2072ed2d104a155507fb5ddababb938d3ae06a95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:39 +0000

    Add package syncloud-lib/108/syncloud-lib-108.tar.gz

---
+++ packages/syncloud-lib/syncloud-lib-108.tar.gz/syncloud-lib-108/syncloudlib/integration/device.py
@@ -0,0 +1,56 @@
+import requests
+from syncloudlib.integration.ssh import run_scp, run_ssh
+
+class Device():
+
+    def __init__(self, main_domain, device_host, domain, device_user, device_password, redirect_user, redirect_password, ssh_env_vars):
+        self.main_domain = main_domain
+        self.device_host = device_host
+        self.domain = domain
+        self.device_user = device_user
+        self.device_password = device_password
+        self.redirect_user = redirect_user
+        self.redirect_password = redirect_password
+        self.ssh_env_vars = ssh_env_vars
+        self.ssh_password = 'syncloud'
+        self.session = None
+
+    def activate(self):
+
+        response = requests.post('http://{0}:81/rest/activate'.format(self.device_host),
+                                 data={'main_domain': self.main_domain,
+                                       'redirect_email': self.redirect_user,
+                                       'redirect_password': self.redirect_password,
+                                       'user_domain': self.domain,
+                                       'device_username': self.device_user,
+                                       'device_password': self.device_password})
+        if response.status_code == 200:
+            self.ssh_password = self.device_password
+        return response
+
+    def login(self, retries=5):
+    
+        retry = 0
+        while True:
+            try:
+                session = requests.session()
+                session.post('https://{0}/rest/login'.format(self.device_host), verify=False, data={'name': self.device_user, 'password': self.device_password})
+                response = session.get('https://{0}/rest/user'.format(self.device_host), verify=False, allow_redirects=False)
+                if response.status_code == 200:
+                    self.session = session
+                    return session
+            except Exception, e:
+                retry += 1
+                if retry > retries:
+                    raise e
+                print(e.message)
+                print('retry {0} of {1}'.format(retry, retries))
+   
+    def app_remove(self, app):
+        return self.session.get('https://{0}/rest/remove?app_id={1}'.format(self.device_host, app), allow_redirects=False, verify=False)
+
+    def run_ssh(self, cmd):
+        return run_ssh(self.device_host, cmd, password=self.ssh_password, env_vars=self.ssh_env_vars)
+
+    def http_get(self, url):
+        return self.session.get('https://{0}{1}'.format(self.device_host, url), allow_redirects=False, verify=False)
