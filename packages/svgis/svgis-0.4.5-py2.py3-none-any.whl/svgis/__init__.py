a686173e2953c400ffee813f855c98bb0bbaee1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:33 +0000

    Add package svgis/0.4.5/svgis-0.4.5-py2.py3-none-any.whl

---
+++ packages/svgis/svgis-0.4.5-py2.py3-none-any.whl/svgis/__init__.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of svgis.
+# https://github.com/fitnr/svgis
+
+# Licensed under the GNU General Public License v3 (GPLv3) license:
+# http://opensource.org/licenses/GPL-3.0
+# Copyright (c) 2015-16, Neil Freeman <contact@fakeisthenewreal.org>
+
+from . import bounding
+from . import draw
+from . import errors
+from . import projection
+from . import svg
+from . import style
+from . import svgis
+from . import transform
+from .svgis import map, SVGIS
+
+__version__ = '0.4.5'
+
+__all__ = [
+    'bounding',
+    'draw',
+    'errors',
+    'projection',
+    'style',
+    'svg',
+    'svgis',
+    'transform',
+]
