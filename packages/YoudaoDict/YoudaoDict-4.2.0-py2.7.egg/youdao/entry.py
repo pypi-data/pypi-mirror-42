72e24c4d638d1b6fa7c031af8ccbd5abb753683c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:52 +0000

    Add package YoudaoDict/4.2.0/YoudaoDict-4.2.0-py2.7.egg

---
+++ packages/YoudaoDict/YoudaoDict-4.2.0-py2.7.egg/youdao/entry.py
@@ -0,0 +1,156 @@
+# coding=utf8
+
+import json
+from functools import wraps
+
+from youdao.racer import Race
+from youdao.sqlsaver import SQLSaver
+
+
+def void_return(fun):
+    """check void return"""
+    @wraps(fun)
+    def check(self):
+        if not self.result or not self.valid:
+            return ''
+        else:
+            return fun(self)
+    return check
+
+
+class Youdao(object):
+    def __init__(self, phrase=''):
+        self.phrase = phrase.lower()
+        self.result = {}
+        self.valid = True
+        self.raw = ''
+        self.is_new = False
+
+    def set_phrase(self, phrase):
+        self.phrase = phrase.lower()
+
+    def valid_check(self):
+        if not self.result:
+            self.is_new = True
+            self.valid = False
+            return False
+
+        if 'errorCode' not in self.result:
+            self.is_new = True
+
+        if not self.is_new:
+            if 'translation' not in self.result or \
+                    (len(self.result['translation']) == 1 and self.result['translation'][0] == self.result['query']):
+                self.valid = False
+
+    def executor(self):
+        race = Race(self.phrase)
+        race.launch_race()
+        self.result = race.result
+        self.valid_check()
+        return self.result
+
+    def check_raw(self):
+        return json.dumps(self.result, indent=2)
+
+    @void_return
+    def web(self):
+        temp = ""
+        if not self.is_new:
+            if "web" not in self.result:
+                return temp
+            temp += "网络释义 \033[01;34m>>>\033[00m\n"
+            for i in self.result['web']:
+                temp += '\t{}\n\t'.format(i['key'])
+                for j in i['value']:
+                    temp += '  ' + j + ','
+                temp += '\n'
+        else:
+            if 'web_translate' in self.result and self.result['web_translate']:
+                temp += "网络释义 \033[01;34m>>>\033[00m\n"
+                for i in self.result['web_translate']:
+                    temp += '\t{}\n'.format(i)
+
+        return temp.strip()
+
+    @void_return
+    def trans(self):
+        temp = ""
+        if not self.is_new:
+            temp += '翻译     \033[01;33m>>>\033[00m\n'
+            for i in self.result['translation']:
+                temp += '\t{}\n'.format(i)
+        else:
+            possibles = self.result.get("possibles", [])
+
+            if possibles:
+                temp += "相关词语     \033[01;33m>>>\033[00m\n"
+                for i in possibles:
+                    temp += "\t{}\n\t{}\n\n".format(i['possible'], i['explain'])
+            else:
+                temp = ""
+
+        return temp
+
+    @void_return
+    def basic(self):
+        temp = ""
+        if not self.is_new:
+            data = self.result.get('basic', {})
+            if not data:
+                return temp
+            temp += "基本释义 \033[01;32m>>>\033[00m\n"
+            phonetic = data.get('phonetic', '')
+            us_phonetic = data.get('us-phonetic', '')
+            uk_phonetic = data.get('uk-phonetic', '')
+            base = data.get('explains', '')
+            if phonetic:
+                temp += "\t[{}]\n".format(phonetic)
+            if us_phonetic:
+                temp += "\tus. [{}]\n".format(us_phonetic)
+            if uk_phonetic:
+                temp += "\tuk. [{}]\n".format(uk_phonetic)
+            if base:
+                for i in base:
+                    temp += "\t{}\n".format(i)
+        else:
+            trans = self.result.get('translate', [])
+            if trans:
+                temp += "基本释义 \033[01;32m>>>\033[00m\n"
+                pronounce = self.result.get("pronounces", [])
+                for i in pronounce:
+                    temp += "\t{}\n".format(i)
+                for i in trans:
+                    temp += "\t{}\n".format(i)
+            else:
+                temp = ""
+
+        return temp
+
+    @staticmethod
+    def shred_auto_complete(shred):
+        shreds = SQLSaver().shred_query(shred)
+        return " ".join((x[0] for x in shreds if len(shreds) > 1 and not x[0] == shred or x[0].startswith(shred)))
+
+    @staticmethod
+    def complete_code():
+        return """###-begin-youdao-completion-###
+# simple youdaoDict word auto completion script
+# Installation: youdao -cp >> ~/.bashrc  (or ~/.zshrc)
+# or youdao -cp >> ~/.bash_profile (.etc)
+#
+_youdao_parser_options()
+{
+  local curr_arg;
+  curr_arg=${COMP_WORDS[COMP_CWORD]}
+  COMPREPLY=( $(compgen -W "$(youdao --shard $curr_arg)" $curr_arg ) );
+}
+complete -F _youdao_parser_options youdao
+###-end-youdao-completion-###"""
+
+
+if __name__ == '__main__':
+    youdao = Youdao()
+    print (youdao.shred_auto_complete('f'))
+
+
---
+++ packages/YoudaoDict/YoudaoDict-4.2.0-py2.7.egg/youdao/spider.py
@@ -0,0 +1,136 @@
+# coding=utf8
+
+import sys
+import bs4
+import urllib
+import requests
+from contextlib import contextmanager
+
+
+class Spider(object):
+    def __init__(self, lang='eng', timeout=3):
+        self.__html_url = "http://dict.youdao.com/w/{}/".format(lang)
+        self.__headers = {
+            'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) "
+                          "AppleWebKit/537.36 (KHTML, like Gecko) "
+                          "Chrome/65.0.3325.146 Safari/537.36",
+            "Cookie": "domain=.youdao.com"
+        }
+        self.__timeout = timeout
+
+    @contextmanager
+    def soup(self, target_word):
+        """get bs soup context"""
+        url = self.__html_url + urllib.quote(target_word.replace('/', ''))
+        try:
+            resp = requests.get(url, timeout=self.__timeout, headers=self.__headers)
+            if not resp.status_code == 200:
+                sys.stderr.write('链接 `{}` 非法状态码: {}\r\n'.format(url, resp.status_code))
+            else:
+                yield bs4.BeautifulSoup(resp.content, 'html.parser')
+        except requests.Timeout:
+            sys.stderr.write('链接 `{}` 请求超时\r\n'.format(url))
+            yield None
+        except requests.ConnectionError:
+            sys.stderr.write('链接 `{}` 连接失败\r\n'.format(url))
+            yield None
+        except Exception as e:
+            sys.stderr.write('链接 `{}` 连接时发生未知错误\r\n'.format(e.message))
+            yield None
+
+    def deploy(self, word):
+        """format spider raw data"""
+        with self.soup(word) as soup:
+            if soup is None:
+                # error handle
+                return None, None
+            match = soup.find(class_='keyword')
+            if match:
+                # pronunciation
+                wordbook = soup.find(class_='wordbook-js')
+                _pronounce = wordbook.find_all(class_='pronounce')
+                pronounces = []
+                translate = []
+                web_translate = []
+                word_phrase = []
+                if not _pronounce:
+                    _pronounce = wordbook.find_all(class_='phonetic')
+                for p in _pronounce:
+                    temp = p.get_text().replace(' ', '').replace('\n', '')
+                    if not temp:
+                        continue
+                    pronounces.append(p.get_text().replace(' ', '').replace('\n', ''))
+
+                # translation
+                _trans = soup.find(class_='trans-container')
+                if _trans and _trans.find('ul'):
+                    _normal_trans = _trans.find('ul').find_all('li')
+                    if not _normal_trans:
+                        _normal_trans = _trans.find('ul').find_all(class_='wordGroup')
+                    for _nt in _normal_trans:
+                        title = _nt.find(class_='contentTitle')
+                        type_ = _nt.find('span')
+                        if title and type_ and title != type_:
+                            title = title.get_text()
+                            type_ = type_.get_text()
+                        else:
+                            title = _nt.get_text()
+                            type_ = ''
+                        tmp = (type_ + title).replace('\n', '')
+                        if tmp.count(' ') > 4:
+                            tmp = tmp.replace("  ", '')
+                        translate.append(tmp)
+
+                # web translation
+                _web_trans = soup.find(id="tWebTrans")
+                if _web_trans:
+                    for i in _web_trans.find_all('span', class_=None):
+                        temp = i.get_text().replace('\n', '').replace(' ', '')
+                        if not temp:
+                            continue
+                        web_translate.append(temp)
+
+                    # word phrase
+                    _word_phrase = _web_trans.find(id='webPhrase')
+                    if _word_phrase:
+                        for i in _word_phrase.find_all(class_='wordGroup'):
+                            title = i.find(class_='contentTitle')
+                            if not title:
+                                continue
+                            title = title.get_text()
+                            word_phrase.append({
+                                'phrase': title,
+                                'explain': i.get_text().replace('\n', '').replace(title, '').replace(' ', '')
+                            })
+                return 0, {
+                    'pronounces': pronounces,
+                    'translate': translate,
+                    'web_translate': web_translate
+                }
+            else:
+                # sentence translate may go here, but I won't use youdao. Better use google translate
+                similar = soup.find(class_='error-typo')
+                if similar:
+                    possibles = []
+                    similar = similar.find_all(class_='typo-rel')
+                    for s in similar:
+                        title = s.find(class_='title')
+                        content = s.get_text()
+                        if title:
+                            title = title.get_text().replace(' ', '').replace('\n', '')
+                            content = content.replace(title, '').replace(' ', '').replace('\n', '')
+                        else:
+                            continue
+                        possibles.append({
+                            'possible': title,
+                            'explain': content
+                        })
+                    return 1, {
+                        'possibles': possibles
+                    }
+                return None, None
+
+
+if __name__ == '__main__':
+    print(Spider().deploy('chinese'))
+
