92a4575cbf290ebf0bddae145f60587e12513170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:00 +0000

    Add package venmo-split/0.1/venmo-split-0.1.tar.gz

---
+++ packages/venmo-split/venmo-split-0.1.tar.gz/venmo-split-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'venmo-split',
+  packages = ['venmo-split'], 
+  version = '0.1',
+  description = 'Split the tab the right way',
+  author = 'Nicholas Fix',
+  author_email = 'njfix6@gmail.com',
+  url = 'https://github.com/njfix6/venmo-split.git',
+  download_url = 'https://github.com/njfix6/venmo-split.git/archive/0.1.tar.gz',
+  keywords = ['venmo'],
+  classifiers = [],
+)
