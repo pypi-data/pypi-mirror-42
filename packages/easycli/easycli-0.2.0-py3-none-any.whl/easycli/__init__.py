3e23a525556212f6dbf416726b9db54954728d3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:09 +0000

    Add package easycli/0.2.0/easycli-0.2.0-py3-none-any.whl

---
+++ packages/easycli/easycli-0.2.0-py3-none-any.whl/easycli/__init__.py
@@ -0,0 +1,9 @@
+
+__version__ = '0.2.0'
+
+
+from .argument import Argument
+from .command import Command, SubCommand
+from .root import Root
+from .progressbar import ProgressBar
+
---
+++ packages/easycli/easycli-0.2.0-py3-none-any.whl/easycli/root.py
@@ -0,0 +1,44 @@
+import argparse
+import sys
+import traceback
+from os import path
+
+from .command import Command
+
+
+class Root(Command):
+    __completion__ = None
+
+    def __init__(self, argv=None):
+        if self.__completion__:
+            import argcomplete
+            from .completion import Completion
+            self.__arguments__.append(Completion)
+
+        super().__init__()
+
+        if self.__completion__:
+            argcomplete.autocomplete(self._parser)
+
+        self.launch(argv)
+
+    def launch(self, argv):
+        args = self._parser.parse_args(argv)
+        try:
+            if hasattr(args, 'func'):
+                exitcode = args.func(args)
+            else:
+                exitcode = self(args)
+        except:
+            traceback.print_exc()
+            exitcode = 1
+
+        sys.exit(exitcode or 0)
+
+    @classmethod
+    def _create_parser(self):
+        return argparse.ArgumentParser(
+            prog=path.basename(sys.argv[0]),
+            description=self.__help__
+        )
+
