b17cb2678d8b12631de02939e61cbdd825ec68d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:41 +0000

    Add package bugtrack/0.0.6/bugtrack-0.0.6.tar.gz

---
+++ packages/bugtrack/bugtrack-0.0.6.tar.gz/bugtrack-0.0.6/bugtrack/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+
+from bugtrack import views
+
+urlpatterns = [
+    url('^bug/$', views.bugs, name='bugs'),
+    url('^bug/new/$', views.bug_new, name='bug_new'),
+    url('^bug/(?P<bug_id>\d+)/$', views.bug, name='bug'),
+    url('^bug/(?P<bug_id>\d+)/edit/$', views.bug_edit, name='bug_edit'),
+    url('^download_document/(?P<document_id>\d+)/$', \
+            views.download_document, name='download_document'),
+]
