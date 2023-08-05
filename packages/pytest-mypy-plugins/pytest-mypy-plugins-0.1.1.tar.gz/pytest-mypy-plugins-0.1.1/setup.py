e873f8dac299a867a4f656c8654d55713b965385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:21 +0000

    Add package pytest-mypy-plugins/0.1.1/pytest-mypy-plugins-0.1.1.tar.gz

---
+++ packages/pytest-mypy-plugins/pytest-mypy-plugins-0.1.1.tar.gz/pytest-mypy-plugins-0.1.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+setup(
+    name='pytest-mypy-plugins',
+    version='0.1.1',
+    description='pytest plugin for writing tests for mypy plugins',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    url="https://github.com/mkurnikov/pytest-mypy-plugins",
+    author="Maksim Kurnikov",
+    author_email="maxim.kurnikov@gmail.com",
+    packages=['pytest_mypy'],
+    # the following makes a plugin available to pytest
+    entry_points={
+        'pytest11': [
+            'pytest-mypy-plugins = pytest_mypy.collect'
+        ]
+    },
+    install_requires=[
+        'pytest',
+        'mypy',
+        'decorator',
+        'dataclasses',
+        'capturer'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ]
+)
