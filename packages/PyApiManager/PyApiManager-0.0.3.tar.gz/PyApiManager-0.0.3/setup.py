5129673a1752f455bd7c999a7e4b1abc1dd13acd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:40 +0000

    Add package PyApiManager/0.0.3/PyApiManager-0.0.3.tar.gz

---
+++ packages/PyApiManager/PyApiManager-0.0.3.tar.gz/PyApiManager-0.0.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyApiManager",
+    version="0.0.3",
+    author="Brian Pierre-Emmanuel",
+    author_email="pebrian@outlook.fr",
+    description="This is a usefull api manager.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pebbrian/PyApiManager",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=[
+        'requests',
+    ],
+)
