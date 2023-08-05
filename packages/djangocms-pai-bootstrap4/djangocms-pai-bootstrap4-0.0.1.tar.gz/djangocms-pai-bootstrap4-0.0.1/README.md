22a84e62e7d754f75b5e4ed944e472e887cc3e18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:01 +0000

    Add package djangocms-pai-bootstrap4/0.0.1/djangocms-pai-bootstrap4-0.0.1.tar.gz

---
+++ packages/djangocms-pai-bootstrap4/djangocms-pai-bootstrap4-0.0.1.tar.gz/djangocms-pai-bootstrap4-0.0.1/README.md
@@ -0,0 +1,14 @@
+# djangocms-pai-bootstrap4
+
+Django-cms Bootstrap4 template app
+
+
+## Description
+
+A longer description of your project goes here...
+
+
+## Note
+
+This project has been set up using PyScaffold 3.1. For details and usage
+information on PyScaffold see https://pyscaffold.org/.
---
+++ packages/djangocms-pai-bootstrap4/djangocms-pai-bootstrap4-0.0.1.tar.gz/djangocms-pai-bootstrap4-0.0.1/src/djangocms_pai_bootstrap4/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = 'djangocms-pai-bootstrap4'
+    __version__ = get_distribution(dist_name).version
+except DistributionNotFound:
+    __version__ = 'unknown'
+finally:
+    del get_distribution, DistributionNotFound
+
+default_app_config = 'djangocms_pai_bootstrap4.apps.DjangocmsPaiBootstrap4Config'
