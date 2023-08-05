177732df0e6e772dfc36652dbe423afa84b39a46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:21 +0000

    Add package adamspy/0.6.14/adamspy-0.6.14.tar.gz

---
+++ packages/adamspy/adamspy-0.6.14.tar.gz/adamspy-0.6.14/adamspy/adripy/__init__.py
@@ -0,0 +1,14 @@
+"""adripy is a set of python tools for manipulating MSC Adams Drill files and data.
+
+Available subpackages
+---------------------
+tiem_orbit
+    Classes representing adams drill objects. (Events, Strings, Stabilizers, ect.)
+
+Available modules
+-----------------
+adripy
+    Functions for manipulating MSC Adams Drill files
+"""
+from .adripy import *
+from . import tiem_orbit
