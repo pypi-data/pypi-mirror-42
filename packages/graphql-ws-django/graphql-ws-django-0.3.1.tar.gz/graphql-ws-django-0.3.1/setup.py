f2cebd2f7d60611696186111b05e695e228271da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:11 +0000

    Add package graphql-ws-django/0.3.1/graphql-ws-django-0.3.1.tar.gz

---
+++ packages/graphql-ws-django/graphql-ws-django-0.3.1.tar.gz/graphql-ws-django-0.3.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='graphql-ws-django',
+    packages=['graphql-ws-django'],
+    description='Websocket server for grapqh with django channels 2',
+    version='0.3.1',
+    author='Manviel',
+    author_email='Mihaylo.Merezhko@kname.edu.ua',
+    url='https://github.com/Manviel/graphql-ws',
+    download_url='https://github.com/Manviel/graphql-ws',
+    keywords=['graphql', 'channels'],
+    install_requires=['grapqh-ws'],
+)
