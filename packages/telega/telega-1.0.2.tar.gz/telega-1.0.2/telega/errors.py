14fba7d521a16bc71ae116994a4b6c13be659a06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:24 +0000

    Add package telega/1.0.2/telega-1.0.2.tar.gz

---
+++ packages/telega/telega-1.0.2.tar.gz/telega-1.0.2/telega/errors.py
@@ -0,0 +1,50 @@
+class TDLibError(Exception):
+    pass
+
+
+class FatalError(TDLibError):
+    pass
+
+
+class NoPermission(TDLibError):
+    pass
+
+
+class UnknownError(TDLibError):
+    pass
+
+
+class ObjectNotFound(TDLibError):
+    pass
+
+
+class TooManyRequests(TDLibError):
+    pass
+
+
+class AlreadyAuthorized(TDLibError):
+    pass
+
+
+class AuthError(TDLibError):
+    pass
+
+
+class InvalidPhoneNumber(AuthError):
+    pass
+
+
+class PhoneCodeInvalid(AuthError):
+    pass
+
+
+class PasswordError(AuthError):
+    pass
+
+
+class TwoFactorPasswordNeeded(AuthError):
+    pass
+
+
+class AlreadyLoggingOut(TDLibError):
+    pass
