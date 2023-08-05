eb987c2797ca767f74dc418a9adc7a8d04c6bb73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:13 +0000

    Add package api-nichotined/0.8.7/api-nichotined-0.8.7.tar.gz

---
+++ packages/api-nichotined/api-nichotined-0.8.7.tar.gz/api-nichotined-0.8.7/API/modules/get.py
@@ -0,0 +1,24 @@
+from pprint import pprint
+
+import curlify
+import requests
+
+from .baseApi import BaseApi
+
+
+class Get(BaseApi):
+    def __init__(self):
+        super().__init__()
+
+    def execute(self):
+        self.logger()
+        self._response = requests.get(url=self.url,
+                                      params=self.params, data=self.data, headers=self.headers, cookies=self.cookies,
+                                      files=self.files, auth=self.auth, timeout=self.timeout,
+                                      allow_redirects=self.allow_redirects, proxies=self.proxies,
+                                      hooks=self.hooks, stream=self.stream, verify=self.verify, cert=self.cert,
+                                      json=self.json)
+        print(' Response '.center(80, '*'))
+        pprint(self._response.text)
+        print(' CURL '.center(80, '*'))
+        pprint(curlify.to_curl(self._response.request))
