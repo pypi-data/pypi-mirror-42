6e752914311516729a510faa59bce24b5025955f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:27 +0000

    Add package t4/0.0.8/t4-0.0.8.tar.gz

---
+++ packages/t4/t4-0.0.8.tar.gz/t4-0.0.8/t4/__init__.py
@@ -0,0 +1,20 @@
+
+"""T4 API"""
+
+# Suppress numpy warnings
+import warnings
+warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+
+from .api import (
+    copy,
+    put,
+    get,
+    list_packages,
+    search,
+    config,
+    delete_package
+)
+
+from .packages import Package
+
+from .bucket import Bucket
