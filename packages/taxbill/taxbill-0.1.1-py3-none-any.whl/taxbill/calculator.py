fa05cbd4c5c3bc5d4d123c1d621e7104ce00ee9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:20 +0000

    Add package taxbill/0.1.1/taxbill-0.1.1-py3-none-any.whl

---
+++ packages/taxbill/taxbill-0.1.1-py3-none-any.whl/taxbill/calculator.py
@@ -0,0 +1,83 @@
+from collections import defaultdict
+
+
+def single_band_tax_amount(taxable_amount, rate, threshold=0):
+    """Compute the taxable amount for a single band of tax.
+
+    e.g. Income tax is split into basic and higher bands. This will compute
+    the amount for either one of those if passed the threshold and rate for that
+    band.
+    """
+    if taxable_amount < threshold:
+        return 0
+
+    return rate * (taxable_amount - threshold)
+
+
+def tax_amount(taxable_amount, bands):
+    """Compute the total amount for a tax.
+
+    This will compute the amount for each band a tax and return the sum of
+    those amounts. e.g For income tax, it will return the sum of both the basic
+    and higher band amounts.
+    """
+    amounts = [
+        single_band_tax_amount(
+            min([taxable_amount, band["limit"]]),
+            band["rate"],
+            band["threshold"],
+        )
+        for band in bands
+    ]
+    return round(sum(amounts), 2)
+
+
+def salary_taxes(salary, rates):
+    """NI and income tax on earnings are banded taxes based on the salary
+    amount. We can use a comprehension to loop over those taxes and
+    compute relevant amounts.
+    """
+    return {
+        tax: tax_amount(salary, rates[tax])
+        for tax in ["employees_ni", "employers_ni", "income_tax_earnings"]
+    }
+
+
+def dividend_tax(salary, dividend, rates):
+    """Dividend tax uses the income tax earnings bands to determine the
+    appropriate rate.
+    """
+    # Work out which income tax band applies to the sum of salary and
+    # dividend amounts.
+    idx, _ = next(
+        (idx, band)
+        for idx, band in enumerate(rates["income_tax_earnings"])
+        if band["limit"] > salary + dividend > band["threshold"]
+    )
+    # Use that band to apply the relevant rate to the dividend amount
+    rate = rates["income_tax_dividend"]["rates"][idx]
+    threshold = rates["income_tax_dividend"]["threshold"]
+    return single_band_tax_amount(dividend, rate, threshold)
+
+
+def all_taxes(salary, dividend, personal_pension, employer_pension, rates):
+    result = defaultdict(int)
+
+    if salary:
+        result.update(salary_taxes(salary, rates))
+
+    if dividend:
+        result["income_tax_dividend"] = dividend_tax(salary, dividend, rates)
+
+    if personal_pension:
+        result["income_tax_pension_relief"] = round(
+            personal_pension * rates["income_tax_earnings"][1]["rate"], 2
+        )
+
+    corporation_tax = single_band_tax_amount(
+        salary + result["employers_ni"] + employer_pension,
+        rates["corporation_tax"],
+    )
+    result["corporation_tax"] = round(corporation_tax, 2)
+
+    return result
---
+++ packages/taxbill/taxbill-0.1.1-py3-none-any.whl/taxbill/optimiser.py
@@ -0,0 +1,72 @@
+from pulp import LpInteger, LpMinimize, LpProblem, LpStatus, LpVariable, lpSum
+
+
+def run(requirement, pension, rates, income_tax_band=1):
+    tax_band = rates["income_tax_earnings"][income_tax_band]
+    employees_ni_band = rates["employees_ni"][income_tax_band]
+    employers_ni_band = rates["employers_ni"][income_tax_band]
+    dividend_threshold = rates["income_tax_dividend"]["threshold"]
+    dividend_rate = rates["income_tax_dividend"]["rates"][income_tax_band]
+
+    problem = LpProblem(name="Optimise Tax", sense=LpMinimize)
+
+    salary = LpVariable(
+        name="salary",
+        lowBound=tax_band["threshold"],
+        upBound=tax_band["limit"],
+        cat=LpInteger,
+    )
+    dividend = LpVariable(
+        name="dividend", lowBound=0, upBound=tax_band["limit"], cat=LpInteger
+    )
+    personal_pension = LpVariable(
+        name="personal_pension",
+        lowBound=0,
+        upBound=tax_band["limit"],
+        cat=LpInteger,
+    )
+    employer_pension = LpVariable(
+        name="employer_pension",
+        lowBound=0,
+        upBound=tax_band["limit"],
+        cat=LpInteger,
+    )
+
+    problem += (
+        # Employees NI contribution
+        employees_ni_band["rate"] * (salary - employees_ni_band["threshold"])
+        # Employers NI contribution
+        + employers_ni_band["rate"] * (salary - employers_ni_band["threshold"])
+        # Corporation tax saving on employers NI contribution
+        - rates["corporation_tax"]
+        * employers_ni_band["rate"]
+        * (salary - employers_ni_band["threshold"])
+        # Income tax on salary
+        + tax_band["rate"] * (salary - tax_band["threshold"])
+        # Income tax on dividend
+        + dividend_rate * (dividend - dividend_threshold)
+        # Corporation tax saving on salary
+        - rates["corporation_tax"] * salary
+        # Income tax relief on personal pension contribution
+        - tax_band["rate"] * personal_pension
+        # Corporation Tax saving on employer pension contribution
+        - rates["corporation_tax"] * employer_pension,
+        "TotalTaxBill",
+    )
+
+    problem += (
+        salary + dividend + employer_pension == requirement,
+        "RequiredAmount",
+    )
+    problem += personal_pension <= salary, "PensionLimitation"
+    problem += (
+        employer_pension + personal_pension == pension,
+        "PensionRequirement",
+    )
+
+    problem.solve()
+
+    if LpStatus[problem.status] != "Optimal":
+        raise ValueError("No solution found")
+
+    return {v.name: v.varValue for v in problem.variables()}
