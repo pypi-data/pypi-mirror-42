45d6062cf8d23eb89b9538c716fe32bd6f0950e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:31 +0000

    Add package incapsula/0.4.9/incapsula-0.4.9.tar.gz

---
+++ packages/incapsula/incapsula-0.4.9.tar.gz/incapsula-0.4.9/incapsula/__init__.py
@@ -0,0 +1,7 @@
+from incapsula import getSiteStatus
+from incapsula import listSites 
+from incapsula import modSiteACL
+from incapsula import modSiteLogLevel
+from incapsula import addSite
+from incapsula import getDomainAproverEmail
+from incapsula import modSiteSecurity
