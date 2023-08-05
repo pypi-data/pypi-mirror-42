0dba69a249d7211a8611563d5675d4463456dd2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:00 +0000

    Add package instabotnet/0.0.4/instabotnet-0.0.4.tar.gz

---
+++ packages/instabotnet/instabotnet-0.0.4.tar.gz/instabotnet-0.0.4/instabotnet/methods/authors.py
@@ -0,0 +1,13 @@
+from funcy import rcompose, mapcat
+from ..nodes import User, Media
+from .common import accepts
+
+
+@accepts(Media)
+def authors(bot, nodes,  args):
+
+
+    process = lambda media: media.get_author(bot)
+    result = mapcat(process, nodes)
+
+    return result, bot.last
---
+++ packages/instabotnet/instabotnet-0.0.4.tar.gz/instabotnet-0.0.4/instabotnet/methods/like.py
@@ -0,0 +1,59 @@
+from typing import List
+from funcy import  rcompose, raiser, ignore
+import time
+from .common import accepts, today, tap
+from ..nodes import Node, User, Media
+
+
+
+
+@accepts(Media)
+def like(bot, nodes,  args):
+
+
+    max = float(args['max']) if 'max' in args else 1
+
+    count = 0
+
+    def increment():
+        bot.total['likes'] += 1
+        nonlocal count
+        count += 1
+
+    stop = raiser(StopIteration)
+
+    process = rcompose(
+        lambda x: stop() if x and count >= max else x,
+        # lambda node: node \
+        #     if bot.suitable(node) \
+        #     else tap(None,lambda: bot.logger.warn('{} not suitable'.format(node))),
+        lambda node: like_media(node, bot=bot) \
+            if node else None,
+        lambda x: tap(x, increment) if x else None,
+    )
+
+
+    liked = map(process, nodes)
+    liked = filter(lambda x: x, liked)
+
+    return liked, bot.last
+
+
+def like_media(media, bot):
+        bot.api.like(media.id)
+        if bot.last['status'] != 'ok':
+            bot.logger.warn('request didn\'t return "ok" liking {}'.format(media.url))
+            bot.sleep('error')
+            return None
+        else:
+            with bot.cache as cache:
+                cache['liked'].insert(
+                    dict(identifier=media.id,
+                        specifier=media.url,
+                        time=today(),
+                        type='media',
+                        )
+                )
+            bot.logger.debug('liked %s' % media.url)
+            bot.sleep('like')
+            return media
