d167468695092cc21354e501d6a2e90131344182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:29 +0000

    Add package spatial-access/0.1.6.3/spatial_access-0.1.6.3.tar.gz

---
+++ packages/spatial-access/spatial_access-0.1.6.3.tar.gz/spatial_access-0.1.6.3/spatial_access/src/Graph.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class Graph
+{
+private:
+    unsigned long int vertices;
+public:
+    Graph()= default;
+    void initializeGraph(unsigned long int vertices);
+    std::vector<std::vector<std::pair<unsigned long int, unsigned short int>>> neighbors;
+    unsigned long int getV() const;
+    void addEdge(unsigned long int src, unsigned long int dest, unsigned short int weight);
+};
---
+++ packages/spatial-access/spatial_access-0.1.6.3.tar.gz/spatial_access-0.1.6.3/spatial_access/src/dataFrame.h
@@ -0,0 +1,74 @@
+#pragma once
+#include <unordered_map>
+#include <vector>
+
+
+/* a pandas-like dataFrame */
+template <class row_label_type, class col_label_type>
+class dataFrame {
+
+public:
+    // Public Members
+    std::vector<std::vector<unsigned short int>> dataset;
+
+    // Initialization:
+    dataFrame(bool isSymmetric, unsigned long int rows, unsigned long int cols);
+    
+    // Getters:
+    unsigned short int getValueByLoc(unsigned long int row_loc, unsigned long int col_loc) const;
+    unsigned short int getValueById(const row_label_type& row_id, const col_label_type& col_id) const;
+
+    bool getIsSymmetric() const;
+    unsigned long int getRows() const;
+    unsigned long int getCols() const;
+    const std::vector<std::pair<col_label_type, unsigned short int>> getValuesByRowId(const row_label_type &row_id, bool sort) const;
+    const std::vector<std::pair<row_label_type, unsigned short int>> getValuesByColId(const col_label_type &col_id, bool sort) const;
+    const std::vector<row_label_type>& getRowIds() const;
+    const std::vector<col_label_type>& getColIds() const;
+    const row_label_type& getRowIdForLoc(unsigned long int row_loc) const;
+    const col_label_type& getColIdForLoc(unsigned long int col_loc) const;
+    unsigned long int getRowLocForId(const row_label_type& row_id) const;
+    unsigned long int getColLocForId(const col_label_type& col_id) const;
+    const std::vector<unsigned short int>& getDatasetRow(unsigned long int row) const;
+    const std::vector<std::vector<unsigned short int>>& getDataset() const;
+
+    // Setters
+    void setValueByLoc(unsigned long int row_loc, unsigned long int col_loc, unsigned short int value);
+    void setValueById(const row_label_type& row_id, const col_label_type& col_id, unsigned short int value);
+    void setIsSymmetric(bool isSymmetric);
+    void setRows(unsigned long int rows);
+    void setCols(unsigned long int cols);
+    void setRowByRowLoc(const std::vector<unsigned short int> &row_data, unsigned long int row_loc);
+    void setRowIds(const std::vector<row_label_type>& row_ids);
+    void setColIds(const std::vector<col_label_type>& col_ids);
+    unsigned long int addToRowIndex(const row_label_type& row_id);
+    unsigned long int addToColIndex(const col_label_type& col_id);
+    void setDatasetRow(const std::vector<unsigned short int>& datasetRow, unsigned long int row);
+    void setDataset(const std::vector<std::vector<unsigned short int>>& dataset);
+
+    // Input/Output:
+    bool writeCSV(const std::string &outfile) const;
+    void printDataFrame() const;
+
+    // Utility
+    bool isUnderDiagonal(unsigned long int row_loc, unsigned long int col_loc) const;
+
+
+private:
+    // Input/Output
+    bool writeToStream(std::ostream& streamToWrite) const;
+
+    // Private Members
+    bool isSymmetric;
+    unsigned long int rows;
+    unsigned long int cols;
+    std::vector<row_label_type> rowIds;
+    std::vector<col_label_type> colIds;
+
+    std::unordered_map<row_label_type, unsigned long int> rowIdsToLoc;
+    std::unordered_map<col_label_type, unsigned long int> colIdsToLoc;
+
+    // Utility
+    unsigned long int symmetricEquivalentLoc(unsigned long int row_loc, unsigned long int col_loc) const;
+    unsigned long int dataset_size;
+};
