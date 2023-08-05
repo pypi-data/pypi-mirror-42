ebb0a3f37cb593e4857dacbed340737f409eb9df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:29 +0000

    Add package djangoldp/0.5.22/djangoldp-0.5.22-py3-none-any.whl

---
+++ packages/djangoldp/djangoldp-0.5.22-py3-none-any.whl/djangoldp/models.py
@@ -0,0 +1,31 @@
+from django.conf import settings
+from django.db import models
+
+class LDPSource(models.Model):
+    container = models.URLField()
+    federation = models.CharField(max_length=255)
+    
+    class Meta:
+        rdf_type = 'sib:source'
+        ordering = ('federation',)
+        permissions = (
+            ('view_source', 'acl:Read'),
+            ('control_source', 'acl:Control'),
+        )
+    
+    def __str__(self):
+        return "{}: {}".format(self.federation, self.container)
+
+
+class LDNotification(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    author = models.URLField()
+    object = models.URLField()
+    type = models.CharField(max_length=255)
+    summary = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        permissions = (
+            ('view_todo', 'Read'),
+            ('control_todo', 'Control'),
+        )
