3115a63813417171aded941dac14666e4e184788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:14 +0000

    Add package god-mode/0.0.1/god_mode-0.0.1.tar.gz

---
+++ packages/god-mode/god_mode-0.0.1.tar.gz/god_mode-0.0.1/setup.py
@@ -0,0 +1,32 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+REQUIRED_PACKAGES = [
+    'pyAesCrypt==0.4.2',
+    'colorama==0.4.1',
+    'PyYAML==3.13'
+]
+
+setup(
+    name="god_mode",
+    version="0.0.1",
+    author="Nicolas Lettiere",
+    author_email="braum.exe@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=REQUIRED_PACKAGES,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = {
+        'console_scripts': [
+            'crypt = god_mode.crypt:main'
+        ],
+    }
+)
