045f3d4e3bf90259af4a221ffe65721aca1c6004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:39 +0000

    Add package mekpie/0.1.7/mekpie-0.1.7-py3-none-any.whl

---
+++ packages/mekpie/mekpie-0.1.7-py3-none-any.whl/mekpie/cache.py
@@ -0,0 +1,14 @@
+from inspect import stack
+import shelve
+
+from .structure import get_cache_path
+
+def quick_cache(value=None):
+    frame = stack()[1]
+    identifier = f'{frame.filename}-{frame.function}'
+    with shelve.open(get_cache_path()) as cache:
+        if value is None:
+            return cache[identifier] if identifier in cache else value
+        else:
+            cache[identifier] = value
+            return value
