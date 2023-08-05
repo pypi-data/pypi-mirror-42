f2b822122c823b7f524c0a6786d1be129b8d8103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:59 +0000

    Add package octohot/0.0.5/octohot-0.0.5.tar.gz

---
+++ packages/octohot/octohot-0.0.5.tar.gz/octohot-0.0.5/octohot/cli/regex/find.py
@@ -0,0 +1,35 @@
+import click
+import os
+
+
+@click.command()
+@click.argument('find_string')
+@click.option('--file_pattern', '-f', default=".*",
+              help='Define a file pattern')
+@click.option('--only_filepath', '-p', is_flag=True,
+              help='print only filepaths')
+def find(find_string, file_pattern, only_filepath):
+    """
+    Find a regular expression in all repos
+    """
+    from octohot.cli.config import repositories
+    from octohot.cli.regex.file import File
+    for repo in repositories:
+        files = repo.files(file_pattern)
+        for file in files:
+            try:
+                f = File(file)
+                matches = f.find(find_string)
+                if matches:
+                    for match in matches:
+                        click.echo(
+                            "%s (%s:%s)" % (
+                                file.replace(os.getcwd(), '.'),
+                                f.line(match.start()),
+                                f.line(match.end())
+                            )
+                        )
+                        if not only_filepath:
+                            click.echo(match.group())
+            except UnicodeDecodeError as e:
+                pass
