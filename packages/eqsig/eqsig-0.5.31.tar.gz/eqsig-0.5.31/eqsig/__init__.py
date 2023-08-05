c45fdcede0e2997d01737057ba0e2c2c7d323971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:49 +0000

    Add package eqsig/0.5.31/eqsig-0.5.31.tar.gz

---
+++ packages/eqsig/eqsig-0.5.31.tar.gz/eqsig-0.5.31/eqsig/__init__.py
@@ -0,0 +1,7 @@
+from eqsig.single import Signal, AccSignal, significant_range
+from eqsig import design_spectra
+from eqsig.functions import *
+from eqsig.multiple import Cluster, combine_at_angle, compute_rotated
+from eqsig.measures import calc_significant_duration, calculate_peak  # deprecated load
+from eqsig import measures
+from eqsig import stockwell
