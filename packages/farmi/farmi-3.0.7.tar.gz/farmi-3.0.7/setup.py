00b944c99411982c8b77ed4a9f7b74a3c42896ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:48 +0000

    Add package farmi/3.0.7/farmi-3.0.7.tar.gz

---
+++ packages/farmi/farmi-3.0.7.tar.gz/farmi-3.0.7/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+setup(
+    name='farmi',
+    install_requires=[
+        'pyzmq',
+        'msgpack',
+        'msgpack-numpy'
+    ],
+    python_requires='>=3',
+    version='3.0.7',
+    description='Multisensory processing framework ',
+    author='Patrik Jonell',
+    author_email='pjjonell@kth.se',
+    packages=find_packages(),
+    scripts=[
+        'bin/farmi-server'
+    ],
+)
