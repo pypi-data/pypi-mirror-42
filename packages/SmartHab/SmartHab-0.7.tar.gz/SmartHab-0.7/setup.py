76e83b85a626df9af4395b4bc0fbd8fc13bbcf62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:29 +0000

    Add package SmartHab/0.7/SmartHab-0.7.tar.gz

---
+++ packages/SmartHab/SmartHab-0.7.tar.gz/SmartHab-0.7/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name="SmartHab",
+    version="0.7",
+    py_modules=["pysmarthab"],
+    author="Baptiste Candellier",
+    author_email="outadoc@gmail.com",
+    description="This package allows controlling devices in a SmartHab-powered home.",
+    install_requires=[
+        'requests'
+    ],
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/outadoc/python-smarthab/",
+    test_suite="test_pysmarthab",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent"
+    ]
+)
