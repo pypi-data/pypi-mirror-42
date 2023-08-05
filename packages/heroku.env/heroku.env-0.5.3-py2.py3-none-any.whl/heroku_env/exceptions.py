ed364a7394b89559a90bf141f1be40bc582a45c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:34 +0000

    Add package heroku.env/0.5.3/heroku.env-0.5.3-py2.py3-none-any.whl

---
+++ packages/heroku.env/heroku.env-0.5.3-py2.py3-none-any.whl/heroku_env/exceptions.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+"""
+heroku_env.exceptions
+-----------------------
+All exceptions used in the heroku.env code base are defined here.
+"""
+
+
+class HerokuEnvException(Exception):
+    """
+    Base exception. All other exceptions
+    inherit from here.
+    """
+
+
+class HerokuRunError(HerokuEnvException):
+    """
+    Exception raised when the Heroku command fails.
+    """
+
+
+class InvalidHerokuAppError(HerokuEnvException):
+    """
+    Exception raised when the given Heroku app is invalid
+    """
+
+
+class InvalidAPIKeyError(HerokuEnvException):
+    """
+    Raised when the provided Heroku API key is wrong
+    """
+
+
+class RateLimitExceededError(HerokuEnvException):
+    """
+    Raised when the Heroku API rate limit has exceeded
+    """
