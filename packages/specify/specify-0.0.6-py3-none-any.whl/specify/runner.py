57a21c67e48f6df0d04b53dec2d6d0725af8c309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:55 +0000

    Add package specify/0.0.6/specify-0.0.6-py3-none-any.whl

---
+++ packages/specify/specify-0.0.6-py3-none-any.whl/specify/runner.py
@@ -0,0 +1,34 @@
+from typing import List
+from prophepy import Mock
+from specify.object_behavior import ObjectBehavior
+from specify.result import Result, ResultLine
+
+def run_spec(spec_class: ObjectBehavior) -> List[ResultLine]:
+    result_lines = []
+    spec = spec_class()
+    tests = [func for func in dir(spec) if func.startswith('it_') and callable(getattr(spec, func))]
+    for test in tests:
+        spec._let()
+        exception = None
+
+        try:
+            getattr(spec, test)()
+            for attr in dir(spec):
+                value = getattr(spec, attr)
+                if isinstance(value, Mock):
+                    value.check_prophecies()
+        except Exception as e:
+            exception = str(e)
+
+        result_lines.append(ResultLine(spec_class, test, exception))
+        spec._let_go()
+
+    return result_lines
+
+def run_specs(specs: List[ObjectBehavior]):
+    result_lines = [] # type: List[ResultLine]
+    for spec in specs:
+        result_lines = result_lines + run_spec(spec)
+
+    result = Result(result_lines)
+    print(result.export())
