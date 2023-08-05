0bd8c3aeee0dbe7d9fd24dc52a7edc961be606a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:41 +0000

    Add package spacetimeengine/0.1.8/spacetimeengine-0.1.8.tar.gz

---
+++ packages/spacetimeengine/spacetimeengine-0.1.8.tar.gz/spacetimeengine-0.1.8/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+from setuptools import setup, find_packages
+import unittest
+
+setup(name="spacetimeengine", packages=find_packages())
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requirements = ['sympy']
+
+setuptools.setup(
+    name="spacetimeengine",
+    version="0.1.8",
+    package_dir={"": "spacetimeengine/"},
+    author="Michael.C Ryan",
+    author_email="spacetime.engineer@gmail.com",
+    description="A python physics utility which will analyze any given metric solution to the Einstein field equations.",
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/spacetimeengineer/spacetimeengine",
+    requirements = requirements,
+    dependency_links = ['https://pypi.python.org/simple'],
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
