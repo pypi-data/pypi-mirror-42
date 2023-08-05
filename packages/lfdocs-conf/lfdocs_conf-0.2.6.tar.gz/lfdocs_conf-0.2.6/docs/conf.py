ee11bf15184f0d77bd826dc6596386110f75e999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:04 +0000

    Add package lfdocs-conf/0.2.6/lfdocs_conf-0.2.6.tar.gz

---
+++ packages/lfdocs-conf/lfdocs_conf-0.2.6.tar.gz/lfdocs_conf-0.2.6/docs/conf.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017-2018 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+import os
+import sys
+# Sys.path for RTD to resolve docs_conf package
+sys.path.insert(0, os.path.abspath('..'))
+
+from pbr.version import VersionInfo
+
+from docs_conf.conf import *
+
+version=str(VersionInfo('lfdocs-conf'))
+release=str(VersionInfo('lfdocs-conf'))
