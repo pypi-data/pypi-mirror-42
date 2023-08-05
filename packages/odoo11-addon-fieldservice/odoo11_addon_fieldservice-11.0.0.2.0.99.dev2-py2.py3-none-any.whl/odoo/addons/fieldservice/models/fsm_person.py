d5429b0a4ca58cb88cc5895273b09b95cbb7568c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:54 +0000

    Add package odoo11-addon-fieldservice/11.0.0.2.0.99.dev2/odoo11_addon_fieldservice-11.0.0.2.0.99.dev2-py2.py3-none-any.whl

---
+++ packages/odoo11-addon-fieldservice/odoo11_addon_fieldservice-11.0.0.2.0.99.dev2-py2.py3-none-any.whl/odoo/addons/fieldservice/models/fsm_person.py
@@ -0,0 +1,36 @@
+# Copyright (C) 2018 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class FSMPerson(models.Model):
+    _name = 'fsm.person'
+    _inherits = {'res.partner': 'partner_id'}
+    _description = 'Field Service Worker'
+
+    partner_id = fields.Many2one('res.partner', string='Related Partner',
+                                 required=True, ondelete='restrict',
+                                 delegate=True, auto_join=True)
+    category_ids = fields.Many2many('fsm.category', string='Categories')
+    location_id = fields.Many2one('fsm.location',
+                                  string='Preferred Location')
+    territory_ids = fields.Many2many('fsm.territory', string='Territories')
+    calendar_id = fields.Many2one('resource.calendar',
+                                  string='Working Schedule')
+
+    @api.model
+    def create(self, vals):
+        vals.update({'fsm_person': True})
+        return super(FSMPerson, self).create(vals)
+
+    @api.multi
+    def get_person_information(self, vals):
+        # get person ids
+        person_ids = self.search([('id', '!=', 0), ('active', '=', True)])
+        person_information_dict = []
+        for person in person_ids:
+            person_information_dict.append({
+                'id': person.id,
+                'name': person.name})
+        return person_information_dict
---
+++ packages/odoo11-addon-fieldservice/odoo11_addon_fieldservice-11.0.0.2.0.99.dev2-py2.py3-none-any.whl/odoo/addons/fieldservice/models/fsm_tag.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2018 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class FSMTag(models.Model):
+    _name = 'fsm.tag'
+    _description = 'Field Service Tag'
+
+    name = fields.Char(string='Name', required=True)
+    parent_id = fields.Many2one('fsm.tag', string='Parent')
+    color = fields.Integer('Color Index', default=10)
+
+    _sql_constraints = [
+        ('name_uniq', 'unique (name)', "Tag name already exists!"),
+    ]
