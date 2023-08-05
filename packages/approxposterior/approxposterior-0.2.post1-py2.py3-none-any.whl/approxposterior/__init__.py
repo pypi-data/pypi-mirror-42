cb9c698d6d0103120cfa58012e6c0d748c3218b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:24 +0000

    Add package approxposterior/0.2.post1/approxposterior-0.2.post1-py2.py3-none-any.whl

---
+++ packages/approxposterior/approxposterior-0.2.post1-py2.py3-none-any.whl/approxposterior/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+@author: David P. Fleming, University of Washington, Seattle
+
+approxposteior package init file.
+
+"""
+
+__version__ = "0.2.post1"
+__author__ = "David Fleming (dflemin3@uw.edu)"
+__copyright__ = "Copyright 2019 David P. Fleming"
+
+from .approx import *
+from .utility import *
+from .likelihood import *
+from .gpUtils import *
+from .mcmcUtils import *
+from .plotUtils import *
+from .gmmUtils import *
+from .pool import *
