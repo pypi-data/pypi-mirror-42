b574e32af3b98a889ce351a9ff52fcb15939c242
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:52 +0000

    Add package hyperspyUI/1.1.0/hyperspyUI-1.1.0.tar.gz

---
+++ packages/hyperspyUI/hyperspyUI-1.1.0.tar.gz/hyperspyUI-1.1.0/hyperspyui/plugins/alignzlp.py
@@ -0,0 +1,27 @@
+from hyperspyui.plugins.plugin import Plugin
+
+
+class Alignzlp(Plugin):
+    name = "AlignZLP"
+
+    def create_actions(self):
+        self.add_action(
+            self.name +
+            '.default',
+            self.name,
+            self.default,
+            icon="align_zero_loss.svg",
+            tip="")
+
+    def create_menu(self):
+        self.add_menuitem('EELS', self.ui.actions[self.name + '.default'])
+
+    def create_toolbars(self):
+        self.add_toolbar_button(
+            'EELS', self.ui.actions[
+                self.name + '.default'])
+
+    def default(self):
+        ui = self.ui
+        s = ui.get_selected_signal()
+        s.align_zero_loss_peak()
