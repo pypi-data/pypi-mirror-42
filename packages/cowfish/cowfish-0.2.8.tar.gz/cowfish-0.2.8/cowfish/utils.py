435ba4aaa1d124e44f5fe15f735fff90b9493434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:22 +0000

    Add package cowfish/0.2.8/cowfish-0.2.8.tar.gz

---
+++ packages/cowfish/cowfish-0.2.8.tar.gz/cowfish-0.2.8/cowfish/utils.py
@@ -0,0 +1,34 @@
+import asyncio
+
+
+async def cancel_on_event(coro, event: asyncio.Event):
+    event_task = asyncio.ensure_future(event.wait())
+    done, pending = await asyncio.wait(
+        [event_task, coro], return_when=asyncio.FIRST_COMPLETED
+    )
+    if pending:
+        pending.pop().cancel()
+    while done:
+        task = done.pop()
+        if task is not event_task:
+            return task.result()
+
+
+def format_params(params: dict) -> str:
+    return ", ".join("{}={}".format(k, v) for k, v in params.items())
+
+
+class ClientMethodProxy:
+    def __init__(self, pool, name: str):
+        self._pool = pool
+        self.name = name
+
+    async def __call__(self, *args, **kw):
+        client = await self._pool.acquire()
+        try:
+            return await getattr(client, self.name)(*args, **kw)
+        finally:
+            try:
+                await self._pool.release(client)
+            finally:
+                self._pool = None
