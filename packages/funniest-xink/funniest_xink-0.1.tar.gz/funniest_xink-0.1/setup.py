ba88327fed1ad52f5927681b28f31754f9f156de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:26 +0000

    Add package funniest-xink/0.1/funniest_xink-0.1.tar.gz

---
+++ packages/funniest-xink/funniest_xink-0.1.tar.gz/funniest_xink-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='funniest_xink',
+      version='0.1',
+      description='A test to make Python pakcage',
+      url='http://github.com/xinkent/funniest_xink.git',
+      author_email='xinkent194@gmail.com',
+      license='MIT',
+      pakages=['funniest_xink'],
+      zip_safe=False)
