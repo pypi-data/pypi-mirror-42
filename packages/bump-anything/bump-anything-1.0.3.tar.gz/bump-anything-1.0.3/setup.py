48108d671429db517e415810c992a372630bfd03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:27 +0000

    Add package bump-anything/1.0.3/bump-anything-1.0.3.tar.gz

---
+++ packages/bump-anything/bump-anything-1.0.3.tar.gz/bump-anything-1.0.3/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup
+
+
+# Get long description (used on PyPI project page)
+def get_long_description():
+    try:
+        # Use pandoc to create reStructuredText README if possible
+        import pypandoc
+        return pypandoc.convert_file('README.md', 'rst')
+    except Exception:
+        return None
+
+
+setup(
+    name='bump-anything',
+    version='1.0.3',
+    description='A CLI utility for bumping the version of any file type',
+    long_description=get_long_description(),
+    url='https://github.com/caleb531/bump-anything',
+    author='Caleb Evans',
+    author_email='caleb@calebevans.me',
+    license='MIT',
+    keywords='semver semantic version bump increment',
+    py_modules=['bump'],
+    install_requires=[
+        'semver >= 2, < 3'
+    ],
+    entry_points={
+        'console_scripts': [
+            'bump=bump:main'
+        ]
+    }
+)
