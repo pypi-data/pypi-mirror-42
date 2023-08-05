5804b50381fab4e61f6d6fdd9f624891fd65012c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:33 +0000

    Add package awap2019/1.2.3/awap2019-1.2.3.tar.gz

---
+++ packages/awap2019/awap2019-1.2.3.tar.gz/awap2019-1.2.3/awap2019/bots/__init__.py
@@ -0,0 +1,8 @@
+name = 'bots'
+from .bot import Bot
+from .jittery import JitteryBot
+from .still import StillBot
+from .follow import LineFollower as FollowBot
+from .extrovert import ExtrovertBot
+from .realistic import RealisticBot
+from .random import RandomBot
