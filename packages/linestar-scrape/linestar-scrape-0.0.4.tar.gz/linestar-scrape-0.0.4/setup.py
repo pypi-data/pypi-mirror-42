f400768720156698f83d826fb0e4188f6557823a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:22 +0000

    Add package linestar-scrape/0.0.4/linestar-scrape-0.0.4.tar.gz

---
+++ packages/linestar-scrape/linestar-scrape-0.0.4.tar.gz/linestar-scrape-0.0.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    package_data = {
+        'templates': ['*'],
+    },
+    name="linestar-scrape",
+    version="0.0.4",
+    author="Brendan Hart",
+    author_email="brendanahart@gmail.com",
+    description="A package to scrape linestarapp.com daily fantasy sports ownership data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/WolverineSportsAnalytics/linestar-scrape",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
