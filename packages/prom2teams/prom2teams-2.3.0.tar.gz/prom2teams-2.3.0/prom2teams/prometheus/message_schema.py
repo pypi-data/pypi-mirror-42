beff8ace7fb16156acd81f5b8dfe44d4b6613c98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:49 +0000

    Add package prom2teams/2.3.0/prom2teams-2.3.0.tar.gz

---
+++ packages/prom2teams/prom2teams-2.3.0.tar.gz/prom2teams-2.3.0/prom2teams/prometheus/message_schema.py
@@ -0,0 +1,65 @@
+from marshmallow import Schema, fields, post_load, EXCLUDE
+import logging
+
+log = logging.getLogger('prom2teams')
+
+class MessageSchema(Schema):
+    receiver = fields.Str()
+    status = fields.Str(default='unknown', missing='unknown')
+    alerts = fields.Nested('AlertSchema', many=True)
+    groupKey = fields.Str()
+    externalURL = fields.Str()
+    version = fields.Str()
+    commonAnnotations = fields.Nested('AnnotationSchema', many=False, unknown=EXCLUDE)
+    commonLabels = fields.Nested('LabelSchema', many=False, unknown=EXCLUDE)    
+    groupLabels = fields.Nested('LabelSchema', many=False, unknown=EXCLUDE)    
+
+    @post_load
+    def get_alerts(self, message):
+        log.debug('JSON received is:\n%s', str(message))
+        prom_alerts = []
+        for alert in message['alerts']:
+            status = alert['status']
+            summary = alert['annotations']['summary']
+            instance = alert['labels']['instance']
+            name = alert['labels']['alertname']
+            description = alert['annotations']['description']
+            severity = alert['labels']['severity']
+
+            alert = PrometheusAlert(name, status, severity, summary, instance, description)
+            prom_alerts.append(alert)
+        return prom_alerts
+
+
+class AlertSchema(Schema):
+    status = fields.Str(default='unknown', missing='unknown')
+    labels = fields.Nested('LabelSchema', many=False, unknown=EXCLUDE)
+    annotations = fields.Nested('AnnotationSchema', many=False, unknown=EXCLUDE)
+    startsAt = fields.Date()
+    endsAt = fields.Date()
+    generatorURL = fields.Str()
+
+
+class LabelSchema(Schema):
+    alertname = fields.Str(default='unknown', missing='unknown')
+    device = fields.Str()
+    fstype = fields.Str()
+    instance = fields.Str(default='unknown', missing='unknown')
+    job = fields.Str()
+    mountpoint = fields.Str()
+    severity = fields.Str(default='unknown', missing='unknown')
+
+
+class AnnotationSchema(Schema):
+    description = fields.Str(default='unknown', missing='unknown')
+    summary = fields.Str(default='unknown', missing='unknown')
+
+
+class PrometheusAlert:
+    def __init__(self, name, status, severity, summary, instance, description):
+        self.name = name
+        self.status = status
+        self.severity = severity
+        self.summary = summary
+        self.instance = instance
+        self.description = description
---
+++ packages/prom2teams/prom2teams-2.3.0.tar.gz/prom2teams-2.3.0/setup.py
@@ -0,0 +1,58 @@
+from setuptools import setup, find_packages
+
+
+with open('requirements.txt') as req:
+    requirements = req.read().splitlines()
+
+
+with open('README.md') as f:
+    try:
+        import pypandoc
+        readme = pypandoc.convert('README.md', 'rst')
+    except (IOError, ImportError) as error:
+        readme = open('README.md').read()
+
+
+with open('LICENSE') as f:
+    license = f.read()
+
+
+setup(name='prom2teams',
+      version='2.3.0',
+      description='Project that redirects Prometheus Alert Manager '
+      'notifications to Microsoft Teams',
+      long_description=readme,
+      install_requires=requirements,
+      setup_requires=[
+        'flake8',
+        'pypandoc'
+      ],
+      scripts=[
+          'bin/prom2teams',
+          'bin/prom2teams_uwsgi'
+      ],
+      package_data={
+        '': ['*.ini', '*.j2'],
+      },
+      include_package_data=True,
+      data_files=[
+          ('/usr/local/etc/prom2teams', ['bin/wsgi.py'])
+      ],
+      url='https://github.com/idealista/prom2teams',
+      author='Idealista, S.A.U',
+      author_email='labs@idealista.com',
+      license=license,
+      packages=find_packages(exclude=('tests', 'docs')),
+      keywords='microsoft teams prometheus alert',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Utilities',
+        'Topic :: Communications :: Chat',
+        'License :: OSI Approved :: Apache Software License',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+      ],
+      zip_safe=False)
