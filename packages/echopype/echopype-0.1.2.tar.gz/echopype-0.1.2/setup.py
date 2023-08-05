93f5bbbd5460f5aed22c23f13ac4217ef0b3d073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:38 +0000

    Add package echopype/0.1.2/echopype-0.1.2.tar.gz

---
+++ packages/echopype/echopype-0.1.2.tar.gz/echopype-0.1.2/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup, find_packages
+PACKAGES = find_packages()
+
+# Get version and release info, which is all stored in echopype/version.py
+ver_file = os.path.join('echopype', 'version.py')
+with open(ver_file) as f:
+    exec(f.read())
+
+# Long description will go up on the pypi page
+with open('README.rst') as file:
+    LONG_DESCRIPTION = file.read()
+
+opts = dict(name=NAME,
+            maintainer=MAINTAINER,
+            maintainer_email=MAINTAINER_EMAIL,
+            description=DESCRIPTION,
+            long_description=LONG_DESCRIPTION,
+            url=URL,
+            download_url=DOWNLOAD_URL,
+            license=LICENSE,
+            classifiers=CLASSIFIERS,
+            author=AUTHOR,
+            author_email=AUTHOR_EMAIL,
+            platforms=PLATFORMS,
+            version=VERSION,
+            packages=PACKAGES,
+            package_data=PACKAGE_DATA,
+            install_requires=INSTALL_REQUIRES,
+            requires=REQUIRES,
+            test_requires=TEST_REQUIRES,
+            scripts=SCRIPTS)
+
+
+if __name__ == '__main__':
+    setup(**opts)
