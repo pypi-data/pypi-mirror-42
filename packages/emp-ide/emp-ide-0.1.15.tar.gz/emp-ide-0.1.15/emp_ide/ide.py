64536243997ad4bd8961923d2aad749eda2c0734
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:55 +0000

    Add package emp-ide/0.1.15/emp-ide-0.1.15.tar.gz

---
+++ packages/emp-ide/emp-ide-0.1.15.tar.gz/emp-ide-0.1.15/emp_ide/ide.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from flask import render_template
+from flask import url_for
+import threading
+import sys
+import os
+import webbrowser
+import time
+import fire
+
+app = Flask(__name__, static_folder='static')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+def wsb(device, port):
+    time.sleep(1)
+    print('==> Launch browser...')
+    webbrowser.open('http://localhost:%s' % port)
+    print(
+        '\033[1;31m --> If the browser is not open as expected: ctrl + click the link below\033[0m')
+    print('\033[1;31m --> http://localhost:%s\033[0m' % port)
+    print('==> Enable empwsb...')
+    os.system('empwsb %s' % device)
+
+
+def runserver(port=5000, device=None):
+    if device is not None:
+        thread = threading.Thread(target=wsb, args=(device, port))
+        thread.start()
+    print('==> Runing server...')
+    app.run(port=port)
+
+
+def main():
+    fire.Fire(runserver)
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/emp-ide/emp-ide-0.1.15.tar.gz/emp-ide-0.1.15/setup.py
@@ -0,0 +1,41 @@
+import os
+from setuptools import find_packages, setup
+
+
+setup(
+    name='emp-ide',
+    version='0.1.15',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    description='emp ide offline version',
+    url='http://emp.1zlab.com/',
+    author='Fuermohao',
+    author_email='Fuermohao@outlook.com',
+    platforms='Linux,Unix',
+    keywords='MicroPython,1ZLAB,EMP,ESP',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    entry_points={
+        'console_scripts': [
+            'empide = emp_ide.ide:main',
+        ],
+    },
+    install_requires=[
+        'flask',
+        'emp-wsb',
+        'fire',
+        'pyqt5',
+        'pyserial'
+    ],
+)
