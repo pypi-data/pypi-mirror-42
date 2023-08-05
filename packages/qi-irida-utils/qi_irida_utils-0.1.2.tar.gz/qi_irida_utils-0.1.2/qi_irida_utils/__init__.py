13adef48c06f75b2bc2ca17f07cdd99e83d4f47a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:01 +0000

    Add package qi-irida-utils/0.1.2/qi_irida_utils-0.1.2.tar.gz

---
+++ packages/qi-irida-utils/qi_irida_utils-0.1.2.tar.gz/qi_irida_utils-0.1.2/qi_irida_utils/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for QI IRIDA Utils."""
+
+__author__ = """Nabil-Fareed Alikhan"""
+__email__ = 'nabil-fareed.alikhan@quadram.ac.uk'
+__version__ = '0.1.2'
+
