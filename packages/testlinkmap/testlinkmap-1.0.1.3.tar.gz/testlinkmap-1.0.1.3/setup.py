cfe7e4bce1ff364ff4e3682882ef61282e56ea01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:53 +0000

    Add package testlinkmap/1.0.1.3/testlinkmap-1.0.1.3.tar.gz

---
+++ packages/testlinkmap/testlinkmap-1.0.1.3.tar.gz/testlinkmap-1.0.1.3/setup.py
@@ -0,0 +1,22 @@
+#! /usr/local/bin/python3
+
+from setuptools import setup
+
+setup(
+      name              = 'testlinkmap',
+      version           = '1.0.1.3',
+      description       = 'find macho linkmap and output otool sections',
+      url               = '',
+      author            = 'LiTing',
+      author_email      = 'match.lt@alibaba-inc.com',
+      license           = 'MIT',
+      classifiers       = [
+                              'License :: OSI Approved :: MIT License',
+                              'Operating System :: OS Independent',
+                              'Programming Language :: Python :: 3',
+                              'Programming Language :: Python :: 3.6',
+                              'Programming Language :: Python :: 3.7',
+                              ],
+      keywords          = 'linkmap macho python',
+      packages          = ['testlinkmap', 'testlinkmap.utils'],
+      )
