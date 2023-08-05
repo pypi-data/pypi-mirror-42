8e54860bc79a8c2bdbcd77e043bc5a348e7f742a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:05 +0000

    Add package faker-microservice/1.0.3/faker-microservice-1.0.3.tar.gz

---
+++ packages/faker-microservice/faker-microservice-1.0.3.tar.gz/faker-microservice-1.0.3/setup.py
@@ -0,0 +1,33 @@
+"""Provider for Faker which adds fake microservice names."""
+
+import setuptools
+
+try:
+    with open("README.markdown", "r") as fh:
+        long_description = fh.read()  # pylint: disable=invalid-name
+except FileNotFoundError:
+    # pylint: disable=invalid-name
+    long_description = (
+        "Provider for [Faker](https://faker.readthedocs.io/) which adds fake "
+        "microservice names."
+    )
+
+setuptools.setup(
+    name="faker-microservice",
+    version="1.0.3",
+    author="Craig Anderson",
+    author_email="craiga@craiga.id.au",
+    description="Provider for Faker which adds fake microservice names.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/craiga/faker-microservice",
+    packages=setuptools.find_packages(),
+    install_requires=["faker"],
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
