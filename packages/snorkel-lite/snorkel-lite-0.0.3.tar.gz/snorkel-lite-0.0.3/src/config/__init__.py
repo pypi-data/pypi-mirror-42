ec44faefdb9e1a93191b8557187b252111c2fd98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:00 +0000

    Add package snorkel-lite/0.0.3/snorkel-lite-0.0.3.tar.gz

---
+++ packages/snorkel-lite/snorkel-lite-0.0.3.tar.gz/snorkel-lite-0.0.3/src/config/__init__.py
@@ -0,0 +1,11 @@
+# LOAD CONFIGS
+import os
+
+CFG_DIR = "config/"
+
+from .default import *
+
+if "ENV" in os.environ:
+    print " * Loading settings from", os.environ["ENV"]
+    with open(os.path.join(CFG_DIR, os.environ["ENV"])) as f:
+        exec(f.read())
---
+++ packages/snorkel-lite/snorkel-lite-0.0.3.tar.gz/snorkel-lite-0.0.3/src/plugins/snorkel_advanced_views/__init__.py
@@ -0,0 +1,5 @@
+from timeline import TimelineView
+from overview import OverviewView
+from forecasting import ForecastView
+from drilldown import DrilldownView
+from weco import WecoView
