d64566f140a522afb6309c578065753b6308edb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:55 +0000

    Add package falguard/0.3/falguard-0.3.tar.gz

---
+++ packages/falguard/falguard-0.3.tar.gz/falguard-0.3/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+setup(
+    name='falguard',
+    version='0.3',
+    author='Remy Gwaramadze',
+    author_email='remy@gwaramadze.com',
+    url='https://github.com/gwaramadze/falguard',
+    description='Falcon requests validation against OpenAPI (Swagger) schema',
+    long_description=open('README.rst').read(),
+    license='Apache 2.0',
+    py_modules=['falguard'],
+    install_requires=[
+        'falcon>=1.1.0,<=2.0.0',
+        'bravado_core>=4.6.0,<=6.0.0',
+    ],
+    extras_require={
+        'tests': [
+            'pytest',
+            'pytest-cov',
+            'pytest-flake8',
+            'pytest-pylint',
+            'webtest',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries',
+    ]
+)
