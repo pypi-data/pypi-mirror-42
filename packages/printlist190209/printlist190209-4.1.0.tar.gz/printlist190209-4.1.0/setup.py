5ea162c0a9a44f0c6bab90c125bb88351716e56f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:31 +0000

    Add package printlist190209/4.1.0/printlist190209-4.1.0.tar.gz

---
+++ packages/printlist190209/printlist190209-4.1.0.tar.gz/printlist190209-4.1.0/setup.py
@@ -0,0 +1,12 @@
+﻿from distutils.core import setup        #从python发布工具导入setup函数
+
+setup(
+    name         = 'printlist190209',                    # 包名
+    version      = '4.1.0',                              # 版本号
+    py_modules   = ['printlist'],
+    author       = 'sonia',
+    author_email = 'sonia_du@163.com',
+    url          = 'http://www.devlve.top',
+    description  = "增加输出到文件功能",
+    )
+    
