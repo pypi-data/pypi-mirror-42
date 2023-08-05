4545ec0a57b91f9b2a9d47529d255b68bfc29e55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:38 +0000

    Add package yks/0.0.9/yks-0.0.9.tar.gz

---
+++ packages/yks/yks-0.0.9.tar.gz/yks-0.0.9/README.md
@@ -0,0 +1,15 @@
+# Yks
+
+### An experiment on setting up a Python project.
+
+[![Build Status](https://travis-ci.com/tonikarppi/yks.svg?branch=master)](https://travis-ci.com/tonikarppi/yks)
+
+This project does not really do anything practical. It's a test for properly configuring a modern Python project with testing and Continuos Integration.
+
+## Installation
+
+This project is available for installation on PyPI.
+
+```
+pip install yks
+```
---
+++ packages/yks/yks-0.0.9.tar.gz/yks-0.0.9/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+with open("README.md", encoding="utf8") as f:
+    readme = f.read()
+
+setup(
+    name="yks",
+    version="0.0.9",
+    description="A sample project",
+    url="https://github.com/tonikarppi/yks",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Toni Karppi",
+    author_email="karppitoni@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords="sample",
+    python_requires=">=3.6",
+    entry_points={"console_scripts": ["yks=yks:console_run"]},
+    py_modules=["yks"],
+)
