693b6c63359ccc50543ab6a2e4b756cd5c9aa524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:43 +0000

    Add package pykakasi/0.94/pykakasi-0.94-py3-none-any.whl

---
+++ packages/pykakasi/pykakasi-0.94-py3-none-any.whl/pykakasi/__init__.py
@@ -0,0 +1,11 @@
+from .j2 import J2
+from .k2 import K2
+from .h2 import H2
+from .sym2 import sym2
+from .a2 import a2
+
+from .kakasi import kakasi
+from .wakati import wakati
+from .exceptions import *
+
+__all__ = ["kakasi"]
