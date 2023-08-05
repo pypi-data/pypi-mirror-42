01997233ded82aa0b7444e89a354677e32a842c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:49 +0000

    Add package oy/0.1.3/oy-0.1.3-py3-none-any.whl

---
+++ packages/oy/oy-0.1.3-py3-none-any.whl/oy/contrib/richtext_page/admin.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+    oy.contrib.richtext_page.admin
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    Provides the admin interface for editing oy rich text pages.
+
+    :copyright: (c) 2018 by Musharraf Omer.
+    :license: MIT, see LICENSE for more details.
+"""
+
+from oy.models import db
+from oy.babel import lazy_gettext
+from oy.contrib.admin import PageAdmin
+from oy.contrib.admin.fields import TinymceTextAreaField
+from .models import RichTextPage
+
+
+class RichTextPageAdmin(PageAdmin):
+
+    form_columns = list(PageAdmin.form_columns)
+    form_columns.insert(4, "content")
+    form_overrides = {"content": TinymceTextAreaField}
+
+
+def register_admin(app, admin):
+    admin.add_view(
+        RichTextPageAdmin(
+            RichTextPage,
+            db.session,
+            name=lazy_gettext("Rich Text Pages"),
+            menu_icon_type="fa",
+            menu_icon_value="fa-newspaper-o",
+            menu_order=700,
+        )
+    )
---
+++ packages/oy/oy-0.1.3-py3-none-any.whl/oy/models/abstract/displayable.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""	
+    oy.models.abstract.displayable
+    ~~~~~~~~~~
+
+    This module provides the core content model for oy.
+    Its core functionality is implemented by other mixin classes
+
+    :copyright: (c) 2018 by Musharraf Omer.
+    :license: MIT, see LICENSE for more details.
+"""
+
+from datetime import datetime
+from sqlalchemy.ext.declarative import declared_attr
+from flask import current_app
+from oy.boot.sqla import db
+from oy.babel import lazy_gettext
+
+from .slugged import Titled
+from .metadata import Metadata
+from .published import Published
+from .user_related import UserRelated
+
+
+class Displayable(db.Model, Titled, Metadata, Published, UserRelated):
+    """The core of all oy content models"""
+
+    __abstract__ = True
+    __slugcolumn__ = "title"
+    __keywordscolumn__ = "title"
+    __metatitle_column__ = "title"
+
+    @declared_attr
+    def contenttype(cls):
+        return db.Column(db.String(128))
+
+    @declared_attr
+    def show_in_menu(cls):
+        return db.Column(
+            db.Boolean,
+            default=True,
+            info=dict(
+                label="Show in menu",
+                description="Show this page in the navigation menus.",
+            ),
+        )
+
+    @declared_attr
+    def __mapper_args__(cls):
+        return dict(
+            polymorphic_identity=cls.__contenttype__, polymorphic_on=cls.contenttype,
+        )
