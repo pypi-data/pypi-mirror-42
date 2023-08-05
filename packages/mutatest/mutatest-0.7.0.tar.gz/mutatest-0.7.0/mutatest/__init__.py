ffb0c45e6687877b36b945de1bd2de14e29f2077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:55 +0000

    Add package mutatest/0.7.0/mutatest-0.7.0.tar.gz

---
+++ packages/mutatest/mutatest-0.7.0.tar.gz/mutatest-0.7.0/mutatest/__init__.py
@@ -0,0 +1,17 @@
+"""Mutation initialization.
+"""
+__version__ = "0.7.0"
+
+__title__ = "mutatest"
+__description__ = "Python mutation testing."
+__url__ = "https://github.com/EvanKepner/mutatest"
+__uri__ = __url__
+__doc__ = __description__ + " <" + __uri__ + ">"
+
+__author__ = "Evan Kepner"
+
+# matches my GitHub commit email, open issues on the repo for comments or questions
+__email__ = "EvanKepner@users.noreply.github.com"
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2018 Evan Kepner"
