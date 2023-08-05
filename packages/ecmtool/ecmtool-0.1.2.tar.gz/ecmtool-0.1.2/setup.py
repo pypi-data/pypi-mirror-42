aae35c3857e19e957705f547f1eb6a008746e7ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:51 +0000

    Add package ecmtool/0.1.2/ecmtool-0.1.2.tar.gz

---
+++ packages/ecmtool/ecmtool-0.1.2.tar.gz/ecmtool-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='ecmtool',
+    version='0.1.2',
+    packages=['ecmtool'],
+    install_requires=['numpy',
+                      'scipy',
+                      'sympy',
+                      'python-libsbml',
+                      'cbmpy',
+                      'pycddlib',
+                      'psutil'],
+    url='https://github.com/tjclement/ecmtool',
+    license='MIT',
+    author='Tom Clement',
+    author_email='mr@tomclement.nl',
+    description='Calculates elementary conversion modes (Urbanczik & Wagner, 2005) of metabolic networks.'
+)
