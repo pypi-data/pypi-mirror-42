78c20f75832ce3ecd8f08b45feffc9a607a545f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:56 +0000

    Add package framler/0.0.1/framler-0.0.1-py2.py3-none-any.whl

---
+++ packages/framler/framler-0.0.1-py2.py3-none-any.whl/framler/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for framler."""
+
+__author__ = """Phan Hoang"""
+__email__ = 'phan.huy.hoang@framgia.com '
+__version__ = '0.0.1'
+
+
+from framler import *  # noqa
