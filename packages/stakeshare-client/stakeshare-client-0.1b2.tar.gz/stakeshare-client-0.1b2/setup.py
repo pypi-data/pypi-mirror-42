6f84ca189349325f56494dc07ff898e2cc54041f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:02 +0000

    Add package stakeshare-client/0.1b2/stakeshare-client-0.1b2.tar.gz

---
+++ packages/stakeshare-client/stakeshare-client-0.1b2.tar.gz/stakeshare-client-0.1b2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='stakeshare-client',
+      version='0.1-beta.2',
+      description='StakeShare RPC client',
+      url='https://github.com/mrmm/stakeshare-python',
+      author='Ronittos',
+      author_email='ronittos.dorittoss@gmail.com',
+      license='Apache 2.0',
+      packages=['stakeshare'],
+      zip_safe=False)
