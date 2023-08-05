2a793ba2013478dec90e6b5591783a33dcb944f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:03 +0000

    Add package file-config/0.3.5/file-config-0.3.5.tar.gz

---
+++ packages/file-config/file-config-0.3.5.tar.gz/file-config-0.3.5/src/file_config/constants.py
@@ -0,0 +1,5 @@
+# Copyright (c) 2019 Stephen Bunn <stephen@bunn.io>
+# ISC License <https://choosealicense.com/licenses/isc>
+
+CONFIG_KEY = "__file_config_metadata"
+REGEX_TYPE_NAME = "__file_config_regex"
