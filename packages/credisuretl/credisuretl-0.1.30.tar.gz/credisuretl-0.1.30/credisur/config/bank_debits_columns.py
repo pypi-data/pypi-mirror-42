0db9bb41743f6bd46762881a552bcafbedf40fb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:54 +0000

    Add package credisuretl/0.1.30/credisuretl-0.1.30.tar.gz

---
+++ packages/credisuretl/credisuretl-0.1.30.tar.gz/credisuretl-0.1.30/credisur/config/bank_debits_columns.py
@@ -0,0 +1,9 @@
+def get_bank_debit_columns():
+    config_list = list()
+    config_list.append(("A", "Tipo de Novedad", 'event_type'))
+    config_list.append(("B", "CBU Bloque 1", 'cbu1'))
+    config_list.append(("C", "CBU Bloque 2", 'cbu2'))
+    config_list.append(("D", "Identificación del cliente", 'customer'))
+#    config_list.append(("E", "Ref. del débito", 'ref_credit'))
+    config_list.append(("E", "Importe", 'amount'))
+    return config_list
