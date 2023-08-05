c0b3dd631c60e3c728e5146b0f04f37c9ee3fd27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:22 +0000

    Add package katka-core/0.8.0/katka_core-0.8.0-py37-none-any.whl

---
+++ packages/katka-core/katka_core-0.8.0-py37-none-any.whl/katka/viewsets.py
@@ -0,0 +1,37 @@
+from katka.constants import STATUS_INACTIVE
+from katka.fields import username_on_model
+from rest_framework import mixins, status
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
+
+
+class ReadOnlyAuditViewMixin(mixins.RetrieveModelMixin,
+                             mixins.ListModelMixin,
+                             GenericViewSet):
+    model = None
+
+    def get_queryset(self):
+        return self.model.objects.exclude(status=STATUS_INACTIVE)
+
+
+class AuditViewSet(mixins.CreateModelMixin,
+                   mixins.UpdateModelMixin,
+                   mixins.DestroyModelMixin,
+                   ReadOnlyAuditViewMixin):
+
+    def create(self, request, *args, **kwargs):
+        with username_on_model(self.model, request.user.username):
+            return super().create(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        with username_on_model(self.model, request.user.username):
+            return super().update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.status = STATUS_INACTIVE
+
+        with username_on_model(self.model, request.user.username):
+            instance.save()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
