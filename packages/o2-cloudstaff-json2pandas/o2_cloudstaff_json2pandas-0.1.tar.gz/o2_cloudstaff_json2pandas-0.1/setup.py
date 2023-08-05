b82b0a066f754a2b41db46c3447dc1e7a02456e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:48 +0000

    Add package o2-cloudstaff-json2pandas/0.1/o2_cloudstaff_json2pandas-0.1.tar.gz

---
+++ packages/o2-cloudstaff-json2pandas/o2_cloudstaff_json2pandas-0.1.tar.gz/o2_cloudstaff_json2pandas-0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="o2_cloudstaff_json2pandas",
+    version="0.1",
+    author="Hudge",
+    author_email="",
+    description="",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="",
+    py_modules=["o2_cloudstaff_json2pandas"],
+    install_requires=[""],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
