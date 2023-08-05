6900147f695a33d020d393187555c0ee7dc16195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:07 +0000

    Add package cloudsmith-cli/0.7.1/cloudsmith_cli-0.7.1-py2.py3-none-any.whl

---
+++ packages/cloudsmith-cli/cloudsmith_cli-0.7.1-py2.py3-none-any.whl/cloudsmith_cli/cli/commands/docs.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""CLI/Commands - Launch the help website."""
+from __future__ import absolute_import, print_function, unicode_literals
+
+import click
+
+from . import main
+from .. import decorators
+from ...core.utils import get_help_website
+
+
+@main.command()
+@decorators.common_cli_config_options
+@click.pass_context
+def docs(ctx, opts):
+    """Launch the help website in your browser."""
+    # pylint: disable=unused-argument
+    click.launch(get_help_website())
