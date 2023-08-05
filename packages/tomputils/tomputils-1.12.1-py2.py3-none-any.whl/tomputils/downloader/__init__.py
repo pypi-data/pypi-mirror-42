299c69c2c312586b20a3c02d49b0845ccdbadce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:17 +0000

    Add package tomputils/1.12.1/tomputils-1.12.1-py2.py3-none-any.whl

---
+++ packages/tomputils/tomputils-1.12.1-py2.py3-none-any.whl/tomputils/downloader/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+#  Purpose: Segmenting downloader.
+#   Author: Tom Parker
+#
+# -----------------------------------------------------------------------------
+"""
+tomputils.downloader
+====================
+
+A simple segmenting downloader.
+
+:license:
+    CC0 1.0 Universal
+    http://creativecommons.org/publicdomain/zero/1.0/
+"""
+
+from .downloader import Downloader, fetch
+
+__all__ = ['fetch', 'Downloader']
