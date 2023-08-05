df2385e0eb66ad16f232d52f8484e1cdd04fb8c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:34 +0000

    Add package romanify/0.0.8/romanify-0.0.8.tar.gz

---
+++ packages/romanify/romanify-0.0.8.tar.gz/romanify-0.0.8/romanify/__init__.py
@@ -0,0 +1,13 @@
+""" Roman numbers converting library """
+from .main import romanify
+
+__author__ = 'Evgenia Turueva'
+__email__ = 'tindomirel1994@gmail.com'
+__license__ = 'MIT'
+__version__ = '0.0.8'
+
+
+__all__ = (
+    'romanify',
+    '__author__', '__email__', '__license__', '__version__'
+)
