3500193269474b9ce960b8658cd9b15f78bf4644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:41 +0000

    Add package simple_lists/0.1.3/simple_lists-0.1.3.tar.gz

---
+++ packages/simple_lists/simple_lists-0.1.3.tar.gz/simple_lists-0.1.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = 'simple_lists',
+    version = '0.1.3',
+    description = 'A Python package example',
+    author = 'Reynir Freyr Hauksson',
+    author_email = 'reynirfreyr9@gmail.com',
+    url = 'https://github.com/reynirf/Simple-Lists',
+    py_modules=['simple_lists'],
+)
