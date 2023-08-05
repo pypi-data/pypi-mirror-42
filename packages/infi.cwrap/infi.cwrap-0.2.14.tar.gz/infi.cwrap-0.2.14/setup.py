67a6ef0ad5af13fac2d9ca7203b1f094875a8467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:18 +0000

    Add package infi.cwrap/0.2.14/infi.cwrap-0.2.14.tar.gz

---
+++ packages/infi.cwrap/infi.cwrap-0.2.14.tar.gz/infi.cwrap-0.2.14/setup.py
@@ -0,0 +1,50 @@
+
+SETUP_INFO = dict(
+    name = 'infi.cwrap',
+    version = '0.2.14',
+    author = 'Arnon Yaari',
+    author_email = 'arnony@infinidat.com',
+
+    url = 'https://github.com/Infinidat/infi.cwrap',
+    license = 'BSD',
+    description = """easy function wrapping for ctypes""",
+    long_description = """Working with ctypes is hard. specifically, wrapping functions is hard. This module helps you do that.""",
+
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers = [
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+
+    install_requires = ['setuptools'],
+    namespace_packages = ['infi'],
+
+    package_dir = {'': 'src'},
+    package_data = {'': []},
+    include_package_data = True,
+    zip_safe = False,
+
+    entry_points = dict(
+        console_scripts = [],
+        gui_scripts = [],
+        ),
+)
+
+if SETUP_INFO['url'] is None:
+    _ = SETUP_INFO.pop('url')
+
+def setup():
+    from setuptools import setup as _setup
+    from setuptools import find_packages
+    SETUP_INFO['packages'] = find_packages('src')
+    _setup(**SETUP_INFO)
+
+if __name__ == '__main__':
+    setup()
+
