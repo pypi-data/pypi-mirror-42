dda4aea1d3f4d3155ce0aa1f3ebe28eb583f7e7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:17 +0000

    Add package ltpackagetemplate/0.0.0.2/ltpackagetemplate-0.0.0.2.tar.gz

---
+++ packages/ltpackagetemplate/ltpackagetemplate-0.0.0.2.tar.gz/ltpackagetemplate-0.0.0.2/setup.py
@@ -0,0 +1,22 @@
+#! /usr/local/bin/python3
+
+from setuptools import setup
+
+setup(
+      name              = 'ltpackagetemplate',
+      version           = '0.0.0.2',
+      description       = 'package template',
+      url               = '',
+      author            = 'LiTing',
+      author_email      = '',
+      license           = 'MIT',
+      classifiers       = [
+                              'License :: OSI Approved :: MIT License',
+                              'Operating System :: OS Independent',
+                              'Programming Language :: Python :: 3',
+                              'Programming Language :: Python :: 3.6',
+                              'Programming Language :: Python :: 3.7',
+                              ],
+      keywords          = 'package template',
+      packages          = ['ltpackagetemplate', 'ltpackagetemplate.utils'],
+      )
