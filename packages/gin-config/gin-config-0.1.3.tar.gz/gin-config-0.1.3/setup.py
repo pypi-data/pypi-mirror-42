8f53c30223f90b6948b3f267967004aa54f6e303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:00 +0000

    Add package gin-config/0.1.3/gin-config-0.1.3.tar.gz

---
+++ packages/gin-config/gin-config-0.1.3.tar.gz/gin-config-0.1.3/setup.py
@@ -0,0 +1,94 @@
+# coding=utf-8
+# Copyright 2018 The Gin-Config Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Setup script for gin-config.
+
+See:
+
+https://github.com/google/gin-config
+
+"""
+
+import codecs
+from os import path
+import sys
+from setuptools import find_packages
+from setuptools import setup
+
+_VERSION = '0.1.3'
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with codecs.open(path.join(here, 'README.md'), encoding='utf-8') as f:
+  long_description = f.read()
+
+install_requires = ['six >= 1.10.0']
+test_requirements = ['six >= 1.10.0', 'absl-py >= 0.1.6']
+
+if sys.version_info < (3, 4):
+  install_requires.append('enum34 >= 1.1.6')
+
+setup(
+    name='gin-config',
+    version=_VERSION,
+    include_package_data=True,
+    packages=find_packages(exclude=['docs']),  # Required
+    package_data={'testdata': ['testdata/*.gin']},
+    install_requires=install_requires,
+    extras_require={  # Optional
+        'tf': ['tensorflow >= 1.6'],
+    },
+    tests_require=test_requirements,
+    description='Gin-config: a lightweight configuration library for Python',
+    long_description=long_description,
+    url='https://github.com/google/gin-config',  # Optional
+    author='The Gin-Config Team',  # Optional
+    classifiers=[  # Optional
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: Apache Software License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+    ],
+    project_urls={  # Optional
+        'Documentation': 'https://github.com/google/gin-config/docs',
+        'Bug Reports': 'https://github.com/google/gin-config/issues',
+        'Source': 'https://github.com/google/gin-config',
+    },
+    license='Apache 2.0',
+    keywords='gin-config gin python configuration machine learning'
+)
