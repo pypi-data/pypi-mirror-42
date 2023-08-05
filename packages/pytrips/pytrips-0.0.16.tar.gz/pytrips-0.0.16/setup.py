3e2985ef98897139f3d1649c85fdec22191330bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:35 +0000

    Add package pytrips/0.0.16/pytrips-0.0.16.tar.gz

---
+++ packages/pytrips/pytrips-0.0.16.tar.gz/pytrips-0.0.16/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install as _install
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+class install(_install):
+    """Post-installation for installation mode."""
+    def run(self):
+        _install.run(self)
+        import nltk
+        nltk.download("wordnet")
+        nltk.download("stopwords")
+
+if __name__ == '__main__':
+    setup(
+        name="pytrips",
+        version="0.0.16",
+        author="Rik Bose",
+        author_email="rbose@cs.rochester.edu",
+        description="A simple python package for accessing the trips ontology and lexicon",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/mrmechko/pytrips",
+        packages=find_packages(exclude=["test"]),
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+            "Operating System :: OS Independent",
+        ],
+        install_requires=["nltk", "jsontrips"],
+        cmdclass={
+            "install": install,
+        },
+    )
