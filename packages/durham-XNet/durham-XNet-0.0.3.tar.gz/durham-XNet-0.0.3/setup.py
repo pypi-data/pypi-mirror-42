99d31274737b7b75741931b4749aaf90c45b1e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:25 +0000

    Add package durham-XNet/0.0.3/durham-XNet-0.0.3.tar.gz

---
+++ packages/durham-XNet/durham-XNet-0.0.3.tar.gz/durham-XNet-0.0.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+        name = "durham-XNet",
+        version = "0.0.3",
+        author = "Joseph Bullock, Carolina Cuesta-Lazaro, & Arnau Quera-Bofarull",
+        author_email = "joseph.bullock@durham.ac.uk",
+        description = "A CNN to segment X-Ray images",
+        long_description = long_description,
+        long_description_content_type = "text/markdown",
+        url = "https://github.com/josephPB/XNet",
+        packages = setuptools.find_packages(),
+        classifiers = [
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            ],
+        install_requires =  setuptools.find_packages(exclude=('tests', 'docs'))
+        )
+
