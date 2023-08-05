aaa8a315bac6181bceed92330be43ec9f4af060a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:48 +0000

    Add package teralytic/1.1.0.dev15/teralytic-1.1.0.dev15.tar.gz

---
+++ packages/teralytic/teralytic-1.1.0.dev15.tar.gz/teralytic-1.1.0.dev15/teralytic/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from teralytic.api.organization_api import OrganizationApi
+from teralytic.api.soils_api import SoilsApi
+from teralytic.api.telemetry_api import TelemetryApi
