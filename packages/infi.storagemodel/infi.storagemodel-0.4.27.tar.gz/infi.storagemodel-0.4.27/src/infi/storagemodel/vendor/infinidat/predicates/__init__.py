5f8dab9d744ff93654176af7180dbad66a6eea6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:21 +0000

    Add package infi.storagemodel/0.4.27/infi.storagemodel-0.4.27.tar.gz

---
+++ packages/infi.storagemodel/infi.storagemodel-0.4.27.tar.gz/infi.storagemodel-0.4.27/src/infi/storagemodel/vendor/infinidat/predicates/__init__.py
@@ -0,0 +1,82 @@
+from infi.storagemodel.predicates import FiberChannelMappingExists, FiberChannelMappingNotExists
+
+from logging import getLogger
+log = getLogger(__name__)
+
+
+class InfinidatVolumeExists(object):
+    """A predicate that checks if an Infinidat volume exists"""
+    def __init__(self, system_serial, volume_id):
+        self.system_serial = system_serial
+        self.volume_id = volume_id
+
+    def __call__(self):
+        from ..shortcuts import get_infinidat_block_devices
+        from infi.instruct.errors import InstructError
+        from infi.asi.errors import AsiException
+        devices_to_query = get_infinidat_block_devices()
+        log.debug("Looking for Infinidat volume id {} from system id {}".format(self.volume_id, self.system_serial))
+        for device in devices_to_query:
+            device.get_scsi_test_unit_ready()
+            try:
+                # As some vendors seem to be inconsistent with the designators passed within the pages, using
+                # vendor-specifc pages seems more safe:
+                if 0xc6 not in device.get_scsi_inquiry_pages():
+                    log.debug("No vendor-specific page 0xc6 for device {!r}, returning False now as this should be fixed by rescan".format(device))
+                    return False
+                volume_id = device.get_vendor().get_volume_id()
+                system_serial = device.get_vendor().get_system_serial()
+                log.debug("Found INFINIDAT volume id {} from system id {}".format(volume_id, system_serial))
+            except (AsiException, InstructError):
+                log.exception("failed to identify INFINIDAT volume, returning False now as this should be fixed by rescan")
+                return False
+        return any(self.volume_id == device.get_vendor().get_volume_id() and
+                   self.system_serial == device.get_vendor().get_system_serial()
+                   for device in devices_to_query)
+
+    def __repr__(self):
+        return "<{}(system_serial={!r}, volume_id={!r})>".format(self.__class__.__name__, self.system_serial, self.volume_id)
+
+
+class InfinidatVolumeDoesNotExist(InfinidatVolumeExists):
+    """A predicate that checks if an Infinidat volume does not exist"""
+    def __call__(self):
+        return not super(InfinidatVolumeDoesNotExist, self).__call__()
+
+
+class FiberChannelMappingExistsUsingLinuxSG(FiberChannelMappingExists):
+    def _get_chain_of_devices(self, model):
+        from itertools import chain
+        return chain(model.get_scsi().get_all_linux_scsi_generic_disk_devices(),
+                     model.get_scsi().get_all_storage_controller_devices())
+
+    def __repr__(self):
+        return "<{}: {!r}>".format(self.__class__.__name__, self.connectivity)
+
+
+class FiberChannelMappingNotExistsUsingLinuxSG(FiberChannelMappingExistsUsingLinuxSG):
+    def __call__(self):
+        return not super(FiberChannelMappingNotExistsUsingLinuxSG, self).__call__()
+
+
+def get_predicate_for_checking_non_zero_host_id(system_serial, cluster_id=False):
+    def all_storage_devices_on_logical_unit_0_of_specific_box_show_non_zero_host_id():
+        from infi.storagemodel.vendor.infinidat.shortcuts import get_infinidat_block_devices_and_controllers__mapped_to_lun0
+        from infi.storagemodel.errors import RescanIsNeeded
+        devices = []
+
+        try:
+            devices.extend(get_infinidat_block_devices_and_controllers__mapped_to_lun0())
+        except RescanIsNeeded:
+            pass
+
+        for device in devices:
+            if cluster_id:
+                if device.get_vendor().get_system_serial() == system_serial and device.get_vendor().get_cluster_id() == 0:
+                    return False
+            else:
+                if device.get_vendor().get_system_serial() == system_serial and device.get_vendor().get_host_id() == 0:
+                    return False
+        return True
+
+    return all_storage_devices_on_logical_unit_0_of_specific_box_show_non_zero_host_id
