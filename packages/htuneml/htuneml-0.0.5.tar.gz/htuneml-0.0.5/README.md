2da8bec3668851259b74e24abb6c24ac4143ea67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:15 +0000

    Add package htuneml/0.0.5/htuneml-0.0.5.tar.gz

---
+++ packages/htuneml/htuneml-0.0.5.tar.gz/htuneml-0.0.5/README.md
@@ -0,0 +1,75 @@
+# HTUNEML - machine learning experiments monitoring and tuning
+
+**Quickstart:** ``pip install htuneml``. See the "Installing" section for more details.
+
+
+Project links:
+
+-  `PyPI <https://pypi.python.org/pypi/htuneml/>`__
+-  `Source code <https://github.com/htuneml/htuneml>`__
+-  `Issue tracker <https://github.com/htuneml/htuneml/issues>`__
+
+
+Examples
+--------
+
+See the ``examples/`` `directory <https://github.com/htuneml/htuneml/tree/master/examples>`__ in the repository root for usage examples:
+
+-  `Tensorflow Mnist <https://github.com/htuneml/htuneml/blob/master/examples/tensorflow-mnist.py>`__
+-  `Keras Mnist <https://github.com/htuneml/htuneml/blob/master/examples/tensorflow-mnist.py>`__
+-  `Pytorch Mnist <https://github.com/htuneml/htuneml/blob/master/examples/tensorflow-mnist.py>`__
+
+
+Requirements
+------------
+
+To use all of the functionality of the library, you should have:
+
+* **Python** 2.6, 2.7, or 3.3+ (required)
+* **PyAudio** 0.2.11+ (required only if you need to use microphone input, ``Microphone``)
+
+
+Quick start
+------------
+
+Register on website http://registru.ml, copy the api_key:
+
+.. code:: python
+
+    import htuneml as ht
+    
+    job = Job('api_key')
+    
+    @job.monitor
+    def train(par1=2,par2=2):
+        for i in range(par1):
+            #do training here
+            job.log({'loss':i*4,'ep':i})
+
+    job.setName('l2')
+    #job.debug()# uncomment and no experiment will be created and no logs sent
+    train(10, 2)
+
+This will print out something like the following:
+
+::
+
+    make experiment
+    got key experimnet 5c5c8eaacbcfb9146641367a
+
+Also it is possible to sent the parameters from the web app. First on gpu/cpu set the lisener:
+
+.. code:: python
+
+    import htuneml as ht
+    
+    job = Job('api_key')
+    
+    def train(par1=2,par2=2):
+        for i in range(par1):
+            #do training here
+            job.log({'loss':i*4,'ep':i})
+
+    job.sentParams(train)#sent the parameters list to the app
+    job.waitTask(train)#wait for parameters from app
+    
---
+++ packages/htuneml/htuneml-0.0.5.tar.gz/htuneml-0.0.5/htuneml/hmonitor.py
@@ -0,0 +1,201 @@
+from threading import Thread
+import requests, time,inspect, json
+import traceback, datetime
+class Job:
+    def __init__(self, key=None, verbose=1):
+        self.server = 'http://registru.ml/dlp'
+        if key=='api_key':
+            print('get the api key from '+self.server)
+            assert False
+        self.problems = {}
+        #if 1 print the messages
+        self.verbose = verbose
+        self.current = None
+        #list of parameters to set to the function
+        self.params = None
+        #stop the waiting for tasks and asking the server
+        self.stop = False
+        #key to the job id/problem id
+        self.key = key
+        self.running = False
+        #the function to call inside the train function
+        self.callb = None
+        #name of the experiment, if none no logs are sent
+        self.name = None
+        self.keyExperiment = None
+        self.stopEx = False
+        self.expars = None
+    def debug(self):
+        # do not sent any logs to server if debug
+        self.name = None
+    def setName(self, name, pars=None):
+        self.name=name        
+        self.expars = pars
+    def trymakenewD(self, values):        
+        #get the pars of the function and try to make new experiments
+        if self.running or self.name is None:
+            return
+        self.makeExperiment(self.name, values)
+    def setFunction(self, n, p):
+        # set the parameters for the function
+        self.problems[n] = p
+        if self.verbose == 1:
+            print('new problem added')
+    def setKey(self,k):
+        self.key = k
+        if self.verbose == 1:
+            print('new key added')
+    def waitTask(self, f):
+        self.setFunction('f',f)
+        self.stop = False
+        if self.key is None:
+            return 'no key'
+        process = Thread(target=self.callServer, args=[])
+        process.start()
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            self.stop=True
+            return 'ended'
+        
+    def sentEnd(self, status, mess):
+        self.name = None
+        if self.keyExperiment is None:
+            print('no exp key')
+            return
+        res = requests.post(self.server, data={"action":'endJob','key':self.keyExperiment, 'status':status,
+                                              'mess':mess})
+        if res.ok:
+            result = res.json()
+    def callServer(self):
+        while True:
+            if self.stop:
+                if self.verbose == 1:
+                    print('stoped')
+                break
+            if self.params is None:
+                time.sleep(5)
+                res = requests.post(self.server, data={"action":'ask','key':self.key})
+                if res.ok:
+                    result = res.json()
+                    #print(result)
+                    if self.verbose == 1:
+                        print('waiting')
+                    if result['new']:       
+                        self.stopEx = False
+                        if self.verbose == 1:
+                            print('new problem starts')
+                            print(result)
+                        self.params = result['params']
+                        self.current = result['current']
+                        self.keyExperiment = result['key']
+                    elif 'stop' in result and result['stop']:
+                        self.stop = True
+            else:
+                self.running = True
+                try:
+                    self.problems[self.current](**self.params)
+                    self.sentEnd('ended','')
+                except Exception as e:
+                    self.sentEnd('error',str(e))
+                    print(e)
+                    print(traceback.format_exc())
+                self.running = False
+                self.params = None
+
+    def log(self, p):
+        if self.keyExperiment is None:
+            if self.verbose == 1:
+                print('no experiment key')
+            return
+        if 'type' not in p:
+            p['type']=0
+        res = requests.post(self.server, json={"action":'addI','key':self.keyExperiment, 'info':p})
+        if res.ok:
+            result = res.json()
+            if result['stop']:
+                self.stopEx = True
+            if result['status'] != 'ok':
+                print('number of logs excided, please upgrade the account!');
+                self.stop=True
+                #print(result['mess'])
+                
+    def makeExperiment(self, name, params):
+        self.stopEx = False        
+        if self.key is None:
+            if self.verbose == 1:
+                print('no key')
+            return
+        data = {"action":'addEx','name':name,'nrExperiment':1,
+                                               'w':0,'status':'running','gran':json.dumps(None),
+                                               'mid':self.key, 'params':json.dumps(params)}
+        if self.expars is not None:
+            data.update(self.expars)
+            
+        res = requests.post(self.server, data=data)
+        self.expars = None
+        if self.verbose == 1:
+            print('make experiment')
+        if res.ok:
+            result = res.json()
+            if result['status'] == 'ok':
+                self.keyExperiment = result['id']
+                if self.verbose == 1:
+                    print('got key experimnet', result['id'])
+            else:
+                print('number of experiments excided, please upgrade!')
+                self.stop=True
+    def getParams(self):
+        return self.params#json.dump(self.params)
+                            
+    def setServer(self,server):
+        self.server= server
+    def getP(self, f):
+        s = inspect.getargspec(f)
+        p = {}
+        for i in s.args:
+            #print(i)
+            p[i] = None
+        n = len(s.args)-1 if len(s.defaults)!=len(s.args) else 0
+        for i in s.defaults:
+            #print(i)
+            p[s.args[n]] = i
+            n = n-1 if len(s.defaults)!=len(s.args) else n+1
+        return p
+    
+    def sentParams(self, f):
+        p = self.getP(f)
+        #print(p)
+        self.initparams = p
+        #return
+        res = requests.post(self.server,json={"action":'addP','key':self.key, 'params':p})
+        if res.ok:
+            print('sent',p.keys())
+    def setCallBack(self, f):
+        self.callb = f
+    def monitor(self, func):
+        def wrapper(*args, **kwargs):
+            values = self.getP(func)
+            s = inspect.getargspec(func)
+            #print(args,s.args,kwargs, s.defaults)
+            for i,k in enumerate(s.args): 
+                if i>=len(args):continue
+                values[k] = args[i]
+            for k,v in  kwargs.items():
+                values[k]=v
+            #print(values)
+            self.trymakenewD(values)
+            if self.callb is not None and 'callback' in kwargs:
+                kwargs['callback'] = kwargs
+            if 'job' in kwargs:
+                kwargs['job'] = self
+            try:
+                out = func(*args, **kwargs)
+                self.sentEnd('ended','')
+                return out
+            except Exception as e:
+                self.sentEnd('error',str(e))
+                print(e)
+                print(traceback.format_exc())
+        return wrapper
