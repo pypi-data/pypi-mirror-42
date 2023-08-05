811a133f66d2389bd15b70b84d9f279106191d31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:03 +0000

    Add package repltalk/0.4.13/repltalk-0.4.13.tar.gz

---
+++ packages/repltalk/repltalk-0.4.13.tar.gz/repltalk-0.4.13/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name='repltalk',
+	version='0.4.13',
+	author='mat',
+	author_email='fake@email.lol',
+	description='Uses the Repl Talk api to do stuff. info below',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	url='https://repl.it/@mat1',
+	packages=setuptools.find_packages(),
+	install_requires='aiohttp',
+	classifiers=[
+		'Programming Language :: Python :: 3',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+	],
+)
