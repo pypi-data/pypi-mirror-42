3c07f31804950c3cf67e0f4327aa15794d205536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:42 +0000

    Add package sphinxcontrib-serializinghtml/1.0.0/sphinxcontrib-serializinghtml-1.0.0.tar.gz

---
+++ packages/sphinxcontrib-serializinghtml/sphinxcontrib-serializinghtml-1.0.0.tar.gz/sphinxcontrib-serializinghtml-1.0.0/tests/test_serializinghtml.py
@@ -0,0 +1,13 @@
+"""
+    test_serializinghtml
+    ~~~~~~~~~~~~~~~~~~~~
+
+    Test for serializinghtml extension.
+
+    :copyright: Copyright 2007-2019 by the Sphinx team, see AUTHORS.
+    :license: BSD, see LICENSE for details.
+"""
+
+
+def test_basic():
+    pass  # nothing to do
