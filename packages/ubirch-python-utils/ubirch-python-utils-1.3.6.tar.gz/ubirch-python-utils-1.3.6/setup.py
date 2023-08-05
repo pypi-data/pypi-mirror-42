502d13987dfe5eeda7901c2f11cc093213554560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:17 +0000

    Add package ubirch-python-utils/1.3.6/ubirch-python-utils-1.3.6.tar.gz

---
+++ packages/ubirch-python-utils/ubirch-python-utils-1.3.6.tar.gz/ubirch-python-utils-1.3.6/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ubirch-python-utils",
+    version="1.3.6",
+    author="Victor Patrin",
+    author_email="victor.patrin150@gmail.com",
+    description="A python utils ubirch for ubirch anchoring services.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ubirch/ubirch-python-utils",
+    packages=setuptools.find_packages(exclude=['bin', 'tests']),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=[
+        'kafka-python >= 1.4.3',
+        'boto3 >= 1.7.80'
+
+    ],
+)
