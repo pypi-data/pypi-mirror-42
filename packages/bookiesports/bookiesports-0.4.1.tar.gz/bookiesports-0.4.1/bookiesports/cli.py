3fbefd2e7729a496be583d337114e92a0a4c2e8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:23 +0000

    Add package bookiesports/0.4.1/bookiesports-0.4.1.tar.gz

---
+++ packages/bookiesports/bookiesports-0.4.1.tar.gz/bookiesports-0.4.1/bookiesports/cli.py
@@ -0,0 +1,46 @@
+import click
+from bookied_sync.lookup import Lookup
+from peerplays.cli.decorators import onlineChain, unlockWallet
+from peerplays.cli.main import main
+from datetime import datetime, timedelta
+from .log import log
+
+
+@main.command()
+@click.option("--approver")
+@click.option("--proposer")
+@click.option("--network", default="beatrice")
+@click.pass_context
+@onlineChain
+@unlockWallet
+def sync(ctx, approver, proposer, network):
+    """ Sync the Entities in BookieSports with the blockchain
+    """
+    w = Lookup(peerplays_instance=ctx.peerplays, network=network)
+    if proposer:
+        w.set_proposing_account(proposer)
+    if approver:
+        w.set_approving_account(approver)
+
+    w.sync_bookiesports()
+
+    broadcast = w.broadcast()
+    log.info(dict(broadcast[0]))
+
+
+@main.command()
+@click.option("--network", default="beatrice")
+@click.pass_context
+@onlineChain
+def test(ctx, network):
+    """ Sync the Entities in BookieSports with the blockchain
+    """
+    w = Lookup(peerplays_instance=ctx.peerplays, network=network)
+    if w.is_bookiesports_in_sync:
+        click.echo("In sync!")
+    else:
+        click.echo("NOT in sync! Needs syncing!")
+
+
+if __name__ == "__main__":
+    main()
