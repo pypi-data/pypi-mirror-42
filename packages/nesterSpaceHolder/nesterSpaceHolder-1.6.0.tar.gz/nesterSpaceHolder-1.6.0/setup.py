40c1b9f97b1d09ccc381ad83316786ce72ac7293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:13 +0000

    Add package nesterSpaceHolder/1.6.0/nesterSpaceHolder-1.6.0.tar.gz

---
+++ packages/nesterSpaceHolder/nesterSpaceHolder-1.6.0.tar.gz/nesterSpaceHolder-1.6.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nesterSpaceHolder',
+    version='1.6.0',
+    py_modules=['nesterSpaceHolder'],
+    author='Stuart',
+    author_email='beefstew2002@outlook.com',
+    url='http://westada.tech/b4/stuarte/',
+    description='A simple printer of nested lists',
+    )
