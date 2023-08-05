68ccc267968fbdb606ea5b9e2d96c1de13d56820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:31 +0000

    Add package pysptk/0.1.16/pysptk-0.1.16.tar.gz

---
+++ packages/pysptk/pysptk-0.1.16.tar.gz/pysptk-0.1.16/lib/SPTK/build/src/SPTK-config.h
@@ -0,0 +1,12 @@
+/* WARNING! All changes made to this file will be lost! */
+
+#ifndef W_SRC_SPTK_CONFIG_H_WAF
+#define W_SRC_SPTK_CONFIG_H_WAF
+
+#define SPTK_VERSION "3.8.9"
+#define HAVE_FCNTL_H 1
+#define HAVE_LIMITS_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+
+#endif /* W_SRC_SPTK_CONFIG_H_WAF */
