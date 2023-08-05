fc3e6325349421e5c1a2a01ec3c14ceb7bd23984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:55 +0000

    Add package hand-detection-v6/0.4.1/hand_detection_v6-0.4.1.tar.gz

---
+++ packages/hand-detection-v6/hand_detection_v6-0.4.1.tar.gz/hand_detection_v6-0.4.1/setup.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+from setuptools import setup, find_packages
+
+setup(
+    name='hand_detection_v6',
+    version='0.4.1',
+    description=(
+        'This is an installation package that detects the hand of the video.'),
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author='llz',
+    author_email='hero1153@sina.com',
+    license='MIT License',
+
+    #packages = find_packages(),
+    packages = ['hand_detection_v6', 'hand_detection_v6/models', 'hand_detection_v6/data'],
+    package_data={  # Optional
+        'hand_detection_v6': ['models/frozen_inference_graph.pb',
+        'data/hand_label_map.pbtxt','data/101968001/101968001_roleid_1_1532946600_out-video-jzccda1caa6f5544519191477b40c3030f_f_1532946591547_t_1532948297067.flv_01703.png']},
+
+    platforms=["all"],
+    #url='<null>',
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+)
+
