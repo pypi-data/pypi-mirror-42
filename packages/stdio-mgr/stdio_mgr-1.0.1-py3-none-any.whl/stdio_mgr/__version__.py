0cc0f1ceec0f827f8a720363923aa4f7226de039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:39 +0000

    Add package stdio-mgr/1.0.1/stdio_mgr-1.0.1-py3-none-any.whl

---
+++ packages/stdio-mgr/stdio_mgr-1.0.1-py3-none-any.whl/stdio_mgr/__version__.py
@@ -0,0 +1,29 @@
+r"""``stdio_mgr`` *version number definition module*.
+
+``stdio_mgr`` provides a context manager for convenient
+mocking and/or wrapping of ``stdin``/``stdout``/``stderr``
+interactions.
+
+**Author**
+    Brian Skinn (bskinn@alum.mit.edu)
+
+**File Created**
+    7 Feb 2019
+
+**Copyright**
+    \(c) Brian Skinn 2018-2019
+
+**Source Repository**
+    http://www.github.com/bskinn/stdio-mgr
+
+**Documentation**
+    See README.rst at the GitHub repository
+
+**License**
+    The MIT License; see |license_txt|_ for full license terms
+
+**Members**
+
+"""
+
+__version__ = "1.0.1"
