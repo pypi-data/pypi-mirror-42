464db1df3c071908c4fc298f8e1c5e06f8e38401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:17 +0000

    Add package zebconf/0.1.7/zebconf-0.1.7.tar.gz

---
+++ packages/zebconf/zebconf-0.1.7.tar.gz/zebconf-0.1.7/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+"""Setup script"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="zebconf",
+    description="Zebra printer configuration",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="Michael Brown",
+    author_email="mbrown@fensystems.co.uk",
+    url="https://github.com/unipartdigital/zebconf",
+    license="GPLv2+",
+    version="0.1.7",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: "
+        "GNU General Public License v2 or later (GPLv2+)",
+        "Programming Language :: Python :: 3",
+        "Topic :: Printing",
+    ],
+    packages=find_packages(),
+    install_requires=[
+        'future',
+        'passlib',
+        'pyusb',
+    ],
+    entry_points={
+        'console_scripts': [
+            'zebconf=zebconf.cli:ZebraCommand.main',
+        ],
+    },
+)
