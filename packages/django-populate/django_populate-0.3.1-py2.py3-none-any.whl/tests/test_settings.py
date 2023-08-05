4ff2463346b917a8ec59dc74cad9f39c6471863a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:42 +0000

    Add package django-populate/0.3.1/django_populate-0.3.1-py2.py3-none-any.whl

---
+++ packages/django-populate/django_populate-0.3.1-py2.py3-none-any.whl/tests/test_settings.py
@@ -0,0 +1,32 @@
+import os
+
+from faker import Faker
+
+fake = Faker()
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        ,
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+INSTALLED_APPS = ("django_populate", "tests")
+SITE_ID = 1
+SECRET_KEY = fake.sha1()
