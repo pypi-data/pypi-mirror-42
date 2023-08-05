bed479b30742d82e960a9c132d16860bc8c2cae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:54 +0000

    Add package pyfastq-reader/1.2.0/pyfastq_reader-1.2.0.tar.gz

---
+++ packages/pyfastq-reader/pyfastq_reader-1.2.0.tar.gz/pyfastq_reader-1.2.0/pyfastq_reader/__init__.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# MIT License see LICENSE
+# -- Andy Hauser <Andreas.Hauser@LMU.de>
+
+from __future__ import print_function
+import sys
+
+def fasta_reader(filename):
+  return fasta_reader_fh(open(filename, 'r'))
+
+def fasta_reader_fh(infile):
+  name = infile.readline().rstrip()
+  while True:
+    seq = ""
+    for s in infile:
+      if len(s) > 0 and s[0] == '>':
+        yield name,seq
+        name = s.rstrip()
+        break
+      else:
+        seq += s.rstrip()
+    else:
+      yield name, seq
+      return
+
+
+def fastq_reader(filename):
+  return fastq_reader_fh(open(filename, 'r'))
+
+def fastq_reader_fh(infile):
+  name = infile.readline().rstrip()
+  while True:
+    seq = ""
+    for s in infile:
+      if s[0] == '+':
+        commentp = s.rstrip()
+        break
+      else:
+        seq += s.rstrip()
+    qual = ""
+    for q in infile:
+      if len(qual) > 0 and  q[0] == '@':
+        yield name, seq, qual
+        name = q.rstrip()
+        break
+      else:
+        qual += q.rstrip()
+    else:
+      yield name, seq, qual
+      return
+
+def main():
+  for filename in sys.argv[1:]:
+    count = 0
+    for head, seq, qual in fastq_reader(filename):
+      count += 1
+    print(count)
+
+if __name__ == '__main__':
+  main()
+
---
+++ packages/pyfastq-reader/pyfastq_reader-1.2.0.tar.gz/pyfastq_reader-1.2.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pyfastq_reader',
+    version='1.2.0',
+    author='Andy Hauser',
+    author_email='Andreas.Hauser@LMU.de',
+    license='LICENSE',
+    description='FASTQ file reader',
+    long_description='Pure python reader for the FASTQ format used in sequencing',
+    url='https://github.com/ahcm/pyfastq_reader',
+    packages=setuptools.find_packages(),
+    entry_points={ 'console_scripts': [ 'pyfastq_reader=pyfastq_reader:main', ], },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
