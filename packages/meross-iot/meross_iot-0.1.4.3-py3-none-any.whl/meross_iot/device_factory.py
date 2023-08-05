4165bfac347be54b5ae6a3ec7d188e8256714108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:58 +0000

    Add package meross-iot/0.1.4.3/meross_iot-0.1.4.3-py3-none-any.whl

---
+++ packages/meross-iot/meross_iot-0.1.4.3-py3-none-any.whl/meross_iot/device_factory.py
@@ -0,0 +1,23 @@
+from meross_iot.supported_devices.power_plugs import Mss310, Mss210, Mss110, Mss425e
+
+
+def build_wrapper(
+        token,
+        key,
+        user_id,
+        device_type,  # type: str
+        device_specs  # type: dict
+):
+    if device_type.lower() == "mss310":
+        return Mss310(token, key, user_id, **device_specs)
+    # Beta support for mss310h using the Mss310
+    elif device_type.lower() == "mss310h":
+        return Mss310(token, key, user_id, **device_specs)
+    elif device_type.lower() == "mss210":
+        return Mss210(token, key, user_id, **device_specs)
+    elif device_type.lower() == "mss110":
+        return Mss110(token, key, user_id, **device_specs)
+    elif device_type.lower() == "mss425e":
+        return Mss425e(token, key, user_id, **device_specs)
+    else:
+        return None
