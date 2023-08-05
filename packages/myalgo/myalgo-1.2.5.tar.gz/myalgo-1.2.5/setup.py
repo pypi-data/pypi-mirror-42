451c21a24566a3551a22d7f0f7e610fc955e8cb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:24 +0000

    Add package myalgo/1.2.5/myalgo-1.2.5.tar.gz

---
+++ packages/myalgo/myalgo-1.2.5.tar.gz/myalgo-1.2.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+
+
+setup(
+    name='myalgo',
+    version='1.2.5',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='nathaniel',
+    author_email='348831271@qq.com',
+    description='a simple trader for CTA trading.'
+)
