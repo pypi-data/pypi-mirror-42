d5abca4fd57db82f470351e1359ff90e6347560f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:05 +0000

    Add package simplegui2pygamemodule/1.0.3/simplegui2pygamemodule-1.0.3.tar.gz

---
+++ packages/simplegui2pygamemodule/simplegui2pygamemodule-1.0.3.tar.gz/simplegui2pygamemodule-1.0.3/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="simplegui2pygamemodule",
+    version="1.0.3",
+    author="jimmy kumar ahalpara",
+    author_email="jimmyahalpara123@gmail.com",
+    description="simplegui programs can be made to run in python",
+    long_description="make simplegui program to run in python",
+    long_description_content_type="text/markdown",
+    url="https://github.com/jimmyahalpara/Pytable/tree/simplegui2pygamemodule",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+    ),
+)
