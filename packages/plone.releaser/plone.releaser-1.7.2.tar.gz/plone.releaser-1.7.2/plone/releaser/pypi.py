a9d77b0b1c84ed64c51d424e6f80063e7a8ed546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:39 +0000

    Add package plone.releaser/1.7.2/plone.releaser-1.7.2.tar.gz

---
+++ packages/plone.releaser/plone.releaser-1.7.2.tar.gz/plone.releaser-1.7.2/plone/releaser/pypi.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+try:
+    from xmlrpclib import ServerProxy
+except ImportError:
+    from xmlrpc.client import ServerProxy
+
+
+def get_users_with_release_rights(package_name):
+    client = ServerProxy('https://pypi.org/pypi')
+    existing_admins = set([
+        user for role, user in client.package_roles(package_name)])
+    return existing_admins
+
+
+def can_user_release_package_to_pypi(user, package_name):
+    return user in get_users_with_release_rights(package_name)
