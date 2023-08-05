ba3d6b919f39fb54b48f0689f19f69760d397d52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:27 +0000

    Add package python-tldap/1.0.2/python-tldap-1.0.2.tar.gz

---
+++ packages/python-tldap/python-tldap-1.0.2.tar.gz/python-tldap-1.0.2/setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+# Copyright 2012-2014 Brian May
+#
+# This file is part of python-tldap.
+#
+# python-tldap is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# python-tldap is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with python-tldap  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+from setuptools import setup
+
+setup(
+    name="python-tldap",
+    version='1.0.2',
+    url='https://github.com/Karaage-Cluster/python-tldap',
+    author='Brian May',
+    author_email='brian@linuxpenguins.xyz',
+    description='High level python LDAP Library',
+    packages=[
+        'tldap', 'tldap.backend',
+        'tldap.django', 'tldap.django.migrations',
+        'tldap.database',
+        'tldap.test', 'tldap.test.ldap_schemas',
+    ],
+    license="GPL3+",
+    long_description=open('README.rst').read(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: "
+            "GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords="ldap",
+    package_data={
+        'tldap.test.ldap_schemas': ['*.schema', ],
+    },
+    install_requires=[
+        "ldap3",
+        "passlib",
+        "six",
+    ],
+    tests_require=[
+        "pytest",
+        "pytest-runner",
+    ],
+)
