e7650cb57440476e68d610217b0f03b4a8ca2466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:14 +0000

    Add package ringserverstats/0.1.0/ringserverstats-0.1.0.tar.gz

---
+++ packages/ringserverstats/ringserverstats-0.1.0.tar.gz/ringserverstats-0.1.0/setup.py
@@ -0,0 +1,53 @@
+from io import open
+
+from setuptools import find_packages, setup
+
+with open('ringserverstats/__init__.py', 'r') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            version = line.strip().split('=')[1].strip(' \'"')
+            break
+    else:
+        version = '0.0.1'
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name='ringserverstats',
+    version=version,
+    description='Export logs from ringserver as influxdb timeseries',
+    long_description=readme,
+    author='Jonathan Schaeffer',
+    author_email='jonathan.schaeffer@univ-grenoble-alpes.fr',
+    maintainer='Jonathan Schaeffer',
+    maintainer_email='jonathan.schaeffer@univ-grenoble-alpes.fr',
+    url='https://github.com/resif/ringserver_stats',
+    license='GPL-3.0',
+    install_requires=[
+        'Click==7.0',
+        'influxdb==5.2.1',
+        'maxminddb==1.4.1',
+        'maxminddb-geolite2==2018.703',
+        'regex==2019.2.7',
+        'geohash2'
+    ],
+    keywords=[
+        '',
+    ],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    tests_require=['coverage', 'pytest'],
+    entry_points='''
+    [console_scripts]
+    ringserverstats=ringserverstats:cli
+    '''
+)
