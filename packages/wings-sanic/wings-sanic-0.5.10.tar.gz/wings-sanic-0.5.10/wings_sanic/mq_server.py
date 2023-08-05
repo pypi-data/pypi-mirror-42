02e7a8207616aee4749c13403f0920522ac147f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:22 +0000

    Add package wings-sanic/0.5.10/wings-sanic-0.5.10.tar.gz

---
+++ packages/wings-sanic/wings-sanic-0.5.10.tar.gz/wings-sanic-0.5.10/wings_sanic/mq_server.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+
+class BaseMqServer:
+    async def start(self):
+        raise NotImplementedError
+
+    async def publish(self, routing_key: str, content: str):
+        raise NotImplementedError
+
+    async def subscribe(self, routing_key: str, handler, max_retry=-1, subscribe=False):
+        raise NotImplementedError
