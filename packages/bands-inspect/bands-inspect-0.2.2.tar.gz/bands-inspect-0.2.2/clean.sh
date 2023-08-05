e3c4613dd7e57b3de83e66ab0090e3249e0f0a0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:44 +0000

    Add package bands-inspect/0.2.2/bands-inspect-0.2.2.tar.gz

---
+++ packages/bands-inspect/bands-inspect-0.2.2.tar.gz/bands-inspect-0.2.2/clean.sh
@@ -0,0 +1,6 @@
+# (c) 2017-2019, ETH Zurich, Institut fuer Theoretische Physik
+# Author: Dominik Gresch <greschd@gmx.ch>
+
+find -name "*.pyc" -delete;
+find -name "*.pyo" -delete;
+find -name "__pycache__" -delete;
