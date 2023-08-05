919cfa8cddbaac80903dae89b90c90800d7b9d4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:39 +0000

    Add package freebsd-sysctl/0.0.4/freebsd_sysctl-0.0.4.tar.gz

---
+++ packages/freebsd-sysctl/freebsd_sysctl-0.0.4.tar.gz/freebsd_sysctl-0.0.4/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import os.path
+import sys
+from setuptools import find_packages, setup
+
+cwd = os.getcwd()
+
+about = {}
+with open(os.path.join(cwd, "freebsd_sysctl", '__version__.py')) as f:
+    VERSION = exec(f.read(), about)
+
+with open(os.path.join(cwd, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+	name="freebsd_sysctl",
+	version=about['__version__'],
+	description="A native Python module for FreeBSD sysctl.",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/gronke/py-freebsd_sysctl",
+	author="Stefan Grönke",
+	author_email="stefan@gronke.net",
+	python_requires=">=3.6",
+	setup_requires=["pytest-runner"],
+	tests_require=["pytest"],
+	packages=find_packages(exclude=('tests',))
+)
