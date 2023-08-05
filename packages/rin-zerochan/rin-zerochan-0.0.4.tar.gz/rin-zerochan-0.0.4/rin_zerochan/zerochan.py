6562b3e66d46e382ebcf9d5ad7b5bf1b4be2e1b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:07 +0000

    Add package rin-zerochan/0.0.4/rin-zerochan-0.0.4.tar.gz

---
+++ packages/rin-zerochan/rin-zerochan-0.0.4.tar.gz/rin-zerochan-0.0.4/rin_zerochan/zerochan.py
@@ -0,0 +1,29 @@
+import random
+
+from . import scrap
+
+
+async def search(query: str, page=random.randint(0, 100)):
+    results = scrap.search(query, p=page)
+    images = []
+    for i in range(scrap.IMAGES_PER_TAG):
+        images.append(await results.__anext__())
+    return images
+
+
+async def image(image_id: str):
+    images: dict = await scrap.image(image_id)
+    return images
+
+
+async def info(query: str):
+    information = await scrap.info(query)
+    return information
+
+
+async def meta(query: str, page=random.randint(0, 100)):
+    meta_tags = scrap.meta(query, p=page)
+    results = []
+    for i in range(scrap.TAGS_PER_META):
+        results.append(await meta_tags.__anext__())
+    return results
