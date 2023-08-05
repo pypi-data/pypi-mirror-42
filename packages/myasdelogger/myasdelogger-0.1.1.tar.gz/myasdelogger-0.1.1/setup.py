62808c0d91da8e5273c9919b40b7b2463665f7b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:27 +0000

    Add package myasdelogger/0.1.1/myasdelogger-0.1.1.tar.gz

---
+++ packages/myasdelogger/myasdelogger-0.1.1.tar.gz/myasdelogger-0.1.1/setup.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name="myasdelogger",
+    version="0.1.1",
+    keywords=("log", "logger"),
+    description="easylogger for easyspider",
+    license="MIT License",
+    install_requires=[],
+    author="zhang1Tian",
+    author_email="hhczy1003@gmail.com",
+    # packages = find_packages(),
+    packages=["."],
+    platforms="any"
+)
