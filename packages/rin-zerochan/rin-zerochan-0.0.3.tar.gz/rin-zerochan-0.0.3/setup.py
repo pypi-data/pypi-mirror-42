9cb9342a24b7ed0b50474880827bc2b9c9367c95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:03 +0000

    Add package rin-zerochan/0.0.3/rin-zerochan-0.0.3.tar.gz

---
+++ packages/rin-zerochan/rin-zerochan-0.0.3.tar.gz/rin-zerochan-0.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='rin-zerochan',
+    version='0.0.3',
+    author='reformed#5680',
+    description='A simple and unofficial Python ZeroChan library.',
+    license='MIT',
+    url='https://github.com/reformed5680/rin_zerochan',
+    packages=find_packages(),
+)
