2d09f65db393ad9f42323c718c73f6e42d2a85a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:30 +0000

    Add package heatrapy/1.0.1/heatrapy-1.0.1.tar.gz

---
+++ packages/heatrapy/heatrapy-1.0.1.tar.gz/heatrapy-1.0.1/heatrapy/mats/__init__.py
@@ -0,0 +1,9 @@
+"""Mats.
+
+This submodule access the properties of materials
+
+"""
+
+from .calmatpro import calmatpro
+
+__all__ = [calmatpro]
---
+++ packages/heatrapy/heatrapy-1.0.1.tar.gz/heatrapy-1.0.1/heatrapy/solvers/explicit_general.py
@@ -0,0 +1,50 @@
+"""Contains the explicit_general solver.
+
+Used to compute thermal processes
+
+"""
+
+import numpy as np
+import copy
+
+
+def explicit_general(obj):
+    """explicit_general solver.
+
+    Used to compute one time step of systems with fixed thermal contuctivity.
+
+    """
+
+    x = copy.copy(obj.temperature)
+
+    # computes
+    for i in range(1, obj.num_points - 1):
+
+        alpha = obj.dt * \
+            obj.k[i] / (obj.rho[i] * obj.Cp[i] *
+                        obj.dx * obj.dx)
+        beta = obj.dt / (obj.rho[i] * obj.Cp[i])
+
+        Tnew = ((1 + beta * obj.Q[i]) * obj.temperature[i][0] +
+                alpha * (obj.temperature[i - 1][0] - 2 *
+                         obj.temperature[i][0] + obj.temperature[i + 1][0]) +
+                beta * (obj.Q0[i] - obj.Q[i] * obj.amb_temperature))
+        x[i][1] = Tnew
+
+    # left boundary for next time step
+    if obj.boundaries[0] == 0:
+        x[0][1] = obj.temperature[1][1]
+    else:
+        x[0][1] = obj.boundaries[0]
+
+    # right boundary for next time step
+    if obj.boundaries[1] == 0:
+        x[obj.num_points - 1][1] = obj.temperature[obj.num_points - 2][1]
+    else:
+        x[obj.num_points - 1][1] = obj.boundaries[1]
+
+    # updates temperature for next iteration
+    for i in range(0, obj.num_points):
+        x[i][0] = x[i][1]
+
+    return x
