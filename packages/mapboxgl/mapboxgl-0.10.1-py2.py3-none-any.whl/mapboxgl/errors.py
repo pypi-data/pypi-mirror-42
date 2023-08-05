ff1fcd912883be5fc7ded0797e3b5cdd18f192e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:00 +0000

    Add package mapboxgl/0.10.1/mapboxgl-0.10.1-py2.py3-none-any.whl

---
+++ packages/mapboxgl/mapboxgl-0.10.1-py2.py3-none-any.whl/mapboxgl/errors.py
@@ -0,0 +1,14 @@
+class TokenError(ValueError):
+    pass
+
+
+class ValueError(ValueError):
+    pass
+
+
+class SourceDataError(ValueError):
+	pass
+
+
+class DateConversionError(ValueError):
+	pass
