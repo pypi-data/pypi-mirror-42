d83882f5a4907fc675885517024ecb6b2366af0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:32 +0000

    Add package romanify/0.0.2/romanify-0.0.2.tar.gz

---
+++ packages/romanify/romanify-0.0.2.tar.gz/romanify-0.0.2/romanify/__init__.py
@@ -0,0 +1,13 @@
+""" Roman numbers converting library """
+from .main import arabic2roman
+
+__author__ = 'Evgenia Turueva'
+__email__ = 'tindomirel1994@gmail.com'
+__license__ = 'MIT'
+__version__ = '0.0.2'
+
+
+__all__ = (
+    'arabic2roman',
+    '__author__', '__email__', '__license__', '__version__'
+)
