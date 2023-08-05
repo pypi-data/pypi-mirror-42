1de14922d53ad0dea8eecbdc20ab79ca0558a08b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:57 +0000

    Add package objectgeekyb/0.2/objectgeekyb-0.2-py3-none-any.whl

---
+++ packages/objectgeekyb/objectgeekyb-0.2-py3-none-any.whl/objectgeekyb-0.2.data/scripts/objectcoco.py
@@ -0,0 +1,217 @@
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+import glob
+import cv2
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
+import scipy.misc
+import time
+
+import re
+numbers = re.compile(r'(\d+)')
+def numericalSort(value):
+    parts = numbers.split(value)
+    parts[1::2] = map(int, parts[1::2])
+    return parts
+
+
+# This is needed since the notebook is stored in the object_detection folder.
+sys.path.append("..")
+from object_detection.utils import ops as utils_ops
+
+from utils import label_map_util
+
+from utils import visualization_utils as vis_util
+
+PATH_TO_CKPT = "C:/Users/TS-3/Desktop/objectgeekyb/frozen_inference_graph.pb"
+PATH_TO_LABELS = "C:/Users/TS-3/Desktop/objectgeekyb/graph.pbtxt"
+PATH_TO_TEST_IMAGES_DIR = "C:/Users/TS-3/Desktop/objectgeekyb/inp"
+PATH_TO_OUTPUT_IMAGES_DIR = "C:/Users/TS-3/Desktop/objectgeekyb/outp"
+VIDEO_NAME = "C:/Users/TS-3/Desktop/objectgeekyb/inp/test2.mp4"
+
+def path_to_model(str):
+	global PATH_TO_CKPT
+	PATH_TO_CKPT = str
+	return PATH_TO_CKPT
+
+def path_to_video(str):
+	global VIDEO_NAME
+	VIDEO_NAME = str
+	return VIDEO_NAME
+
+def path_to_label(str):
+	global PATH_TO_LABELS
+	PATH_TO_LABELS = str
+	return PATH_TO_LABELS
+
+def load_image_into_numpy_array(image):
+    (im_width, im_height) = image.size
+    return np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)
+
+def path_to_image_directory(str):
+	global PATH_TO_TEST_IMAGES_DIR
+	PATH_TO_TEST_IMAGES_DIR = str
+	return PATH_TO_TEST_IMAGES_DIR
+
+def path_to_output_directory(str):
+	global PATH_TO_OUTPUT_IMAGES_DIR
+	PATH_TO_OUTPUT_IMAGES_DIR = str
+	return PATH_TO_OUTPUT_IMAGES_DIR
+
+def run_inference_for_single_image(image, graph):
+    with graph.as_default():
+        with tf.Session() as sess:
+            # Get handles to input and output tensors
+            ops = tf.get_default_graph().get_operations()
+            all_tensor_names = {output.name for op in ops for output in op.outputs}
+            tensor_dict = {}
+            for key in [
+                'num_detections', 'detection_boxes', 'detection_scores',
+                'detection_classes', 'detection_masks'
+            ]:
+                tensor_name = key + ':0'
+                if tensor_name in all_tensor_names:
+                    tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
+                        tensor_name)
+            if 'detection_masks' in tensor_dict:
+                # The following processing is only for single image
+                detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
+                detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
+                # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
+                real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
+                detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
+                detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
+                detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+                    detection_masks, detection_boxes, image.shape[0], image.shape[1])
+                detection_masks_reframed = tf.cast(tf.greater(detection_masks_reframed, 0.5), tf.uint8)
+                # Follow the convention by adding back the batch dimension
+                tensor_dict['detection_masks'] = tf.expand_dims(detection_masks_reframed, 0)
+            image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+            # Run inference
+            output_dict = sess.run(tensor_dict,feed_dict={image_tensor: np.expand_dims(image, 0)})
+            # all outputs are float32 numpy arrays, so convert types as appropriate
+            output_dict['num_detections'] = int(output_dict['num_detections'][0])
+            output_dict['detection_classes'] = output_dict['detection_classes'][0].astype(np.uint8)
+            output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+            output_dict['detection_scores'] = output_dict['detection_scores'][0]
+            if 'detection_masks' in output_dict:
+                output_dict['detection_masks'] = output_dict['detection_masks'][0]
+    return output_dict
+
+def run_detection_on_video():
+	NUM_CLASSES = 91
+	detection_graph = tf.Graph()
+	with detection_graph.as_default():
+		od_graph_def = tf.GraphDef()
+		with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+			serialized_graph = fid.read()
+			od_graph_def.ParseFromString(serialized_graph)
+			tf.import_graph_def(od_graph_def, name='')
+
+	label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+	categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+	category_index = label_map_util.create_category_index(categories)
+	print(VIDEO_NAME)
+
+	cap = cv2.VideoCapture(VIDEO_NAME)
+
+#Define the codec and create VideoWriter object
+	width = cap.get(3)   # float
+	height = cap.get(4)
+	fourcc = cv2.VideoWriter_fourcc(*'XVID')
+	out = cv2.VideoWriter(PATH_TO_OUTPUT_IMAGES_DIR+'/output.avi',fourcc, 20.0, (int(width),int(height)))
+
+	while(cap.isOpened()):
+		ret, frame = cap.read()
+		
+		if ret==True:
+			image_np = frame		# Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+			#image_np_expanded = np.expand_dims(image_np, axis=0)
+		#Actual detection.
+			ts = time.clock()
+			output_dict = run_inference_for_single_image(image_np, detection_graph)
+			ts2 = time.clock()
+			print(ts2-ts)
+		#Visualization of the results of a detection.
+			vis_util.visualize_boxes_and_labels_on_image_array(
+				image_np,
+				output_dict['detection_boxes'],
+				output_dict['detection_classes'],
+				output_dict['detection_scores'],
+				category_index,
+				instance_masks=output_dict.get('detection_masks'),
+				use_normalized_coordinates=True,
+				line_thickness=1)
+
+			#write the flipped frame			
+			out.write(image_np)
+
+			cv2.imshow('frame',frame)
+			
+			
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				break
+		else:
+			break
+
+#Release everything if job is finished
+	cap.release()
+	out.release()
+	cv2.destroyAllWindows()
+
+def run_detection_on_image():
+	NUM_CLASSES = 91
+	detection_graph = tf.Graph()
+	with detection_graph.as_default():
+		od_graph_def = tf.GraphDef()
+		with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+			serialized_graph = fid.read()
+			od_graph_def.ParseFromString(serialized_graph)
+			tf.import_graph_def(od_graph_def, name='')
+
+	label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+	categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+	category_index = label_map_util.create_category_index(categories)
+
+	TEST_IMAGE_PATHS = glob.glob(PATH_TO_TEST_IMAGES_DIR+"/*.jpg")
+	TEST_IMAGE_PATHS=sorted(TEST_IMAGE_PATHS,key=numericalSort)
+
+	print(TEST_IMAGE_PATHS)
+
+# Size, in inches, of the output images.
+	IMAGE_SIZE = (12, 8)
+
+	for image_path in TEST_IMAGE_PATHS:
+		image = Image.open(image_path)
+		print(image_path.split('\\')[-1])
+		# the array based representation of the image will be used later in order to prepare the
+		# result image with boxes and labels on it.
+		image_np = load_image_into_numpy_array(image)
+		# Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+		image_np_expanded = np.expand_dims(image_np, axis=0)
+		# Actual detection.
+		ts = time.clock()
+		output_dict = run_inference_for_single_image(image_np, detection_graph)
+		ts2 = time.clock()
+		print(ts2-ts)
+		# Visualization of the results of a detection.
+		vis_util.visualize_boxes_and_labels_on_image_array(
+			image_np,
+			output_dict['detection_boxes'],
+			output_dict['detection_classes'],
+			output_dict['detection_scores'],
+			category_index,
+			instance_masks=output_dict.get('detection_masks'),
+			use_normalized_coordinates=True,
+			line_thickness=1)
+
+		scipy.misc.imsave(PATH_TO_OUTPUT_IMAGES_DIR+"/{}".format(image_path.split('\\')[-1]), image_np)
+
+
