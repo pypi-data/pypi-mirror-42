f7898bac59953941796546fde4cadeed5115deda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:45 +0000

    Add package fastprint/1.1/fastprint-1.1.tar.gz

---
+++ packages/fastprint/fastprint-1.1.tar.gz/fastprint-1.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fastprint",
+    version="1.1",
+    author="ThatOneCalculator (Kainoa Kanter)",
+    author_email="kainoakanter@gmail.com",
+    description="Make easy, long docstring prints with breaks between each line!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ThatOneCalculator/Python-Easy-Print",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
