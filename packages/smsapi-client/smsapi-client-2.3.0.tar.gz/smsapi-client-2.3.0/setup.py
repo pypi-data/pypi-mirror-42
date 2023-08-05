d467778f0340c60c9238df0ad30d6e72c69d3c93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:01 +0000

    Add package smsapi-client/2.3.0/smsapi-client-2.3.0.tar.gz

---
+++ packages/smsapi-client/smsapi-client-2.3.0.tar.gz/smsapi-client-2.3.0/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from smsapi import version, name
+
+from setuptools import setup, find_packages
+
+setup(
+    name=name,
+    version=version,
+    description='SmsAPI client',
+    long_description=open('README.md').read(),
+    author='SMSAPI',
+    author_email='bok@smsapi.pl',
+    url='https://github.com/smsapi/smsapi-python-client',
+    packages=find_packages(exclude=["tests.*",  "tests"]),
+    license=open('LICENSE').read(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+    ],
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: Apache 2.0',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ),
+    test_suite='tests.suite'
+)
