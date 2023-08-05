1ba4aa6017daf8e331414c848e11bd7abab205e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:01 +0000

    Add package pcuf/0.1.3/pcuf-0.1.3.tar.gz

---
+++ packages/pcuf/pcuf-0.1.3.tar.gz/pcuf-0.1.3/pcuf/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from . import file, functions, time, warnings
+
+__author__ = """Alberto J. Marin"""
+__email__ = "alberto@ajmar.in"
+__version__ = "0.1.3"
+
+__all__ = ["file", "functions", "time", "warnings"]
