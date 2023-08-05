95f40624bafad287d128dfd63691ba88ad419ca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:28 +0000

    Add package tfp-nightly/0.7.0.dev20190219/tfp_nightly-0.7.0.dev20190219-py2.py3-none-any.whl

---
+++ packages/tfp-nightly/tfp_nightly-0.7.0.dev20190219-py2.py3-none-any.whl/tensorflow_probability/python/math/__init__.py
@@ -0,0 +1,61 @@
+# Copyright 2018 The TensorFlow Probability Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""TensorFlow Probability math functions."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from tensorflow_probability.python.math.custom_gradient import custom_gradient
+from tensorflow_probability.python.math.diag_jacobian import diag_jacobian
+from tensorflow_probability.python.math.gradient import value_and_gradient
+from tensorflow_probability.python.math.interpolation import batch_interp_regular_1d_grid
+from tensorflow_probability.python.math.interpolation import interp_regular_1d_grid
+from tensorflow_probability.python.math.linalg import lu_matrix_inverse
+from tensorflow_probability.python.math.linalg import lu_reconstruct
+from tensorflow_probability.python.math.linalg import lu_solve
+from tensorflow_probability.python.math.linalg import pinv
+from tensorflow_probability.python.math.linalg import sparse_or_dense_matmul
+from tensorflow_probability.python.math.linalg import sparse_or_dense_matvecmul
+from tensorflow_probability.python.math.numeric import clip_by_value_preserve_gradient
+from tensorflow_probability.python.math.numeric import log1psquare
+from tensorflow_probability.python.math.numeric import soft_threshold
+from tensorflow_probability.python.math.random_ops import random_rademacher
+from tensorflow_probability.python.math.random_ops import random_rayleigh
+from tensorflow_probability.python.math.root_search import secant_root
+
+from tensorflow.python.util.all_util import remove_undocumented
+
+_allowed_symbols = [
+    'batch_interp_regular_1d_grid',
+    'clip_by_value_preserve_gradient',
+    'custom_gradient',
+    'diag_jacobian',
+    'interp_regular_1d_grid',
+    'log1psquare',
+    'soft_threshold',
+    'lu_matrix_inverse',
+    'lu_reconstruct',
+    'lu_solve',
+    'pinv',
+    'sparse_or_dense_matvecmul',
+    'sparse_or_dense_matmul',
+    'random_rademacher',
+    'random_rayleigh',
+    'secant_root',
+    'value_and_gradient',
+]
+
+remove_undocumented(__name__, _allowed_symbols)
