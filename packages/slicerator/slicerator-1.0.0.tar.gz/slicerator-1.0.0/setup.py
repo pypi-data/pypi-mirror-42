3941cd382fd4e357bd5b5c7b6a0ac0e11a8063df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:39 +0000

    Add package slicerator/1.0.0/slicerator-1.0.0.tar.gz

---
+++ packages/slicerator/slicerator-1.0.0.tar.gz/slicerator-1.0.0/setup.py
@@ -0,0 +1,37 @@
+import os
+import versioneer
+from setuptools import setup
+
+try:
+    descr = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+except IOError:
+    descr = ''
+
+try:
+    from pypandoc import convert
+    descr = convert(descr, 'rst', format='md')
+except ImportError:
+    pass
+
+setup(name='slicerator',
+      version=versioneer.get_version(),
+      author='Daniel B. Allan',
+      author_email='daniel.b.allan@gmail.com',
+      packages=['slicerator'],
+      description='A lazy-loading, fancy-sliceable iterable.',
+      url='http://github.com/soft-matter/slicerator',
+      install_requires=['six'],
+      cmdclass=versioneer.get_cmdclass(),
+      platforms='Cross platform (Linux, Mac OSX, Windows)',
+      license="BSD",
+      classifiers=['Development Status :: 4 - Beta',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   ],
+      long_description=descr,
+      )
