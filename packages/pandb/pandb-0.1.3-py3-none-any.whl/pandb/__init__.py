55579dcd78bb67cda895b61da01aeb99f3e85275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:16 +0000

    Add package pandb/0.1.3/pandb-0.1.3-py3-none-any.whl

---
+++ packages/pandb/pandb-0.1.3-py3-none-any.whl/pandb/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019 Patrick Michl
+#
+# This file is part of pandora, https://frootlab.github.io/pandora
+#
+#  pandora is free software: you can redistribute it and/or modify it under the
+#  terms of the GNU General Public License as published by the Free Software
+#  Foundation, either version 3 of the License, or (at your option) any later
+#  version.
+#
+#  pandora is distributed in the hope that it will be useful, but WITHOUT ANY
+#  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+#  A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License along with
+#  pandora. If not, see <http://www.gnu.org/licenses/>.
+#
+"""Pandora database proxy."""
+
+__version__ = '0.1.3'
+__license__ = 'GPLv3'
+__copyright__ = 'Copyright (c) 2019 Patrick Michl'
+__organization__ = 'frootlab'
+__author__ = 'frootlab'
+__email__ = 'frootlab@gmail.com'
+__maintainer__ = 'Patrick Michl'
+__authors__ = ['Patrick Michl <patrick.michl@gmail.com>']
+__docformat__ = 'google'
---
+++ packages/pandb/pandb-0.1.3-py3-none-any.whl/pandb/proxy/sql.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019 Patrick Michl
+#
+# This file is part of pandora, https://frootlab.github.io/pandora
+#
+#  pandora is free software: you can redistribute it and/or modify it under the
+#  terms of the GNU General Public License as published by the Free Software
+#  Foundation, either version 3 of the License, or (at your option) any later
+#  version.
+#
+#  pandora is distributed in the hope that it will be useful, but WITHOUT ANY
+#  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+#  A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License along with
+#  pandora. If not, see <http://www.gnu.org/licenses/>.
+#
+"""Table Proxy Classes."""
+
+__author__ = 'Patrick Michl'
+__email__ = 'frootlab@gmail.com'
+__license__ = 'GPLv3'
+__docformat__ = 'google'
+
+from pandb.core import table
+
+#
+# Classes
+#
+
+class Table(table.Proxy):
+    """SQL-Table Proxy."""
