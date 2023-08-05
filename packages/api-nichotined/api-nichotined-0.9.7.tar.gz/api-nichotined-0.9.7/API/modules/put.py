15c965aecb57a851dacb7c12c3451bfd1f07f232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:44 +0000

    Add package api-nichotined/0.9.7/api-nichotined-0.9.7.tar.gz

---
+++ packages/api-nichotined/api-nichotined-0.9.7.tar.gz/api-nichotined-0.9.7/API/modules/put.py
@@ -0,0 +1,24 @@
+import json
+
+import requests
+
+from API.modules.json.pyJson import PyJSON
+from .baseApi import BaseApi
+
+
+class Put(BaseApi):
+    def __init__(self):
+        super().__init__()
+
+    def execute(self):
+        self._method_name = "PUT"
+        self.logger_request()
+        self._response = requests.put(url=self.url,
+                                      params=self.params, data=self.data, headers=self.headers, cookies=self.cookies,
+                                      files=self.files, auth=self.auth, timeout=self.timeout,
+                                      allow_redirects=self.allow_redirects, proxies=self.proxies,
+                                      hooks=self.hooks, stream=self.stream, verify=self.verify, cert=self.cert,
+                                      json=self.json)
+        self.logger_response()
+        if self._response:
+            self._json_object = "#NoBody on PUT response"
