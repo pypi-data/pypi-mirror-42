8df2ccb6924d4285f71a548557aeb9a11f4547f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:56 +0000

    Add package dx-distributions/0.1/dx_distributions-0.1.tar.gz

---
+++ packages/dx-distributions/dx_distributions-0.1.tar.gz/dx_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dx_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dx_distributions'],
+      zip_safe=False)
