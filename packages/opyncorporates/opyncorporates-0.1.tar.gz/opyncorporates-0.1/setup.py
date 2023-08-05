fe2f94285f4558e76a64f13a58c4496f7c970a35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:55 +0000

    Add package opyncorporates/0.1.0/opyncorporates-0.1.tar.gz

---
+++ packages/opyncorporates/opyncorporates-0.1.tar.gz/opyncorporates-0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='opyncorporates',
+    version='0.1',
+    description='a Python package for calling the OpenCorporates API',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://opyncorporates.readthedocs.io/en/latest/',
+    author='Patrick J. Ryan',
+    author_email='pjryan126@gmail.com',
+    license='MIT',
+    install_requires=[
+        'requests'
+    ],
+    packages=[
+        'opyncorporates',
+    ],
+    test_suite='tests',
+    zip_safe=False,
+)
