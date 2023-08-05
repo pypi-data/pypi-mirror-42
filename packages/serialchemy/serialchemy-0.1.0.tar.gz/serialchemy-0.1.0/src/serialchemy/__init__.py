a7537424d4819478773d03757def19ce4e437820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:58 +0000

    Add package serialchemy/0.1.0/serialchemy-0.1.0.tar.gz

---
+++ packages/serialchemy/serialchemy-0.1.0.tar.gz/serialchemy-0.1.0/src/serialchemy/__init__.py
@@ -0,0 +1,5 @@
+from .field import Field
+from .nested_fields import NestedModelField, NestedAttributesField, PrimaryKeyField, \
+    NestedModelListField
+from .modelserializer import ModelSerializer
+from .serializer import ColumnSerializer, Serializer
