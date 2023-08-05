d1c6770bb7046666f156bc2327beb542d8fb3ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:34 +0000

    Add package pymbtiles/0.3.1/pymbtiles-0.3.1.tar.gz

---
+++ packages/pymbtiles/pymbtiles-0.3.1.tar.gz/pymbtiles-0.3.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name="pymbtiles",
+    version="0.3.1",
+    packages=["pymbtiles"],
+    url="https://github.com/consbio/pymbtiles",
+    license="ISC",
+    author="Brendan Ward",
+    author_email="bcward@consbio.org",
+    description="MapBox Mbtiles Utilities",
+    long_description_content_type="text/markdown",
+    long_description=open("README.md").read(),
+    install_requires=[],
+    include_package_data=True,
+)
