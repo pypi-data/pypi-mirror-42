16030c9d612834a4b3afd611a0930593b8e2e76f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:21 +0000

    Add package nesterZT/1.1.0/nesterZT-1.1.0.tar.gz

---
+++ packages/nesterZT/nesterZT-1.1.0.tar.gz/nesterZT-1.1.0/nesterZT.py
@@ -0,0 +1,14 @@
+"""  This is the “nesterZT.py" module, and it provides one function called
+print_lol() which prints lists that may or may not include nested lists."""
+
+def print_lol(the_list,level):
+    """ This function takes a positional argument called “the_list", which is any
+    Python list (of, possibly, nested lists). Each data item in the provided list
+    is (recursively) printed to the screen on its own line. """
+    for x in the_list:
+        if isinstance(x, list):
+            print_lol(x, level+1)
+        else:
+            for z in range(level):
+                print("\t", end=' ')
+            print(x)
