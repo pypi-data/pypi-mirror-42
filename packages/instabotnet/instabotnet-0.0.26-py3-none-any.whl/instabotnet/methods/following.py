77377d3d52edd8898be849ad77d1c1aabbc09c11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:51 +0000

    Add package instabotnet/0.0.26/instabotnet-0.0.26-py3-none-any.whl

---
+++ packages/instabotnet/instabotnet-0.0.26-py3-none-any.whl/instabotnet/methods/following.py
@@ -0,0 +1,23 @@
+from typing import List
+from funcy import rcompose, mapcat
+import time
+from ..bot import Bot
+from ..nodes import User, Media
+from .common import accepts, cycled_api_call
+
+
+@accepts(User)
+def following(bot, nodes,  args) -> List[User]:
+
+    pack_user = lambda item: User(id=item['pk'], username=item['username'], data=item)
+    amount = args.get('amount')
+
+    process = rcompose(
+        lambda user: user.id if user.id else user.get_id(bot),
+        lambda id: cycled_api_call(amount, bot, bot.api.get_user_followings, id, 'users'),
+        lambda gen: map(pack_user, gen)
+    )
+
+    result = mapcat(process, nodes)
+
+    return result, {}
