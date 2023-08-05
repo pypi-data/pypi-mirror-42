695740608d0a1fb760602220dde26b4171bfb732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:12 +0000

    Add package directory-constants/12.4.0/directory_constants-12.4.0-py3-none-any.whl

---
+++ packages/directory-constants/directory_constants-12.4.0-py3-none-any.whl/directory_constants/constants/cms.py
@@ -0,0 +1,97 @@
+# CMS services
+FIND_A_SUPPLIER = 'FIND_A_SUPPLIER'
+GREAT_DOMESTIC = EXPORT_READINESS = 'EXPORT_READINESS'
+INVEST = 'INVEST'
+COMPONENTS = 'COMPONENTS'
+GREAT_INTERNATIONAL = 'GREAT_INTERNATIONAL'
+
+
+# CMS page slugs
+COMPONENTS_BANNER_DOMESTIC_SLUG = 'eu-exit-banner-domestic'
+COMPONENTS_BANNER_INTERNATIONAL_SLUG = 'eu-exit-banner-international'
+# Export Readiness
+EXPORT_READINESS_HOME_SLUG = 'home'
+EXPORT_READINESS_HOME_INTERNATIONAL_SLUG = 'international'
+EXPORT_READINESS_EUEXIT_FORM_SUCCESS_SLUG = 'eu-exit-form-success'
+EXPORT_READINESS_EUEXIT_INTERNATIONAL_FORM_SLUG = 'eu-exit-international'
+EXPORT_READINESS_EUEXIT_DOMESTIC_FORM_SLUG = 'eu-exit-domestic'
+EXPORT_READINESS_TERMS_AND_CONDITIONS_SLUG = 'terms-and-conditions'
+EXPORT_READINESS_PRIVACY_AND_COOKIES_SLUG = 'privacy-and-cookies'
+EXPORT_READINESS_GET_FINANCE_SLUG = 'get-finance'
+EXPORT_READINESS_HELP_EXOPP_ALERTS_IRRELEVANT_SLUG = 'alerts-not-relevant'
+EXPORT_READINESS_HELP_MISSING_VERIFY_EMAIL_SLUG = 'no-verification-email'
+EXPORT_READINESS_HELP_PASSWORD_RESET_SLUG = 'password-reset'
+EXPORT_READINESS_HELP_COMPANIES_HOUSE_LOGIN_SLUG = 'companies-house-login'
+EXPORT_READINESS_HELP_VERIFICATION_CODE_ENTER_SLUG = 'verification-letter-code'
+EXPORT_READINESS_HELP_VERIFICATION_CODE_LETTER_SLUG = 'no-verification-letter'
+EXPORT_READINESS_HELP_EXOPP_NO_RESPONSE = 'opportunity-no-response'
+EXPORT_READINESS_EU_EXIT_DOMESTIC_NEWS_SLUG = 'eu-exit-news'
+EXPORT_READINESS_EU_EXIT_INTERNATIONAL_NEWS_SLUG = 'international-eu-exit-news'
+EXPORT_READINESS_PERFORMANCE_DASHBOARD_SLUG = 'performance-dashboard'
+EXPORT_READINESS_PERFORMANCE_DASHBOARD_EXOPPS_SLUG = (
+    'performance-dashboard-export-opportunities'
+)
+EXPORT_READINESS_PERFORMANCE_DASHBOARD_SOO_SLUG = (
+    'performance-dashboard-selling-online-overseas'
+)
+EXPORT_READINESS_PERFORMANCE_DASHBOARD_TRADE_PROFILE_SLUG = (
+    'performance-dashboard-trade-profiles'
+)
+EXPORT_READINESS_PERFORMANCE_DASHBOARD_INVEST_SLUG = (
+    'performance-dashboard-invest'
+)
+EXPORT_READINESS_PERFORMANCE_DASHBOARD_NOTES_SLUG = (
+    'performance-dashboard-notes'
+)
+EXPORT_READINESS_CONTACT_US_FORM_SUCCESS_SLUG = 'contact-success-form'
+EXPORT_READINESS_CONTACT_US_FORM_SUCCESS_EVENTS_SLUG = (
+    'contact-events-success-form'
+)
+EXPORT_READINESS_CONTACT_US_FORM_SUCCESS_DSO_SLUG = (
+    'contact-dso-success-form'
+)
+EXPORT_READINESS_CONTACT_US_FORM_SUCCESS_EXPORT_ADVICE_SLUG = (
+    'contact-export-advice-success-form'
+)
+EXPORT_READINESS_CONTACT_US_FORM_SUCCESS_FEEDBACK_SLUG = (
+    'contact-feedback-success-form'
+)
+EXPORT_READINESS_CONTACT_US_FORM_SUCCESS_FIND_COMPANIES_SLUG = (
+    'contact-find-companies-success-form'
+)
+EXPORT_READINESS_CONTACT_US_FORM_SUCCESS_INTERNATIONAL_SLUG = (
+    'contact-international-success-form'
+)
+EXPORT_READINESS_MARKETING_PAGES_SLUG = 'export-readiness-marketing-pages'
+EXPORT_READINESS_SITE_POLICY_PAGES_SLUG = 'export-readiness-site-policy-pages'
+EXPORT_READINESS_CONTACT_US_FORM_SUCCESS_SOO_SLUG = 'contact-soo-success-form'
+EXPORT_READINESS_ADVICE_SLUG = 'advice'
+EXPORT_READINESS_HELP_ACCOUNT_COMPANY_NOT_FOUND_SLUG = 'company-not-found'
+
+# Invest
+INVEST_SECTOR_LANDING_PAGE_SLUG = 'sector-landing-page'
+INVEST_GUIDE_LANDING_PAGE_SLUG = 'setup-guide-landing-page'
+INVEST_HIGH_POTENTIAL_OPPORTUNITY_FORM_SLUG = (
+    'high-potential-opportunity-form'
+)
+INVEST_HIGH_POTENTIAL_OPPORTUNITY_FORM_SUCCESS_SLUG = (
+    'high-potential-opportunity-submit-success'
+)
+INVEST_UK_REGION_LANDING_PAGE_SLUG = 'uk-region-landing-page'
+INVEST_HOME_PAGE_SLUG = 'home-page'
+
+# Find A Supplier
+FIND_A_SUPPLIER_INDUSTRY_LANDING_SLUG = 'industries-landing-page'
+FIND_A_SUPPLIER_LANDING_SLUG = 'landing-page'
+FIND_A_SUPPLIER_INDUSTRY_CONTACT_SLUG = 'industry-contact'
+
+# Great International
+GREAT_INTERNATIONAL_MARKETING_PAGES_SLUG = (
+    'great-international-marketing-pages'
+)
+GREAT_INTERNATIONAL_UK_HQ_PAGES_SLUG = 'great-international-uk-hq-pages'
+
+
+# CMS page types
+FIND_A_SUPPLIER_INDUSTRY_TYPE = 'find_a_supplier.IndustryPage'
+FIND_A_SUPPLIER_INDUSTRY_ARTICLE_TYPE = 'find_a_supplier.IndustryArticlePage'
