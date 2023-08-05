eac11c4ddd5fe67b670b86fb9ed8def121c39fb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:38 +0000

    Add package nethub-manager/0.0.3/nethub-manager-0.0.3.tar.gz

---
+++ packages/nethub-manager/nethub-manager-0.0.3.tar.gz/nethub-manager-0.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from setuptools import find_packages
+
+EXCLUDE_FROM_PACKAGES = [
+        'manager.templates',
+]
+
+setup(name='nethub-manager',
+      version='0.0.3',
+      author='Nethub',
+      author_email='nethub@yandex.ru',
+      scripts=['manager/bin/nethub-manager.py'],
+      entry_points={'console_scripts': [
+          'nhm = manager.core.managment: execute_from_command_line'
+      ]},
+      install_requires=['h5py'],
+      include_package_data=True,
+      packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES))
