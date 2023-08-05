c56348009957156e45eae55c0f7cfc81c7165841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:44 +0000

    Add package holtwinters/1.0/holtwinters-1.0.tar.gz

---
+++ packages/holtwinters/holtwinters-1.0.tar.gz/holtwinters-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='holtwinters',
+      version='1.0',
+      description='Holt winters for anomaly detection',
+      url='https://pip.pypa.io/',
+      author='arunsechergy',
+      author_email='arun.rvbe@gmail.com',
+      license='MIT',
+      packages=['holtwinters'],
+      zip_safe=False)
+
