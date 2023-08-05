04a1087c27e5421c6770ecd371734b09e918b4b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:20 +0000

    Add package jtyoui/19.2.19/jtyoui-19.2.19-py3-none-any.whl

---
+++ packages/jtyoui/jtyoui-19.2.19-py3-none-any.whl/jtyoui/data/constant.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time : 2019/2/19
+# @Email : jtyoui@qq.com
+
+# 常见的照片格式
+Photo_Format = (
+    'bmp', 'jpg', 'png', 'tif', 'gif', 'pcx', 'tga', 'exif', 'fpx', 'svg', 'psd', 'cdr', 'pcd', 'dxf', 'ufo', 'eps',
+    'ai', 'raw', 'WMF', 'webp')
+
+DECODE = ('Unicode', 'ASCII', 'GBK', 'GB2312', 'UTF-8', 'ISO-8859-1', 'UTF-16', 'GB18030')
---
+++ packages/jtyoui/jtyoui-19.2.19-py3-none-any.whl/jtyoui/imagepdf/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time : 2019/2/9 0009
+# @Email : jtyoui@qq.com
+# @Software : PyCharm
+
+from jtyoui.imagepdf.pyqt import start  # pyQt5写的界面
+from jtyoui.imagepdf.tk import window_gui  # tkinter写的界面
