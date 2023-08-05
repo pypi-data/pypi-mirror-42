6eb8feb42ea005f787fc161adbbe9a33c82e3c2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:35 +0000

    Add package SearchEngineSpider/0.1.1/SearchEngineSpider-0.1.1.tar.gz

---
+++ packages/SearchEngineSpider/SearchEngineSpider-0.1.1.tar.gz/SearchEngineSpider-0.1.1/search_engine_spider/__init__.py
@@ -0,0 +1,1 @@
+from .search_engine_manager import SearchEngineManager
---
+++ packages/SearchEngineSpider/SearchEngineSpider-0.1.1.tar.gz/SearchEngineSpider-0.1.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+setup(
+    name='SearchEngineSpider',
+    version='0.1.1',
+    description="A search engine spider",
+    install_requires=['pyquery>=1.2.17', 'requests>=2.12.4', 'cchardet', 'zcommand', 'bs4'],
+    author='izhengzhixian',
+    author_email='izhengzhixian@gmail.com',
+    platforms='any',
+    packages=find_packages(),
+    package_data={'search_engine_spider': ['data/*.txt'], 'search_engine_spider.search_engine': ['data/*.txt']},
+    scripts=['bin/search_engine_spider'],
+)
