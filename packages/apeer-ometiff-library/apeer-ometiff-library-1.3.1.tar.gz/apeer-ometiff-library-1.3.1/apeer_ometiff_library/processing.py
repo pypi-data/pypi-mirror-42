eaacfab3a8ad0de279bc7baa3ba53f28f12d1147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:18 +0000

    Add package apeer-ometiff-library/1.3.1/apeer-ometiff-library-1.3.1.tar.gz

---
+++ packages/apeer-ometiff-library/apeer-ometiff-library-1.3.1.tar.gz/apeer-ometiff-library-1.3.1/apeer_ometiff_library/processing.py
@@ -0,0 +1,32 @@
+import itertools as it
+import numpy as np
+
+
+def apply_2d_trafo(trafo_2d, array_5d, **kwargs):
+    array_out_5d = np.zeros_like(array_5d)
+    n_t, n_z, n_c, n_x, n_y = np.shape(array_out_5d)
+
+    for t, z, c in it.product(range(n_t), range(n_z), range(n_c)):
+        array_out_5d[t, z, c, :, :] = trafo_2d(array_5d[t, z, c, :, :], **kwargs)
+
+    return array_out_5d
+
+
+def apply_3d_trafo_zstack(trafo_3d, array_5d, **kwargs):
+    array_out_5d = np.zeros_like(array_5d)
+    n_t, n_z, n_c, n_x, n_y = np.shape(array_out_5d)
+
+    for t, c in it.product(range(n_t), range(n_c)):
+        array_out_5d[t, :, c, :, :] = trafo_3d(array_5d[t, :, c, :, :], **kwargs)
+
+    return array_out_5d
+
+
+def apply_3d_trafo_rgb(trafo_3d, array_5d, **kwargs):
+    array_out_5d = np.zeros_like(array_5d)
+    n_t, n_z, n_c, n_x, n_y = np.shape(array_out_5d)
+
+    for t, z in it.product(range(n_t), range(n_z)):
+        array_out_5d[t, z, :, :, :] = trafo_3d(array_5d[t, z, :, :, :], **kwargs)
+
+    return array_out_5d
