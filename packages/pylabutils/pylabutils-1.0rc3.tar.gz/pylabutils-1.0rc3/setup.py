7006482d5e4fd788e298c2b48a00b4bde1a244fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:41 +0000

    Add package pylabutils/1.0rc3/pylabutils-1.0rc3.tar.gz

---
+++ packages/pylabutils/pylabutils-1.0rc3.tar.gz/pylabutils-1.0rc3/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import setuptools
+
+setuptools.setup(
+    name = 'pylabutils',
+    version = '1.0rc3',
+    author = 'Mario García',
+    author_email = 'mariogarcc@gmail.com',
+    description = 'Some utils for working with python'
+        ' in a simple lab-like environment',
+    long_description = open('README.md', 'r').read(),
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/mariogarcc/pylabutils',
+    license = 'MIT',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3.7',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Education',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    # https://test.pypi.org/pypi?%3Aaction=list_classifiers
+    platforms = ['any'],
+    keywords = 'laboratory utils tools',
+    project_urls = {},
+    packages = setuptools.find_packages(),
+    py_modules = [],
+    install_requires = ['ipython', 'numpy', 'matplotlib', 'scipy', 'uncertainties'],
+    python_requires = '~=3.7',
+    # package_data = {}
+    include_package_data = True,
+)
