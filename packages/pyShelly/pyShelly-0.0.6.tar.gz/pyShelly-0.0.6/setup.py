7d2c11340c7bb18a749dc992d9a4673869d63f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:13 +0000

    Add package pyShelly/0.0.6/pyShelly-0.0.6.tar.gz

---
+++ packages/pyShelly/pyShelly-0.0.6.tar.gz/pyShelly-0.0.6/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyShelly",
+    version="0.0.6",
+    author="StyraHem / Tarra AB",
+    author_email="info@styrahem.se",
+    description="Library for Shelly smart home devices",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/StyraHem/pyShelly",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
