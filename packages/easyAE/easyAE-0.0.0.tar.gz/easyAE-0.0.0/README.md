d30cc713823c728148c3818a91f5a734742c9c1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:46 +0000

    Add package easyAE/0.0.0/easyAE-0.0.0.tar.gz

---
+++ packages/easyAE/easyAE-0.0.0.tar.gz/easyAE-0.0.0/README.md
@@ -0,0 +1,4 @@
+This repository is for creating auto-encoders easily. 
+The main focus of the auto-encoders on this page is for
+ genetic and spectral data analysis but likely could 
+ be used for any high dimensional data
---
+++ packages/easyAE/easyAE-0.0.0.tar.gz/easyAE-0.0.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+                 name="easyAE",
+                 version="0.0.0",
+                 author="Samuel Revolinski",
+                 author_email="samuel.revolinski@wsu.edu",
+                 description="easy way for non-linear dimensionality increase or reduction",
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 url="https://github.com/Zhiwu-Zhang-Lab/genetic_spectral_AutoEncoder",
+                 packages=setuptools.find_packages(),
+                 classifiers=[
+                              "Programming Language :: Python :: 3",
+                              "License :: OSI Approved :: MIT License",
+                              "Operating System :: OS Independent",
+                              ],
+                 )
