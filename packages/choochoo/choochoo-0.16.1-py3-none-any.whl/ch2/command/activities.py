790295471148b7c45ced23efb7f759cad94b7e15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:19 +0000

    Add package choochoo/0.16.1/choochoo-0.16.1-py3-none-any.whl

---
+++ packages/choochoo/choochoo-0.16.1-py3-none-any.whl/ch2/command/activities.py
@@ -0,0 +1,21 @@
+
+from ..command.args import PATH, FORCE, FAST
+from ..squeal.tables.pipeline import PipelineType
+from ..stoats.calculate import run_pipeline_after, run_pipeline_paths
+
+
+def activities(args, log, db):
+    '''
+## activities
+
+    > ch2 activities PATH [PATH ...]
+
+Read activities data from FIT files.
+
+Note: When using bash use `shopt -s globstar` to enable ** globbing.
+    '''
+    force, fast, paths = args[FORCE], args[FAST], args[PATH]
+    run_pipeline_paths(log, db, PipelineType.ACTIVITY, paths, force=force)
+    if not fast:
+        # don't force this - it auto-detects need
+        run_pipeline_after(log, db, PipelineType.STATISTIC)
