7c113e6be29575be87808a111567079ba33a75c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:12 +0000

    Add package nuntius/0.3.3/nuntius-0.3.3-py3-none-any.whl

---
+++ packages/nuntius/nuntius-0.3.3-py3-none-any.whl/tests/tests/test_subscribers.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+
+from nuntius.models import BaseSubscriber
+from tests.models import TestSubscriber
+
+
+class SimpleSubscriberTestCase(TestCase):
+    def test_use_email_property(self):
+        subscriber = TestSubscriber(email="email@example.com")
+        self.assertEqual(subscriber.get_subscriber_email(), "email@example.com")
+
+    def test_use_status_property(self):
+        subscriber = TestSubscriber(
+            email="email@example.com",
+            subscriber_status=AbstractSubscriber.STATUS_SUBSCRIBED,
+        )
+        self.assertEqual(
+            subscriber.get_subscriber_status(), AbstractSubscriber.STATUS_SUBSCRIBED
+        )
