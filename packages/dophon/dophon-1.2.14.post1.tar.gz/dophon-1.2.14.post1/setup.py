14687b44f3cde82f6093571ee9710df78c088be3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:28 +0000

    Add package dophon/1.2.14.post1/dophon-1.2.14.post1.tar.gz

---
+++ packages/dophon/dophon-1.2.14.post1.tar.gz/dophon-1.2.14.post1/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['dophon',
+ 'dophon.annotation',
+ 'dophon.annotation.req',
+ 'dophon.annotation.res',
+ 'dophon.tools',
+ 'dophon.tools.framework_const',
+ 'dophon.tools.gc']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['dophon-logger',
+ 'dophon-manager',
+ 'dophon-properties',
+ 'flask',
+ 'gevent',
+ 'pyyaml>=3.13,<4.0',
+ 'schedule',
+ 'tornado',
+ 'urllib3']
+
+setup_kwargs = {
+    'name': 'dophon',
+    'version': '1.2.14.post1',
+    'description': 'dophon web framework like springboot',
+    'long_description': None,
+    'author': 'CallMeE',
+    'author_email': 'ealohu@163.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
