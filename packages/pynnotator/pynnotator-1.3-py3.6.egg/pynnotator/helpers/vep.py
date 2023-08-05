1ff8dfc8118675739a5dd37a82b04e5eea1c2ce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:32 +0000

    Add package pynnotator/1.3/pynnotator-1.3-py3.6.egg

---
+++ packages/pynnotator/pynnotator-1.3-py3.6.egg/pynnotator/helpers/vep.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import os
+import subprocess
+from datetime import datetime
+from subprocess import call
+
+from pynnotator import settings
+
+toolname = 'vep'
+
+
+class Vep(object):
+    def __init__(self, vcffile=None):
+
+        self.vcffile = vcffile
+
+        self.filename = os.path.splitext(os.path.basename(str(vcffile)))[0]
+
+        # create folder vep if it doesn't exists
+        if not os.path.exists('vep'):
+            os.makedirs('vep')
+            # enter inside folder
+            # os.chdir('vep')
+
+    def run(self):
+        tstart = datetime.now()
+        print(tstart, 'Starting vep annotation: ', self.vcffile)
+
+        self.annotate()
+
+        tend = datetime.now()
+        annotation_time = tend - tstart
+        print(tend, 'Finished vep annotation, it took: ', annotation_time)
+
+    # convert and annotate the vcf file to vep
+    def annotate(self):
+
+        command = '''perl %s/vep \
+        -i %s \
+        --format vcf \
+        --dir %s \
+        -sift b -polyphen b \
+        -o vep/vep.output.vcf --vcf --cache --force_overwrite \
+        --no_progress \
+        --no_intergenic \
+        --numbers \
+        --biotype \
+        --total_length \
+        --coding_only \
+        --pick \
+        --offline \
+        --symbol \
+        1>vep/vep.log \
+        --fork %s \
+        ''' % (settings.vep_dir, self.vcffile, settings.vep_cache_dir, settings.vep_cores)
+        # 1> vepreport.log \
+        ##--pick \
+        # condel_plugin, dbscsnv_plugin, 
+        # -plugin %s \
+        # --plugin %s \
+
+        p = subprocess.call(command,
+                            cwd=os.getcwd(),
+                            shell=True)
+
+        tend = datetime.now()
+
+        # if p == 0:
+        #     print(tend, 'This vcf was sucessfully annotated by %s!' % (toolname))
+        # else:
+        #     print(tend, 'Sorry this vcf could not be annotated by %s' % (toolname))
+
+        # command = '(grep ^# output.vep.vcf; grep -v ^# output.vep.vcf|sort -k1,1N -k2,2n) > output.vep.sorted.vcf'
+        # Sort VCF file 
+
+
+        command = '''grep '^#' vep/vep.output.vcf > vep/vep.output.sorted.vcf'''
+        call(command, shell=True)
+
+        command = '''grep -E -v '^X|^Y|^M|^#|^GL' vep/vep.output.vcf | sort -n -k1 -k2 >> vep/vep.output.sorted.vcf'''
+        call(command, shell=True)
+
+        command = '''grep -E '^X' vep/vep.output.vcf | sort -k1,1d -k2,2n >> vep/vep.output.sorted.vcf'''
+        call(command, shell=True)
+
+        command = '''grep -E '^Y' vep/vep.output.vcf | sort -k1,1d -k2,2n >> vep/vep.output.sorted.vcf'''
+        call(command, shell=True)
+
+        command = '''grep -E '^M' vep/vep.output.vcf | sort -k1,1d -k2,2n >> vep/vep.output.sorted.vcf'''
+        call(command, shell=True)
+
+        tend = datetime.now()
+        # print(tend, 'Finished sorting vep vcf.')
+
+        command = 'rm vep/vep.output.vcf'
+        call(command, shell=True)
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Annotate a VCF File with VEP.')
+    parser.add_argument('-i', dest='vcffile', required=True, metavar='example.vcf', help='a VCF file to be annotated')
+
+    args = parser.parse_args()
+
+    vep = Vep(args.vcffile)
+    vep.run()
