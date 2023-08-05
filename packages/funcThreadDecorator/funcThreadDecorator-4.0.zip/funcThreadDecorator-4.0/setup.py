56179e3896f7ba318e6ebb516da0490eacca02d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:29 +0000

    Add package funcThreadDecorator/4.0/funcThreadDecorator-4.0.zip

---
+++ packages/funcThreadDecorator/funcThreadDecorator-4.0.zip/funcThreadDecorator-4.0/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#coding=utf-8
+import sys
+from distutils.core import setup
+
+setup(name="funcThreadDecorator", version="4.0", description="函数线程装饰器", author="wanwan",
+      author_email="1296935454@qq.com",
+      url="http://blog.hui2017.top",
+      py_modules=['funcThreadDecorator.funcThreadDecorator'])
