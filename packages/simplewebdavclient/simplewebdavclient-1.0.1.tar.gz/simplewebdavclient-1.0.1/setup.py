84bfa6a5ec1d964993ee0a10d1d40301b6f59b34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:00 +0000

    Add package simplewebdavclient/1.0.1/simplewebdavclient-1.0.1.tar.gz

---
+++ packages/simplewebdavclient/simplewebdavclient-1.0.1.tar.gz/simplewebdavclient-1.0.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+packages = [
+    'simplewebdavclient'
+]
+
+install_requires = [
+    'requests == 2.21.0',
+]
+
+tests_require = [
+    'pytest',
+]
+
+setup(
+    name='simplewebdavclient',
+    version='1.0.1',
+    python_requires='~=3.3',
+    description='This is a library used to make WebDav Client Connections simple.',
+    keywords='webdav simple',
+    url='https://github.com/btr1975/simplewebdavclient',
+    author='Benjamin P. Trachtenberg',
+    author_email='e_ben_75-python@yahoo.com',
+    license='MIT',
+    packages=packages,
+    include_package_data=True,
+    install_requires=install_requires,
+    test_suite='pytest',
+    tests_require=tests_require,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
