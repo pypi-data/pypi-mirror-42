6b97e2e8874dbc9d74dc9d466decd66992f87123
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:18 +0000

    Add package infi.recipe.python/0.6.23/infi.recipe.python-0.6.23.tar.gz

---
+++ packages/infi.recipe.python/infi.recipe.python-0.6.23.tar.gz/infi.recipe.python-0.6.23/src/infi/recipe/python/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.6.23"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.6.23)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'd0dcb42618e97ba8d5a4c9f64eeeef9267b75e5a'
+__git_head_subject__ = u'Finished Release v0.6.23'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2019-02-18 13:18:25'
