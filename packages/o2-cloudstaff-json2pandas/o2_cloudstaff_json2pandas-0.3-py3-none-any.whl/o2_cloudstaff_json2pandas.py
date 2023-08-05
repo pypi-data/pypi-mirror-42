144802efc14459548c64a3dcf472a24ce9aca015
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:51 +0000

    Add package o2-cloudstaff-json2pandas/0.3/o2_cloudstaff_json2pandas-0.3-py3-none-any.whl

---
+++ packages/o2-cloudstaff-json2pandas/o2_cloudstaff_json2pandas-0.3-py3-none-any.whl/o2_cloudstaff_json2pandas.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+import json
+import pandas as pd
+from money2float import money
+from pathlib import Path
+
+
+# In[19]:
+
+
+def get_json_data(json_filename):
+    if json_filename.exists():
+        print("Exists")
+        with open(json_filename) as json_data:
+            data = json.load(json_data)
+    return data
+
+
+# In[35]:
+
+
+def create_dataframe_from_json(json_data):
+    json_lines = json_data["content"]["test_line_items"]
+    line_df = pd.DataFrame(json_lines)
+    line_df["group"] = (
+        line_df["description"]
+        .apply(lambda x: x if "***" in x else None)
+        .fillna(method="ffill")
+    )
+    line_df["group"] = line_df["group"].apply(lambda x: x.strip("***"))
+    line_df = line_df.set_index(["group"])
+    line_df["qty"] = line_df["qty"].apply(lambda x: money(f"{x}"))
+    line_df["unit_price"] = line_df["unit_price"].apply(lambda x: money(f"{x}"))
+    line_df["line_total"] = line_df["line_total"].apply(lambda x: money(f"{x}"))
+    return line_df
+
+
+# In[ ]:
