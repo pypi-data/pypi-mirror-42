93945a9690091c737bf36865e2e4423193ac0929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:57 +0000

    Add package m6e.grpc_sdk/0.0.112/m6e.grpc_sdk-0.0.112.tar.gz

---
+++ packages/m6e.grpc_sdk/m6e.grpc_sdk-0.0.112.tar.gz/m6e.grpc_sdk-0.0.112/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='m6e.grpc_sdk',
+    packages=['m6e.grpc_sdk'],
+    version='0.0.112',
+    description='',
+    author='',
+    author_email='',
+    include_package_data=True,
+    url='https://github.com/m6e/grpc-sdk',
+    download_url='',
+    keywords=[],
+    package_data={'m6e.grpc_sdk':['*', '*/*', '*/*/*']},
+    install_requires=[
+        'googleapis-common-protos',
+        'grpcio-tools'
+    ],
+)
