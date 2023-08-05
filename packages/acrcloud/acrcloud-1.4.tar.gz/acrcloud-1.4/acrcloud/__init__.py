cdc5d4f58532659eb6cc8df0f89cd32d7003c50e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:15 +0000

    Add package acrcloud/1.4/acrcloud-1.4.tar.gz

---
+++ packages/acrcloud/acrcloud-1.4.tar.gz/acrcloud-1.4/acrcloud/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import time
+import hmac
+import base64
+import hashlib
+import requests
+def recognizer(config, audio):
+    timestamp = time.time()
+    string_to_sign = "POST" + "\n" + "/v1/identify" + "\n" + config['key'] + "\n" + "audio" + "\n" + "1" + "\n" + str(timestamp)
+    sign = base64.b64encode(hmac.new(bytes(config['secret'].encode()), bytes(string_to_sign.encode()), hashlib.sha1).digest())
+    data = {"access_key": config['key'],
+            "sample_bytes": 1000000,
+            "sample": base64.b64encode(open(audio, "rb").read(1000000)),
+            "timestamp": str(timestamp),
+            "signature": sign,
+            "data_type": "audio",
+            "signature_version": "1"
+    }
+    return requests.post(config['host'] + "/v1/identify", data).json()
