48b5843b18fa5ea1d1784df35dcc0a0d010e9ad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:34 +0000

    Add package example-pkg-wei/1.0.3/example-pkg-wei-1.0.3.tar.gz

---
+++ packages/example-pkg-wei/example-pkg-wei-1.0.3.tar.gz/example-pkg-wei-1.0.3/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Sun Wei"
+__copyright__ = "Copyright (C) 2019 GFLoan Co. LTD"
+__license__ = "Private"
+__version__ = "1.0"
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-wei",
+    version="1.0.3",
+    author="Example Author",
+    author_email="sw@gfloan.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    scripts=['bin/hello-joke'],
+    entry_points={
+        'console_scripts': ['entry_joke=example_pkg_wei.command_line:main']
+    }
+)
