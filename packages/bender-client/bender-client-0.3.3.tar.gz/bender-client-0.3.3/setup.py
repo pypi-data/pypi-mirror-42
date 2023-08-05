0d5cceb286cb0217783de21212717e43d4fc3446
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:24 +0000

    Add package bender-client/0.3.3/bender-client-0.3.3.tar.gz

---
+++ packages/bender-client/bender-client-0.3.3.tar.gz/bender-client-0.3.3/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+import benderclient
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='bender-client',
+    version=benderclient.__version__,
+    packages=["benderclient"],
+    author="Dreem",
+    author_email='valentin@dreem.com',
+    description='Bender Python Client',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://bender.dreem.com",
+    install_requires=[
+        'requests==2.20.1',
+        'python-jose==3.0.1',
+    ],
+)
