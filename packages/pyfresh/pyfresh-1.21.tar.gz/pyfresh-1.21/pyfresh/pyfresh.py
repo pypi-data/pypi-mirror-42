a9b6f8b8a96bfd5c9a2a2b05d4ecd5dd98996b58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:53 +0000

    Add package pyfresh/1.21/pyfresh-1.21.tar.gz

---
+++ packages/pyfresh/pyfresh-1.21.tar.gz/pyfresh-1.21/pyfresh/pyfresh.py
@@ -0,0 +1,51 @@
+import os
+import shutil
+
+
+def files(path, confirm=False):
+    for root, dirs, filenames in os.walk(path):
+        if root.endswith("__pycache__"):
+            print(f"Removed directory {root}")
+            if confirm is True:
+                shutil.rmtree(root)
+            continue
+        for filename in filenames:
+            if filename.endswith(".pyc"):
+                print(f"Removed compiled file {root}/{filename}")
+                if confirm is True:
+                    os.remove(root + os.sep + filename)
+            if filename.endswith(".pyo"):
+                print(f"Removed object file {root}/{filename}")
+                if confirm is True:
+                    os.remove(root + os.sep + filename)
+
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Clean up python source files"
+    )
+    parser.add_argument(
+        "-c",
+        "--confirm",
+        action="store_true",
+        default=False,
+        help="Actually perform clean up",
+    )
+    parser.add_argument(
+        "-p",
+        "--path",
+        dest="path",
+        default="./",
+        action="store",
+        help="folder to clean up",
+    )
+
+    args = parser.parse_args()
+
+    files(os.path.abspath("./"), args.confirm)
+
+
+if __name__ == "__main__":
+    main()
