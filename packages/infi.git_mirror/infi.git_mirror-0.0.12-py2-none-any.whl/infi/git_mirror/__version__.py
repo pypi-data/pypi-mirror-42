1bd5e2f431202dd2a5dd8ea2a818afbb7ad1d90b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:21 +0000

    Add package infi.git_mirror/0.0.12/infi.git_mirror-0.0.12-py2-none-any.whl

---
+++ packages/infi.git_mirror/infi.git_mirror-0.0.12-py2-none-any.whl/infi/git_mirror/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.0.12"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.0.12)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'cfa460cbbb65247bc3b357a245cca74dc66773c8'
+__git_head_subject__ = u'Finished Release v0.0.12'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2019-02-18 12:42:52'
