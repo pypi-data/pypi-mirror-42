b46ec4717a44b55a3161d211f10fad4367643d8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:15 +0000

    Add package bible-male-names/1.0.2/bible_male_names-1.0.2.tar.gz

---
+++ packages/bible-male-names/bible_male_names-1.0.2.tar.gz/bible_male_names-1.0.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("readme.md", "r") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name = "bible_male_names",
+    packages = ["bible_male_names"],
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    version = "1.0.2",
+    description = "Get male names from The Bible",
+    author = "Carlos Abraham",
+    author_email = "abraham@abranhe.com",
+    url = "https://p.abranhe.com/bible-male-names.py",
+    classifiers=(
+        "Programming Language :: Python",
+        "Natural Language :: English",
+        "Environment :: Plugins",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+    ),
+    project_urls={
+        'Source': 'https://github.com/abranhe/bible-male-names.py',
+    },
+)
