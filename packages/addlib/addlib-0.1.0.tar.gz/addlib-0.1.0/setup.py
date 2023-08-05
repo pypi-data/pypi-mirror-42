5992c768987c24c1e325d7ff450141728775b0b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:22 +0000

    Add package addlib/0.1.0/addlib-0.1.0.tar.gz

---
+++ packages/addlib/addlib-0.1.0.tar.gz/addlib-0.1.0/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup, Extension
+
+addlib = Extension(
+    'addlib',
+    sources=[],
+    extra_objects=['addlib/addlib.so'],
+)
+
+setup(
+    name='addlib',
+    version=__import__('addlib').__version__,
+    description="Address Library (CN).",
+    author='qx3501332',
+    author_email='x.qiu@qq.com',
+    license="MIT License",
+    packages=['addlib'],
+    package_data={'addlib': ['lib.add/*.data', 'addlib.so']},
+    ext_modules=[],
+    include_package_date=True,
+    zip_safe=True,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
