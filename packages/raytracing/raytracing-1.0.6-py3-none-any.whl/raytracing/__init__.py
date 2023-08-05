0dae765b06eb5944e800790306aea90a2b8f7502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:28 +0000

    Add package raytracing/1.0.6/raytracing-1.0.6-py3-none-any.whl

---
+++ packages/raytracing/raytracing-1.0.6-py3-none-any.whl/raytracing/__init__.py
@@ -0,0 +1,11 @@
+# We import everything by default, in the general namespace
+# because it is simpler for everyone
+
+from .abcd import *
+from .objectives import *
+from .axicon import *
+
+__version__ = "1.0.6"
+__author__ = "Daniel Cote <dccote@cervo.ulaval.ca>"
+print("Main documentation at https://github.com/DCC-Lab/RayTracing")
+
