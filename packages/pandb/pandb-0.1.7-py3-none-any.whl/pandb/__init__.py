739a1bc51c0abb7c1af18d25260399a70d79d50d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:21 +0000

    Add package pandb/0.1.7/pandb-0.1.7-py3-none-any.whl

---
+++ packages/pandb/pandb-0.1.7-py3-none-any.whl/pandb/__init__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019 Frootlab Developers
+#
+# This file is part of Pandora, https://frootlab.github.io/pandora
+#
+#  Pandora is free software: you can redistribute it and/or modify it under the
+#  terms of the GNU General Public License as published by the Free Software
+#  Foundation, either version 3 of the License, or (at your option) any later
+#  version.
+#
+#  Pandora is distributed in the hope that it will be useful, but WITHOUT ANY
+#  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+#  A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License along with
+#  Pandora. If not, see <http://www.gnu.org/licenses/>.
+#
+"""Pandora database engine.
+
+*Pandora* is a SQL database engine and a universal database proxy. Pandora aims
+to provide a unified SQL:2016 interface to a broad variety of different data
+sources, including SQL Databases and flat files from laboratory measurements.
+Pandora is open source and based on the Python programming language.
+
+"""
+
+__version__ = '0.1.7'
+__license__ = 'GPLv3'
+__copyright__ = 'Copyright (c) 2019 Frootlab Developers'
+__organization__ = 'frootlab'
+__author__ = 'frootlab'
+__email__ = 'frootlab@gmail.com'
+__maintainer__ = 'Patrick Michl'
+__docformat__ = 'google'
+__authors__ = ['Patrick Michl <patrick.michl@gmail.com>']
