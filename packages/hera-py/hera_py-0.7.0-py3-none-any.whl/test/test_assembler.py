5228e0972b30d83b199555bbd4eb91b75ddaafb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:13 +0000

    Add package hera-py/0.7.0/hera_py-0.7.0-py3-none-any.whl

---
+++ packages/hera-py/hera_py-0.7.0-py3-none-any.whl/test/test_assembler.py
@@ -0,0 +1,81 @@
+from hera.main import main
+
+
+def test_assemble_set_inc(capsys):
+    main(["assemble", "--code", "--stdout", "test/assets/asm/set_inc.hera"])
+
+    captured = capsys.readouterr()
+    assert captured.err == ""
+
+    with open("test/assets/asm/set_inc.hera.lcode") as f:
+        assert captured.out == f.read()
+
+
+def test_assemble_binary_op(capsys):
+    main(["assemble", "--code", "--stdout", "test/assets/asm/binary_op.hera"])
+
+    captured = capsys.readouterr()
+    assert captured.err == ""
+
+    with open("test/assets/asm/binary_op.hera.lcode") as f:
+        assert captured.out == f.read()
+
+
+def test_assemble_branch(capsys):
+    main(["assemble", "--code", "--stdout", "test/assets/asm/branch.hera"])
+
+    captured = capsys.readouterr()
+    assert captured.err == ""
+
+    with open("test/assets/asm/branch.hera.lcode") as f:
+        assert captured.out == f.read()
+
+
+def test_assemble_flag(capsys):
+    main(["assemble", "--code", "--stdout", "test/assets/asm/flag.hera"])
+
+    captured = capsys.readouterr()
+    assert captured.err == ""
+
+    with open("test/assets/asm/flag.hera.lcode") as f:
+        assert captured.out == f.read()
+
+
+def test_assemble_shift(capsys):
+    main(["assemble", "--code", "--stdout", "test/assets/asm/shift.hera"])
+
+    captured = capsys.readouterr()
+    assert captured.err == ""
+
+    with open("test/assets/asm/shift.hera.lcode") as f:
+        assert captured.out == f.read()
+
+
+def test_assemble_misc(capsys):
+    main(["assemble", "--code", "--stdout", "test/assets/asm/misc.hera"])
+
+    captured = capsys.readouterr()
+    assert captured.err == ""
+
+    with open("test/assets/asm/misc.hera.lcode") as f:
+        assert captured.out == f.read()
+
+
+def test_assemble_data(capsys):
+    main(["assemble", "--data", "--stdout", "test/assets/asm/data.hera"])
+
+    captured = capsys.readouterr()
+    assert captured.err == ""
+
+    with open("test/assets/asm/data.hera.ldata") as f:
+        assert captured.out == f.read()
+
+
+def test_assemble_data_with_big_stack(capsys):
+    main(["assemble", "--data", "--stdout", "--big-stack", "test/assets/asm/data.hera"])
+
+    captured = capsys.readouterr()
+    assert captured.err == ""
+
+    with open("test/assets/asm/data_big_stack.hera.ldata") as f:
+        assert captured.out == f.read()
