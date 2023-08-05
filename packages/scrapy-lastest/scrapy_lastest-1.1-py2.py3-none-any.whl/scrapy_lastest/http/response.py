09e8aacb412edecf7611e11ae841dfd4ef04a5c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:07 +0000

    Add package scrapy-lastest/1.1/scrapy_lastest-1.1-py2.py3-none-any.whl

---
+++ packages/scrapy-lastest/scrapy_lastest-1.1-py2.py3-none-any.whl/scrapy_lastest/http/response.py
@@ -0,0 +1,8 @@
+class Response(object):
+    """框架内置Response对象"""
+
+    def __init__(self, url, status_code, headers, body):
+        self.url = url  # 响应url
+        self.status_code = status_code  # 响应状态码
+        self.headers = headers  # 响应头
+        self.body = body  # 响应体
