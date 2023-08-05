6e39a2cc4b5a6c03850aa0f9d4b63ebbcd002f65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:12 +0000

    Add package kamidana/0.6.2/kamidana-0.6.2-py2.py3-none-any.whl

---
+++ packages/kamidana/kamidana-0.6.2-py2.py3-none-any.whl/kamidana/interfaces.py
@@ -0,0 +1,29 @@
+import abc
+
+
+class ITemplateLoader(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def load(self, filename):
+        pass
+
+    @abc.abstractproperty
+    def data(self):
+        pass
+
+    @abc.abstractmethod
+    def additionals(self):
+        pass
+
+
+class IDriver(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def load(self, template_file):
+        pass
+
+    @abc.abstractmethod
+    def dump(self, d, dst):
+        pass
+
+    @abc.abstractmethod
+    def run(self, src, dst):
+        pass
