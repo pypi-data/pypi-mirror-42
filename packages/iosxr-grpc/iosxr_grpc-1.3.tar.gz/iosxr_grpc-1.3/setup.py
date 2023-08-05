021b2853fb827b6f48a277ea60045c3d31c31406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:35 +0000

    Add package iosxr-grpc/1.3/iosxr_grpc-1.3.tar.gz

---
+++ packages/iosxr-grpc/iosxr_grpc-1.3.tar.gz/iosxr_grpc-1.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(name='iosxr_grpc',
+      version='1.3',
+      description='gRPC library for IOS-XR > 6.1.1',
+      url='https://github.com/cisco-grpc-connection-libs/ios-xr-grpc-python',
+      author='Karthik Kumaravel',
+      authoer_email='srirudrankumaravel@gmail.com',
+      licencse='Apache 2.0',
+      packages=['iosxr_grpc'],
+      install_requires=[
+          'grpcio==1.18.0',
+          'protobuf==3.6.0',
+      ],
+      zip_safe=False)
