0da61788fa0b766980563e2ba16417cb6f72cb42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:20 +0000

    Add package infi.gevent-utils/0.2.6/infi.gevent_utils-0.2.6-py2-none-any.whl

---
+++ packages/infi.gevent-utils/infi.gevent_utils-0.2.6-py2-none-any.whl/infi/gevent_utils/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.2.6"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.2.6)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'cae355d79213d28cf74632d6f3550db6e387c9d0'
+__git_head_subject__ = u'Finished Release v0.2.6'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2019-02-18 12:40:02'
