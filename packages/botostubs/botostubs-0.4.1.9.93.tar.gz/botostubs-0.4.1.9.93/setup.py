46ca7e3d5af88d6b417c641bb312c61753d96e7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:45 +0000

    Add package botostubs/0.4.1.9.93/botostubs-0.4.1.9.93.tar.gz

---
+++ packages/botostubs/botostubs-0.4.1.9.93.tar.gz/botostubs-0.4.1.9.93/setup.py
@@ -0,0 +1,23 @@
+import boto3
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="botostubs",
+    version="0.4." + boto3.__version__,
+    author="Jeshan G. BABOOA",
+    author_email="j@jeshan.co",
+    description="boto3 code assistance for any API in any IDE, always up to date",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jeshan/botostubs",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+)
