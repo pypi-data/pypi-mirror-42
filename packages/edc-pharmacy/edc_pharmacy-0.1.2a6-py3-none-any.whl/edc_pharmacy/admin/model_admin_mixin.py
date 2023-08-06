19a20e1fcbfb0edf3a3bc8cff6744fb10b702494
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:47 +0000

    Add package edc-pharmacy/0.1.2a6/edc_pharmacy-0.1.2a6-py3-none-any.whl

---
+++ packages/edc-pharmacy/edc_pharmacy-0.1.2a6-py3-none-any.whl/edc_pharmacy/admin/model_admin_mixin.py
@@ -0,0 +1,21 @@
+from django_revision.modeladmin_mixin import ModelAdminRevisionMixin
+from edc_model_admin import (
+    ModelAdminNextUrlRedirectMixin,
+    ModelAdminFormInstructionsMixin,
+    ModelAdminFormAutoNumberMixin,
+    ModelAdminAuditFieldsMixin,
+    ModelAdminReadOnlyMixin,
+    ModelAdminInstitutionMixin,
+)
+
+
+class ModelAdminMixin(
+    ModelAdminNextUrlRedirectMixin,
+    ModelAdminFormInstructionsMixin,
+    ModelAdminFormAutoNumberMixin,
+    ModelAdminRevisionMixin,
+    ModelAdminAuditFieldsMixin,
+    ModelAdminReadOnlyMixin,
+    ModelAdminInstitutionMixin,
+):
+    pass
