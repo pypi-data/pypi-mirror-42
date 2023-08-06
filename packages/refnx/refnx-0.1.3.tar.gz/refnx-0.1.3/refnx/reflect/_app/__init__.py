1748389ed7e1ea1da2c5acb7c200fbb14e779693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:39 +0000

    Add package refnx/0.1.3/refnx-0.1.3.tar.gz

---
+++ packages/refnx/refnx-0.1.3.tar.gz/refnx-0.1.3/refnx/reflect/_app/__init__.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def gui():
+    from PyQt5 import QtGui, QtWidgets
+    from refnx.reflect._app.view import MotofitMainWindow
+    from refnx.reflect._app import resources_rc
+
+    app = QtWidgets.QApplication(sys.argv)
+    app.setWindowIcon(QtGui.QIcon(':icons/scattering.png'))
+    myapp = MotofitMainWindow()
+
+    myapp.show()
+    myapp.raise_()
+
+    v = app.exec_()
+    return v
+
+
+def main():
+    sys.exit(gui())
+
+
+__all__ = [gui, main]
---
+++ packages/refnx/refnx-0.1.3.tar.gz/refnx-0.1.3/refnx/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM REFNX SETUP.PY
+short_version = '0.1.3'
+version = '0.1.3'
+full_version = '0.1.3'
+git_revision = '0391901f7ed9b7466181b8034515eff82210b960'
+release = True
+if not release:
+    version = full_version
