e23f59eab408649f5512a774a5b73c19c92696fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:09 +0000

    Add package mininet-topology-yaml/0.1.5/mininet-topology-yaml-0.1.5.tar.gz

---
+++ packages/mininet-topology-yaml/mininet-topology-yaml-0.1.5.tar.gz/mininet-topology-yaml-0.1.5/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+setuptools.setup(
+    name="mininet-topology-yaml",
+    version="0.1.5",
+    #url="https://github.com/luminanetworks/mininet-topology-yaml",
+
+    author="Lumina NetDev",
+    author_email="oss-dev@luminanetworks.com",
+
+    description="Mininet specific topology generator and parser",
+    long_description=open('README.rst').read(),
+
+    packages=setuptools.find_packages(),
+
+    install_requires=['click', 'topology-yaml==0.1.5', 'pytest', 'coloredlogs'],
+
+    setup_requires=['pytest-runner'],
+
+    tests_require=['requests_mock', 'requests'],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    entry_points='''
+        [topology.plugins]
+        mininet=mn_topo.cli:mininet
+    ''',
+)
