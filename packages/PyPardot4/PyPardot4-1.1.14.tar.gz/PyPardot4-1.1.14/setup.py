2c5b940d2abf971addd28a5a956f678a46403bd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:34 +0000

    Add package PyPardot4/1.1.14/PyPardot4-1.1.14.tar.gz

---
+++ packages/PyPardot4/PyPardot4-1.1.14.tar.gz/PyPardot4-1.1.14/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name="PyPardot4",
+    version="1.1.14",
+    author="Matt Needham",
+    author_email="matthew.m.needham@gmail.com",
+	license="MIT",
+    description="API wrapper for APIv4 of Pardot marketing automation software.",
+    keywords="pardot",
+    url="https://github.com/mneedham91/PyPardot4",
+    packages=['pypardot', 'pypardot.objects'],
+    install_requires=['requests'],
+)
