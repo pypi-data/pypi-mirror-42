6b5f68fce6f3bc5a77672c6f0079011cc9d28fd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:24 +0000

    Add package mplview/0.0.6/mplview-0.0.6.tar.gz

---
+++ packages/mplview/mplview-0.0.6.tar.gz/mplview-0.0.6/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+import setuptools
+from setuptools import setup
+import versioneer
+
+
+with open("README.rst") as readme_file:
+    readme = readme_file.read()
+
+with open("HISTORY.rst") as history_file:
+    history = history_file.read()
+
+requirements = [
+    "matplotlib <3",
+    "npctypes",
+    "numpy",
+]
+
+test_requirements = []
+
+
+setup(
+    name="mplview",
+    version=versioneer.get_version(),
+    description="A simple, embeddable Matplotlib-based image viewer.",
+    long_description=readme + "\n\n" + history,
+    author="John Kirkham",
+    author_email="kirkhamj@janelia.hhmi.org",
+    url="https://github.com/jakirkham/mplview",
+    cmdclass=versioneer.get_cmdclass(),
+    packages=setuptools.find_packages(exclude=["tests*"]),
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD 3-Clause",
+    zip_safe=False,
+    keywords="mplview",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+    test_suite="tests",
+    tests_require=test_requirements
+)
