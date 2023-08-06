1d30300e70f90985d209dcae3a88dbb58bff89e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:22 +0000

    Add package kfwy/0.0.17/kfwy-0.0.17.tar.gz

---
+++ packages/kfwy/kfwy-0.0.17.tar.gz/kfwy-0.0.17/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kfwy",
+    version="0.0.17",
+    author="Tiffany Nguyen",
+    description="A data analysis toolkit for the KFW lab (Windows)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tiffkwin/KFW",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
