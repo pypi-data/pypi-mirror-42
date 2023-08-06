b4104f4c895a08157b8a2302281c912a61f302eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:28 +0000

    Add package sorting-ms/0.1/sorting_ms-0.1.tar.gz

---
+++ packages/sorting-ms/sorting_ms-0.1.tar.gz/sorting_ms-0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(name='sorting_ms',
+      version='0.1',
+      description='some simple sorting functions',
+      long_description='Exercise to create a python package that has some simple sorting functions included',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Processing :: Linguistic',
+      ],
+      keywords='sorting function marita salz',
+      url='https://gitlab.propulsion-home.ch/marita-salz/week-3/day-4',
+      author='Marita Salz',
+      author_email='marita.salz@hispeed.ch',
+      license='MIT',
+      packages=['sorting_ms'],
+      install_requires=[
+          'markdown',
+      ],
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+ )
+
