1e88912e02a9bbc5403c32af07dc3ed13603923c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:52 +0000

    Add package paynspray/0.2.10/paynspray-0.2.10-py2-none-any.whl

---
+++ packages/paynspray/paynspray-0.2.10-py2-none-any.whl/paynspray/xor.py
@@ -0,0 +1,24 @@
+import amncore.compat
+import struct
+
+def xor(key, buffer):
+    """Returns an XOR encoded buffer"""
+    if type(key) == int:
+        xor_key = int(key)
+    elif type(key) == NoneType:
+        xor_key = 0
+    else:
+        xor_key = int(key, 16)
+
+    if not (0 <= xor_key <= 255):
+        raise ValueError("XOR key must be a value between 0 and 255")
+
+    buf = ''
+
+    for byte in bytearray(buffer):
+        xor_byte = byte ^ xor_key
+        buf += amncore.compat.pack_s('B', xor_byte)
+
+    return buf
+
+    
