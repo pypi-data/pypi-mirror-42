f49cd76a54ab28678769dea1f9db7dede87f1110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:05 +0000

    Add package Flask-Exceptions/1.2.3/Flask_Exceptions-1.2.3-py3-none-any.whl

---
+++ packages/Flask-Exceptions/Flask_Exceptions-1.2.3-py3-none-any.whl/flask_exceptions/__init__.py
@@ -0,0 +1,7 @@
+"""Flask-Exceptions module.
+
+Import items here that are necessary to make imports cleaner when using app.
+"""
+# pylint: disable=wildcard-import
+from . import extension  # noqa
+from .extension import *  # noqa
