31daad790a941a85412cd5b674a21aea13e16d37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:29 +0000

    Add package sdktest/0.1.1/sdktest-0.1.1.tar.gz

---
+++ packages/sdktest/sdktest-0.1.1.tar.gz/sdktest-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+	name = "sdktest",
+	version = "0.1.1",
+	author="Peng Shiyu",
+    license = 'MIT License',  
+    author_email="pengshiyuyx@gmail.com",
+	url = 'https://github.com/snowroll/python-sdk.git',
+	long_description = open('README.md').read(),
+	packages = find_packages(),
+)
