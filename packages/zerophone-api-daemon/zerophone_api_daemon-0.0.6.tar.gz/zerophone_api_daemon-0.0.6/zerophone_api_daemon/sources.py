8e309361e7e7d98ca74753fbbaedd659cd41fe57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:17 +0000

    Add package zerophone-api-daemon/0.0.6/zerophone_api_daemon-0.0.6.tar.gz

---
+++ packages/zerophone-api-daemon/zerophone_api_daemon-0.0.6.tar.gz/zerophone_api_daemon-0.0.6/zerophone_api_daemon/sources.py
@@ -0,0 +1,22 @@
+from time import time
+
+class UnassignedValue(object):
+    pass
+
+class ThrottledSource(object):
+
+    def __init__(self, callback, timeout):
+        self.cb = callback
+        self.timeout = timeout
+        self.time = time()
+        self.value = UnassignedValue()
+
+    def timed_out(self):
+        return time() > self.timeout+self.time
+
+    def get(self):
+        if isinstance(self.value, UnassignedValue) or self.timed_out():
+            print("Value timed out, calling {}".format(self.cb.__name__))
+            self.value = self.cb()
+            self.time = time()
+        return self.value
