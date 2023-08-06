a96546b56f0678f7089e692b00238157aaa42ebf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:32 +0000

    Add package gopass-chrome-importer/1.1.0/gopass-chrome-importer-1.1.0.tar.gz

---
+++ packages/gopass-chrome-importer/gopass-chrome-importer-1.1.0.tar.gz/gopass-chrome-importer-1.1.0/README.md
@@ -0,0 +1,181 @@
+# gopass-chrome-importer  [![Build Status](https://travis-ci.org/markusressel/gopass-chrome-importer.svg?branch=master)](https://travis-ci.org/markusressel/gopass-chrome-importer) [![PyPI version](https://badge.fury.io/py/gopass-chrome-importer.svg)](https://badge.fury.io/py/gopass-chrome-importer)
+A simple tool to import Chrome passwords into [gopass](https://github.com/gopasspw/gopass).
+
+# How it works
+
+**gopass-chrome-importer** parses the `.csv` file that can be exported from chrome
+and loops over each entry in the list which provides
+
+* a **name** (of the entry)
+* the **url**
+* the **username**
+* the **password**
+
+## Determining the secret file path
+
+1. Since not only passwords for websites are stored in chrome all entries are divided into categories 
+which are later used as subfolders when creating the actual secrets in gopass. 
+2. Next the url is simplified based on the category to provide a human readable name for the secret.
+3. To improve usability with the gopass chrome and firefox plugin the username is used as the file name of the secret.
+
+* `website`
+  * the url is stripped down to only contain the domain (and all subdomains) f.ex. `import/website/smile.amazon.com/your@mail.com`
+* `ip`
+  * since saved passwords of IPs can be duplicates based on the network you are in
+    these passwords are sorted into their own category and the url is stripped to only contain the IP (v4)
+  * IPv6 is currently not detected and would be handled as a website
+* `android`
+  * the url is stripped down to only contain the package of the app f.ex. `import/android/com.paypal.android.p2pmobile/your@mail.com`
+
+## Secret file content
+
+The first line of a secret is always supposed to be a password 
+with additional info added below separated by a `---` divider.
+When a username is available it will be added to the additional info section
+which can then be used by the chrome and firefox plugin:
+
+```text
+mypassword1234
+---
+user: myuser
+```
+
+If no user is available just the password is written to the file:
+
+```text
+mypassword1234
+```
+
+## Creating the secret
+
+Since **gopass** does not provide a non-interactive way to create or modify secrets the fact that
+secrets can not only be edited but also created using the `gopass edit` command is used to work around this
+(security related) limitation. When executing `gopass edit /some/secret` **gopass** creates a temporary decrypted 
+file inside `/dev/shm` for this secret which is then passed to `$EDITOR`. **gopass-chrome-importer** uses this to 
+its advantage and writes itself into that env variable to process the temporarily decrypted file. 
+First of all the existing file is checked to prevent overwriting of existing secret data. 
+If those checks succeed the secret is written to the file in the form described above.
+After that **gopass** encrypts the temporary file with the selected recipients and (if a remote is available) 
+pushes it to the server. 
+
+
+# Installing
+
+Now that you hopefully have a good understanding of how this tool works lets actually try it out!
+
+Since this package is not completely without dependencies it is recommended to install it inside a **virtualenv**:
+
+```bash
+python3 -m venv ~/path/to/new/virtual/environment
+source ~/path/to/new/virtual/environment/bin/activate
+pip3 install gopass-chrome-importer
+```
+
+# Usage
+
+For basic usage the `import` command should be all you ever need.
+**gopass-chrome-importer** has a couple of parameters to customize the import result
+and you should take a look what they offer before actually importing anything.
+For a quick overview call:
+
+```bash
+gopass-chrome-importer import --help
+```
+
+## Dry Run
+
+To test things out before actually importing passwords into **gopass** (and possibly 
+pushing them to origin) you can use the `-d` or `--dry-run` option. This will only output
+secret paths and contents where passwords are masked as asterisks to give you an idea of what would 
+happen.
+
+An example output would look similar to this:
+```text
+Would import: /import/ip/127.0.0.1/joe
+******
+---
+user: joe
+
+Would import: /import/ip/192.168.0.1/admin
+********************
+---
+user: admin
+```
+
+For safety **all** examples on this page will include this option. 
+
+
+## Set the path to the chrome password export .csv
+
+To set the path to the `export.csv` file just use the `-p` or `--path` option:
+
+```bash
+gopass-chrome-importer import --path "~/Downloads/Chrome Passwords.csv" --dry-run
+```
+
+## Changing the base import path
+
+By default **gopass-chrome-importer** will import secrets into an `import/` folder. To change this simply
+use the `-gb` or `--gopass-basepath` option:
+
+```bash
+gopass-chrome-importer import --path "~/Downloads/Chrome Passwords.csv" --gopass-basepath /test/ --dry-run
+```
+
+## --yes
+
+By default **gopass** will ask you some questions when creating a secret like which recipients to use for this secret.
+For large lists of passwords this can be quite a hassle. **gopass** has a built in `--yes` option to circumvent this 
+by always using "Y" or the default (if *yes* is not an option). This parameter can also be used (in addition to `-y`)
+in **gopass-chrome-importer** and will just pass it on to **gopass**.
+
+```bash
+gopass-chrome-importer import --path "~/Downloads/Chrome Passwords.csv" --gopass-basepath /test/ --yes --dry-run
+```
+
+## Overwrite existing passwords/file contents
+
+When **gopass-chrome-importer** tries to save a password in a file that
+already exists and that file is **not** empty it will (by default) **not change the file contents**
+to prevent accidentally overwriting of existing passwords.
+
+You can force **gopass-chrome-importer** to ignore this check by using the `-f` or `--force` option.
+
+Although your existing passwords are backed up in a git and (hopefully) synced to a 
+server side backend **be careful and think twice before using this option.**
+
+```bash
+gopass-chrome-importer import --path "~/Downloads/Chrome Passwords.csv" --gopass-basepath /test/ --yes --force --dry-run
+```
+
+# Contributing
+
+GitHub is for social coding: if you want to write code, I encourage contributions through pull requests from forks
+of this repository. Create GitHub tickets for bugs and new features and comment on the ones that you are interested in.
+
+
+# License
+
+```
+MIT License
+
+Copyright (c) 2018 Markus Ressel
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```
---
+++ packages/gopass-chrome-importer/gopass-chrome-importer-1.1.0.tar.gz/gopass-chrome-importer-1.1.0/gopass_chrome_importer/gopass_chrome_importer.py
@@ -0,0 +1,340 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+from enum import Enum
+
+import click
+
+from gopass_chrome_importer.summary.summary_manager import SummaryManager
+
+SUMMARY_TMP_FILE_ENV_VARIABLE_NAME = 'GOPASS_CHROME_IMPORTER_SUMMARY_TMP_FILE_PATH'
+SECRET_PATH_ENV_VARIABLE_NAME = 'GOPASS_CHROME_IMPORTER_STORE_SECRET_PATH'
+USERNAME_ENV_VARIABLE_NAME = 'GOPASS_CHROME_IMPORTER_STORE_USER'
+PASSWORD_ENV_VARIABLE_NAME = 'GOPASS_CHROME_IMPORTER_STORE_PASS'
+EDITOR_ENV_VARIABLE_NAME = 'EDITOR'
+IPV4_REGEX = "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
+
+
+class UrlType(Enum):
+    ANDROID = "android"
+    IP = "ip"
+    DOMAIN = "domain"
+
+
+def _run_shell_command(cmd: str, run_as: str = None) -> None:
+    """Run a shell command"""
+    if run_as is not None:
+        cmd = "sudo -u %s %s" % (run_as, cmd)
+
+    result = subprocess.call(cmd, shell=True)
+    if result is not 0:
+        raise ValueError("An error occurred")
+
+
+def _read_csv(path: str) -> [dict]:
+    """
+    Parses a chrome password export csv file to a list
+
+    :param path: the path of the csv file
+    :return: file parsed to a list
+    """
+    import csv
+
+    result = []
+
+    with open(path) as file:
+        reader = csv.reader(file, delimiter=',')
+        for row in reader:
+            result.append({
+                "name": row[0],
+                "url": row[1],
+                "username": row[2],
+                "password": row[3]
+            })
+
+    return result
+
+
+def _find_type(url: str) -> UrlType:
+    """
+    Checks the type of url, f.ex. a normal website, an IP Address or an android app specific url
+
+    :param url: the url to analyze
+    :return: the type of the url to insert
+    """
+
+    import re
+
+    if url.startswith("android://"):
+        return UrlType.ANDROID
+    elif re.match(IPV4_REGEX, url):
+        return UrlType.IP
+    else:
+        return UrlType.DOMAIN
+
+
+def _format_site(url: str) -> str:
+    """
+    Formats the given URL for the use as a secret file name
+
+    :param url: The URL to format
+    :return: the formatted name
+    """
+
+    url_type = _find_type(url)
+
+    result = url
+    if url_type is UrlType.IP or url_type is UrlType.DOMAIN:
+        result = result.replace('https://', '')
+        result = result.replace('http://', '')
+        result = result.replace('www.', '')
+
+        if url_type is UrlType.IP:
+            result = "ip/" + result
+        else:
+            result = "website/" + result
+
+    if url_type is UrlType.ANDROID:
+        import re
+        # strip away everything except app package name
+        matches = re.search(r"(?:==@)([\w\d.]*)", result)
+        if not matches:
+            echo("No match found: %s" % url, err=True)
+            exit(1)
+
+        result = "android/" + matches.group(1)
+
+    return result
+
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+CMD_STORE_INTERNAL = "store_internal"
+
+PARAM_PATH = "path"
+PARAM_GOPASS_PATH = "gopass-path"
+PARAM_FORCE = "force"
+PARAM_YES = "yes"
+PARAM_DRY_RUN = "dry-run"
+
+CMD_OPTION_NAMES = {
+    PARAM_PATH: ['--path', '-p'],
+    PARAM_GOPASS_PATH: ['--gopass-basepath', '-gb'],
+    PARAM_FORCE: ['--force', '-f'],
+    PARAM_YES: ['--yes', '-y'],
+    PARAM_DRY_RUN: ['--dry-run', '-d']
+}
+
+SUMMARY_MANAGER = SummaryManager()
+
+
+@click.group(context_settings=CONTEXT_SETTINGS)
+def cli():
+    pass
+
+
+def get_option_names(parameter: str) -> list:
+    """
+    Returns a list of all valid console parameter names for a given parameter
+    :param parameter: the parameter to check
+    :return: a list of all valid names to use this parameter
+    """
+    return CMD_OPTION_NAMES[parameter]
+
+
+@cli.command(name="import")
+@click.option(*get_option_names(PARAM_PATH), required=True, type=str,
+              help='Path to the chrome password export .csv file.')
+@click.option(*get_option_names(PARAM_GOPASS_PATH), required=False, type=str, default="imported/",
+              help='The path in gopass where all entries are imported to.')
+@click.option(*get_option_names(PARAM_FORCE), required=False, default=False, is_flag=True,
+              help='When set existing passwords will be overwritten. USE WITH CAUTION!')
+@click.option(*get_option_names(PARAM_YES), required=False, default=False, is_flag=True,
+              help='When set no questions will be asked during execution. '
+                   'This effectively sets the --yes flag on gopass. '
+                   'Note that this will NOT overwrite any existing data (see "-f" to do that)')
+@click.option(*get_option_names(PARAM_DRY_RUN), required=False, default=False, is_flag=True,
+              help='When set no passwords will actually be written and a preview of what WOULD be done will be printed.')
+def c_import(path: str, gopass_basepath: str, force: bool, yes: bool, dry_run: bool):
+    """
+    Imports items from a chrome password export
+
+    :param path: Path to the chrome password export .csv file
+    :param gopass_basepath: The base path to insert secrets into within gopass
+    :param force: If set to True existing secrets will be overwritten by imported data
+    :param yes: Automatically answer to gopass requests with "yes" if applicable (see gopass documentation)
+    :param dry_run: If set to True no changes will be made to the gopass store
+    """
+
+    # set custom "editor" that will process the password
+    editor_command = "gopass-chrome-importer %s" % CMD_STORE_INTERNAL
+    if force:
+        editor_command += " %s" % get_option_names(PARAM_FORCE)[0]
+    if dry_run:
+        echo("This is a dry run. Nothing will be changed.", warn=True)
+        editor_command += " %s" % get_option_names(PARAM_DRY_RUN)[0]
+    os.environ[EDITOR_ENV_VARIABLE_NAME] = editor_command
+
+    # set path to summary tmp file used for this run
+    os.environ[SUMMARY_TMP_FILE_ENV_VARIABLE_NAME] = SUMMARY_MANAGER.get_tmp_file_path()
+
+    # TODO: maybe we could use this to be able to only update existing secrets, without creating new ones
+    # dont know if this is an actual usecase though...
+    create_new = True
+
+    entries = _read_csv(path)
+
+    for entry in entries:
+        if entry.get("name") == "name" and entry.get("url") == "url" and entry.get(
+                "username") == "username" and entry.get("password") == "password":
+            # this is the header of the csv file and can be ignored
+            # it would be possible to just ignore the first line of the csv
+            # but you never know wo might think it's a good idea to delete that...
+            continue
+
+        if entry["name"]:
+            site = _format_site(entry["name"])
+        else:
+            site = _format_site(entry["url"])
+
+        if entry["username"]:
+            user = entry["username"]
+        else:
+            user = "site_pw"
+
+        secret_path = "%s%s/%s" % (gopass_basepath, site, user)
+        password = entry["password"]
+
+        # this command is a simple workaround to use gopass in a non-interactive way
+        # by using the edit command and a 'fake' editor that is this python script and
+        # stores the password in the temporarily decrypted file until
+        # gopass encrypts it.
+
+        # store final path to the secret in an env variable
+        os.environ[SECRET_PATH_ENV_VARIABLE_NAME] = secret_path
+        # store username
+        os.environ[USERNAME_ENV_VARIABLE_NAME] = user
+        # store password
+        os.environ[PASSWORD_ENV_VARIABLE_NAME] = password
+
+        # append the actual gopass command
+        gopass_command = "gopass"
+        if yes:
+            gopass_command += " --yes"
+        gopass_command += " edit"
+
+        if create_new:
+            gopass_command += " --create"
+
+        gopass_command += " '%s'" % secret_path
+
+        _run_shell_command(gopass_command)
+
+    SUMMARY_MANAGER.print_summary()
+
+
+def _create_secret_content(username: str or None, password: str, mask_pw: bool = False) -> str:
+    """
+    Creates the text that is written to a secret
+
+    :param username: a username, if present
+    :param password: the password
+    :param mask_pw: when set to true the password will be masked
+    :return: the secret file content
+    """
+    if mask_pw:
+        content = len(password) * '*'
+    else:
+        content = password
+
+    if username:
+        content += "\n---\n"
+        content += "user: %s" % username
+
+    return content
+
+
+def echo(text: str, info: bool = False, warn: bool = False, err: bool = False) -> None:
+    """
+    Simple wrapper for the click.echo function
+
+    :param text: the text to print
+    :param info: set to true, when this is a info message
+    :param warn: set to true, when this is a warning message
+    :param err: set to true, when this is an error message
+    """
+
+    if info:
+        foreground_color = 'green'
+    elif warn:
+        foreground_color = 'yellow'
+        SUMMARY_MANAGER.add_warning(text)
+    elif err:
+        foreground_color = 'red'
+        SUMMARY_MANAGER.add_error(text)
+    else:
+        foreground_color = 'white'
+
+    click.echo(click.style(text, fg=foreground_color), err=err)
+
+
+@cli.command(name=CMD_STORE_INTERNAL, hidden=True)
+@click.argument('file_path', required=True, type=str)
+@click.option(*get_option_names(PARAM_FORCE), required=False, default=False, is_flag=True,
+              help='When set to true existing passwords will be overwritten. USE WITH CAUTION!')
+@click.option(*get_option_names(PARAM_DRY_RUN), required=False, default=False, is_flag=True,
+              help='When set no passwords will actually be written and a preview of what WOULD be done will be printed.')
+def c_store_internal(file_path: str, force: bool, dry_run: bool):
+    """
+    NOTE: This is an internal command and not meant to be used from outside.
+    Stores a password in the given file path.
+
+    As it could be security critical to pass a password as a command line parameter this method uses
+    an environment variable instead.
+
+    :param file_path:
+    :param force: When set to true existing passwords will be overwritten. USE WITH CAUTION!
+    :param dry_run: When set no passwords will actually be written and a preview of what WOULD be done will be printed.
+    """
+
+    SUMMARY_MANAGER.set_tmp_file(os.environ[SUMMARY_TMP_FILE_ENV_VARIABLE_NAME])
+
+    final_secret_path = os.environ[SECRET_PATH_ENV_VARIABLE_NAME]
+
+    username = os.environ[USERNAME_ENV_VARIABLE_NAME]
+    password = os.environ[PASSWORD_ENV_VARIABLE_NAME]
+
+    secret_content = _create_secret_content(username, password)
+
+    # check if the file is empty
+    file_stats = os.stat(file_path)
+    if file_stats.st_size is not 0:
+        # check if existing content matches the one we are about to write
+        with open(file_path, 'r') as file:
+            existing_content = file.read()
+            if existing_content == secret_content:
+                echo("Non-empty secret with identical content ignored: %s" % final_secret_path, info=True)
+                return
+
+        if not force:
+            echo("Non-empty file with unequal content will NOT be overwritten: %s" % final_secret_path, warn=True)
+            return
+        else:
+            echo("Non-empty file with unequal content WILL BE overwritten: %s" % final_secret_path, warn=True)
+
+    if dry_run:
+        # just print what would be executed
+        secret_content = _create_secret_content(username, password, mask_pw=True)
+        echo("%s:\n%s\n" % (final_secret_path, secret_content), info=True)
+        SUMMARY_MANAGER.add_info("Would import: %s" % final_secret_path)
+        return
+    else:
+        with open(file_path, 'w') as file:
+            file.write(secret_content)
+        SUMMARY_MANAGER.add_info("Imported %s" % final_secret_path)
+        return
+
+
+if __name__ == '__main__':
+    cli()
