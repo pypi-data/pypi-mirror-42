3330564d3d4add5e9af741c79a54405f17f16ae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:41 +0000

    Add package scikit-lego/0.1.0/scikit_lego-0.1.0-py2.py3-none-any.whl

---
+++ packages/scikit-lego/scikit_lego-0.1.0-py2.py3-none-any.whl/tests/test_transformers/test_randomadder.py
@@ -0,0 +1,6 @@
+from sklego.transformers import RandomAdder
+
+
+def test_shape_does_not_change(random_xy_dataset):
+    X, y = random_xy_dataset
+    assert RandomAdder().fit(X, y).transform(X).shape == X.shape
