342d0f986d585ce4de24fa3f08a41805ff04014e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:51 +0000

    Add package edc-appointment/0.2.37/edc_appointment-0.2.37-py3-none-any.whl

---
+++ packages/edc-appointment/edc_appointment-0.2.37-py3-none-any.whl/edc_appointment/models/appointment.py
@@ -0,0 +1,29 @@
+from edc_base.model_managers import HistoricalRecords
+from edc_base.model_mixins import BaseUuidModel
+from edc_base.sites import CurrentSiteManager, SiteModelMixin
+
+from ..managers import AppointmentManager
+from ..model_mixins import AppointmentModelMixin
+
+
+class Appointment(AppointmentModelMixin, SiteModelMixin, BaseUuidModel):
+
+    on_site = CurrentSiteManager()
+
+    objects = AppointmentManager()
+
+    history = HistoricalRecords()
+
+    def natural_key(self):
+        return (
+            self.subject_identifier,
+            self.visit_schedule_name,
+            self.schedule_name,
+            self.visit_code,
+            self.visit_code_sequence,
+        )
+
+    natural_key.dependencies = ["sites.Site"]
+
+    class Meta(AppointmentModelMixin.Meta):
+        pass
