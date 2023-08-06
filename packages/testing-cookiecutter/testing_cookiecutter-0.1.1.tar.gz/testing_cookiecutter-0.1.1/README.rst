692dd52db139a7d9f966a890af48b7b7d5836757
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:39 +0000

    Add package testing-cookiecutter/0.1.1/testing_cookiecutter-0.1.1.tar.gz

---
+++ packages/testing-cookiecutter/testing_cookiecutter-0.1.1.tar.gz/testing_cookiecutter-0.1.1/README.rst
@@ -0,0 +1,32 @@
+testing_cookiecutter
+====================
+
+.. image:: https://img.shields.io/pypi/v/testing_cookiecutter.svg
+    :target: https://pypi.python.org/pypi/testing_cookiecutter
+    :alt: Latest PyPI version
+
+.. image:: https://travis-ci.org/borntyping/cookiecutter-pypackage-minimal.png
+   :target: https://travis-ci.org/borntyping/cookiecutter-pypackage-minimal
+   :alt: Latest Travis CI build status
+
+testing on an opinionated, minimal cookiecutter template for Python packages
+
+Usage
+-----
+
+Installation
+------------
+
+Requirements
+^^^^^^^^^^^^
+
+Compatibility
+-------------
+
+Licence
+-------
+
+Authors
+-------
+
+`testing_cookiecutter` was written by `Train Chen <train981316@gmail.com>`_.
---
+++ packages/testing-cookiecutter/testing_cookiecutter-0.1.1.tar.gz/testing_cookiecutter-0.1.1/setup.py
@@ -0,0 +1,42 @@
+import io
+import os
+import re
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(filename):
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    text_type = type(u"")
+    with io.open(filename, mode="r", encoding='utf-8') as fd:
+        return re.sub(text_type(r':[a-z]+:`~?(.*?)`'), text_type(r'``\1``'), fd.read())
+
+
+setup(
+    name="testing_cookiecutter",
+    version="0.1.1",
+    url="https://github.com/TrainTravel/testing_pypackaging.git",
+    license='MIT',
+
+    author="Train Chen",
+    author_email="train981316@gmail.com",
+
+    description="testing on an opinionated, minimal cookiecutter template for Python packages",
+    long_description=read("README.rst"),
+
+    packages=find_packages(exclude=('tests',)),
+
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        #'Programming Language :: Python :: 2',
+        #'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
