c70f0afe28beda9e19696e3fff06408c622f7084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:32 +0000

    Add package dss-python/0.10.2/dss_python-0.10.2-cp34-cp34m-macosx_10_6_x86_64.whl

---
+++ packages/dss-python/dss_python-0.10.2-cp34-cp34m-macosx_10_6_x86_64.whl/dss/v7.py
@@ -0,0 +1,16 @@
+'''A compatibility layer for DSS C-API V7 that mimics the official OpenDSS COM interface.
+
+Copyright (c) 2016-2018 Paulo Meira
+'''
+from __future__ import absolute_import
+from ._dss_capi_v7 import ffi, lib
+from ._cffi_api_util import CffiApiUtil, use_com_compat, DssException
+from . import dss_capi_gr, dss_capi_ir, enums
+from .enums import *
+
+# Bind to the FFI module instance for OpenDSS-v7
+api_util = CffiApiUtil(ffi, lib)
+
+DSS_GR = dss_capi_gr.IDSS(api_util)
+DSS_IR = dss_capi_ir.IDSS(api_util)
+DSS = DSS_GR
