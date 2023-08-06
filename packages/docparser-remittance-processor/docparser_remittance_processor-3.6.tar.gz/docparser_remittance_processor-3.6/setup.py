17f471d52f9baea47b8b9b8b1106782135f5a7f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:16 +0000

    Add package docparser-remittance-processor/3.6/docparser_remittance_processor-3.6.tar.gz

---
+++ packages/docparser-remittance-processor/docparser_remittance_processor-3.6.tar.gz/docparser_remittance_processor-3.6/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="docparser_remittance_processor",
+    version="3.6",
+    author="Hudge",
+    author_email="",
+    description="",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="",
+    py_modules=["docparser_remittance_processor"],
+    install_requires=["xlrd"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
