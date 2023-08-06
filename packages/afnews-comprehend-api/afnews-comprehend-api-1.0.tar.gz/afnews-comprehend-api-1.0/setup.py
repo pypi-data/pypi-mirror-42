6d5b146f3d893de66efe828f609886d10483dfd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:10 +0000

    Add package afnews-comprehend-api/1.0/afnews-comprehend-api-1.0.tar.gz

---
+++ packages/afnews-comprehend-api/afnews-comprehend-api-1.0.tar.gz/afnews-comprehend-api-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+# from distutils.core import setup  
+
+setup(
+        name='afnews-comprehend-api',     # 包名字
+        version='1.0',   # 包版本
+        description='handy funcs for aws python api',   # 简单描述
+        author='afnews',  # 作者
+        # packages=['detect'],                 # 包
+)
