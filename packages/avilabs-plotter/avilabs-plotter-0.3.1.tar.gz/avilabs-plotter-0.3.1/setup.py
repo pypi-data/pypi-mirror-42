6394d547892b448839704578e491915db28dd454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:25 +0000

    Add package avilabs-plotter/0.3.1/avilabs-plotter-0.3.1.tar.gz

---
+++ packages/avilabs-plotter/avilabs-plotter-0.3.1.tar.gz/avilabs-plotter-0.3.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md') as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='avilabs-plotter',
+    version='0.3.1',
+    description='Python plotting APIs',
+    long_description=long_description,
+    author='Avilay Parekh',
+    author_email='avilay@gmail.com',
+    license='MIT',
+    url='https://bitbucket.org/avilay/plotter',
+    packages=find_packages(),
+    install_requires=['matplotlib', 'numpy'],
+)
