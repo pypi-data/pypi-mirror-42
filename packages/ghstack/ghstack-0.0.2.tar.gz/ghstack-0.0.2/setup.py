7d29e01625fbe440140991cea4f4b016e626509f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:59 +0000

    Add package ghstack/0.0.2/ghstack-0.0.2.tar.gz

---
+++ packages/ghstack/ghstack-0.0.2.tar.gz/ghstack-0.0.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ghstack",
+    version="0.0.2",
+    author="Edward Z. Yang",
+    author_email="ezyang@mit.edu",
+    description="Stack diff support for GitHub",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/ghexport",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'ghstack = ghstack.__main__:main'
+        ]
+    },
+)
