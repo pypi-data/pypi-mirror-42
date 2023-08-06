06f827862c3777d344cec408019c023214fd99d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:18 +0000

    Add package lib3to6/201902.30/lib3to6-201902.30.tar.gz

---
+++ packages/lib3to6/lib3to6-201902.30.tar.gz/lib3to6-201902.30/src/lib3to6/__init__.py
@@ -0,0 +1,13 @@
+# This file is part of the lib3to6 project
+# https://gitlab.com/mbarkhau/lib3to6
+#
+# Copyright (c) 2019 Manuel Barkhau (mbarkhau@gmail.com) - MIT License
+# SPDX-License-Identifier: MIT
+
+from .packaging import fix
+from .transpile import transpile_module
+from .utils import parsedump_ast, parsedump_source
+
+__version__ = "v201902.0030"
+
+__all__ = ["fix", "transpile_module", "parsedump_ast", 'parsedump_source']
