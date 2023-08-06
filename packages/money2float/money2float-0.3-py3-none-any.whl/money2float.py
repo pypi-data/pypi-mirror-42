4b093da83314619e0b851afb2904b17849e24db5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:55 +0000

    Add package money2float/0.3/money2float-0.3-py3-none-any.whl

---
+++ packages/money2float/money2float-0.3-py3-none-any.whl/money2float.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+from decimal import *
+import re
+
+
+# In[9]:
+
+
+def money(text, places=2):
+    try:
+        text = text.replace(",", "")
+    except:
+        pass
+    try:
+        text = text.replace("$", "")
+    except:
+        pass
+    try:
+        response = float(round(Decimal(text), places))
+    except:
+        text = re.sub("[^\d\.]", "", text)
+        try:
+            response = float(round(Decimal(text), places))
+        except:
+            response = 0
+    return response
+
+
+# In[ ]:
