29c006679eaec125d593dbccc03a40e304f54721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:28 +0000

    Add package Products.PrintingMailHost/1.1.1/Products.PrintingMailHost-1.1.1.tar.gz

---
+++ packages/Products.PrintingMailHost/Products.PrintingMailHost-1.1.1.tar.gz/Products.PrintingMailHost-1.1.1/Products/PrintingMailHost/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from App.config import getConfiguration
+
+import logging
+import os
+
+LOG = None
+ENABLED = None
+FIXED_ADDRESS = []
+
+TRUISMS = ['yes', 'y', 'true', 'on']
+
+
+def initialize(context):
+    global LOG
+    global FIXED_ADDRESS
+    global ENABLED
+    LOG = logging.getLogger('PrintingMailHost')
+
+    ENABLED = os.environ.get('ENABLE_PRINTING_MAILHOST', None)
+    addresses = os.environ.get('PRINTING_MAILHOST_FIXED_ADDRESS', '')
+    FIXED_ADDRESS = [addr for addr in addresses.strip().split(' ') if addr]
+
+    # check to see if the environment var is set to a 'true' value
+    if (ENABLED is not None and ENABLED.lower() in TRUISMS) or \
+       (ENABLED is None and getConfiguration().debug_mode is True):
+        LOG.warning("Hold on to your hats folks, I'm a-patchin'")
+        from Products.PrintingMailHost import Patch
+        Patch  # pyflakes
