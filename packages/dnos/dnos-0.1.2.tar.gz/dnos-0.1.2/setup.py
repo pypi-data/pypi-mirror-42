9d7917a6c386251ef4a748ace681423baded62b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:51 +0000

    Add package dnos/0.1.2/dnos-0.1.2.tar.gz

---
+++ packages/dnos/dnos-0.1.2.tar.gz/dnos-0.1.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dnos",
+    version="0.1.2",
+    author="Adib Rastegarnia",
+    author_email="adib.rastegarnia@gmail.com",
+    description="Protobuf models for DNOS application",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dnosproject/dnos-core-grpc-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
