6a8dcb133cbc851a797e8d909d21087346bf3949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:33 +0000

    Add package alice-images/0.1.0/alice-images-0.1.0.tar.gz

---
+++ packages/alice-images/alice-images-0.1.0.tar.gz/alice-images-0.1.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='alice-images',
+    version='0.1.0',
+    author='Aleksandr Alekseev',
+    author_email='alekseevavx@gmail.com',
+    description='Work with alice skills images: upload|list|delete',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/AlekseevAV/alice-images',
+    packages=setuptools.find_packages(),
+    keywords='yandex alice images',
+    install_requires=[
+        'Click',
+        'requests'
+    ],
+    entry_points='''
+        [console_scripts]
+        alice_images=alice_images.cli:cli
+    ''',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
