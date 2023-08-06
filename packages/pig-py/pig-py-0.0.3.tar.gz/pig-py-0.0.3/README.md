41ab0cfa8525acaa900db8959d066ea1a83ee50d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:07 +0000

    Add package pig-py/0.0.3/pig-py-0.0.3.tar.gz

---
+++ packages/pig-py/pig-py-0.0.3.tar.gz/pig-py-0.0.3/README.md
@@ -0,0 +1,33 @@
+# Pig-Py
+
+Pig-py is a python console application that creates icon and splash images for android and ios platforms
+
+## Instalation
+```
+pip install pig-py
+```
+
+## Usage
+```
+pig [platform] [options]
+```
+
+> For more experience use 1024x1024 images for icons and 2732x2732 for splash
+
+#### Platform types
+* ionic
+* ios
+* android `to do`
+
+#### Options
+See options too using `pig --help`
+
+| Command | Required | Description |
+| ------- | -------- | ----------- |
+| `-f` `--file` | true | file name |
+| `-o` `--output` | false | destination folder of images |
+
+#### Example
+```
+pig ionic -f file.png
+```
---
+++ packages/pig-py/pig-py-0.0.3.tar.gz/pig-py-0.0.3/setup.py
@@ -0,0 +1,53 @@
+from setuptools import find_packages, setup
+
+dependencies = ['click', 'python-resize-image', 'Pillow']
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setup(
+  name='pig-py',
+  version='0.0.3',
+  author="Francisco Junior",
+  author_email="fcojunr@gmail.com",
+  description="A package for generate icons for Ionic",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/FcoJunior/pig-py",
+  packages=find_packages(),
+  include_package_data=True,
+  zip_safe=False,
+  platforms='any',
+  install_requires=dependencies,
+  data_files=[('pig/dimensions', [
+    'pig/dimensions/ionic.dim.json',
+    'pig/dimensions/ios.dim.json',
+  ])],
+  entry_points={
+    'console_scripts': [
+      'pig = pig.cli:main',
+    ],
+  },
+  classifiers=[
+    # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    # 'Development Status :: 1 - Planning',
+    # 'Development Status :: 2 - Pre-Alpha',
+    # 'Development Status :: 3 - Alpha',
+    'Development Status :: 4 - Beta',
+    # 'Development Status :: 5 - Production/Stable',
+    # 'Development Status :: 6 - Mature',
+    # 'Development Status :: 7 - Inactive',
+    # 'Environment :: Console',
+    # 'Intended Audience :: Developers',
+    # 'License :: OSI Approved :: BSD License',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: POSIX',
+    'Operating System :: MacOS',
+    'Operating System :: Unix',
+    'Operating System :: Microsoft :: Windows',
+    # 'Programming Language :: Python',
+    # 'Programming Language :: Python :: 2',
+    # 'Programming Language :: Python :: 3',
+    # 'Topic :: Software Development :: Libraries :: Python Modules',
+  ]
+)
