64e2a76ff8e382faff9134950ce066974e9cb557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:42 +0000

    Add package dirutility/0.5.6/dirutility-0.5.6.tar.gz

---
+++ packages/dirutility/dirutility-0.5.6.tar.gz/dirutility-0.5.6/dirutility/__init__.py
@@ -0,0 +1,10 @@
+from dirutility.move import FlattenTree, CreateTree, move_files_to_folders
+from dirutility.walk import DirPaths, DirTree
+from dirutility.view import desktop, open_window
+from dirutility.backup import ZipBackup
+from dirutility.ftp import FTP
+from dirutility.permissions import Permissions
+
+
+__all__ = ['FlattenTree', 'CreateTree', 'move_files_to_folders', 'DirTree', 'DirPaths', 'desktop', 'ZipBackup', 'FTP',
+           'open_window', 'Permissions']
