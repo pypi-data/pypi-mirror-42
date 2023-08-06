97b02b98c496b4264bc2c1f7a284d8309cc45915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:58 +0000

    Add package epcpm/2019.2.3rc0/epcpm-2019.2.3rc0-py3-none-any.whl

---
+++ packages/epcpm/epcpm-2019.2.3rc0-py3-none-any.whl/epcpm/__build.py
@@ -0,0 +1,9 @@
+# This file has been generated
+
+
+build_system = 'AppVeyor'
+build_id = '22817607'
+build_number = '371'
+build_version = '1.0.371'
+job_id = 'j6mtdjgqjk8lxtcc'
+job_url = 'https://ci.appveyor.com/project/KyleAltendorf/pm/build/job/j6mtdjgqjk8lxtcc'
