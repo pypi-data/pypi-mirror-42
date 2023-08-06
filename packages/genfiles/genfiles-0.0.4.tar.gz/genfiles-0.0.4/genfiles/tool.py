87d2cc2df7699d4a1a5bca66db34d82b8e148356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:24 +0000

    Add package genfiles/0.0.4/genfiles-0.0.4.tar.gz

---
+++ packages/genfiles/genfiles-0.0.4.tar.gz/genfiles-0.0.4/genfiles/tool.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2.7
+#coding:utf-8
+
+import sys
+import genfiles
+
+def main():
+  args = []
+  for i in range(2, len(sys.argv)):
+    args.append(sys.argv[i])
+
+  getattr(sys.modules[__name__], sys.argv[1])(args)
+
+#args: templatePath, dataPath=None, outputPath=None
+def gf_generate (args):
+  #print("executing genfiles " + str(args))
+  gf = genfiles.GenFiles()
+  gf.generate(*args)
+
+if __name__ == '__main__':
+    main()
