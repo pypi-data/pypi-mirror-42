a963c1635b84c19aa13aa37f9b4e26bf7003447a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:50 +0000

    Add package ByteSplitter/1.0.4/ByteSplitter-1.0.4.tar.gz

---
+++ packages/ByteSplitter/ByteSplitter-1.0.4.tar.gz/ByteSplitter-1.0.4/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='ByteSplitter',
+    version='1.0.4',
+    author='AivanF.',
+    author_email='projects@aivanf.com',
+    description='A tiny Python utility for splitting any binary files into parts and combining them back',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/AivanF/ByteSplitter',
+    packages=['bysp'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Information Technology',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Environment :: Console',
+        'Topic :: Utilities',
+        'Topic :: Security',
+        'Topic :: System :: Filesystems',
+        'License :: Freely Distributable',
+    ],
+    entry_points={
+        'console_scripts': ['bysp=bysp:main'],
+    },
+)
