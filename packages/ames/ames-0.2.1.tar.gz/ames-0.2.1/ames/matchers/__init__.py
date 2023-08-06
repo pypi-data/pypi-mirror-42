8efe030c4afed4b26d882aef05ea1e9d491db76d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:33 +0000

    Add package ames/0.2.1/ames-0.2.1.tar.gz

---
+++ packages/ames/ames-0.2.1.tar.gz/ames-0.2.1/ames/matchers/__init__.py
@@ -0,0 +1,6 @@
+from .caltechdata import match_cd_refs
+from .caltechdata import match_codemeta
+from .caltechdata import fix_multiple_links
+from .caltechdata import add_citation
+from .datacite import update_datacite_metadata
+from .datacite import update_datacite_media
