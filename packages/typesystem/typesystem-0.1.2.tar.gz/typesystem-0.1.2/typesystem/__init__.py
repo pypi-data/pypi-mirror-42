534a12ea60b73cec0c2a997cc6b77a610003a530
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:53 +0000

    Add package typesystem/0.1.2/typesystem-0.1.2.tar.gz

---
+++ packages/typesystem/typesystem-0.1.2.tar.gz/typesystem-0.1.2/typesystem/__init__.py
@@ -0,0 +1,37 @@
+from typesystem.forms import Jinja2Forms
+from typesystem.schemas import Schema
+from typesystem.fields import (
+    Boolean,
+    Choice,
+    Date,
+    DateTime,
+    Decimal,
+    Field,
+    Float,
+    Integer,
+    Nested,
+    Object,
+    String,
+    Text,
+    Time,
+)
+
+
+__version__ = "0.1.2"
+__all__ = [
+    "Boolean",
+    "Choice",
+    "Date",
+    "DateTime",
+    "Decimal",
+    "Integer",
+    "Jinja2Forms",
+    "Field",
+    "Float",
+    "Nested",
+    "Object",
+    "Schema",
+    "String",
+    "Text",
+    "Time",
+]
