2f1b020c5e91f26bab39674c6f030e2a20681035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:44 +0000

    Add package list-files-py/1.0.0.dev1/list-files-py-1.0.0.dev1.tar.gz

---
+++ packages/list-files-py/list-files-py-1.0.0.dev1.tar.gz/list-files-py-1.0.0.dev1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='list-files-py',
+    version='1.0.0.dev1',
+    description='A list file python project',
+    url='',
+    author='dong jian',
+    author_email='',
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='list file python',
+    packages=['list'],
+)
+
