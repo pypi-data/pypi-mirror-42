34a4ffae7eff636049614e1a25034e91a66567e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:11 +0000

    Add package smamp/0.33/smamp-0.33-py3-none-any.whl

---
+++ packages/smamp/smamp-0.33-py3-none-any.whl/smamp/__init__.py
@@ -0,0 +1,11 @@
+"""SMAMP module."""
+
+# Modules by Johannes
+from . import insertHbyList
+
+
+# Modules by Lukas
+from . import average_cost
+from . import charges_to_rft
+from . import tools
+from . import convert_UA_to_AA
