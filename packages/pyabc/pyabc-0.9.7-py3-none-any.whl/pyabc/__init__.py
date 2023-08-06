6b5b90d2b90e4fe26a04719a066efafb31f08539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:40 +0000

    Add package pyabc/0.9.7/pyabc-0.9.7-py3-none-any.whl

---
+++ packages/pyabc/pyabc-0.9.7-py3-none-any.whl/pyabc/__init__.py
@@ -0,0 +1,104 @@
+"""
+Approximate Bayesian computation - Sequential Monte Carlo
+=========================================================
+
+ABCSMC algorithms for Bayesian model selection.
+"""
+
+
+import os
+import logging
+
+
+from .parameters import Parameter
+from .random_variables import (Distribution,
+                               ModelPerturbationKernel,
+                               RV,
+                               RVBase,
+                               RVDecorator,
+                               LowerBoundDecorator)
+from .distance_functions import (DistanceFunction,
+                                 NoDistance,
+                                 SimpleFunctionDistance,
+                                 PNormDistance,
+                                 AdaptivePNormDistance,
+                                 ZScoreDistanceFunction,
+                                 PCADistanceFunction,
+                                 MinMaxDistanceFunction,
+                                 PercentileDistanceFunction,
+                                 RangeEstimatorDistanceFunction,
+                                 DistanceFunctionWithMeasureList)
+from .epsilon import (Epsilon,
+                      ConstantEpsilon,
+                      QuantileEpsilon,
+                      MedianEpsilon,
+                      ListEpsilon)
+from .smc import ABCSMC
+from .storage import History
+from .acceptor import (Acceptor,
+                       SimpleAcceptor,
+                       accept_use_current_time,
+                       accept_use_complete_history)
+from .model import (Model,
+                    SimpleModel,
+                    ModelResult,
+                    IntegratedModel)
+from .transition import (MultivariateNormalTransition,
+                         LocalTransition)
+from .populationstrategy import (AdaptivePopulationSize,
+                                 ConstantPopulationSize)
+from .transition import GridSearchCV
+from .version import __version__  # noqa: F401
+
+__all__ = [
+    "ABCSMC",
+    # Distance start
+    "DistanceFunction",
+    "NoDistance",
+    "SimpleFunctionDistance",
+    "PNormDistance",
+    "AdaptivePNormDistance",
+    "ZScoreDistanceFunction",
+    "PCADistanceFunction",
+    "MinMaxDistanceFunction",
+    "PercentileDistanceFunction",
+    "RangeEstimatorDistanceFunction",
+    "DistanceFunctionWithMeasureList",
+    # Distance end
+    "Epsilon",
+    "ConstantEpsilon",
+    "ListEpsilon",
+    "QuantileEpsilon",
+    "MedianEpsilon",
+    # random_variables start
+    "RVBase",
+    "RV",
+    "RVDecorator",
+    "LowerBoundDecorator",
+    "Distribution",
+    "ModelPerturbationKernel",
+    # random_variables end
+    "Parameter",
+    "GridSearchCV",
+    "ConstantPopulationSize",
+    "AdaptivePopulationSize",
+    "MultivariateNormalTransition",
+    "LocalTransition",
+    "Acceptor",
+    "SimpleAcceptor",
+    "accept_use_current_time",
+    "accept_use_complete_history",
+    "ModelResult",
+    "Model",
+    "SimpleModel",
+    "IntegratedModel",
+    "History"
+]
+
+
+try:
+    loglevel = os.environ["ABC_LOG_LEVEL"].upper()
+except KeyError:
+    loglevel = "INFO"
+
+logging.basicConfig(level=loglevel)
