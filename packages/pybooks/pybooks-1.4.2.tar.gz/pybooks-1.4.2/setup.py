010783e674625239ccf8980fae6ec8b4d1868570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:17 +0000

    Add package pybooks/1.4.2/pybooks-1.4.2.tar.gz

---
+++ packages/pybooks/pybooks-1.4.2.tar.gz/pybooks-1.4.2/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='pybooks',
+    packages=['pybooks'],
+    version='1.4.2',
+    license='MIT',
+    description='Find a book download URL from multiple online book sources',
+    author='Anh Van Giang',
+    author_email='vangianganh@gmail.com',
+    url='https://github.com/AnhVanGiang/pybooks',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    download_url='https://github.com/AnhVanGiang/pybooks/archive/v_14.tar.gz',
+    package_data={'pybooks': ['sources.json', 'pbook.exe']},
+    include_package_data=True,
+    keywords=['scraper', 'books', 'python'],
+    install_requires=[
+        'requests',
+        'bs4',
+        'lxml',
+        'sdebugger'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
