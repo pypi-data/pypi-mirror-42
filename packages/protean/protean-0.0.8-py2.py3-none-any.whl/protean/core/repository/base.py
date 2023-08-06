6b52bbf6f6ca8a1e3049fe2f338d29a9c46bf56e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:19 +0000

    Add package protean/0.0.8/protean-0.0.8-py2.py3-none-any.whl

---
+++ packages/protean/protean-0.0.8-py2.py3-none-any.whl/protean/core/repository/base.py
@@ -0,0 +1,148 @@
+""" Define the interfaces for Repository implementations """
+import logging
+from abc import ABCMeta
+from abc import abstractmethod
+from typing import Any
+
+from protean.core.entity import Entity
+from protean.core.exceptions import ConfigurationError
+from protean.utils import inflection
+from protean.utils.meta import OptionsMeta
+from protean.utils.query import RegisterLookupMixin
+
+from .pagination import Pagination
+
+logger = logging.getLogger('protean.repository')
+
+
+class BaseAdapter(RegisterLookupMixin, metaclass=ABCMeta):
+    """Adapter interface to interact with databases
+
+    :param conn: A connection/session to the data source of the model
+    :param model_cls: The model class registered with this repository
+    """
+
+    def __init__(self, conn, model_cls):
+        self.conn = conn
+        self.model_cls = model_cls
+        self.entity_cls = model_cls.opts_.entity_cls
+        self.model_name = model_cls.opts_.model_name
+
+    def _extract_lookup(self, key):
+        """Extract lookup method based on key name format"""
+        parts = key.split('__')
+        # 'exact' is the default lookup if there was no explicit comparison op in `key`
+        #   Assume there is only one `__` in the key.
+        #   FIXME Change for child attribute query support
+        op = 'exact' if len(parts) == 1 else parts[1]
+
+        # Construct and assign the lookup class as a filter criteria
+        return (parts[0], self.get_lookup(op))
+
+    @abstractmethod
+    def _filter_objects(self, page: int = 1, per_page: int = 10,
+                        order_by: list = (), excludes_: dict = None,
+                        **filters) -> Pagination:
+        """
+        Filter objects from the repository. Method must return a `Pagination`
+        object
+        """
+
+    @abstractmethod
+    def _create_object(self, model_obj: Any):
+        """Create a new model object from the entity"""
+
+    @abstractmethod
+    def _update_object(self, model_obj: Any):
+        """Update a model object in the repository and return it"""
+
+    @abstractmethod
+    def _delete_objects(self, **filters):
+        """Delete a Record from the Repository"""
+
+
+class ModelOptions(object):
+    """class Meta options for the :class:`BaseModel`."""
+
+    def __init__(self, meta, model_cls):
+        self.entity_cls = getattr(meta, 'entity', None)
+        if not self.entity_cls or not issubclass(self.entity_cls, Entity):
+            raise ConfigurationError(
+                '`entity` option must be set and be a subclass of `Entity`.')
+
+        # Get the model name to be used, if not provided default it
+        self.model_name = getattr(meta, 'model_name', None)
+        if not self.model_name:
+            self.model_name = inflection.underscore(model_cls.__name__)
+
+        # Get the database bound to this model
+        self.bind = getattr(meta, 'bind', 'default')
+
+        # Default ordering of the filter response
+        self.order_by = getattr(meta, 'order_by', ())
+
+
+class BaseModel(metaclass=OptionsMeta):
+    """Model that defines an index/table in the repository"""
+    options_cls = ModelOptions
+    opts_ = None
+
+    class Meta(object):
+        """Options object for a Model.
+        Example usage: ::
+            class Meta:
+                entity = Dog
+        Available options:
+        - ``base``: Indicates that this is a base model so ignore the meta
+        - ``entity``: the entity associated with this model.
+        - ``model_name``: name of this model that will be used as table/index
+        names, defaults to underscore version of the class name.
+        - ``bind``: the name of the repository connection associated with this
+        model, default value is `default`.
+        - ``order_by``: default ordering of objects returned by filter queries.
+        """
+        base = True
+
+    @classmethod
+    def from_entity(cls, entity):
+        """Initialize Repository Model object from Entity object"""
+        raise NotImplementedError()
+
+    @classmethod
+    def to_entity(cls, *args, **kwargs):
+        """Convert Repository Model Object to Entity Object"""
+        raise NotImplementedError()
+
+
+class BaseConnectionHandler(metaclass=ABCMeta):
+    """ Interface to manage connections to the database """
+
+    @abstractmethod
+    def get_connection(self):
+        """ Get the connection object for the repository"""
+
+    @abstractmethod
+    def close_connection(self, conn):
+        """ Close the connection object for the repository"""
+
+
+class Lookup(metaclass=ABCMeta):
+    """Base Lookup class to implement in Adapters"""
+    lookup_name = None
+
+    def __init__(self, source, target):
+        """Source is LHS and Target is RHS of a comparsion"""
+        self.source, self.target = source, target
+
+    def process_source(self):
+        """Blank implementation; returns source"""
+        return self.source
+
+    def process_target(self):
+        """Blank implementation; returns target"""
+        return self.target
+
+    @abstractmethod
+    def as_expression(self):
+        """To be implemented in each Adapter for its Lookups"""
+        raise NotImplementedError
---
+++ packages/protean/protean-0.0.8-py2.py3-none-any.whl/protean/core/repository/factory.py
@@ -0,0 +1,132 @@
+""" Factory class for managing repository connections"""
+import importlib
+import logging
+from threading import local
+
+from protean.conf import active_config
+from protean.core.exceptions import ConfigurationError
+
+from .base import BaseAdapter
+from .base import BaseModel
+
+logger = logging.getLogger('protean.repository')
+
+
+class RepositoryFactory:
+    """Repository Factory interface to retrieve Resource Objects from Repositories
+
+    FIXME RepositoryFactory should prepare adapter objects JIT, so that connections
+        are kept open only for the necessary duration. Once the request is handled, it should
+        be let go.
+    """
+
+    def __init__(self):
+        """"Initialize repository factory"""
+        self._registry = {}
+        self._entity_registry = {}
+        self._model_registry = {}
+        self._connections = local()
+        self._repositories = None
+
+    @property
+    def repositories(self):
+        """ Return the databases configured for the application"""
+        if self._repositories is None:
+            self._repositories = active_config.REPOSITORIES
+
+        if not isinstance(self._repositories, dict) or self._repositories == {}:
+            raise ConfigurationError(
+                "'REPOSITORIES' config must be a dict and at least one "
+                "database must be defined")
+
+        if 'default' not in self._repositories:
+            raise ConfigurationError(
+                "You must define a 'default' repository")
+
+        return self._repositories
+
+    @property
+    def connections(self):
+        """ Return the registered repository connections"""
+        try:
+            return self._connections.connections
+        except AttributeError:
+            self._connections.connections = {}
+            return self._connections.connections
+
+    def register(self, model_cls, repo_cls=None):
+        """ Register the given model with the factory
+        :param model_cls: class of the model to be registered
+        :param repo_cls: Optional repository class to use if not the
+        `Repository` defined by the provider is userd
+        """
+        if not issubclass(model_cls, BaseModel):
+            raise AssertionError(
+                f'Model {model_cls} must be subclass of `BaseModel`')
+
+        if repo_cls and not issubclass(repo_cls, BaseAdapter):
+            raise AssertionError(
+                f'Repository {repo_cls} must be subclass of `BaseAdapter`')
+
+        # Register the model if it does not exist
+        model_name = model_cls.__name__
+        entity_name = model_cls.Meta.entity.__name__
+        if not self._registry.get(entity_name):
+            # Lookup the connection details for the model
+            try:
+                conn_info = self.repositories[model_cls.opts_.bind]
+            except KeyError:
+                raise ConfigurationError(
+                    f"'{model_cls.opts_.bind}' repository not found in "
+                    f"'REPOSITORIES'")
+
+            # Load the repository provider
+            provider = importlib.import_module(conn_info['PROVIDER'])
+
+            # If no connection exists then build it
+            if model_cls.opts_.bind not in self.connections:
+                conn_handler = provider.ConnectionHandler(
+                    model_cls.opts_.bind, conn_info)
+                self._connections.connections[model_cls.opts_.bind] = \
+                    conn_handler.get_connection()
+
+            # Finally register the model against the provider repository
+            repo_cls = repo_cls or provider.Adapter
+            self._registry[entity_name] = \
+                repo_cls(self.connections[model_cls.opts_.bind], model_cls)
+            self._model_registry[entity_name] = model_cls
+            self._entity_registry[entity_name] = model_cls.Meta.entity
+            logger.debug(
+                f'Registered model {model_name} for entity {entity_name} with repository provider '
+                f'{conn_info["PROVIDER"]}.')
+
+    def get_model(self, entity_name):
+        """Retrieve Model class connected to Entity"""
+        try:
+            return self._model_registry[entity_name]
+        except KeyError:
+            raise AssertionError('No Model registered for {entity_name}'.format(entity_name))
+
+    def get_entity(self, entity_name):
+        """Retrieve Entity class registered by `entity_name`"""
+        try:
+            return self._entity_registry[entity_name]
+        except KeyError:
+            raise AssertionError('No Entity registered with name {entity_name}'.format(entity_name))
+
+    def __getattr__(self, entity_name):
+        try:
+            return self._registry[entity_name]
+        except KeyError:
+            raise AssertionError('No Model registered for {entity_name}'.format(entity_name))
+
+    def close_connections(self):
+        """ Close all connections registered with the repository """
+        for conn_name, conn_obj in self.connections.items():
+            conn_info = self.repositories[conn_name]
+            provider = importlib.import_module(conn_info['PROVIDER'])
+            conn_handler = provider.ConnectionHandler(conn_name, conn_info)
+            conn_handler.close_connection(conn_obj)
+
+
+repo_factory = RepositoryFactory()
