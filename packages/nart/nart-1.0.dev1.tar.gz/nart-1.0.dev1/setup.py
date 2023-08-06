7662972b71aaa7ba8455a91953dfbeb7448f4848
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:52 +0000

    Add package nart/1.0.dev1/nart-1.0.dev1.tar.gz

---
+++ packages/nart/nart-1.0.dev1.tar.gz/nart-1.0.dev1/setup.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='nart',
+    version='1.0.dev1',
+    description='NART: Naver RealTime Keyword',
+    author='Keunhyun Oh',
+    author_email='ocworld@gmail.com',
+    url='https://github.com/ocworld/nart',
+    packages=find_packages(exclude=('tests', 'docs')),
+    platforms=['any'],
+    install_requires=[
+        'requests==2.21.0',
+        'schedule==0.6.0',
+        'beautifulsoup4==4.7.1',
+        'python-crontab==2.3.6',
+        'schedule==0.6.0',
+    ],
+    setup_requires=[
+        'pytest-runner==4.4',
+    ],
+    tests_require=[
+        'pytest==4.3.0',
+    ],
+    entry_points={
+        'console_scripts': ['nart=nart.main:main'],
+    },
+    classifiers={
+        'Programming Language :: Python',
+        'Environment :: Console',
+        'Environment :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Office/Business :: News/Diary',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    },
+    project_url={
+        'Bug Reports': 'https://github.com/ocworld/nart/issues',
+        'Source': 'https://github.com/ocworld/nart'
+    }
+)
