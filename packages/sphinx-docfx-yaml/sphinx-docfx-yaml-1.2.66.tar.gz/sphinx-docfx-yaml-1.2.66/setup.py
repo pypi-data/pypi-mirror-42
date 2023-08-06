f369145a89c63857df90460e8711af6585b46cab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:21 +0000

    Add package sphinx-docfx-yaml/1.2.66/sphinx-docfx-yaml-1.2.66.tar.gz

---
+++ packages/sphinx-docfx-yaml/sphinx-docfx-yaml-1.2.66.tar.gz/sphinx-docfx-yaml-1.2.66/setup.py
@@ -0,0 +1,27 @@
+import codecs
+from setuptools import setup, find_packages
+extra_setup = dict(
+install_requires=[
+    'PyYAML',
+    'wheel>=0.24.0',
+    'sphinx==1.5.5',
+    'unidecode',
+],
+setup_requires=['pytest-runner'],
+tests_require=['pytest', 'mock'],
+)
+
+setup(
+    name='sphinx-docfx-yaml',
+    version='1.2.66',
+    author='Eric Holscher',
+    author_email='eric@ericholscher.com',
+    url='https://github.com/ericholscher/sphinx-docfx-yaml',
+    description='Sphinx Python Domain to DocFX YAML Generator',
+    package_dir={'': '.'},
+    packages=find_packages('.', exclude=['tests']),
+    long_description=codecs.open("README.rst", "r", "utf-8").read(),
+    # trying to add files...
+    include_package_data=True,
+    **extra_setup
+)
