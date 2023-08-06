301610d7c0734f5597124d9f3e766c36795bf961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:23 +0000

    Add package cherry-rl/0.0.6.1/cherry-rl-0.0.6.1.tar.gz

---
+++ packages/cherry-rl/cherry-rl-0.0.6.1.tar.gz/cherry-rl-0.0.6.1/cherry/distributions.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import torch as th
+from torch.distributions import Distribution, Normal
+
+
+class TanhNormal(Distribution):
+
+    """
+    Adapted from Vitchyr Pong's RLkit:
+    https://github.com/vitchyr/rlkit/blob/master/rlkit/torch/distributions.py
+    """
+
+    def __init__(self, normal_mean, normal_std):
+        self.normal_mean = normal_mean
+        self.normal_std = normal_std
+        self.normal = Normal(normal_mean, normal_std)
+
+    def sample_n(self, n):
+        z = self.normal.sample_n(n)
+        return th.tanh(z)
+
+    def log_prob(self, value):
+        pre_tanh_value = (th.log1p(value) - th.log1p(-value)).mul(0.5)
+        offset = th.log1p(-value**2 + 1e-6)
+        return self.normal.log_prob(pre_tanh_value) - offset
+
+    def sample(self):
+        z = self.normal.sample().detach()
+        return th.tanh(z)
+
+    def sample_and_log_prob(self):
+        z = self.normal.sample().detach()
+        value = th.tanh(z)
+        offset = th.log1p(-value**2 + 1e-6)
+        log_prob = self.normal.log_prob(z) - offset
+        return value, log_prob
+
+    def rsample_and_log_prob(self):
+        z = self.normal.rsample()
+        value = th.tanh(z)
+        offset = th.log1p(-value**2 + 1e-6)
+        log_prob = self.normal.log_prob(z) - offset
+        return value, log_prob
+
+    def rsample(self):
+        z = self.normal.rsample()
+        z.requires_grad_()
+        return th.tanh(z)
+
---
+++ packages/cherry-rl/cherry-rl-0.0.6.1.tar.gz/cherry-rl-0.0.6.1/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from setuptools import (
+        setup,
+        find_packages,
+        )
+
+VERSION = '0.0.6.1'
+
+setup(
+    name='cherry-rl',
+    packages=find_packages(),
+    version=VERSION,
+    description='PyTorch Reinforcement Learning Framework for Researchers',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Seb Arnold',
+    author_email='smr.arnold@gmail.com',
+    url = 'https://seba-1511.github.com/cherry',
+    download_url = 'https://github.com/seba-1511/cherry/archive/' + str(VERSION) + '.zip',
+    license='License :: OSI Approved :: Apache Software License',
+    classifiers=[],
+    scripts=[],
+    install_requires=[
+        'numpy>=1.15.4',
+        'gym>=0.10.9',
+        'torch>=1.0.0',
+    ],
+)
