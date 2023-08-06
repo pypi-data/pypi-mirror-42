616700fa4d0e65c4650d2dbd9e3cbb05526e9e7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:23 +0000

    Add package django-support-lite/0.2.6/django_support_lite-0.2.6-py3-none-any.whl

---
+++ packages/django-support-lite/django_support_lite-0.2.6-py3-none-any.whl/django_support_lite/models/ticket.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+from django_support_lite.enums import Priority, ResponseType, Status
+
+
+class TicketManager(models.Manager):
+    def count_open(self, user=None):
+        query = super().get_queryset().filter(status=Status.OPEN)
+
+        if user:
+            query = query.filter(user=user)
+
+        return query.count()
+
+    def count_pending(self, is_manager=True):
+        return super().get_queryset().filter(response_type=ResponseType.USER_RESPONSE).count()
+
+class Ticket(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, related_name='user', on_delete=models.CASCADE)
+    user_close = models.ForeignKey(User, related_name='user_close', null=True, on_delete=models.CASCADE)
+    title = models.CharField(max_length=300)
+    response_type = models.IntegerField(choices=ResponseType.choices())
+    status = models.IntegerField(choices=Status.choices())
+    priority = models.IntegerField(choices=Priority.choices())
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = TicketManager()
