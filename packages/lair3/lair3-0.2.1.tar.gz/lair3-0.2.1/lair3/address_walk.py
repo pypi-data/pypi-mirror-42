71fa7be226bc681e12dc21607a962b84520a2afa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:33 +0000

    Add package lair3/0.2.1/lair3-0.2.1.tar.gz

---
+++ packages/lair3/lair3-0.2.1.tar.gz/lair3-0.2.1/lair3/address_walk.py
@@ -0,0 +1,322 @@
+import copy
+import re
+import socket
+try:
+    import sockets
+except ImportError:
+    from . import sockets
+
+class SubnetWalker(object):
+    def __init__(self, subnet, netmask):
+        self.subnet = sockets.resolv_to_dotted(subnet)
+        self.netmask = sockets.resolv_to_dotted(netmask)
+        self.num_ips = None
+        self.curr_ip = None
+        self.curr_ip_idx = None
+        
+        self.reset()
+        
+    def reset(self):
+        self.curr_ip = self.subnet.split('.')
+        self.num_ips = (1 << (32 - int(sockets.net2bitmask(self.netmask))))
+        self.curr_ip_idx = 0
+        
+    def next_ip(self):
+        if (self.curr_ip_idx >= self.num_ips):
+            return None
+            
+        if (self.curr_ip_idx > 0):
+            self.curr_ip[3] = (int(self.curr_ip[3]) + 1) % 256
+            if self.curr_ip[3] == 0:
+                self.curr_ip[2] = (int(self.curr_ip[2]) + 1) % 256
+            if self.curr_ip[2] == 0:
+                self.curr_ip[1] = (int(self.curr_ip[1]) + 1) % 256
+            if self.curr_ip[1] == 0:
+                self.curr_ip[0] = (int(self.curr_ip[0]) + 1) % 256
+                
+        self.curr_ip_idx += 1
+        
+        return '.'.join(map(str, self.curr_ip))
+            
+
+class IpRange(object):
+    def __init__(self, start = None, stop = None, **kwargs):
+        self.start = start
+        self.stop = stop
+        self.options = kwargs
+        
+    def __eq__(self, other):
+        return (other.start == self.start and other.stop == self.stop and other.is_ipv6 == self.is_ipv6 and other.options == self.options)
+        
+    def __len__(self):
+        return (self.stop - self.start + 1)
+        
+    @property
+    def is_ipv6(self):
+        return ('ipv6' in self.options and self.options['ipv6'] == True)
+        
+
+class IpRangeWalker(object):
+    def __init__(self, parse_obj):
+        self.curr_range_index = 0
+        self.curr_addr = None
+        self.length = 0
+        
+        # copy constructor
+        if isinstance(parse_obj, IpRangeWalker) == True:
+            self.ranges = copy.copy(parse_obj.ranges)
+        else:
+            self.ranges = self.parse(parse_obj)
+            
+        self.reset()
+
+    def __len__(self):
+        return self.length
+        
+    def parse(self, parse_obj):
+        if parse_obj is None:
+            return None
+        ranges = []
+        for arg in [y for x in map(lambda a: a.split(' '), parse_obj.split(', ')) for y in x]:
+            opts = {}
+            
+            v4range_mach = re.search(r'^([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})-([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})$', arg)
+            if ":" in arg:
+                addrs = arg.split('-', 1)
+                
+                if len(addrs) == 1:
+                    try:
+                        addr, scope_id = addrs[0].split('%')
+                    except ValueError:
+                        addr = addrs[0].split('%')[0]
+                        scope_id = None
+                        
+                    if scope_id is not None:
+                        opts['scope_id'] = scope_id
+                        
+                    opts['ipv6'] = True
+                    
+                    if sockets.is_ipv6(addr) == False:
+                        return False
+                    addr = sockets.addr_atoi(addr)
+                    ranges.append(IpRange(addr, addr, **opts))
+                    continue
+                    
+                try:
+                    addr1, scope_id = addrs[0].split('%')
+                except ValueError:
+                    addr1 = addrs[0].split('%')[0]
+                    scope_id = None
+                    
+                if scope_id is not None:
+                    opts['scope_id'] = scope_id
+                    
+                try:
+                    addr2, scope_id = addrs[0].split('%')
+                except ValueError:
+                    addr2 = addrs[0].split('%')[0]
+                    scope_id = None
+                    
+                if scope_id is not None and 'scope_id' not in opts:
+                    opts['scope_id'] = scope_id
+                    
+                if sockets.is_ipv6(addr1) == False or sockets.is_ipv6(addr2) == False:
+                    return False
+                    
+                addr1 = sockets.addr_atoi(addr1)
+                addr2 = sockets.addr_atoi(addr2)
+                
+                ranges.append(IpRange(addr1, addr2, **opts))
+                continue
+
+            elif '/' in arg:
+                if re.search(r'[,-]', arg) is not None:
+                    return False
+                if len(re.findall("/", arg)) > 1:
+                    return False
+                
+                try:
+                    ip_part, mask_part = arg.split("/")
+                except ValueError:
+                    ip_part = arg.split("/")[0]
+                    mask_part = None
+
+                if ip_part is None or len(ip_part) == 0 or mask_part is None or len(mask_part) == 0:
+                    return False
+
+                if re.search(r'^[0-9]{1,2}$', mask_part) is None:
+                    return False
+
+                if int(mask_part) > 32:
+                    return False
+
+                if re.search(r'^\d{1,3}(\.\d{1,3}){1,3}$', ip_part) is not None:
+                    if re.search(sockets.MATCH_IPV4, ip_part) is None:
+                        return False
+
+                try:
+                    sockets.getaddress(ip_part)
+                except (socket.gaierror, socket.error) as e:
+                    return False
+
+                expanded = self.expand_cidr(arg)
+                if expanded is not None and not expanded == False:
+                    ranges.append(expanded)
+                else:
+                    return False
+
+            elif re.search(r'[^-0-9,.*]', arg) is not None:
+                try:
+                    ranges += map(lambda a: IpRange(a, a, **opts), sockets.resolv_nbo_i_list(arg))
+                except (socket.gaierror, socket.error) as e:
+                    return False
+
+            elif v4range_mach is not None:
+                try:
+                    start, stop = sockets.addr_atoi(v4range_mach.group(1)), sockets.addr_atoi(v4range_mach.group(2))
+                    if start > stop:
+                        return False
+                    ranges.append(IpRange(start, stop, **opts))
+                except (socket.gaierror, socket.error) as e:
+                    return False
+
+            else:
+                expanded = self.expand_nmap(arg)
+                if expanded:
+                    for r in expanded:
+                        ranges.append(r)
+
+        def list_uniq(duplicate): 
+            final_list = [] 
+            for num in duplicate: 
+                if num not in final_list: 
+                    final_list.append(num) 
+            return final_list 
+
+        ranges = list_uniq(ranges)
+        return ranges
+            
+    def reset(self):
+        if self.valid == False:
+            return False
+        self.curr_range_index = 0
+        self.curr_addr = self.ranges[0].start
+        self.length = 0
+        for r in self.ranges:
+            self.length += len(r)
+
+        return self
+    
+    def next_ip(self):
+        if not self.valid:
+            return False
+
+        if (self.curr_addr > self.ranges[self.curr_range_index].stop):
+            try:
+                if self.ranges[self.curr_range_index + 1] is None:
+                    return False
+            except IndexError:
+                return False
+
+            self.curr_range_index += 1
+
+            self.curr_addr = self.ranges[self.curr_range_index].start
+        addr = sockets.addr_itoa(self.curr_addr, self.ranges[self.curr_range_index].is_ipv6)
+
+        if 'scope_id' in self.ranges[self.curr_range_index].options:
+            addr = addr + '%' + self.ranges[self.curr_range_index].options['scope_id']
+
+        self.curr_addr += 1
+        return addr
+
+    @property
+    def valid(self):
+        return ((self.ranges is not None or not self.ranges == False) and (len(self.ranges) != 0))
+                    
+    def expand_cidr(self, arg):
+        try:
+            start, stop = sockets.cidr_crack(arg)
+        except ValueError:
+            return False
+            
+        return IpRange(sockets.addr_atoi(start), sockets.addr_atoi(stop), ipv6 = (':' in arg))
+        
+    def expand_nmap(self, arg):
+        if ':' in arg:
+            return False
+            
+        if ',-' in arg or '-,' in arg:
+            return False
+            
+        bytes = []
+        sections = arg.split('.')
+        if len(sections) != 4:
+            return False
+            
+        for section in sections:
+            if len(section) == 0:
+                section = "0"
+                
+            if section == "*":
+                section = "0-255"
+            elif '*' in section:
+                return False
+
+            ranges = section.split(',', -1)
+            sets = []
+            for r in ranges:
+                bounds = []
+                if '-' in r:
+                    bounds = r.split('-', -1)
+                    if len(bounds) > 2:
+                        return False
+                
+                    if bounds[0] is None or len(bounds[0]) == 0:
+                        bounds[0] = 0
+                        
+                    if bounds[1] is None or len(bounds[0]) == 0:
+                        bounds[1] = 255
+
+                    bounds = map(lambda b: int(b), bounds)
+                    if (bounds[0] > bounds[1]):
+                        return False
+                else:
+                    bounds.insert(0, int(r))
+                if bounds[0] > 255 or (len(bounds) > 1 and bounds[1] > 255):
+                    return False
+                if len(bounds) > 1 and (bounds[0] > bounds[1]):
+                    return False
+                if len(bounds) > 1:
+                    for i in range(bounds[0], bounds[1] + 1):
+                        sets.append(i)
+                elif len(bounds) == 1:
+                    sets.append(bounds[0])
+            bytes.append(sorted(set(sets)))
+
+        addrs = []
+        for a in bytes[0]:
+            for b in bytes[1]:
+                for c in bytes[2]:
+                    for d in bytes[3]:
+                        ip = (a << 24) + (b << 16) + (c << 8) + d
+                        addrs.append(ip)
+
+        addrs = sorted(set(addrs))
+        rng = IpRange()
+        rng.options = {'ipv6':False}
+        rng.start = addrs[0]
+
+        ranges = []
+        for idx in range(1, len(addrs)):
+            if addrs[idx - 1] + 1 == addrs[idx]:
+                continue
+            else:
+                rng.stop = addrs[idx - 1]
+                ranges.append(copy.copy(rng))
+                rng.start = addrs[idx]
+        rng.stop = addrs[len(addrs) - 1]
+        ranges.append(copy.copy(rng))
+        return ranges
+            
+                
+                
---
+++ packages/lair3/lair3-0.2.1.tar.gz/lair3-0.2.1/lair3/securesocket.py
@@ -0,0 +1,433 @@
+import amncore.sync
+import amncore.xcepts
+import bombfuse
+import datetime
+import OpenSSL
+import random
+import select
+import socket
+try:
+    import sockets
+except ImportError:
+    from . import sockets
+try:
+    import x509
+except ImportError:
+    from . import x509
+
+class SecureSock(object):
+
+    class CertProv(object):
+        @classmethod
+        def ssl_generate_subject(self):
+            import paynspray.rando
+            nom = OpenSSL.crypto.X509Name(OpenSSL.crypto.X509().get_subject())
+
+            st = str(paynspray.rando.rand_state())
+            loc = str(paynspray.rando.rand_name()).capitalize()
+            org = str(paynspray.rando.rand_name()).capitalize()
+            cn = str(paynspray.rando.rand_hostname())
+
+            nom.C = "US"
+            nom.ST = st
+            nom.L = loc
+            nom.O = org
+            nom.CN = cn
+            return nom
+
+        @classmethod
+        def ssl_generate_issuer(self):
+            import paynspray.rando
+            nom = OpenSSL.crypto.X509Name(OpenSSL.crypto.X509().get_issuer())
+
+            org = str(paynspray.rando.rand_name()).capitalize()
+            cn = str(paynspray.rando.rand_name()).capitalize() + " " + str(paynspray.rando.rand_name()).capitalize()
+
+            nom.C = "US"
+            nom.O = org
+            nom.CN = cn
+            return nom
+        
+        @classmethod
+        def ssl_generate_certificate(self):
+            yr = 24 * 3600 * 365
+            vf = datetime.datetime.now() - datetime.timedelta(seconds = (random.randrange(yr * 3) + yr))
+            vt = vf + datetime.timedelta(seconds = ((random.randrange(9) + 1) * yr))
+            vf = vf.strftime("%Y%m%d%H%M%SZ")
+            vt = vt.strftime("%Y%m%d%H%M%SZ")
+            subject = self.ssl_generate_subject()
+            issuer = self.ssl_generate_issuer()
+            key = OpenSSL.crypto.PKey()
+            key.generate_key(OpenSSL.crypto.TYPE_RSA, 2048)
+            cert = OpenSSL.crypto.X509()
+            cert.set_version(2)
+            cert.set_serial_number((random.randrange(0xFFFFFFFF) << 32) + random.randrange(0xFFFFFFFF))
+            cert.set_subject(subject)
+            cert.set_issuer(issuer)
+            cert.set_notBefore(vf)
+            cert.set_notAfter(vt)
+            cert.set_pubkey(key)
+        
+            ef = OpenSSL.crypto.X509Extension
+            cert.add_extensions([ef("basicConstraints", True, "CA:FALSE")])
+        
+            cert.sign(key, 'sha256')
+        
+            return [key, cert, None]
+
+    cert_prov = CertProv
+
+    def __init__(self, *args, **kwargs):
+        if hasattr(self, "sock") == False:
+            self.sock = None
+        return super(SecureSock, self).__init__(*args, **kwargs)
+
+    @property
+    def cert_provider(self):
+        return self.cert_prov
+
+    @cert_provider.setter
+    def cert_provider(self, val):
+        self.cert_prov = val
+
+    @classmethod
+    def ssl_parse_pem(self, ssl_cert):
+        return x509.X509Cert.parse_pem(ssl_cert)
+
+    @classmethod
+    def ssl_generate_subject(self):
+        return self.cert_prov.ssl_generate_subject()
+
+    @classmethod
+    def ssl_generate_issuer(self):
+        return self.cert_prov.ssl_generate_issuer()
+
+    @classmethod
+    def ssl_generate_certificate(self):
+        return self.cert_prov.ssl_generate_certificate()
+
+    def makessl(self, params):
+        import paynspray.rando
+        if hasattr(params, "ssl_cert") == True and params.ssl_cert is not None:
+            key, cert, chain = self.ssl_parse_pem(params.ssl_cert)
+        else:
+            key, cert, chain = self.ssl_generate_certificate()
+
+        ctx = OpenSSL.SSL.Context(OpenSSL.SSL.TLSv1_2_METHOD)
+        
+        if key is not None:
+            ctx.use_privatekey(key)
+        if cert is not None:
+            ctx.use_certificate(cert)
+        if chain is not None and chain != []:
+            ctx.add_extra_chain_cert(chain)
+
+        ctx.set_options(0)
+        
+        if hasattr(params, "ssl_cipher") == True:
+            if params.ssl_cipher is not None:
+                ctx.set_cipher_list(params.ssl_cipher)
+
+        if hasattr(OpenSSL.SSL, "OP_NO_COMPRESSION") == True:
+            if hasattr(params, "ssl_compression") == True:
+                if params.ssl_compression is not None and params.ssl_compression == False:
+                    ctx.set_options(OpenSSL.SSL.OP_NO_COMPRESSION)
+
+        ctx.set_session_id(paynspray.rando.rand_text(16))
+        
+        return ctx
+
+    def allow_nonblock(self, sock = None):
+        import amncore.compat
+        if sock is None:
+            if hasattr(self, "sock") == True:
+                sock = self.sock
+        avail = hasattr(sock, "setblocking")
+        if avail == True and amncore.compat.is_windows() == True:
+            return True
+        return False
+        
+class SslTcpSock(sockets.TcpSock):
+    def __init__(self, *args, **kwargs):
+        if hasattr(self, "ssl_negotiated_version") == False:
+            self.ssl_negotiated_version = None
+        if hasattr(self, "sslsock") == False:
+            self.sslsock = None
+        if hasattr(self, "sslctx") == False:
+            self.sslctx = None
+        return super(SslTcpSock, self).__init__(*args, **kwargs)
+
+    @classmethod
+    def create(self, **kwargs):
+        kwargs['SSL'] = True
+        return self.create_param(sockets.SocketParams.from_kwargs(**kwargs))
+    
+    @classmethod
+    def create_param(self, param):
+        param.ssl = True
+        return super(SslTcpSock, self).create_param(param)
+
+    def initsock(self, params = None):
+        super(SslTcpSock, self).initsock(params)
+
+        SSLv2 = 1
+        SSLv3 = 2
+        SSLv23 = 3
+        TLSv1 = 4
+        TLSv1_1 = 5
+        TLSv1_2 = 6
+
+        version = OpenSSL.SSL.SSLv23_METHOD
+
+        if params is not None and params.ssl_version is not None:
+            if params.ssl_version == 'SSL2' or params.ssl_version == SSLv2:
+                version = OpenSSL.SSL.SSLv2_METHOD
+            elif params.ssl_version == 'SSL23' or params.ssl_version == SSLv23 or params.ssl_version == 'TLS' or params.ssl_version == 'Auto':
+                version = OpenSSL.SSL.SSLv23_METHOD
+            elif params.ssl_version == 'SSL3' or params.ssl_version == SSLv3:
+                version = OpenSSL.SSL.SSLv3_METHOD
+            elif params.ssl_version == 'TLS1' or params.ssl_version == 'TLS1.0' or params.ssl_version == TLSv1:
+                version = OpenSSL.SSL.TLSv1_METHOD
+            elif params.ssl_version == 'TLS1.1' or params.ssl_version == TLSv1_1:
+                version = OpenSSL.SSL.TLSv1_1_METHOD
+            elif params.ssl_version == 'TLS1.2' or params.ssl_version == TLSv1_2:
+                version = OpenSSL.SSL.TLSv1_2_METHOD
+
+        self.initsock_with_ssl_version(params, version)
+
+        self.ssl_negotiated_version = version
+
+        return self.ssl_negotiated_version
+
+    def initsock_with_ssl_version(self, params, version):
+        self.sslctx = OpenSSL.SSL.Context(version)
+
+        if params is not None and params.ssl_client_cert is not None:
+            tmp_cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, params.ssl_client_cert)
+            self.sslctx.use_certificate(tmp_cert)
+            
+        if params is not None and params.ssl_client_key is not None:
+            tmp_key = OpenSSL.crypto.load_privatekey(OpenSSL.crypto.FILETYPE_PEM, params.ssl_client_key)
+            self.sslctx.use_privatekey(tmp_key)
+
+        if params is not None and params.ssl_verify_mode is not None:
+            verify_mode = getattr(OpenSSL.SSL, "VERIFY_{}".format(params.ssl_verify_mode))
+        else:
+            verify_mode = OpenSSL.SSL.VERIFY_PEER
+            
+        self.sslctx.set_verify(verify_mode, verify_cb)
+        
+        self.sslctx.set_options(OpenSSL.SSL.OP_ALL)
+
+        if hasattr(params, "ssl_cipher") == True:
+            if params.ssl_cipher is not None:
+                self.sslctx.set_cipher_list(params.ssl_cipher)
+            
+        self.sslsock = OpenSSL.SSL.Connection(self.sslctx, self.sock)
+        self.sslsock.set_connect_state()
+        self.sslsock.set_tlsext_host_name(params.peerhost)
+        
+        def nonblock_handshake_proc():
+            while True:
+                try:
+                    self.sslsock.do_handshake()
+                    break
+                except OpenSSL.SSL.WantReadError as e:
+                    select.select([self.sslsock], [], [], 0.10)
+                    continue
+                except OpenSSL.SSL.WantWriteError as e:
+                    select.select([], [self.sslsock], [], 0.10)
+                    continue
+                except Exception as e:
+                    raise e
+
+        if self.allow_nonblock() == False:
+            try:
+                self.sslsock.do_handshake()
+            except socket.timeout as e:
+                raise amncore.xcepts.ConnectionTimeout(params.peerhost, params.peerport)
+        else:
+            self.sslsock.setblocking(0)
+            try:
+                bombfuse.timeout(params.timeout, nonblock_handshake_proc)
+            except bombfuse.TimeoutError as e:
+                raise amncore.xcepts.ConnectionTimeout(params.peerhost, params.peerport)
+
+    def write(self, buf, **kwargs):
+        if self.allow_nonblock() == False:
+            self.sslsock.setblocking(1)
+            return self.sslsock.send(buf)
+
+        total_sent = 0
+        total_length = len(buf)
+        block_size = 16384
+        retry_time = 0.5
+        
+        self.sslsock.setblocking(0)
+        while True:
+            try:
+                while (total_sent < total_length):
+                    s = amncore.sync.select_s([], [self.sslsock], [], 0.25)
+                    if s == ([],[],[]) or self.sslsock not in s[1]:
+                        continue
+                    data = buf[total_sent:block_size]
+                    sent = self.sslsock.send(data)
+                    if sent > 0:
+                        total_sent += sent
+                break
+            except IOError as e:
+                return None
+            except OpenSSL.SSL.WantReadError as e:
+                select.select([self.sslsock], [], [], retry_time)
+                continue
+            except OpenSSL.SSL.WantWriteError as e:
+                select.select([], [self.sslsock], [], retry_time)
+                continue
+            except OpenSSL.SSL.Error as e:
+                return None
+            except Exception as e:
+                raise e
+
+        return total_sent
+
+    def read(self, length = None, **kwargs):
+        if self.allow_nonblock() == False:
+            self.sslsock.setblocking(1)
+            if length is None:
+                length = 16384
+            try:
+                return self.sslsock.recv(length)
+            except (IOError, OpenSSL.SSL.Error) as e:
+                return None
+            return
+
+        self.sslsock.setblocking(0)
+        while True:
+            try:
+                s = amncore.sync.select_s([self.sslsock], [], [], 0.10)
+                if (s == ([],[],[]) or self.sslsock not in s[0]):
+                    continue
+                return self.sslsock.recv(length)
+            except IOError as e:
+                return None
+            except OpenSSL.SSL.WantReadError as e:
+                select.select([self.sslsock], [], [], 0.5)
+                continue
+            except OpenSSL.SSL.WantWriteError as e:
+                select.select([], [self.sslsock], [], 0.5)
+                continue
+            except OpenSSL.SSL.Error as e:
+                return None
+            except Exception as e:
+                raise e
+                
+    def close(self):
+        try:
+            self.sslsock.close()
+        except Exception as e:
+            pass
+        return super(SslTcpSock, self).close()
+
+    def shutdown(how = socket.SHUT_RDWR):
+        pass
+
+    @property
+    def peer_cert(self):
+        if self.sslsock is not None:
+            return self.sslsock.get_peer_certificate()
+
+    @property
+    def peer_cert_chain(self):
+        if self.sslsock is not None:
+            return self.sslsock.get_peer_cert_chain()
+
+    def recv(self, *args, **kwargs):
+        raise RuntimeError("Invalid recv() call on SSL socket")
+
+    def send(self, *args, **kwargs):
+        raise RuntimeError("Invalid send() call on SSL socket")
+
+    def allow_nonblock(self):
+        import amncore.compat
+        avail = (hasattr(self.sslsock, "setblocking") == True)
+        if avail == True and amncore.compat.is_windows() == True:
+            return True
+        return False
+        
+class SslTcpServer(sockets.TcpServer, SecureSock):
+    def __init__(self, *args, **kwargs):
+        if hasattr(self, "sslctx") == False:
+            self.sslctx = None
+        return super(SslTcpServer, self).__init__(*args, **kwargs)
+
+    @classmethod
+    def create(self, **kwargs):
+        kwargs['Proto'] = 'tcp'
+        kwargs['Server'] = True
+        kwargs['SSL'] = True
+        return self.create_param(sockets.SocketParams.from_kwargs(**kwargs))
+
+    @classmethod
+    def create_param(self, param):
+        param.proto = 'tcp'
+        param.server = True
+        param.ssl = True
+        return sockets.create_param(param)
+
+    def initsock(self, params = None):
+        if params is not None and params.sslctx is not None and params.sslctx.__class__ == OpenSSL.SSL.Context:
+            self.sslctx = params.sslctx
+        else:
+            self.sslctx = self.makessl(params)
+        return super(SslTcpServer, self).initsock(params)
+
+    def accept(self, **kwargs):
+        try:
+            r, _, _ = select.select([self.sock],[],[],0.5)
+            if self.sock in r:
+                _sock, addr = self.sock.accept()
+            else:
+                _sock = None
+        except socket.error as e:
+            _sock = None
+
+        if _sock is None:
+            return None
+
+        try:
+            ssl = OpenSSL.SSL.Connection(self.sslctx, _sock)
+            ssl.set_accept_state()
+
+            if self.allow_nonblock(ssl) == False:
+                try:
+                    ssl.setblocking(1)
+                    ssl.do_handshake()
+                except socket.timeout as e:
+                    _sock.close()
+                    raise OpenSSL.SSL.Error
+            else:
+                while True:
+                    try:
+                        ssl.setblocking(0)
+                        ssl.do_handshake()
+                        break
+                    except OpenSSL.SSL.WantReadError as e:
+                        select.select([ssl], [], [], 0.10)
+                        continue
+                    except OpenSSL.SSL.WantWriteError as e:
+                        select.select([], [ssl], [], 0.10)
+                        continue
+                    except Exception as e:
+                        raise e
+    
+            cls = SslTcpSock
+            sock = cls(sock = _sock, sslsock = ssl)
+            sock.sslctx = self.sslctx
+            
+            return sock
+        except OpenSSL.SSL.Error as e:
+            _sock.close()
+            return None
+            
+def verify_cb(conn, cert, errno, depth, result):
+    return True
