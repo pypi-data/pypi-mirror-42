88b12622eb5f1a85f56726e5bc32fb1a0f7c35ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:13 +0000

    Add package torch-text-classifier/0.0.55/torch_text_classifier-0.0.55-py3-none-any.whl

---
+++ packages/torch-text-classifier/torch_text_classifier-0.0.55-py3-none-any.whl/torch_text_classifier/__init__.py
@@ -0,0 +1,7 @@
+import os
+from .text_classifier import TextClassifier
+
+CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
+__VERSION__ = open(os.path.join(CURRENT_DIR, 'version.txt')).read()
+
+__all__ = ['TextClassifier']
