ed7501991d0fd3185c3f6cf9347f6e224cefe6b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:29 +0000

    Add package edc-model/0.1.1/edc_model-0.1.1-py3-none-any.whl

---
+++ packages/edc-model/edc_model-0.1.1-py3-none-any.whl/edc_model/model_mixins/__init__.py
@@ -0,0 +1,5 @@
+from .address_mixin import AddressMixin
+from .base_model import BaseModel
+from .base_uuid_model import BaseUuidModel
+from .report_status_model_mixin import ReportStatusModelMixin
+from .url_model_mixin import UrlModelMixin, UrlModelMixinNoReverseMatch
