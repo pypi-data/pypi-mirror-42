9948d77adb8dc25a82dc66b3402d2d0985145563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:19 +0000

    Add package ai-utils/1.1.2/ai_utils-1.1.2.tar.gz

---
+++ packages/ai-utils/ai_utils-1.1.2.tar.gz/ai_utils-1.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(name='ai_utils',
+      version='1.1.2',
+      description='Utilities to be used with Tensorflow and Keras for AI techniques',
+      url='http://github.com/rcluan/ai_utils',
+      author='Luan Campos',
+      author_email='luan.rios.campos@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'tensorflow',
+          'pandas',
+          'matplotlib',
+          'numpy',
+          'sklearn',
+          'xlrd'
+      ],
+      zip_safe=False)
