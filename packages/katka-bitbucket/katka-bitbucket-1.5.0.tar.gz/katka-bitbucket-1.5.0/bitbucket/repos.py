39d42674aab6b797494911332fde136e37f10e2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:15 +0000

    Add package katka-bitbucket/1.5.0/katka-bitbucket-1.5.0.tar.gz

---
+++ packages/katka-bitbucket/katka-bitbucket-1.5.0.tar.gz/katka-bitbucket-1.5.0/bitbucket/repos.py
@@ -0,0 +1,17 @@
+import logging
+
+from dataclasses import dataclass
+
+from .service import BitbucketService
+
+log = logging.getLogger(__name__)
+
+
+@dataclass
+class BitbucketRepos(BitbucketService):
+    project_key: str = None
+
+    def get_repos(self, params: dict = None) -> dict:
+        path = f'projects/{self.project_key}/repos'
+
+        return super().get(path=path, params=params)
