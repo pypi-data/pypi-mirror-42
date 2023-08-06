e82253f8862fc3662624d07a82046773794ad3ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:16 +0000

    Add package pytsa/0.3.0/pytsa-0.3.0.tar.gz

---
+++ packages/pytsa/pytsa-0.3.0.tar.gz/pytsa-0.3.0/src/rule/sa_list.py
@@ -0,0 +1,86 @@
+import inspect
+import os
+
+from src.pytsa import sa_int, sa_bool, sa_type
+from src.utils import none_checker
+
+
+def _format_list(val):
+    if len(val) <= 3:
+        return str(val)
+    return '[{}, {}, {}, ...]'.format(val[0], val[1], val[2])
+
+
+def sa_list(arg_name, **rules):
+    """
+    Ensures the given parameter is of type list and not None, and abides by all given rules
+    """
+
+    def _sa_list(func):
+        if os.environ.get('PYTSA_DISABLED', 'False') == 'True':
+            return func
+
+        args_spec = inspect.getfullargspec(func).args
+        assert arg_name in args_spec, 'list argument name \'{}\' not found in argument specification'.format(arg_name)
+
+        arg_index = args_spec.index(arg_name)
+
+        allow_none = rules.get('allow_none', False)
+        rules.pop('allow_none', None)
+
+        def _checker(*args, **kwargs):
+            val = args[arg_index]
+            assert allow_none or val is not None, 'list argument \'{}\' was None'.format(arg_name)
+            assert (allow_none and val is None) or isinstance(val,
+                                                              list), 'list argument \'{}\' with value {} was of type {)}, not of type \'list\''.format(
+                arg_name, val, type(val))
+            return func(*args, **kwargs)
+
+        for rule in rules:
+            assert rule in LIST_RULES, 'rule \'{}\' is unknown for sa_list'.format(rule)
+            _checker = none_checker(allow_none, LIST_RULES[rule](arg_name, rules[rule], _checker))
+
+        return _checker
+
+    return _sa_list
+
+
+@sa_int('rule_val')
+def _list_len(arg_name, rule_val, func):
+    def _check(val):
+        assert len(val) == rule_val, 'list argument \'{}\' with value {} and length of {} was not equal to {}'.format(
+            arg_name, _format_list(val), len(val), rule_val)
+        func(val)
+
+    return _check
+
+
+@sa_type('rule_val')
+def _list_type(arg_name, rule_val, func):
+    def _check(val):
+        for i, v in enumerate(val):
+            assert isinstance(v,
+                              rule_val), 'list argument \'{}\' with type {} had value with type {} on index {}'.format(
+                arg_name, rule_val, type(v), i)
+        func(val)
+
+    return _check
+
+
+@sa_bool('rule_val')
+def _list_not_empty(arg_name, rule_val, func):
+    if not rule_val:
+        return func
+
+    def _check(val):
+        assert len(val) != 0, 'list argument \'{}\' was an empty array'.format(arg_name)
+        func(val)
+
+    return _check
+
+
+LIST_RULES = {
+    'len': _list_len,
+    'type': _list_type,
+    'not_empty': _list_not_empty,
+}
---
+++ packages/pytsa/pytsa-0.3.0.tar.gz/pytsa-0.3.0/test/test_sa_bool.py
@@ -0,0 +1,109 @@
+from unittest import TestCase, mock
+
+from src.pytsa import sa_bool
+
+
+class TestSaBoolRules(TestCase):
+    # Test that the rules for sa_bool works as specified
+
+    def test_rule_allow_none_true(self):
+        @sa_bool('a', allow_none=True)
+        def _test(a):
+            return a
+
+        # Correct usage
+        _test(None)
+        _test(True)
+        _test(False)
+
+        # Incorrect usage, got float
+        with self.assertRaises(Exception):
+            _test(2.5)
+
+    def test_rule_allow_none_false(self):
+        @sa_bool('a', allow_none=False)
+        def _test(a):
+            return a
+
+        # Correct usage
+        _test(True)
+        _test(False)
+
+        # Incorrect usage, got None
+        with self.assertRaises(Exception):
+            _test(None)
+        # Incorrect usage, got float
+        with self.assertRaises(Exception):
+            _test(2.5)
+
+
+class TestSaBoolBase(TestCase):
+    # Test that the sa_bool works as specified
+
+    def test(self):
+        # no exceptions should be thrown with correct usage
+        @sa_bool('a')
+        def _test(a):
+            return a
+
+    def test_args_name_missing(self):
+        # the annotation should throw an exception if the name passed in the decorator is not present in the argument
+        with self.assertRaises(Exception):
+            @sa_bool('b')
+            def _test(a):
+                return a
+
+    def test_incorrect_rule(self):
+        # if an unknown rule is provided, throw an exception
+        with self.assertRaises(Exception):
+            @sa_bool('a', unknown_rule=True)
+            def _test(a):
+                return a
+
+    def test_global_disable(self):
+        # if the environment variable 'PYTSA_DISABLED' is set to True, the decorator should be ignored
+        def _test(a):
+            return a
+
+        with mock.patch.dict('os.environ', {'PYTSA_DISABLED': 'True'}):
+            _test_true = sa_bool('a')(_test)
+            assert _test_true == _test
+
+        with mock.patch.dict('os.environ', {'PYTSA_DISABLED': 'False'}):
+            _test_false = sa_bool('a')(_test)
+            assert _test_false != _test
+
+        with mock.patch.dict('os.environ', {}):
+            _test_false = sa_bool('a')(_test)
+            assert _test_false != _test
+
+    def test_type(self):
+        @sa_bool('a')
+        def _test(a):
+            return a
+
+        # correct
+        _test(True)
+        _test(False)
+
+        # not zero
+        with self.assertRaises(Exception):
+            _test(0)
+        # not None
+        with self.assertRaises(Exception):
+            _test(None)
+        # not int
+        with self.assertRaises(Exception):
+            _test(1)
+        # not float
+        with self.assertRaises(Exception):
+            _test(3.0)
+        # not string
+        with self.assertRaises(Exception):
+            _test('abc')
+        # not list
+        with self.assertRaises(Exception):
+            _test([1, 'a'])
+        # not type
+        with self.assertRaises(Exception):
+            _test(bool)
