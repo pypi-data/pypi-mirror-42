b327802997b2c18465ed2cea5c8e0825cc22150a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:06 +0000

    Add package pycali/1.0.1/pycali-1.0.1-py3-none-any.whl

---
+++ packages/pycali/pycali-1.0.1-py3-none-any.whl/pycali/examples/license_constraints.py
@@ -0,0 +1,20 @@
+from pycali.vocabulary.ontologies.cali_onto import Duty, Prohibition, Permission
+from pycali.vocabulary.vocabulary import ODRL, CC
+
+
+def CommercialUse_Not_Duty(vocabulary, license):
+    return license.get_state(vocabulary, CC['CommericalUse']) != Duty
+
+
+def ShareAlike_Not_Prohibition(vocabulary, license):
+    return license.get_state(vocabulary, CC['ShareAlike']) != Prohibition
+
+
+def CommercialUse_Include_Use(vocabulary, license):
+    if license.get_state(vocabulary, ODRL['use']) == Prohibition:
+        if (
+            license.get_state(vocabulary, CC['CommericalUse']) == Permission or
+            license.get_state(vocabulary, CC['CommericalUse']) == Duty
+           ):
+            return False
+    return True
---
+++ packages/pycali/pycali-1.0.1-py3-none-any.whl/tests/test_deontic_lattice.py
@@ -0,0 +1,32 @@
+from unittest import TestCase
+from rdflib import Graph
+
+from pycali.deontic_lattice import DeonticLattice
+from pycali.vocabulary.ontologies.cali_onto import Undefined, Permission, Prohibition, Duty
+
+
+class testDeonticLattice(TestCase):
+    """Test for DeonticLattice object."""
+
+    def test_odrl_vocabulary(self):
+        """Test if ODRL vocabulary is well instanciated."""
+        DL1 = DeonticLattice(Graph().parse(location='cali/examples/deontic_lattices/DL1.ttl', format='ttl'))
+        DL2 = DeonticLattice(Graph().parse(location='cali/examples/deontic_lattices/DL2.ttl', format='ttl'))
+        self.assertCountEqual([Undefined, Permission, Prohibition, Duty], DL1.restrictiveness[Undefined])
+        self.assertCountEqual([Permission, Prohibition, Duty], DL1.restrictiveness[Permission])
+        self.assertCountEqual([Prohibition, Duty], DL1.restrictiveness[Prohibition])
+        self.assertCountEqual([Duty], DL1.restrictiveness[Duty])
+        self.assertCountEqual([Permission, Prohibition, Duty], DL2.restrictiveness[Permission])
+        self.assertCountEqual([Prohibition, Duty], DL2.restrictiveness[Prohibition])
+        self.assertCountEqual([Duty], DL2.restrictiveness[Duty])
+
+    def test_restrictiveness(self):
+        """Test restrictiveness function of the deontic lattice."""
+        DL1 = DeonticLattice(Graph().parse(location='cali/examples/deontic_lattices/DL1.ttl', format='ttl'))
+        self.assertTrue(DL1.is_less_restrictive(Undefined, Permission))
+        self.assertTrue(DL1.is_less_restrictive(Undefined, Duty))
+        self.assertTrue(DL1.is_less_restrictive(Permission, Duty))
+        self.assertTrue(DL1.is_less_restrictive(Duty, Duty))
+        self.assertFalse(DL1.is_less_restrictive(Duty, Undefined))
+        self.assertFalse(DL1.is_less_restrictive(Prohibition, Permission))
+        self.assertFalse(DL1.is_less_restrictive(Permission, Undefined))
