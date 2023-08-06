61f07671059740c6fe062a86ac048f9c7d20c99d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:57 +0000

    Add package pulathisi/0.0.9/pulathisi-0.0.9.tar.gz

---
+++ packages/pulathisi/pulathisi-0.0.9.tar.gz/pulathisi-0.0.9/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pulathisi",
+    version="0.0.9",
+    author="Dewmal Nilanka",
+    author_email="dewmal@egreen.io",
+    description="Brain",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dewmal/pybrain",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
