5d195b5cd1f5462ce291beff1f5f0fb86d81154a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:02 +0000

    Add package glue-geospatial/0.2/glue-geospatial-0.2.tar.gz

---
+++ packages/glue-geospatial/glue-geospatial-0.2.tar.gz/glue-geospatial-0.2/CHANGES.rst
@@ -0,0 +1,9 @@
+0.2 (2019-02-27)
+----------------
+
+- Fixed compatibility with rasterio 1.x.
+
+0.1 (2017-08-23)
+----------------
+
+- Initial release
---
+++ packages/glue-geospatial/glue-geospatial-0.2.tar.gz/glue-geospatial-0.2/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+from setuptools import setup, find_packages
+
+entry_points = """
+[glue.plugins]
+glue_geospatial=glue_geospatial:setup
+"""
+
+with open('glue_geospatial/version.py') as infile:
+    exec(infile.read())
+
+with open('README.rst') as infile:
+    LONG_DESCRIPTION = infile.read()
+
+setup(name='glue-geospatial',
+      version=__version__,
+      description='Experimental glue plugin for geospatial imagery',
+      long_description=LONG_DESCRIPTION,
+      url="https://github.com/glue-viz/glue-geospatial",
+      author='Thomas Robitaille',
+      author_email='thomas.robitaille@gmail.com',
+      packages=find_packages(),
+      package_data={},
+      entry_points=entry_points,
+      install_requires=['glue-core>=0.11', 'rasterio', 'pyproj'])
