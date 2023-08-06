f1ffeb5c50633e09b4beed36057c894a0f3f5c79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:18 +0000

    Add package pylightning/0.0.7/pylightning-0.0.7.tar.gz

---
+++ packages/pylightning/pylightning-0.0.7.tar.gz/pylightning-0.0.7/lightning/__init__.py
@@ -0,0 +1,2 @@
+from .lightning import LightningRpc, RpcError, Millisatoshi
+from .plugin import Plugin, monkey_patch
---
+++ packages/pylightning/pylightning-0.0.7.tar.gz/pylightning-0.0.7/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='pylightning',
+      version='0.0.7',
+      description='Client library for lightningd',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='http://github.com/ElementsProject/lightning',
+      author='Christian Decker',
+      author_email='decker.christian@gmail.com',
+      license='MIT',
+      packages=['lightning'],
+      scripts=['lightning-pay'],
+      zip_safe=True)
