80154759411bbfc30438f9de9936b879d31cd2fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:51 +0000

    Add package amncore/0.1.4/amncore-0.1.4.tar.gz

---
+++ packages/amncore/amncore-0.1.4.tar.gz/amncore-0.1.4/amncore/__init__.py
@@ -0,0 +1,6 @@
+name = "amncore"
+
+import logger
+
+if logger.is_log_source_registered(name) == False:
+    logger.register_log_source(name, logger.FlatFileSink("{}.log".format(name)), logger.LEV_1)
