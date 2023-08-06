e7b6d2e2b6df3b6dbb1a6bff14816676c6b6de4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:05 +0000

    Add package pytsa/0.3.5/pytsa-0.3.5.tar.gz

---
+++ packages/pytsa/pytsa-0.3.5.tar.gz/pytsa-0.3.5/test/test_sa_type.py
@@ -0,0 +1,112 @@
+from unittest import TestCase, mock
+
+from src.pytsa import sa_type
+
+
+class TestSaTypeRules(TestCase):
+    # Test that the rules for sa_type works as specified
+
+    def test_rule_allow_none_true(self):
+        @sa_type('a', allow_none=True)
+        def _test(a):
+            return a
+
+        # Correct usage
+        _test(None)
+        _test(int)
+        _test(type(int))
+        _test(bool)
+
+        # Incorrect usage, got float
+        with self.assertRaises(Exception):
+            _test(2.5)
+
+    def test_rule_allow_none_false(self):
+        @sa_type('a', allow_none=False)
+        def _test(a):
+            return a
+
+        # Correct usage
+        _test(int)
+        _test(type(int))
+        _test(bool)
+
+        # Incorrect usage, got None
+        with self.assertRaises(Exception):
+            _test(None)
+        # Incorrect usage, got float
+        with self.assertRaises(Exception):
+            _test(2.5)
+
+
+class TestSaTypeBase(TestCase):
+    # Test that the sa_type works as specified
+
+    def test(self):
+        # no exceptions should be thrown with correct usage
+        @sa_type('a')
+        def _test(a):
+            return a
+
+    def test_args_name_missing(self):
+        # the annotation should throw an exception if the name passed in the decorator is not present in the argument
+        with self.assertRaises(Exception):
+            @sa_type('b')
+            def _test(a):
+                return a
+
+    def test_incorrect_rule(self):
+        # if an unknown rule is provided, throw an exception
+        with self.assertRaises(Exception):
+            @sa_type('a', unknown_rule=True)
+            def _test(a):
+                return a
+
+    def test_global_disable(self):
+        # if the environment variable 'PYTSA_DISABLED' is set to True, the decorator should be ignored
+        def _test(a):
+            return a
+
+        with mock.patch.dict('os.environ', {'PYTSA_DISABLED': 'True'}):
+            _test_true = sa_type('a')(_test)
+            assert _test_true == _test
+
+        with mock.patch.dict('os.environ', {'PYTSA_DISABLED': 'False'}):
+            _test_false = sa_type('a')(_test)
+            assert _test_false != _test
+
+        with mock.patch.dict('os.environ', {}):
+            _test_false = sa_type('a')(_test)
+            assert _test_false != _test
+
+    def test_type(self):
+        @sa_type('a')
+        def _test(a):
+            return a
+
+        # correct
+        _test(int)
+        _test(type)
+        _test(str)
+
+        # not zero
+        with self.assertRaises(Exception):
+            _test(0)
+        # not None
+        with self.assertRaises(Exception):
+            _test(None)
+        # not int
+        with self.assertRaises(Exception):
+            _test(1)
+        # not float
+        with self.assertRaises(Exception):
+            _test(3.0)
+        # not string
+        with self.assertRaises(Exception):
+            _test('abc')
+        # not list
+        with self.assertRaises(Exception):
+            _test([1, 'a'])
+        # not bool
+        with self.assertRaises(Exception):
+            _test(True)
