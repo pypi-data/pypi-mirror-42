9d454f367dbd180b27465fc1b0d9a9835689616c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:19 +0000

    Add package bizarro/0.0.0.1.dev5/bizarro-0.0.0.1.dev5.tar.gz

---
+++ packages/bizarro/bizarro-0.0.0.1.dev5.tar.gz/bizarro-0.0.0.1.dev5/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+install_requires = [
+    'zathura',
+]
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='bizarro',  # alternative name docket
+      packages=['bizarro', 'bizarro.utility', 'bizarro.template', 'bizarro..buttons'],
+      version='0.0.0.1.dev5',
+      description='',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/ibtehaz-shawon/bizarro',
+      author='Ibtehaz Shawon',
+      author_email='ibtehaz.92@gmail.com',
+      license='MIT',
+      install_requires=[
+          install_requires
+      ],
+      entry_points={
+          'console_scripts': [
+              'bizarro = bizarro.__init__:create_app'
+          ]
+      },
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+      zip_safe=False)
