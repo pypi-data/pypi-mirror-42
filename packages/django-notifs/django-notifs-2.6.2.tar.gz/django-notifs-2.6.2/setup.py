6dd6ba3a3754ad679ccf90d11fe91928023fd0f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:15 +0000

    Add package django-notifs/2.6.2/django-notifs-2.6.2.tar.gz

---
+++ packages/django-notifs/django-notifs-2.6.2.tar.gz/django-notifs-2.6.2/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+
+def package_files(directory):
+    """Recursively add subfolders and files."""
+    paths = []
+    for (path, _, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+EXTRA_FILES = package_files('notifications')
+
+setup(
+    name='django-notifs', version='2.6.2',
+    description='Re-usable notification app for Django',
+    url='https://github.com/danidee10/django-notifs', author='Osaetin Daniel',
+    author_email='osaetindaniel@gmail.com', license='MIT',
+    packages=['notifications'], package_data={'': EXTRA_FILES},
+    install_requires=['django', 'pika', 'celery', 'six'], zip_safe=False
+)
