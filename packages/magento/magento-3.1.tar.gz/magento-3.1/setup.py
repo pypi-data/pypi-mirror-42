bb9d924740f2c1bdfb975e93c86b0a03db147fa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:45 +0000

    Add package magento/3.1/magento-3.1.tar.gz

---
+++ packages/magento/magento-3.1.tar.gz/magento-3.1/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+    Magento API
+
+    :license: BSD, see LICENSE for more details
+
+'''
+import os
+from setuptools import setup
+
+exec(open(os.path.join('magento', 'version.py')).read())
+
+setup(
+    name = 'magento',
+    version=VERSION,
+    url='https://github.com/fulfilio/magento/',
+    license='BSD 3-Clause',
+    author='Sharoon Thomas, Openlabs Technologies',
+    author_email='info@fulfil.io',
+    description='Magento Core API Client',
+    long_description=open('README.rst').read(),
+    packages=['magento'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'suds-jurko>=0.6',
+        'six',
+    ],
+    classifiers=[
+        'Development Status :: 6 - Mature',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
