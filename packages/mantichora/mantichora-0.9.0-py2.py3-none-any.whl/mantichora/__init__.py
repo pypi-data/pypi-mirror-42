58008e40f315fe2f696de68c9eebc4f8c6ff5c6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:14 +0000

    Add package mantichora/0.9.0/mantichora-0.9.0-py2.py3-none-any.whl

---
+++ packages/mantichora/mantichora-0.9.0-py2.py3-none-any.whl/mantichora/__init__.py
@@ -0,0 +1,9 @@
+# Tai Sakuma <tai.sakuma@gmail.com>
+from .main import mantichora
+
+from .hub import MultiprocessingDropbox
+from .hub import TaskPackage
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
