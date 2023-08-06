9d2b55d39b921fa77dadd9b8c088e94ffd6b983c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:08 +0000

    Add package libervia/0.7.0a4/libervia-0.7.0a4-py2-none-any.whl

---
+++ packages/libervia/libervia-0.7.0a4-py2-none-any.whl/libervia/pages/p/blog/atom.xml/page_meta.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+
+def render(self, request):
+    return u""
---
+++ packages/libervia/libervia-0.7.0a4-py2-none-any.whl/libervia/pages/u/atom.xml/page_meta.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+redirect = u"blog_feed_atom"
