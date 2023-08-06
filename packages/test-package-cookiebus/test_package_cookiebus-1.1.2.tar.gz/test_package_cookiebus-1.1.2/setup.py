b07dc04ec26a4e19dcea202686719701f9907966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:51 +0000

    Add package test-package-cookiebus/1.1.2/test_package_cookiebus-1.1.2.tar.gz

---
+++ packages/test-package-cookiebus/test_package_cookiebus-1.1.2.tar.gz/test_package_cookiebus-1.1.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='test_package_cookiebus',
+    version='1.1.2',
+    py_modules=['fact', 'sum'],
+    author='cookiebus',
+    author_email='ngncmh@gmail.com',
+    url='',
+    description='test test_package_cookiebus'
+)
+
