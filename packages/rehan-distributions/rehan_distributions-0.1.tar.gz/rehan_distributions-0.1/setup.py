50f6853a653d7df7f817bbc02317f4f1454edf33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:12 +0000

    Add package rehan-distributions/0.1/rehan_distributions-0.1.tar.gz

---
+++ packages/rehan-distributions/rehan_distributions-0.1.tar.gz/rehan_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='rehan_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['rehan_distributions'],
+      author = 'Rehan Ahmar Sk',
+      zip_safe=False)
