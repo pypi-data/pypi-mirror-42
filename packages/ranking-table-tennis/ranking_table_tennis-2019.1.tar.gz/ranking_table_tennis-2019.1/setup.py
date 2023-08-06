a7ea5b51f6011cf24d4956813f4d91ab3ee328e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:35 +0000

    Add package ranking-table-tennis/2019.1/ranking_table_tennis-2019.1.tar.gz

---
+++ packages/ranking-table-tennis/ranking_table_tennis-2019.1.tar.gz/ranking_table_tennis-2019.1/setup.py
@@ -0,0 +1,62 @@
+from setuptools import setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+from subprocess import check_call
+import os
+import shutil
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+# Loads some names from config.yaml
+user_config_path = os.path.expanduser("~") + "/.config/ranking_table_tennis/config.yaml"
+
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+
+    def run(self):
+        # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
+        import ranking_table_tennis as rtt
+        shutil.copy(os.path.dirname(rtt.__file__) + "/config/config.yaml", user_config_path)
+        develop.run(self)
+
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
+        import ranking_table_tennis as rtt
+        # check_call("cp ranking_table_tennis/config/config.yaml ~/.config/ranking_table_tennis/config.yaml".split())
+        shutil.copy(os.path.dirname(rtt.__file__) + "/config/config.yaml", user_config_path)
+        install.run(self)
+
+
+setup(name='ranking_table_tennis',
+      version='2019.1',
+      description='A ranking table tennis system',
+      url='http://github.com/srvanrell/ranking-table-tennis',
+      author='Sebastian Vanrell',
+      author_email='srvanrell@gmail.com',
+      license='MIT',
+      packages=['ranking_table_tennis'],
+      scripts=['bin/rtt'],
+      include_package_data=True,
+      install_requires=[
+          'gspread>=3.1.0',
+          'oauth2client>=4.1.2',
+          'PyYAML>=3.12',
+          'urllib3>=1.23',
+          'openpyxl>=2.4.2',
+          'Unidecode>=1.0.22',
+          'pandas>=0.20.3'
+      ],
+      cmdclass={
+        'develop': PostDevelopCommand,
+        'install': PostInstallCommand,
+      },
+      zip_safe=False)
+
