2bd0d4cbf2fda4616a2b365ac230998bcc26d381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:18 +0000

    Add package pplpy/0.8.4/pplpy-0.8.4.tar.gz

---
+++ packages/pplpy/pplpy-0.8.4.tar.gz/pplpy-0.8.4/ppl/ppl_shim.cc
@@ -0,0 +1,11 @@
+#include "ppl_shim.hh"
+
+Poly_Gen_Relation* new_relation_with(const Polyhedron &p, const Generator &g)
+{
+  return new Poly_Gen_Relation(p.relation_with(g));
+}
+
+Poly_Con_Relation* new_relation_with(const Polyhedron &p, const Constraint &c)
+{
+  return new Poly_Con_Relation(p.relation_with(c));
+}
