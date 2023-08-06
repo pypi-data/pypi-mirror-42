0bf632c00ab445e2e0d31fdc405198bcbea8cd79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:51 +0000

    Add package django-multidb-router/0.9/django-multidb-router-0.9.tar.gz

---
+++ packages/django-multidb-router/django-multidb-router-0.9.tar.gz/django-multidb-router-0.9/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='django-multidb-router',
+    version='0.9',
+    description='Round-robin multidb router for Django.',
+    long_description=open('README.rst').read(),
+    author='Jeff Balogh',
+    author_email='jbalogh@mozilla.com',
+    url='https://github.com/jbalogh/django-multidb-router',
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=["Django>=1.11"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        # I don't know what exactly this means, but why not?
+        'Environment :: Web Environment :: Mozilla',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
