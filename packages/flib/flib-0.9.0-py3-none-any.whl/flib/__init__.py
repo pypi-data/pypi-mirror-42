b79073cb8aca2212a75a39833c8072cc2a9b80c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:28 +0000

    Add package flib/0.9.0/flib-0.9.0-py3-none-any.whl

---
+++ packages/flib/flib-0.9.0-py3-none-any.whl/flib/__init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 Frootlab Developers
+#
+# This file is part of the Frootlab Shared Library, https://github.com/frootlab
+#
+#  The Frootlab Shared Library (flib) is free software: you can redistribute it
+#  and/or modify it under the terms of the GNU General Public License as
+#  published by the Free Software Foundation, either version 3 of the License,
+#  or (at your option) any later version.
+#
+#  The Frootlab Shared Library (flib) is distributed in the hope that it will be
+#  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+#  Public License for more details. You should have received a copy of the GNU
+#  General Public License along with the frootlab shared library. If not, see
+#  <http://www.gnu.org/licenses/>.
+#
+"""Frootlab Shared Libarary.
+
+The Frootlab Shared Library (flib) is a multi-purpose Python library, which
+primarily aims to support Frootlab projects by a common base library. The
+majority of the provided modules, however, are kept generic to facilitate the
+application in other projects as well.
+
+"""
+
+__version__ = '0.9.0'
+__license__ = 'GPLv3'
+__copyright__ = 'Copyright (c) 2019 Frootlab Developers'
+__email__ = 'frootlab@gmail.com'
+__organization__ = 'Frootlab'
+__author__ = 'Frootlab Developers'
+__maintainer__ = 'Patrick Michl'
+__docformat__ = 'google'
+__authors__ = ['Patrick Michl <patrick.michl@gmail.com>']
