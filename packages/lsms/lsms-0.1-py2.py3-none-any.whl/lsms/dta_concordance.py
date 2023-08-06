ebaad2cae2118755b03ef1a12526c7ae36a67533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:52 +0000

    Add package lsms/0.1/lsms-0.1-py2.py3-none-any.whl

---
+++ packages/lsms/lsms-0.1-py2.py3-none-any.whl/lsms/dta_concordance.py
@@ -0,0 +1,33 @@
+
+# -*- coding: utf-8 -*-
+
+import sys
+try: # Only of python 2
+    reload(sys).setdefaultencoding('utf-8')
+except NameError: pass 
+import pandas as pd
+from pandas.io import stata
+from glob import glob
+from collections import defaultdict
+from warnings import warn
+
+dtafiles=glob(dir+'*.dta')
+
+lbls={}
+concordance=defaultdict(list)
+for fn in dtafiles:
+    try:
+        sr=stata.StataReader(fn)
+    except ValueError as failed:
+        warn("Can't read %s; %s" % (fn,failed.message))
+        continue
+    new=sr.variable_labels()
+    lbls.update(new)
+    for k in new.keys():
+        concordance[k].append(fn)
+
+for k in sorted(list(concordance.keys())):
+    print("| %s | %s | " % (k,lbls[k]), end='')
+    for f in concordance[k]:
+        print(f,end=' ')
+    print("|")
