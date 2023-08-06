488099293e92bbcb753a678fa66eaae1e641170d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:03 +0000

    Add package pandoctools/2.6.0.3/pandoctools-2.6.0.3.tar.gz

---
+++ packages/pandoctools/pandoctools-2.6.0.3.tar.gz/pandoctools-2.6.0.3/pandoctools/shared_vars/shared_vars.py
@@ -0,0 +1,96 @@
+import os
+import os.path as p
+import sys
+from typing import Tuple, Iterable
+from knitty.tools import where, KnittyError
+from subprocess import run, PIPE
+
+
+class PandotoolsError(Exception):
+    pass
+
+
+pandoctools_core = p.join(p.dirname(p.dirname(p.abspath(__file__))), 'sh')
+if os.name == 'nt':
+    pandoctools_user_data = r"%APPDATA%\pandoc\pandoctools"
+    pandoctools_user = p.join(os.environ["APPDATA"], "pandoc", "pandoctools")
+    env_path = p.dirname(sys.executable)
+    search_dirs = [env_path,
+                   p.join(env_path, r'Library\mingw-w64\bin'),
+                   p.join(env_path, r'Library\usr\bin'),
+                   p.join(env_path, r'Library\bin'),
+                   p.join(env_path, 'Scripts'),
+                   p.join(env_path, 'bin')]
+else:
+    pandoctools_user_data = "$HOME/.pandoc/pandoctools"
+    pandoctools_user = p.join(os.environ["HOME"], ".pandoc", "pandoctools")
+    env_path = p.dirname(p.dirname(sys.executable))
+    search_dirs = [p.join(env_path, 'bin')]
+
+
+def bash_cygpath(bash_from_conf: str='') -> Tuple[str, str]:
+    """ Returns (bash, cygpath) """
+    if os.name != 'nt':
+        return where('bash', search_dirs), ''
+    try:
+        bash = where('bash', search_dirs, exe_only=False)
+    except KnittyError:
+        if p.isfile(bash_from_conf):
+            bash = bash_from_conf
+        else:
+            git = p.expandvars(r'%PROGRAMFILES%\Git')
+            for bash in (rf'{git}\bin\bash.exe', rf'{git}\usr\bin\bash.exe'):
+                if p.isfile(bash):
+                    break
+            else:
+                raise PandotoolsError(
+                    "bash wasn't found in: python environment, $PATH, " +
+                    r"win_bash path in config, %PROGRAMFILES%\Git"
+                )
+
+    ret = run([bash, '-c', 'cygpath -w $(which cygpath)'], stderr=PIPE, stdout=PIPE)
+    cygpath = None
+    if ret.stdout and (ret.returncode == 0) and not ret.stderr:
+        cygpath = ret.stdout.decode().strip().splitlines()[0]
+        if not p.isfile(cygpath):
+            cygpath = None
+    if not cygpath:
+        bash_dir = p.dirname(bash)
+        cygpath = where('cygpath', [bash_dir, rf'{p.dirname(bash_dir)}\usr\bin',
+                        rf'{bash_dir}\usr\bin'])
+    return bash, cygpath
+
+
+def is_bin_ext_maybe(output: str, to: str=None, search_dirs: Iterable[str]=None,
+                     force_pandoc: bool=False) -> bool:
+    """
+    Nice guess if the ``output`` extension (or ``to`` if no ext) means
+    that Pandoc needs adding ``-o "${output_file}"`` option.
+
+    :param output: Pandoc writer option
+    :param to: Pandoc writer option.
+        Used only if output doesn't have an extension
+    :param search_dirs: extra dirs to look for executables
+    :param force_pandoc: ignore hardcoded logic and always "ask" Pandoc.
+        Useful for testing hardcoded logic.
+    """
+    ext = p.splitext(p.basename(output))[1][1:]
+    if not ext:
+        ext = to
+
+    if not ext:
+        return False
+    elif ext in ('pdf', 'docx', 'epub', 'odt') and (not force_pandoc):  # TODO add more
+        return True
+    else:
+        import re
+        from knitty.pandoc_filter_arg import doc
+
+        pandoc, panfl = where('pandoc', search_dirs), where('panfl', search_dirs)
+        err = run([pandoc, '-f', 'markdown', '--filter', panfl, '-t', ext],
+                  stderr=PIPE, stdout=PIPE, input=doc.encode()).stderr
+        err = err.decode() if err else ''
+        if re.search(r"(Cannot write \w+ output to terminal|specify an output file)", err):
+            return True
+        else:
+            return False
---
+++ packages/pandoctools/pandoctools-2.6.0.3.tar.gz/pandoctools-2.6.0.3/setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup, find_packages
+import io
+import os
+import versioneer
+
+here = os.path.abspath(os.path.dirname(__file__))
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='pandoctools',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+
+    description='Profile manager of text processing pipelines: Pandoc filters, any text CLI filters. Atom+Markdown+Pandoc+Jupyter workflow, export to ipynb.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    url='https://github.com/kiwi0fruit/pandoctools',
+
+    author='Peter Zagubisalo',
+    author_email='peter.zagubisalo@gmail.com',
+
+    license='GPLv2+',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    # keywords='sample setuptools development',
+    packages=find_packages(exclude=['docs', 'tests']),
+    python_requires='>=3.6',
+    install_requires=['click', 'pyyaml', 'notebook', 'jupyter', 'libsass',
+                      'panflute>=1.11.2', 'knitty>=0.5.0', 'pyppdf>=0.0.10',
+                      'sugartex>=0.1.16', 'shortcutter>=0.1.19', 'numpy',
+                      'py-pandoc>=2.6', 'py-pandoc-crossref>=0.3.4.0.3', 'py-mathjax'],
+    # click pyyaml notebook jupyter libsass "panflute>=1.11.2" "knitty>=0.5.0" "pyppdf>=0.0.10" "sugartex>=0.1.16" "shortcutter>=0.1.19" "numpy" "py-pandoc>=2.6" "py-pandoc-crossref>=0.3.4.0.3" py-mathjax
+
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'cat-md=pandoctools.cat_md.cat_md:cli',
+            'pandoctools=pandoctools.cli.cli:pandoctools',
+            'pandoctools-resolve=pandoctools.pandoctools_resolve.resolve:cli',
+            'pandoctools-ready=pandoctools.ready.ready:ready',
+        ],
+    },
+)
