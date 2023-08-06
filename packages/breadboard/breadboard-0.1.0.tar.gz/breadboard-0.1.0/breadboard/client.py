38c041de15b0441722b1457d4cebcdf24f609b52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:20 +0000

    Add package breadboard/0.1.0/breadboard-0.1.0.tar.gz

---
+++ packages/breadboard/breadboard-0.1.0.tar.gz/breadboard-0.1.0/breadboard/client.py
@@ -0,0 +1,41 @@
+import requests
+import json
+
+from breadboard.auth import BreadboardAuth
+from breadboard.mixins import ImageMixins
+
+
+class BreadboardClient(ImageMixins.ImageMixin):
+    def __init__(self, config_path, lab_name=None):
+
+        if not config_path:
+            raise ValueError("Please enter a directory for your API configuration json file")
+
+        with open(config_path) as file:
+            api_config = json.load(file)
+
+        self.auth = BreadboardAuth(api_config.get('api_key'))
+
+        if api_config.get('api_url')==None:
+            self.api_url = 'http://breadboard-215702.appspot.com'
+        else:
+            self.api_url = api_config.get('api_url').rstrip('/')
+
+        if lab_name==None:
+            if api_config.get('lab_name')==None:
+                raise ValueError("Please enter a lab name.")
+            else:
+                self.lab_name = api_config.get('lab_name')
+        else:
+            self.lab_name = lab_name
+
+        self.session = requests.Session()
+
+
+    def _send_message(self, method, endpoint, params=None, data=None):
+        """ Send an HTTP message to the API
+        """
+        url = self.api_url + endpoint
+        r = self.session.request(method, url, params=params, data=data,
+                                 headers=self.auth.headers, timeout=30)
+        return r
---
+++ packages/breadboard/breadboard-0.1.0.tar.gz/breadboard-0.1.0/breadboard/mixins/ImageMixins.py
@@ -0,0 +1,145 @@
+
+import json
+import pandas as pd
+import datetime
+
+TIMEFORMATS = {
+    'BEC1': '%m-%d-%Y_%H_%M_%S',
+    'FERMI3':'%Y-%m-%d_%H_%M_%S'
+}
+
+def timestr_to_datetime(time_string, format=None):
+    time_string = re.sub(' ','0',time_string[0:19])
+    if not format: format = TIMEFORMATS['FERMI3']
+    return datetime.datetime.strptime(time_string,format)
+
+
+def clean_image_time(image_time):
+    if type(image_time)==datetime.datetime:
+        return image_time.isoformat()+'Z'
+    else:
+        return image_time
+
+
+class ImageMixin:
+    """ Useful functions for Image queries through the breadboard Client
+    Plugs into breadboard/client.py
+    """
+
+    def update_image(self, id, image_name, params ):
+        # return all the API data corresponding to a set of images as JSON
+        # todo: validate inputs
+        payload = {
+            'name': image_name
+        }
+        payload = {**payload, **params}
+        if isinstance(id, float):
+            id = int(id)
+        update_url =  '/images/'+str(id)+'/'
+        response = self._send_message('PUT', update_url,
+                            data=json.dumps(payload)
+                            )
+        return response
+
+
+    def get_images_json(self, image_names=None, auto_time=False, image_times=None, force_match=False, datetime_range=None ):
+        # return all the API data corresponding to a set of images as JSON
+        # todo: handle error codes
+        """
+        Query modes:
+        0) Nothing: returns a list of images
+        1) Just a list of image names
+        2) Image names + image times
+        3) Force match : if you want to set the runtimes of the images
+        4) datetime range: a [start, end] array of python datetimes
+        """
+        if image_names:
+            if isinstance(image_names,str):
+                image_names = [image_names]
+            namelist = ','.join(image_names)
+        else:
+            namelist = None
+
+        if self.lab_name=='bec1':
+            imagetimeformat = TIMEFORMATS['BEC1']
+        else:
+            imagetimeformat = TIMEFORMATS['FERMI3']
+
+        if auto_time:
+            image_times = [timestr_to_datetime(image_name, format=imagetimeformat) for image_name in image_names]
+
+        if image_times:
+            image_times = [clean_image_time(image_time) for image_time in image_times]
+
+        if datetime_range:
+            datetime_range = [clean_image_time(image_time) for image_time in datetime_range]
+
+        payload_dirty = {
+            'lab': self.lab_name,
+            'names': namelist,
+            'force_match': force_match,
+            'image_times': image_times,
+            'datetime_range': datetime_range
+        }
+        payload_clean = {k: v for k, v in payload_dirty.items() if not (
+                        v==None or
+                        (isinstance(v, tuple) and (None in v))
+                )}
+
+        response = self._send_message('get', '/images', params=payload_clean)
+        return response
+
+
+
+
+    def get_images_df(self, image_names, paramsin="*"):
+        """ Return a pandas dataframe for the given imagenames
+        """
+        if isinstance(image_names,str):
+            image_names = [image_names]
+
+        # Get data
+        response = self.get_images_json(image_names)
+        jsonresponse = response.json()
+
+        # Prepare df
+        df = pd.DataFrame(columns = ['imagename'])
+        df['imagename'] = image_names
+
+        # Prepare params:
+        paramsall = set(jsonresponse[0].keys())
+        if paramsin=='*':
+            #  Get all params
+            for jr in jsonresponse:
+                params = set(jr['run']['parameters'].keys())
+                paramsall = paramsall.union(params)
+        else:
+            if isinstance(paramsin, str):
+                paramsin = [paramsin]
+            # use set of params provided
+            paramsall = paramsall.union(set(paramsin))
+
+        removeparams = set([
+                    'run',
+                    'name',
+                    'thumbnail',
+                    'atomsperpixel',
+                    'odpath',
+                    'settings',
+                    'ListBoundVariables',
+                    'camera',
+                    ])
+        paramsall = paramsall - removeparams
+
+        # Populate dataframe
+        for i,r in df.iterrows():
+            for param in paramsall:
+                try: # to get the run parameters
+                    df.at[i,param] = jsonresponse[i]['run']['parameters'][param]
+                except:
+                    try:# to get the bare image parameters
+                        df.at[i,param] = jsonresponse[i][param]
+                    except: # nan the rest
+                        df.at[i,param] = float('nan')
+
+        return df
