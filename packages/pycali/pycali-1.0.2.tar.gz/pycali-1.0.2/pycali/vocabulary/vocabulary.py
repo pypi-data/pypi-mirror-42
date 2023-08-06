671e6eedc7a778a0752c2abfa973a7e1387c0145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:08 +0000

    Add package pycali/1.0.2/pycali-1.0.2.tar.gz

---
+++ packages/pycali/pycali-1.0.2.tar.gz/pycali-1.0.2/pycali/vocabulary/vocabulary.py
@@ -0,0 +1,43 @@
+from rdflib import Graph, RDF, Namespace
+
+ODRL = Namespace('http://www.w3.org/ns/odrl/2/')
+CC = Namespace('http://creativecommons.org/ns#')
+
+
+class Vocabulary(object):
+    """A Vocabulary.
+
+    A vocabulary is a set of actions.
+    """
+
+    def __init__(self, rdf_vocabulary_graph):
+        """
+        Vocabulary Constructor.
+
+        Init function accepts a rdf vocabulary (rdflib.Graph)
+        and creates an attribute 'actions' containing
+        all actions of the vocabulary.
+        """
+        self.actions = []
+
+
+class ODRLVocabulary(Vocabulary):
+    """The ODRL Vocabulary.
+
+    Contains a set of all actions from ODRL
+    vocabulary 2.2
+    see. https://www.w3.org/TR/odrl-vocab/#actionConcepts
+    """
+
+    def __init__(self, rdf_vocabulary_graph=None):
+        """
+        Vocabulary Constructor.
+
+        Init function accepts ODRL rdf vocabulary
+        and create an attribute 'actions' containing
+        all actions of ODRL vocabulary.
+        """
+        Vocabulary.__init__(self, None)
+        odrl = Graph().parse(location='pycali/vocabulary/ontologies/ODRL22.rdf')
+        for action in odrl.subjects(predicate=RDF.type, object=ODRL['Action']):
+            self.actions.append(action)
