57b3f5e005a651204c4d09b2fe29678efa338ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:09 +0000

    Add package jj/2.0.0.dev2/jj-2.0.0.dev2.tar.gz

---
+++ packages/jj/jj-2.0.0.dev2.tar.gz/jj-2.0.0.dev2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="jj",
+    version="2.0.0-dev.2",
+    description="",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Nikita Tsvetkov",
+    author_email="nikitanovosibirsk@yandex.com",
+    python_requires=">=3.6.0",
+    url="https://github.com/nikitanovosibirsk/jj",
+    license="Apache 2",
+    packages=find_packages(exclude=("tests",)),
+    install_requires=[
+        "aiohttp==3.5.4",
+        "undecorated==0.3.0",
+    ],
+    tests_require=[
+        "asynctest==0.12.2",
+        "mypy==0.670",
+        "flake8==3.7.7",
+        "coverage==4.5.2",
+        "codecov==2.0.15",
+    ],
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
