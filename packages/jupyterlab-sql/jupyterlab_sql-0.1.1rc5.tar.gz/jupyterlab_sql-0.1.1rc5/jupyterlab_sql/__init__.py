50105f1876a4bb9513e9f3a775af7ffddc6e1bc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:43 +0000

    Add package jupyterlab-sql/0.1.1rc5/jupyterlab_sql-0.1.1rc5.tar.gz

---
+++ packages/jupyterlab-sql/jupyterlab_sql-0.1.1rc5.tar.gz/jupyterlab_sql-0.1.1rc5/jupyterlab_sql/__init__.py
@@ -0,0 +1,12 @@
+from .handlers import register_handlers
+
+
+def _jupyter_server_extension_paths():
+    return [{
+        "module": "jupyterlab_sql"
+    }]
+
+
+def load_jupyter_server_extension(nbapp):
+    nbapp.log.info("Loading server extension jupyterlab_sql")
+    register_handlers(nbapp)
