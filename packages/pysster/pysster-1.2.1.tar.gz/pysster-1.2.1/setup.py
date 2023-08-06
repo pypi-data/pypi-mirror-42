cf8739741016d9b6bd214566fb79a2cd26458973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:18 +0000

    Add package pysster/1.2.1/pysster-1.2.1.tar.gz

---
+++ packages/pysster/pysster-1.2.1.tar.gz/pysster-1.2.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name = 'pysster',
+    version = '1.2.1',
+    description = 'a Sequence/STructure classifiER for biological sequences',
+    url = 'https://github.com/budach/pysster',
+    author = 'Stefan Budach',
+    author_email = 'budach@molgen.mpg.de',
+    license = 'MIT',
+    install_requires =  [
+        'numpy>=1.14.0',
+        'matplotlib',
+        'seaborn',
+        'scikit-learn',
+        'keras>=2.1.3',
+        'tensorflow!=1.8',
+        'h5py',
+        'logging_exceptions',
+        'Pillow',
+        'forgi'
+    ],
+    packages = ['pysster'],
+    python_requires = '>=3.5',
+    include_package_data = True,
+    zip_safe = False
+)
