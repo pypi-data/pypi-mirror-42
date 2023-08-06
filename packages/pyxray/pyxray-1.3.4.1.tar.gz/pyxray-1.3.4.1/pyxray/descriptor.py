cc74eb280e7c591a316e38f3b9a30dc5bab334a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:59 +0000

    Add package pyxray/1.3.4.1/pyxray-1.3.4.1.tar.gz

---
+++ packages/pyxray/pyxray-1.3.4.1.tar.gz/pyxray-1.3.4.1/pyxray/descriptor.py
@@ -0,0 +1,248 @@
+"""
+Definition of descriptors.
+"""
+
+__all__ = ['Element', 'AtomicShell', 'AtomicSubshell', 'XrayTransition',
+           'XrayTransitionSet', 'XrayLine', 'Language', 'Notation', 'Reference']
+
+# Standard library modules.
+
+# Third party modules.
+
+# Local modules.
+from pyxray.cbook import Immutable, Cachable, Validable, Reprable
+
+# Globals and constants variables.
+
+class _Descriptor(Immutable, Validable, Cachable, Reprable):
+    pass
+
+class Element(metaclass=_Descriptor,
+              attrs=('atomic_number',)):
+
+    @classmethod
+    def validate(cls, atomic_number):
+        if atomic_number < 1 or atomic_number > 118:
+            raise ValueError('Atomic number ({0}) must be [1, 118]'
+                             .format(atomic_number))
+
+    def _repr_inner(self):
+        return 'z={0}'.format(self.z)
+
+    @property
+    def z(self):
+        return self.atomic_number
+
+class AtomicShell(metaclass=_Descriptor,
+                  attrs=('principal_quantum_number',)):
+
+    @classmethod
+    def validate(cls, principal_quantum_number):
+        if principal_quantum_number < 1:
+            raise ValueError('Principal quantum number ({0}) must be [1, inf['
+                             .format(principal_quantum_number))
+
+    def _repr_inner(self):
+        return 'n={0}'.format(self.n)
+
+    @property
+    def n(self):
+        return self.principal_quantum_number
+
+class AtomicSubshell(metaclass=_Descriptor,
+                     attrs=('atomic_shell',
+                            'azimuthal_quantum_number',
+                            'total_angular_momentum_nominator')):
+
+    @classmethod
+    def validate(cls,
+                 atomic_shell,
+                 azimuthal_quantum_number,
+                 total_angular_momentum_nominator):
+        if not isinstance(atomic_shell, AtomicShell):
+            atomic_shell = AtomicShell(atomic_shell)
+
+        lmin = 0
+        lmax = atomic_shell.principal_quantum_number - 1
+        jmin_n = 2 * abs(azimuthal_quantum_number - 0.5)
+        jmax_n = 2 * abs(azimuthal_quantum_number + 0.5)
+
+        if azimuthal_quantum_number < lmin or \
+                azimuthal_quantum_number > lmax:
+            raise ValueError('Azimuthal quantum number ({0}) must be between [{1}, {2}]'
+                             .format(azimuthal_quantum_number, lmin, lmax))
+        if total_angular_momentum_nominator < jmin_n or \
+                total_angular_momentum_nominator > jmax_n:
+            raise ValueError('Total angular momentum ({0}) must be between [{1}, {2}]'
+                             .format(total_angular_momentum_nominator, jmin_n, jmax_n))
+
+        return (atomic_shell,
+                azimuthal_quantum_number,
+                total_angular_momentum_nominator)
+
+    def _repr_inner(self):
+        return 'n={0}, l={1}, j={2:.1f}'.format(self.n, self.l, self.j)
+
+    @property
+    def principal_quantum_number(self):
+        return self.atomic_shell.principal_quantum_number
+
+    @property
+    def n(self):
+        return self.principal_quantum_number
+
+    @property
+    def l(self):
+        return self.azimuthal_quantum_number
+
+    @property
+    def j_n(self):
+        return self.total_angular_momentum_nominator
+
+    @property
+    def total_angular_momentum(self):
+        return self.total_angular_momentum_nominator / 2.0
+
+    @property
+    def j(self):
+        return self.total_angular_momentum
+
+class XrayTransition(metaclass=_Descriptor,
+                     attrs=('source_subshell',
+                            'destination_subshell')):
+
+    @classmethod
+    def is_radiative(cls, source_subshell, destination_subshell):
+        """
+        Inspired from NIST EPQ library by Nicholas Ritchie.
+        """
+        def electric_dipole_permitted(n0, l0, j0_n, n1, l1, j1_n):
+            delta_j_n = abs(j1_n - j0_n)
+            if delta_j_n > 2:
+                return False
+            assert delta_j_n == 0 or delta_j_n == 2
+            return abs(l1 - l0) == 1
+
+        def electric_quadrupole_permitted(n0, l0, j0_n, n1, l1, j1_n):
+            delta_j_n = abs(j1_n - j0_n)
+            if delta_j_n > 4:
+                return False
+            if j0_n == 1 and j1_n == 1:
+                return False
+            assert delta_j_n == 0 or delta_j_n == 2 or delta_j_n == 4
+
+            delta_l = abs(l1 - l0)
+            return delta_l == 0 or delta_l == 2
+
+        n0 = source_subshell.n
+        l0 = source_subshell.l
+        j0_n = source_subshell.j_n
+        n1 = destination_subshell.n
+        l1 = destination_subshell.l
+        j1_n = destination_subshell.j_n
+
+        if n0 == n1:
+            return False
+
+        if not(electric_dipole_permitted(n0, l0, j0_n, n1, l1, j1_n) or \
+               electric_quadrupole_permitted(n0, l0, j0_n, n1, l1, j1_n)):
+            return False
+
+        return True
+
+    @classmethod
+    def validate(cls, source_subshell, destination_subshell):
+        if not isinstance(source_subshell, AtomicSubshell):
+            source_subshell = AtomicSubshell(source_subshell)
+        if not isinstance(destination_subshell, AtomicSubshell):
+            destination_subshell = AtomicSubshell(destination_subshell)
+
+        return (source_subshell, destination_subshell)
+
+    def _repr_inner(self):
+        r = '[n={src.n}, l={src.l}, j={src.j:.1f}]'
+        r += ' -> [n={dest.n}, l={dest.l}, j={dest.j:.1f}]'
+        return r.format(src=self.source_subshell,
+                        dest=self.destination_subshell)
+
+class XrayTransitionSet(metaclass=_Descriptor,
+                        attrs=('possible_transitions',)):
+
+    @classmethod
+    def validate(cls, possible_transitions):
+        transitions2 = set()
+        for transition in possible_transitions:
+            if not isinstance(transition, XrayTransition):
+                transition = XrayTransition(*transition)
+            transitions2.add(transition)
+        return (frozenset(transitions2),)
+
+    def _repr_inner(self):
+        return '{0:d} possible transitions'.format(len(self.possible_transitions))
+
+class XrayLine(metaclass=_Descriptor,
+               attrs=('element', 'transitions', 'iupac', 'siegbahn', 'energy_eV')):
+
+    @classmethod
+    def validate(cls, element, transitions, iupac, siegbahn, energy_eV):
+        if not isinstance(element, Element):
+            element = Element(element)
+        transitions = tuple(transitions)
+        return (element, transitions, iupac, siegbahn, energy_eV)
+
+    def _repr_inner(self):
+        return self.iupac
+
+    @property
+    def atomic_number(self):
+        return self.element.atomic_number
+
+    @property
+    def z(self):
+        return self.element.atomic_number
+
+class Language(metaclass=_Descriptor,
+               attrs=('code',)):
+
+    @classmethod
+    def validate(cls, code):
+        lencode = len(code)
+        if lencode < 2 or lencode > 3:
+            raise ValueError('Code must be between 2 and 3 characters')
+        code = code.lower()
+        return (code,)
+
+class Notation(metaclass=_Descriptor,
+               attrs=('name',)):
+
+    @classmethod
+    def validate(cls, name):
+        if not name:
+            raise ValueError('Name cannot be empty')
+        name = name.lower()
+        return (name,)
+
+class Reference(metaclass=_Descriptor,
+                attrs=('bibtexkey', 'author', 'year', 'title', 'type',
+                       'booktitle', 'editor', 'pages', 'edition',
+                       'journal', 'school', 'address', 'url', 'note',
+                       'number', 'series', 'volume', 'publisher',
+                       'organization', 'chapter', 'howpublished', 'doi')):
+
+    @classmethod
+    def validate(cls, bibtexkey, author=None, year=None, title=None,
+                type=None, booktitle=None, editor=None, pages=None, #@ReservedAssignment
+                edition=None, journal=None, school=None, address=None,
+                url=None, note=None, number=None, series=None, volume=None,
+                publisher=None, organization=None, chapter=None,
+                howpublished=None, doi=None):
+        if not bibtexkey:
+            raise ValueError('A BibTeX key must be defined')
+
+        return (bibtexkey, author, year, title, type, booktitle, editor,
+                pages, edition, journal, school, address, url, note,
+                number, series, volume, publisher, organization,
+                chapter, howpublished, doi)
+
+    def _repr_inner(self):
+        return '{0}'.format(self.bibtexkey)
---
+++ packages/pyxray/pyxray-1.3.4.1.tar.gz/pyxray-1.3.4.1/pyxray/test_descriptor.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env python
+""" """
+
+# Standard library modules.
+import unittest
+import logging
+
+# Third party modules.
+
+# Local modules.
+from pyxray.descriptor import \
+    (Element, AtomicShell, AtomicSubshell, Reference, XrayLine, XrayTransition)
+
+# Globals and constants variables.
+
+class TestElement(unittest.TestCase):
+
+    def setUp(self):
+        super().setUp()
+
+        self.element = Element(6)
+
+    def test__init__(self):
+        self.assertEqual(6, self.element.z)
+        self.assertEqual(6, self.element.atomic_number)
+
+    def test__hash__(self):
+        self.assertEqual(hash(Element(6)), hash(self.element))
+
+    def testvalidable(self):
+        self.assertRaises(ValueError, Element, 0)
+        self.assertRaises(ValueError, Element, 119)
+        self.assertRaises(ValueError, Element.validate, 0)
+        self.assertRaises(ValueError, Element.validate, 119)
+
+    def testimmutable(self):
+        self.assertRaises(AttributeError, setattr,
+                          self.element, 'atomic_number', 7)
+        self.assertRaises(AttributeError, delattr,
+                          self.element, 'atomic_number')
+        self.assertRaises(AttributeError, setattr,
+                          self.element, 'abc', 7)
+
+    def testcachable(self):
+        self.assertEqual(Element(6), self.element)
+        self.assertIs(Element(6), self.element)
+
+    def testreprable(self):
+        self.assertEqual('Element(z=6)', repr(self.element))
+
+class TestAtomicShell(unittest.TestCase):
+
+    def setUp(self):
+        super().setUp()
+
+        self.atomicshell = AtomicShell(3)
+
+    def test__init__(self):
+        self.assertEqual(3, self.atomicshell.n)
+        self.assertEqual(3, self.atomicshell.principal_quantum_number)
+
+    def testvalidable(self):
+        self.assertRaises(ValueError, AtomicShell, 0)
+        self.assertRaises(ValueError, AtomicShell.validate, 0)
+
+    def testimmutable(self):
+        self.assertRaises(AttributeError, setattr,
+                          self.atomicshell, 'principal_quantum_number', 4)
+        self.assertRaises(AttributeError, delattr,
+                          self.atomicshell, 'principal_quantum_number')
+        self.assertRaises(AttributeError, setattr,
+                          self.atomicshell, 'abc', 7)
+
+    def testcachable(self):
+        self.assertEqual(AtomicShell(3), self.atomicshell)
+        self.assertIs(AtomicShell(3), self.atomicshell)
+
+    def testreprable(self):
+        self.assertEqual('AtomicShell(n=3)', repr(self.atomicshell))
+
+class TestAtomicSubshell(unittest.TestCase):
+
+    def setUp(self):
+        super().setUp()
+
+        self.atomicsubshell = AtomicSubshell(3, 0, 1)
+
+    def test__init__(self):
+        self.assertEqual(AtomicShell(3), self.atomicsubshell.atomic_shell)
+        self.assertEqual(3, self.atomicsubshell.n)
+        self.assertEqual(3, self.atomicsubshell.principal_quantum_number)
+        self.assertEqual(0, self.atomicsubshell.l)
+        self.assertEqual(0, self.atomicsubshell.azimuthal_quantum_number)
+        self.assertEqual(1, self.atomicsubshell.j_n)
+        self.assertEqual(1, self.atomicsubshell.total_angular_momentum_nominator)
+        self.assertAlmostEqual(0.5, self.atomicsubshell.j)
+        self.assertAlmostEqual(0.5, self.atomicsubshell.total_angular_momentum)
+
+    def testvalidable(self):
+        self.assertRaises(ValueError, AtomicSubshell, 3, 0, 5)
+        self.assertRaises(ValueError, AtomicSubshell.validate, 3, 0, 5)
+        self.assertRaises(ValueError, AtomicSubshell, 3, -1, 1)
+        self.assertRaises(ValueError, AtomicSubshell.validate, 3, -1, 1)
+        self.assertRaises(ValueError, AtomicSubshell, 3, 3, 1)
+        self.assertRaises(ValueError, AtomicSubshell.validate, 3, 3, 1)
+
+    def testimmutable(self):
+        self.assertRaises(AttributeError, setattr,
+                          self.atomicsubshell, 'principal_quantum_number', 4)
+        self.assertRaises(AttributeError, delattr,
+                          self.atomicsubshell, 'principal_quantum_number')
+        self.assertRaises(AttributeError, setattr,
+                          self.atomicsubshell, 'abc', 7)
+
+    def testcachable(self):
+        self.assertEqual(AtomicSubshell(3, 0, 1), self.atomicsubshell)
+        self.assertIs(AtomicSubshell(3, 0, 1), self.atomicsubshell)
+
+    def testreprable(self):
+        self.assertEqual('AtomicSubshell(n=3, l=0, j=0.5)', repr(self.atomicsubshell))
+
+class TestReference(unittest.TestCase):
+
+    def setUp(self):
+        super().setUp()
+
+        self.ref = Reference('doe2016')
+
+    def test__init__(self):
+        self.assertEqual('doe2016', self.ref.bibtexkey)
+        self.assertIsNone(self.ref.author)
+
+    def testvalidable(self):
+        self.assertRaises(ValueError, Reference, '')
+        self.assertRaises(ValueError, Reference.validate, '')
+        self.assertRaises(ValueError, Reference, None)
+        self.assertRaises(ValueError, Reference.validate, None)
+
+    def testimmutable(self):
+        self.assertRaises(AttributeError, setattr,
+                          self.ref, 'bibtexkey', 'john')
+        self.assertRaises(AttributeError, delattr,
+                          self.ref, 'bibtexkey')
+        self.assertRaises(AttributeError, setattr,
+                          self.ref, 'abc', 7)
+
+    def testcachable(self):
+        self.assertEqual(Reference('doe2016'), self.ref)
+        self.assertIs(Reference('doe2016'), self.ref)
+
+    def testreprable(self):
+        self.assertEqual('Reference(doe2016)', repr(self.ref))
+
+class TestXrayLine(unittest.TestCase):
+
+    def setUp(self):
+        super().setUp()
+
+        element = Element(118)
+        K = AtomicSubshell(1, 0, 1)
+        L3 = AtomicSubshell(2, 1, 3)
+        transition = XrayTransition(L3, K)
+        self.xrayline = XrayLine(element, [transition], 'a', 'b', 0.1)
+
+    def test__init__(self):
+        self.assertEqual(118, self.xrayline.element.atomic_number)
+        self.assertEqual(1, len(self.xrayline.transitions))
+        self.assertEqual('a', self.xrayline.iupac)
+        self.assertEqual('b', self.xrayline.siegbahn)
+        self.assertAlmostEqual(0.1, self.xrayline.energy_eV, 4)
+
+    def test__hash__(self):
+        K = AtomicSubshell(1, 0, 1)
+        L3 = AtomicSubshell(2, 1, 3)
+        transition = XrayTransition(L3, K)
+        other = XrayLine(118, [transition], 'a', 'b', 0.1)
+        self.assertEqual(hash(self.xrayline), hash(other))
+        self.assertEqual(hash(self.xrayline), hash(self.xrayline))
+
+    def testimmutable(self):
+        self.assertRaises(AttributeError, setattr,
+                          self.xrayline, 'element', 'john')
+        self.assertRaises(AttributeError, delattr,
+                          self.xrayline, 'element')
+
+    def testcachable(self):
+        K = AtomicSubshell(1, 0, 1)
+        L3 = AtomicSubshell(2, 1, 3)
+        transition = XrayTransition(L3, K)
+        other = XrayLine(118, [transition], 'a', 'b', 0.1)
+
+        self.assertEqual(other, self.xrayline)
+        self.assertIs(other, self.xrayline)
+
+    def testreprable(self):
+        self.assertEqual('XrayLine(a)', repr(self.xrayline))
+
+if __name__ == '__main__': #pragma: no cover
+    logging.getLogger().setLevel(logging.DEBUG)
+    unittest.main()
