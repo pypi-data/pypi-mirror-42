301b348e8c868f38d189c00cf387b57b0b71356c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:21 +0000

    Add package example-pkg-sylar.liu123/0.0.2/example_pkg_sylar.liu123-0.0.2.tar.gz

---
+++ packages/example-pkg-sylar.liu123/example_pkg_sylar.liu123-0.0.2.tar.gz/example_pkg_sylar.liu123-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = 'example_pkg_sylar.liu123',
+    version = '0.0.2',
+    author = 'sylar.liu',
+    author_email = 'sylar_liu65@163.com',
+    description = 'A small example package',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/pypa/sampleproject',
+    packages = setuptools.find_packages(),
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent' ,
+    ],
+)
