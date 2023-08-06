337df6797e70e6bfaea07841c3e525deb65ea0d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:38 +0000

    Add package pybel-jupyter/0.2.1/pybel_jupyter-0.2.1-py3-none-any.whl

---
+++ packages/pybel-jupyter/pybel_jupyter-0.2.1-py3-none-any.whl/pybel_jupyter/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+"""A PyBEL extension for Jupyter notebooks."""
+
+from .inline import to_jupyter, to_jupyter_str  # noqa: F401
+from .utils import get_version  # noqa: F401
+from .visualization import to_html, to_html_file, to_html_path  # noqa: F401
+
+__all__ = [
+    'to_html',
+    'to_html_file',
+    'to_html_path',
+    'to_jupyter',
+    'to_jupyter_str',
+]
+
+__version__ = '0.2.1'
+
+__title__ = 'pybel_jupyter'
+__description__ = 'A PyBEL extension for Jupyter notebooks'
+__url__ = 'https://github.com/pybel/pybel-jupyter'
+
+__author__ = 'Charles Tapley Hoyt'
+__email__ = 'charles.hoyt@scai.fraunhofer.de'
+
+__license__ = 'MIT License'
+__copyright__ = 'Copyright (c) 2018 Charles Tapley Hoyt'
