f297e1b26bdb1ca4e40c234fd34e4dce22e7f055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:47 +0000

    Add package torch-gpipe/0.0.0a0/torch-gpipe-0.0.0a0.tar.gz

---
+++ packages/torch-gpipe/torch-gpipe-0.0.0a0.tar.gz/torch-gpipe-0.0.0a0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='torch-gpipe',
+    version='0.0.0-alpha',
+    author='Kakao Brain',
+    maintainer='Heungsub Lee',
+    maintainer_email='heungsub.lee@kakaobrain.com',
+    zip_safe=False,
+    py_modules=['torch_gpipe'],
+)
