7deaf6e599ac2670eb767bcead60a7415da95924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:47 +0000

    Add package pandas-npi/0.45/pandas_npi-0.45.tar.gz

---
+++ packages/pandas-npi/pandas_npi-0.45.tar.gz/pandas_npi-0.45/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(name='pandas_npi',
+      version='0.45',
+      description='NPI validation made easy with Pandas.',
+      url='https://github.com/Lyonk71/pandas-npi',
+      author='Keith Lyons',
+      author_email='lyonk71@gmail.com',
+      #license='MIT',
+      packages=['pandas_npi'],
+      install_requires=[
+          'pandas',
+          'numpy',
+      ],
+      zip_safe=False,
+      
+      include_package_data=True,
+      
+      #Enable pypi description
+      long_description=long_description,
+      long_description_content_type='text/markdown')
