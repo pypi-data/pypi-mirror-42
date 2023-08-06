5f430927d3b999a42a965e058e80496f445b7e5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:13 +0000

    Add package ost-kyc-sdk-python/2.0.2/ost_kyc_sdk_python-2.0.2.tar.gz

---
+++ packages/ost-kyc-sdk-python/ost_kyc_sdk_python-2.0.2.tar.gz/ost_kyc_sdk_python-2.0.2/setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name='ost_kyc_sdk_python',    
+      version = '2.0.2',
+      description = 'OST KYC SDK in python',
+      long_description = read('README.md'),
+      long_description_content_type='text/markdown',
+      url='https://github.com/ostdotcom/ost-kyc-sdk-python',
+      packages=find_packages(),
+      install_requires=['requests'],
+      license="MIT",
+      zip_safe=False
+      )
