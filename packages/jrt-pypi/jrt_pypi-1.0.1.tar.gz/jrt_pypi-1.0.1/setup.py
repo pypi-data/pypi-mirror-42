875acf955ec0f9ed0c4e00895f6c8c75c26fc5c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:51 +0000

    Add package jrt-pypi/1.0.1/jrt_pypi-1.0.1.tar.gz

---
+++ packages/jrt-pypi/jrt_pypi-1.0.1.tar.gz/jrt_pypi-1.0.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='jrt_pypi',
+    version='1.0.1',
+    description='python lib',
+    author='runtao',
+    author_email='heavycross@163.com',
+    url='https://github.com/HeavyCross/my_site',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+    ],  
+    keywords='pypi',
+    packages=['jrt_pypi'],
+)
