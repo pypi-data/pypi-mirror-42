f2f14fd45cf1ab247a531898f0a9964850df6c98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:24 +0000

    Add package onegov.winterthur/0.3.5/onegov.winterthur-0.3.5-py3-none-any.whl

---
+++ packages/onegov.winterthur/onegov.winterthur-0.3.5-py3-none-any.whl/onegov/winterthur/layout.py
@@ -0,0 +1,45 @@
+from cached_property import cached_property
+from onegov.org.layout import DefaultLayout
+from onegov.core.elements import Link, Intercooler
+from onegov.winterthur import _
+from onegov.winterthur.collections import AddressCollection
+
+
+class AddressLayout(DefaultLayout):
+
+    @cached_property
+    def breadcrumbs(self):
+        return [
+            Link(_("Homepage"), self.homepage_url),
+            Link(_("Addresses"), '#'),
+        ]
+
+    @cached_property
+    def editbar_links(self):
+        if not self.request.is_manager:
+            return
+
+        return [
+            Link(
+                text=_("Update"),
+                url=self.csrf_protected_url(
+                    self.request.link(self.model, '+update')
+                ),
+                attrs={'class': 'sync'},
+                traits=Intercooler(
+                    request_method='POST',
+                    redirect_after=self.request.url
+                )
+            )
+        ]
+
+
+class AddressSubsetLayout(DefaultLayout):
+
+    @cached_property
+    def breadcrumbs(self):
+        return [
+            Link(_("Homepage"), self.homepage_url),
+            Link(_("Addresses"), self.request.class_link(AddressCollection)),
+            Link(_(self.model.street), '#')
+        ]
