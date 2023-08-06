258f17df22048831fe3a6e9b47c10073020cf26f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:10 +0000

    Add package pytransport/1.3.0/pytransport-1.3.0.tar.gz

---
+++ packages/pytransport/pytransport-1.3.0.tar.gz/pytransport-1.3.0/pytransport/__init__.py
@@ -0,0 +1,24 @@
+"""
+pytransport - Royal Holloway utility to manipulate TRANSPORT data and models.
+
+Authors:
+
+ * William Shields
+ * Jochem Snuverink
+
+Copyright Royal Holloway, University of London 2019.
+
+"""
+
+__version__ = "1.2.1"
+
+import _General
+import Compare
+import Convert
+import Data
+import Reader
+
+__all__ = ['Compare',
+           'Convert',
+           'Data',
+           'Reader']
