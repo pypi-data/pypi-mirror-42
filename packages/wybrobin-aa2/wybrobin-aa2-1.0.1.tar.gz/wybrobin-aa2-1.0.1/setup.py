8b3a2f08d57a5a2f9d0e4cbe868a0429b45f6134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:47 +0000

    Add package wybrobin-aa2/1.0.1/wybrobin-aa2-1.0.1.tar.gz

---
+++ packages/wybrobin-aa2/wybrobin-aa2-1.0.1.tar.gz/wybrobin-aa2-1.0.1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages            #这个包没有的可以pip一下
+
+setup(
+    name = "wybrobin-aa2",      #这里是pip项目发布的名称
+    version = "1.0.1",  #版本号，数值大的会优先被pip
+    keywords = ("pip", "aa"),
+    description = "aa test",
+    long_description = "aa test long",
+    license = "MIT Licence",
+
+    url = "https://github.com/wybrobin/packtest",     #项目相关文件地址，一般是github
+    author = "wyb",
+    author_email = "wyb_robin@163.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = []          #这个项目需要的第三方库
+)
