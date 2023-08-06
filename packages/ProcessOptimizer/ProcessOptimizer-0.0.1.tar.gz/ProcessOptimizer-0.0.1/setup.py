c36d708933cd20c8589c0e0ea2ca482565533513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:24 +0000

    Add package ProcessOptimizer/0.0.1/ProcessOptimizer-0.0.1.tar.gz

---
+++ packages/ProcessOptimizer/ProcessOptimizer-0.0.1.tar.gz/ProcessOptimizer-0.0.1/setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(name='ProcessOptimizer',
+      version='0.0.1',
+      description='Sequential model-based optimization toolbox (forked from scikit-optimize)',
+      long_description=open('README.md').read(),
+      url='https://github.com/bytesandbrains/ProcessOptimizer',
+      license='BSD',
+      author='Bytes and Brains',
+      packages=['ProcessOptimizer', 'ProcessOptimizer.learning', 'ProcessOptimizer.optimizer', 'ProcessOptimizer.space',
+                'ProcessOptimizer.learning.gaussian_process'],
+      install_requires=['pyaml', 'numpy', 'matplotlib', 'scipy>=0.14.0',
+                        'scikit-learn>=0.19.1']
+      )
