08c9829d04e635817590db9912283f7e5bc4e295
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:30 +0000

    Add package pysemigroup/0.3a2/pysemigroup-0.3a2-py2-none-any.whl

---
+++ packages/pysemigroup/pysemigroup-0.3a2-py2-none-any.whl/pysemigroup/__init__.py
@@ -0,0 +1,5 @@
+from .automata import Automaton
+from .transition_semigroup import TransitionSemiGroup
+from .transition_semigroup import BuchiTransitionOmegaSG
+from .transition_semigroup import monoidElement
+from .regular_language import RegularLanguage
