22274330cae9c33d72fab30232ad7b80f377bcf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:55 +0000

    Add package docker-detect/0.0.1/docker-detect-0.0.1.tar.gz

---
+++ packages/docker-detect/docker-detect-0.0.1.tar.gz/docker-detect-0.0.1/dockerdetect/detect.py
@@ -0,0 +1,9 @@
+import logging
+import os
+
+
+logger = logging.getLogger(__name__)
+
+
+def is_inside_container():
+    return False
---
+++ packages/docker-detect/docker-detect-0.0.1.tar.gz/docker-detect-0.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "rb") as fh:
+    long_description = fh.read().decode()
+
+setuptools.setup(
+    name="docker-detect",
+    version="0.0.1",
+    author="agmarx",
+    author_email="agmarx@protonmail.com",
+    description="Detect whether the current process is running inside Docker/Kubernetes/LXC container",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/agmarx/docker-detect",
+    packages=setuptools.find_packages(),
+    install_requires=[
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
