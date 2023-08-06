e8a1a075bb250dc73c4c844e84048409812eb2dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:41 +0000

    Add package pyforwarder/1.0.0/pyforwarder-1.0.0.tar.gz

---
+++ packages/pyforwarder/pyforwarder-1.0.0.tar.gz/pyforwarder-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name="pyforwarder",
+      version="1.0.0",
+      author="Rob Day",
+      author_email="rkd@rkd.me.uk",
+      url="https://github.com/rkday/pyforwarder",
+      tests_require=["pytest", "pytest-cov"],
+      setup_requires=['pytest-runner'],
+      description="Method forwarding library for Python",
+      long_description=open('README.rst').read(),
+      license="MIT",
+)
