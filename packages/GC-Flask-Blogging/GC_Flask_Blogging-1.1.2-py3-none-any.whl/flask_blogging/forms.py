a29a45b0d09d36265d113a50da2453246661dab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:01 +0000

    Add package GC-Flask-Blogging/1.1.2/GC_Flask_Blogging-1.1.2-py3-none-any.whl

---
+++ packages/GC-Flask-Blogging/GC_Flask_Blogging-1.1.2-py3-none-any.whl/flask_blogging/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField, BooleanField
+from wtforms.validators import DataRequired
+
+
+class BlogEditor(FlaskForm):
+    title = StringField("title", validators=[DataRequired()])
+    cover = StringField("cover", validators=[DataRequired()])
+    text = TextAreaField("text", validators=[DataRequired()])
+    tags = StringField("tags", validators=[DataRequired()])
+    draft = BooleanField("draft", default=False)
+    submit = SubmitField("submit")
