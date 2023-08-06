6247b9c37e70b75135cc30212b1277346dd97d50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:42 +0000

    Add package gokart/0.1.9/gokart-0.1.9.tar.gz

---
+++ packages/gokart/gokart-0.1.9.tar.gz/gokart-0.1.9/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+readme_note = """\
+.. note::
+
+   For the latest source, discussion, etc, please visit the
+   `GitHub repository <https://github.com/m3dev/gokart>`_\n\n
+"""
+
+with open('README.md') as f:
+    long_description = readme_note + f.read()
+
+setup(
+    name='gokart',
+    version='0.1.9',
+    description='A wrapper of luigi. This make it easy to define tasks.',
+    long_description=long_description,
+    author='M3, inc.',
+    url='https://github.com/m3dev/gokart',
+    license='MIT License',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+    ],
+)
