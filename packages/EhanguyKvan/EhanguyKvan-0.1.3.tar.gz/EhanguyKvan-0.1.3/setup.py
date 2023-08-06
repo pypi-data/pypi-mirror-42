a30c737410f086e8d69fc25a76ebe6d5d997d856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:38 +0000

    Add package EhanguyKvan/0.1.3/EhanguyKvan-0.1.3.tar.gz

---
+++ packages/EhanguyKvan/EhanguyKvan-0.1.3.tar.gz/EhanguyKvan-0.1.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "EhanguyKvan",
+    version = "0.1.3",
+    keywords = ("pip", "codemao"),
+    description = "自制的一个杂库，目前推广于codemao平台",
+    license = "MIT Licence",
+    url = "https://ehanguykvan.github.io/EhanguyKvan/EhanguyKvan.py",
+    author = "EhanguyKvan",
+    author_email = "1162971013@qq.com",
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = []
+)
