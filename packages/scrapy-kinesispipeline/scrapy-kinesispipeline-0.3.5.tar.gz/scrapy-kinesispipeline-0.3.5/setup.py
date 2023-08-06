21342553c17305128fc03cfde4f8191d3c9a4c30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:39 +0000

    Add package scrapy-kinesispipeline/0.3.5/scrapy-kinesispipeline-0.3.5.tar.gz

---
+++ packages/scrapy-kinesispipeline/scrapy-kinesispipeline-0.3.5.tar.gz/scrapy-kinesispipeline-0.3.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(name='scrapy-kinesispipeline',
+      version='0.3.5',
+      description='Scrapy pipeline to store aggregated items into AWS Kinesis',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Framework :: Scrapy',
+      ],
+      keywords='scrapy pipeline aws kinesis serverless',
+      url='https://github.com/fleiheit/scrapy-kinesispipeline',
+      author='Atabak Hafeez',
+      author_email='atabakhafeez@hotmail.com',
+      license='MIT',
+      packages=['kinesispipeline'],
+      install_requires=[
+          'Scrapy>=1.1',
+          'boto3',
+          'botocore'
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      zip_safe=False)
