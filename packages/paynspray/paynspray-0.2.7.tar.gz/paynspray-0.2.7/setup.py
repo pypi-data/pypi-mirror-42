b8b0b73bc9bf248f1255b87f96c84465053b2d45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:38 +0000

    Add package paynspray/0.2.7/paynspray-0.2.7.tar.gz

---
+++ packages/paynspray/paynspray-0.2.7.tar.gz/paynspray-0.2.7/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fd:
+    long_description = fd.read()
+
+setuptools.setup(
+    name="paynspray",
+    version="0.2.7",
+    author="The Munshi Group",
+    author_email="support@munshigroup.com",
+    description="Provides access to functions pertaining to text generation, manipulation, & presentation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/munshigroup/paynspray",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
