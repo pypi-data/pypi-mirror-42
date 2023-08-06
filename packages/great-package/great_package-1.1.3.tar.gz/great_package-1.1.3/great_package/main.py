a2fdaf87bc6e39f3efe5620c0866690a8054d342
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:17 +0000

    Add package great-package/1.1.3/great_package-1.1.3.tar.gz

---
+++ packages/great-package/great_package-1.1.3.tar.gz/great_package-1.1.3/great_package/main.py
@@ -0,0 +1,5 @@
+from package.sum.sum_file import sum
+
+n = int(input())
+
+print(sum(n))
---
+++ packages/great-package/great_package-1.1.3.tar.gz/great_package-1.1.3/setup.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='great_package',
+    version='1.1.3',
+    # py_modules = ['fact', 'sum'],
+    author='bowei',
+    author_email='billboy0428@outlook.com',
+    url='',
+    description='test',
+    packages=find_packages(),
+    platforms='any'
+)
