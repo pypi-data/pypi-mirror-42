36170e19b2f48582b5fc8e1dfb5b53731c8718b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:19 +0000

    Add package prosrs/0.0.1/prosrs-0.0.1-py2-none-any.whl

---
+++ packages/prosrs/prosrs-0.0.1-py2-none-any.whl/prosrs/__init__.py
@@ -0,0 +1,10 @@
+"""
+Copyright (C) 2019 Chenchao Shou
+
+Licensed under Illinois Open Source License (see the file LICENSE). For more information
+about the license, see http://otm.illinois.edu/disclose-protect/illinois-open-source-license.
+
+"""
+from .problem.problem import Problem
+from .problem.benchmark import benchmark
+from .optimizer.optimizer import Optimizer
