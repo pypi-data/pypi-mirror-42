d3fbec1c02d72e42be2d257d10259bebffa0ca2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:07 +0000

    Add package suanpan/0.7.15/suanpan-0.7.15.tar.gz

---
+++ packages/suanpan/suanpan-0.7.15.tar.gz/suanpan-0.7.15/suanpan/arguments.py
@@ -0,0 +1,167 @@
+# coding=utf-8
+from __future__ import print_function
+
+import argparse
+
+from suanpan import objects
+from suanpan.log import logger
+from suanpan.utils import json
+
+DEFAULT_ARG_VALUE_DELIMITER = "::"
+DEFAULT_ACTUAL_ARG_VALUE_DELIMITER = "_"
+
+
+class Arg(objects.HasName):
+    ARG_VALUE_DELIMITER = DEFAULT_ARG_VALUE_DELIMITER
+    ACTUAL_ARG_VALUE_DELIMITER = DEFAULT_ACTUAL_ARG_VALUE_DELIMITER
+
+    def __init__(self, key, **kwargs):
+        if "default" in kwargs:
+            kwargs.update(required=False)
+
+        self.key = key
+        self.value = None
+        self.type = kwargs.pop("type", str)
+        self.kwargs = self.cleanParams(kwargs)
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+    @property
+    def isSet(self):
+        return self.kwargs.get("required") or self.kwargs.get("default") != self.value
+
+    def addParserArguments(self, parser):
+        parser.add_argument(
+            "--{}".format(self.key), type=self.typeDecorator(self.type), **self.kwargs
+        )
+
+    def typeDecorator(self, typeFunc):
+        def _decorator(value):
+            return getattr(self, "default", None) if value == "" else typeFunc(value)
+
+        return _decorator
+
+    def load(self, args):
+        self.value = getattr(args, self.key)
+        self.value = self.fixArgValue(self.value)
+        self.logLoaded(self.value)
+        return self.value
+
+    def format(self, context):
+        return self.value
+
+    def clean(self, context):
+        return self.value
+
+    def save(self, context, result):
+        self.logSaved(result.value)
+        return result.value
+
+    def cleanParams(self, params):
+        return {k: v for k, v in params.items() if not k.startswith("_")}
+
+    def logLoaded(self, value):
+        logger.info(
+            "({type}) {key} loaded: {value}".format(
+                type=self.name, key=self.key, value=value
+            )
+        )
+
+    def logSaved(self, value):
+        logger.info(
+            "({type}) {key} saved: {value}".format(
+                type=self.name, key=self.key, value=value
+            )
+        )
+
+    def fixArgValue(self, value):
+        return value
+
+
+class String(Arg):
+    def __init__(self, key, **kwargs):
+        super(String, self).__init__(key, type=str, **kwargs)
+
+
+class Int(Arg):
+    def __init__(self, key, **kwargs):
+        super(Int, self).__init__(key, type=int, **kwargs)
+
+
+class Float(Arg):
+    def __init__(self, key, **kwargs):
+        super(Float, self).__init__(key, type=float, **kwargs)
+
+
+class Bool(Arg):
+    def __init__(self, key, **kwargs):
+        kwargs.update(default=False)
+        super(Bool, self).__init__(key, type=type(self).str2bool, **kwargs)
+
+    @classmethod
+    def str2bool(cls, string):
+        if string.lower() in ("yes", "true", "t", "y", "1"):
+            return True
+        elif string.lower() in ("no", "false", "f", "n", "0"):
+            return False
+        else:
+            raise argparse.ArgumentTypeError("Boolean value expected.")
+
+
+class List(Arg):
+    def __init__(self, key, **kwargs):
+        super(List, self).__init__(key, type=type(self).str2list, **kwargs)
+
+    @classmethod
+    def str2list(cls, string):
+        try:
+            return [cls.transform(i.strip()) for i in string.split(",") if i.strip()]
+        except:
+            raise argparse.ArgumentTypeError("{} value expected.".format(cls.__name__))
+
+    @classmethod
+    def transform(cls, item):
+        return item
+
+
+class ListOfString(List):
+    pass
+
+
+class ListOfInt(List):
+    @classmethod
+    def transform(cls, item):
+        return int(item)
+
+
+class ListOfFloat(List):
+    @classmethod
+    def transform(cls, item):
+        return float(item)
+
+
+class ListOfBool(List):
+    @classmethod
+    def transform(cls, item):
+        return Bool.str2bool(item)
+
+
+class Json(String):
+    def __init__(self, key, **kwargs):
+        if "default" in kwargs:
+            kwargs["default"] = json.dumps(kwargs["default"])
+        super(Json, self).__init__(key, **kwargs)
+
+    def format(self, context):
+        if self.value is not None:
+            self.value = json.loads(self.value)
+        return self.value
+
+    def save(self, context, result):
+        data = (
+            result.value.to_dict()
+            if isinstance(result.value, objects.Context)
+            else result.value
+        )
+        self.logSaved(data)
+        return json.dumps(data)
