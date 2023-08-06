7f0ab9c2704953704474b83d042a2affd58ba26f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:54 +0000

    Add package adafruit-circuitpython-esp-atcontrol/0.2.3/adafruit-circuitpython-esp-atcontrol-0.2.3.tar.gz

---
+++ packages/adafruit-circuitpython-esp-atcontrol/adafruit-circuitpython-esp-atcontrol-0.2.3.tar.gz/adafruit-circuitpython-esp-atcontrol-0.2.3/examples/espatcontrol_cheerlights.py
@@ -0,0 +1,111 @@
+"""
+This example will query ThingSpeak channel 1417 "CheerLights" and display the
+color on a NeoPixel ring or strip
+"""
+
+import gc
+import time
+import board
+import busio
+from digitalio import DigitalInOut
+from adafruit_espatcontrol import adafruit_espatcontrol
+from adafruit_espatcontrol import adafruit_espatcontrol_requests as requests
+import neopixel
+import adafruit_fancyled.adafruit_fancyled as fancy
+
+# Get wifi details and more from a secrets.py file
+try:
+    from secrets import secrets
+except ImportError:
+    print("WiFi secrets are kept in secrets.py, please add them there!")
+    raise
+
+#              CONFIGURATION
+TIME_BETWEEN_QUERY = 10  # in seconds
+
+# Cheerlights!
+DATA_SOURCE = "http://api.thingspeak.com/channels/1417/feeds.json?results=1"
+DATA_LOCATION = ["feeds", 0, "field2"]
+
+uart = busio.UART(board.TX, board.RX, timeout=0.1)
+resetpin = DigitalInOut(board.D5)
+rtspin = DigitalInOut(board.D6)
+
+
+# With a Particle Argon
+"""
+RX = board.ESP_TX
+TX = board.ESP_RX
+resetpin = DigitalInOut(board.ESP_WIFI_EN)
+rtspin = DigitalInOut(board.ESP_CTS)
+uart = busio.UART(TX, RX, timeout=0.1)
+esp_boot = DigitalInOut(board.ESP_BOOT_MODE)
+from digitalio import Direction
+esp_boot.direction = Direction.OUTPUT
+esp_boot.value = True
+"""
+
+
+
+# Create the connection to the co-processor and reset
+esp = adafruit_espatcontrol.ESP_ATcontrol(uart, 115200, run_baudrate=460800,
+                                          reset_pin=resetpin,
+                                          rts_pin=rtspin, debug=True)
+esp.hard_reset()
+
+requests.set_interface(esp)
+
+# neopixels
+pixels = neopixel.NeoPixel(board.A1, 16, brightness=0.3)
+pixels.fill(0)
+builtin = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.1)
+builtin[0] = 0
+
+# we'll save the value in question
+last_value = value = None
+the_time = None
+times = 0
+
+while True:
+    try:
+        while not esp.is_connected:
+            builtin[0] = (100, 0, 0)
+            # secrets dictionary must contain 'ssid' and 'password' at a minimum
+            esp.connect(secrets)
+        builtin[0] = (0, 100, 0)
+        # great, lets get the data
+        print("Retrieving data source...", end='')
+        builtin[0] = (100, 100, 0)
+        r = requests.get(DATA_SOURCE)
+        builtin[0] = (0, 0, 100)
+        print("Reply is OK!")
+    except (ValueError, RuntimeError, adafruit_espatcontrol.OKError) as e:
+        print("Failed to get data, retrying\n", e)
+        continue
+    print('-'*40,)
+    print("Headers: ", r.headers)
+    print("Text:", r.text)
+    print('-'*40)
+    # For mystery reasons, there's two numbers before and after the json data
+    value = r.json()
+    for x in DATA_LOCATION:
+        value = value[x]
+    builtin[0] = (100, 100, 100)
+    if not value:
+        continue
+    if last_value != value:
+        color = int(value[1:],16)
+        red = color >> 16 & 0xFF
+        green = color >> 8 & 0xFF
+        blue = color& 0xFF
+        gamma_corrected = fancy.gamma_adjust(fancy.CRGB(red, green, blue)).pack()
+
+        pixels.fill(gamma_corrected)
+        last_value = value
+    times += 1
+
+    # normally we wouldn't have to do this, but we get bad fragments
+    r = None
+    gc.collect()
+    print(gc.mem_free())  # pylint: disable=no-member
+    time.sleep(TIME_BETWEEN_QUERY)
