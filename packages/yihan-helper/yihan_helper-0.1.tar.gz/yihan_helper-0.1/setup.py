931266bbb2ca5d39193ef70b5d66c2694137644c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:10 +0000

    Add package yihan-helper/0.1/yihan_helper-0.1.tar.gz

---
+++ packages/yihan-helper/yihan_helper-0.1.tar.gz/yihan_helper-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='yihan_helper',
+version='0.1',
+description='Yihan Helper base code',
+url='https://git.un.org/gsd/unite.un.org.ml.unisdr.ds.git',
+author='Yihan Bao',
+author_email='yihan.bap@un.org',
+license='MIT',
+packages=['yihan_helper'],
+zip_safe=False)
