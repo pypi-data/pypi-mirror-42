82ee51451bf9d987bf52ff9e47f4966719ce31c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:58 +0000

    Add package ybc-bot/1.0.14/ybc_bot-1.0.14.tar.gz

---
+++ packages/ybc-bot/ybc_bot-1.0.14.tar.gz/ybc_bot-1.0.14/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ybc_bot',
+      version='1.0.14',
+      description='Smart bot.',
+      long_description='Smart bot little ape.',
+      author='hurs',
+      author_email='hurs@fenbi.com',
+      keywords=['python', 'smart bot', 'bot'],
+      url='http://pip.zhenguanyu.com/',
+      packages=['ybc_bot'],
+      package_data={'ybc_bot': ['*.py']},
+      license='MIT',
+      install_requires=[
+            'requests',
+            'ybc_config',
+            'ybc_exception',
+            'webrtcvad',
+            'pyaudio',
+            'ybc_speech',
+            'ybc_browser',
+            'ybc_tuya',
+            'pypinyin']
+      )
