142ae0bca74d66bcfc55965c53cc96567d6d9944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:28 +0000

    Add package andaluh/0.1.0/andaluh-0.1.0.tar.gz

---
+++ packages/andaluh/andaluh-0.1.0.tar.gz/andaluh-0.1.0/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# vim: ts=4
+###
+# 
+# Copyright (c) 2018-2019 Andalugeeks
+# Authors:
+# - Ksar Feui <a.moreno.losana@gmail.com>
+# - J. Félix Ontañón <felixonta@gmail.com>
+
+# -*- coding: utf-8 -*- 
+
+from setuptools import setup
+
+setup(name='andaluh',
+    version='0.1.0',
+    description='Transliterate castellano (español) spelling to andaluz proposals',
+    author='J. Félix Ontañón',
+    author_email='felixonta@gmail.com',
+    url='https://andaluh.es',
+    project_urls={
+        "Source Code": "https://github.com/andalugeeks/andaluh-py"
+    },
+    platforms=['win32', 'linux2'],
+    license='GNU LESSER GENERAL PUBLIC LICENSE',
+
+    packages = ['andaluh'],
+    package_dir =  {'andaluh': 'andaluh'},
+
+    scripts=['bin/andaluh']
+)
