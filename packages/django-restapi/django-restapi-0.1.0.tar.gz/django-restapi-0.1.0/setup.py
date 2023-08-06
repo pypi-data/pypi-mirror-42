c280cd570a4bcbd9cca1a1a0801b3f5d7751c124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:14 +0000

    Add package django-restapi/0.1.0/django-restapi-0.1.0.tar.gz

---
+++ packages/django-restapi/django-restapi-0.1.0.tar.gz/django-restapi-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+ 
+setup(
+  name = "django-restapi",
+  version = "0.1.0",
+  keywords = ("pip", "api","REST", "django"),
+  description = "REST api for django",
+  long_description = "REST api for django",
+  license = "MIT Licence",
+ 
+  url = "https://github.com/reindexer/django-restapi",
+  author = "reindexer",
+  author_email = "konglingkai@metatype.cn",
+ 
+  packages = find_packages(),
+  include_package_data = True,
+  platforms = "any",
+  install_requires = ['django']
+)
