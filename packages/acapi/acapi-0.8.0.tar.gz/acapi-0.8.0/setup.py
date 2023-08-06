e7c0d8e656c0ff28ed512a0bacc0919182d059ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:01 +0000

    Add package acapi/0.8.0/acapi-0.8.0.tar.gz

---
+++ packages/acapi/acapi-0.8.0.tar.gz/acapi-0.8.0/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+"""Setup ACAPI package."""
+
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__name__), "README.md")) as f:
+    long_description = f.read()
+
+setup(
+    name="acapi",
+    version="0.8.0",
+    description="Acquia Cloud API client.",
+    long_description=long_description,
+    author="Dave Hall",
+    author_email="me@davehall.com.au",
+    url="http://github.com/skwashd/python-acquia-cloud",
+    install_requires=["requests==2.21.0", "requests-cache==0.4.13"],
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Internet",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["acapi", "acapi.resources"],
+)
