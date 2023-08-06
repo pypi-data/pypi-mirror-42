361a4bb9750d5d4f551fd65effe17bdec31ebf2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:24 +0000

    Add package django-fsa/2.7.1/django-fsa-2.7.1.tar.gz

---
+++ packages/django-fsa/django-fsa-2.7.1.tar.gz/django-fsa-2.7.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+try:
+    long_description = open('README.rst').read()
+except IOError:
+    long_description = ''
+
+setup(
+    name='django-fsa',
+    version='2.7.1',
+    description='Django friendly finite state machine support, forked from django-fsm',
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    author='Antoine Fontaine',
+    author_email='antoine.fontaine@gmail.com',
+    url='http://github.com/mdziwny/django-fsm',
+    keywords="django",
+    packages=['django_fsm', 'django_fsm.management', 'django_fsm.management.commands'],
+    include_package_data=True,
+    zip_safe=False,
+    license='MIT License',
+    platforms=['any'],
+    install_requires=["django>=1.11"],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        "Framework :: Django",
+        "Framework :: Django :: 1.11",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Framework :: Django',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
