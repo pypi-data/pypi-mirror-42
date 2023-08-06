95c7e775a32cf7b556d3f77c5fc8b7a7041bd27b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:02 +0000

    Add package reel/0.0.7/reel-0.0.7-py2.py3-none-any.whl

---
+++ packages/reel/reel-0.0.7-py2.py3-none-any.whl/reel/cmd/icecast.py
@@ -0,0 +1,20 @@
+"""The Icecast streaming server."""
+# import reel
+
+
+# class Icecast:
+#     """A streaming server."""
+#
+#     def __init__(self, host, port, mount, password):
+#         """Initialize an icecast subprocess."""
+#         # self._exe = 'icecast'
+#         # self._flags = ['-c', '{config_file}']
+
+# async with Icecast() + Aria2() + Postgres() as server:
+#     async with playlist | out as player:
+#         async with trio.open_nursery() as nursery:
+#             await server.start_daemon(nursery)
+#             await player.start_daemon(nursery)
+#             # loop & serve
+#             await player.stop()
+#             await server.stop()
