f3b03fa19308210180887d74dcdfdfea16042a3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:27 +0000

    Add package gargoyle-yplan/2.0.0/gargoyle_yplan-2.0.0-py2.py3-none-any.whl

---
+++ packages/gargoyle-yplan/gargoyle_yplan-2.0.0-py2.py3-none-any.whl/gargoyle/compat.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import django
+
+# Django 1.9
+
+# url(prefix, include(urls, namespace, name)) -> url(prefix, (urls, namespace, name))
+if django.VERSION[:2] >= (1, 9):
+    def subinclude(urls_tuple):
+        return urls_tuple  # (urls, namespace, name)
+else:
+    from django.conf.urls import include as subinclude
+
+# Django 2.0
+
+try:
+    from django.urls import reverse  # noqa pragma: no cover
+except ImportError:
+    from django.core.urlresolvers import reverse  # noqa pragma: no cover
+
+
+__all__ = ['subinclude']
