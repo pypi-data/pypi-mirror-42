c5448fee692129be51307b9d71f2b40c33494d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:15 +0000

    Add package pyutils2/0.0.2/pyutils2-0.0.2.tar.gz

---
+++ packages/pyutils2/pyutils2-0.0.2.tar.gz/pyutils2-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyutils2",
+    version="0.0.2",
+    author="Xin Deng",
+    author_email="xindeng3@gmail.com",
+    description="python custom utils",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xindzju",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
