ab6c03634dfe3f8d6cdabe7fb47c82f35acc8585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:31 +0000

    Add package python-dispatch/0.1.2/python-dispatch-0.1.2.tar.gz

---
+++ packages/python-dispatch/python-dispatch-0.1.2.tar.gz/python-dispatch-0.1.2/setup.py
@@ -0,0 +1,29 @@
+import sys
+from setuptools import setup, find_packages
+
+setup(
+    name = "python-dispatch",
+    version = "v0.1.2",
+    author = "Matthew Reid",
+    author_email = "matt@nomadic-recording.com",
+    description = "Lightweight Event Handling",
+    url='https://github.com/nocarryr/python-dispatch',
+    license='MIT',
+    packages=find_packages(exclude=['tests*']),
+    include_package_data=True,
+    keywords='event properties dispatch',
+    platforms=['any'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
