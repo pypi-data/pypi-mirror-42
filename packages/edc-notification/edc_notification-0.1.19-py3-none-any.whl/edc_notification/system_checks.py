ab35a1e8023788a1dc37db4f0aee68ee42460698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:40 +0000

    Add package edc-notification/0.1.19/edc_notification-0.1.19-py3-none-any.whl

---
+++ packages/edc-notification/edc_notification-0.1.19-py3-none-any.whl/edc_notification/system_checks.py
@@ -0,0 +1,32 @@
+from django.apps import apps as django_apps
+from django.core.checks import Warning, register
+from django.db.models import Q
+
+
+@register()
+def edc_notification_check(app_configs, **kwargs):
+    errors = []
+
+    User = django_apps.get_model("auth.user")
+
+    for user in User.objects.filter(
+        (
+            Q(first_name__isnull=True)
+            | Q(last_name__isnull=True)
+            | Q(email__isnull=True)
+        ),
+        is_active=True,
+        is_staff=True,
+    ):
+        errors.append(
+            Warning(
+                (
+                    f"User account is incomplete. Check that first name, "
+                    f"last name and email are complete. See {user}"
+                ),
+                hint="Complete the user's account details.",
+                obj=User,
+                id="edc_notification.W001",
+            )
+        )
+    return errors
---
+++ packages/edc-notification/edc_notification-0.1.19-py3-none-any.whl/edc_notification/tests/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from edc_base.model_mixins import BaseUuidModel
+from edc_base.model_managers import HistoricalRecords
+from edc_base.sites import SiteModelMixin
+from edc_base.utils import get_utcnow
+
+
+class TestModel(BaseUuidModel):
+
+    pass
+
+
+class Condition(BaseUuidModel):
+
+    name = models.CharField(max_length=25, default="anemia")
+
+
+class AnyModel(BaseUuidModel):
+
+    name = models.CharField(max_length=25, default="anemia")
+
+    history = HistoricalRecords()
+
+
+class AE(SiteModelMixin, BaseUuidModel):
+
+    ae_grade = models.CharField(max_length=10)
+
+    subject_identifier = models.CharField(max_length=10)
+
+    conditions = models.ManyToManyField(Condition)
+
+    history = HistoricalRecords()
+
+
+class Death(SiteModelMixin, BaseUuidModel):
+
+    subject_identifier = models.CharField(max_length=10)
+
+    report_datetime = models.DateTimeField(default=get_utcnow)
+
+    cause = models.CharField(max_length=10)
+
+    history = HistoricalRecords()
