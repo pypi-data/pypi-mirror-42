d7c0a4da81d2cab307b6b8d481de307f7b6cba67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:37 +0000

    Add package tamr-unify-client/0.3.0/tamr_unify_client-0.3.0-py3-none-any.whl

---
+++ packages/tamr-unify-client/tamr_unify_client-0.3.0-py3-none-any.whl/tamr_unify_client/models/project/categorization.py
@@ -0,0 +1,16 @@
+from tamr_unify_client.models.project.resource import Project
+from tamr_unify_client.models.machine_learning_model import MachineLearningModel
+
+
+class CategorizationProject(Project):
+    """A Categorization project in Unify."""
+
+    def model(self):
+        """Machine learning model for this Categorization project.
+        Learns from verified labels and predicts categorization labels for unlabeled records.
+
+        :returns: The machine learning model for categorization.
+        :rtype: :class:`~tamr_unify_client.models.machine_learning_model.MachineLearningModel`
+        """
+        alias = self.api_path + "/categorizations/model"
+        return MachineLearningModel(self.client, None, alias)
