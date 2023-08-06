f50f6646e08bdf9af005c9ad83bb23228441e62c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:02 +0000

    Add package isbnlib-dnb/0.0.3/isbnlib_dnb-0.0.3-py2.py3-none-any.whl

---
+++ packages/isbnlib-dnb/isbnlib_dnb-0.0.3-py2.py3-none-any.whl/isbnlib_dnb/simpletest.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+# pylint: skip-file
+"""Simple checks with a few examples. Just to see if it works."""
+
+#from .._dnb import query
+from _dnb import query
+
+def test_dnb():
+    """Simple tests for some remarkable cases."""
+    #print(query('9783897215672'))
+    #print(query('9783658161408'))
+    print(query('9783788804312'))
+    #print(query('9783437583032'))
+    #print(query('365816140X'))
+    #print(query('978-3-9818084-4-5')) # two languages
+
+
+if __name__ == '__main__':
+    test_dnb()
