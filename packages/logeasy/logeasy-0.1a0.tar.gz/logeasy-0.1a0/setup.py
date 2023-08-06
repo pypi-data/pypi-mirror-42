bb11d68894c89961357aa010894dec63ba94ad9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:02 +0000

    Add package logeasy/0.1a0/logeasy-0.1a0.tar.gz

---
+++ packages/logeasy/logeasy-0.1a0.tar.gz/logeasy-0.1a0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='logeasy',
+    version='0.1a',
+    description='Utilities for logging in Python',
+    author='Caio Belfort',
+    author_email='caiobelfort90@gmail.com',
+    license='GPL',
+    packages=['logeasy'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Software Development',
+        'Intended Audience :: Developers',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ]
+)
