a3d299adf4ce2cc4a43aab01233f69addbae9437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:57 +0000

    Add package D2R/0.5.0/D2R-0.5.0.tar.gz

---
+++ packages/D2R/D2R-0.5.0.tar.gz/D2R-0.5.0/README.md
@@ -0,0 +1,6 @@
+# D2R
+This is a library for efficient detection of repetitive elements in biological sequences.
+
+Usage:
+	from D2R.stats import D2R
+	D2R(seq="ATATACAT", model=nullModel, k=3, method="D2R")
---
+++ packages/D2R/D2R-0.5.0.tar.gz/D2R-0.5.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='D2R',
+    version='0.5.0',
+    keywords = ('statistics', 'D2R', 'alignment-free', 'DNA', 'repeat'),
+    description='A new statistic for efficient detection of repetitive sequences',
+    license='Free',
+    author='Sijie Chen',
+    author_email='chansigit@gmail.com',
+    url='https://github.com/chansigit/D2R_codes',
+    platforms = 'any'
+)
