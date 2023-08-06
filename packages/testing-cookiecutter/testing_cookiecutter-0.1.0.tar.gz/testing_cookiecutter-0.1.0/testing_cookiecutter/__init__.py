a258fe7451a4f4fa970e7a0c2d4814271a897d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:30 +0000

    Add package testing-cookiecutter/0.1.0/testing_cookiecutter-0.1.0.tar.gz

---
+++ packages/testing-cookiecutter/testing_cookiecutter-0.1.0.tar.gz/testing_cookiecutter-0.1.0/testing_cookiecutter/__init__.py
@@ -0,0 +1,6 @@
+"""testing_cookiecutter - testing on an opinionated, minimal cookiecutter template for Python packages"""
+from testing_cookiecutter.sounds_generator import generator
+
+__version__ = '0.1.0'
+__author__ = 'Train Chen <train981316@gmail.com>'
+__all__ = []
