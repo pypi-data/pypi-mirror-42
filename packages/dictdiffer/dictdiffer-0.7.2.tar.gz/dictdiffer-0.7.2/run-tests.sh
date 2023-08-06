470de14a02610240320bfb9fe91e77d13a05f5c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:56 +0000

    Add package dictdiffer/0.7.2/dictdiffer-0.7.2.tar.gz

---
+++ packages/dictdiffer/dictdiffer-0.7.2.tar.gz/dictdiffer-0.7.2/run-tests.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# This file is part of Dictdiffer.
+#
+# Copyright (C) 2013 Fatih Erikli.
+# Copyright (C) 2014, 2016 CERN.
+#
+# Dictdiffer is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+pydocstyle dictdiffer && \
+isort -rc -c -df **/*.py && \
+check-manifest --ignore ".travis-*" && \
+sphinx-build -qnNW docs docs/_build/html && \
+python setup.py test
---
+++ packages/dictdiffer/dictdiffer-0.7.2.tar.gz/dictdiffer-0.7.2/setup.py
@@ -0,0 +1,96 @@
+# This file is part of Dictdiffer.
+#
+# Copyright (C) 2013 Fatih Erikli.
+# Copyright (C) 2014, 2015, 2016 CERN.
+# Copyright (C) 2017 ETH Zurich, Swiss Data Science Center, Jiri Kuncar.
+#
+# Dictdiffer is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Dictdiffer is a library that helps you to diff and patch dictionaries."""
+
+from __future__ import absolute_import, print_function
+
+import os
+import re
+
+from setuptools import find_packages, setup
+
+readme = open('README.rst').read()
+history = open('CHANGES').read()
+
+tests_require = [
+    'check-manifest>=0.25',
+    'coverage>=4.0',
+    'isort>=4.2.2',
+    'mock>=1.3.0',
+    'pydocstyle>=1.0.0',
+    'pytest-cache>=1.0',
+    'pytest-cov>=1.8.0',
+    'pytest-pep8>=1.0.6',
+    'pytest>=2.8.0',
+    'tox>=3.7.0',
+]
+
+extras_require = {
+    ':python_version=="2.6"': [
+        'unittest2>=1.1.0',
+    ],
+    'docs': [
+        'Sphinx>=1.4.4',
+        'sphinx-rtd-theme>=0.1.9',
+    ],
+    'numpy': [
+        'numpy>=1.11.0',
+    ],
+    'tests': tests_require,
+}
+
+extras_require['all'] = []
+for key, reqs in extras_require.items():
+    if ':' == key[0]:
+        continue
+    extras_require['all'].extend(reqs)
+
+setup_requires = [
+    'pytest-runner>=2.7',
+]
+
+packages = find_packages()
+
+
+# Get the version string. Cannot be done with import!
+with open(os.path.join('dictdiffer', 'version.py'), 'rt') as f:
+    version = re.search(
+        '__version__\s*=\s*"(?P<version>.*)"\n',
+        f.read()
+    ).group('version')
+
+setup(
+    name='dictdiffer',
+    version=version,
+    description=__doc__,
+    long_description=readme + '\n\n' + history,
+    author='Invenio Collaboration',
+    author_email='info@inveniosoftware.org',
+    url='https://github.com/inveniosoftware/dictdiffer',
+    packages=['dictdiffer'],
+    zip_safe=False,
+    extras_require=extras_require,
+    setup_requires=setup_requires,
+    tests_require=tests_require,
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Utilities',
+    ],
+)
