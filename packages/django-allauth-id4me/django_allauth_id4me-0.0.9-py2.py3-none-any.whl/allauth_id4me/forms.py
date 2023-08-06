afda274ed2ab7258883dec95711bc7c8cab34b18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:58 +0000

    Add package django-allauth-id4me/0.0.9/django_allauth_id4me-0.0.9-py2.py3-none-any.whl

---
+++ packages/django-allauth-id4me/django_allauth_id4me-0.0.9-py2.py3-none-any.whl/allauth_id4me/forms.py
@@ -0,0 +1,15 @@
+
+from django import forms
+
+
+class LoginForm(forms.Form):
+    id4me = forms.CharField(
+        label=('ID4me'),
+        help_text=(
+            'Get an <a href="https://id4me.org/">ID4me</a>'))
+    next = forms.CharField(
+        widget=forms.HiddenInput,
+        required=False)
+    process = forms.CharField(
+        widget=forms.HiddenInput,
+        required=False)
