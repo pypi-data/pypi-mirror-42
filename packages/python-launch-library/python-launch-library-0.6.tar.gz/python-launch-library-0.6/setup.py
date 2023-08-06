492aa7b925bbacd9d3a9b780ad73b7a1fd99e38d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:31 +0000

    Add package python-launch-library/0.6/python-launch-library-0.6.tar.gz

---
+++ packages/python-launch-library/python-launch-library-0.6.tar.gz/python-launch-library-0.6/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python-launch-library",
+    version="0.6",
+    author="Nir Harel",
+    author_email="nir@nirharel.space",
+    description="A wrapper for the launchlibrary.net API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Plutoberth/python-launch-library",
+    packages=setuptools.find_packages(exclude=".gitignore"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3.6"
+    ],
+    install_requires=[
+        "requests",
+        "python-dateutil",
+        "aiohttp",
+        "unidecode"
+    ],
+    python_requires='>=3.6'
+)
