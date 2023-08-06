4655ace58bde3e0758dadcc79c12d1902e31c87b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:46 +0000

    Add package scvae/0.0.1/scvae-0.0.1.tar.gz

---
+++ packages/scvae/scvae-0.0.1.tar.gz/scvae-0.0.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="scvae",
+    version="0.0.1",
+    author="Christopher Heje Grønbech",
+    author_email="christopher.heje.groenbech@regionh.dk",
+    description="Deep learning for single-cell transcript counts",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
