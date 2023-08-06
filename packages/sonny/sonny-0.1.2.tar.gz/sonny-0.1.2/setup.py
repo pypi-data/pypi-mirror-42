fd460b390ac1e83d7710dcda791cac439c14df1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:44 +0000

    Add package sonny/0.1.2/sonny-0.1.2.tar.gz

---
+++ packages/sonny/sonny-0.1.2.tar.gz/sonny-0.1.2/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+from codecs import open
+from sonny import __version__
+from setuptools import setup
+
+
+def read(f):
+    return open(f, encoding='utf-8').read()
+
+
+setup(
+    name='sonny',
+    version=__version__,
+    description='A command line translation software.',
+    long_description=read('README.rst') + '\n\n' + read('HISTORY.rst'),
+    author='suowei',
+    author_email='suowei_66@qq.com',
+    url='',
+    packages=[
+        'sonny'
+    ],
+    py_modules=['run'],
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+    ],
+    entry_points={
+        'console_scripts': ['sonny=run:main']
+    },
+    license='MIT',
+    zip_safe=False,
+    classifiers=[
+        'Environment :: Console',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython'
+    ]
+)
