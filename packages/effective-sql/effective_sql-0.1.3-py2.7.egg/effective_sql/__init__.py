6d598db348a73f10c92d2f09d1cdb55c8266f7de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:28 +0000

    Add package effective-sql/0.1.3/effective_sql-0.1.3-py2.7.egg

---
+++ packages/effective-sql/effective_sql-0.1.3-py2.7.egg/effective_sql/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    File Name: setup.py
+    Author: HuHao
+    Mail: whohow20094702@163.com
+    Created Time:  '2019/2/28 20:48:00'
+    Info: A effective style to operate sql
+          Support for HA-connection, retry-support, high-tolerance params, self-batch execution
+    Licence: GPL Licence
+    Url: https://github.com/GitHuHao/effective_sql.git
+"""
