87a1f68637d63b6b4e8c8f00ce5c8f88b68f3625
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:05 +0000

    Add package iutil/0.1.2/iutil-0.1.2-py3-none-any.whl

---
+++ packages/iutil/iutil-0.1.2-py3-none-any.whl/iutil/pdf_helper.py
@@ -0,0 +1,54 @@
+"""
+
+require: https://wkhtmltopdf.org/
+
+Bug on Ubuntu:
+
+QXcbConnection: Could not connect to display
+https://github.com/wkhtmltopdf/wkhtmltopdf/issues/2037
+
+Run:
+sudo apt install xvfb
+to install xvfb-run command
+
+Ubunntu:
+
+xvfb-run -- wkhtmltopdf https://www.baidu.com/ baidu.pdf
+
+Mac:
+
+wkhtmltopdf https://www.baidu.com/ baidu.pdf
+"""
+import os
+# import sys
+import subprocess
+
+from iutil.hashes import (
+    md5_for_text,
+)
+
+
+# cmd_url2pdf_choices = {
+#     'linux': 'wkhtmltopdf --log-level none'.split(),  # ubuntu 18.04
+#     'darwin': 'wkhtmltopdf --log-level none'.split(),  # Mac
+# }
+
+# platform = sys.platform
+
+# cmd_url2pdf = cmd_url2pdf_choices[platform]
+
+cmd_url2pdf = 'wkhtmltopdf --log-level none'.split()
+
+
+def url2pdf(url, out_dir='.', out_filename=None):
+    if not out_filename:
+        url_hash = md5_for_text(url)
+        out_filename = '%s.pdf' % url_hash
+
+    out_file = os.path.join(out_dir, out_filename)
+
+    full_cmd = cmd_url2pdf + [url, out_file]
+    # print(' '.join(full_cmd))
+    res = subprocess.call(full_cmd)
+
+    return res, out_file
