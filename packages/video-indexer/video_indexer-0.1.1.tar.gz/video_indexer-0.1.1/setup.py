5c5a2128b1475fff5c8e9500d16ec2dcb099a356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:26 +0000

    Add package video-indexer/0.1.1/video_indexer-0.1.1.tar.gz

---
+++ packages/video-indexer/video_indexer-0.1.1.tar.gz/video_indexer-0.1.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+
+setuptools.setup(
+    name='video_indexer',
+    version='0.1.1',
+    description='Common function to query Microsoft Video Indexer',
+    author='BK Lim',
+    author_email='bklim5@hotmail.com',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
