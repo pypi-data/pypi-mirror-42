ed25bd59bea568c06d0a6ccf9101c756fdd5e4bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:57 +0000

    Add package ejplugins/0.11.0/ejplugins-0.11.0-py2.py3-none-any.whl

---
+++ packages/ejplugins/ejplugins-0.11.0-py2.py3-none-any.whl/ejplugins/__init__.py
@@ -0,0 +1,43 @@
+__version__ = "0.11.0"
+
+def load_all_parsers():
+    """ load all parsers
+
+    Returns
+    -------
+    errors: list
+
+    """
+    from jsonextended import plugins
+    from ejplugins.crystal import (
+        CrystalOutputPlugin, CrystalSCFLogPlugin, CrystalDOSPlugin,
+        BANDPlugin, ECH3CubePlugin, ECH3OutPlugin)
+    from ejplugins.qespresso import (
+        QEmainPlugin, QEbandPlugin, QEChargeDensityPlugin,
+        QELowdinPlugin, QEnscfPlugin, QEdosPlugin)
+    from ejplugins.gulp import GULPOutPlugin
+    from ejplugins.lammps import LAMMPSAtomDumpPlugin, LAMMPSSysDumpPlugin
+
+    parsers = [CrystalOutputPlugin, CrystalSCFLogPlugin, CrystalDOSPlugin,
+               BANDPlugin, ECH3CubePlugin, ECH3OutPlugin,
+               QEmainPlugin, QEbandPlugin, QEChargeDensityPlugin, QELowdinPlugin,
+               GULPOutPlugin,
+               LAMMPSAtomDumpPlugin, LAMMPSSysDumpPlugin]
+    try:
+        from ejplugins.cif import CIFPlugin
+        parsers.append(CIFPlugin)
+    except ImportError:
+        pass
+
+    return plugins.load_plugin_classes(parsers, "parsers")
+
+
+def load_all_encoders():
+    from jsonextended import plugins
+    try:
+        from ejplugins.pymatgen_decode import Encode_Pymatgen
+        from ejplugins.ase_decode import Encode_ASE
+    except ImportError:
+        raise ImportError("pymatgen and/or ase not installed")
+    return plugins.load_plugin_classes([Encode_Pymatgen, Encode_ASE])
+
