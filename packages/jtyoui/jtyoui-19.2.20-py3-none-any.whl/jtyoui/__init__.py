d5a495a7497b294cdb43013855a008b284bc642d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:47 +0000

    Add package jtyoui/19.2.20/jtyoui-19.2.20-py3-none-any.whl

---
+++ packages/jtyoui/jtyoui-19.2.20-py3-none-any.whl/jtyoui/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time    : 2018/2/2 10:58
+# @Email  : jtyoui@qq.com
+# @Software: PyCharm
+
+from .bs import binary_system  # 任意进制转换
+from .code import cr  # 二维码识别
+from .imagepdf import *  # 照片和PDF互转
+from .plunar import Lunar  # 农历
+from .sogou import SoGou  # 下载搜狗
+from .word import analysis  # 新词发现
+from .wx import *  # 启动微信聊天机器人
+from .mail import *  # 邮箱
+from .data import *  # 常量
+from .fractal import *  # 分形
+from .web import *  # 网页
+from .language import *  # 语言
+
+__all__ = [a for a in dir() if not a.startswith('_')]
