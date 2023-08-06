c6ddb8bf211c7fac817aa2508fb0661b83a10e59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:53 +0000

    Add package bqup/0.0.5/bqup-0.0.5-py3-none-any.whl

---
+++ packages/bqup/bqup-0.0.5-py3-none-any.whl/bqup/dataset.py
@@ -0,0 +1,33 @@
+import os
+from functools import partial
+from bqup.table import Table
+
+
+class Dataset():
+
+    tables = []
+
+    def __init__(self, project, export_schema, bq_dataset):
+        self.project = project
+        self.project.datasets.append(self)
+        self.dataset_id = bq_dataset.dataset_id
+        print('\tLoading dataset {}...'.format(self.dataset_id))
+
+        # To support multiple version of google-cloud-bigquery
+        if hasattr(project.client, 'list_dataset_tables'):
+            self.tables = list(
+                map(partial(Table, self, export_schema), project.client.list_dataset_tables(bq_dataset)))
+        else:
+            self.tables = list(
+                map(partial(Table, self, export_schema), project.client.list_tables(bq_dataset.reference)))
+
+    def print(self):
+        print("\t[DATASET] {}".format(self.dataset_id))
+        for t in self.tables:
+            t.print()
+
+    def export(self, project_dir):
+        dataset_dir = "{}/{}".format(project_dir, self.dataset_id)
+        os.makedirs(dataset_dir)
+        for t in self.tables:
+            t.export(dataset_dir)
---
+++ packages/bqup/bqup-0.0.5-py3-none-any.whl/bqup/project.py
@@ -0,0 +1,36 @@
+import os
+import shutil
+from google.cloud import bigquery
+from functools import partial
+from bqup.dataset import Dataset
+
+
+class Project():
+
+    datasets = []
+
+    def __init__(self, project_id=None, export_schema=False):
+        self.client = bigquery.Client(project_id)
+        self.project_id = self.client.project
+        print('Loading project {}...'.format(self.project_id))
+        self.datasets = list(
+            map(partial(Dataset, self, export_schema), self.client.list_datasets()))
+
+    def print(self):
+        print("[PROJECT] {}".format(self.project_id))
+        for d in self.datasets:
+            d.print()
+
+    def export(self, directory, force=False):
+        """Exports a project's contents to the given directory in the filesystem."""
+        if os.path.exists(directory):
+            if force:
+                shutil.rmtree(directory)
+            else:
+                raise Exception(
+                    "Cannot extract project contents to an existing directory: [{}]".format(directory))
+
+        project_dir = "{}/{}".format(directory, self.project_id)
+        os.makedirs(project_dir)
+        for d in self.datasets:
+            d.export(project_dir)
