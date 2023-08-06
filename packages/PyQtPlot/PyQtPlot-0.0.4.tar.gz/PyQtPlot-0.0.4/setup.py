c5400848ff3f114907b230fcb336b78e3c68dbe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:55 +0000

    Add package PyQtPlot/0.0.4/PyQtPlot-0.0.4.tar.gz

---
+++ packages/PyQtPlot/PyQtPlot-0.0.4.tar.gz/PyQtPlot-0.0.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyQtPlot",
+    version="0.0.4",
+    author="Vlad Schekochikhin",
+    author_email="vladschekochikhin@gmail.com",
+    description="PyQt native library for plotting data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/merdovash/PyQtPlot",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['PyQt5'],
+    python_requires='>=3.6',
+)
