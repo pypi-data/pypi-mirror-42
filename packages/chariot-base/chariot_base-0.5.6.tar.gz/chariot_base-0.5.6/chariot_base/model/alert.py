305ba75d8931a5ae2b575b297dbc6d01b0892d8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:43 +0000

    Add package chariot-base/0.5.6/chariot_base-0.5.6.tar.gz

---
+++ packages/chariot-base/chariot_base-0.5.6.tar.gz/chariot_base-0.5.6/chariot_base/model/alert.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import json
+import datetime
+
+
+class Alert(object):
+    """
+    Describe each alert raised by Chariot
+    """
+    def __init__(self, name, msg=None, severity=100):
+        self.timestamp = datetime.datetime.now().isoformat()
+        self.name = name
+        self.message = msg
+        self.severity = severity
+        self.sensor_id = None
+
+    def dict(self):
+        return {
+            'name': self.name,
+            'sensor_id': self.sensor_id
+            'message': self.message,
+            'severity': self.severity,
+            'timestamp': self.timestamp
+        }
+
+    def __str__(self):
+        return self.__unicode__()
+
+    def __unicode__(self):
+        return json.dumps(self.dict())
