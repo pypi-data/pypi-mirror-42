ea9635ab1d618ef407f11a5bf02f1470fa2e3563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:53 +0000

    Add package toad/0.0.35/toad-0.0.35.tar.gz

---
+++ packages/toad/toad-0.0.35.tar.gz/toad-0.0.35/setup.py
@@ -0,0 +1,55 @@
+import os
+from setuptools import setup, find_packages, Extension
+from distutils.util import convert_path
+
+NAME = 'toad'
+
+
+CURRENT_PATH = os.path.abspath(os.path.dirname(__file__))
+VERSION_FILE = os.path.join(CURRENT_PATH, NAME, 'version.py')
+
+def get_version():
+    ns = {}
+    with open(VERSION_FILE) as f:
+        exec(f.read(), ns)
+    return ns['__version__']
+
+setup(
+    name = NAME,
+    version = get_version(),
+    description = 'python utils for detect data',
+    long_description = open('README.md').read(),
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/amphibian-dev/toad',
+    author = 'ESC Team',
+    author_email = 'secbone@gmail.com',
+    packages = find_packages(exclude = ['tests']),
+    python_requires = '>=3.5',
+    setup_requires = [
+        'setuptools',
+    ],
+    install_requires = [
+        'numpy >= 1.15',
+        'pandas',
+        'scipy',
+        'sklearn',
+        'statsmodels',
+        'seaborn',
+    ],
+    tests_require=[
+        'pytest'
+    ],
+    license = 'MIT',
+    classifiers = [
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    entry_points = {
+        'console_scripts': [
+            'toad = toad.cli:main',
+        ],
+    },
+)
