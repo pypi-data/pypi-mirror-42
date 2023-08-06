7e6b584bd27372aa4e8470a98f912677e9d7d660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:04 +0000

    Add package frostmark/0.0.1/frostmark-0.0.1-py3-none-any.whl

---
+++ packages/frostmark/frostmark-0.0.1-py3-none-any.whl/frostmark-0.0.1.data/data/frostmark/precache-manifest.500917180ce0e735032fec7b2c0df784.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1b8a1a416d62a8080ce",
+    "url": "/static/css/main.95bdf028.chunk.css"
+  },
+  {
+    "revision": "a1b8a1a416d62a8080ce",
+    "url": "/static/js/main.a1b8a1a4.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d217a87168bb5fa48f7f",
+    "url": "/static/css/2.1bfe98c7.chunk.css"
+  },
+  {
+    "revision": "d217a87168bb5fa48f7f",
+    "url": "/static/js/2.d217a871.chunk.js"
+  },
+  {
+    "revision": "b6588a78e067db8dc2e78d63c998a821",
+    "url": "/index.html"
+  }
+];
