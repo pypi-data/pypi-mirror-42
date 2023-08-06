2be2094cea600a3f2b47a8c262fc188db6f38367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:02 +0000

    Add package pyaug/0.1.0/pyaug-0.1.0.tar.gz

---
+++ packages/pyaug/pyaug-0.1.0.tar.gz/pyaug-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='pyaug',
+      version='0.1.0',
+      author='Joshua David Wood',
+      author_email='pyaugment@gmail.com',
+      description='A package to generate augmented data using Pandas DataFrames',
+      url='https://github.com/pyaug/pyaug',
+      classifiers=['Programming Language :: Python :: 3',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent'],
+      packages=['pyaug'])
+
