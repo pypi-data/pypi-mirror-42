50c4ed7fbb92cdb21c6cfb51c8698c4f7a00ec0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:53 +0000

    Add package lineintf/1.0.2/lineintf-1.0.2.tar.gz

---
+++ packages/lineintf/lineintf-1.0.2.tar.gz/lineintf-1.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(name="lineintf",
+      version="1.0.2",
+      description="LineIntf - Line Interface makes easy to do tkinter interfaces placing the widgets in line styles.",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="http://github.com/pointel-com-br/lineintf",
+      author="Pointel.com.br",
+      author_email="master@pointel.com.br",
+      license="MIT",
+      packages=["lineintf"],
+      zip_safe=False)
