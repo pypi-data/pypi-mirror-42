1c65d6d6974ca7d096207e21ab6cd20a1f4b9495
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:12 +0000

    Add package spark-emr/0.1.1/spark_emr-0.1.1.tar.gz

---
+++ packages/spark-emr/spark_emr-0.1.1.tar.gz/spark_emr-0.1.1/spark_emr/const.py
@@ -0,0 +1,12 @@
+import os
+
+home = os.path.expanduser("~")
+
+CONFIG_DIR = os.path.join(home, ".config", "spark-emr.yaml")
+NAMESPACE = "spark_emr"
+
+SH_INSTALL_PYTHON = "install_python.sh"
+SH_YARN_LOG = "yarn_log.sh"
+SH_PYPI_PYPACKAGE = "install_pypi_pypackage.sh"
+SH_LOCAL_PYPACKAGE = "install_local_pypackage.sh"
+PYTHON_VERSION = {"python35": "3.5", "python36": "3.6"}
---
+++ packages/spark-emr/spark_emr-0.1.1.tar.gz/spark_emr-0.1.1/test/test_main.py
@@ -0,0 +1,43 @@
+import unittest
+import os
+import moto
+import pytest
+from spark_emr.__main__ import main
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+CONFIG = os.path.join(HERE, "config.yaml")
+
+
+class TestMain(unittest.TestCase):
+    @pytest.fixture(autouse=True)
+    def capsys(self, capsys):
+        self.capsys = capsys
+
+    def test_cli(self):
+        self.assertRaises(SystemExit, main, [])
+        out, err = self.capsys.readouterr()
+        self.assertTrue(
+            "{start,status,list,stop}" in out)
+
+    def test_cli_start(self):
+        self.assertRaises(SystemExit, main, ["start"])
+        out, err = self.capsys.readouterr()
+        self.assertTrue(
+            "required: --name, --cmdline, --package" in
+            err)
+
+    def test_cli_stop(self):
+        self.assertRaises(SystemExit, main, ["stop"])
+        out, err = self.capsys.readouterr()
+        self.assertTrue("required: --cluster-id" in err)
+
+    def test_cli_status(self):
+        self.assertRaises(SystemExit, main, ["status"])
+        out, err = self.capsys.readouterr()
+        self.assertTrue("required: --cluster-id" in err)
+
+    def test_cli_list(self):
+        with moto.mock_emr():
+            main(["list", "--config", CONFIG])
+            out, err = self.capsys.readouterr()
+            self.assertTrue("id, name, reason, state, created" in out)
