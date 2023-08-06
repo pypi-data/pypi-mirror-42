44b5f163547bb086ffecd309e3b6bd0bfb469151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:11 +0000

    Add package p1-notif/0.1.40405805/p1-notif-0.1.40405805.tar.gz

---
+++ packages/p1-notif/p1-notif-0.1.40405805.tar.gz/p1-notif-0.1.40405805/notif/channels/list_group_whatsapp.py
@@ -0,0 +1,10 @@
+from .abstract_channel import AbstractChannel
+
+class ListGroupWhatsapp(AbstractChannel):
+
+    def json(self):
+        payload = dict()
+        return payload
+
+    def notif_type(self):
+        return 'LIST_GROUP_WHATSAPP'
