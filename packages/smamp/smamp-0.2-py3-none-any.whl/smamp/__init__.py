29ae99ac41a7e95b568a71b760e10a0dd6404fc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:50 +0000

    Add package smamp/0.2/smamp-0.2-py3-none-any.whl

---
+++ packages/smamp/smamp-0.2-py3-none-any.whl/smamp/__init__.py
@@ -0,0 +1,10 @@
+"""SMAMP module."""
+
+# Modules by Johannes
+from . import insertHbyList
+
+# Modules by Lukas
+# from . import average_cost
+# from . import charges_to_rft
+# from . import calc_horton_charges
+# from . import gpaw_optimize_and_esp
---
+++ packages/smamp/smamp-0.2-py3-none-any.whl/smamp/calc_horton_charges.py
@@ -0,0 +1,20 @@
+""" Calculate MD point charges with HORTON.
+Copyright 2019 Simulation Lab
+University of Freiburg
+Author: Lukas Elflein <elfleinl@cs.uni-freiburg.de>
+"""
+
+import pandas as pd
+import argparse
+import shutil
+
+
+	
+	
+def main():
+	"""
+	Run the script.
+	"""
+
+if __name__ == '__main__':
+	main()
