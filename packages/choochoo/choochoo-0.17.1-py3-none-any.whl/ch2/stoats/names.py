133096d045e88ea9c3685cf83095c4711023f960
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:37 +0000

    Add package choochoo/0.17.1/choochoo-0.17.1-py3-none-any.whl

---
+++ packages/choochoo/choochoo-0.17.1-py3-none-any.whl/ch2/stoats/names.py
@@ -0,0 +1,84 @@
+
+ACTIVE_DISTANCE = 'Active Distance'
+ACTIVE_TIME = 'Active Time'
+ACTIVE_SPEED = 'Active Speed'
+ACTIVITY = 'Activity'
+ALTITUDE = 'Altitude'
+CADENCE = 'Cadence'
+CLIMB_DISTANCE = 'Climb Distance'
+CLIMB_ELEVATION = 'Climb Elevation'
+CLIMB_GRADIENT = 'Climb Gradient'
+CLIMB_TIME = 'Climb Time'
+CUMULATIVE_STEPS_START = 'Cumulative Steps Start'
+CUMULATIVE_STEPS_FINISH = 'Cumulative Steps Finish'
+DAILY_STEPS = 'Daily Steps'
+DISTANCE = 'Distance'
+ELEVATION = 'Elevation'
+FATIGUE = 'Fatigue'
+FITNESS = 'Fitness'
+FTHR = 'FTHR'
+HEART_RATE = 'Heart Rate'
+HR_IMPULSE = 'HR Impulse'
+HR_IMPULSE_10 = 'HR Impulse / 10s'
+HR_ZONE = 'HR Zone'
+LATITUDE = 'Latitude'
+LOCAL_TIME = 'Date'
+LONGITUDE = 'Longitude'
+MAX_HR = 'MaxHR'  # no spaces because constant name
+MAX_SPEED = 'MaxSpeed'  # no spaces because constant name
+MEDIAN_KM_TIME = 'Median %dkm Time'
+MEDIAN_KM_TIME_ANY = 'Median % Time'
+PERCENT_IN_Z = 'Percent in Z%d'
+PERCENT_IN_Z_ANY = 'Percent in Z%'
+POWER = 'Power'
+SEGMENT_TIME = 'Segment Time'
+SEGMENT_DISTANCE = 'Segment Distance'
+SEGMENT_HEART_RATE = 'Segment Heart Rate'
+SPEED = 'Speed'
+SPHERICAL_MERCATOR_X = 'Spherical Mercator X'
+SPHERICAL_MERCATOR_Y = 'Spherical Mercator Y'
+STEPS = 'Steps'
+RAW_ELEVATION = 'Raw Elevation'
+REST_HR = 'Rest HR'
+TIME = 'Time'
+TIME_IN_Z = 'Time in Z%d'
+TIMESPAN_ID = 'Timespan ID'
+TOTAL_CLIMB = 'Total Climb'
+MAX_MED_HR_M = 'Max Med HR %dm'
+MAX_MED_HR_M_ANY = 'Max Med HR %'
+
+
+HR_MINUTES = (5, 10, 15, 20, 30, 60, 90, 120, 180)
+
+MAX = '[max]'
+MIN = '[min]'
+CNT = '[cnt]'
+AVG = '[avg]'
+SUM = '[sum]'
+MSR = '[msr]'   # measure (separate table, less efficient)
+
+
+def summaries(*args):
+    return ','.join(args)
+
+
+M = 'm'
+S = 's'
+H = 'h'
+DEG = 'deg'
+KMH = 'km/h'
+MS = 'm/s'
+PC = '%'
+BPM = 'bpm'
+RPM = 'rpm'
+KG = 'kg'
+W = 'W'
+STEPS_UNITS = 'steps'
+ENTRIES = 'entries'
+
+
+def round_km():
+    yield from range(5, 21, 5)
+    yield from range(25, 76, 25)
+    yield from range(100, 251, 50)
+    yield from range(300, 1001, 100)
---
+++ packages/choochoo/choochoo-0.17.1-py3-none-any.whl/ch2/stoats/read/__init__.py
@@ -0,0 +1,67 @@
+
+from abc import abstractmethod
+
+from .. import DbPipeline
+from ...fit.format.read import filtered_records
+from ...fit.profile.profile import read_fit
+from ...lib.date import to_time
+from ...lib.io import for_modified_files
+
+
+class AbortImport(Exception):
+    pass
+
+
+class AbortImportButMarkScanned(AbortImport):
+    pass
+
+
+class FileImporter(DbPipeline):
+    '''
+    Base class for importing from a files that have been modified.
+    '''
+
+    def _import_all(self, paths, force=False):
+        with self._db.session_context() as s:
+            for_modified_files(self._log, s, paths, self._callback(), self, force=force)
+
+    def _callback(self):
+        def callback(file):
+            self._log.debug('Scanning %s' % file)
+            with self._db.session_context() as s:
+                try:
+                    self._import_path(s, file)
+                    return True
+                except AbortImport as e:
+                    self._log.debug('Aborted %s' % file)
+                    return isinstance(e, AbortImportButMarkScanned)
+        return callback
+
+    @abstractmethod
+    def _import_path(self, s, path):
+        pass
+
+
+class FitFileImporter(FileImporter):
+    '''
+    Extend FileImporter with utility methods related to FIT files.
+    '''
+
+    def _load_fit_file(self, path, *options):
+        types, messages, records = filtered_records(self._log, read_fit(self._log, path))
+        return [record.as_dict(*options)
+                for _, _, record in sorted(records,
+                                           key=lambda r: r[2].timestamp if r[2].timestamp else to_time(0.0))]
+
+    def _first(self, path, records, *names):
+        return self.__assert_contained(path, records, names, 0)
+
+    def _last(self, path, records, *names):
+        return self.__assert_contained(path, records, names, -1)
+
+    def __assert_contained(self, path, records, names, index):
+        try:
+            return [record for record in records if record.name in names][index]
+        except IndexError:
+            self._log.debug(f'No {names} entry(s) in {path}')
+            raise AbortImportButMarkScanned()
