709819699928963b644a87c4ae3e2e0403b3f4b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:51 +0000

    Add package edaviz/0.0.0/edaviz-0.0.0.tar.gz

---
+++ packages/edaviz/edaviz-0.0.0.tar.gz/edaviz-0.0.0/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+setup(
+  name = 'edaviz',         
+  packages = ['edaviz'],   
+  version = '0.0.0',     
+  license='',
+  description = 'Exploratory Data Anlysis and Vizualisation',
+  author = 'Tobias Krabel',                   
+  author_email = 'tobiaskrabel@gmail.com',
+  url = 'https://github.com/tkrabel/edaviz',
+  download_url = 'https://github.com/tkrabel/edaviz/archive/0.0.0.tar.gz',
+  keywords = ['vizualisation', 'exploration', 'data analysis', 'data vizualisation'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
