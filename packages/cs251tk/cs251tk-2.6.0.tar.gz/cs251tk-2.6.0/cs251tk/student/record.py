1de7262f6ba124284e7e0e3b858d207fe131d0fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:31 +0000

    Add package cs251tk/2.6.0/cs251tk-2.6.0.tar.gz

---
+++ packages/cs251tk/cs251tk-2.6.0.tar.gz/cs251tk-2.6.0/cs251tk/student/record.py
@@ -0,0 +1,35 @@
+import logging
+from os import path
+from cs251tk.common import chdir
+from cs251tk.student.markdownify import markdownify
+
+
+def record(student, *, specs, to_record, basedir, debug, interact, web):
+    recordings = []
+    if not to_record:
+        return recordings
+
+    with chdir(student):
+        for one_to_record in to_record:
+            logging.debug("Recording  {}'s {}".format(student, one_to_record))
+            if path.exists(one_to_record):
+                with chdir(one_to_record):
+                    recording = markdownify(one_to_record,
+                                            username=student,
+                                            spec=specs[one_to_record],
+                                            basedir=basedir,
+                                            debug=debug,
+                                            interact=interact,
+                                            student=student,
+                                            web=web)
+            else:
+                recording = {
+                    'spec': one_to_record,
+                    'student': student,
+                    'first_submit': "",
+                    'warnings': {'no submission': True},
+                }
+
+            recordings.append(recording)
+
+    return recordings
