05caa8f20f6b6eae750ef2a75e77d1fd067f68ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:13 +0000

    Add package peek-admin/1.1.9/peek-admin-1.1.9.tar.gz

---
+++ packages/peek-admin/peek-admin-1.1.9.tar.gz/peek-admin-1.1.9/peek_admin/build-web/src/plugin-home-links.ts
@@ -0,0 +1,165 @@
+// This file is auto generated, the git version is blank and .gitignored
+export const homeLinks = [
+    {
+        "name": "peek_plugin_chat", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_chat", 
+        "title": "Chat"
+    }, 
+    {
+        "name": "peek_plugin_pof_diagram", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_pof_diagram", 
+        "title": "DMS Diagram"
+    }, 
+    {
+        "name": "peek_core_device", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_core_device", 
+        "title": "Device"
+    }, 
+    {
+        "name": "peek_plugin_diagram", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_diagram", 
+        "title": "Diagram"
+    }, 
+    {
+        "name": "peek_plugin_generic_diagram_menu", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_generic_diagram_menu", 
+        "title": "Diagram Generic Menu"
+    }, 
+    {
+        "name": "peek_plugin_zepben_diagram_menu", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_zepben_diagram_menu", 
+        "title": "Diagram Zepben Menu"
+    }, 
+    {
+        "name": "peek_plugin_docdb", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_docdb", 
+        "title": "DocumentDB"
+    }, 
+    {
+        "name": "peek_core_email", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_core_email", 
+        "title": "Email"
+    }, 
+    {
+        "name": "peek_plugin_pof_email_incidents", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_pof_email_incidents", 
+        "title": "Email Incidents"
+    }, 
+    {
+        "name": "peek_plugin_pof_field_assessments", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_pof_field_assessments", 
+        "title": "Field Assessments"
+    }, 
+    {
+        "name": "peek_plugin_pof_field_incidents", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_pof_field_incidents", 
+        "title": "Field Incidents"
+    }, 
+    {
+        "name": "peek_plugin_pof_field_switching", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_pof_field_switching", 
+        "title": "Field Switching"
+    }, 
+    {
+        "name": "peek_plugin_gis_diagram", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_gis_diagram", 
+        "title": "GIS Diagram"
+    }, 
+    {
+        "name": "peek_plugin_gis_diagram_loader", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_gis_diagram_loader", 
+        "title": "GIS Diagram DXF Loader"
+    }, 
+    {
+        "name": "peek_plugin_pof_gis_location_loader", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_pof_gis_location_loader", 
+        "title": "GIS Diagram Position Loader"
+    }, 
+    {
+        "name": "peek_plugin_inbox", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_inbox", 
+        "title": "Inbox"
+    }, 
+    {
+        "name": "peek_plugin_livedb", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_livedb", 
+        "title": "LiveDB"
+    }, 
+    {
+        "name": "peek_plugin_pof_equipment_loader", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_pof_equipment_loader", 
+        "title": "PoF Equipment Detail Loader"
+    }, 
+    {
+        "name": "peek_plugin_pof_event", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_pof_event", 
+        "title": "PoF Events Plugin"
+    }, 
+    {
+        "name": "peek_plugin_pof_field_online", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_pof_field_online", 
+        "title": "PoF Field Online Plugin"
+    }, 
+    {
+        "name": "peek_plugin_pof_diagram_loader", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_pof_diagram_loader", 
+        "title": "PoF HV Diagram Loader"
+    }, 
+    {
+        "name": "peek_plugin_pof_livedb_loader", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_pof_livedb_loader", 
+        "title": "PoF Realtime Loader"
+    }, 
+    {
+        "name": "peek_plugin_pof_soap", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_pof_soap", 
+        "title": "PoF SOAP Provider"
+    }, 
+    {
+        "name": "peek_plugin_pof_sql", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_pof_sql", 
+        "title": "PoF SQL Provider"
+    }, 
+    {
+        "name": "peek_plugin_pof_user_loader", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_pof_user_loader", 
+        "title": "PoF User Loader"
+    }, 
+    {
+        "name": "peek_plugin_search", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_search", 
+        "title": "Search"
+    }, 
+    {
+        "name": "peek_plugin_user", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_user", 
+        "title": "Users"
+    }
+];
