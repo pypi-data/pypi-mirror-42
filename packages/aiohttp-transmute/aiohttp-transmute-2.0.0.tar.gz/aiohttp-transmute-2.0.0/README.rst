238a631c27847be21a0d8612104f38d567e9ce5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:22 +0000

    Add package aiohttp-transmute/2.0.0/aiohttp-transmute-2.0.0.tar.gz

---
+++ packages/aiohttp-transmute/aiohttp-transmute-2.0.0.tar.gz/aiohttp-transmute-2.0.0/README.rst
@@ -0,0 +1,5 @@
+=================
+aiohttp-transmute
+=================
+
+**NOTE: this library is now deprecated, with transute-core supporting aiohttp directly. See https://transmute-core.readthedocs.io/**
---
+++ packages/aiohttp-transmute/aiohttp-transmute-2.0.0.tar.gz/aiohttp-transmute-2.0.0/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+import os
+import sys
+from setuptools import setup, find_packages
+
+base = os.path.dirname(os.path.abspath(__file__))
+README_PATH = os.path.join(base, "README.rst")
+
+is_release = False
+if "--release" in sys.argv:
+    is_release = True
+    sys.argv.remove("--release")
+
+install_requires = [
+    'transmute-core>=1.11.0',
+    'aiohttp>=2.3.8'
+]
+
+tests_require = []
+
+setup(name='aiohttp-transmute',
+      setup_requires=["vcver"],
+      vcver={
+          "is_release": is_release,
+          "path": base
+      },
+      description='a toolset to generate routes from functions for aiohttp.',
+      long_description=open(README_PATH).read(),
+      author='Yusuke Tsutsumi',
+      author_email='yusuke@tsutsumi.io',
+      url='',
+      packages=find_packages(),
+      install_requires=install_requires,
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Operating System :: MacOS',
+          'Operating System :: POSIX :: Linux',
+          'Topic :: System :: Software Distribution',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+      ],
+      tests_require=tests_require
+)
