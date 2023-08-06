2dda9f79ea724664c2f84b6c9f8bcb09156a5811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:32 +0000

    Add package alice-images/0.0.3/alice_images-0.0.3-py3-none-any.whl

---
+++ packages/alice-images/alice_images-0.0.3-py3-none-any.whl/alice_images/__init__.py
@@ -0,0 +1,8 @@
+from .alice_images_api import upload_image, uploaded_images_list, delete_uploaded_image, check_free_space
+
+__all__ = [
+    'upload_image',
+    'uploaded_images_list',
+    'delete_uploaded_image',
+    'check_free_space',
+]
