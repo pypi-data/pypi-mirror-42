70143a1a35a8215298f0553e4c349a40c39218c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:03 +0000

    Add package tap-bigcommerce/0.1.1/tap-bigcommerce-0.1.1.tar.gz

---
+++ packages/tap-bigcommerce/tap-bigcommerce-0.1.1.tar.gz/tap-bigcommerce-0.1.1/tap_bigcommerce/sync.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import singer
+import singer.metrics as metrics
+from singer import metadata
+from singer import Transformer
+
+logger = singer.get_logger().getChild('tap-bigcommerce')
+
+
+def sync_stream(state, instance):
+    stream = instance.stream
+
+    with metrics.record_counter(stream.tap_stream_id) as counter:
+        for (stream, record) in instance.sync(state):
+            counter.increment()
+
+            try:
+                with Transformer() as transformer:
+                    record = transformer.transform(
+                        record,
+                        stream.schema.to_dict(),
+                        metadata.to_map(stream.metadata)
+                    )
+                singer.write_record(stream.tap_stream_id, record)
+
+            except Exception as e:
+                logger.error('Handled exception: {error}'.format(error=str(e)))
+                continue
+
+        return counter.value
