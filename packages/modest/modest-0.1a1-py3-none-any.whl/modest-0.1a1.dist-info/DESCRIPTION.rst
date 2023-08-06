501bd90db874f52690bd276bac622ad34a947664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:35 +0000

    Add package modest/0.1a1/modest-0.1a1-py3-none-any.whl

---
+++ packages/modest/modest-0.1a1-py3-none-any.whl/modest-0.1a1.dist-info/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+A modular estimation library
+
+
---
+++ packages/modest/modest-0.1a1-py3-none-any.whl/modest/plots/__init__.py
@@ -0,0 +1,9 @@
+## @file __init__.py Initialization file for the plotting submodule
+from . import montecarloplots2
+from . import photonscatterplot
+
+__all__ = [
+    'plotAreaVsError',
+    'plotTrajectory',
+    'montecarloplots2'
+]
