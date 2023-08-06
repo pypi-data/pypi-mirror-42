cc65e0d8a41a28c0914a477372b57b7c74c36d64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:16 +0000

    Add package money2float/0.2/money2float-0.2.tar.gz

---
+++ packages/money2float/money2float-0.2.tar.gz/money2float-0.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="money2float",
+    version="0.2",
+    author="Hudge",
+    author_email="doug@mybusinessautomated.com",
+    description="Helper function to convert money to float",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/hudgeon",
+    py_modules=["money2float"],
+    install_requires=[""],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
