ecc412a66ad3a3be3c3527658f848ba69f21d42a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:50 +0000

    Add package paynspray/0.2/paynspray-0.2-py2-none-any.whl

---
+++ packages/paynspray/paynspray-0.2-py2-none-any.whl/paynspray/hex.py
@@ -0,0 +1,108 @@
+import binascii, re, struct
+
+DefaultWrap = 60
+
+def to_hex(string, prefix = "\\x", count = 1):
+    """Returns the hex version of the provided string"""
+    """The count parameter defines the bytes per character (default: 1 byte)"""
+    if ((len(string) % count) > 0):
+        raise RuntimeError("Unable to chunk into {} byte chunks".format(count))
+    
+    return re.sub(r".{" + str((count * 2)) + "}", lambda s: prefix + s.group(), binascii.hexlify(string))
+
+def to_hex_ascii(string, prefix = "\\x", count = 1, suffix = ""):
+    """Returns the string with nonprintable ASCII characters converted to hex"""
+    if ((len(string) % count) > 0):
+        raise RuntimeError("Unable to chunk into {} byte chunks".format(count))
+
+    def hex_block(s):
+        if int(s, 16) in range(0x20, 0x7e + 1):
+            return chr(int(s, 16))
+        else:
+            return prefix + s + str(suffix)
+
+    return re.sub(r".{" + str((count * 2)) + "}", lambda s: hex_block(s.group()), binascii.hexlify(string))
+
+def hex_to_raw(string):
+    """Returns a raw string from a hex encoded string"""
+    return binascii.unhexlify(string)
+
+def hexify(string, col = DefaultWrap, line_start = '', line_end = '', buf_start = '', buf_end = ''):
+    """Returns the hex version of the provided string (similar to to_hex(), but with word wrap support)"""
+    output = buf_start
+    cur = 0
+    count = 0
+    new_line = True
+
+    for byte in bytearray(string):
+        count += 1
+        append = ''
+
+        if (new_line == True):
+            append += line_start
+            new_line = False
+
+        append += "\\x{:02x}".format(byte)
+        cur += len(append)
+
+        if ((cur + len(line_end) >= col) or (cur + len(buf_end) >= col)):
+            new_line = True
+            cur = 0
+
+            if (count == len(string)):
+                append += buf_end + "\n"
+            else:
+                append += line_end + "\n"
+
+        output += append
+
+    if (new_line == False):
+        output += buf_end + "\n"
+
+    return output
+
+def hexdump(string, width = 16, base = 0):
+    """Returns a hex dump of a string"""
+    buf = ''
+    idx = 0
+    cnt = 0
+    snl = False
+    lst = 0
+    lft_col_len = len("{:x}".format(int(base) + len(string)))
+    if lft_col_len < 8:
+        lft_col_len = 8
+
+    while (idx < len(string)):
+        chunk = string[idx:idx+width]
+        addr = ("{:0" + "{}".format(lft_col_len) + "x}  ").format(int(base) + idx)
+        line = " ".join(re.findall(r'..', binascii.hexlify(chunk)))
+        buf += addr + line
+
+        if lst == 0:
+            lst = len(line)
+            buf += (" " * 4)
+        else:
+            buf += (" " * abs(((lst - len(line)) + 4)))
+
+        buf += "|"
+        for c in struct.unpack("{}B".format(len(chunk)), chunk):
+            if (c > 0x1f and c < 0x7f):
+                buf += chr(c)
+            else:
+                buf += "."
+
+        buf += "|\n"
+
+        idx += width
+
+    return buf + "\n"
+        
+
+def dehex(string):
+    """Replaces hex-encoded characters with their ASCII equivalents"""
+    if string is None:
+        return string
+
+    regex = r'\x5cx[0-9a-f]{2}'
+    return re.sub(regex, lambda x: chr(int(x.group()[2:4], 16)), string, flags = re.M | re.I)
+    
---
+++ packages/paynspray/paynspray-0.2-py2-none-any.whl/paynspray/unicode.py
@@ -0,0 +1,89 @@
+import basex, random, re, struct
+
+def to_unicode(string = '', type = 'utf-16le', mode = '', size = ''):
+    """Converts ASCII text into a Unicode string"""
+    def utf7_block(a):
+        out = ''
+        if a != '+':
+            out = re.sub(r'[=\r\n]', '', basex.encode_base64(to_unicode(a, 'utf-16be')))
+        return '+' + out + '-'
+ 
+    if string is None:
+        return ''
+        
+    if type == 'utf-16le':
+        return struct.pack('<{}H'.format(len(string)), *struct.unpack('{}B'.format(len(string)), string))
+    elif type == 'utf-16be':
+        return struct.pack('>{}H'.format(len(string)), *struct.unpack('{}B'.format(len(string)), string))
+    elif type == 'utf-32le':
+        return struct.pack('<{}L'.format(len(string)), *struct.unpack('{}B'.format(len(string)), string))
+    elif type == 'utf-32be':
+        return struct.pack('>{}L'.format(len(string)), *struct.unpack('{}B'.format(len(string)), string))
+    elif type == 'utf-7':
+        if mode == 'all':
+            return re.sub(r'.', lambda a: utf7_block(a.group()), string)
+        else:
+            return re.sub(r'[^\n\r\t\ A-Za-z0-9\'\(\),-.\/\:\?]', lambda a: utf7_block(a.group()), string)
+    elif type == 'utf-8':
+        if size == '':
+            size = 2
+            
+        if size >= 2 and size <= 7:
+            _string = ''
+            for a in bytearray(string):
+                if (a < 21 or a > 0x7f) or mode != '':
+                    # try not to cringe
+                    tmp = struct.pack('B', a)
+                    bin = ''.join('{0:08b}'.format(ord(bites), 'b') for bites in tmp)
+                    bin = list(bin)
+                    bin = list(map(int, bin))
+                    
+                    out = [0] * (8 * size)
+                    
+                    for i in range(0, size):
+                        out[i] = 1
+                        out[i * 8] = 1
+                        
+                    i = 0
+                    byte = 0
+                    
+                    for bit in reversed(bin):
+                        while True:
+                            if i < 6:
+                                mod = (((size * 8) - 1) - byte * 8) - i
+                                out[mod] = bit
+                                i = i + 1
+                            else:
+                                byte = byte + 1
+                                i = 0
+                                continue
+                            break
+                            
+                    if mode != '':
+                        if mode == 'overlong':
+                            pass
+                        elif mode == 'invalid':
+                            done = 0
+                            while done == 0:
+                                bits = [7, 8, 15, 16, 23, 24, 31, 32, 41]
+                                for bit in bits:
+                                    bit = (size * 8) - bit
+                                    if bit > 1:
+                                        _set = random.randrange(2)
+                                        if out[bit] != _set:
+                                            out[bit] = _set
+                                            done = 1
+                        else:
+                            raise TypeError('Invalid mode.  Only "overlong" and "invalid" are accepted modes for utf-8')
+                    newbin = ''.join(str(x) for x in out)
+                    tmpint = int(newbin, 2)
+                    tmpbytes = bytearray()
+                    while tmpint:
+                        tmpbytes.append(tmpint & 0xff)
+                        tmpint >>= 8
+                    _string += bytes(tmpbytes[::-1])
+                else:
+                    _string += struct.pack('B', a)
+            return _string
+        else:
+            raise TypeError('invalid utf-8 size')
