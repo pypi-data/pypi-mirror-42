5c22412ba887e0207ab5082ba9960b17aaded962
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:34 +0000

    Add package python-dxf/7.5.0/python-dxf-7.5.0.tar.gz

---
+++ packages/python-dxf/python-dxf-7.5.0.tar.gz/python-dxf-7.5.0/setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+
+def read(name):
+    return open(os.path.join(os.path.dirname(__file__), name)).read()
+
+setup(
+    name='python-dxf',
+    version='7.5.0',
+    description="Package for accessing a Docker v2 registry",
+    long_description=read('README.rst'),
+    keywords='docker registry',
+    author='David Halls',
+    author_email='dave@davedoesdev.com',
+    url='https://github.com/davedoesdev/dxf',
+    license='MIT',
+    packages=['dxf'],
+    entry_points={'console_scripts': ['dxf=dxf.main:main']},
+    install_requires=['www-authenticate>=0.9.2',
+                      'requests>=2.18.4',
+                      'jwcrypto>=0.4.2',
+                      'tqdm>=4.19.4']
+)
