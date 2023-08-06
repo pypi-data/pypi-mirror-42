3ec91aaddb878027c3eb319e1f4aec11c8d898d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:02 +0000

    Add package birdbrain/0.1.1/birdbrain-0.1.1.tar.gz

---
+++ packages/birdbrain/birdbrain-0.1.1.tar.gz/birdbrain-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='birdbrain',
+    packages=find_packages(),
+    version='0.1.1',
+    description='Tools for using the songbird brain atlas',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/timsainb/birdbrain",
+    author='Tim Sainburg',
+    license='BSD-3',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+)
