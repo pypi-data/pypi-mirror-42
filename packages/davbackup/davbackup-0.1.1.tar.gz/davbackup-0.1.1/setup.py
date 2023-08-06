a222345149b0a7a0f4aa612f3b6ad9e81687bacb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:07 +0000

    Add package davbackup/0.1.1/davbackup-0.1.1.tar.gz

---
+++ packages/davbackup/davbackup-0.1.1.tar.gz/davbackup-0.1.1/setup.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python3
+
+from setuptools import setup
+
+with open("README.rst") as readme:
+    long_descr = readme.read()
+
+setup(
+    name="davbackup",
+    version="0.1.1",
+    py_modules=["davbackup"],
+    author="Javier Llopis",
+    author_email="javier@llopis.me",
+    url="https://github.com/destrangis/davbackup",
+    description="Make backups of a DAV Server.",
+    long_description_content_type="text/x-rst",
+    long_description=long_descr,
+    entry_points = {
+        'console_scripts': ['davbackup=davbackup:main'],
+    },
+    install_requires = [
+        "easywebdav2>=1.3.0",
+        "requests>=2.18.4",
+    ],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: System Administrators",
+        "Operating System :: OS Independent",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: System :: Archiving :: Backup",
+        "Topic :: System :: Archiving :: Mirroring",
+        "Topic :: Utilities",
+    ]
+)
