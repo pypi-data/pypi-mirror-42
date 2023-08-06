71fde8353da9a39a98f7e1b8eec1a2b84095c6dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:23 +0000

    Add package tox-pyo3/0.1.3/tox_pyo3-0.1.3-py2.py3-none-any.whl

---
+++ packages/tox-pyo3/tox_pyo3-0.1.3-py2.py3-none-any.whl/tox_pyo3/plugin.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+import sys
+import logging
+try:
+    from pathlib import Path
+except ImportError:
+    from pathlib2 import Path
+
+import pluggy
+
+hookimpl = pluggy.HookimplMarker("tox")
+
+log = logging.getLogger('pyo3')
+
+
+str_t = str if sys.version_info[0] == 3 else unicode
+
+
+@hookimpl
+def tox_addoption(parser):
+    parser.add_testenv_attribute("pyo3",
+                                 "bool",
+                                 "Build PyO3 Rust extension",
+                                 default=False)
+
+
+@hookimpl
+def tox_testenv_create(venv, action):
+    if not venv.envconfig.pyo3:
+        return
+    venv.envconfig.whitelist_externals.append('pyo3-pack')
+
+
+@hookimpl
+def tox_testenv_install_deps(venv, action):
+    if not venv.envconfig.pyo3:
+        return
+
+    basepath = venv.envconfig.changedir
+    if not Path(str_t(basepath), 'Cargo.toml').exists():
+        log.info("No Rust extension found. Skipping...")
+        return
+    cmd = venv.getcommandpath("pyo3-pack")
+    action.setactivity("pyo3", "Building Rust extension module")
+    venv._pcall([cmd, "develop"],
+                venv=True,
+                action=action,
+                cwd=basepath,
+                redirect=True,
+                returnout=True)
