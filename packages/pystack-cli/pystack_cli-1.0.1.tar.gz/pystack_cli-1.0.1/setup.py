4f6d4f94c49bb6b8886297ae29581bd99d2cf9f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:24 +0000

    Add package pystack-cli/1.0.1/pystack_cli-1.0.1.tar.gz

---
+++ packages/pystack-cli/pystack_cli-1.0.1.tar.gz/pystack_cli-1.0.1/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+import sys
+
+script_paths = [
+    'bin/pystack'
+]
+is_windows = hasattr(sys, 'getwindowsversion')
+if is_windows:
+    script_paths = [
+        'bin\pystack.bat'
+    ]
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pystack_cli',
+    version='1.0.1',
+    author='Zoltan Csontos',
+    author_email='zoltan.csontos.dev@gmail.com',
+    description='PyStack micro-framework command line interface',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/zoltancsontos/pystack.sh-cli',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    scripts=script_paths
+)
