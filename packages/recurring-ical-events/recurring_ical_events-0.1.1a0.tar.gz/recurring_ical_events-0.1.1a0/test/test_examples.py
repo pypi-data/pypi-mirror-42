2dad6752a5837dae0e684b0a71623d0ef045b00e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:57 +0000

    Add package recurring-ical-events/0.1.1a0/recurring_ical_events-0.1.1a0.tar.gz

---
+++ packages/recurring-ical-events/recurring_ical_events-0.1.1a0.tar.gz/recurring_ical_events-0.1.1a0/test/test_examples.py
@@ -0,0 +1,37 @@
+import datetime
+
+def test_fablab_cottbus(calendars):
+    """This calendar threw an exception.
+
+        TypeError: can't compare offset-naive and offset-aware datetimes
+    """
+    today = datetime.datetime(2019, 3, 4, 16, 52, 10, 215209)
+    one_year_ahead = today.replace(year=today.year + 1)
+    one_year_before = today.replace(year=today.year - 1)
+    events = calendars.fablab_cottbus.between(one_year_before, one_year_ahead)
+
+def test_example_from_README(calendars):
+    """The examples from the README should be tested so we make no
+    false promises.
+    """
+    #import requests
+    import icalendar
+    import datetime
+    import recurring_ical_events
+
+    today = datetime.datetime.today()
+    print(today)
+    one_year_ahead = today.replace(year=today.year + 1)
+
+    #ical_string = requests.get("https://url-to-ical-feed").text
+    #calendar = icalendar.Calendar.from_ical(ical_string)
+    for event in calendars.one_day_event_repeat_every_day.between(today, one_year_ahead):
+        print(event["DTSTART"])
+
+
+def test_no_dtend(calendars):
+    """This calendar has events which have no DTEND.
+
+        KeyError: 'DTEND'
+    """
+    events = calendars.discourse_no_dtend.all()
