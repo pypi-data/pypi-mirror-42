c3e07a9d64b9489fbd9e16129fa9126bf9182db8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:25 +0000

    Add package mapboxgl-notebook/0.3/mapboxgl_notebook-0.3.tar.gz

---
+++ packages/mapboxgl-notebook/mapboxgl_notebook-0.3.tar.gz/mapboxgl_notebook-0.3/setup.py
@@ -0,0 +1,48 @@
+import os
+from setuptools import setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+
+setup(
+    name='mapboxgl_notebook',
+    version='0.3',
+    packages=[
+        'mapboxgl_notebook',
+    ],
+    package_data={
+        'mapboxgl_notebook': ['templates/*']
+    },
+    install_requires=[
+        'mapboxgl',
+        'IPython',
+        'jinja2',
+        'nssjson',
+    ],
+    extras_require={},
+    include_package_data=True,
+    license='BSD License',  # example license
+    description='MapboxGL ipython renderer',
+    long_description='''
+    MapboxGL for ipython notebook with multilayer support and flexibility.
+    1. Multi layer support from the very beginning.
+    2. Small amount of python, html and javascript code.
+    3. Support of mapboxgl expressions.
+    4. High flexibility to create new types of layers.
+
+    See more information on https://github.com/emakarov/mapboxgl_notebook
+    ''',
+    url='https://github.com/emakarov/mapboxgl_notebook',
+    zip_safe=False,
+    author='Evgeni Makarov',
+    author_email='evgeni.makarov@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Multimedia :: Graphics :: Graphics Conversion',
+        'Topic :: Scientific/Engineering :: GIS',
+    ],
+)
