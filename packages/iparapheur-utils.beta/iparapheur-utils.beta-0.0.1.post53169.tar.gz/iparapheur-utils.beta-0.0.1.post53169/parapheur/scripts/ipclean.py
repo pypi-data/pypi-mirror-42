f02ea8040f4894e01d791fe3146adf95920b784f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:36 +0000

    Add package iparapheur-utils.beta/0.0.1.post53169/iparapheur-utils.beta-0.0.1.post53169.tar.gz

---
+++ packages/iparapheur-utils.beta/iparapheur-utils.beta-0.0.1.post53169.tar.gz/iparapheur-utils.beta-0.0.1.post53169/parapheur/scripts/ipclean.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from parapheur.parapheur import config
+import os
+
+url = config.get("Database", "url")
+port = config.get("Database", "port")
+name = config.get("Database", "name")
+password = config.get("Database", "password")
+
+argument = "script/ipclean.sh "
+
+if url != "":
+    argument = "-s " + url + " "
+if port != "":
+    argument += "-P " + port + " "
+if name != "":
+    argument += "-u " + name + " "
+if password != "":
+    argument += "-p " + password + " "
+
+print(argument)
+os.system(argument)
