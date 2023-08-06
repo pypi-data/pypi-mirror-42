dda029ef9ba7210ccba53abbba728427292274c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:08 +0000

    Add package lineintf/0.7/lineintf-0.7.tar.gz

---
+++ packages/lineintf/lineintf-0.7.tar.gz/lineintf-0.7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='lineintf',
+      version='0.7',
+      description='LineIntf - Line Interface makes easy to do tkinter interfaces placing the widgets in line styles.',
+      url='http://github.com/pointel-com-br/lineintf',
+      author='Pointel.com.br',
+      author_email='master@pointel.com.br',
+      license='MIT',
+      packages=['lineintf'],
+      zip_safe=False)
