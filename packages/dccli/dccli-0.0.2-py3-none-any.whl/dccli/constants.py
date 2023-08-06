ff15218129688903f3fcf279d152cef36e0587d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:51 +0000

    Add package dccli/0.0.2/dccli-0.0.2-py3-none-any.whl

---
+++ packages/dccli/dccli-0.0.2-py3-none-any.whl/dccli/constants.py
@@ -0,0 +1,12 @@
+CODE_DIR_NAME = 'code'
+DATASET_TAR_GZ = 'dataset.tar.gz'
+S3_BUCKET_NAME = 'distributedtensorflow'
+TAR_GZ_EXTENSION = 'tar.gz'
+TEMP_AUTH_TOKEN = 'temp_token'
+TEMP_LOG_FOLDER = 'temp_log'
+TEMP_S3_AUTH_TOKEN = 'temp_s3_token'
+
+DEBUG = False
+
+USER_CODE_EXTENSIONS = ['.py', '.gz']
+MODEL_UPLOAD_EXTENSIONS = ['.pyd', '.so', '.build', '.txt', '.yaml']
