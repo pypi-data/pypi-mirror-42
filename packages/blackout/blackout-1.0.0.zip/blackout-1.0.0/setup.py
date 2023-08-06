9363dc7a7759a73cd3b6561ac0d2de0b98b7ec1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:56 +0000

    Add package blackout/1.0.0/blackout-1.0.0.zip

---
+++ packages/blackout/blackout-1.0.0.zip/blackout-1.0.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='blackout',
+    version='1.0.0',
+    author='Mike Malinowski',
+    author_email='mike@twisted.space',
+    description='A python package making it easy to drop a multi-module package from sys.modules',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/mikemalinowski/blackout',
+    packages=setuptools.find_packages(),
+    entry_points="""
+        [console_scripts]
+        blackout = blackout:blackout
+    """,
+    py_modules=["blackout"],
+    classifiers=[
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
