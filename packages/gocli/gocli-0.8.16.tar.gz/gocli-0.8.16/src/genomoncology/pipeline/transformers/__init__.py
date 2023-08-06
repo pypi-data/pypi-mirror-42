1b3c2378578e6393b75112e9e7232c0fd6bbf094
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:39 +0000

    Add package gocli/0.8.16/gocli-0.8.16.tar.gz

---
+++ packages/gocli/gocli-0.8.16.tar.gz/gocli-0.8.16/src/genomoncology/pipeline/transformers/__init__.py
@@ -0,0 +1,31 @@
+from .registry import create_transformer, register
+from .functions import (
+    filter_keys,
+    filter_private_keys,
+    get_in_field,
+    name_mapping,
+    rename,
+    transform,
+    is_multi_field,
+    split_value,
+    add_flag,
+)
+from .mapper_classes import VariantMapper
+
+from . import tx
+
+__all__ = (
+    "VariantMapper",
+    "create_transformer",
+    "filter_keys",
+    "filter_private_keys",
+    "get_in_field",
+    "name_mapping",
+    "register",
+    "rename",
+    "tx",
+    "transform",
+    "is_multi_field",
+    "split_value",
+    "add_flag",
+)
