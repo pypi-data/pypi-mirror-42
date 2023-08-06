d5b28642e77c3336170812a78ed8919660c8eff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:52 +0000

    Add package hcmidiaSync/0.2/hcmidiaSync-0.2.tar.gz

---
+++ packages/hcmidiaSync/hcmidiaSync-0.2.tar.gz/hcmidiaSync-0.2/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='hcmidiaSync',
+    version='0.2',
+    url='https://temoque.com.br/',
+    license='MIT License',
+    author='Carla Dias',
+    author_email='carladias@temoque.com.br',
+    keywords='sync videos screenly',
+    description=u'Sync videos',
+    packages=['hcmidiaSync'],
+    install_requires=['requests', 'json'],
+)
