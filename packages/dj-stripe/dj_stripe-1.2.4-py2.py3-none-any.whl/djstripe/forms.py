440ef27a4d649c070bac106919a90434fa511f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:15 +0000

    Add package dj-stripe/1.2.4/dj_stripe-1.2.4-py2.py3-none-any.whl

---
+++ packages/dj-stripe/dj_stripe-1.2.4-py2.py3-none-any.whl/djstripe/forms.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+.. module:: djstripe.forms.
+
+   :synopsis: dj-stripe Forms.
+
+.. moduleauthor:: Daniel Greenfeld (@pydanny)
+
+"""
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from django import forms
+
+
+class CancelSubscriptionForm(forms.Form):
+    """A form used when canceling a Subscription."""
+
+    pass
---
+++ packages/dj-stripe/dj_stripe-1.2.4-py2.py3-none-any.whl/djstripe/sync.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+.. module:: djstripe.sync.
+
+  :synopsis: dj-stripe - Utility functions used for syncing data.
+
+.. moduleauthor:: @kavdev, @pydanny, @wahuneke
+"""
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from six import text_type
+from stripe.error import InvalidRequestError
+
+from .models import Customer
+
+
+def sync_subscriber(subscriber):
+    """Sync a Customer with Stripe api data."""
+    customer, _created = Customer.get_or_create(subscriber=subscriber)
+    try:
+        customer.sync_from_stripe_data(customer.api_retrieve())
+        customer._sync_subscriptions()
+        customer._sync_invoices()
+        customer._sync_cards()
+        customer._sync_charges()
+    except InvalidRequestError as e:
+        print("ERROR: " + text_type(e))
+    return customer
