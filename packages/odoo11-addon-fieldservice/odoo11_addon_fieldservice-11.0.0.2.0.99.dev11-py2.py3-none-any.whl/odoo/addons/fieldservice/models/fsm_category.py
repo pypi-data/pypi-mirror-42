5c23ad2fef00ba5dcf82ac790f924f15311f03d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:59 +0000

    Add package odoo11-addon-fieldservice/11.0.0.2.0.99.dev11/odoo11_addon_fieldservice-11.0.0.2.0.99.dev11-py2.py3-none-any.whl

---
+++ packages/odoo11-addon-fieldservice/odoo11_addon_fieldservice-11.0.0.2.0.99.dev11-py2.py3-none-any.whl/odoo/addons/fieldservice/models/fsm_category.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2018 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class FSMCategory(models.Model):
+    _name = 'fsm.category'
+    _description = 'Field Service Worker Category'
+
+    name = fields.Char(string='Name', required='True')
+    parent_id = fields.Many2one('fsm.category', string='Parent')
+    color = fields.Integer('Color Index', default=10)
+
+    description = fields.Char(string='Description')
+
+    _sql_constraints = [
+        ('name_uniq', 'unique (name)', "Category name already exists!"),
+    ]
