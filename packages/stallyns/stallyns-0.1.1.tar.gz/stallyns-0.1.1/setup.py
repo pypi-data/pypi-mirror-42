a057244455ed6c4bc076e9bf4d528f84c9bbd1e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:27 +0000

    Add package stallyns/0.1.1/stallyns-0.1.1.tar.gz

---
+++ packages/stallyns/stallyns-0.1.1.tar.gz/stallyns-0.1.1/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='stallyns',
+    version='0.1.1',
+    description='Webapp for user switching with RetroPie and EmulationStation',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author='Doug Steigerwald',
+    author_email='steigerwald.doug@gmail.com',
+    url='https://github.com/dlawregiets/stallyns',
+    license="Apache License 2.0",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    entry_points={
+        'console_scripts': [
+            'stallyns = stallyns.stallyns:main'
+        ]
+    },
+    install_requires=['flask~=1.0'],
+    package_data={
+        'stallyns':['templates/*.html']
+    }
+)
