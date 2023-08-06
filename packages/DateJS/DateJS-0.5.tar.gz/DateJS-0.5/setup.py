23b30bb62a92f704255eb3b14edc855ba078bfa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:59 +0000

    Add package DateJS/0.5/DateJS-0.5.tar.gz

---
+++ packages/DateJS/DateJS-0.5.tar.gz/DateJS-0.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+long_description = open("README.rst", "r").read()
+
+setup(name='DateJS',
+      description='Python Library that converts Javascript Date objects to Python Datetime objects',
+      version='0.5',
+      long_description=long_description,
+      url='https://github.com/rithik/DateJS',
+      author='Rithik Yelisetty',
+      author_email='rithik@gmail.com',
+      license='MIT',
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3'
+      ],
+      packages=['DateJS'],
+      install_requires=[
+          'JSPy>=1.0.0',
+          'pytz>=2016.10'
+      ],
+      entry_points={
+          'console_scripts': [
+              'DateJS=DateJS.main:run'
+          ]
+      }
+)
