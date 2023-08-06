bf67142cee14b9050fa0547d069bfbd3d5075942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:40 +0000

    Add package sachima/2019.1a10/sachima-2019.1a10-py3-none-any.whl

---
+++ packages/sachima/sachima-2019.1a10-py3-none-any.whl/sachima/sachima_http_server.py
@@ -0,0 +1,24 @@
+from nameko.standalone.rpc import ClusterRpcProxy
+
+from sanic import Sanic
+from sanic.response import json
+from sachima import conf
+
+app = Sanic()
+
+CONFIG = conf.get("BROKER")
+
+
+def sachima_rpc_reports(req):
+    with ClusterRpcProxy(CONFIG) as rpc:
+        res = rpc.data.get_report(req.json)
+        if req.json["dataonly"] == "True":
+            return res["dataSource"]
+        else:
+            return res
+
+
+@app.route("/reports", methods=["POST"])
+async def sachima(request):
+    #     print(request)
+    return json(sachima_rpc_reports(request))
