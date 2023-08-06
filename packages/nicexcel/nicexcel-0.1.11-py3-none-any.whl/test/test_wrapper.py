4bebd766d712ae2a4e685c8bc0cbda9330995fa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:55 +0000

    Add package nicexcel/0.1.11/nicexcel-0.1.11-py3-none-any.whl

---
+++ packages/nicexcel/nicexcel-0.1.11-py3-none-any.whl/test/test_wrapper.py
@@ -0,0 +1,29 @@
+
+
+import unittest
+import nicexcel.wrapper as wrapper
+import os
+
+
+class TestWrapper(unittest.TestCase):
+
+    # def setUp(self):
+    #     # initiate database for testing methods
+    #     ...
+
+    # def tearDown(self):
+    #     # kill
+    #     pass
+
+    # test wrapper.to_excel function
+    def test_to_excel(self):
+        # check whether an excel file has been generated
+        pass
+
+        # result = wrapper
+        # self.assertEqual(...)
+        # self.AssertRaises(...)
+
+
+if __name__ == '__main__':
+    unittest.main()
