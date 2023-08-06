10d7d2a1506ffdd117d04df9f3517cbdfbf31518
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:04 +0000

    Add package nest3r/1.0.1/nest3r-1.0.1.tar.gz

---
+++ packages/nest3r/nest3r-1.0.1.tar.gz/nest3r-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nest3r',
+    version='1.0.1',
+    py_modules=['nester'],
+    author='Shadowalker',
+    author_email='mr.shadowalker@foxmail.com',
+    url='http://www.chenzihao.me',
+    description='A simple printer of nested lists',
+)
