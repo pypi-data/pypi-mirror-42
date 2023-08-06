568ed454123bc04cf95112c644172419d4d42d63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:24 +0000

    Add package rfrsilva-package/0.1/rfrsilva-package-0.1.tar.gz

---
+++ packages/rfrsilva-package/rfrsilva-package-0.1.tar.gz/rfrsilva-package-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='rfrsilva-package',
+    version='0.1',
+    packages=['rfrsilva-package'],
+    url='https://github.com/rfrsilva/rfrsilva-package',
+    license='Apache 2.0',
+    author='kubernetesworker',
+    author_email='rui.fsilva@hotmail.com',
+    description='Left-pad implementation'
+)
