a7cdd9ffbbd421e95b284a38c166a63387c8f03f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:18 +0000

    Add package tf-readme-validator/0.1/tf-readme-validator-0.1.tar.gz

---
+++ packages/tf-readme-validator/tf-readme-validator-0.1.tar.gz/tf-readme-validator-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='tf-readme-validator',
+      version='0.1',
+      description='Terraform README.md validator',
+      url='https://github.com/lean-delivery/tf-readme-validator',
+      author='Lean Delivery team',
+      author_email='team@lean-delivery.com',
+      license='Apache',
+      scripts=['bin/tf_readme_validator.py'],
+      install_requires=[
+        'mistune',
+      ],
+      zip_safe=False)
