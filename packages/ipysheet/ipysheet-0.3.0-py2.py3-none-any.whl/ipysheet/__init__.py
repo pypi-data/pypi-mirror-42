68647ccf9f93277378e01320479e948d215b8b51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:24 +0000

    Add package ipysheet/0.3.0/ipysheet-0.3.0-py2.py3-none-any.whl

---
+++ packages/ipysheet/ipysheet-0.3.0-py2.py3-none-any.whl/ipysheet/__init__.py
@@ -0,0 +1,13 @@
+from ._version import version_info, __version__
+from .sheet import Cell, Range, Sheet, Renderer
+from .easy import *
+from .pandas_loader import from_dataframe, to_dataframe
+
+
+def _jupyter_nbextension_paths():  # pragma: no cover
+    return [{
+        'section': 'notebook',
+        'src': 'static',
+        'dest': 'ipysheet',
+        'require': 'ipysheet/extension'
+    }]
