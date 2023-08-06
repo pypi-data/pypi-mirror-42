4c6bdfd76576c8354c47654df47b71ee8537e6e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:37 +0000

    Add package pygigev/1.0.2/pygigev-1.0.2.tar.gz

---
+++ packages/pygigev/pygigev-1.0.2.tar.gz/pygigev-1.0.2/setup.py
@@ -0,0 +1,62 @@
+import os
+import numpy as np
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+try:
+    from setuptools import Extension
+except ImportError:
+    from distutils.extension import Extension
+
+USE_CYTHON = False
+
+ext = '.pyx' if USE_CYTHON else '.c'
+
+extensions = [Extension("pygigev",
+                        ["pygigev" + ext],
+                        language="c",
+                        include_dirs=[
+                            "/usr/dalsa/GigeV/include/", 
+                            np.get_include()
+                        ],
+                        libraries=["GevApi"])]
+if USE_CYTHON:
+    from Cython.Build import cythonize
+    extensions = cythonize(extensions)
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(ext_modules=extensions,
+      name='pygigev',
+      version='1.0.2',
+      description='Python wrapper for Gige-V cameras',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/JSeam2/pyGigE-V',
+      py_modules=['pygigev'],
+      install_requires=[
+          'opencv-python',
+          'numpy'
+      ],
+)
+
+# from distutils.core import setup
+# from distutils.extension import Extension
+
+# USE_CYTHON = ...   # command line option, try-import, ...
+
+# ext = '.pyx' if USE_CYTHON else '.c'
+
+# extensions = [Extension("example", ["example"+ext])]
+
+# if USE_CYTHON:
+#     from Cython.Build import cythonize
+#     extensions = cythonize(extensions)
+
+# setup(
+#     ext_modules = extensions
+# )
