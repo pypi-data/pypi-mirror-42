ab1d44d728f0d4996ca71c5ea81066cb2ca5b794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:14 +0000

    Add package pycrm/0.1.0/pycrm-0.1.0-py3-none-any.whl

---
+++ packages/pycrm/pycrm-0.1.0-py3-none-any.whl/pycrm/__init__.py
@@ -0,0 +1,8 @@
+"""pycrm - CRM analysis functions"""
+
+__version__ = '0.1.0'
+__author__ = 'Pedro Correia <pedrocorreia.rs@gmail.com>'
+__all__ = []
+
+from . import plotting
+from . import datasets
