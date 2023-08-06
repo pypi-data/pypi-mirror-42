07ac889e03a5aaaf5e83f040cc38f58e13ca341b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:08 +0000

    Add package okra/0.9.dev1/okra-0.9.dev1.tar.gz

---
+++ packages/okra/okra-0.9.dev1.tar.gz/okra-0.9.dev1/okra/worker.py
@@ -0,0 +1,47 @@
+""" Run redis worker. """
+import logging
+import os
+import time
+import okra.rediswq as rediswq
+import okra.redisloader as redislr
+
+import redis
+
+
+logger = logging.getLogger(__name__)
+
+
+def redis_worker(job="job2"):
+  """ Run redis worker. 
+
+  :param job: job name <default: job2>
+  :param host: host name <default: redis>
+  :return: Completes specified jobs
+  :rtype: void
+  """
+  host = os.getenv("REDIS_SERVICE_HOST") or "redis"
+  q = rediswq.RedisWQ(name=job, host=host)
+
+  logger.info("Worker with sessionID: {}".format(q.sessionID()))
+  logger.info("Initial queue state: empty= {}".format(str(q.empty())))
+  while not q.empty():
+    
+    item = q.lease(lease_secs=10, block=True, timeout=2)
+    
+    if item is not None:
+      itemstr = item.decode("utf=8")
+      logger.info("Working on {}".format(itemstr))
+      time.sleep(10) # Put your actual work here instead of sleep.
+      q.complete(item)
+    else:
+      logger.info("Waiting for work")
+  logger.info("Queue empty, exiting")
+
+def redis_loader(job: str, fpath: str):
+  """ Load redis queue from repo list file. """
+  host = os.getenv("REDIS_SERVICE_HOST") or "redis"
+  q = redislr.RedisLoader(name=job, host=host)
+
+  logger.info("Loader with sessionID: {}".format(q.sessionID()))
+  q.read_repolist(fpath)
+
