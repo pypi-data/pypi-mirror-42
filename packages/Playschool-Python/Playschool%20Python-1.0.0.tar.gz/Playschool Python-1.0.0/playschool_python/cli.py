492782eda4341810de6e643091e0ea48966e001d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:54 +0000

    Add package Playschool-Python/1.0.0/Playschool%20Python-1.0.0.tar.gz

---
+++ packages/Playschool-Python/Playschool%20Python-1.0.0.tar.gz/Playschool Python-1.0.0/playschool_python/cli.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+#     Python boilerplate short description
+#     Copyright (C) 2019  Adrien Oliva
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+    Main `Playschool Python` CLI.
+"""
+import os
+import sys
+import click
+
+from playschool_python import __version__
+
+
+def version_msg():
+    """Return the Playschool Python version,
+    location and Python powering it.
+    """
+    python_version = sys.version[:3]
+    location = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    message = u'Playschool Python %(version)s from {} (Python {})'
+    return message.format(location, python_version)
+
+
+@click.command(context_settings=dict(help_option_names=[u'-h', u'--help']))
+@click.version_option(__version__, u'-V', u'--version', message=version_msg())
+def main():
+    """Playschool Python main function
+    """
+    sys.exit(0)
