6279921597e765f582af05ef327c9d35947cab80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:54 +0000

    Add package vdhost/2.0.4/vdhost-2.0.4.tar.gz

---
+++ packages/vdhost/vdhost-2.0.4.tar.gz/vdhost-2.0.4/vdhost/cli/env.py
@@ -0,0 +1,11 @@
+"""
+DO NOT change any of these values.
+"""
+
+
+# the base API url
+BASE_API_URL = 'https://api.vectordash.com'
+
+# the Vectordash installation path
+BASE_INSTALL_PATH = '/var/vectordash'
+
