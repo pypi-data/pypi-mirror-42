248fefa7c660613cb486df9cab04d15806e27867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:33 +0000

    Add package djangorestframework-reactive/4.0.0a2/djangorestframework_reactive-4.0.0a2-py3-none-any.whl

---
+++ packages/djangorestframework-reactive/djangorestframework_reactive-4.0.0a2-py3-none-any.whl/rest_framework_reactive/routing.py
@@ -0,0 +1,32 @@
+from django.conf.urls import url
+
+from channels.routing import ChannelNameRouter, ProtocolTypeRouter, URLRouter
+
+from .consumers import (
+    ClientConsumer,
+    PollObserversConsumer,
+    ThrottleConsumer,
+    WorkerConsumer,
+)
+from .protocol import CHANNEL_POLL_OBSERVER, CHANNEL_THROTTLE, CHANNEL_WORKER_NOTIFY
+
+application = ProtocolTypeRouter(
+    {
+        # Client-facing consumers.
+        'websocket': URLRouter(
+            [
+                # To change the prefix, you can import ClientConsumer in your custom
+                # Channels routing definitions instead of using these defaults.
+                url(r'^ws/(?P<subscriber_id>.+)$', ClientConsumer)
+            ]
+        ),
+        # Background worker consumers.
+        'channel': ChannelNameRouter(
+            {
+                CHANNEL_POLL_OBSERVER: PollObserversConsumer,
+                CHANNEL_THROTTLE: ThrottleConsumer,
+                CHANNEL_WORKER_NOTIFY: WorkerConsumer,
+            }
+        ),
+    }
+)
