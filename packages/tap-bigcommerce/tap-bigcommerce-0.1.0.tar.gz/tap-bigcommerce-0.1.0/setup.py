53a70b5221be67c938d73beaf0c4c30d5d845697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:57 +0000

    Add package tap-bigcommerce/0.1.0/tap-bigcommerce-0.1.0.tar.gz

---
+++ packages/tap-bigcommerce/tap-bigcommerce-0.1.0.tar.gz/tap-bigcommerce-0.1.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="tap-bigcommerce",
+    version="0.1.0",
+    description="Sync data from your BigCommerce Store",
+    author="Chris Goddard",
+    url="https://github.com/chrisgoddard",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["tap_bigcommerce"],
+    install_requires=[
+        "singer-python==5.4.1",
+        "requests==2.21.0",
+        "requests-futures==0.9.9"
+    ],
+    entry_points="""
+    [console_scripts]
+    tap-bigcommerce=tap_bigcommerce:main
+    """,
+    packages=["tap_bigcommerce"],
+    package_data = {
+        "schemas": ["tap_bigcommerce/schemas/*.json"]
+    },
+    include_package_data=True,
+)
