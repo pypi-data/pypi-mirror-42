1024bcfa6978071c0b9055044d1966ac306a0184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:15 +0000

    Add package sprite/0.1.0/sprite-0.1.0.tar.gz

---
+++ packages/sprite/sprite-0.1.0.tar.gz/sprite-0.1.0/setup.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2019 bluelief.
+# This source code is licensed under the MIT license.
+
+from setuptools import setup, find_packages
+
+setup(
+    name="sprite",
+    version="0.1.0",
+    author="bluelief",
+    description="Python project.",
+    license="MIT",
+    keywords="python",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Japanese'
+    ],
+)
+
