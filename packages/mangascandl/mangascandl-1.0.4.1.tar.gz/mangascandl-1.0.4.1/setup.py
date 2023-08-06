ab00c8e07ab55d7618c1425d352e75652df7494e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:15 +0000

    Add package mangascandl/1.0.4.1/mangascandl-1.0.4.1.tar.gz

---
+++ packages/mangascandl/mangascandl-1.0.4.1.tar.gz/mangascandl-1.0.4.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup,find_packages
+
+requirements = [
+    'tqdm',
+    'beautifulsoup4',
+    'requests'
+]
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+setup(
+   name='mangascandl',
+   version='1.0.4.1',
+   description='Tool for downloading mangascan from websites',
+   author='Suartha Gautama',
+   author_email='suartha.gautama@gmail.com',
+   long_description=long_description,
+   long_description_content_type="text/markdown",
+   url="https://github.com/SuarthaGautama/mangascandl",
+   packages=find_packages(),
+   entry_points = {
+       'console_scripts':[
+           'mangascandl = mangascandl.mangascandl:main'
+       ]
+   } ,
+   install_requires=requirements, 
+)
