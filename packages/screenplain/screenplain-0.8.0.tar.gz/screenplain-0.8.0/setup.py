2c6745d3826ad05f2db6e759e3eb32c2048acc17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:22 +0000

    Add package screenplain/0.8.0/screenplain-0.8.0.tar.gz

---
+++ packages/screenplain/screenplain-0.8.0.tar.gz/screenplain-0.8.0/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='screenplain',
+    version='0.8.0',
+    description='Convert text file to viewable screenplay.',
+    author='Martin Vilcans',
+    author_email='screenplain@librador.com',
+    url='http://www.screenplain.com/',
+    project_urls={
+        'Web Page': 'http://www.screenplain.com/',
+        'Source': 'https://github.com/vilcans/screenplain',
+    },
+    license='MIT',
+    install_requires=[
+        'six',
+    ],
+    extras_require={
+        'PDF': 'reportlab'
+    },
+    packages=[
+        'screenplain',
+        'screenplain.export',
+        'screenplain.parsers',
+    ],
+    package_data={
+        'screenplain.export': ['default.css']
+    },
+    scripts=[
+        'bin/screenplain'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
