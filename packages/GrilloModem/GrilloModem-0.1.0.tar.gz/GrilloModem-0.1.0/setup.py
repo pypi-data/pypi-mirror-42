163c3c31021783699189c9b7d853eb743cfc3038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:40 +0000

    Add package GrilloModem/0.1.0/GrilloModem-0.1.0.tar.gz

---
+++ packages/GrilloModem/GrilloModem-0.1.0.tar.gz/GrilloModem-0.1.0/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import os.path
+from setuptools import setup, find_packages
+
+
+# https://packaging.python.org/guides/single-sourcing-package-version/
+version = {}
+with open(os.path.join("grillo", "version.py")) as fp:
+    exec(fp.read(), version)
+
+
+setup(
+    name="GrilloModem",
+    version=version["__version__"],
+    author="Juan Pedro Fisanotti",
+    author_email="fisadev@gmail.com",
+    license="GPLv3",
+    description=("A small tool to easily send data (files, clipboard) between computers with 0 "
+                 "config, just using audio and mic."),
+    long_description=open("README.md").read(),
+    keywords=["audio", "modem", "clipboard", "transfer"],
+    packages=find_packages(exclude=["tests"]),
+    url="https://github.com/fisadev/grillo",
+    entry_points = {
+        'console_scripts': ['grillo=grillo.grillo:main'],
+    },
+    install_requires=[
+        "chirpsdk",
+        "pyperclip",
+        "fire",
+        "termcolor",
+        "traitlets",  # dependency of chirp, but missing in their requirements
+    ],
+)
