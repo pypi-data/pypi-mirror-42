dd46d48e9e1802c207a7edd568d6000a93e2319a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:28 +0000

    Add package termpixels/0.0.2/termpixels-0.0.2-py3-none-any.whl

---
+++ packages/termpixels/termpixels-0.0.2-py3-none-any.whl/termpixels/terminfo.py
@@ -0,0 +1,29 @@
+import curses
+import operator
+import re
+from functools import lru_cache
+
+class Terminfo:
+    def __init__(self):
+        curses.setupterm()
+    
+    @lru_cache(128)
+    def flag(self, name):
+        return curses.tigetflag(name) > 0
+    
+    @lru_cache(128)
+    def num(self, name):
+        return curses.tigetnum(name)
+
+    @lru_cache(128)
+    def string(self, name):
+        return curses.tigetstr(name)
+
+    def parameterize(self, name, *args, require=False):
+        string = self.string(name)
+        if string is None:
+            if not require:
+                return ""
+            else:
+                raise Exception("Terminal does not support required capability: '{}'".format(name))
+        return curses.tparm(string, *args)
