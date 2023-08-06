a7fd05d0660cfbbe79dda3efbb94b4e23250bce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:58 +0000

    Add package readpy/0.0.1.dev1/readpy-0.0.1.dev1.tar.gz

---
+++ packages/readpy/readpy-0.0.1.dev1.tar.gz/readpy-0.0.1.dev1/setup.py
@@ -0,0 +1,22 @@
+
+from setuptools import setup, find_packages
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='readpy',
+    version='0.0.1.dev1',
+    description='A package designed to syntactically mimic the readr R package',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/durrantmm/readpy',
+    author='Matt Durrant',
+    author_email='matthewgeorgedurrant@gmail.com',
+    keywords='readr R syntax',  # Optional
+    packages=find_packages(exclude=['contrib', 'docs', 'tests'])  # Required
+)
