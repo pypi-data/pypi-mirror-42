237bb1b9182cce4dc653b1d4e3dc20d0335c0b35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:42 +0000

    Add package secretpy/0.5.0/secretpy-0.5.0-py2.py3-none-any.whl

---
+++ packages/secretpy/secretpy-0.5.0-py2.py3-none-any.whl/secretpy/ciphers/simplesubstitution.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+class SimpleSubstitution:
+	def __init__(self):
+		return
+
+	def __encDec(self, alphabet, key, text, isEncrypt):
+		if len(alphabet) != len(key): return 
+
+		ans = ""
+		for i in range(len(text)):
+			m = text[i]
+			k = ""
+			if isEncrypt == 1:
+				k = key[alphabet.index(m)]
+			else:
+				k = alphabet[key.index(m)]
+			ans += k
+		return ans
+
+	def encrypt(self, text, key, alphabet=u"abcdefghijklmnopqrstuvwxyz"):
+		return self.__encDec(alphabet, key, text, 1)
+
+	def decrypt(self, text, key, alphabet=u"abcdefghijklmnopqrstuvwxyz"):
+		return self.__encDec(alphabet, key, text, -1)
