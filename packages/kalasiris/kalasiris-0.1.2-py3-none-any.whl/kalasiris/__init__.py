c031a1118ac63b3088dfe3b9004010d3f3ee7807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:49 +0000

    Add package kalasiris/0.1.2/kalasiris-0.1.2-py3-none-any.whl

---
+++ packages/kalasiris/kalasiris-0.1.2-py3-none-any.whl/kalasiris/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for kalasiris."""
+
+__author__ = """Ross A. Beyer"""
+__email__ = 'rbeyer@seti.org'
+__version__ = '0.1.2'
+
+from .kalasiris import *                      # noqa: F401,F403
+from .k_funcs import *                        # noqa: F401,F403
+from .Histogram import Histogram              # noqa: F401
+from .cubenormDialect import cubenormDialect  # noqa: F401
