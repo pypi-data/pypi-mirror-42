85c1a864630ac062ac5d5bc9db8786cb5366df0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:27 +0000

    Add package ui-translator/0.1/ui-translator-0.1.tar.gz

---
+++ packages/ui-translator/ui-translator-0.1.tar.gz/ui-translator-0.1/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+  name = 'ui-translator',
+  packages = ['ui-translator'],
+  version = '0.1',
+  license='MIT',
+  description = 'Python module used for translating Qt .ui files between languages using Google Translate',
+  author = 'Bogdan Caleta Ivkovic',
+  author_email = 'bogdan.caleta@gmail.com',
+  url = 'https://github.com/Raptr3x/qt-ui-translator',
+  download_url = 'https://github.com/Raptr3x/qt-ui-translator/archive/v_01.tar.gz',
+  keywords = ['ui', 'ui translator', 'translate'],
+  install_requires=['ptranslator'],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+  ],
+)
