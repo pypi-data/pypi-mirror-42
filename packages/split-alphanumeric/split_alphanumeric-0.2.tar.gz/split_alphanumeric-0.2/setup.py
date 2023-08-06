afbf7b237f8f8fdc87365ef0586c31cfa575f823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:00 +0000

    Add package split-alphanumeric/0.2/split_alphanumeric-0.2.tar.gz

---
+++ packages/split-alphanumeric/split_alphanumeric-0.2.tar.gz/split_alphanumeric-0.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="split_alphanumeric",
+    version="0.2",
+    author="Hudge",
+    author_email="",
+    description="",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="",
+    py_modules=["split_alphanumeric"],
+    install_requires=["money2float"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
