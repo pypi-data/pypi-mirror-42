946f354af5b6f7f756bd8fa58a98dbbd0173831f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:43 +0000

    Add package pyluach/1.0.1/pyluach-1.0.1.tar.gz

---
+++ packages/pyluach/pyluach-1.0.1.tar.gz/pyluach-1.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import io
+
+setup(name='pyluach',
+      version='1.0.1',
+      author='MS List',
+      author_email='simlist@gmail.com',
+      packages=['pyluach', ],
+      url='https://github.com/simlist/pyluach',
+      license='MIT',
+      description=("""Pyluach is a Python package for manipulating Hebrew dates,
+                    Gregorian-Hebrew calendar conversions, and other Jewish
+                    calendar related calculations."""),
+      long_description=io.open('README.rst').read(),
+      classifiers=['Development Status :: 5 - Production/Stable',
+                     'Intended Audience :: Developers',
+                     'License :: OSI Approved :: MIT License',
+                     'Programming Language :: Python :: 2.7',
+                     'Programming Language :: Python :: 3'],
+      keywords=['hebrew', 'calendar', 'jewish', 'luach', 'gregorian',
+                  'julian', 'days', 'dates', 'date', 'conversion',
+                  'parsha', 'holiday']
+      )
