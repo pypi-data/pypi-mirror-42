95b75c49e32510d8369d9b8fbb64db3d66ccdf3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:28 +0000

    Add package pygrisb/2019.2.24.3/pygrisb-2019.2.24.3.tar.gz

---
+++ packages/pygrisb/pygrisb-2019.2.24.3.tar.gz/pygrisb-2019.2.24.3/pygrisb/basic/prints.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+
+
+def print_2d_array(dm, msg):
+    print(msg)
+    print('real part')
+    for row in dm:
+        print(''.join('%9.4f' % (x.real) for x in row))
+    print('imag part')
+    for row in dm:
+        print(''.join('%9.4f' % (x.imag) for x in row))
