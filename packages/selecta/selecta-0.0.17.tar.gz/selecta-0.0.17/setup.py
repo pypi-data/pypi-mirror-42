475111841e72ac225cec6edd5685152e334cd427
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:16 +0000

    Add package selecta/0.0.17/selecta-0.0.17.tar.gz

---
+++ packages/selecta/selecta-0.0.17.tar.gz/selecta-0.0.17/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='selecta',
+    version='0.0.17',
+    author='Thomas Schüßler',
+    author_email='vindolin@gmail.com',
+    packages=['selecta'],
+    scripts=['bin/selecta', 'bin/selecta_install_bash'],
+    url='https://github.com/vindolin/selecta',
+    license='MIT',
+    description='Interactively select an entry from your bash/zsh history.',
+    long_description=open('README.rst').read(),
+    install_requires=['urwid >= 1.3.1'],
+    include_package_data=True,
+    keywords=['bash', 'zsh', 'curses', 'history'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console :: Curses',
+        'Programming Language :: Python :: 3',
+    ],
+)
