3f92a7731b0179be8a6da1fabaa747a9fdf42b7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:28 +0000

    Add package kashgari/0.1.7/kashgari-0.1.7.tar.gz

---
+++ packages/kashgari/kashgari-0.1.7.tar.gz/kashgari-0.1.7/kashgari/tasks/classification/__init__.py
@@ -0,0 +1,16 @@
+# encoding: utf-8
+"""
+@author: BrikerMan
+@contact: eliyar917@gmail.com
+@blog: https://eliyar.biz
+
+@version: 1.0
+@license: Apache Licence
+@file: __init__.py.py
+@time: 2019-01-19 11:49
+
+"""
+from .base_model import ClassificationModel
+from .blstm_model import BLSTMModel
+from .cnn_lstm_model import CNNLSTMModel
+from .cnn_model import CNNModel
