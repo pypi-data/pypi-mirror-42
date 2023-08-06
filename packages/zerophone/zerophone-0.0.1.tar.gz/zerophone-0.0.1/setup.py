eadd7456b3b5864aa3fe22c5b7582b0366c45d89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:31 +0000

    Add package zerophone/0.0.1/zerophone-0.0.1.tar.gz

---
+++ packages/zerophone/zerophone-0.0.1.tar.gz/zerophone-0.0.1/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+version = "0.0.1"
+
+try:
+    import fastentrypoints
+except ImportError:
+    from setuptools.command import easy_install
+    import pkg_resources
+    easy_install.main(['fastentrypoints'])
+    pkg_resources.require('fastentrypoints')
+    import fastentrypoint
+
+import sys
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.version_info[0] == 2:
+    jsonrpclib = "jsonrpclib"
+elif sys.version_info[0] == 3:
+    jsonrpclib = "jsonrpclib-pelix"
+
+setup(
+    name='zerophone',
+    py_modules=['zerophone'],
+    version=version,
+    description='ZeroPhone API library, with CLI',
+    author='CRImier',
+    author_email='crimier@yandex.ru',
+    install_requires=[
+        jsonrpclib,
+        "zerophone_api_daemon"
+    ],
+    url = 'https://github.com/ZeroPhone/Zerophone-API-Python',
+    download_url = 'https://github.com/ZeroPhone/zerophone-api-py/archive/{}.tar.gz'.format(version),
+    entry_points={"console_scripts": ["zp = zerophone:main"]},
+)
