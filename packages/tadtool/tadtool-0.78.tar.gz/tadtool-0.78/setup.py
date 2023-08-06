4c571d7efee57b93b817ba9de019473780a874ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:57 +0000

    Add package tadtool/0.78/tadtool-0.78.tar.gz

---
+++ packages/tadtool/tadtool-0.78.tar.gz/tadtool-0.78/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tadtool',
+    version='0.78',
+    description='Assistant to find cutoffs in TAD calling algorithms.',
+    packages=find_packages(exclude=["test"]),
+    install_requires=[
+        'numpy>=1.9.0',
+        'matplotlib',
+        'progressbar2',
+        'future'
+    ],
+    author='Vaquerizas lab',
+    author_email='kai.kruse@mpi-muenster.mpg.de',
+    url='https://github.com/vaquerizaslab/tadtool',
+    download_url='https://github.com/vaquerizaslab/tadtool/tarball/0.77',
+    keywords=['bioinformatics', 'hi-c', 'genomics', 'tad'],
+    classifiers=[],
+    scripts=['bin/tadtool']
+)
