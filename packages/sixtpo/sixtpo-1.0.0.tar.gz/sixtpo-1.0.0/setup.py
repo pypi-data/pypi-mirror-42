13090daee0435b9ebd8febe3f099ba33c612e040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:53 +0000

    Add package sixtpo/1.0.0/sixtpo-1.0.0.tar.gz

---
+++ packages/sixtpo/sixtpo-1.0.0.tar.gz/sixtpo-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='sixtpo',
+    version='1.0.0',
+    py_modules=['sixtpo'],
+    author='hfp'
+    #author_email='hfpython@headfirstlabs.com',
+    #url='http://www.headfirstlapbs.com',
+    #description='a simple printer of nested lists',
+    )
