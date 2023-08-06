12a726f96f1441462d34e79355546f61c80a0a0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:15 +0000

    Add package detools/0.6.0/detools-0.6.0.tar.gz

---
+++ packages/detools/detools-0.6.0.tar.gz/detools-0.6.0/detools/create_patch.py
@@ -0,0 +1,53 @@
+import os
+import struct
+from lzma import LZMACompressor
+
+try:
+    from . import csais as sais
+    from . import cbsdiff as bsdiff
+except ImportError:
+    print('detools: Failed to import C extensions. Using Python fallback.')
+    from . import sais
+    from . import bsdiff as bsdiff
+
+
+def _get_fsize(f):
+    f.seek(0, os.SEEK_END)
+
+    return f.tell()
+
+
+def fread(f):
+    f.seek(0, os.SEEK_SET)
+
+    return f.read()
+
+
+def _pack_i64(value):
+    return struct.pack('>q', value)
+
+
+def _write_header(fpatch, fto):
+    fpatch.write(b'detools0')
+    fpatch.write(_pack_i64(_get_fsize(fto)))
+
+
+def _write_data(ffrom, fto, fpatch):
+    from_data = fread(ffrom)
+    suffix_array = sais.sais(from_data)
+    chunks = bsdiff.create_patch(suffix_array, from_data, fread(fto))
+    compressor = LZMACompressor()
+
+    for chunk in chunks:
+        fpatch.write(compressor.compress(chunk))
+
+    fpatch.write(compressor.flush())
+
+
+def create_patch(ffrom, fto, fpatch):
+    """Create a patch from `ffrom` to `fto` and write it to `fpatch`.
+
+    """
+
+    _write_header(fpatch, fto)
+    _write_data(ffrom, fto, fpatch)
