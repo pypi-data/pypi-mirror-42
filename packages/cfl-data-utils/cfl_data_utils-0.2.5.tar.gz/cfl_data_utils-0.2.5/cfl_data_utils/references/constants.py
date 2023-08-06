88ed2a0a430ea6882d2072e091eff37e815a0c42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:05 +0000

    Add package cfl-data-utils/0.2.5/cfl_data_utils-0.2.5.tar.gz

---
+++ packages/cfl-data-utils/cfl_data_utils-0.2.5.tar.gz/cfl_data_utils-0.2.5/cfl_data_utils/references/constants.py
@@ -0,0 +1,23 @@
+from ._type_tests import datetime, datetime_type_test, float_type_test, int_type_test
+
+RESTART_OPTION = '[Restart]'
+Q = 'question'
+EXIT_OPTION = '[Exit]'
+NEW_MONTH_FLAG = datetime.today().day == 1
+
+TYPE_TESTS = [
+    (int, int_type_test),
+    (float, float_type_test),
+    (datetime, datetime_type_test)
+]
+
+SQL_TYPE_DICT = {
+    int: 'INT',
+    float: 'DOUBLE',
+    datetime: 'DATE',
+    str: 'TEXT'
+}
+
+WINDOWS = 'Windows'
+LINUX = 'Linux'
+MAC_OS = 'Darwin'
