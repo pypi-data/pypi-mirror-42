e4ceb4ea81fd03135b68a8c26a77bf99b1631d01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:11 +0000

    Add package django-ubuntu-deployer/4.1.0/django_ubuntu_deployer-4.1.0.tar.gz

---
+++ packages/django-ubuntu-deployer/django_ubuntu_deployer-4.1.0.tar.gz/django_ubuntu_deployer-4.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='django_ubuntu_deployer',
+      version='4.1.0',
+      description='Simple way to deploy Django app in Ubuntu Server.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/Maksych/django_ubuntu_deployer',
+      author='Maksym Sichkaruk',
+      author_email='maxim.18.08.1997@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False)
