383d8abba7c750b676a1dc6fc97cc257ede4fdb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:58 +0000

    Add package hd-llz/0.0.0.2/hd_llz-0.0.0.2.tar.gz

---
+++ packages/hd-llz/hd_llz-0.0.0.2.tar.gz/hd_llz-0.0.0.2/setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+from setuptools import setup
+from setuptools import find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="hd_llz",
+    version="0.0.0.2",
+    description="This is an installation package that detects the hand of the video.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    packages = find_packages(), 
+    include_package_data = True,    # include everything in source control
+    #packages = ['hd_llz', 'hd_llz/models', 'hd_llz/data'], 
+    package_data = {
+        'hd_llz':['models/v2_our6_rfcnmv_anchor12_320_500000', 'models/v2_our6_rfcnmv_anchor9_180_369818', 'data/test.png', 'data/hand_label_map.pbtxt']},
+
+ 
+    license = "MIT",
+    author="llz",
+    author_email="hero1153@sina.com",
+    #url='<null>',
+    platforms = 'any',
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
