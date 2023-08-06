5c8fc227ae75eeeabe74173434510a6f874cc98b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:26 +0000

    Add package nirmapper/1.1.6/nirmapper-1.1.6.tar.gz

---
+++ packages/nirmapper/nirmapper-1.1.6.tar.gz/nirmapper-1.1.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nirmapper',
+    version='1.1.6',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    package_data={'': ['license.txt']},
+    url='https://github.com/fechbmaster/3DNIRmapper',
+    license='Apache-2.0',
+    author='fechbmaster',
+    author_email='bernd.fecht1@hs-augsburg.de',
+    description='A 3D Mapper to map NIR images to a 3d tooth model.',
+    dependency_links=['https://github.com/pywavefront/PyWavefront', 'https://github.com/pycollada/pycollada'],
+    python_requires='>2.7',
+    install_requires=['numpy', 'Click', 'PyWavefront', 'pycollada'],
+    scripts=['bin/nirmapper']
+)
