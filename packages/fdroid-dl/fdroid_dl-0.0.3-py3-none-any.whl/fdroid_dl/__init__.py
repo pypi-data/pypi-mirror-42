c8fcaf19ab348c1be978fafe2607ba26382e158a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:46 +0000

    Add package fdroid-dl/0.0.3/fdroid_dl-0.0.3-py3-none-any.whl

---
+++ packages/fdroid-dl/fdroid_dl-0.0.3-py3-none-any.whl/fdroid_dl/__init__.py
@@ -0,0 +1,7 @@
+name = 'fdroid-dl'
+version = '0.0.3'
+author = 't4skforce'
+author_mail = '7422037+t4skforce@users.noreply.github.com'
+__version__ = version
+
+__all__ = ['name','version','author','author_mail','__version__']
