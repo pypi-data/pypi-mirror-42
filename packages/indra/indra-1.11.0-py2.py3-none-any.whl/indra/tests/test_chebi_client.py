0a613e74ffd7af030f90259cf8dd53356f1cb447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:36 +0000

    Add package indra/1.11.0/indra-1.11.0-py2.py3-none-any.whl

---
+++ packages/indra/indra-1.11.0-py2.py3-none-any.whl/indra/tests/test_chebi_client.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import, print_function, unicode_literals
+from builtins import dict, str
+from indra.databases import chebi_client
+from indra.util import unicode_strs
+from nose.plugins.attrib import attr
+
+
+def test_read_chebi_to_pubchem():
+    (ctop, ptoc) = chebi_client._read_chebi_to_pubchem()
+    assert ctop['85673'] == '11652416'
+    assert ptoc['11652416'] == '85673'
+    assert unicode_strs((ctop, ptoc))
+
+
+def test_read_chebi_to_chembl():
+    ctoc = chebi_client._read_chebi_to_chembl()
+    assert ctoc['50729'] == 'CHEMBL58'
+    assert unicode_strs(ctoc)
+
+
+def test_cas_to_chebi():
+    assert chebi_client.get_chebi_id_from_cas('23261-20-3') == '18035'
+    assert chebi_client.get_chebi_id_from_cas('100-51-6') == '17987'
+    assert chebi_client.get_chebi_id_from_cas('-1') is None
