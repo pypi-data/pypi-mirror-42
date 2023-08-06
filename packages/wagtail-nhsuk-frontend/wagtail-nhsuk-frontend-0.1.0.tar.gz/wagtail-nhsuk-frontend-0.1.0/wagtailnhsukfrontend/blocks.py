0ef276907d47c2ba901163fe0eea4ec40107cfa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:50 +0000

    Add package wagtail-nhsuk-frontend/0.1.0/wagtail-nhsuk-frontend-0.1.0.tar.gz

---
+++ packages/wagtail-nhsuk-frontend/wagtail-nhsuk-frontend-0.1.0.tar.gz/wagtail-nhsuk-frontend-0.1.0/wagtailnhsukfrontend/blocks.py
@@ -0,0 +1,45 @@
+from wagtail.core.blocks import (
+    CharBlock,
+    ChoiceBlock,
+    RichTextBlock,
+    StructBlock,
+    URLBlock,
+)
+
+
+class ActionLinkBlock(StructBlock):
+
+    text = CharBlock(label="link text", required=True)
+    external_url = URLBlock(label="external URL", required=True)
+
+    class Meta:
+        template = 'wagtailnhsukfrontend/action_link.html'
+
+
+class CareCardBlock(StructBlock):
+
+    type = ChoiceBlock([
+        ('primary', 'Primary'),
+        ('urgent', 'Urgent'),
+        ('immediate', 'Immediate'),
+    ], required=True)
+    title = CharBlock(required=True)
+    body = RichTextBlock(required=True)
+
+    def get_context(self, value, parent_context=None):
+        context = super().get_context(value, parent_context)
+        context['accessible_title_prefix'] = {
+            'primary': 'Non-urgent advice: ',
+            'urgent': 'Urgent advice:',
+            'immediate': 'Immediate action required:',
+        }[value['type']]
+        return context
+
+    class Meta:
+        template = 'wagtailnhsukfrontend/care_card.html'
+
+
+class WarningCalloutBlock(RichTextBlock):
+
+    class Meta:
+        template = 'wagtailnhsukfrontend/warning_callout.html'
---
+++ packages/wagtail-nhsuk-frontend/wagtail-nhsuk-frontend-0.1.0.tar.gz/wagtail-nhsuk-frontend-0.1.0/wagtailnhsukfrontend/settings/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class SettingsAppConfig(AppConfig):
+    name = 'wagtailnhsukfrontend.settings'
+    label = 'wagtailnhsukfrontendsettings'
+    verbose_name = "Wagtail NHS Style Settings"
