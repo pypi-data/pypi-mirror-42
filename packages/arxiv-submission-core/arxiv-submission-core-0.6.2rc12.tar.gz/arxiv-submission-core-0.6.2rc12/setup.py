b309f4599927346b3b98b47c4d1ff4f5009c1e4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:51 +0000

    Add package arxiv-submission-core/0.6.2rc12/arxiv-submission-core-0.6.2rc12.tar.gz

---
+++ packages/arxiv-submission-core/arxiv-submission-core-0.6.2rc12.tar.gz/arxiv-submission-core-0.6.2rc12/setup.py
@@ -0,0 +1,28 @@
+"""Install arXiv submission core package."""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='arxiv-submission-core',
+    version='0.6.2rc12',
+    packages=[f'arxiv.{package}' for package
+              in find_packages('arxiv')],
+    zip_safe=False,
+    install_requires=[
+        'arxiv-base',
+        'arxiv-auth',
+        'flask',
+        'mysqlclient',
+        'bleach',
+        'unidecode',
+        'python-dateutil',
+        'sqlalchemy',
+        'dataclasses',
+        'celery==4.1.0',
+        'kombu==4.1.0',
+        'redis==2.10.6',
+        'mypy_extensions==0.4.1',
+        'requests==2.21.0',
+        'semver==2.8.1'
+    ]
+)
