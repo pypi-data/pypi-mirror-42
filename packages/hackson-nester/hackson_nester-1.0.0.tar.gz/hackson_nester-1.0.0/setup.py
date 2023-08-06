05428748c0fa0d5a4e03c652a6b28255e39ff83e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:19 +0000

    Add package hackson-nester/1.0.0/hackson_nester-1.0.0.tar.gz

---
+++ packages/hackson-nester/hackson_nester-1.0.0.tar.gz/hackson_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+    name          ='hackson_nester',
+    version       ='1.0.0',
+    py_modules    =['nester'],
+    author        ='大将军王',
+    author_email  ='hackson99@qq.com',
+    url = 'https://www.baidd.com',
+    license = 'MIT License',
+    zip_safe = False,
+    )
