fbfc51af66c659af4034da320847ee5b05b290c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:46 +0000

    Add package cryptotik/0.35.7/cryptotik-0.35.7.tar.gz

---
+++ packages/cryptotik/cryptotik-0.35.7.tar.gz/cryptotik-0.35.7/cryptotik/__init__.py
@@ -0,0 +1,12 @@
+
+from cryptotik.poloniex import Poloniex, PoloniexNormalized
+from cryptotik.bittrex import Bittrex, BittrexNormalized
+from cryptotik.wex import Wex, WexNormalized
+from cryptotik.therock import TheRock, TheRockNormalized
+from cryptotik.hitbtc import Hitbtc, HitbtcNormalized
+from cryptotik.bitstamp import Bitstamp, BitstampNormalized
+from cryptotik.binance import Binance, BinanceNormalized
+from cryptotik.bitkonan import Bitkonan
+from cryptotik.kraken import Kraken, KrakenNormalized
+from cryptotik.cryptopia import Cryptopia, CryptopiaNormalized
+from cryptotik.coinmarketcap import CoinMarketCap
