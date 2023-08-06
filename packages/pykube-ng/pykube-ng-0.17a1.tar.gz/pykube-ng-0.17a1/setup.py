d076fc45881b237040fafe9b242bae40402bd3b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:06 +0000

    Add package pykube-ng/0.17a1/pykube-ng-0.17a1.tar.gz

---
+++ packages/pykube-ng/pykube-ng-0.17a1.tar.gz/pykube-ng-0.17a1/setup.py
@@ -0,0 +1,52 @@
+import sys
+
+from setuptools import setup, find_packages
+
+
+with open("README.rst") as fp:
+    long_description = fp.read()
+
+install_requires = [
+    "requests>=2.12",
+    "PyYAML",
+    "six>=1.10.0",
+    "tzlocal",
+]
+
+if sys.version_info < (3,):
+    install_requires.extend([
+        "ipaddress",
+    ])
+
+setup(
+    name="pykube-ng",
+    version="0.17a1",
+    description="Python client library for Kubernetes",
+    long_description=long_description,
+    author="Eldarion, Inc.",
+    author_email="development@eldarion.com",
+    license="Apache",
+    url="https://github.com/hjacobs/pykube",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+    zip_safe=False,
+    packages=find_packages(),
+    entry_points={
+        "httpie.plugins.transport.v1": [
+            "httpie_pykube = pykube.contrib.httpie_plugin:PyKubeTransportPlugin"
+        ],
+    },
+    install_requires=install_requires,
+    extras_require={
+        "gcp": [
+            "google-auth",
+            "jsonpath-ng",
+        ]
+    },
+)
