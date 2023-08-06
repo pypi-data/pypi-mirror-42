fd6b61107b8e4e160fdb950ccf85018252eb0111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:15 +0000

    Add package django-brfied/0.6.1/django_brfied-0.6.1.tar.gz

---
+++ packages/django-brfied/django_brfied-0.6.1.tar.gz/django_brfied-0.6.1/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+setup(
+    name='django_brfied',
+    description='Django Application specific brazilian fields types',
+    long_description='Utils classes for EGE project',
+    license='MIT',
+    author='Kelson da Costa Medeiros',
+    author_email='kelsoncm@gmail.com',
+    packages=['django_brfied', 'django_brfied/migrations', 'django_brfied/management/commands', 'django_brfied/static', ],
+    package_data = {'static': ['*'], },
+#    package_dir={'django_brfied': 'django_brfied'},
+#    packages=['ege_theme', 'ege_theme/migrations', 'ege_theme/static', 'ege_theme/templates', 'ege_theme/templatetags'],
+    include_package_data=True,
+    version='0.6.1',
+    download_url='https://github.com/kelsoncm/django_brfied/releases/tag/0.6.1',
+    url='https://github.com/kelsoncm/django_brfied',
+    keywords=['django', 'BR', 'Brazil', 'Brasil', 'model', 'form', 'locale', ],
+    classifiers=[]
+)
+
