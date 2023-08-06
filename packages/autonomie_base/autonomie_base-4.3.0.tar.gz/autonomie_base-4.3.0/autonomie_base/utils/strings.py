475a2bec9f71b232f435d71240f5c1c50af85fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:27 +0000

    Add package autonomie_base/4.3.0/autonomie_base-4.3.0.tar.gz

---
+++ packages/autonomie_base/autonomie_base-4.3.0.tar.gz/autonomie_base-4.3.0/autonomie_base/utils/strings.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# * Authors:
+#       * TJEBBES Gaston <g.t@majerti.fr>
+#       * Arezki Feth <f.a@majerti.fr>;
+#       * Miotte Julien <j.m@majerti.fr>;
+import locale
+
+
+def format_quantity(quantity):
+    """
+        format the quantity
+    """
+    if quantity is not None:
+        result = locale.format('%g', quantity, grouping=True)
+        if isinstance(result, str):
+            result = result.decode('utf-8')
+        return result
+    else:
+        return ""
