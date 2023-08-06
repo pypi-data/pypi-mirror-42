21ffdb9c4a71b9348e0c585055d4e4535c006129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:39 +0000

    Add package rough-graph-mapper/0.0.1/rough_graph_mapper-0.0.1.tar.gz

---
+++ packages/rough-graph-mapper/rough_graph_mapper-0.0.1.tar.gz/rough_graph_mapper-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='rough_graph_mapper',
+      version='0.0.1',
+      description='Rough Graph Mapper',
+      url='http://github.com/uio-bmi/rough_graph_mapper',
+      author='Ivar Grytten and Knut Rand',
+      author_email='',
+      license='MIT',
+      zip_safe=False,
+      install_requires=['numpy', 'pygssw==0.0.6', 'python-coveralls', 'pysam',
+                        'pyfaidx', 'offsetbasedgraph', 'pyvg', 'graph_peak_caller', 'tqdm'],
+      classifiers=[
+            'Programming Language :: Python :: 3'
+      ],
+      entry_points = {
+        'console_scripts': ['rough_graph_mapper=rough_graph_mapper.command_line_interface:main'],
+      })
