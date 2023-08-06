492e17d33f7b8b595da12d47b85ced963ad9cdb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:21 +0000

    Add package okra/0.5.dev1/okra-0.5.dev1.tar.gz

---
+++ packages/okra/okra-0.5.dev1.tar.gz/okra-0.5.dev1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='okra',
+    version='0.5dev1',
+    packages=["okra", "okra/protobuf"],
+    license='MIT',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'protobuf==3.6.1',
+    ],
+    scripts=['bin/okra'],
+    setup_requires=['pytest-runner'],
+    test_requires=[
+        'pytest',
+    ],
+    test_suite='pytest',
+    zip_safe=False
+)
