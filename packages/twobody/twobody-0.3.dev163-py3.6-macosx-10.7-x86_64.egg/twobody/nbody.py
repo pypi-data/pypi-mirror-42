0d75a78ec11393da7eb6b12f13ac3a6341166176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:01 +0000

    Add package twobody/0.3.dev163/twobody-0.3.dev163-py3.6-macosx-10.7-x86_64.egg

---
+++ packages/twobody/twobody-0.3.dev163-py3.6-macosx-10.7-x86_64.egg/twobody/nbody.py
@@ -0,0 +1,16 @@
+"""
+Despite the name of this module, this is for dealing with uninteracting /
+hierarchical N-body systems, not doing N-body integrations.
+"""
+
+
+class HierarchicalTriple:
+
+    def __init__(self, inner_elements, outer_elements, barycenter=None):
+        """
+        TODO:
+        """
+        pass
+
+    def get_orbit(self, num):
+        pass
