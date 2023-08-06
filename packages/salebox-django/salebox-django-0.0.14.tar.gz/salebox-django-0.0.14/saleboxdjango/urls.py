34cfa3551be227c54d22bf58abe5e15af04d2475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:29 +0000

    Add package salebox-django/0.0.14/salebox-django-0.0.14.tar.gz

---
+++ packages/salebox-django/salebox-django-0.0.14.tar.gz/salebox-django-0.0.14/saleboxdjango/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from saleboxdjango.views import *
+
+urlpatterns = [
+    path('delivery-address-list/', addresslist_ajax_view),
+    path('basket/', basket_ajax_view),
+    path('rating/', rating_ajax_view),
+    path('switch-basket-wishlist/', switch_basket_wishlist_ajax_view),
+    path('wishlist/', wishlist_ajax_view),
+]
