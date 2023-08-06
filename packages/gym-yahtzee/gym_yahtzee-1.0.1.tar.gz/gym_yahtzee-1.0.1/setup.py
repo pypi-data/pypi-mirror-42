e382c66f07bbf15a3c2de4508e3c33e7f038f5ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:06 +0000

    Add package gym-yahtzee/1.0.1/gym_yahtzee-1.0.1.tar.gz

---
+++ packages/gym-yahtzee/gym_yahtzee-1.0.1.tar.gz/gym_yahtzee-1.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='gym_yahtzee',
+    version='1.0.1',
+    description='Yahtzee game engine for OpenAI Gym',
+    author='Ville Brofeldt',
+    author_email='ville.brofeldt@iki.fi',
+    maintainer='Ville Brofeldt',
+    maintainer_email='ville.brofeldt@iki.fi',
+    url='https://github.com/villebro/gym-yahtzee',
+    license='MIT',
+    packages=find_packages(exclude='tests'),
+    install_requires=[
+        'gym',
+        'pyhtzee',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+    ],
+)
