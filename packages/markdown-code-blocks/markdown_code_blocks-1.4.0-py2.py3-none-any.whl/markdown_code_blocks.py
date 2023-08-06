034e475cb812506970ff021e3fde70db071dab5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:24 +0000

    Add package markdown-code-blocks/1.4.0/markdown_code_blocks-1.4.0-py2.py3-none-any.whl

---
+++ packages/markdown-code-blocks/markdown_code_blocks-1.4.0-py2.py3-none-any.whl/markdown_code_blocks.py
@@ -0,0 +1,40 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import argparse
+import io
+
+import mistune
+import pygments.formatters
+import pygments.lexers
+import pygments.util
+
+
+class CodeRenderer(mistune.Renderer):
+    def block_code(self, code, lang):
+        try:
+            lexer = pygments.lexers.get_lexer_by_name(lang, stripnl=False)
+            cssclass = 'highlight {}'.format(lang)
+        except pygments.util.ClassNotFound:
+            lexer = pygments.lexers.get_lexer_by_name('text', stripnl=False)
+            cssclass = 'highlight'
+        formatter = pygments.formatters.HtmlFormatter(cssclass=cssclass)
+        return pygments.highlight(code, lexer=lexer, formatter=formatter)
+
+
+def highlight(doc, Renderer=CodeRenderer):
+    return mistune.Markdown(Renderer(escape=True, hard_wrap=False))(doc)
+
+
+def main(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', default='/dev/stdin')
+    args = parser.parse_args(argv)
+
+    with io.open(args.filename) as f:
+        hl = highlight(f.read())
+        print('<!doctype html><html><body>{}</body></html>'.format(hl))
+
+
+if __name__ == '__main__':
+    exit(main())
