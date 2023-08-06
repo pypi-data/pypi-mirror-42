98e86899492359e16c2741e27cc86fc966519abb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:48 +0000

    Add package m2cgen/0.1.1/m2cgen-0.1.1.tar.gz

---
+++ packages/m2cgen/m2cgen-0.1.1.tar.gz/m2cgen-0.1.1/m2cgen/interpreters/__init__.py
@@ -0,0 +1,10 @@
+from .java.interpreter import JavaInterpreter
+from .python.interpreter import PythonInterpreter
+from .c.interpreter import CInterpreter
+
+
+__all__ = [
+    JavaInterpreter,
+    PythonInterpreter,
+    CInterpreter,
+]
