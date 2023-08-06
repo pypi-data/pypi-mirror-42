47dc4f0c0ae237aa250556b8f95f87fe4e9bd9c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:06 +0000

    Add package hwtypes/1.0.1/hwtypes-1.0.1.tar.gz

---
+++ packages/hwtypes/hwtypes-1.0.1.tar.gz/hwtypes-1.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+# from pip.req import parse_requirements
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+
+# install_requires = []
+# extra_requires = {}
+# for item in parse_requirements("requirements.txt", session=False):
+#     req = str(item.req)
+#     if item.markers is not None:
+#         req += ";" + str(item.markers)
+#     install_requires.append(req)
+
+setup(
+    name='hwtypes',
+    url='https://github.com/leonardt/hwtypes',
+    author='Leonard Truong',
+    author_email='lenny@cs.stanford.edu',
+    version='1.0.1',
+    description='Python implementations of fixed size hardware types (Bit, '
+                'BitVector, UInt, SInt, ...)',
+    scripts=[],
+    packages=[
+        "hwtypes",
+    ],
+    install_requires=['numpy'],
+    # python_requires='>=3.6'
+)
