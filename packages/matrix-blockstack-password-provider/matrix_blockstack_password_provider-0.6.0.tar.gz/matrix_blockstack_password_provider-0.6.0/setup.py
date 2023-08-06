2afca8f64f2d2fc020f3cc669f0f638b867d6c76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:56 +0000

    Add package matrix-blockstack-password-provider/0.6.0/matrix_blockstack_password_provider-0.6.0.tar.gz

---
+++ packages/matrix-blockstack-password-provider/matrix_blockstack_password_provider-0.6.0.tar.gz/matrix_blockstack_password_provider-0.6.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="matrix_blockstack_password_provider",
+    version="0.6.0",
+    author="OpenIntents",
+    author_email="support@openintents.org",
+    description="Password provider for blockstack/EOS synapse server",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/friedger/matrix_blockstack_password_provider",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["requests", "blockstack_zones", "twisted"],
+)
