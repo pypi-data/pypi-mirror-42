00bbe9ce58dbe3c552ae194e3c653c133ebc9729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:05 +0000

    Add package gridfinder/1.0.0/gridfinder-1.0.0.tar.gz

---
+++ packages/gridfinder/gridfinder-1.0.0.tar.gz/gridfinder-1.0.0/gridfinder/__init__.py
@@ -0,0 +1,12 @@
+#!python3
+# __init__.py
+
+"""
+gridfinder package contains the following modules:
+ - gridfinder.py : main implementation of Djikstra's algorithm
+ - prepare.py : transforming input data into the cost and targets arrays
+ - post.py : postprocess the algorithm output and check accuracy
+ - _util.py : helper functions used internally
+"""
+
+__version__ = "1.0.0"
