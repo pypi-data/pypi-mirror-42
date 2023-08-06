532f1626543e68043abf2f1aa997fb316fb2da5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:45 +0000

    Add package tosheets/0.4.0/tosheets-0.4.0.tar.gz

---
+++ packages/tosheets/tosheets-0.4.0.tar.gz/tosheets-0.4.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='tosheets',
+    version='0.4.0',
+    author='Timotej Kapus',
+    author_email='kren1@users.noreply.github.com',
+    description=('Cmd utility that send stdin to google sheets'),
+    long_description=open('README.md').read(),
+    license='MIT',
+    keywords='utility sheets command line',
+    url='https://github.com/kren1/tosheets',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3'
+    ],
+    install_requires=[
+        'docopt',
+        'oauth2client',
+        'google-api-python-client'
+    ],
+    packages=find_packages(),
+    package_data={'tosheets': ['client.json'] },
+    entry_points={
+        'console_scripts': [
+            'tosheets = tosheets.tosheets:main'
+        ],
+    },
+
+)
