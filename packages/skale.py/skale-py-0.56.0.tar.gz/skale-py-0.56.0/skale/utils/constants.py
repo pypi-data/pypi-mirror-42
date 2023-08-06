71be85fb4fa4f87f86e9b32391bad858c12ef564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:31 +0000

    Add package skale.py/0.56.0/skale-py-0.56.0.tar.gz

---
+++ packages/skale.py/skale-py-0.56.0.tar.gz/skale-py-0.56.0/skale/utils/constants.py
@@ -0,0 +1,17 @@
+NODE_DEPOSIT = 100000000000000000000
+
+GAS = {
+    'create_node': 4500000,
+    'get_bounty': 2000000,
+    'send_verdict': 200000,
+
+    'token_transfer': 60000,
+    'add_authorized': 100000
+}
+
+OP_TYPES = {
+    'create_node': 0x1,
+    'create_schain': 0x10
+}
+
+LONG_LINE = '=' * 100
