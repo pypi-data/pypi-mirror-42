424c54eae4479a8487b65318c531046bd32bdf02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:27 +0000

    Add package hashget/0.108/hashget-0.108.tar.gz

---
+++ packages/hashget/hashget-0.108.tar.gz/hashget-0.108/hashget/__init__.py
@@ -0,0 +1,18 @@
+import datetime
+
+#from .cacheget import CacheGet
+# from .hashutils import walk_arc, unpack_deb
+#from .debian import DebPackage, load_release
+#from .hashdb import HashDB, DirHashDB, HashPackage, HashDBClient
+#from .submiturl import submit_url
+#from .file import File, FileList
+import os
+import requests
+#import cachecontrol
+from .anchor import AnchorList
+
+from . import cacheget
+
+__all__ = ["utils", "cacheget", "package", "restorefile", "hashdb", "hashpackage", "file", "submiturl"]
+
+cacheget.user_agent = 'HashGet/0.106'
