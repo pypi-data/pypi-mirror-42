5dffa2afd36fb13fd74fe73f135d402cd0b82bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:53 +0000

    Add package macdaily/2019.3.1/macdaily-2019.3.1-cp37-none-macosx_10_14_x86_64.whl

---
+++ packages/macdaily/macdaily-2019.3.1-cp37-none-macosx_10_14_x86_64.whl/macdaily/util/compat.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+# pathlib & subprocess
+if sys.version_info[:2] <= (3, 4):
+    import pathlib2 as pathlib
+    import subprocess32 as subprocess
+else:
+    import pathlib
+    import subprocess
+
+# threading
+try:
+    import threading
+except ImportError:
+    import dummy_threading as threading
+
+# multiprocessing
+try:
+    import multiprocessing
+except ImportError:
+    multiprocessing = threading
+
+    # make alias for multiprocessing.Process
+    setattr(multiprocessing, 'Process', threading.Thread)
+
+    # make alias for multiprocessing.Queue
+    import queue
+    setattr(multiprocessing, 'Queue', queue.Queue)
