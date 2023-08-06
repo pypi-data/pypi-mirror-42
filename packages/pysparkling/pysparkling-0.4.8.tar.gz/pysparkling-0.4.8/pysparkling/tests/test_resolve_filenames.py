bcb5e9d0d0e81b401130853b47e32037eb961a01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:50 +0000

    Add package pysparkling/0.4.8/pysparkling-0.4.8.tar.gz

---
+++ packages/pysparkling/pysparkling-0.4.8.tar.gz/pysparkling-0.4.8/pysparkling/tests/test_resolve_filenames.py
@@ -0,0 +1,39 @@
+from __future__ import print_function
+
+import os
+
+import pytest
+
+from pysparkling.fileio import File
+
+
+CURRENT_FILE_LOCATION = __file__
+
+
+def test_local_1():
+    filenames = File.resolve_filenames(
+        '{}/*'.format(os.path.dirname(CURRENT_FILE_LOCATION))
+    )
+    assert CURRENT_FILE_LOCATION in filenames
+
+
+def test_local_2():
+    filenames = File.resolve_filenames(CURRENT_FILE_LOCATION)
+    assert filenames == [CURRENT_FILE_LOCATION]
+
+
+@pytest.mark.skipif(not os.getenv('AWS_ACCESS_KEY_ID'), reason='no AWS env')
+def test_s3_1():
+    filenames = File.resolve_filenames(
+        's3n://aws-publicdatasets/common-crawl/'
+        'crawl-data/CC-MAIN-2015-11/warc.paths.*'
+    )
+    print(filenames)
+    assert ('s3n://aws-publicdatasets/common-crawl/'
+            'crawl-data/CC-MAIN-2015-11/warc.paths.gz' in filenames)
+
+
+if __name__ == '__main__':
+    test_local_1()
+    test_local_2()
+    test_s3_1()
