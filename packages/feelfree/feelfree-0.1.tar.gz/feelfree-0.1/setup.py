7790eab4aa3cfce7ce79291bd8e01c29f5b80865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:21 +0000

    Add package feelfree/0.1/feelfree-0.1.tar.gz

---
+++ packages/feelfree/feelfree-0.1.tar.gz/feelfree-0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+	
+setup(name='feelfree',
+      version='0.1',
+      description='The free version of FEEL package',
+	  long_description=long_description,
+	  long_description_content_type="text/markdown",
+      url='https://github.com/fangj99/feelfree',
+      author='Lau & Fang',
+      author_email='fangj99@gmail.com',
+      license='GNU',
+      packages=['feelfree'],
+      zip_safe=False,
+	  #packages=setuptools.find_packages(),
+	  classifiers=[
+	      "Programming Language :: Python :: 3",
+		  "Operating System :: OS Independent",
+	  ],
+)
