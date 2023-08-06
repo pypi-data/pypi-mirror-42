2208326054f3e62737e04f59a83c3fd9a5d1e140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:47 +0000

    Add package dox-data-docnews-digest/0.0.1/dox-data_docnews_digest-0.0.1.tar.gz

---
+++ packages/dox-data-docnews-digest/dox-data_docnews_digest-0.0.1.tar.gz/dox-data_docnews_digest-0.0.1/README.md
@@ -0,0 +1,1 @@
+dox-data_docnews_digest
---
+++ packages/dox-data-docnews-digest/dox-data_docnews_digest-0.0.1.tar.gz/dox-data_docnews_digest-0.0.1/setup.py
@@ -0,0 +1,7 @@
+import setuptools
+
+setuptools.setup(
+    name="dox-data_docnews_digest",
+    version="0.0.1",
+    packages=setuptools.find_packages(),
+)
