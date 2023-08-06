690bffc8789d382f74870c5cef36038d99b86f93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:36 +0000

    Add package SR700-Artisan-Server/1.5/SR700_Artisan_Server-1.5-py3-none-any.whl

---
+++ packages/SR700-Artisan-Server/SR700_Artisan_Server-1.5-py3-none-any.whl/SR700_Artisan_Server/Roaster_Cooling.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# SR700-Artisan-PDServer, released under GPLv3
+# Roaster Run Recipe
+
+import Pyro4
+import time
+
+def main():
+    roast_control = Pyro4.Proxy("PYRONAME:roaster.sr700")
+    roast_control.enable_temp_manual_mode()
+    roast_control.enable_fan_manual_mode()
+    roast_control.set_fan_speed(9)
+    roast_control.run_cooling()
+    time.sleep(2)
+    roast_control.set_fan_speed(9)
+
+if __name__ == '__main__':
+    main()
