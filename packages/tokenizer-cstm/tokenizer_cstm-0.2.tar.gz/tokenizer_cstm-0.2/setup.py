5a14bf27e4a1797fbd618d5efcf55a60dcbe92de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:15 +0000

    Add package tokenizer-cstm/0.2/tokenizer_cstm-0.2.tar.gz

---
+++ packages/tokenizer-cstm/tokenizer_cstm-0.2.tar.gz/tokenizer_cstm-0.2/setup.py
@@ -0,0 +1,11 @@
+
+import setuptools
+from distutils.core import setup
+
+setup(name='tokenizer_cstm',
+      version='0.2',
+      description='fining sentences from raw text and tokenizing',
+      author='Andrin Pelican',
+      author_email='andrin.pelican@bluewin.ch',
+      packages=['tokenizer_cstm'],
+     )
