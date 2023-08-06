052d1394f72e39de4f6fb69fa17b457e2c382741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:21 +0000

    Add package skil/1.2.1/skil-1.2.1-py3-none-any.whl

---
+++ packages/skil/skil-1.2.1-py3-none-any.whl/skil/resources/base.py
@@ -0,0 +1,19 @@
+class Resource:
+    """Resource
+
+    A SKIL  resource is an abstraction for (cloud and on-premise)
+    compute or storage capabilities.
+    """
+    __metaclass__ = type
+
+    def __init__(self, skil, resource_id=None, *args, **kwargs):
+        """Add the resource to SKIL.
+        """
+        self.skil = skil
+        self.resource_id = resource_id
+
+    def delete(self):
+        """Delete the resource from SKIL.
+        """
+        if self.resource_id:
+            self.skil.api.delete_resource_by_id(resource_id=self.resource_id)
