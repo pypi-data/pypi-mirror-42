166faeb417b2789a1f0d71eca5b2c721fc0a5e4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:02 +0000

    Add package images-utils/0.0.6/images-utils-0.0.6.tar.gz

---
+++ packages/images-utils/images-utils-0.0.6.tar.gz/images-utils-0.0.6/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+
+# with open("README.md", "r") as fh:
+# 	long_description = fh.read()
+long_description = 'images-utils'
+
+
+setuptools.setup(
+	name = "images-utils",
+	version="0.0.6",
+	auth="Huang Xu Hui",
+	author_email="13250270761@163.com",
+	description="images-utils has recognition function",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/suckmybigdick/flask-wechat-utils",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 2",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	install_requires=[
+		"setuptools==20.7.0",
+		"Pillow",
+		"numpy",
+		"imutils",
+	],
+)
