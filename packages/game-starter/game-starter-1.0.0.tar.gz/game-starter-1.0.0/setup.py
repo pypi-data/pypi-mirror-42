461b79a7da0a9bd3b1fa557b6f5202676c323045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:48 +0000

    Add package game-starter/1.0.0/game-starter-1.0.0.tar.gz

---
+++ packages/game-starter/game-starter-1.0.0.tar.gz/game-starter-1.0.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="game-starter",
+    version="1.0.0",
+    author="Daniel Bailey",
+    author_email="danieljabailey@gmail.com",
+    description="Decides when to start a game, based on who's holding a button.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/danieljabailey/GameStarter",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
