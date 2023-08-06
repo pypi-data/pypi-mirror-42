e94a5209dbdf6fc343c58e2dea7b35a2944f6cde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:37 +0000

    Add package fastapi/0.6.1/fastapi-0.6.1-py3-none-any.whl

---
+++ packages/fastapi/fastapi-0.6.1-py3-none-any.whl/fastapi/__init__.py
@@ -0,0 +1,8 @@
+"""FastAPI framework, high performance, easy to learn, fast to code, ready for production"""
+
+__version__ = "0.6.1"
+
+from .applications import FastAPI
+from .routing import APIRouter
+from .params import Body, Path, Query, Header, Cookie, Form, File, Security, Depends
+from .exceptions import HTTPException
