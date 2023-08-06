6ef388af82f717162d71e98f478bb0e9dcd55ade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:04 +0000

    Add package pytest-tomato/1.0.4/pytest-tomato-1.0.4.tar.gz

---
+++ packages/pytest-tomato/pytest-tomato-1.0.4.tar.gz/pytest-tomato-1.0.4/setup.py
@@ -0,0 +1,48 @@
+from setuptools import find_packages, setup
+
+install_requires = ["tomato-lib==1.0.4"]
+
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except Exception:
+    long_description = ''
+
+setup(
+    name="pytest-tomato",
+    version='1.0.4',
+    description="",
+    long_description=long_description,
+    author="Moshe Zada",
+    url="https://github.com/moshe/python-tomato",
+    platforms=["linux", "osx", "win32"],
+    packages=find_packages(exclude=["testing", "example"]),
+    entry_points={
+        "pytest11": ["tomato = pytest_tomato.plugin"]
+    },
+    zip_safe=False,
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    install_requires=install_requires,
+    setup_requires=[],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Pytest",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS :: MacOS X",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: Utilities",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
