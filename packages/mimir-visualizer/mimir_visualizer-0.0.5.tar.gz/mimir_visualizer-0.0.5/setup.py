caadd49a0802975d2a2bd8aca19ae59440b7d386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:37 +0000

    Add package mimir-visualizer/0.0.5/mimir_visualizer-0.0.5.tar.gz

---
+++ packages/mimir-visualizer/mimir_visualizer-0.0.5.tar.gz/mimir_visualizer-0.0.5/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="mimir_visualizer",
+    version="0.0.5",
+    author="Steven Hicks",
+    author_email="steven@simula.no",
+    description="Mimir's visualization module.",
+    url="https://github.com/stevenah/mimir-visualizer",
+    packages=setuptools.find_packages(exclude=['tests*']),
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
+)
