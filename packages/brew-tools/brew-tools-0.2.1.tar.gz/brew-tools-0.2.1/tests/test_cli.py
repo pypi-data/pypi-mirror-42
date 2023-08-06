365f233f5ded9e6a54329b517c322324f8885d58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:57 +0000

    Add package brew-tools/0.2.1/brew-tools-0.2.1.tar.gz

---
+++ packages/brew-tools/brew-tools-0.2.1.tar.gz/brew-tools-0.2.1/tests/test_cli.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Sven Steinbauer"
+__copyright__ = "Sven Steinbauer"
+__license__ = "mit"
+
+from click.testing import CliRunner
+from brew_tools import command_line
+
+
+def test_abv():
+    runner = CliRunner()
+    result = runner.invoke(command_line.main, 'abv -og 1.05 -fg 1.02 -adjust')
+
+    assert result.exit_code == 0
+    assert result.output == "Estimated ABV: 5.63%\n"
+
+
+def test_abv_no_adjust():
+    runner = CliRunner()
+    result = runner.invoke(command_line.main, 'abv -og 1.05 -fg 1.02 ')
+
+    assert result.exit_code == 0
+    assert result.output == "Estimated ABV: 3.94%\n"
+
+
+def test_abv_high_fg():
+    runner = CliRunner()
+    result = runner.invoke(command_line.main, 'abv -og 1.05 -fg 1.06')
+
+    assert result.exit_code == 1
+    assert result.output == ("Final gravity cannot be higher "
+                             "than original gravity\n")
+
+
+def test_abv_range():
+    runner = CliRunner()
+    result = runner.invoke(command_line.main, 'abv -og 2.0 -fg 1.06')
+    assert result.exit_code == 1
+    assert result.output == "ERROR: Value must be between 1.0 and 1.2\n"
+
+
+def test_kegpsi():
+    runner = CliRunner()
+    result = runner.invoke(command_line.main, 'kegpsi -vol 2.0 -temp 15')
+    assert result.exit_code == 0
+    assert result.output == "Keg pressure required: 15.49psi\n"
+
+
+def test_kegpsi_imp():
+    runner = CliRunner()
+    result = runner.invoke(command_line.main,
+                           '-imperial kegpsi -vol 2.0 -temp 36')
+    assert result.exit_code == 0
+    assert result.output == "Keg pressure required: 5.27psi\n"
+
+
+def test_prime():
+    runner = CliRunner()
+    result = runner.invoke(command_line.main,
+                           'prime -beer 19 -vol 2.2 -temp 15')
+    assert result.exit_code == 0
+    assert result.output == ("\nUse only one of the following:\n"
+                             "Table sugar: 90.87g\n"
+                             "Corn Sugar: 99.91g\n"
+                             "DME: 133.63g\n")
+
+
+def test_prime_imperial():
+    runner = CliRunner()
+    result = runner.invoke(command_line.main,
+                           '-imperial prime -beer 5 -vol 2.2 -temp 68')
+    assert result.exit_code == 0
+    assert result.output == ("\nUse only one of the following:\n"
+                             "Table sugar: 3.59oz\n"
+                             "Corn Sugar: 3.94oz\n"
+                             "DME: 5.27oz\n")
+
+
+def test_infuse():
+    runner = CliRunner()
+    result = runner.invoke(command_line.main,
+                           ("infuse -temp 66 -target 70 -ratio 1 "
+                            "-grain 5 -water 100"))
+    assert result.exit_code == 0
+    assert result.output == "Infuse with 0.80 liters @ 100.0C\n"
+
+
+def test_imperial():
+    runner = CliRunner()
+    result = runner.invoke(command_line.main,
+                           ("-imperial infuse -temp 152 -target 168 "
+                            "-ratio 1.5 -grain 10 -water 212"))
+    assert result.exit_code == 0
+    assert result.output == "Infuse with 6.18 quarts @ 212.0F\n"
+
+
+def test_dme():
+    runner = CliRunner()
+    result = runner.invoke(command_line.main,
+                           (" dme -points 5 -vol 12.3"))
+    assert result.exit_code == 0
+    assert result.output == "Add 167.48g of DME to raise the wort gravity by 5 points\n"
+
+
+def test_dme_imperial():
+    runner = CliRunner()
+    result = runner.invoke(command_line.main,
+                           ("-imperial dme -points 5 -vol 3.25"))
+    assert result.exit_code == 0
+    assert result.output == "Add 5.91oz of DME to raise the wort gravity by 5 points\n"
+
+
+def test_attenuation():
+    runner = CliRunner()
+    result = runner.invoke(command_line.main,
+                           ("attenuation -og 1.06 -fg 1.023"))
+    assert result.exit_code == 0
+    assert result.output == ("Apparent attenuation: 60.46%\n"
+                             "Real attenuation: 49.53%\n")
+
+
+def test_fg_from_att():
+    runner = CliRunner()
+    result = runner.invoke(command_line.main,
+                           ("fg-from-att -og 1.04 -att 49"))
+    assert result.exit_code == 0
+    assert result.output == ("FG for 49.0% attenuation: 1.020\n")
+
+
+def test_adj_grav_up_vol():
+    runner = CliRunner()
+    result = runner.invoke(command_line.main,
+                           ("adjust-gravity -og 1.04 -ng 1.06 -vol 3"))
+    assert result.exit_code == 0
+    assert result.output == ("\nNew volume of wort will be 2.00\n"
+                             "Boil off 1.00 liter of wort\n")
+
+
+def test_adj_grav_down_vol():
+    runner = CliRunner()
+    result = runner.invoke(command_line.main,
+                           ("adjust-gravity -og 1.05 -ng 1.04 -vol 5"))
+    assert result.exit_code == 0
+    assert result.output == ("\nNew volume of wort will be 6.25\n"
+                             "Dilute wort with 1.25 liter of water\n")
+
+
+def test_adj_vol():
+    runner = CliRunner()
+    result = runner.invoke(command_line.main,
+                           ("adjust-volume -og 1.05 -newvol 4 -vol 5"))
+    assert result.exit_code == 0
+    assert result.output == ("The new gravity will be 1.062\n")
---
+++ packages/brew-tools/brew-tools-0.2.1.tar.gz/brew-tools-0.2.1/tests/test_maths.py
@@ -0,0 +1,123 @@
+import pytest
+
+from brew_tools import brew_maths as bm
+
+
+def test_oz_to_g():
+    g = bm.oz_to_g(34)
+    assert g == pytest.approx(963.883, 0.001)
+
+
+def test_g_to_oz():
+    oz = bm.g_to_oz(34)
+    assert oz == pytest.approx(1.199, 0.001)
+
+
+def test_lbs_to_oz():
+    oz = bm.lbs_to_oz(2)
+    assert oz == pytest.approx(32, 0.01)
+
+
+def test_c_to_f():
+    f = bm.c_to_f(28)
+    assert f == pytest.approx(82.4, 0.1)
+
+
+def test_f_to_c():
+    c = bm.f_to_c(82.4)
+    assert c == pytest.approx(28, 0.1)
+
+
+def test_f_to_c_to_f():
+    c = bm.f_to_c(244.4)
+    f = bm.c_to_f(c)
+    assert f == 244.4
+
+
+def test_c_to_f_to_c():
+    f = bm.c_to_f(115)
+    c = bm.f_to_c(f)
+    assert c == 115
+
+
+def test_l_to_g():
+    g = bm.l_to_g(47)
+    assert g == pytest.approx(12.416, 0.001)
+
+
+def test_g_to_l():
+    l = bm.g_to_l(12.41)
+    assert l == pytest.approx(47, 0.1)
+
+
+def test_l_to_q():
+    q = bm.l_to_q(12)
+    assert q == pytest.approx(12.68, 0.001)
+
+
+def test_kg_to_lbs():
+    lbs = bm.kg_to_lbs(5.4)
+    assert lbs == pytest.approx(11.904, 0.001)
+
+
+def test_to_brix():
+    brix = bm.to_brix(1.045)
+    assert brix == pytest.approx(11.195, 0.001)
+
+
+def test_adjust_gravity():
+    adjusted = bm.adjust_gravity(1.075, 1.022)
+    assert adjusted == pytest.approx(1.004, 0.001)
+
+
+def test_priming():
+    prim_amount = bm.priming(68, 5, 2)
+    assert prim_amount == pytest.approx(86.498, 0.001)
+
+
+def test_infusion():
+    infuse_water = bm.infusion(1.5, 152, 168, 212, 10)
+    assert infuse_water == pytest.approx(6.1818, 0.001)
+
+
+def test_abv():
+    abv = bm.abv(1.04, 1.01, True)
+    assert abv == pytest.approx(5.199, 0.001)
+
+
+def test_abv_noadjust():
+    abv = bm.abv(1.058, 1.008, False)
+    assert abv == pytest.approx(6.56, 0.001)
+
+
+def test_pre_boil_dme():
+    dme = bm.pre_boil_dme(5, 3.25)
+    assert dme == pytest.approx(5.91, 0.01)
+
+
+def test_apparent_attenuation():
+    aa = bm.apparent_attenuation(1.032, 1.015)
+    assert aa == pytest.approx(0.5246, 0.001)
+
+
+def test_real_attenuation():
+    aa = bm.real_attenuation(1.032, 1.015)
+    assert aa == pytest.approx(0.4298, 0.001)
+
+
+def test_fg_from_attenuation():
+    fg = bm.fg_from_attenuation(1.04, 49)
+    assert fg == pytest.approx(1.02, 0.001)
+
+
+def test_adjust_gravity_volume():
+    new_vol = bm.adjust_gravity_volume(4, 1.04, 1.07)
+    assert new_vol == pytest.approx(2.29, 0.01)
+
+    new_vol = bm.adjust_gravity_volume(7, 1.06, 1.04)
+    assert new_vol == pytest.approx(10.50, 0.01)
+
+
+def test_adjust_volume_gravity():
+    new_grav = bm.adjust_volume_gravity(5, 1.04, 3)
+    assert new_grav == pytest.approx(1.067, 0.001)
