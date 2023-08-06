d44080d7937e7f8b694abcb048d578ba59889c4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:01 +0000

    Add package safitty/0.9/safitty-0.9.tar.gz

---
+++ packages/safitty/safitty-0.9.tar.gz/safitty-0.9/safitty/__init__.py
@@ -0,0 +1,21 @@
+from safitty.core import get, set
+from safitty.types import star, dstar, Storage, Key, Transform
+from safitty.parser import argparser, load_config, \
+    update_config_from_args, load_config_from_args
+
+
+__all__ = [
+    "get",
+    "set",
+    "star",
+    "dstar",
+    "Storage",
+    "Key",
+    "Transform",
+    "argparser",
+    "load_config",
+    "update_config_from_args",
+    "load_config_from_args"
+]
+
+__author__ = "Roman Tezikov"
