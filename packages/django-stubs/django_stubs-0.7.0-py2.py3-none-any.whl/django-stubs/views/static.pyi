5b76fb496515106987c5b7de0534d77c2046ce94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:36 +0000

    Add package django-stubs/0.7.0/django_stubs-0.7.0-py2.py3-none-any.whl

---
+++ packages/django-stubs/django_stubs-0.7.0-py2.py3-none-any.whl/django-stubs/views/static.pyi
@@ -0,0 +1,12 @@
+from typing import Any, Optional
+
+from django.core.handlers.wsgi import WSGIRequest
+from django.http.response import FileResponse
+
+def serve(request: WSGIRequest, path: str, document_root: str = ..., show_indexes: bool = ...) -> FileResponse: ...
+
+DEFAULT_DIRECTORY_INDEX_TEMPLATE: str
+template_translatable: Any
+
+def directory_index(path: Any, fullpath: Any): ...
+def was_modified_since(header: Optional[str] = ..., mtime: float = ..., size: int = ...) -> bool: ...
