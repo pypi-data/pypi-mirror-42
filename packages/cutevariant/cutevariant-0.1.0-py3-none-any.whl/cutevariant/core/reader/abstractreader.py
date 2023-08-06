178c9847b052f2914ab600ee3d3fa4d0f91aaf70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:08 +0000

    Add package cutevariant/0.1.0/cutevariant-0.1.0-py3-none-any.whl

---
+++ packages/cutevariant/cutevariant-0.1.0-py3-none-any.whl/cutevariant/core/reader/abstractreader.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractclassmethod
+
+
+class AbstractReader(ABC):
+    def __init__(self, device):
+        super(AbstractReader, self).__init__()
+        self.device = device
+        self.file_size = 0
+
+    @abstractclassmethod
+    def get_variants(self):
+        """
+        Must return list of variant items . 
+        exemple: {"chr": "chr3", "pos": 234 ....}
+        """
+        raise NotImplemented()
+
+    def get_variants_count(self):
+        count = 0
+        for v in self.get_variants():
+            count+=1
+        return count 
+
+
+
+    @abstractclassmethod
+    def get_fields(self):
+        """
+        must return list of field item with name, sqlite type, category and description 
+        exemple: {"name": "chr", "type": "text", "category": None, "description": None} 
+        """
+
+        raise NotImplemented()
+
+    def get_samples(self):
+        return []
+  
---
+++ packages/cutevariant/cutevariant-0.1.0-py3-none-any.whl/cutevariant/core/reader/vcfreader.py
@@ -0,0 +1,121 @@
+from .abstractreader import AbstractReader
+import vcf
+
+
+class VcfReader(AbstractReader):
+
+    type_mapping = {
+        "Float": "Float",
+        "Integer": "Integer",
+        "Flag": "Boolean",
+        "String": "String",
+    }
+
+    def __init__(self, device):
+        super(VcfReader, self).__init__(device)
+
+    def get_variants(self):
+        fields = list(self.get_fields())
+        self.device.seek(0)
+
+        vcf_reader = vcf.Reader(self.device)
+
+        for record in vcf_reader:
+
+            for index, alt in enumerate(record.ALT):
+                variant = {
+                    "chr": record.CHROM,
+                    "pos": record.POS,
+                    "ref": record.REF,
+                    "alt": str(alt),
+                }
+
+                # Read annotations
+                for field in fields:
+                    category = field["category"]
+                    name = field["name"]
+                    ftype = field["type"]
+                    colname = name
+                    value = None
+
+                    # PARSE INFO
+                    if category == "info":
+                        # Test flags
+                        if ftype == "Flag":
+                            variant[colname] = True if name in record.INFO else False
+                        else:
+                            if name in record.INFO:
+                                if isinstance(record.INFO[name], list):
+                                    value = record.INFO[name][0]
+                                else:
+                                    value = record.INFO[name]
+                            variant[colname] = value
+
+                    #         # PARSE GENOTYPE / SAMPLE
+                    if category == "sample":
+                        variant["samples"] = list()
+                        for sample in record.samples:
+                            gt = -1
+                            if sample["GT"] == "0/1":
+                                gt = 1
+                            if sample["GT"] == "0/0":
+                                gt = 0
+                            if sample["GT"] == "1/1":
+                                gt = 2
+
+                            variant["samples"].append({"name": sample.sample, "gt": gt})
+
+            yield variant
+
+    def get_fields(self):
+
+        yield {
+            "name": "chr",
+            "category": "variant",
+            "description": "chromosom",
+            "type": "text",
+        }
+        yield {
+            "name": "pos",
+            "category": "variant",
+            "description": "chromosom",
+            "type": "integer",
+        }
+        yield {
+            "name": "ref",
+            "category": "variant",
+            "description": "chromosom",
+            "type": "text",
+        }
+        yield {
+            "name": "alt",
+            "category": "variant",
+            "description": "chromosom",
+            "type": "text",
+        }
+
+        self.device.seek(0)
+        vcf_reader = vcf.Reader(self.device)
+
+        # Annotation ... TODO
+        # for key, info in vcf_reader.infos.items():
+        #     yield {
+        #         "name": key,
+        #         "category": "info",
+        #         "description": info.desc,
+        #         "type": VcfReader.type_mapping.get(info.type, "String"),
+        #     }
+
+        # PEUVENT SE METTRE AUTOMATIQUEMENT ...
+        for sample in vcf_reader.samples:
+            yield {
+                "name": f"gt{sample}.gt",
+                "category": "sample",
+                "description": "sample genotype",
+                "type": "text",
+            }
+
+    def get_samples(self):
+        self.device.seek(0)
+        vcf_reader = vcf.Reader(self.device)
+        return vcf_reader.samples
