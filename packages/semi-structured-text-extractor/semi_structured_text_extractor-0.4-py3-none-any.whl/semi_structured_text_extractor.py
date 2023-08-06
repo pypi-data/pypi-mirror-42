a23b763712e9a82e19e7aadf8a43658fe6f758a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:43 +0000

    Add package semi-structured-text-extractor/0.4/semi_structured_text_extractor-0.4-py3-none-any.whl

---
+++ packages/semi-structured-text-extractor/semi_structured_text_extractor-0.4-py3-none-any.whl/semi_structured_text_extractor.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import re
+
+from datetime import date, timedelta
+from pathlib import Path
+
+from dateutil.parser import parse
+from dateutil.relativedelta import relativedelta
+
+
+# In[17]:
+
+
+def split_text(text, symbol):
+    lines = text.split(symbol)
+    lines = [line.split() for line in lines if line.strip()]
+    return lines
+
+
+# In[38]:
+
+
+def find_objects(lines, object_type):
+    return_lines = []
+    for line in lines:
+        return_line = []
+        for word in line:
+            if object_type == "dates":
+                try:
+                    if len(word) > 5:
+                        raw_date = parse(word, dayfirst=True)
+                        formatted_date = raw_date.strftime(format="%Y-%m-%d")
+                        elapsed_years = relativedelta(raw_date, date.today()).years
+                        if elapsed_years > -10:
+                            if formatted_date:
+                                return_line.append(formatted_date)
+                except:
+                    pass
+            elif object_type == "numbers":
+                r1 = re.match(r"[\$0-9\.,]+", word)
+                try:
+                    if r1.group(0):
+                        return_line.append(r1.group(0))
+                except:
+                    pass
+        if return_line:
+            return_lines.append(return_line)
+    return return_lines
+
+
+# In[ ]:
