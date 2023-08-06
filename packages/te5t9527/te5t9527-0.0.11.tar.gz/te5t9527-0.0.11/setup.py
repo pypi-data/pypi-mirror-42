bd1f326aa91c8734c4828195dbf956ea26421055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:18 +0000

    Add package te5t9527/0.0.11/te5t9527-0.0.11.tar.gz

---
+++ packages/te5t9527/te5t9527-0.0.11.tar.gz/te5t9527-0.0.11/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages, Extension
+
+NAME = 'te5t9527'
+
+setup(
+    name = NAME,
+    version = __import__(NAME).__version__,
+    description = 'test python publish by travis-ci',
+    long_description = open('README.md').read(),
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/Secbone/py_publish_test',
+    author = 'Secbone',
+    author_email = 'secbone@gmail.com',
+    packages = find_packages(exclude = ['tests']),
+    python_requires = '>=3.5',
+    setup_requires = [
+        'setuptools',
+    ],
+    license = 'MIT',
+    entry_points = {
+        'console_scripts': [
+            'te5t9527 = te5t9527.cli:main',
+        ],
+    },
+)
---
+++ packages/te5t9527/te5t9527-0.0.11.tar.gz/te5t9527-0.0.11/te5t9527/__init__.py
@@ -0,0 +1,3 @@
+__version_info__ = (0, 0, 11)
+__version__ = '.'.join(str(x) for x in __version_info__)
+VERSION = __version__
