b6001fbccf8c7e3644f73b47ef26b49c141cf714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:43 +0000

    Add package funniest-roncaglionidaniele-new/0.1/funniest_roncaglionidaniele_new-0.1.tar.gz

---
+++ packages/funniest-roncaglionidaniele-new/funniest_roncaglionidaniele_new-0.1.tar.gz/funniest_roncaglionidaniele_new-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='funniest_roncaglionidaniele_new',
+      version='0.1',
+      description='The funniest joke in the world',
+      author='Daniele Roncaglioni',
+      author_email='daniler@propulsionacademy.ch',
+      license='MIT',
+      packages=['funniest_roncaglionidaniele_new'],
+      zip_safe=False)
