ecbda1f0c82b8c80e0e158e65ddc07b78d7ea1d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:53 +0000

    Add package swh.objstorage/0.0.30/swh.objstorage-0.0.30-py3-none-any.whl

---
+++ packages/swh.objstorage/swh.objstorage-0.0.30-py3-none-any.whl/swh/objstorage/tests/test_objstorage_api.py
@@ -0,0 +1,43 @@
+# Copyright (C) 2015-2019  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+import shutil
+import tempfile
+import unittest
+
+from swh.core.tests.server_testing import ServerTestFixtureAsync
+from swh.objstorage import get_objstorage
+from swh.objstorage.api.server import make_app
+from swh.objstorage.tests.objstorage_testing import ObjStorageTestFixture
+
+
+class TestRemoteObjStorage(ServerTestFixtureAsync, ObjStorageTestFixture,
+                           unittest.TestCase):
+    """ Test the remote archive API.
+    """
+
+    def setUp(self):
+        self.tmpdir = tempfile.mkdtemp()
+        self.config = {
+            'objstorage': {
+                'cls': 'pathslicing',
+                'args': {
+                    'root': self.tmpdir,
+                    'slicing': '0:1/0:5',
+                    'allow_delete': True,
+                }
+            },
+            'client_max_size': 8 * 1024 * 1024,
+        }
+
+        self.app = make_app(self.config)
+        super().setUp()
+        self.storage = get_objstorage('remote', {
+            'url': self.url()
+        })
+
+    def tearDown(self):
+        super().tearDown()
+        shutil.rmtree(self.tmpdir)
