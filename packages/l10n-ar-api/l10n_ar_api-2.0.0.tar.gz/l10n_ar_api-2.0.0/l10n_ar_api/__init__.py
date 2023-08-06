bab40b71e7007e1b3def780c3ecc8fef567355ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:35 +0000

    Add package l10n-ar-api/2.0.0/l10n_ar_api-2.0.0.tar.gz

---
+++ packages/l10n-ar-api/l10n_ar_api-2.0.0.tar.gz/l10n_ar_api-2.0.0/l10n_ar_api/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+from . import documents
+from . import afip_webservices
+from . import arba_webservices
+from . import presentations
+from . import padron
---
+++ packages/l10n-ar-api/l10n_ar_api-2.0.0.tar.gz/l10n_ar_api-2.0.0/l10n_ar_api/afip_webservices/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+from . import wsaa
+from . import wsbfe
+from . import wsfe
+from . import wsfex
