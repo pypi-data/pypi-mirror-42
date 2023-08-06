d5c0c6bfa4feeff71c7f14aff385a5301a9e658e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:08 +0000

    Add package accupy/0.1.4/accupy-0.1.4.tar.gz

---
+++ packages/accupy/accupy-0.1.4.tar.gz/accupy-0.1.4/accupy/__init__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#
+from __future__ import print_function
+
+from .__about__ import (
+    __author__,
+    __email__,
+    __copyright__,
+    __license__,
+    __version__,
+    __status__,
+)
+
+from .dot import kdot, fdot
+from .ill_cond import generate_ill_conditioned_sum, generate_ill_conditioned_dot_product
+from .sums import knuth_sum, decker_sum, distill, ksum, fsum, kahan_sum
+
+__all__ = [
+    "__author__",
+    "__email__",
+    "__copyright__",
+    "__license__",
+    "__version__",
+    "__status__",
+    "kdot",
+    "fdot",
+    "generate_ill_conditioned_sum",
+    "generate_ill_conditioned_dot_product",
+    "knuth_sum",
+    "decker_sum",
+    "distill",
+    "ksum",
+    "fsum",
+    "kahan_sum",
+]
+
+try:
+    import pipdate
+except ImportError:
+    pass
+else:
+    if pipdate.needs_checking(__name__):
+        print(pipdate.check(__name__, __version__), end="")
