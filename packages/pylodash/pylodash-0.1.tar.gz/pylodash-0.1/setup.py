66d149077f96af4ee4bdaca2f2bb3f4020aea449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:02 +0000

    Add package pylodash/0.1/pylodash-0.1.tar.gz

---
+++ packages/pylodash/pylodash-0.1.tar.gz/pylodash-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pylodash',
+    version='0.1',
+    description='A modern Python utility library delivering modularity, performance & extras.',
+    url='https://gitlab.asoft-python.com/g-tuanluu/python-training',
+    author='Tuan Luu',
+    author_email='tuan.luu@asnet.com.vn',
+    license='MIT',
+    packages=['pylodash'],
+    zip_safe=False)
