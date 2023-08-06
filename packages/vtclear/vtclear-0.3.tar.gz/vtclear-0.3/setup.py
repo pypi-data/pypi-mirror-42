4f1d3f172e2a968c65c1d8b850a3376bcc3c5258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:00 +0000

    Add package vtclear/0.3/vtclear-0.3.tar.gz

---
+++ packages/vtclear/vtclear-0.3.tar.gz/vtclear-0.3/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+
+setup(
+    name="vtclear",
+    packages=["vtclear"],
+    version="0.3",
+    license="GPL3",
+    description="Python3 ANSI VT100 implementation of Erase Screen.",
+    author="Carlos A. Planchón",
+    author_email="bubbledoloresuruguay2@gmail.com",
+    url="https://github.com/carlosplanchon/vtclear",
+    download_url="https://github.com/carlosplanchon/"
+        "reponame/archive/v0.3.tar.gz",
+    keywords=["CLEAR", "VT100", "ANSI"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
