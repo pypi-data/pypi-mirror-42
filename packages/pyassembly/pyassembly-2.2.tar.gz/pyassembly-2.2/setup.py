6c80361e92416938c6ee0a890dd60935f7a4467f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:04 +0000

    Add package pyassembly/2.2/pyassembly-2.2.tar.gz

---
+++ packages/pyassembly/pyassembly-2.2.tar.gz/pyassembly-2.2/setup.py
@@ -0,0 +1,41 @@
+try:
+    from pip._internal.req import parse_requirements
+except ImportError:  # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
+from setuptools import setup, find_packages
+
+
+VERSION = "2.2"
+PACKAGE_NAME = "pyassembly"
+
+reqs = parse_requirements('requirements.txt', session=False)
+requirements = [str(ir.req) for ir in reqs]
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+
+    description="Creates an egg or a zip distribution with all its transitive dependencies",
+
+    author="Aleksey Zhukov",
+    author_email='alekseyig@hotmail.com',
+
+    license='MIT',
+    platforms=["any"],
+    url="https://github.com/alekseyig/pyassembly",
+    keywords=['egg', 'zip', 'transitive', 'dependencies', 'assembly'],
+
+    packages=find_packages(include=['pyassembly', 'pyassembly.*'],
+                           exclude=['*.test.*', '*.test']),
+
+    install_requires=requirements,
+
+    package_data={
+        PACKAGE_NAME: ['../requirements.txt']
+    },
+
+    entry_points={
+        'distutils.commands': ['pyassembly = pyassembly.main:pyassembly']
+    }
+)
