e877e27041ca5f67d78fbeb6f26cf24eb092fed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:40 +0000

    Add package DaMa-ML/1.0a1/DaMa_ML-1.0a1-py3-none-any.whl

---
+++ packages/DaMa-ML/DaMa_ML-1.0a1-py3-none-any.whl/dama/commands/plot.py
@@ -0,0 +1,3 @@
+from dama.utils.config import get_settings
+
+settings = get_settings("paths")
---
+++ packages/DaMa-ML/DaMa_ML-1.0a1-py3-none-any.whl/dama/fmtypes.py
@@ -0,0 +1,22 @@
+from collections import namedtuple
+from numpy import dtype
+
+Fmtype = namedtuple('Fmtype', 'name dtype db_type')
+Slice = namedtuple('Slice', 'batch slice')
+
+DEFAUL_GROUP_NAME = "g0"
+BOOLEAN = Fmtype(name="boolean", dtype=dtype(bool), db_type="BOOLEAN")
+NANBOOLEAN = Fmtype(name="nan boolean", dtype=dtype(int), db_type="INTEGER")
+ORDINAL = Fmtype(name="int", dtype=dtype(int), db_type="INTEGER")
+DENSE = Fmtype(name="float", dtype=dtype(float), db_type="FLOAT")
+TEXT = Fmtype(name="text", dtype=dtype(object), db_type="TEXT")
+DATETIME = Fmtype(name="datetime", dtype=dtype("datetime64[ns]"), db_type="TIMESTAMP")
+
+fmtypes_map = {
+    BOOLEAN.dtype: BOOLEAN,
+    NANBOOLEAN.dtype: NANBOOLEAN,
+    ORDINAL.dtype: ORDINAL,
+    DENSE.dtype: DENSE,
+    TEXT.dtype: TEXT,
+    DATETIME.dtype: DATETIME
+}
