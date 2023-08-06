1afc08019ebd1ae5820687883cabaad564164a81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:25 +0000

    Add package hvac-cli/1.0.5/hvac_cli-1.0.5.tar.gz

---
+++ packages/hvac-cli/hvac_cli-1.0.5.tar.gz/hvac_cli-1.0.5/hvac_cli/cli.py
@@ -0,0 +1,23 @@
+import hvac
+
+
+class CLI(object):
+
+    def __init__(self, args):
+        self.args = args
+        self.open_vault()
+
+    def open_vault(self):
+        if self.args.tls_skip_verify:
+            verify = False
+        else:
+            if self.args.ca_cert:
+                verify = self.args.ca_cert
+            else:
+                verify = True
+
+        cert = (self.args.client_cert, self.args.client_key)
+        self.vault = hvac.Client(url=self.args.address,
+                                 token=self.args.token,
+                                 cert=cert,
+                                 verify=verify)
