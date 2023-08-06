b2fe1fc58487c2e5c014f2e88ee0e2acbcaaad95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:03 +0000

    Add package weave/0.17.0/weave-0.17.0.tar.gz

---
+++ packages/weave/weave-0.17.0.tar.gz/weave-0.17.0/doc/source/release.rst
@@ -0,0 +1,10 @@
+*************
+Release Notes
+*************
+
+.. toctree::
+   :maxdepth: 1
+
+   release.0.15.0
+   release.0.16.0
+   release.0.17.0
---
+++ packages/weave/weave-0.17.0.tar.gz/weave-0.17.0/weave/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM WEAVE SETUP.PY
+short_version = '0.17.0'
+version = '0.17.0'
+full_version = '0.17.0'
+git_revision = '55d48df39b9685164a71fd29050ad45d307b0a97'
+release = True
+
+if not release:
+    version = full_version
