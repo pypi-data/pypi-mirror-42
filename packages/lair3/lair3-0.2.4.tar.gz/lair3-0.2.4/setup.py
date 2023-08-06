ace75d7f66a437e5307a243a26467c030a8726d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:58 +0000

    Add package lair3/0.2.4/lair3-0.2.4.tar.gz

---
+++ packages/lair3/lair3-0.2.4.tar.gz/lair3-0.2.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fd:
+    long_description = fd.read()
+
+setuptools.setup(
+    name="lair3",
+    version="0.2.4",
+    author="The Munshi Group",
+    author_email="support@munshigroup.com",
+    description="AmmuNation Networking Library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/munshigroup/lair3",
+    packages=setuptools.find_packages(),
+    install_requires = ['amncore', 'bombfuse', 'bs4', 'kthread', 'paynspray', 'pyopenssl'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
