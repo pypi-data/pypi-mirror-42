ae856fea463bc329d83cf8dc7d6dc08ce1562459
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:50 +0000

    Add package r2c-cli/0.0.6/r2c_cli-0.0.6-py3-none-any.whl

---
+++ packages/r2c-cli/r2c_cli-0.0.6-py3-none-any.whl/r2c/cli/__main__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import logging
+import os
+from distutils.util import strtobool
+
+import click
+
+from r2c.cli.core import cli
+
+logger = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    try:
+        cli(obj={}, prog_name="r2c")
+    except Exception as e:
+        debug = strtobool(os.getenv("DEBUG", "False"))
+        if debug:
+            logger.exception(e)
+        else:
+            click.echo(
+                f"❌ there was an unexpected error. Please use `r2c --debug <CMD>`, to get error log. For more help, reach out to R2C  at support@ret2.co"
+            )
