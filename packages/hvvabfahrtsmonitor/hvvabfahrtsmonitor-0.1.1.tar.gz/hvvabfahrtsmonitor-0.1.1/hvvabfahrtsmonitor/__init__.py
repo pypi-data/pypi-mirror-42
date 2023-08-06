bbc4f982da82796c5eb752ed003c5ef637f3fece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:04 +0000

    Add package hvvabfahrtsmonitor/0.1.1/hvvabfahrtsmonitor-0.1.1.tar.gz

---
+++ packages/hvvabfahrtsmonitor/hvvabfahrtsmonitor-0.1.1.tar.gz/hvvabfahrtsmonitor-0.1.1/hvvabfahrtsmonitor/__init__.py
@@ -0,0 +1,4 @@
+from hvvabfahrtsmonitor.hvvabfahrtsmonitor import HvvAbfahrtsmonitor
+__version__ = '0.1.1'
+__author__ = 'Manuel Cantu <m.cantu.reinhard@gmail.com>'
+__all__ = []
---
+++ packages/hvvabfahrtsmonitor/hvvabfahrtsmonitor-0.1.1.tar.gz/hvvabfahrtsmonitor-0.1.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hvvabfahrtsmonitor",
+    version="0.1.1",
+    author="Manuel Catu",
+    author_email="m.cantu.reinhard@gmail.com",
+    description="Do requests to the hvv abfahrtsmonitor and get parsed data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mcantureinhard/hvvabfahrtsmonitor",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
