a3e9b06f3794f1176fc684a274a89db6da2c33cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:35 +0000

    Add package pgbar/0.2/pgbar-0.2.tar.gz

---
+++ packages/pgbar/pgbar-0.2.tar.gz/pgbar-0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pgbar',
+    version='0.2',
+    packages=['pgbar'],
+    url='https://gitlab.com/sajeeshen/python-terminal-progress',
+    license='',
+    author='SajeeshNamboothiri',
+    author_email='sajeeshe@gmail.com',
+    description='Terminal Progress Bar',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
