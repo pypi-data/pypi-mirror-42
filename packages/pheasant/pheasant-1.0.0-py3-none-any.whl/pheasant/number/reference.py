97259b417ca75738a1a0de612bc788f98d0f181d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:58 +0000

    Add package pheasant/1.0.0/pheasant-1.0.0-py3-none-any.whl

---
+++ packages/pheasant/pheasant-1.0.0-py3-none-any.whl/pheasant/number/reference.py
@@ -0,0 +1,39 @@
+from typing import Generator
+
+from pheasant.markdown.splitter import escaped_splitter
+from pheasant.number.config import config
+
+
+def convert(source: str, label: dict) -> str:
+    return ''.join(render(source, label))
+
+
+def render(source: str, label: dict) -> Generator[str, None, None]:
+    """
+    Generate splitted reference and body text from `source`.
+
+    some text {ref}, another text
+
+    Parameters
+    ----------
+    source : str
+        Markdown source string.
+
+    Yields
+    ------
+    splitted source : str or dict
+    """
+
+    pattern_escape = r'(^```(.*?)^```$)|(^~~~(.*?)^~~~$)|(<pre>(.*?)</pre>)'
+    pattern_label = config['label_pattern']
+
+    for splitted in escaped_splitter(pattern_label, pattern_escape, source):
+        if isinstance(splitted, str):
+            yield splitted
+        else:
+            entity = splitted.group(1)
+            if entity in label:
+                yield config['template'].render(reference=True, config=config,
+                                                **label[entity])
+            else:
+                yield splitted.group()
