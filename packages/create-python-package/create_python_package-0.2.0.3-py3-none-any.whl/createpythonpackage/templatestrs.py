66379a295b57b1b111f57886cd643961826da012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:47 +0000

    Add package create-python-package/0.2.0.3/create_python_package-0.2.0.3-py3-none-any.whl

---
+++ packages/create-python-package/create_python_package-0.2.0.3-py3-none-any.whl/createpythonpackage/templatestrs.py
@@ -0,0 +1,23 @@
+readme = """# {name}
+{description}
+"""
+
+gitignore = """*.pyc
+venv/
+bin/
+build/
+develop-eggs/
+dist/
+eggs/
+lib/
+lib64/
+parts/
+sdist/
+var/
+*.egg-info/
+.installed.cfg
+*.egg
+*.eggs
+.mypy_cache/
+.vscode
+"""
