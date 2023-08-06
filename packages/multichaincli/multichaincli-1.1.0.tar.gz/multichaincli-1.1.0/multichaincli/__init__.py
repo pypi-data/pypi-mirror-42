f60a4ab9765034ddcd92346e4b850a460ac90b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:45 +0000

    Add package multichaincli/1.1.0/multichaincli-1.1.0.tar.gz

---
+++ packages/multichaincli/multichaincli-1.1.0.tar.gz/multichaincli-1.1.0/multichaincli/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+from multichaincli.client import Multichain
---
+++ packages/multichaincli/multichaincli-1.1.0.tar.gz/multichaincli-1.1.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name='multichaincli',
+      version='1.1.0',
+      description='A python binding for Multichain Json-RPC API',
+      long_description=read('README.md'),
+      license="BSD",
+      author='Alex Khaerov, Federico Cardoso',
+      author_email='alex.khaerov@chainstack.com,'
+                   'federico.cardoso@dxmarkets.com',
+      url='https://github.com/chainstack/multichaincli',
+      keywords='multichain python blockchain jsonrpc',
+      packages=find_packages(),
+      install_requires=[
+          'requests>=2.20.0',
+      ],
+      )
