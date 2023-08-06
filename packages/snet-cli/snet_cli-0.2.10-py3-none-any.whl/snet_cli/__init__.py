7579e52e85db494baab4f1656f7209405efede9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:10 +0000

    Add package snet-cli/0.2.10/snet_cli-0.2.10-py3-none-any.whl

---
+++ packages/snet-cli/snet_cli-0.2.10-py3-none-any.whl/snet_cli/__init__.py
@@ -0,0 +1,28 @@
+import sys
+
+from snet_cli import arguments
+from snet_cli.config import Config
+
+__version__ = "0.2.10"
+
+
+def main():
+    try:
+        argv = sys.argv[1:]
+        conf   = Config()
+        parser = arguments.get_root_parser(conf)
+
+        try:
+            args = parser.parse_args(argv)
+        except TypeError:
+            args = parser.parse_args(argv + ["-h"])
+
+        getattr(args.cmd(conf, args), args.fn)()
+    except Exception as e:
+        if (sys.argv[1] == "--print-traceback"):
+            raise
+        else:
+            print("Error:", e)
+            print("If you want to see full Traceback then run:")
+            print("snet --print-traceback [parameters]")
+            sys.exit(42)
