7561b629a63b0889adc8a4e5419fba6822cc7b74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:24 +0000

    Add package django-ecsmanage/0.0.0/django-ecsmanage-0.0.0.tar.gz

---
+++ packages/django-ecsmanage/django-ecsmanage-0.0.0.tar.gz/django-ecsmanage-0.0.0/setup.py
@@ -0,0 +1,44 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+description = (
+    'Run any Django management command on an AWS Elastic Container Service'
+    '(ECS) cluster.'
+)
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-ecsmanage',
+    version='0.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    license='Apache License 2.0',
+    description=description,
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/azavea/django-ecsmanage/',
+    author='Azavea',
+    author_email='yourname@example.com',
+    python_requires='>=3.5',
+    install_requires=[
+        'Django >=1.11, <=2.1',
+        'boto3 >=1.9.0'
+    ],
+    extras_require={'tests': ['moto >= 1.3.3', 'flake8 >= 3.7.7']},
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
