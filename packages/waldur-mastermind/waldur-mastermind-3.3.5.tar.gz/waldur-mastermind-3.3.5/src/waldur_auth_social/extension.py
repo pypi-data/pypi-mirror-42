a330d8635a9f88e51565ba8e851fc14ad55c6bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:18 +0000

    Add package waldur-mastermind/3.3.5/waldur-mastermind-3.3.5.tar.gz

---
+++ packages/waldur-mastermind/waldur-mastermind-3.3.5.tar.gz/waldur-mastermind-3.3.5/src/waldur_auth_social/extension.py
@@ -0,0 +1,44 @@
+from __future__ import unicode_literals
+
+from waldur_core.core import WaldurExtension
+
+
+class AuthSocialExtension(WaldurExtension):
+
+    class Settings:
+        # wiki: https://opennode.atlassian.net/wiki/display/WD/AuthSocial+plugin+configuration
+        WALDUR_AUTH_SOCIAL = {
+            'GOOGLE_SECRET': '',
+            'GOOGLE_CLIENT_ID': '',
+
+            'FACEBOOK_SECRET': '',
+            'FACEBOOK_CLIENT_ID': '',
+
+            'SMARTIDEE_SECRET': '',
+            'SMARTIDEE_CLIENT_ID': '',
+
+            'TARA_SECRET': '',
+            'TARA_CLIENT_ID': '',
+            'TARA_SANDBOX': True,
+
+            'USER_ACTIVATION_URL_TEMPLATE': 'http://example.com/#/activate/{user_uuid}/{token}/',
+        }
+
+    @staticmethod
+    def get_public_settings():
+        return [
+            'GOOGLE_CLIENT_ID',
+            'FACEBOOK_CLIENT_ID',
+            'SMARTIDEE_CLIENT_ID',
+            'TARA_CLIENT_ID',
+            'TARA_SANDBOX',
+        ]
+
+    @staticmethod
+    def django_app():
+        return 'waldur_auth_social'
+
+    @staticmethod
+    def django_urls():
+        from .urls import urlpatterns
+        return urlpatterns
