161558d1f03a532ad3b5f55d53882fe83039de7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:41 +0000

    Add package pipecashwallets/0.1.0/pipecashwallets-0.1.0.tar.gz

---
+++ packages/pipecashwallets/pipecashwallets-0.1.0.tar.gz/pipecashwallets-0.1.0/pipecashwallets/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for pipecash-wallets."""
+
+__author__ = """Aleksandar Nikolaev Dinkov"""
+__email__ = 'alexander.n.dinkov@gmail.com'
+__version__ = '0.1.0'
+
+from pipecashwallets.btc_rpc import *
+
+from pipecashwallets.bsv_rpc import *
+from pipecashwallets.bsv_bitindex import *
+
+from pipecashwallets.examples import *
---
+++ packages/pipecashwallets/pipecashwallets-0.1.0.tar.gz/pipecashwallets-0.1.0/tests/test_pipecashwallets.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `pipecashwallets` package."""
+
+
+import unittest
+
+from pipecashwallets import BitcoinBTC_RPC
+from pipecashwallets import BitcoinSV_RPC
+from pipecashwallets import BitcoinSV_BitIndex
+
+from pipecashwallets import ExampleMinimalWallet
+from pipecashwallets import ExampleWallet
+
+
+class TestPipecashwallets(unittest.TestCase):
+    """Tests for `pipecashwallets` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
