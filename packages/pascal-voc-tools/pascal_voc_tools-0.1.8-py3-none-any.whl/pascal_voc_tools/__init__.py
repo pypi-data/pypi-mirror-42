7590bd3eebbfd069353ada05bd37fc3d0bd20f22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:22 +0000

    Add package pascal-voc-tools/0.1.8/pascal_voc_tools-0.1.8-py3-none-any.whl

---
+++ packages/pascal-voc-tools/pascal_voc_tools-0.1.8-py3-none-any.whl/pascal_voc_tools/__init__.py
@@ -0,0 +1,11 @@
+from ._version import version
+from .xmlreader import XmlReader
+from .xmlwriter import XmlWriter
+from .resize import DatasetResize
+from .datasplit import DataSplit
+from .image_annotation_split import SplitImageAnnotation
+
+
+name = 'pascal_voc_tools'
+__version__ = version
+
