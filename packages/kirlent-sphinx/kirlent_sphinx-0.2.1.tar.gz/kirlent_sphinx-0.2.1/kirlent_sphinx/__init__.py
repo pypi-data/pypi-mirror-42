1d8f3c9c047e509bb43a7001425d2ece3e68935e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:05 +0000

    Add package kirlent-sphinx/0.2.1/kirlent_sphinx-0.2.1.tar.gz

---
+++ packages/kirlent-sphinx/kirlent_sphinx-0.2.1.tar.gz/kirlent_sphinx-0.2.1/kirlent_sphinx/__init__.py
@@ -0,0 +1,35 @@
+# Copyright (C) 2019 H. Turgut Uyar <uyar@tekir.org>
+
+from pathlib import Path
+
+from . import slides, tables
+
+
+__version__ = "0.2.1"  # sig: str
+
+
+_template_path = Path(Path(__file__).parent, "templates")
+
+
+def get_path():
+    """Get the base path for templates.
+
+    :sig: () -> str
+    :return: Base path for the templates of this extension.
+    """
+    return str(_template_path)
+
+
+def setup(app):
+    """Register this extension with Sphinx.
+
+    This will add the directives and themes of this extension to Sphinx.
+
+    :sig: (sphinx.application.Sphinx) -> Dict[str, str]
+    :param app: Sphinx application to register this extension with.
+    :return: Information about this extension.
+    """
+    tables.setup(app)
+    slides.setup(app)
+    app.add_html_theme("kirlent", str(Path(_template_path, "kirlent")))
+    return {"version": __version__}
