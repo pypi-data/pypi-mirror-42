bc26643b53e716880e8a05cd3b29a74fb036ae42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:17 +0000

    Add package python-picard/0.4/python-picard-0.4.tar.gz

---
+++ packages/python-picard/python-picard-0.4.tar.gz/python-picard-0.4/picard/__init__.py
@@ -0,0 +1,22 @@
+"""PICARD software for ICA."""
+
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+# Generic release markers:
+#   X.Y
+#   X.Y.Z   # For bugfix releases
+#
+# Admissible pre-release markers:
+#   X.YaN   # Alpha release
+#   X.YbN   # Beta release
+#   X.YrcN  # Release Candidate
+#   X.Y     # Final release
+#
+# Dev branch marker is: 'X.Y.devN' where N is an integer.
+#
+
+__version__ = '0.4'
+
+from .solver import picard  # noqa
+from ._tools import permute  # noqa
