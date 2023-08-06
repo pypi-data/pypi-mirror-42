251ce0486178b1f575f6f9c6b10360468fb4facd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:24 +0000

    Add package OApackage/2.6.2/OApackage-2.6.2-cp35-cp35m-win32.whl

---
+++ packages/OApackage/OApackage-2.6.2-cp35-cp35m-win32.whl/oapackage/__init__.py
@@ -0,0 +1,27 @@
+""" Orthogonal Array package
+
+The Orthogonal Array package is a package to generate and analyse orthogonal
+arrays, optimal designs and conference matrices. For more information see
+
+http://github.com/eendebakpt/oapackage
+
+"""
+
+import oalib
+import oapackage.Doptim
+from . import conference
+from oalib import *
+from oalib import arraydata_t, array_link, exampleArray, ParetoDoubleLong, reduceOAnauty, arraylink2arraydata, reduceGraphNauty, transformGraphMatrix
+
+oapackage.oalib.setloglevel(oapackage.oalib.SYSTEM)
+oapackage.oalib.log_print(-oapackage.oalib.SYSTEM, '')
+__version__ = oapackage.oalib.version()
+
+from . oahelper import *
+from . Doptim import *
+from . import scanf
+
+
+__description__ = "Orthogonal Array package"
+__uri__ = "http://www.pietereendebak.nl/oapackage/index.html"
+#__doc__ = __description__ + " <" + __uri__ + ">"
