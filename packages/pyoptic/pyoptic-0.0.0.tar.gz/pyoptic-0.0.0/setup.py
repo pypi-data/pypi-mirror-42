988218377d44c9af3392641ad75a835feaa8de49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:00 +0000

    Add package pyoptic/0.0.0/pyoptic-0.0.0.tar.gz

---
+++ packages/pyoptic/pyoptic-0.0.0.tar.gz/pyoptic-0.0.0/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='pyoptic',  
+    version='0.0.0',
+    author="Tim Cogan",
+    author_email="timothycogan@gmail.com",
+    packages=setuptools.find_packages(),
+)
