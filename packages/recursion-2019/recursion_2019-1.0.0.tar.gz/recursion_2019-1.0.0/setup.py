548eb8693808bcdd1677d1886d01d4e003ba9ff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:24 +0000

    Add package recursion-2019/1.0.0/recursion_2019-1.0.0.tar.gz

---
+++ packages/recursion-2019/recursion_2019-1.0.0.tar.gz/recursion_2019-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = 'recursion_2019',
+    version = '1.0.0',
+    py_modules = ['recursion_2019'],
+    author = 'Yoo',
+    authour_email = 'doughngol@gmail.com',
+    url = 'http://www.headfirstlabs.com',
+    description ='A simple printer of nested lists',
+    )
