b1196ae1e961cc038a071f8c72477d68bbd043f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:49 +0000

    Add package openweathermap-cli/1.1.1/openweathermap_cli-1.1.1-py2.py3-none-any.whl

---
+++ packages/openweathermap-cli/openweathermap_cli-1.1.1-py2.py3-none-any.whl/weather/commands/today.py
@@ -0,0 +1,54 @@
+"""The today command."""
+
+import pyowm
+
+import time
+
+import requests
+
+import json
+
+from .base import Base
+
+import os, configparser
+
+from tabulate import tabulate
+
+from weather.user_settings import UserSettings
+
+class Today(Base):
+
+    def __init__(self, lat, lon, *args, **kwargs):
+        self.lat = lat
+        self.lon = lon
+
+        super(Today,self).__init__(*args, **kwargs)
+    
+    def run(self):
+
+        user_settings = UserSettings()
+        api_key = user_settings.get_api_key()
+
+        owm = pyowm.OWM(api_key)
+
+        observation = owm.weather_at_coords(self.lat,self.lon)
+        w = observation.get_weather()
+
+        location = observation.get_location()
+
+        table = []
+        table.append(["Temperature",str(w.get_temperature('celsius')['temp'])])
+        table.append(["Temp Max",str(w.get_temperature('celsius')['temp_max'])])
+        table.append(["Temp Min",str(w.get_temperature('celsius')['temp_min'])])
+        table.append(["Summary",w.get_status()])
+        table.append(["Detail", w.get_detailed_status()])
+        table.append(["Sunrise", time.strftime('%H:%M:%S', time.localtime(w.get_sunrise_time()))])
+        table.append(["Sunset", time.strftime('%H:%M:%S', time.localtime(w.get_sunset_time()))])
+        
+        headers = [location.get_name(),'']
+        print(tabulate(table, headers, tablefmt="psql"))
+
+
+
+        
+ 
