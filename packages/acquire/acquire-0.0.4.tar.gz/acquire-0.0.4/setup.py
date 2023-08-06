dba816ee90fffd62777de312e252734ce8182fb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:07 +0000

    Add package acquire/0.0.4/acquire-0.0.4.tar.gz

---
+++ packages/acquire/acquire-0.0.4.tar.gz/acquire-0.0.4/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="acquire",
+    version="0.0.4",
+    python_requires='>=3.6.0',
+    author="Christopher Woods",
+    author_email="chryswoods@gmail.com",
+    description="A serverless identity, access, accounting, storage and compute management system",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chryswoods/acquire",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License  ",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "pyotp>=2",
+        "cachetools>=3",
+        "tblib>=1.2",
+        "cryptography>=2",
+        "pyyaml>=3.0",
+        "requests>=2.10"
+        # note that qrcode is an optional dependency
+    ]
+)
