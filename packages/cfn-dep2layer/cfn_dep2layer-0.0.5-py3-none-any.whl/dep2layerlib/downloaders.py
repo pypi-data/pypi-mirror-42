ea8e3539ecc836d179ac7966041f81c6b3770f9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:05 +0000

    Add package cfn-dep2layer/0.0.5/cfn_dep2layer-0.0.5-py3-none-any.whl

---
+++ packages/cfn-dep2layer/cfn_dep2layer-0.0.5-py3-none-any.whl/dep2layerlib/downloaders.py
@@ -0,0 +1,49 @@
+import subprocess
+import hashlib
+import os
+
+class PackagerBase:
+  image = None
+  rundir = None # Need "run.sh" in this dir
+  prefix = None
+  
+  def __init__(self, resource, basedir):
+    self.resource = resource
+    self.basedir = basedir
+    self.hash = None
+  
+  def getdeplist(self):
+    pass
+    
+  def gethash(self):
+    if self.hash is None:
+      sha = hashlib.sha256()
+      sha.update('|'.join(self.getdeplist()).encode('utf8'))
+      self.hash = sha.hexdigest()
+    return self.hash
+  
+  def package(self, tmpdir):
+    args = [ 'docker', 'run', '--rm', '--entrypoint', '', \
+      '-v', '{}:/var/task/packager:ro'.format(os.path.abspath(self.rundir)), \
+      '-v', '{}:/var/task/src:ro'.format(os.path.join(self.basedir, self.resource['Properties']['CodeUri'])), \
+      '-v', '{}:/tmp'.format(tmpdir), \
+      self.image, 'bash', '/var/task/packager/run.sh']
+    
+    print(args)
+    proc = subprocess.Popen(args)
+    proc.wait()
+    
+    return proc.returncode == 0
+
+class Python37Packager(PackagerBase):
+  image = 'lambci/lambda:python3.7'
+  rundir = 'python3'
+  prefix = 'Python37'
+  
+  def getdeplist(self):
+    requirementpath = os.path.join(self.basedir, self.resource['Properties']['CodeUri'], 'requirements.txt')
+    return [i.strip() for i in open(requirementpath).readlines()]
+
+cls = {
+  'python3.7': Python37Packager
+}
