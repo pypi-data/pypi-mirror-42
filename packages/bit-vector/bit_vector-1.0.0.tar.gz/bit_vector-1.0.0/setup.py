db40f9aaa9612ea90ebc26ab0633097c0e08b2e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:04 +0000

    Add package bit-vector/1.0.0/bit_vector-1.0.0.tar.gz

---
+++ packages/bit-vector/bit_vector-1.0.0.tar.gz/bit_vector-1.0.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+# from pip.req import parse_requirements
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+
+# install_requires = []
+# extra_requires = {}
+# for item in parse_requirements("requirements.txt", session=False):
+#     req = str(item.req)
+#     if item.markers is not None:
+#         req += ";" + str(item.markers)
+#     install_requires.append(req)
+
+setup(
+    name='bit_vector',
+    url='https://github.com/leonardt/bit_vector',
+    author='Leonard Truong',
+    author_email='lenny@cs.stanford.edu',
+    version='1.0.0',
+    description='A BitVector class for Python',
+    scripts=[],
+    packages=[
+        "bit_vector",
+    ],
+    install_requires=['numpy'],
+    # python_requires='>=3.6'
+)
