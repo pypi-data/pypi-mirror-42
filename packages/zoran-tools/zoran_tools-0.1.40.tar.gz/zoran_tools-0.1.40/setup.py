8ac9d7ed640127ddc2b7f3492c1d2adb5554adf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:32 +0000

    Add package zoran-tools/0.1.40/zoran_tools-0.1.40.tar.gz

---
+++ packages/zoran-tools/zoran_tools-0.1.40.tar.gz/zoran_tools-0.1.40/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+
+setup(
+    name='zoran_tools',
+    version='0.1.40',
+    author='bkiu',
+    author_email='chenzhongrun@bonc.com.cn',
+    description='自用的一些小工具,包括列出文件夹下的文件, 读写CSV等',
+    packages=['zoran_tools'],
+)
