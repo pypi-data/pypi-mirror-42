e19d14e93f47fec7dae285e0d7436eb78a254859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:12 +0000

    Add package dvc/0.30.0/dvc-0.30.0-py2.py3-none-any.whl

---
+++ packages/dvc/dvc-0.30.0-py2.py3-none-any.whl/tests/utils/__init__.py
@@ -0,0 +1,24 @@
+import os
+from mock import MagicMock
+from contextlib import contextmanager
+
+
+def spy(method_to_decorate):
+    mock = MagicMock()
+
+    def wrapper(self, *args, **kwargs):
+        mock(*args, **kwargs)
+        return method_to_decorate(self, *args, **kwargs)
+
+    wrapper.mock = mock
+    return wrapper
+
+
+@contextmanager
+def cd(newdir):
+    prevdir = os.getcwd()
+    os.chdir(os.path.expanduser(newdir))
+    try:
+        yield
+    finally:
+        os.chdir(prevdir)
