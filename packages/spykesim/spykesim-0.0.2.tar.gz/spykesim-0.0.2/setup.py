62acf7224139bf0916c926d9c771f2da152b1544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:35 +0000

    Add package spykesim/0.0.2/spykesim-0.0.2.tar.gz

---
+++ packages/spykesim/spykesim-0.0.2.tar.gz/spykesim-0.0.2/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='spykesim',
+    version='0.0.2',
+    description='Python module that offers functions for measuring the similarity between two segmented multi-neuronal spiking activities.',
+    long_description=readme,
+    author='Keita Watanabe',
+    author_email='keitaw09@gmail.com',
+    install_requirements=['numpy', 'cython'],
+    url='https://github.com/KeitaW/spikesim',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
