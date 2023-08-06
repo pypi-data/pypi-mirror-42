2c60a622a623262bf39d402ddae51b8f71a5f53b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:39 +0000

    Add package toolofsamma/1.0/toolofsamma-1.0.tar.gz

---
+++ packages/toolofsamma/toolofsamma-1.0.tar.gz/toolofsamma-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='toolofsamma',
+    version='1.0',
+    packages=['mydecorate'],
+    url='',
+    license='',
+    author='samma',
+    author_email='1570858572@qq.com',
+    description='the private tool of samma'
+)
