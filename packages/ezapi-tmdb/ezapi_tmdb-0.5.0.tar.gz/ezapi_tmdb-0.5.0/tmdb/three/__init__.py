aa92352c71e111b8b4a9914d94efd3f2a6bcff08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:24 +0000

    Add package ezapi-tmdb/0.5.0/ezapi_tmdb-0.5.0.tar.gz

---
+++ packages/ezapi-tmdb/ezapi_tmdb-0.5.0.tar.gz/ezapi_tmdb-0.5.0/tmdb/three/__init__.py
@@ -0,0 +1,64 @@
+import requests
+
+from .account import AccountMixin
+from .authentication import AuthenticationMixin
+from .certifications import CertificationsMixin
+from .changes import ChangesMixin
+from .collections import CollectionsMixin
+from .companies import CompaniesMixin
+from .configuration import ConfigurationMixin
+from .credits import CreditsMixin
+from .discover import DiscoverMixin
+from .find import FindMixin
+from .genres import GenresMixin
+from .guest_sessions import GuestSessionsMixin
+from .keywords import KeywordsMixin
+from .lists import ListsMixin
+from .movies import MoviesMixin
+from .networks import NetworksMixin
+from .people import PeopleMixin
+from .search import SearchMixin
+from .reviews import ReviewsMixin
+from .trending import TrendingMixin
+from .tv import TVsMixin
+from .tv_episodes import TVEpisodesMixin
+from .tv_episode_groups import TVEpisodeGroupsMixin
+from .tv_seasons import TVSeasonsMixin
+
+mixins = [
+    AccountMixin,
+    AuthenticationMixin,
+    CertificationsMixin,
+    ChangesMixin,
+    CollectionsMixin,
+    CompaniesMixin,
+    ConfigurationMixin,
+    CreditsMixin,
+    DiscoverMixin,
+    FindMixin,
+    GenresMixin,
+    GuestSessionsMixin,
+    KeywordsMixin,
+    ListsMixin,
+    MoviesMixin,
+    NetworksMixin,
+    PeopleMixin,
+    ReviewsMixin,
+    SearchMixin,
+    TrendingMixin,
+    TVEpisodeGroupsMixin,
+    TVEpisodesMixin,
+    TVSeasonsMixin,
+    TVsMixin,
+]
+
+
+class TMDb(*mixins):
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.session = requests.Session()
+        self.session.headers.update({"Content-Type": "application/json;charset=utf-8"})
+
+    def make_request(self, method, url, params, payload=None):
+        params.update({"api_key": self.api_key})
+        return self.session.request(method, url, params=params, json=payload)
