6031ce2a6e8da4d11c2d5350224792cc85821cf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:32 +0000

    Add package phytools/0.1.0/phytools-0.1.0.tar.gz

---
+++ packages/phytools/phytools-0.1.0.tar.gz/phytools-0.1.0/README.md
@@ -0,0 +1,12 @@
+# phytools
+Useful tools for physicists
+
+## Installation
+Download or clone repository and:
+````
+python setup.py install
+````
+Or via pip:
+````
+pip install https://github.com/kricki/phytools/archive/master.zip
+````
---
+++ packages/phytools/phytools-0.1.0.tar.gz/phytools-0.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md') as fp:
+    long_description = fp.read()
+
+CLASSIFIERS = """
+Development Status :: 3 - Alpha
+Intended Audience :: Science/Research
+Intended Audience :: Developers
+Programming Language :: Python :: 3.6
+Topic :: Software Development
+Topic :: Scientific/Engineering
+"""
+
+setup(
+    name='phytools',
+    version='0.1.0',
+    author='',
+    author_email='euphiment@gmx.de',
+    url='https://github.com/kricki/phytools',
+    description='Tools for physicists',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    classifiers=[f for f in CLASSIFIERS.split('\n') if f],
+    install_requires=['numpy', 'scipy']
+)
