93840ae5ad2e1dc8f4780cb70c123213c6705891
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:52 +0000

    Add package spell/0.24.6/spell-0.24.6-py2.py3-none-any.whl

---
+++ packages/spell/spell-0.24.6-py2.py3-none-any.whl/spell/cli/constants.py
@@ -0,0 +1,27 @@
+BLACKLISTED_FILES = {
+    ".DS_Store"
+}
+
+WHITELISTED_FILEEXTS = {
+    "ipynb",
+}
+
+DEFAULT_SUPPORTED_OPTIONS = {
+    "frameworks": {
+        "values": [
+            "caffe",
+            "caffe2",
+            "mxnet",
+            "pytorch",
+            "tensorflow",
+        ],
+    },
+    "machine_types": {
+        "values": [
+            "CPU",
+            "K80",
+            "V100",
+        ],
+        "default": "CPU",
+    },
+}
