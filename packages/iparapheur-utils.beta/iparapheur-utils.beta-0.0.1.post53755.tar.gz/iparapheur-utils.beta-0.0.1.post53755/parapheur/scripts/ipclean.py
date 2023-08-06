56c9e3115aba812797b68b7d89a1f2a3d0a78b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:34 +0000

    Add package iparapheur-utils.beta/0.0.1.post53755/iparapheur-utils.beta-0.0.1.post53755.tar.gz

---
+++ packages/iparapheur-utils.beta/iparapheur-utils.beta-0.0.1.post53755.tar.gz/iparapheur-utils.beta-0.0.1.post53755/parapheur/scripts/ipclean.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from parapheur.parapheur import config
+import os
+from os import path as os_path
+
+PATH = os_path.abspath(os_path.split(__file__)[0])
+script = PATH + "/script/ipclean.sh "
+
+url = config.get("Ipclean", "url")
+port = config.get("Ipclean", "port")
+name = config.get("Ipclean", "name")
+password = config.get("Ipclean", "password")
+
+if url != "":
+    script = "-s " + url + " "
+
+if port != "":
+    script += "-P " + port + " "
+
+if name != "":
+    script += "-u " + name + " "
+
+if password != "":
+    script += "-p " + password + " "
+
+os.chmod(script, 0755)
+os.system(script)
