87af3dcf5722eac716cc1a71ae056f5899f14444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:30 +0000

    Add package writeasapi/0.1.4/writeasapi-0.1.4.tar.gz

---
+++ packages/writeasapi/writeasapi-0.1.4.tar.gz/writeasapi-0.1.4/uri.py
@@ -0,0 +1,7 @@
+import requests
+import json
+
+URI = 'https://write.as/api/auth/login'
+POST_URI = 'https://write.as/api/posts'
+COLL_URI = 'https://write.as/api/collections'
+ME_URI = 'https://write.as/api/me'
