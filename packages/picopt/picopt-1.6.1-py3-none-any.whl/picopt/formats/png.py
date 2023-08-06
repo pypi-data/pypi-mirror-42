47dced3a8efd47fdcb610ab9f77b9d2a41f3f6b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:52 +0000

    Add package picopt/1.6.1/picopt-1.6.1-py3-none-any.whl

---
+++ packages/picopt/picopt-1.6.1-py3-none-any.whl/picopt/formats/png.py
@@ -0,0 +1,39 @@
+"""PNG format."""
+from __future__ import absolute_import, division, print_function
+
+from .. import extern
+
+_PNG_FORMAT = 'PNG'
+FORMATS = set([_PNG_FORMAT])
+LOSSLESS_FORMATS = set(('PNM', 'PPM', 'BMP', 'GIF'))
+CONVERTABLE_FORMATS = LOSSLESS_FORMATS | FORMATS
+OUT_EXT = '.'+_PNG_FORMAT.lower()
+
+_OPTIPNG_ARGS = ['optipng', '-o6', '-fix', '-preserve', '-force', '-quiet']
+_ADVPNG_ARGS = ['advpng', '-z', '-4', '-f']
+_PNGOUT_ARGS = ['pngout', '-q', '-force', '-y']
+
+
+def optipng(ext_args):
+    """Run the external program optipng on the file."""
+    args = _OPTIPNG_ARGS + [ext_args.new_filename]
+    extern.run_ext(args)
+    return _PNG_FORMAT
+
+
+def advpng(ext_args):
+    """Run the external program advpng on the file."""
+    args = _ADVPNG_ARGS + [ext_args.new_filename]
+    extern.run_ext(args)
+    return _PNG_FORMAT
+
+
+def pngout(ext_args):
+    """Run the external program pngout on the file."""
+    args = _PNGOUT_ARGS + [ext_args.old_filename, ext_args.new_filename]
+    extern.run_ext(args)
+    return _PNG_FORMAT
+
+
+PROGRAMS = (optipng, advpng, pngout)
+BEST_ONLY = False
