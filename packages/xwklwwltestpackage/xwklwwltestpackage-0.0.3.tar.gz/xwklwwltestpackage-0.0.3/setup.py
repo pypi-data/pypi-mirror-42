823c51cfdbde5426c746b9a7ad3be426a474b1c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:38 +0000

    Add package xwklwwltestpackage/0.0.3/xwklwwltestpackage-0.0.3.tar.gz

---
+++ packages/xwklwwltestpackage/xwklwwltestpackage-0.0.3.tar.gz/xwklwwltestpackage-0.0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='xwklwwltestpackage',
+    license='Apache License 2.0',
+    version='0.0.3',
+    # packages=['testpackage'],
+    packages=find_packages(where='.', exclude=(), include=('*',)),
+    zip_safe=False,
+    include_package_data=True,
+    url='https://github.com/xwklwwlf/testpackage',
+    author='xwklwwlf',
+    author_email='xwklwwlf@gmail.com',
+    description='test',
+    install_requires=[],
+    )
