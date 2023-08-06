4977a4eb140fa93854c6b382592ab6bc2e8f6b7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:18 +0000

    Add package legalabs-scrapy-libs/0.0.18/legalabs_scrapy_libs-0.0.18.tar.gz

---
+++ packages/legalabs-scrapy-libs/legalabs_scrapy_libs-0.0.18.tar.gz/legalabs_scrapy_libs-0.0.18/legalabs_scrapy_libs/__init__.py
@@ -0,0 +1,15 @@
+# This package will contain the spiders of your Scrapy project
+#
+# Please refer to the documentation for information on how to create and manage
+# your spiders.
+
+import stopwords
+import settings
+import create_elastic
+import utils
+import validation_functions
+import items
+import legallabs_spyder
+import validator
+import pipelines
+import middlewares
