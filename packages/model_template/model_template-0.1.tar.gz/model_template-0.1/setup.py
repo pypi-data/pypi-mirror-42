48f2655f3a3e55fcd7fd2bd90d93e7f2685f77da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:19 +0000

    Add package model_template/0.1/model_template-0.1.tar.gz

---
+++ packages/model_template/model_template-0.1.tar.gz/model_template-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='model_template',
+      version='0.1',
+      description='A abstract Template class for running multiples machine \
+                   learning models in the same system.',
+      url='https://gitlab.com/bumbleblo/modeltemplate',
+      author='Felipe Borges',
+      author_email='bumbleblo2013@gmail.com',
+      license='MIT',
+      packages=['model_template'],
+      zip_safe=False)
+    
