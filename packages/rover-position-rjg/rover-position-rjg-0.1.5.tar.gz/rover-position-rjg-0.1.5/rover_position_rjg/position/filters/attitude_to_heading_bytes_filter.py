d2387a1c13ba71154b886279857f701633c96485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:58 +0000

    Add package rover-position-rjg/0.1.5/rover-position-rjg-0.1.5.tar.gz

---
+++ packages/rover-position-rjg/rover-position-rjg-0.1.5.tar.gz/rover-position-rjg-0.1.5/rover_position_rjg/position/filters/attitude_to_heading_bytes_filter.py
@@ -0,0 +1,16 @@
+import struct
+import time
+
+from rover_position_rjg.position.filters.attitude_filter import AttitudeOutput
+from rover_position_rjg.data.data import Data
+from rover_position_rjg.data.data_filter import DataFilter
+
+
+class AttitudeToHeadingBytesFilter(DataFilter[AttitudeOutput, bytes]):
+    def __init__(self, name: str = ''):
+        super().__init__(name)
+
+    def receive(self, data: Data[AttitudeOutput]) -> None:
+        a = data.value.attitude.to_tait_bryan()
+        packed = struct.pack(">ffff", a.x, a.y, a.z, float(data.timestamp) / 1e9)
+        self.send(Data(packed, data.timestamp))
