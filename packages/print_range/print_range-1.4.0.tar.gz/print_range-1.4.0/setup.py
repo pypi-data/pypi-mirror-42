807ce8f44808324ad1749b800ffb25601fc28140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:12 +0000

    Add package print_range/1.4.0/print_range-1.4.0.tar.gz

---
+++ packages/print_range/print_range-1.4.0.tar.gz/print_range-1.4.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name       = 'print_range',
+    version    = '1.4.0',
+    py_modules = ['print_range'],
+    author = 'sanku_py',
+    author_email = 'hfpython@headfirstlabs.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'a smiple print range loop program'
+    )
+    
