3bb5f7b381ae0106ce34a1171c5835aede450a24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:13 +0000

    Add package drf-dynamic-fields/0.3.1/drf_dynamic_fields-0.3.1.tar.gz

---
+++ packages/drf-dynamic-fields/drf_dynamic_fields-0.3.1.tar.gz/drf_dynamic_fields-0.3.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+readme = open('README.rst').read()
+
+setup(name='drf_dynamic_fields',
+      version='0.3.1',
+      description='Dynamically return subset of Django REST Framework serializer fields',
+      author='Danilo Bargen',
+      author_email='mail@dbrgn.ch',
+      url='https://github.com/dbrgn/drf-dynamic-fields',
+      packages=['drf_dynamic_fields'],
+      zip_safe=True,
+      include_package_data=True,
+      license='MIT',
+      keywords='drf restframework rest_framework django_rest_framework serializers',
+      long_description=readme,
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+          'Framework :: Django',
+          'Environment :: Web Environment',
+      ],
+)
