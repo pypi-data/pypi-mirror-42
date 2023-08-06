e1f74ffbc0f9fc4bd4a88ae25046b7929f5a5d52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:32 +0000

    Add package heiafr.isc.se.tpchecker/1.0.6/heiafr.isc.se.tpchecker-1.0.6.tar.gz

---
+++ packages/heiafr.isc.se.tpchecker/heiafr.isc.se.tpchecker-1.0.6.tar.gz/heiafr.isc.se.tpchecker-1.0.6/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='heiafr.isc.se.tpchecker',
+    description='Checker for Embedded System assignments',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Jacques Supcik',
+    author_email='jacques.supcik@hefr.ch',
+    url='https://gitlab.forge.hefr.ch/jacques.supcik/se12-checker',
+    py_modules=['check'],
+    packages=['heiafr.isc.se.tpchecker'],
+    include_package_data=True,
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    install_requires=[
+        'Click',
+        'jinja2',
+        'weasyprint',
+    ],
+    entry_points='''
+        [console_scripts]
+        heiafr-check-se=check:main
+    ''',
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Education",
+        "Topic :: Education",
+    ],
+)
