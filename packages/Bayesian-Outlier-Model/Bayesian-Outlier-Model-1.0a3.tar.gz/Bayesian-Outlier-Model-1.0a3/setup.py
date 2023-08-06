493dfa46cfbd62eaf7c701cb8bdebe4c9e036729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:52 +0000

    Add package Bayesian-Outlier-Model/1.0a3/Bayesian-Outlier-Model-1.0a3.tar.gz

---
+++ packages/Bayesian-Outlier-Model/Bayesian-Outlier-Model-1.0a3.tar.gz/Bayesian-Outlier-Model-1.0a3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(name='Bayesian-Outlier-Model',
+      version='1.0a3',
+      description='A Bayesian model for identifying outliers for N-of-1 samples in gene expression data',
+      url='https://github.com/jvivian/Bayesian-Outlier-Model',
+      author='John Vivian',
+      author_email='jtvivian@gmail.com',
+      license='MIT',
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      install_requires=[
+            'pymc3',
+            'pandas',
+            'tables',
+            'numpy',
+            'click',
+            'tqdm',
+            'matplotlib',
+            'scipy',
+            'seaborn',
+            'scikit-learn'
+      ],
+      entry_points='''
+            [console_scripts]
+            outlier-model=outlier_model.main:cli
+      ''',
+      )
