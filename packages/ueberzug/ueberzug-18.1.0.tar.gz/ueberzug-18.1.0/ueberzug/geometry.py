d7b93a76d472f88b9dac5e18c452d88daadc0caa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:31 +0000

    Add package ueberzug/18.1.0/ueberzug-18.1.0.tar.gz

---
+++ packages/ueberzug/ueberzug-18.1.0.tar.gz/ueberzug-18.1.0/ueberzug/geometry.py
@@ -0,0 +1,17 @@
+"""Module which defines classes all about geometry"""
+
+
+class Point:
+    """Data class which holds a coordinate."""
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Distance:
+    """Data class which holds the distance values in all directions."""
+    def __init__(self, top=0, left=0, bottom=0, right=0):
+        self.top = top
+        self.left = left
+        self.bottom = bottom
+        self.right = right
