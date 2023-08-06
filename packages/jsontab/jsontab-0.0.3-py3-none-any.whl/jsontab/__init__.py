f5dd1df8859a3b5122faee5fd57ab1b6a3a99a01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:28 +0000

    Add package jsontab/0.0.3/jsontab-0.0.3-py3-none-any.whl

---
+++ packages/jsontab/jsontab-0.0.3-py3-none-any.whl/jsontab/__init__.py
@@ -0,0 +1,10 @@
+"""Agnostically load JSON or tabular data
+
+Functions
+---------
+from_json_or_tab
+    load a pandas DataFrame from data that may be in either JSON or tabular
+    format
+"""
+
+from jsontab.jsontab import from_json_or_tab
