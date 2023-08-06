b16082e8eb2f99bc1584bacf54b21e8a06ebaded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:00 +0000

    Add package cockroachdb/0.3.1/cockroachdb-0.3.1.tar.gz

---
+++ packages/cockroachdb/cockroachdb-0.3.1.tar.gz/cockroachdb-0.3.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+install_requires = [
+    'psycopg2',
+]
+
+setup(
+    name='cockroachdb',
+    version='0.3.1',
+    author='Cockroach Labs',
+    author_email='cockroach-db@googlegroups.com',
+    url='https://github.com/cockroachdb/cockroachdb-python',
+    description='CockroachDB adapter for SQLAlchemy',
+    license="http://www.apache.org/licenses/LICENSE-2.0",
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        ],
+
+    packages=['cockroachdb', 'cockroachdb.sqlalchemy'],
+    install_requires=install_requires,
+    entry_points={
+        'sqlalchemy.dialects': [
+            'cockroachdb = cockroachdb.sqlalchemy.dialect:CockroachDBDialect',
+        ],
+    },
+)
