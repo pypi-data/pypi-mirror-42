1ad1c3b88a759400517143ceff65900d781a3c8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:47 +0000

    Add package listfiles/1.0.0/listfiles-1.0.0.tar.gz

---
+++ packages/listfiles/listfiles-1.0.0.tar.gz/listfiles-1.0.0/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='listfiles',
+    version='1.0.0',
+    description='A list file python project',
+    url='',
+    author='dong jian',
+    author_email='',
+    license='MIT',
+    classifiers=[
+    ],
+    keywords='list file python',
+    packages=['list'],
+)
+
