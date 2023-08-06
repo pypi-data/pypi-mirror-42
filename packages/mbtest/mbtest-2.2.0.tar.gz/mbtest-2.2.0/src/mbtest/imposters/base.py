551f40593f614ac2ea5d5be28cacc5e72c589f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:25 +0000

    Add package mbtest/2.2.0/mbtest-2.2.0.tar.gz

---
+++ packages/mbtest/mbtest-2.2.0.tar.gz/mbtest-2.2.0/src/mbtest/imposters/base.py
@@ -0,0 +1,21 @@
+# encoding=utf-8
+from abc import ABCMeta, abstractmethod
+
+
+class JsonSerializable(metaclass=ABCMeta):
+    @abstractmethod
+    def as_structure(self):  # pragma: no cover
+        """
+        :returns Structure suitable for JSON serialisation.
+        :rtype: dict
+        """
+        raise NotImplementedError()
+
+    @staticmethod
+    def _add_if_true(dictionary, key, value):
+        if value:
+            dictionary[key] = value
+
+    def _set_if_in_dict(self, dictionary, key, name):
+        if key in dictionary:
+            setattr(self, name, dictionary[key])
