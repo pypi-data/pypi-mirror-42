0ef2d41de078f4e9720bf7bfbf67161b4228ee0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:08 +0000

    Add package phone-db/1.0.0/phone-db-1.0.0.tar.gz

---
+++ packages/phone-db/phone-db-1.0.0.tar.gz/phone-db-1.0.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    README = f.read()
+
+NAME = 'phone-db'
+DESCRIPTION = '手机号码SQLite数据库'
+AUTHOR = 'lalala223'
+AUTHOR_EMAIL = 'hi@lalala.ink'
+URL = 'https://github.com/lalala223/phone-db'
+VERSION = '1.0.0'
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=README,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license='MIT',
+    url=URL,
+    include_package_data=True,
+    install_requires=['sqlalchemy'],
+    packages=find_packages(),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    zip_safe=False,
+)
