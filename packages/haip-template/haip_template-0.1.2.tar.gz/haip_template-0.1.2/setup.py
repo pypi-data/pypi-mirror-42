80321000a9bfa3b687151e9adc0c528fbe1c1e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:02 +0000

    Add package haip-template/0.1.2/haip_template-0.1.2.tar.gz

---
+++ packages/haip-template/haip_template-0.1.2.tar.gz/haip_template-0.1.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("Readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="haip_template",
+    version="0.1.2",
+    author="Reinhard Hainz",
+    author_email="reinhard.hainz@gmail.com",
+    description="Jinja2 based template module.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/haipdev/template",
+    packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "haip_config>=0.1.6",
+        "jinja2>=2.10"
+    ]
+)
