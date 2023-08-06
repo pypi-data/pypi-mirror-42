c3272c2270f7e0fce02b20b8d072b1e73a1235b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:54 +0000

    Add package NanostructureUCPH/0.0.7/NanostructureUCPH-0.0.7.tar.gz

---
+++ packages/NanostructureUCPH/NanostructureUCPH-0.0.7.tar.gz/NanostructureUCPH-0.0.7/NanostructureUCPH/PearsonFitting/__init__.py
@@ -0,0 +1,4 @@
+from .pearsonFitting import pearsonFitting
+from .pearsonFitting import create_array
+from .pearsonFitting import scipyOptimize
+from .pearsonFitting import plotRecipe
---
+++ packages/NanostructureUCPH/NanostructureUCPH-0.0.7.tar.gz/NanostructureUCPH-0.0.7/NanostructureUCPH/PearsonFitting/test.py
@@ -0,0 +1,5 @@
+var = 7
+
+print "{:>5}".format('hej')
+
+print "{0:>{1}.{1}}".format('hej',var)
