4c49ce8489bf92f1134a63674e86881df483b692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:46 +0000

    Add package sqlalchemy-mate/0.0.5/sqlalchemy_mate-0.0.5-py2.py3-none-any.whl

---
+++ packages/sqlalchemy-mate/sqlalchemy_mate-0.0.5-py2.py3-none-any.whl/sqlalchemy_mate/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from ._version import __version__
+
+__short_description__ = "A library extend sqlalchemy module, makes CRUD easier."
+__license__ = "MIT"
+
+try:
+    from . import engine_creator, io, pt
+    from .credential import Credential, EngineCreator
+    from .crud import selecting, inserting, updating
+    from .orm.extended_declarative_base import ExtendedBase
+except:  # pragma: no cover
+    pass
