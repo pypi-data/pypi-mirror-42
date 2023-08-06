3c872b816dbf2f23deaf8a827f871c623626d9d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:14 +0000

    Add package django-scim2/0.8.2/django-scim2-0.8.2.tar.gz

---
+++ packages/django-scim2/django-scim2-0.8.2.tar.gz/django-scim2-0.8.2/django_scim/tests/test_adapters.py
@@ -0,0 +1,266 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase, RequestFactory
+
+from django_scim import constants
+from django_scim.adapters import SCIMMixin
+from django_scim.utils import get_user_adapter
+from django_scim.utils import get_group_adapter
+from django_scim.utils import get_group_model
+
+
+class SCIMUserTestCase(TestCase):
+    maxDiff = None
+    request = RequestFactory().get('/fake/request')
+
+    def test_display_name(self):
+        ford = get_user_model().objects.create(
+            first_name='Robert',
+            last_name='Ford',
+            username='rford',
+        )
+        ford = get_user_adapter()(ford, self.request)
+
+        self.assertEqual(ford.display_name, 'Robert Ford')
+
+    def test_emails(self):
+        ford = get_user_model().objects.create(
+            first_name='Robert',
+            last_name='Ford',
+            username='rford',
+            email='rford@ww.com',
+        )
+        ford = get_user_adapter()(ford, self.request)
+
+        self.assertEqual(
+            ford.emails,
+            [{'primary': True, 'value': 'rford@ww.com'}]
+        )
+
+    def test_groups(self):
+        behavior = get_group_model().objects.create(
+            name='Behavior Group',
+        )
+        ford = get_user_model().objects.create(
+            first_name='Robert',
+            last_name='Ford',
+            username='rford',
+        )
+        ford.groups.add(behavior)
+        ford = get_user_adapter()(ford, self.request)
+
+        expected = [
+            {
+                'display': u'Behavior Group',
+                'value': '1',
+                '$ref': u'https://localhost/scim/v2/Groups/1',
+            }
+        ]
+
+        self.assertEqual(ford.groups, expected)
+
+    def test_meta(self):
+        ford = get_user_model().objects.create(
+            first_name='Robert',
+            last_name='Ford',
+            username='rford',
+            email='rford@ww.com',
+        )
+
+        expected = {
+            'resourceType': 'User',
+            'lastModified': ford.date_joined.isoformat(),
+            'location': u'https://localhost/scim/v2/Users/1',
+            'created': ford.date_joined.isoformat(),
+        }
+
+        ford = get_user_adapter()(ford, self.request)
+        self.assertEqual(ford.meta, expected)
+
+    def test_to_dict(self):
+        behavior = get_group_model().objects.create(
+            name='Behavior Group',
+        )
+        ford = get_user_model().objects.create(
+            first_name='Robert',
+            last_name='Ford',
+            username='rford',
+            email='rford@ww.com',
+        )
+        ford.groups.add(behavior)
+
+        expected = {
+            'schemas': [constants.SchemaURI.USER],
+            'userName': 'rford',
+            'meta': {
+                'resourceType': 'User',
+                'lastModified': ford.date_joined.isoformat(),
+                'location': u'https://localhost/scim/v2/Users/1',
+                'created': ford.date_joined.isoformat(),
+            },
+            'displayName': u'Robert Ford',
+            'name': {
+                'givenName': 'Robert',
+                'familyName': 'Ford',
+                'formatted': 'Robert Ford',
+            },
+            'groups': [
+                {
+                    'display': u'Behavior Group',
+                    'value': '1',
+                    '$ref': u'https://localhost/scim/v2/Groups/1'
+                }
+            ],
+            'active': True,
+            'id': '1',
+            'emails': [{'primary': True, 'value': 'rford@ww.com'}],
+        }
+
+        ford = get_user_adapter()(ford, self.request)
+        self.assertEqual(ford.to_dict(), expected)
+
+    def test_resource_type_dict(self):
+        ford = get_user_model().objects.create(
+            first_name='Robert',
+            last_name='Ford',
+            username='rford',
+            email='rford@ww.com',
+        )
+        ford = get_user_adapter()(ford, self.request)
+
+        expected = {
+            'endpoint': u'/scim/v2/Users',
+            'description': 'User Account',
+            'name': 'User',
+            'meta': {
+                'resourceType': 'ResourceType',
+                'location': u'https://localhost/scim/v2/ResourceTypes/User'
+            },
+            'schemas': [constants.SchemaURI.RESOURCE_TYPE],
+            'id': 'User',
+            'schema': constants.SchemaURI.USER,
+        }
+
+        self.assertEqual(ford.resource_type_dict(), expected)
+
+
+class SCIMMixinPathParserTestCase(TestCase):
+    maxDiff = None
+
+    def test_azure_ad_style_paths(self):
+        """
+        Test paths typically sent by AzureAD.
+        """
+        paths_and_values = [
+            ("addresses[type eq \"work\"].country", 1),
+            ("addresses[type eq \"work\"].locality", 1),
+            ("addresses[type eq \"work\"].postalCode", 1),
+            ("addresses[type eq \"work\"].streetAddress", 1),
+            ("externalId", 1),
+            ("urn:ietf:params:scim:schemas:extension:enterprise:2.0:User:department", 1),
+        ]
+
+        func = SCIMMixin(None).parse_path_and_value
+        result_paths = list(map(lambda x: func(*x), paths_and_values))
+        self.assertEqual(result_paths, paths_and_values)
+
+
+class SCIMGroupTestCase(TestCase):
+    request = RequestFactory().get('/fake/request')
+
+    def test_display_name(self):
+        behavior = get_group_model().objects.create(
+            name='Behavior Group',
+        )
+        behavior = get_group_adapter()(behavior, self.request)
+        self.assertEqual(behavior.display_name, 'Behavior Group')
+
+    def test_members(self):
+        behavior = get_group_model().objects.create(
+            name='Behavior Group',
+        )
+        ford = get_user_model().objects.create(
+            first_name='Robert',
+            last_name='Ford',
+            username='rford',
+            email='rford@ww.com',
+        )
+        ford.groups.add(behavior)
+
+        RequestFactory()
+        behavior = get_group_adapter()(behavior, self.request)
+
+        expected =  [
+            {
+                'display': u'Robert Ford',
+                'value': '1',
+                '$ref': u'https://localhost/scim/v2/Users/1'
+            }
+        ]
+
+        self.assertEqual(behavior.members, expected)
+
+    def test_meta(self):
+        behavior = get_group_model().objects.create(
+            name='Behavior Group',
+        )
+        behavior = get_group_adapter()(behavior, self.request)
+
+        expected = {
+            'resourceType': 'Group',
+            'location': u'https://localhost/scim/v2/Groups/1'
+        }
+
+        self.assertEqual(behavior.meta, expected)
+
+    def test_to_dict(self):
+        behavior = get_group_model().objects.create(
+            name='Behavior Group',
+        )
+        ford = get_user_model().objects.create(
+            first_name='Robert',
+            last_name='Ford',
+            username='rford',
+            email='rford@ww.com',
+        )
+        ford.groups.add(behavior)
+
+        expected = {
+            'meta': {
+                'resourceType': 'Group',
+                'location': u'https://localhost/scim/v2/Groups/1'
+            },
+            'displayName': 'Behavior Group',
+            'id': '1',
+            'members': [
+                {
+                    'display': u'Robert Ford',
+                    'value': '1',
+                    '$ref': u'https://localhost/scim/v2/Users/1'
+                }
+            ],
+            'schemas': [constants.SchemaURI.GROUP]
+        }
+
+        behavior = get_group_adapter()(behavior, self.request)
+        self.assertEqual(behavior.to_dict(), expected)
+
+    def test_resource_type_dict(self):
+        behavior = get_group_model().objects.create(
+            name='Behavior Group',
+        )
+        behavior = get_group_adapter()(behavior, self.request)
+
+        expected = {
+            'endpoint': u'/scim/v2/Groups',
+            'description': 'Group',
+            'name': 'Group',
+            'meta': {
+                'resourceType': 'ResourceType',
+                'location': u'https://localhost/scim/v2/ResourceTypes/Group'
+            },
+            'schemas': [constants.SchemaURI.RESOURCE_TYPE],
+            'id': 'Group',
+            'schema': constants.SchemaURI.GROUP
+        }
+
+        self.assertEqual(behavior.resource_type_dict(), expected)
---
+++ packages/django-scim2/django-scim2-0.8.2.tar.gz/django-scim2-0.8.2/django_scim/tests/test_middleware.py
@@ -0,0 +1,36 @@
+import six
+if six.PY3:
+    from unittest import mock
+else:
+    import mock
+from django.test import TestCase, RequestFactory
+from django.contrib.auth.models import AnonymousUser
+
+from django_scim.middleware import SCIMAuthCheckMiddleware
+
+
+class SCIMMiddlewareTestCase(TestCase):
+    def test_middleware_for_django2(self):
+        """
+        Regression test for https://github.com/15five/django-scim2/issues/13
+        """
+        middleware = SCIMAuthCheckMiddleware()
+
+        request = RequestFactory().get(middleware.reverse_url)
+        request.user = AnonymousUser()
+
+        response = middleware.process_request(request)
+        self.assertEqual(response.status_code, 401)
+
+    @mock.patch('django_scim.middleware.SCIMAuthCheckMiddleware.log_call')
+    def test_log_called_only_for_scim_calls(self, log_call_func):
+        middleware = SCIMAuthCheckMiddleware()
+
+        request = RequestFactory().get('/')
+        middleware.process_response(request, None)
+        log_call_func.assert_not_called()
+
+        request = RequestFactory().get(middleware.reverse_url)
+        middleware.process_response(request, None)
+        log_call_func.assert_called()
+
