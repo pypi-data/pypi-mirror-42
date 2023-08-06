de630f67e8699ca116652ee744e2eac930feb02b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:01 +0000

    Add package qcportal/0.5.0/qcportal-0.5.0-py3-none-any.whl

---
+++ packages/qcportal/qcportal-0.5.0-py3-none-any.whl/qcportal/__init__.py
@@ -0,0 +1,12 @@
+"""
+DQM Client base folder
+"""
+
+from . import collections
+from . import data
+from . import dict_utils
+from . import orm
+from .client import FractalClient
+
+# Add imports here
+from .models.common_models import Molecule
