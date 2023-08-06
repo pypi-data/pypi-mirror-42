244916ae7279b34ec009f63f7e45c7aac45cc1bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:44 +0000

    Add package filesender/1.2/filesender-1.2-py2-none-any.whl

---
+++ packages/filesender/filesender-1.2-py2-none-any.whl/filesender/copy_script/FTP.py
@@ -0,0 +1,17 @@
+from filesender.copy_script.Scripter import Scripter
+
+
+class FTP(Scripter):
+    cmd = [
+        'cd  <DEST_DIR> > <FTP_FILE>',
+        'echo open <URL> >> <FTP_FILE>',
+        'echo anonymous >> <FTP_FILE>',
+        'echo whatever >> <FTP_FILE>',
+        'echo binary >> <FTP_FILE>',
+        'echo get <FILE> >> <FTP_FILE>',
+        'echo bye >> <FTP_FILE>',
+        'ftp - s:<FTP_FILE>',
+    ]
+
+    def replace_personnal_tag(self, cmd):
+        return cmd.replace("<WFTP_FILE>", self.create_path_to_file(self.destination_directory, "ftp.txt"))
