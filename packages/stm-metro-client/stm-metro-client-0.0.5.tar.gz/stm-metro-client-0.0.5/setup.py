4803e9e084a6c35730d2d488c8ace8bbde35bc3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:47 +0000

    Add package stm-metro-client/0.0.5/stm-metro-client-0.0.5.tar.gz

---
+++ packages/stm-metro-client/stm-metro-client-0.0.5.tar.gz/stm-metro-client-0.0.5/setup.py
@@ -0,0 +1,18 @@
+"""Metadata for pypi uploads."""
+import setuptools
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name="stm-metro-client",
+    version="0.0.5",
+    author="Karim Roukoz",
+    author_email="roukoz@gmail.com",
+    description="A python client for Montreal's STM metro line status",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    keywords=['montreal', 'transit', 'metro', 'subway', 'quebec'],
+    url="https://github.com/kkr16/stm-metro-client",
+    packages=setuptools.find_packages(),
+)
