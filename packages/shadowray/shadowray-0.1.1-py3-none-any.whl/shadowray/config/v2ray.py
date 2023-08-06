46992aeb0cb7eeb68b98a93068191168cfd463c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:31 +0000

    Add package shadowray/0.1.1/shadowray-0.1.1-py3-none-any.whl

---
+++ packages/shadowray/shadowray-0.1.1-py3-none-any.whl/shadowray/config/v2ray.py
@@ -0,0 +1,16 @@
+import os
+
+PROJECT_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+V2RAY_FOLDER = os.path.join(PROJECT_PATH, "v2ray")
+V2RAY_BINARY = os.path.join(V2RAY_FOLDER, "v2ray")
+EXECUTE_ARGS = "-config=stdin:"
+
+RESOURCES_FOLDER = os.path.join(PROJECT_PATH, "resources")
+SUBSCRIBE_FILE = os.path.join(RESOURCES_FOLDER, "subscribes.json")
+SERVER_FILE = os.path.join(RESOURCES_FOLDER, "servers.json")
+
+PROJECT_CONFIG_FILE = os.path.join(PROJECT_PATH, "config", "config.json")
+
+SERVER_KEY_FROM_ORIGINAL = "servers_original"
+SERVER_KEY_FROM_SUBSCRIBE = "servers_subscribe"
