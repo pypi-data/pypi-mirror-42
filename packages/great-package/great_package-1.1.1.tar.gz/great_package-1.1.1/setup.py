310b2430256cd1767150d0aae02d0b49839234ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:18:18 +0000

    Add package great-package/1.1.1/great_package-1.1.1.tar.gz

---
+++ packages/great-package/great_package-1.1.1.tar.gz/great_package-1.1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='great_package',
+    version='1.1.1',
+    py_modules = ['fact', 'sum'],
+    author='bowei',
+    author_email='billboy0428@outlook.com',
+    url='',
+    description='test'
+)
