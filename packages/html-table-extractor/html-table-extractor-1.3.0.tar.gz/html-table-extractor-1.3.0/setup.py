8cb3539f6d3ee0c950cfce9adbbde1c65ef36702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:44 +0000

    Add package html-table-extractor/1.3.0/html-table-extractor-1.3.0.tar.gz

---
+++ packages/html-table-extractor/html-table-extractor-1.3.0.tar.gz/html-table-extractor-1.3.0/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""A tool to extract data from html table
+
+See:
+https://github.com/yuanxu-li/html-table-extractor
+"""
+
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name='html-table-extractor',
+    url='https://github.com/yuanxu-li/html-table-extractor',
+    version='1.3.0',
+    author='Justin Li',
+    author_email='yuanxu.lee@gmail.com',
+    description='A python library for extracting data from html table',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='html table beautifulsoup crawler scrape',
+    packages=[
+        'html_table_extractor',
+    ],
+    install_requires=[
+        'beautifulsoup4==4.5.3',
+    ],
+    long_description=long_description,
+)
