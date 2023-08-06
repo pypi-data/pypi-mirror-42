365799ab27045fdebca7fbed1ce35482ede1b30a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:21 +0000

    Add package metatype/0.0.2/metatype-0.0.2.tar.gz

---
+++ packages/metatype/metatype-0.0.2.tar.gz/metatype-0.0.2/metatype/mft1/__init__.py
@@ -0,0 +1,15 @@
+KEYMAP = {
+    'id': '-',
+    'type': '*',
+    'auth': '+',
+    'intent': '^'
+}
+
+class Dict(dict):
+
+    def __init__(self, *args, **kwargs):
+        self.update(*args, **kwargs)
+
+        for key in KEYMAP:
+            if KEYMAP[key] is not None:
+                setattr(self, key, self.get(KEYMAP[key]))
