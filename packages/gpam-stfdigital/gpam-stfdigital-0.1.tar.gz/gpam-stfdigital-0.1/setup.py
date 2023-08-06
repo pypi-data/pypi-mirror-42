f4929708e597340f2616fe18f1062115496d83f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:08 +0000

    Add package gpam-stfdigital/0.1/gpam-stfdigital-0.1.tar.gz

---
+++ packages/gpam-stfdigital/gpam-stfdigital-0.1.tar.gz/gpam-stfdigital-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup 
+
+setup(name='gpam-stfdigital',
+      version='0.1',
+      description='Client for integrate service with STF Digital',
+      url='https://supremotribunalfederal.gitlab.io/',
+      author='GPAM',
+      author_email='gpamunb@gmail.com',
+      license='MIT',
+      packages=[    
+                'gpam-stfdigital'
+                ],
+     zip_safe=False)
