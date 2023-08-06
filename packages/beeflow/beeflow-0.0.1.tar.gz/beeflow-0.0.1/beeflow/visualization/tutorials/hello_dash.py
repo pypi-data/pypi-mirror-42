538789428e4bf65482105542c386d76d7a2e440b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:35 +0000

    Add package beeflow/0.0.1/beeflow-0.0.1.tar.gz

---
+++ packages/beeflow/beeflow-0.0.1.tar.gz/beeflow-0.0.1/beeflow/visualization/tutorials/hello_dash.py
@@ -0,0 +1,58 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+
+app.layout = html.Div(
+    style={'backgroundColor': colors['background']},
+    children=[
+        html.H1(
+            children='Hello Dash',
+            style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }
+        ),
+
+        html.Div(
+            children='Dash: A web application framework for Python.',
+            style={
+                'textAlign': 'center',
+                'color': colors['text']}
+        ),
+
+        dcc.Graph(
+            id='example-graph',
+            figure={
+                'data': [
+                    {'x': [1, 2], 'y': [4, 1], 'type': 'bar', 'name': 'SF'},
+                    {'x': [1, 2], 'y': [2, 4], 'type': 'bar', 'name':
+                        u'Montréal'},
+                ],
+                'layout': {
+                    'title': 'Dash Data Visualization',
+                    'plot_bgcolor': colors['background'],
+                    'paper_bgcolor': colors['background'],
+                    'font': {
+                        'color': colors['text']
+                    }
+                }
+            }
+        )
+    ]
+)
+
+if __name__ == '__main__':
+    # Automated refreshing when code changes.
+    app.run_server(debug=True, port=8050)
+
+    # No automated refreshing.
+    # app.run_server(debug=False, dev_tools_hot_reload=False, port=8050)
