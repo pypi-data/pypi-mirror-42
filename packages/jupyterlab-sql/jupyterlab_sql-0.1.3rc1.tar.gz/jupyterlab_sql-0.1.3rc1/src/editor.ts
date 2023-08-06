aa5058e8417d933b8cd48e15e3248908d49b62c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:25 +0000

    Add package jupyterlab-sql/0.1.3rc1/jupyterlab_sql-0.1.3rc1.tar.gz

---
+++ packages/jupyterlab-sql/jupyterlab_sql-0.1.3rc1.tar.gz/jupyterlab_sql-0.1.3rc1/src/editor.ts
@@ -0,0 +1,32 @@
+import {
+  IEditorFactoryService,
+  CodeEditor,
+  CodeEditorWrapper
+} from '@jupyterlab/codeeditor';
+
+import { ISignal, Signal } from '@phosphor/signaling';
+
+export class Editor extends CodeEditorWrapper {
+  constructor(editorFactory: IEditorFactoryService) {
+    super({
+      model: new CodeEditor.Model(),
+      factory: editorFactory.newInlineEditor
+    });
+    this.editor.addKeydownHandler((_, evt) => this._onKeydown(evt));
+    this.addClass('p-Sql-Editor');
+  }
+
+  get executeRequest(): ISignal<this, string> {
+    return this._executeRequest;
+  }
+
+  _onKeydown(event: KeyboardEvent): boolean {
+    if (event.shiftKey && event.key === 'Enter') {
+      this._executeRequest.emit(this.model.value.text);
+      return true;
+    }
+    return false;
+  }
+
+  private _executeRequest = new Signal<this, string>(this);
+}
