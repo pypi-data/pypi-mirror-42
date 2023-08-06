6ff10fda5ae144c19dd6990f9aacc1ba140129e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:15 +0000

    Add package sigpy/0.0.9/sigpy-0.0.9.tar.gz

---
+++ packages/sigpy/sigpy-0.0.9.tar.gz/sigpy-0.0.9/setup.py
@@ -0,0 +1,31 @@
+import sys
+from setuptools import setup, find_packages
+
+if sys.version_info < (3, 5):
+    sys.exit('Sorry, Python < 3.5 is not supported')
+
+REQUIRED_PACKAGES = ['numpy', 'pywavelets', 'numba', 'tqdm']
+
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
+setup(name='sigpy',
+      version='0.0.9',
+      description='Python package for signal reconstruction.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='http://github.com/mikgroup/sigpy',
+      author='Frank Ong',
+      author_email='frankong@berkeley.edu',
+      license='BSD',
+      packages=find_packages(),
+      install_requires=REQUIRED_PACKAGES,
+      scripts=['bin/image_plot',
+               'bin/line_plot',
+               'bin/scatter_plot'],
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: BSD License",
+          "Operating System :: OS Independent",
+      ]
+)
