ce25f2a209b578649723cf860dedfaa12a6348a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:44 +0000

    Add package mdmpy/0.0.15.17/mdmpy-0.0.15.17.tar.gz

---
+++ packages/mdmpy/mdmpy-0.0.15.17.tar.gz/mdmpy-0.0.15.17/setup.py
@@ -0,0 +1,33 @@
+"""
+mdmpy is an implementation of Marginal Distribution Models (MDM) which can be
+applied to discrete choice modelling.
+"""
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mdmpy",
+    version="0.0.15.17",
+    author="mdmpy Authors",
+    author_email="mdmpyatgithub@gmail.com",
+    description="A package that implements Marginal Distribution Models (MDMs)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/justanothergithubber/mdmpy",
+    packages=setuptools.find_packages('src'),
+    package_dir={'':'src'},
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+    'pandas',
+    'numpy',
+    'scipy',
+    'pyomo'
+    ],
+)
