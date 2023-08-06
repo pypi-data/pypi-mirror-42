4f469dd53918839369ce78e40937af7e2f40bed8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:35 +0000

    Add package pawpyseed/0.4.0/pawpyseed-0.4.0.tar.gz

---
+++ packages/pawpyseed/pawpyseed-0.4.0.tar.gz/pawpyseed-0.4.0/docs/documentation/search/classes_5.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['nclpseudowavefunction',['NCLPseudoWavefunction',['../classpawpyseed_1_1core_1_1noncollinear_1_1NCLPseudoWavefunction.html',1,'pawpyseed::core::noncollinear']]],
+  ['nclwavefunction',['NCLWavefunction',['../classpawpyseed_1_1core_1_1noncollinear_1_1NCLWavefunction.html',1,'pawpyseed::core::noncollinear']]]
+];
---
+++ packages/pawpyseed/pawpyseed-0.4.0.tar.gz/pawpyseed-0.4.0/docs/documentation/search/functions_b.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['ncl_5frealspace_5fstate',['ncl_realspace_state',['../density_8h.html#a60d5417a86495000dea6e47b30f9394f',1,'density.h']]]
+];
