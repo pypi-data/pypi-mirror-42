718fe2fc0112637a15143c0d4bd762da1261c9fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:30 +0000

    Add package zdict/2.0.0/zdict-2.0.0.tar.gz

---
+++ packages/zdict/zdict-2.0.0.tar.gz/zdict-2.0.0/zdict/dictionaries/urban.py
@@ -0,0 +1,58 @@
+import json
+
+from zdict.dictionary import DictBase
+from zdict.exceptions import NotFoundError
+from zdict.models import Record
+
+
+class UrbanDict(DictBase):
+
+    API = 'http://api.urbandictionary.com/v0/define?term={word}'
+
+    @property
+    def provider(self):
+        return 'urban'
+
+    @property
+    def title(self):
+        return 'Urban Dictionary'
+
+    def _get_url(self, word) -> str:
+        return self.API.format(word=word)
+
+    def show(self, record: Record):
+        content = json.loads(record.content)
+
+        data = content['list'][0]
+
+        # print word
+        self.color.print(data.get('word', ''), 'yellow')
+
+        self.color.print(
+            data.get('definition', ''),
+            'org',
+            indent=2,
+        )
+
+        for example in data.get('example', '').split('\n'):
+            self.color.print(
+                example,
+                'indigo',
+                indent=2,
+            )
+
+        print()
+
+    def query(self, word: str):
+        content = self._get_raw(word)
+
+        if "no_results" in content:
+            raise NotFoundError(word)
+
+        record = Record(
+            word=word,
+            content=content,
+            source=self.provider,
+        )
+
+        return record
