7bb457b3e512568780dada8bb630546918c508d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:11 +0000

    Add package nlp-pie/0.2.2/nlp_pie-0.2.2-py2.py3-none-any.whl

---
+++ packages/nlp-pie/nlp_pie-0.2.2-py2.py3-none-any.whl/test/model.py
@@ -0,0 +1,41 @@
+
+import uuid
+import torch
+import os
+import unittest
+
+from pie.models import SimpleModel
+from pie.data import MultiLabelEncoder, Reader, Dataset
+from pie.settings import settings_from_file
+
+
+testpath = os.path.join(os.path.dirname(__file__), 'testconfig.json')
+settings = settings_from_file(testpath)
+label_encoder = MultiLabelEncoder.from_settings(settings)
+reader = Reader(settings, settings.input_path)
+label_encoder.fit_reader(reader)
+dataset = Dataset(settings, label_encoder, reader)
+
+
+class TestModelSerialization(unittest.TestCase):
+    def setUp(self):
+        emb_dim, hidden_size, num_layers = 64, 100, 1
+        self.model = SimpleModel(
+            label_encoder, settings.tasks,
+            emb_dim, emb_dim, hidden_size, num_layers)
+
+    def test_serialization(self):
+        model = self.model
+        fid = '/tmp/{}'.format(str(uuid.uuid1()))
+        model.save(fid)
+        model2 = SimpleModel.load(fid)
+        os.remove('{}.tar'.format(fid))
+        self.assertEqual(model.label_encoder, model2.label_encoder)
+
+        m1, m2 = dict(model.named_modules()), dict(model2.named_modules())
+
+        for m in m1:
+            if m != '':         # skip parent
+                self.assertTrue(m in m2)
+                for p1, p2 in zip(m1[m].parameters(), m2[m].parameters()):
+                    self.assertTrue(torch.allclose(p1, p2))
