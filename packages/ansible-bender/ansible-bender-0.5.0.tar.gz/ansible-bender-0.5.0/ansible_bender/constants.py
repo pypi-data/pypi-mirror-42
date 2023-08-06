f281a730b66fc56f3003bb1b6b83c9a4e7dec613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:29 +0000

    Add package ansible-bender/0.5.0/ansible-bender-0.5.0.tar.gz

---
+++ packages/ansible-bender/ansible-bender-0.5.0.tar.gz/ansible-bender-0.5.0/ansible_bender/constants.py
@@ -0,0 +1,7 @@
+OUT_LOGGER = "ab-out"
+OUT_LOGGER_FORMAT = "%(message)s"
+TIMESTAMP_FORMAT = "%Y%m%d-%H%M%S%f"
+NO_CACHE_TAG = "no-cache"
+
+# configuration related constants
+ANNOTATIONS_KEY = "annotations"
