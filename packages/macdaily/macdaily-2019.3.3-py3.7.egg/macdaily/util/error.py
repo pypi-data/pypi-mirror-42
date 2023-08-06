a0c6f10acd62651d4ade165d7aa0a0666da85d7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:19 +0000

    Add package macdaily/2019.3.3/macdaily-2019.3.3-py3.7.egg

---
+++ packages/macdaily/macdaily-2019.3.3-py3.7.egg/macdaily/util/error.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""refined errors"""
+
+import sys
+
+
+class Error(Exception):
+    def __init__(self, *args, **kwargs):
+        # sys.tracebacklimit = 0
+        super().__init__(*args, **kwargs)
+
+
+class ChildExit(Error, ChildProcessError):
+    pass
+
+
+class CommandNotImplemented(Error, NotImplementedError):
+    pass
+
+
+class ConfigNotFoundError(Error, FileNotFoundError):
+    pass
+
+
+class TimeExpired(Error, TimeoutError):
+    pass
+
+
+class UnsupportedOS(Error, RuntimeError):
+    pass
