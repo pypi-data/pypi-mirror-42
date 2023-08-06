a88ba79edf92adb437aa4df09177677bbdeead2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:17 +0000

    Add package deepoc/1.1.1/deepoc-1.1.1-py3-none-any.whl

---
+++ packages/deepoc/deepoc-1.1.1-py3-none-any.whl/tests/test_prediction.py
@@ -0,0 +1,31 @@
+import csv
+import logging
+import os
+
+from deepoc import DeepOCClassifier
+
+logging.basicConfig(level=logging.INFO)
+logging.getLogger("tensorflow").setLevel(logging.ERROR)
+logger = logging.getLogger(__name__)
+
+train_file = os.path.join('dataset', 'training.csv')
+test_file = os.path.join('dataset', 'testing.csv')
+val_file = os.path.join('dataset', 'validating.csv')
+class_file = os.path.join('dataset', 'classes.txt')
+workspace = 'model'
+
+
+with open(class_file) as f:
+    content = f.readlines()
+
+classes = [x.strip() for x in content]
+classifier = DeepOCClassifier(workspace, 'GD', [120, 60], 0.0001, train_file, test_file, classes)
+
+with open(val_file) as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for row in csv_reader:
+        predict_result = classifier.predict(row)
+        logger.info('Model %s: %s', row['model'], predict_result)
+
+
+
