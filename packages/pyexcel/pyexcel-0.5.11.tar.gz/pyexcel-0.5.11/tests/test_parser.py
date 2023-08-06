3362567022154f662cf11ada3fece3ceec8bca4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:17 +0000

    Add package pyexcel/0.5.11/pyexcel-0.5.11.tar.gz

---
+++ packages/pyexcel/pyexcel-0.5.11.tar.gz/pyexcel-0.5.11/tests/test_parser.py
@@ -0,0 +1,38 @@
+from nose.tools import raises
+from pyexcel.parser import DbParser, AbstractParser
+
+
+@raises(NotImplementedError)
+def test_default_parser():
+    parser = AbstractParser("xls")
+    parser.parse_file("afile.xls")
+
+
+@raises(NotImplementedError)
+def test_default_parser_2():
+    parser = AbstractParser("xls")
+    parser.parse_file_stream("afile xls stream")
+
+
+@raises(NotImplementedError)
+def test_default_parser_3():
+    parser = AbstractParser("xls")
+    parser.parse_file_content("afile content")
+
+
+@raises(Exception)
+def test_dbparser():
+    parser = DbParser("sql")
+    parser.parse_file("some name")
+
+
+@raises(Exception)
+def test_dbparser_1():
+    parser = DbParser("sql")
+    parser.parse_file_content("some content")
+
+
+@raises(NotImplementedError)
+def test_dbparser_2():
+    parser = DbParser("sql")
+    parser.parse_db("some name")
