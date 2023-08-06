67c43a04eb56901381740a0e556ea6267ae85ddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:01 +0000

    Add package pdbtest/1.2/pdbtest-1.2.tar.gz

---
+++ packages/pdbtest/pdbtest-1.2.tar.gz/pdbtest-1.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+    name='pdbtest',
+    packages=['pdbtest'],
+    version='1.2',
+    description='A unittest shim to make PDB debugging of test cases easy.',
+    author='Zachary J. McCord',
+    author_email='zjmccord@gmail.com',
+    url='https://github.com/zack112358/pdbtest/',
+    keywords=['testing', 'pdb', 'unittest'],
+    classifiers=[],
+)
