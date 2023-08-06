724b384aa447d61fe83cd3f4990a8c1d23911f9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:14 +0000

    Add package fireant/2.1.2/fireant-2.1.2-py2.py3-none-any.whl

---
+++ packages/fireant/fireant-2.1.2-py2.py3-none-any.whl/fireant/__init__.py
@@ -0,0 +1,8 @@
+# noinspection PyUnresolvedReferences
+from .database import *
+# noinspection PyUnresolvedReferences
+from .slicer import *
+# noinspection PyUnresolvedReferences
+from .slicer.widgets import *
+
+__version__ = '2.1.2'
