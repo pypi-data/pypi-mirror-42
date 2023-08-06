e887c05f195a0191eaf33fbd0518dfccd2a5c791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:18 +0000

    Add package simplecharts/0.0.1/simplecharts-0.0.1.tar.gz

---
+++ packages/simplecharts/simplecharts-0.0.1.tar.gz/simplecharts-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='simplecharts',
+    description='SVG charts without dependencies',
+    version='0.0.1',
+    py_modules=['simplecharts'],
+    url='https://github.com/xi/simplecharts',
+    license='GPLv3+',
+    classifiers=[
+        'Topic :: Scientific/Engineering :: Visualization',
+    ]
+)
