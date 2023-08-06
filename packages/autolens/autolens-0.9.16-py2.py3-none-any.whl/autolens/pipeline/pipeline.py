3b9ffc6b1ff263a663618068ba50bd33a93e6c85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:55 +0000

    Add package autolens/0.9.16/autolens-0.9.16-py2.py3-none-any.whl

---
+++ packages/autolens/autolens-0.9.16-py2.py3-none-any.whl/autolens/pipeline/pipeline.py
@@ -0,0 +1,65 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class Pipeline(object):
+
+    def __init__(self, pipeline_name, *phases):
+        """
+
+        Parameters
+        ----------
+        pipeline_name: str
+            The phase_name of this pipeline
+        """
+        self.pipeline_name = pipeline_name
+        self.phases = phases
+
+    def __add__(self, other):
+        """
+        Compose two runners
+
+        Parameters
+        ----------
+        other: Pipeline
+            Another pipeline
+
+        Returns
+        -------
+        composed_pipeline: Pipeline
+            A pipeline that runs all the  phases from this pipeline and then all the phases from the other pipeline
+        """
+        return self.__class__("{} + {}".format(self.pipeline_name, other.pipeline_name), *(self.phases + other.phases))
+
+
+class PipelineImaging(Pipeline):
+
+    def __init__(self, pipeline_name, *phases):
+        super(PipelineImaging, self).__init__(pipeline_name, *phases)
+
+    def run(self, data, mask=None, positions=None):
+
+        from autolens.pipeline import phase as ph
+        results = []
+        for i, phase in enumerate(self.phases):
+            logger.info("Running Phase {} (Number {})".format(phase.optimizer.name, i))
+            results.append(phase.run(data=data, previous_results=ph.ResultsCollection(results), mask=mask,
+                                     positions=positions))
+        return results
+
+
+class PipelinePositions(Pipeline):
+
+    def __init__(self, pipeline_name, *phases):
+        super(PipelinePositions, self).__init__(pipeline_name, *phases)
+
+    def run(self, positions, pixel_scale):
+        from autolens.pipeline import phase as ph
+
+        results = []
+        for i, phase in enumerate(self.phases):
+            logger.info("Running Phase {} (Number {})".format(phase.optimizer.name, i))
+            results.append(phase.run(positions=positions, pixel_scale=pixel_scale,
+                                     previous_results=ph.ResultsCollection(results)))
+        return results
