90a7e4826ffb62fcf10830cf69307582a9d2840d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:30 +0000

    Add package exttr/2019.2.1/exttr-2019.2.1-py3-none-any.whl

---
+++ packages/exttr/exttr-2019.2.1-py3-none-any.whl/exttr/__init__.py
@@ -0,0 +1,38 @@
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
+import functools
+
+from exttr.core import (
+    AttrsCollisionError,
+    get,
+    get_all,
+    Keyword,
+    KeywordCollisionError,
+    Plugin,
+    Registry,
+    UnknownKeywordError,
+)
+
+registry = Registry()
+
+@functools.wraps(registry.create_attribute)
+def ib(*args, **kwargs):
+    return registry.create_attribute(*args, **kwargs)
+
+@functools.wraps(registry.register_keywords)
+def register_keywords(*args, **kwargs):
+    return registry.register_keywords(*args, **kwargs)
+
+__all__ = [
+    'AttrsCollisionError',
+    'ib',
+    'get',
+    'Keyword',
+    'KeywordCollisionError',
+    'Plugin',
+    'register_keywords',
+    'Registry',
+    'UnknownKeywordError',
+]
