e0c40379f1b8d510235dc7402b037b6867d525ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:48 +0000

    Add package opencv-draw-tools-fernaperg/0.1.6/opencv-draw-tools-fernaperg-0.1.6.tar.gz

---
+++ packages/opencv-draw-tools-fernaperg/opencv-draw-tools-fernaperg-0.1.6.tar.gz/opencv-draw-tools-fernaperg-0.1.6/opencv_draw_tools/__init__.py
@@ -0,0 +1,21 @@
+from opencv_draw_tools.SelectZone import *
+
+name = 'opencv_draw_tools'
+help = '''
+MIT License
+Copyright (c) 2019 Fernando Perez
+For more information visit: https://github.com/fernaper/opencv-draw-tools
+Also you can write complete_help'''
+__version__ =  '0.1.6'
+
+try:
+    complete_help = '''
+    {} - {}
+    {}
+    {}
+    '''.format(name, __version__, help, get_complete_help())
+except Exception as e:
+    complete_help = '''
+    {} - v{}
+    {}
+    '''.format(name, __version__, help)
