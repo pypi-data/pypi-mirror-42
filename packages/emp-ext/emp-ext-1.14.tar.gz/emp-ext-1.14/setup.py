10e68d996a7d140aac66bf87d9d04e88b74fa4c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:43 +0000

    Add package emp-ext/1.14/emp-ext-1.14.tar.gz

---
+++ packages/emp-ext/emp-ext-1.14.tar.gz/emp-ext-1.14/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='emp-ext',
+    version='1.14',
+    py_modules=['emp_wifi', 'emp_webide', 'emp_ide', 'emp_utils'],
+    author='singein',
+    author_email='singein@outlook.com',
+    url='http://emp.1zlab.com',
+    description='EMP(Easy MicroPython) is a upy module to make things Easy on MicroPython.'
+)
