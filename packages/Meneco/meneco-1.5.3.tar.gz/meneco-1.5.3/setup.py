dfffb150c7f051a9f7702f74ec8f41dd06c3ed32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:21 +0000

    Add package Meneco/1.5.3/meneco-1.5.3.tar.gz

---
+++ packages/Meneco/meneco-1.5.3.tar.gz/meneco-1.5.3/setup.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2014, Sven Thiele <sthiele78@gmail.com>
+#
+# This file is part of meneco.
+#
+# meneco is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# meneco is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with meneco.  If not, see <http://www.gnu.org/licenses/>.
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+  name             = 'meneco',
+  version          = '1.5.3',
+  url              = 'http://bioasp.github.io/meneco/',
+  license          = 'GPLv3+',
+  description      = 'Metabolic Network Completion. Compute minimal completions '
+                     'to your draft network with reactions from a repair network.',
+  long_description = open('README.md').read(),
+  long_description_content_type="text/markdown",
+  author           = 'Sven Thiele',
+  author_email     = 'sthiele78@gmail.com',
+  packages         = ['__meneco__'],
+  package_dir      = {'__meneco__' : 'src'},
+  package_data     = {'__meneco__' : ['encodings/*.lp']},
+  scripts          = ['meneco.py'],
+  install_requires = ['pyasp == 1.4.4']
+)
