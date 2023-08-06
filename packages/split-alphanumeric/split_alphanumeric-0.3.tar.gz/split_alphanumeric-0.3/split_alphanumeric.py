297ef76b6776efa438eefe5ec5e20e365626bb09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:52 +0000

    Add package split-alphanumeric/0.3/split_alphanumeric-0.3.tar.gz

---
+++ packages/split-alphanumeric/split_alphanumeric-0.3.tar.gz/split_alphanumeric-0.3/split_alphanumeric.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Split alphanumeric
+#
+# This module takes a string of letters and numbers and return a list of paired values that can by mapped to a dictionary.
+
+# In[1]:
+
+
+import re
+
+from money2float import money
+
+
+# In[14]:
+
+
+def append_count(terms):
+    seen_dict = {}
+    return_list = []
+    for term in terms:
+        if seen_dict.get(term, 0) > 0:
+            seen_dict[term] += 1
+            term = f"{term}{seen_dict[term]}"
+        else:
+            seen_dict[term] = 1
+        return_list.append(term)
+    return return_list
+
+
+# In[13]:
+
+
+def build_fee_group_list(fee_group, substitute_characters=[]):
+    extended_characters = [(" ", ""), (",", ""), ("$", ""), ("CR", "-")]
+    substitute_characters.extend(extended_characters)
+    for item in substitute_characters:
+        fee_group = fee_group.replace(*item)
+    letters_regex = re.compile("[A-Z]")
+    numbers_regex = re.compile("[0-9\.\-]+")
+    letters = letters_regex.findall(fee_group)
+    numbers = numbers_regex.findall(fee_group)
+    letters = append_count(letters)
+    return list(zip(letters, numbers))
+
+
+# In[10]:
+
+
+def map_fee_group_to_fields(fee_group_list, fee_mapping):
+    fees_dict = {}
+    for item in fee_group_list:
+        mapped_name = fee_mapping.get(item[0], None)
+        if mapped_name:
+            fees_dict[mapped_name] = item[1]  # + fees_dict[mapped_name]
+    return fees_dict
+
+
+# In[ ]:
