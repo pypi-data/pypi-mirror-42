e0382d9d97ea7c96cce1c66ce7e39ca060af9ba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:33 +0000

    Add package biblioteca-info/0.0.1/biblioteca_info-0.0.1-py3-none-any.whl

---
+++ packages/biblioteca-info/biblioteca_info-0.0.1-py3-none-any.whl/Biblioteca/pacote1.py
@@ -0,0 +1,5 @@
+def soma(vetor):
+    soma = 0
+    for i in vetor:
+        soma += i
+    return soma
---
+++ packages/biblioteca-info/biblioteca_info-0.0.1-py3-none-any.whl/Biblioteca/testa_pacotes.py
@@ -0,0 +1,6 @@
+from pacote1 import Pacote1
+
+pct1 = Pacote1()
+
+soma = pct1.soma(1,2)
+print(soma)
