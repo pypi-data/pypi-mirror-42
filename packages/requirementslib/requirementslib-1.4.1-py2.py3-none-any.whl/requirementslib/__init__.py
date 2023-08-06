b23c72753f748ae547166ca65271dbb302ec6230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:09 +0000

    Add package requirementslib/1.4.1/requirementslib-1.4.1-py2.py3-none-any.whl

---
+++ packages/requirementslib/requirementslib-1.4.1-py2.py3-none-any.whl/requirementslib/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding=utf-8 -*-
+from __future__ import absolute_import, print_function
+__version__ = '1.4.1'
+
+import logging
+import warnings
+from vistir.compat import ResourceWarning
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+warnings.filterwarnings("ignore", category=ResourceWarning)
+
+from .models.requirements import Requirement
+from .models.lockfile import Lockfile
+from .models.pipfile import Pipfile
+
+__all__ = ["Lockfile", "Pipfile", "Requirement"]
