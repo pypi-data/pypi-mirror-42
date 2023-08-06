7ff4e603d0705840a7c29ea594f8a31df26fd376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:35 +0000

    Add package mbuild/0.8.3/mbuild-0.8.3.tar.gz

---
+++ packages/mbuild/mbuild-0.8.3.tar.gz/mbuild-0.8.3/mbuild/__init__.py
@@ -0,0 +1,11 @@
+from mbuild.box import Box
+from mbuild.coarse_graining import coarse_grain
+from mbuild.coordinate_transform import *
+from mbuild.compound import *
+from mbuild.pattern import *
+from mbuild.packing import *
+from mbuild.port import Port
+from mbuild.recipes import *
+from mbuild.lattice import Lattice
+
+from mbuild.version import version
---
+++ packages/mbuild/mbuild-0.8.3.tar.gz/mbuild-0.8.3/mbuild/version.py
@@ -0,0 +1,7 @@
+
+# This file is generated in setup.py at build time.
+version = '0.8.3'
+short_version = '0.8.3'
+full_version = '0.8.3'
+git_revision = '66cb44dcf03285973fa43f68fb9faf46692fa273'
+release = True
