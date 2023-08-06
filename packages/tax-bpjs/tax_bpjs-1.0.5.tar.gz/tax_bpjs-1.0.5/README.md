4668f69d09960c3ef674bd9df6a4604d780ee43e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:58 +0000

    Add package tax-bpjs/1.0.5/tax_bpjs-1.0.5.tar.gz

---
+++ packages/tax-bpjs/tax_bpjs-1.0.5.tar.gz/tax_bpjs-1.0.5/README.md
@@ -0,0 +1,29 @@
+# tax_bpjs
+Easy Tax & BPJS Calculation according to Indonesia Govermennt Regulation using Python!:)
+
+Features:
+	- BPJS Calculation
+	- Tax Calculation
+
+## Installation
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install tax_bpjs.
+```bash
+pip install tax_bpjs
+```
+
+## Usage
+```python
+from tax_bpjs.bpjs import Bpjs
+from tax_bpjs.tax import Tax
+```
+
+## Quick Start
+check example.py
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ packages/tax-bpjs/tax_bpjs-1.0.5.tar.gz/tax_bpjs-1.0.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+from setuptools import find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='tax_bpjs',
+      version='1.0.5',
+      description='Easy Tax & BPJS Calculation using Python!',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/vousmeevoyez/tax_bpjs',
+      author='Kelvin Desman',
+      author_email='kelvindsmn@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False,
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+      install_requires=["python-dateutil"],
+      python_requires='>=3')
