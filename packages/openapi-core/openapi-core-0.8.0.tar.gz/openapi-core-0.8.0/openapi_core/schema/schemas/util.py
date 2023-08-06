16ca2eaa8528aea41bbac1f5bcca9ff9adfba143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:54 +0000

    Add package openapi-core/0.8.0/openapi-core-0.8.0.tar.gz

---
+++ packages/openapi-core/openapi-core-0.8.0.tar.gz/openapi-core-0.8.0/openapi_core/schema/schemas/util.py
@@ -0,0 +1,26 @@
+"""OpenAPI core schemas util module"""
+import datetime
+from distutils.util import strtobool
+from json import dumps
+from six import string_types
+import strict_rfc3339
+
+
+def forcebool(val):
+    if isinstance(val, string_types):
+        val = strtobool(val)
+
+    return bool(val)
+
+
+def dicthash(d):
+    return hash(dumps(d, sort_keys=True))
+
+
+def format_date(value):
+    return datetime.datetime.strptime(value, '%Y-%m-%d').date()
+
+
+def format_datetime(value):
+    timestamp = strict_rfc3339.rfc3339_to_timestamp(value)
+    return datetime.datetime.utcfromtimestamp(timestamp)
