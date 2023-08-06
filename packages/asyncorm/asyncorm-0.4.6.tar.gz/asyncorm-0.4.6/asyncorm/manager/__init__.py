ee799a5916179a49e266b8c30a13f1196c962b7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:30 +0000

    Add package asyncorm/0.4.6/asyncorm-0.4.6.tar.gz

---
+++ packages/asyncorm/asyncorm-0.4.6.tar.gz/asyncorm-0.4.6/asyncorm/manager/__init__.py
@@ -0,0 +1,3 @@
+from asyncorm.manager.managers import Queryset, ModelManager
+
+__all__ = ["Queryset", "ModelManager"]
---
+++ packages/asyncorm/asyncorm-0.4.6.tar.gz/asyncorm-0.4.6/asyncorm/serializers/__init__.py
@@ -0,0 +1,7 @@
+from asyncorm.serializers.serializer import (
+    ModelSerializerMeta,
+    SerializerMethod,
+    ModelSerializer,
+)
+
+__all__ = ("ModelSerializerMeta", "SerializerMethod", "ModelSerializer")
