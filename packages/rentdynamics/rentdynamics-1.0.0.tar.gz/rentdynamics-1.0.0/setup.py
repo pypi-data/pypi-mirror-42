684476abc685e0f1d4e6ba5a7a42c249aee3c6ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:52 +0000

    Add package rentdynamics/1.0.0/rentdynamics-1.0.0.tar.gz

---
+++ packages/rentdynamics/rentdynamics-1.0.0.tar.gz/rentdynamics-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='rentdynamics',
+    version='1.0.0',
+    description='Rent Dynamics Client Library',
+    author='Rent Dynamics',
+    author_email='dev-accounts@rentdynamics.com',
+    url='https://github.com/RentDynamics/rentdynamics-py',
+    packages=['rentdynamics']
+)
