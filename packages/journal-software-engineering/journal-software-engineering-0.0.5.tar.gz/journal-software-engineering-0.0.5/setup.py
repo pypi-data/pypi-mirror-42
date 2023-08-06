e3a2631ea27e22ae4bed272a9d7d55a6f2ce527b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:55 +0000

    Add package journal-software-engineering/0.0.5/journal-software-engineering-0.0.5.tar.gz

---
+++ packages/journal-software-engineering/journal-software-engineering-0.0.5.tar.gz/journal-software-engineering-0.0.5/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+import os
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+VERSION = os.getenv("TRAVIS_BRANCH") if os.getenv("TRAVIS_BRANCH") else "test"
+setup(
+    name="journal-software-engineering",
+    version=VERSION,
+    py_modules=["journal"],
+    package_dir={'': 'src'},
+    install_requires=['GitPython==2.1.11', 'colorama==0.4.1'],
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=['pytest'],
+    # metadata to display on PyPI
+    author="Sage Smith",
+    author_email="sagersmith8@gmail.com",
+    description="Provides tooling for scrum based engineering journal",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="Apache Commons 2.0",
+    url="http://github.com/sagersmith8/journal",
+    platforms=[2.7],
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ]
+)
